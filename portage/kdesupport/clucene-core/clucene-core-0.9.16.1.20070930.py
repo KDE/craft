3ededdd97b94bb5a6373ec56067bd7f3f1dd7a35
Patrick Spendrin <ps_ml@gmx.de>
2008-01-11 21:01:29 +0000

    change indentation standard and add function kdeSvnPath
    svn path=/trunk/kdesupport/kdewin32/emerge/; revision=760063

---
+++ portage/kdesupport/clucene-core/clucene-core-0.9.16.1.20070930.py
@@ -0,0 +1,56 @@
+import base
+import utils
+import shutil
+import os
+import sys
+
+SRC_URI= """
+http://garr.dl.sourceforge.net/sourceforge/clucene/clucene-core-0.9.16a.tar.bz2
+"""
+
+DEPEND = """
+virtual/base
+"""
+
+class subclass(base.baseclass):
+    def __init__(self):
+        base.baseclass.__init__( self, SRC_URI )
+        self.instsrcdir = os.path.join( "clucene-core-0.9.16a", "src" )
+
+    def unpack( self ):
+        if( not base.baseclass.unpack( self ) ):
+            return True
+
+        # we have an own cmake script - copy it to the right place
+        mydir = os.path.join( self.workdir, self.instsrcdir )
+        cmake_script = os.path.join( self.packagedir , "CMakeLists.txt" )
+        cmake_dest = os.path.join( mydir, "CMakeLists.txt" )
+        shutil.copy( cmake_script, cmake_dest )
+        cmake_script = os.path.join( self.packagedir , "clucene-config.h.cmake" )
+        cmake_dest = os.path.join( mydir, "Clucene", "clucene-config.h.cmake" )
+        shutil.copy( cmake_script, cmake_dest )
+
+        return True
+
+    def kdeSvnPath( self ):
+        return False
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        # FIXME?
+        if self.traditional:
+            self.instdestdir = "kde"
+            self.instsrcdir = "clucene"
+            return self.doPackaging( "clucene", "0.9.16a-1", True )
+        else:
+            self.instsrcdir = "clucene-core-0.9.16a"
+            return self.doPackaging( "clucene-core", os.path.basename(sys.argv[0]).replace("clucene-core-", "").replace(".py", ""), True )
+
+    
+if __name__ == '__main__':
+    subclass().execute()
