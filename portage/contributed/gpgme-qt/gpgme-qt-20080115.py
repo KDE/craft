b6cd9d2c18aae131a8eaf9d53ebf8cae864eeb32
Patrick Spendrin <ps_ml@gmx.de>
2008-01-15 22:49:00 +0000

    it checks out now
    svn path=/trunk/kdesupport/emerge/; revision=762034

---
+++ portage/contributed/gpgme-qt/gpgme-qt-20080115.py
@@ -0,0 +1,43 @@
+import base
+import utils
+import os
+import shutil
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = False
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, "" )
+    # cmake scripts are not in src root...
+    self.instsrcdir = "gpgme-qt"
+
+  def unpack( self ):
+    print "gpgme-qt unpack called"
+    # do the svn fetch/update
+    repo = "svn://cvs.gnupg.org/gpgme/trunk/gpgme"
+    self.svnFetch( repo )
+
+    return True
+
+  def compile( self ):
+    return self.kdeCompile()
+
+  def install( self ):
+    return self.kdeInstall()
+
+  def make_package( self ):
+
+    # now do packaging with kdewin-packager
+    self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
+
+    return True
+    
+if __name__ == '__main__':
+    subclass().execute()
