0b1379ab82a697b9833f1ec878c9d34db47bca9a
Patrick Spendrin <ps_ml@gmx.de>
2008-01-23 20:57:02 +0000

    change cmake version
    svn path=/trunk/kdesupport/emerge/; revision=765319

--- portage/dev-util/cmake/cmake-2.4.8.py
+++ portage/dev-util/cmake/cmake-2.4.8.py
@@ -1,18 +1,18 @@
 import base
 import info
 
-SRC_URI= "http://www.cmake.org/files/v2.4/cmake-2.4.7-win32-x86.zip"
+SRC_URI= "http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip"
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
         """ """
-        self.targets['2.4.7'] = 'http://www.cmake.org/files/v2.4/cmake-2.4.7-win32-x86.zip'
+        self.targets['2.4.7'] = 'http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip'
         self.defaultTarget = '2.4.7'
 
 class subclass(base.baseclass):
   def __init__(self):
     base.baseclass.__init__( self, SRC_URI )
-    self.instsrcdir = "cmake-2.4.7-win32-x86"
+    self.instsrcdir = "cmake-2.4.8-win32-x86"
     self.subinfo = subinfo()
     if self.traditional:
       self.instdestdir = "cmake"
