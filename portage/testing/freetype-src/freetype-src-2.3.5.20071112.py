89b1b652e5ae05f3ca271c0fff3c76e1392af9b7
Patrick Spendrin <ps_ml@gmx.de>
2008-01-26 23:46:13 +0000

    make buildsystem cmake based
    svn path=/trunk/kdesupport/emerge/; revision=766942

--- portage/testing/freetype-src/freetype-src-2.3.5.20071112.py
+++ portage/testing/freetype-src/freetype-src-2.3.5.20071112.py
@@ -16,7 +16,7 @@ http://download.savannah.gnu.org/releases/""" + PACKAGE_NAME + """/""" + PACKAGE
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['2.3.5-1'] = SRC_URI
+        self.targets['2.3.5-1'] = SRC_URI
         self.defaultTarget = '2.3.5-1'
     
     def setDependencies( self ):
@@ -29,43 +29,21 @@ class subclass(base.baseclass):
         self.createCombinedPackage = True
         self.subinfo = subinfo()
 
-    def execute( self ):
-        base.baseclass.execute( self )
-        if self.compiler <> "mingw":
-            print "error: can only be build with MinGW right now."
-            exit( 1 )
-
     def unpack( self ):
         if( not base.baseclass.unpack( self ) ):
             return False
+        cmd = "cd %s && patch -p0 < %s" % \
+              ( self.workdir, os.path.join( self.packagedir, "freetype.diff" ) )
+        utils.system( cmd ) or utils.die( "patch" )
         return True
-
-    def msysConfigureFlags ( self ):
-        flags = "CPPFLAGS=-I/d/cygopt/kde-root/include LDFLAGS=-L/d/cygopt/kde-root/lib --prefix=/ "
-        return flags
 
     def compile( self ):
-        return self.msysCompile( False )
+        return self.kdeCompile()
 
     def install( self ):
-        if not self.msysInstall( False ):
-            return False
-        src = os.path.join( self.imagedir, self.instdestdir, "bin", PACKAGE_DLL_NAME + ".dll" )
-        dst = os.path.join( self.imagedir, self.instdestdir, "bin", "lib" + PACKAGE_NAME + ".dll" )
-        shutil.copyfile( src, dst )
-        if os.path.exists( os.path.join( self.imagedir, self.instdestdir, "bin", PACKAGE_NAME + "-config" ) ):
-            os.remove(os.path.join( self.imagedir, self.instdestdir, "bin", PACKAGE_NAME + "-config" ) )
-        return True
+        return self.kdeInstall()
 
     def make_package( self ):
-        dst = os.path.join( self.imagedir, self.instdestdir, "lib" )
-        utils.cleanDirectory( dst )
-        src = os.path.join( self.imagedir, self.instdestdir, "bin", PACKAGE_DLL_NAME + ".dll" )
-        dst = os.path.join( self.imagedir, self.instdestdir, "bin", "lib" + PACKAGE_NAME + ".dll" )
-        shutil.copyfile( src, dst )
-        if os.path.exists( os.path.join( self.imagedir, self.instdestdir, "bin", PACKAGE_NAME + "-config" ) ):
-            os.remove( os.path.join( self.imagedir, self.instdestdir, "bin", PACKAGE_NAME + "-config" ) )
-
         #self.stripLibs( PACKAGE_DLL_NAME )
         #self.createImportLibs( "lib" + PACKAGE_NAME )
         # now do packaging with kdewin-packager
