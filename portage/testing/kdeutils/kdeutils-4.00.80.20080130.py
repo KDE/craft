c56fdd384db144162eb59fa5e15fbc67f56db2b9
Patrick Spendrin <ps_ml@gmx.de>
2008-01-30 18:48:13 +0000

    fulfilling the request from Dakon, here is the new kdeutils script
    svn path=/trunk/kdesupport/emerge/; revision=768829

---
+++ portage/testing/kdeutils/kdeutils-4.00.80.20080130.py
@@ -0,0 +1,36 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdeutils'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        self.hardDependencies['kde/kdepimlibs'] = 'default'
+        self.hardDependencies['kde/kdebase'] = 'default'
+    
+class subclass(base.baseclass):
+    def __init__(self):
+        self.buildType="Debug"
+        base.baseclass.__init__( self, "" )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "kdeutils", os.path.basename(sys.argv[0]).replace("kdeutils-", "").replace(".py", ""), True )
+
+if __name__ == '__main__':
+    subclass().execute()
