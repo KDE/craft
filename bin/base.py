6e87548b1685cdfa61f4ddcfe8adc91964118eae
Patrick Spendrin <ps_ml@gmx.de>
2008-02-02 10:38:58 +0000

    add some lines to make gpgme-qt work again
    svn path=/trunk/kdesupport/emerge/; revision=769879

--- bin/base.py
+++ bin/base.py
@@ -300,7 +300,8 @@ class baseclass:
         if self.subinfo.buildTarget in self.subinfo.targets.keys() and self.subinfo.buildTarget in self.subinfo.targetInstSrc.keys():
             self.instsrcdir = self.subinfo.targetInstSrc[ self.subinfo.buildTarget ]
         else:
-            self.instsrcdir = os.path.basename( self.Targets[ self.subinfo.buildTarget ] )
+            if self.subinfo.buildTarget in self.Targets.keys() and self.Targets[ self.subinfo.buildTarget ]:
+                self.instsrcdir = os.path.basename( self.Targets[ self.subinfo.buildTarget ] )
             
         self.msys.setDirectories( self.rootdir, self.imagedir, self.workdir, self.instsrcdir, self.instdestdir )
         self.kde.setDirectories( self.rootdir, self.imagedir, self.workdir, self.instsrcdir, self.instdestdir, self.subinfo )
