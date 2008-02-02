d5e2d537c328c6df01de00db6ea421f2836fd305
Patrick Spendrin <ps_ml@gmx.de>
2008-02-02 13:00:38 +0000

    use target specific instsrcdir
    svn path=/trunk/kdesupport/emerge/; revision=769934

--- portage/dev-util/perl/perl-5.8.8.822.py
+++ portage/dev-util/perl/perl-5.8.8.822.py
@@ -6,12 +6,12 @@ SRC_URI= "http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['5.8.8'] = SRC_URI
+        self.targetInstSrc['5.8.8'] = "ActivePerl-5.8.8.822-MSWin32-x86-280952\\perl"
         self.defaultTarget = '5.8.8'
         
 class subclass(base.baseclass):
   def __init__(self):
     base.baseclass.__init__( self, SRC_URI )
-    self.instsrcdir = "ActivePerl-5.8.8.822-MSWin32-x86-280952\\perl"
     self.subinfo = subinfo()
     if self.traditional:
         self.instdestdir = "perl"
