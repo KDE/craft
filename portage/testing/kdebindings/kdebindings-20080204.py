71c10d3df98ccdd444bec5db31692898235b4978
Patrick Spendrin <ps_ml@gmx.de>
2008-02-05 02:14:27 +0000

    adding kdebindings - but only krosspython is enabled for now. PyKDE4 still needs a lot of love - it seems there hasn't yet been one attempt on Win32
    svn path=/trunk/kdesupport/emerge/; revision=771039

---
+++ portage/testing/kdebindings/kdebindings-20080204.py
@@ -0,0 +1,49 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdebindings'
+        self.svnTargets['4.0.0'] = 'tags/KDE/4.0.0/kdebindings'
+        self.targets['4.0.60'] = 'ftp://ftp.rz.uni-wuerzburg.de/pub/unix/kde/unstable/4.0.60/src/kdebindings-4.0.60.tar.bz2'
+        self.targetInstSrc['4.0.60'] = 'kdebindings-4.0.60'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['testing/pyqt'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__(self):
+        base.baseclass.__init__( self, "" )
+        self.subinfo = subinfo()
+        self.kdeCustomDefines = ""
+        self.kdeCustomDefines += "-DBUILD_ruby=OFF "
+#        self.kdeCustomDefines += "-DBUILD_python=OFF "
+        self.kdeCustomDefines += "-DENABLE_PYKDE4=OFF "
+        self.kdeCustomDefines += "-DBUILD_kalyptus=OFF "
+        self.kdeCustomDefines += "-DBUILD_csharp=OFF "
+        self.kdeCustomDefines += "-DBUILD_smoke=OFF "
+        self.kdeCustomDefines += "-DBUILD_xparts=OFF "
+        
+        
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdebindings", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdebindings", os.path.basename(sys.argv[0]).replace("kdebindings-", "").replace(".py", ""), True )
+
+if __name__ == '__main__':
+    subclass().execute()
