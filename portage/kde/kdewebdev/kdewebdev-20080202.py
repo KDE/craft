0697339f8dc648ac086d3af19c7d9553cccdebe8
Paulo Moura Guedes <moura@kdewebdev.org>
2008-02-07 17:12:47 +0000

    Add kdewebdev module
    svn path=/trunk/kdesupport/emerge/; revision=772054

---
+++ portage/kde/kdewebdev/kdewebdev-20080202.py
@@ -0,0 +1,44 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['4.0.0'] = 'tags/KDE/4.0.0/kdewebdev'
+        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdewebdev'
+        self.targets['4.0.60'] = 'ftp://ftp.rz.uni-wuerzburg.de/pub/unix/kde/unstable/4.0.60/src/kdewebdev-4.0.60.tar.bz2'
+        self.targetInstSrc['4.0.60'] = 'kdewebdev-4.0.60'        
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self ):
+        base.baseclass.__init__( self, "" )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_quanta=OFF "
+        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kfilereplace=OFF "
+        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kxsldbg=OFF "
+
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdewebdev", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdewebdev", os.path.basename(sys.argv[0]).replace("kdewebdev-", "").replace(".py", ""), True )
+
+
+if __name__ == '__main__':
+    subclass().execute()
