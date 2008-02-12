a63ef4377399c1c6a7b4c9f93c81a95044d7c79a
Shane King <kde@dontletsstart.com>
2008-02-12 23:57:42 +0000

    Remove another kdereview package from the build for this. Rename package to match others in only having a date, no version number.
    svn path=/trunk/kdesupport/emerge/; revision=774317

---
+++ portage/extragear/phonon/phonon-20080213.py
@@ -0,0 +1,34 @@
+import base
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/kdereview'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+    
+class subclass(base.baseclass):
+    def __init__( self ):
+        base.baseclass.__init__( self, "" )
+        self.instsrcdir = "phonon"
+        self.kdeCustomDefines = "-DBUILD_binary-clock=OFF -DBUILD_fuzzy-clock=OFF -DBUILD_ksystemlog=OFF -DBUILD_kollision=OFF -DBUILD_decibel=OFF"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "phonon", os.path.basename(sys.argv[0]).replace("phonon-", "").replace(".py", ""), True )
+
+if __name__ == '__main__':		
+    subclass().execute()
