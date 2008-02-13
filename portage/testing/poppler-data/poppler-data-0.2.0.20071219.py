8cf2d3372ae59dc0eaf51fcfc24d131d731c022d
Patrick Spendrin <ps_ml@gmx.de>
2008-02-13 01:42:00 +0000

    make this package work again
    svn path=/trunk/kdesupport/emerge/; revision=774334

--- portage/testing/poppler-data/poppler-data-0.2.0.20071219.py
+++ portage/testing/poppler-data/poppler-data-0.2.0.20071219.py
@@ -1,7 +1,7 @@
 import base
 import os
 import utils
-from utils import die
+import info
 
 PACKAGE_NAME         = "poppler-data"
 PACKAGE_VER          = "0.2.0"
@@ -18,11 +18,17 @@ gnuwin32/patch
 ##http://poppler.freedesktop.org/""" + PACKAGE_FULL_NAME + """.tar.gz
 ##"""
 
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['0.2.0'] = 'http://poppler.freedesktop.org/poppler-data-0.2.0.tar.gz'
+        self.targetInstSrc['0.2.0'] = 'poppler-data-0.2.0'
+        self.defaultTarget = '0.2.0'
+
 class subclass(base.baseclass):
     def __init__( self ):
-        base.baseclass.__init__( self, SRC_URI )
+        base.baseclass.__init__( self, "" )
         self.instsrcdir = PACKAGE_FULL_NAME
-        self.createCombinedPackage = True
+        self.subinfo = subinfo()
 
     def unpack( self ):
         if( not base.baseclass.unpack( self ) ):
@@ -34,7 +40,7 @@ class subclass(base.baseclass):
               ( self.workdir, os.path.join( self.packagedir , "poppler-data-cmake.patch" ) )
         if utils.verbose() >= 1:
             print cmd
-        self.system( cmd ) or die( "patch" )
+        self.system( cmd ) or utils.die( "patch" )
 
         return True
         
