63dea2286f29e4149b2cfc09ec74a731892c6cb1
Patrick Spendrin <ps_ml@gmx.de>
2008-02-20 22:46:26 +0000

    koffice does not depend on the whole kdebase only on the runtime part
    svn path=/trunk/kdesupport/emerge/; revision=777576

--- portage/testing/koffice/koffice-1.97.20080113.py
+++ portage/testing/koffice/koffice-1.97.20080113.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.hardDependencies['win32libs-sources/lcms-src'] = 'default'
         self.hardDependencies['kde/kdelibs'] = 'default'
-        self.hardDependencies['kde/kdebase'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
 
         self.softDependencies['kdesupport/eigen'] = 'default'
         self.softDependencies['kdesupport/qca'] = 'default'
