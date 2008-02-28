2fcf8734b1214adbfd12f23ecadf0f7820535c37
Jeff Mitchell <mitchell@kde.org>
2008-02-28 20:12:15 +0000

    Initial kdemultimedia ebuild for testing
    svn path=/trunk/kdesupport/emerge/; revision=780330

---
+++ portage/testing/kdemultimedia/kdemultimedia-20080202.py
@@ -0,0 +1,40 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdemultimedia'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        self.hardDependencies['kdesupport/taglib'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self ):
+        base.baseclass.__init__( self, "" )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        path = os.path.join( self.rootdir, "win32libs" )
+        
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdemultimedia", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdemultimedia", os.path.basename(sys.argv[0]).replace("kdepimlibs-", "").replace(".py", ""), True )
+
+
+if __name__ == '__main__':
+    subclass().execute()
