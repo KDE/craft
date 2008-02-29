96d8a14ffa11a0fc5b95156bd4134a92d58198de
Patrick Spendrin <ps_ml@gmx.de>
2008-02-29 00:31:49 +0000

    make kdebase a virtual package - it should reside in kde for historical reasons though
    svn path=/trunk/kdesupport/emerge/; revision=780407

--- portage/kde/kdebase/kdebase-20080202.py
+++ portage/kde/kdebase/kdebase-20080202.py
@@ -1,38 +1,16 @@
 import base
-import os
-import sys
 import info
 
 class subinfo(info.infoclass):
-    def setTargets( self ):
-        self.svnTargets['4.0.0'] = 'tags/KDE/4.0.0/kdebase'
-        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdebase'
-        self.defaultTarget = 'svnHEAD'
-    
     def setDependencies( self ):
         self.hardDependencies['kde/kdelibs'] = 'default'
-        self.hardDependencies['kde/kdepimlibs'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['kde/kdebase-apps'] = 'default'
     
 class subclass(base.baseclass):
     def __init__( self ):
         base.baseclass.__init__( self, "" )
         self.subinfo = subinfo()
 
-    def unpack( self ):
-        return self.kdeSvnUnpack()
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        if not self.buildTarget == 'svnHEAD':
-            return self.doPackaging( "kdebase", self.buildTarget, True )
-        else:
-            return self.doPackaging( "kdebase", os.path.basename(sys.argv[0]).replace("kdebase-", "").replace(".py", ""), True )
-
-		
 if __name__ == '__main__':
     subclass().execute()
