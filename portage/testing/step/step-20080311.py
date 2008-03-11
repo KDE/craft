09c5ea832d5b5f3b0b28d0eb58b673160b0c7c6a
Patrick Spendrin <ps_ml@gmx.de>
2008-03-11 16:07:11 +0000

    step goes to kdereview now and will be moved to kdeedu as soon as possible I hope
    svn path=/trunk/kdesupport/emerge/; revision=784533

---
+++ portage/testing/step/step-20080311.py
@@ -0,0 +1,33 @@
+import base
+import os
+import utils
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdereview/step'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['libs/qt'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self ):
+        base.baseclass.__init__( self, "" )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "step", "20080311", True )
+		
+if __name__ == '__main__':
+    subclass().execute()
