609f57c5718211975fc8acae76cc7d24836d6da5
Patrick Spendrin <ps_ml@gmx.de>
2008-03-26 23:54:13 +0000

    add buildscripts for playground - here you can find keduca and kverbos and some more stuff
    svn path=/trunk/kdesupport/emerge/; revision=790592

---
+++ portage/testing/kdeedu-playground/kdeedu-playground-20080326.py
@@ -0,0 +1,35 @@
+import base
+import os
+import utils
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/playground/edu'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['libs/qt'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['kde/kdeedu'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self ):
+        base.baseclass.__init__( self, "" )
+        self.subinfo = subinfo()
+        self.instsrcdir = "edu"
+        
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "kdeedu-playground", "20080326", True )
+		
+if __name__ == '__main__':
+    subclass().execute()
