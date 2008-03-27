c735c54a272be9c6bde8ea4bb221f1bc2f0d9791
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2008-03-27 18:42:34 +0000

    2.6.0-rc5 is out
    svn path=/trunk/kdesupport/emerge/; revision=790877

--- portage/dev-util/cmake/cmake-2.4.8.py
+++ portage/dev-util/cmake/cmake-2.4.8.py
@@ -1,21 +1,17 @@
 import base
 import info
 
-SRC_URI= "http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip"
-
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        """ """
         self.targets['2.4.8'] = 'http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip'
-        self.targets['2.5.0'] = 'http://www.cmake.org/files/vCVS/cmake-2.5.20080311-win32-x86.zip'
+        self.targets['2.6.0'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.0-RC-5-win32-x86.zip'
         self.targetInstSrc['2.4.8'] = 'cmake-2.4.8-win32-x86'
-        self.targetInstSrc['2.5.0'] = 'cmake-2.5.20080311-win32-x86'
+        self.targetInstSrc['2.6.0'] = 'cmake-2.6.0-RC-5-win32-x86'
         self.defaultTarget = '2.4.8'
 
 class subclass(base.baseclass):
   def __init__(self):
-    base.baseclass.__init__( self, SRC_URI )
-    self.instsrcdir = "cmake-2.4.8-win32-x86"
+    base.baseclass.__init__( self, "" )
     self.subinfo = subinfo()
     if self.traditional:
       self.instdestdir = "cmake"
