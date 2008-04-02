b953802525ea02c1dcca7f5b0448c0778e4fb2f0
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2008-04-02 18:33:10 +0000

    forgot to change this
    svn path=/trunk/kdesupport/emerge/; revision=792976

--- portage/dev-util/cmake/cmake-2.4.8.py
+++ portage/dev-util/cmake/cmake-2.4.8.py
@@ -4,7 +4,7 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['2.4.8'] = 'http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip'
-        self.targets['2.6.0'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.0-RC-5-win32-x86.zip'
+        self.targets['2.6.0'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.0-RC-6-win32-x86.zip'
         self.targetInstSrc['2.4.8'] = 'cmake-2.4.8-win32-x86'
         self.targetInstSrc['2.6.0'] = 'cmake-2.6.0-RC-6-win32-x86'
         self.defaultTarget = '2.4.8'
