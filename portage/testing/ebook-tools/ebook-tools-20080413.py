ef9aca880ba4386753a5924494df73da78c99f82
Patrick Spendrin <ps_ml@gmx.de>
2008-04-13 16:56:50 +0000

    epub - another plugin for okular
    svn path=/trunk/kdesupport/emerge/; revision=796480

---
+++ portage/testing/ebook-tools/ebook-tools-20080413.py
@@ -0,0 +1,30 @@
+import base
+import utils
+import os
+import shutil
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['0.1.0'] = 'http://downloads.sourceforge.net/ebook-tools/ebook-tools-0.1.0.tar.gz'
+        self.targetInstSrc['0.1.0'] = 'ebook-tools-0.1.0'
+        self.defaultTarget = '0.1.0'
+    
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['testing/libzip-src'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__(self):
+        base.baseclass.__init__( self, "" )
+        self.subinfo = subinfo()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+if __name__ == '__main__':
+    subclass().execute()
