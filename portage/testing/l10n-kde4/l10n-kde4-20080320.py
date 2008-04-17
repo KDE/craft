d398215848a23e66c022f010b8730fe78e4425db
Patrick Spendrin <ps_ml@gmx.de>
2008-04-17 16:05:12 +0000

    fix dependency msys
    svn path=/trunk/kdesupport/emerge/; revision=798138

--- portage/testing/l10n-kde4/l10n-kde4-20080320.py
+++ portage/testing/l10n-kde4/l10n-kde4-20080320.py
@@ -12,6 +12,7 @@ class subinfo(info.infoclass):
     
     def setDependencies( self ):
         self.hardDependencies['dev-util/cmake'] = 'default'
+        self.hardDependencies['dev-util/msys'] = 'default'
         self.hardDependencies['testing/gettext-tools'] = 'default'
         
 class subclass(base.baseclass):
