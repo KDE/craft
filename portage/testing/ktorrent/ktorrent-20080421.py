8797ef776ce305f89901c4dbc09877635a00030b
Carlo Segato <brandon.ml@gmail.com>
2008-04-21 13:45:28 +0000

    add ktorrent and fix some errors in libgmp
    svn path=/trunk/kdesupport/emerge/; revision=799406

---
+++ portage/testing/ktorrent/ktorrent-20080421.py
@@ -0,0 +1,35 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'branches/ktorrent/windows_port'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        self.hardDependencies['testing/libgmp-src'] = 'default'
+    
+class subclass(base.baseclass):
+    def __init__(self):
+        self.buildType = "Debug"
+        base.baseclass.__init__( self, "" )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "ktorrent", self.buildTarget, True )
+
+if __name__ == '__main__':
+    subclass().execute()
--- portage/testing/libgmp-src/libgmp-src-4.1.py
+++ portage/testing/libgmp-src/libgmp-src-4.1.py
@@ -8,41 +8,45 @@ import shutil
 class subinfo(info.infoclass):
 
     def setTargets( self ):
-        if os.getenv("KDECOMPILER") == "mingw":
-            self.targets['4.1'] = "http://www.cs.nyu.edu/exact/core/gmp/gmp-static-mingw-4.1.tar.gz"
-        else:
-            self.targets['4.1'] = "http://www.cs.nyu.edu/exact/core/gmp/gmp-static-vc-4.1.2.zip"
+        self.targets['4.1'] = "http://www.cs.nyu.edu/exact/core/gmp/gmp-4.1.tar.gz"
+        self.targetInstSrc['4.1'] = "gmp-4.1"
         self.defaultTarget = '4.1'
 
 class subclass(base.baseclass):
     def __init__(self):
         base.baseclass.__init__( self, "" )
         self.subinfo = subinfo()
+        self.buildType = "Release"
 
-    def unpack( self ):
-        base.baseclass.unpack( self )
+    def unpack( self ):    
+        if( not base.baseclass.unpack( self ) ):
+            return False
+        # we have an own cmake script - copy it to the right place
+        cmake_script = os.path.join( self.packagedir , "CMakeLists.txt" )
+        cmake_dest = os.path.join( self.workdir, self.instsrcdir, "CMakeLists.txt" )
+        shutil.copy( cmake_script, cmake_dest )
+        
+        config_src = os.path.join( self.packagedir , "config.in" )
+        config_dest = os.path.join( self.workdir, self.instsrcdir, "config.in" )
+        shutil.copy( config_src, config_dest )
+
+        fl  = os.path.join( self.workdir, self.instsrcdir, "mpn", "generic", "gmp-mparam.h")
+        
+        os.remove(fl)
+
         return True
+        
+    def install( self ):
+        return self.kdeInstall()
+    
+    def compile( self ):
+        return self.kdeCompile()
 
     def install( self ):
-        if os.getenv("KDECOMPILER") == "mingw":
-            utils.copySrcDirToDestDir( os.path.join(self.workdir, "gmp"), self.imagedir )
-        else:
-            dst = os.path.join( self.imagedir, "include" )
-            utils.cleanDirectory( dst )
-            dst = os.path.join( self.imagedir, "lib" )
-            utils.cleanDirectory( dst )
-
-            src = os.path.join( self.workdir, "gmp-static", "gmp.lib" )
-            dst = os.path.join( self.imagedir, "lib", "gmp.lib" )
-            shutil.copy( src, dst )
-            src = os.path.join( self.workdir, "gmp-static", "gmpDebug.lib" )
-            dst = os.path.join( self.imagedir, "lib", "gmpDebug.lib" )
-            shutil.copy( src, dst )
-            src = os.path.join( self.workdir, "gmp-static", "gmp.h" )
-            dst = os.path.join( self.imagedir, "include", "gmp.h" )
-            shutil.copy( src, dst )
+        return self.kdeInstall()
+    
+    def make_package( self ):
+        self.instsrcdir = ""
         
-        return True
-
 if __name__ == '__main__':
     subclass().execute()
