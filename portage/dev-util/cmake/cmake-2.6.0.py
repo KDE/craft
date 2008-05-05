78579d6c1dd4864dd94c1093b1cdc4b76f286828
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2008-05-05 17:26:06 +0000

    update to 2.6.0-rc10
    svn path=/trunk/kdesupport/emerge/; revision=804309

--- portage/dev-util/cmake/cmake-2.6.0.py
+++ portage/dev-util/cmake/cmake-2.6.0.py
@@ -4,9 +4,9 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['2.4.8'] = 'http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip'
-        self.targets['2.6.0'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.0-RC-9-win32-x86.zip'
+        self.targets['2.6.0'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.0-RC-10-win32-x86.zip'
         self.targetInstSrc['2.4.8'] = 'cmake-2.4.8-win32-x86'
-        self.targetInstSrc['2.6.0'] = 'cmake-2.6.0-RC-9-win32-x86'
+        self.targetInstSrc['2.6.0'] = 'cmake-2.6.0-RC-10-win32-x86'
         self.defaultTarget = '2.6.0'
 
 class subclass(base.baseclass):
