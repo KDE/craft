ec778a7bc05bb1f2c521e3853107bbda1b8f8280
Patrick Spendrin <ps_ml@gmx.de>
2008-05-09 14:15:25 +0000

    add the list of targets currently contained in kdegames - as this may change in future this is a rather bad solution.
    svn path=/trunk/kdesupport/emerge/; revision=805878

--- portage/kde/kdegames/kdegames-20080202.py
+++ portage/kde/kdegames/kdegames-20080202.py
@@ -26,6 +26,38 @@ class subclass(base.baseclass):
         return True
 
     def compile( self ):
+        self.kdeCustomDefines = ""
+#        self.kdeCustomDefines += " -DBUILD_doc=OFF"
+#        self.kdeCustomDefines += " -DBUILD_bovo=OFF"
+#        self.kdeCustomDefines += " -DBUILD_lskat=OFF"
+#        self.kdeCustomDefines += " -DBUILD_katomic=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kbattleship=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kblackbox=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kblocks=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kbounce=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kbreakout=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kdiamond=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kfourinline=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kgoldrunner=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kiriki=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kjumpingcube=OFF"
+#        self.kdeCustomDefines += " -DBUILD_klines=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kmahjongg=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kmines=OFF"
+#        self.kdeCustomDefines += " -DBUILD_knetwalk=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kolf=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kollision=OFF"
+#        self.kdeCustomDefines += " -DBUILD_konquest=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kpat=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kreversi=OFF"
+#        self.kdeCustomDefines += " -DBUILD_ksame=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kshisen=OFF"
+#        self.kdeCustomDefines += " -DBUILD_ksirk=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kspaceduel=OFF"
+#        self.kdeCustomDefines += " -DBUILD_ksquares=OFF"
+#        self.kdeCustomDefines += " -DBUILD_ktuberling=OFF"
+#        self.kdeCustomDefines += " -DBUILD_ksudoku=OFF"
+#        self.kdeCustomDefines += " -DBUILD_kubrick=OFF"
         return self.kdeCompile()
     
     def install( self ):
