4ad60f69cdea8f3478e90078f8f017ac0d8d891a
Patrick Spendrin <ps_ml@gmx.de>
2008-06-03 22:55:12 +0000

    update poppler to the next release - this one works again
    svn path=/trunk/kdesupport/emerge/; revision=816468

--- portage/win32libs-sources/poppler-src/poppler-src-0.8.3.20080603.py
+++ portage/win32libs-sources/poppler-src/poppler-src-0.8.3.20080603.py
@@ -17,7 +17,9 @@ class subinfo(info.infoclass):
         self.targetInstSrc['0.8.1'] = 'poppler-0.8.1'
         self.targets['0.8.2'] = 'http://poppler.freedesktop.org/poppler-0.8.2.tar.gz'
         self.targetInstSrc['0.8.2'] = 'poppler-0.8.2'
-        self.defaultTarget = '0.8.2'
+        self.targets['0.8.3'] = 'http://poppler.freedesktop.org/poppler-0.8.3.tar.gz'
+        self.targetInstSrc['0.8.3'] = 'poppler-0.8.3'
+        self.defaultTarget = '0.8.3'
     
     def setDependencies( self ):
         self.hardDependencies['win32libs-sources/fontconfig-src'] = 'default'
