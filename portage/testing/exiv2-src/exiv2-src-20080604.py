9a6359f50bc9d05b409a0390c5269a3ac84ca9d5
Patrick Spendrin <ps_ml@gmx.de>
2008-06-09 14:32:30 +0000

    add new version and fix last one
    svn path=/trunk/kdesupport/emerge/; revision=818758

--- portage/testing/exiv2-src/exiv2-src-20080604.py
+++ portage/testing/exiv2-src/exiv2-src-20080604.py
@@ -9,7 +9,9 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['0.16'] = 'http://www.exiv2.org/exiv2-0.16.tar.gz'
         self.targetInstSrc['0.16'] = 'exiv2-0.16'
-        self.defaultTarget = '0.16'
+        self.targets['0.17'] = 'http://www.exiv2.org/exiv2-0.17.tar.gz'
+        self.targetInstSrc['0.17'] = 'exiv2-0.17'
+        self.defaultTarget = '0.17'
     
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
@@ -24,7 +26,11 @@ class subclass(base.baseclass):
             return False
 
         os.chdir( self.workdir )
-        self.system( "cd %s && patch -p0 < %s" % ( self.workdir, os.path.join( self.packagedir, "exiv2-cmake.diff" ) ) )
+        if self.buildTarget == '0.16':
+            self.system( "cd %s && patch -p0 < %s" % ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir, "exiv2-cmake.diff" ) ) )
+        else:
+            self.system( "cd %s && patch -p0 < %s" % ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir, "exiv2-0.17-cmake.diff" ) ) )
+        
         return True
         
     def compile( self ):
