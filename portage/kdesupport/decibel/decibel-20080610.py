dc446868e37508df581cbf59ce77138fb3ed514f
Patrick Spendrin <ps_ml@gmx.de>
2008-06-10 12:42:50 +0000

    add new libraries for realtime communication framework
    svn path=/trunk/kdesupport/emerge/; revision=819031

---
+++ portage/kdesupport/decibel/decibel-20080610.py
@@ -0,0 +1,38 @@
+import base
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['libs/qt'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/decibel'
+        self.defaultTarget = 'svnHEAD'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.instsrcdir = "decibel"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if self.traditional:
+            self.instdestdir = "kde"
+            return self.doPackaging( "decibel", "20080610", True )
+        else:
+            return self.doPackaging( "decibel", os.path.basename(sys.argv[0]).replace("decibel-", "").replace(".py", ""), True )
+
+if __name__ == '__main__':
+    subclass().execute()
