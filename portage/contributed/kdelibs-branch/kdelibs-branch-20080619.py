610cf5cd975568068fcc156a67e5b208845ffbe9
Jarosław Staniek <staniek@kde.org>
2008-06-19 11:49:46 +0000

    add kdelibs-branch (enterprise4) for convenience
    svn path=/trunk/kdesupport/emerge/; revision=822171

---
+++ portage/contributed/kdelibs-branch/kdelibs-branch-20080619.py
@@ -0,0 +1,52 @@
+import base
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = ''
+        self.svnTargets['svnHEAD'] = 'branches/kdepim/enterprise4/kdelibs-4.0.83'
+        for ver in ['74', '80', '83']:
+          self.targets['4.0.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.0.' + ver + '/src/kdelibs-4.0.' + ver + '.tar.bz2'
+          self.targetInstSrc['4.0.' + ver] = 'kdelibs-enterprise4-4.0.' + ver
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kdesupport/kdewin32'] = 'default'
+        self.hardDependencies['kdesupport/qimageblitz'] = 'default'
+        self.hardDependencies['kdesupport/soprano'] = 'default'
+        self.hardDependencies['kdesupport/strigi'] = 'default'
+        self.hardDependencies['kdesupport/phonon'] = 'default'
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['dev-util/win32libs'] = 'default'
+    
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        self.buildType = "Debug"
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        self.kdeCustomDefines = " -DKDE4_BUILD_TESTS=OFF "
+        if self.compiler == "mingw":
+          self.kdeCustomDefines += " -DKDE_DISTRIBUTION_TEXT=\"MinGW 3.4.5\" "
+        if self.compiler == "msvc2005":
+          self.kdeCustomDefines += " -DKDE_DISTRIBUTION_TEXT=\"MS Visual Studio 2005 SP1\" "
+
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdelibs-enterprise4", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdelibs-enterprise4", os.path.basename(sys.argv[0]).replace("kdelibs-", "").replace(".py", ""), True )
+
+if __name__ == '__main__':
+    subclass().execute()
