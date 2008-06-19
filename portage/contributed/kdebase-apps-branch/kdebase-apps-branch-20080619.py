5b18234c083ed4ff2172effb33ef8b329f186a5f
Jarosław Staniek <staniek@kde.org>
2008-06-19 12:08:01 +0000

    add kdebase-{runtime|apps}-branch (enterprise4) for convenience
    svn path=/trunk/kdesupport/emerge/; revision=822173

---
+++ portage/contributed/kdebase-apps-branch/kdebase-apps-branch-20080619.py
@@ -0,0 +1,41 @@
+import base
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'branches/kdepim/enterprise4/kdebase-4.0.83/apps'
+        for ver in ['74', '80', '83']:
+          self.targets['4.0.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.0.' + ver + '/src/kdebase-enterprise4-4.0.' + ver + '.tar.bz2'
+          self.targetInstSrc['4.0.' + ver] = 'kdebase-enterprise4-4.0.' + ver
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        self.hardDependencies['kde/kdepimlibs'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+    
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdebase-apps-enterprise4", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdebase-apps-enterprise4", os.path.basename(sys.argv[0]).replace("kdebase-apps-", "").replace(".py", ""), True )
+
+		
+if __name__ == '__main__':
+    subclass().execute()
---
+++ portage/contributed/kdebase-runtime-branch/kdebase-runtime-branch-20080619.py
@@ -0,0 +1,42 @@
+import base
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'branches/kdepim/enterprise4/kdebase-4.0.83/runtime'
+        for ver in ['74', '80', '83']:
+          self.targets['4.0.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.0.' + ver + '/src/kdebase-runtime-4.0.' + ver + '.tar.bz2'
+          self.targetInstSrc['4.0.' + ver] = 'kdebase-runtime-enterprise4-4.0.' + ver
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        self.hardDependencies['kde/kdepimlibs'] = 'default'
+    
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.instsrcdir = "runtime"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        self.kde.buildTests=False
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdebase-runtime-enterprise4", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdebase-runtime-enterprise4", os.path.basename(sys.argv[0]).replace("kdebase-runtime-", "").replace(".py", ""), True )
+
+		
+if __name__ == '__main__':
+    subclass().execute()
