2f3342602d77e1383f1fa38c2b9059eb490414cb
Patrick Spendrin <ps_ml@gmx.de>
2008-06-30 21:51:53 +0000

    add new music player juk to test phonon
    svn path=/trunk/kdesupport/emerge/; revision=826531

---
+++ portage/testing/juk/juk-20080630.py
@@ -0,0 +1,46 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdemultimedia'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        self.hardDependencies['kdesupport/phonon'] = 'default'
+        self.hardDependencies['kdesupport/taglib'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        self.kde.kdeCustomDefines = "-DBUILD_dragonplayer=OFF"
+        self.kde.kdeCustomDefines += " -DBUILD_kscd=OFF"
+        self.kde.kdeCustomDefines += " -DBUILD_libkcompactdisc=OFF"
+#        self.kde.kdeCustomDefines += " -DBUILD_juk=OFF"
+        self.kde.kdeCustomDefines += " -DBUILD_strigi-analyzer=OFF"
+        self.kde.kdeCustomDefines += " -DBUILD_kioslave=OFF"        # audiocd kioslave
+        self.kde.kdeCustomDefines += " -DBUILD_kmix=OFF"
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdemultimedia", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdemultimedia", os.path.basename(sys.argv[0]).replace("kdepimlibs-", "").replace(".py", ""), True )
+
+
+if __name__ == '__main__':
+    subclass().execute()
