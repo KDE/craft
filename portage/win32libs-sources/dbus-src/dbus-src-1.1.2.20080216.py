60dc5e6c8a07ff48d4326852a51d3e1394618928
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2008-07-02 17:53:13 +0000

    update for 1.1.2 and trunk
    svn path=/trunk/kdesupport/emerge/; revision=827342

--- portage/win32libs-sources/dbus-src/dbus-src-1.1.2.20080216.py
+++ portage/win32libs-sources/dbus-src/dbus-src-1.1.2.20080216.py
@@ -4,15 +4,13 @@ import os
 import shutil
 import info
 
-
-PACKAGE_NAME = "dbus"
-PACKAGE_FULL_VER = "1.1.2.20080216"
-
-
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['1.1.2'] = False
-        self.defaultTarget = '1.1.2'
+        self.svnTargets['1.1.2'] = 'tags/1.1.2'
+        self.targetInstSrc['1.1.2'] = os.path.join( "dbus", "cmake" )
+        self.svnTargets['svnHEAD'] = 'trunk'
+        self.targetInstSrc['svnHEAD'] = os.path.join( "dbus", "cmake" )
+        self.defaultTarget = 'svnHEAD'  # currently 1.2.1
     
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
@@ -20,57 +18,62 @@ class subinfo(info.infoclass):
 class subclass(base.baseclass):
   def __init__( self, **args ):
     base.baseclass.__init__( self, args=args )
-    if self.traditional:
-        self.instdestdir = "kde"
-    # cmake scripts are not in src root...
-    self.instsrcdir = os.path.join( "dbus", "cmake" )
     self.subinfo = subinfo()
     
   def unpack( self ):
-    print "dbus unpack called"
+    print "dbus unpack called for %s" % self.subinfo.buildTarget
     # do the svn fetch/update
-    repo = "https://windbus.svn.sourceforge.net/svnroot/windbus/tags/1.1.2"
-    self.svnFetch( repo )
+    repo = 'https://windbus.svn.sourceforge.net/svnroot/windbus/'
+    if self.subinfo.buildTarget in self.subinfo.svnTargets.keys():
+        if( self.subinfo.buildTarget != 'svnHEAD' ):
+            self.svndir += '/tags'
+        self.svnFetch( repo + self.subinfo.svnTargets[ self.subinfo.buildTarget ] )
+    else:
+        return False
 
-    utils.cleanDirectory( self.workdir )
+    if self.buildTarget == '1.1.2':
+      utils.cleanDirectory( self.workdir )
+  
+      # now copy the tree below destdir/trunk to workdir
+      srcdir = self.svndir
+      destdir = os.path.join( self.workdir, "dbus" )
+      utils.copySrcDirToDestDir( srcdir, destdir )
+  
+      os.chdir( destdir )
+      os.system( "patch -p0 < DBus-win32.patch" )
+  
+      #copy the needed changed cmake files over...
+      destdir = os.path.join( self.workdir, "dbus", "cmake", "modules" )
+      utils.copySrcDirToDestDir( self.filesdir, destdir )
+  
+      file = r"cmake\CMakeLists.txt"
+      os.rename( file, "%s.orig" % file )
+  
+      # disable doc subdir in recent dbus svn
+      sedcommand = r""" "s/add_subdirectory( doc )/###add_subdirectory( doc )/" """
+  
+      command = "type %s.orig | sed -e %s > %s" % ( file, sedcommand, file )
+      #print "command:", command
+      os.system( command )
 
-    # now copy the tree below destdir/trunk to workdir
-    srcdir = os.path.join( self.svndir, "trunk" )
-    destdir = os.path.join( self.workdir, "dbus" )
-    utils.copySrcDirToDestDir( srcdir, destdir )
-
-    os.chdir( destdir )
-    os.system( "patch -p0 < DBus-win32.patch" )
-
-    #copy the needed changed cmake files over...
-    destdir = os.path.join( self.workdir, "dbus", "cmake", "modules" )
-    utils.copySrcDirToDestDir( self.filesdir, destdir )
-
-    file = r"cmake\CMakeLists.txt"
-    os.rename( file, "%s.orig" % file )
-
-    # disable doc subdir in recent dbus svn
-    sedcommand = r""" "s/add_subdirectory( doc )/###add_subdirectory( doc )/" """
-
-    command = "type %s.orig | sed -e %s > %s" % ( file, sedcommand, file )
-    #print "command:", command
-    os.system( command )
+    if( not os.path.exists( self.workdir ) ):
+        os.makedirs( self.workdir )
 
     return True
 
-
   def compile( self ):
+    if self.subinfo.buildTarget in self.subinfo.svnTargets.keys():
+        self.kde.sourcePath = os.path.join( self.svndir, self.subinfo.svnTargets[ self.subinfo.buildTarget ], "cmake" )
+    else:
+        return False
     return self.kdeCompile()
 
   def install( self ):
     return self.kdeInstall()
 
   def make_package( self ):
-    # for src package
-    self.instsrcdir = "dbus"
-
     # now do packaging with kdewin-packager
-    self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
+    self.doPackaging( "dbus", self.buildTarget, True )
 
     return True
 
