b310486a00ad90214a741adf88891fa4075eda75
Ralf Habacker <ralf.habacker@freenet.de>
2008-07-02 06:56:54 +0000

    added astyle binary package from winkde.org
    svn path=/trunk/kdesupport/emerge/; revision=827041

---
+++ portage/dev-util/astyle/astyle-1.22.py
@@ -0,0 +1,24 @@
+import base
+import os
+import info
+
+SRC_URI= "http://www.winkde.org/pub/kde/ports/win32/repository/other/astyle-1.22-bin.zip"
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['1.22'] = SRC_URI
+        self.defaultTarget = '1.22'
+    
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, SRC_URI )
+    if self.traditional:
+        self.instdestdir = "astyle"
+    self.subinfo = subinfo()
+
+  def unpack(self):
+    res = base.baseclass.unpack( self )
+    return res
+
+if __name__ == '__main__':
+    subclass().execute()
