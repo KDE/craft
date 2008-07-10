d82bc776d96ff77e61eec7038ad33fa64afa9e09
Pau Garcia i Quiles <pgquiles@elpauer.org>
2008-07-10 17:49:11 +0000

    * Document Microsoft Visual C++ 2008 is now supported * Set environment for DirectX SDK, as it's required to build the Phonon DirectShow9 backend CCMAIL: danny@commit-digest.org
    svn path=/trunk/kdesupport/emerge/; revision=830535

--- kdesettings-example.bat
+++ kdesettings-example.bat
@@ -5,7 +5,8 @@ set KDEROOT=c:\kderoot
 
 rem Here you set the compiler to be used.
 rem * mingw - use the mingw gcc compiler (recommended)
-rem * msvc2005 - use the Microsoft Visual C++ 2005 compiler or newer
+rem * msvc2005 - use the Microsoft Visual C++ 2005 compiler
+rem * msvc2008 - use the Microsoft Visual C++ 2008 compiler
 set KDECOMPILER=mingw
 
 rem Here you set the path to your Python installation,
@@ -21,17 +22,17 @@ set MSYSDIR=%KDEROOT%\msys
 rem Here you set the path to your platform SDK installation.
 rem This path will be automatically included then.
 set PSDKDIR=%PROGRAMFILES%\Microsoft Platform SDK for Windows Server 2003 R2
+rem Here you set the path to your Microsoft DirectX SDK installation
+rem This is not needed if you use MinGW
+set MSDXSDKDIR=%PROGRAMFILES%\Microsoft DirectX SDK (June 2008)
 
 rem Here you set the location of the vcvarsall.bat file that adds
 rem Visual C++ environment variables into the build environment.
 rem if you are not building on x86 change that to something appropriate.
-if %KDECOMPILER% == msvc2005 ( 
-call "%PROGRAMFILES%\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
-)
 
-if %KDECOMPILER% == msvc2008 ( 
-call "%PROGRAMFILES%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
-)
+if %KDECOMPILER% == msvc2005 call "%MSDXSDKDIR%\Utilities\bin\dx_setenv.cmd" && call "%PROGRAMFILES%\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
+
+if %KDECOMPILER% == msvc2008 call "%MSDXSDKDIR%\Utilities\bin\dx_setenv.cmd" && call "%PROGRAMFILES%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 
 rem Here you change the download directory.
 rem If you want, so you can share the same download directory between
