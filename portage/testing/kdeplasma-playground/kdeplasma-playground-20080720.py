7410169dc47ed27cf53eafae00af1512c0004a9d
Patrick Spendrin <ps_ml@gmx.de>
2008-07-20 06:50:59 +0000

    add playground
    svn path=/trunk/kdesupport/emerge/; revision=835202

---
+++ portage/testing/kdeplasma-playground/kdeplasma-playground-20080720.py
@@ -0,0 +1,39 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/playground/base/plasma'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['kde/kdebase-workspace'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+        self.kdeCustomDefines = ""
+        
+        
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdeplasma-playground", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdeplasma-playground", os.path.basename(sys.argv[0]).replace("kdeplasma-playground-", "").replace(".py", ""), True )
+
+if __name__ == '__main__':
+    subclass().execute()
