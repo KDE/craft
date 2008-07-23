83ca4d567716c2ac49382da01e85c98c332ab369
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2008-07-23 20:32:53 +0000

    add 2.6.1-rc11 for testing
    svn path=/trunk/kdesupport/emerge/; revision=837119

--- portage/dev-util/cmake/cmake-2.6.0.py
+++ portage/dev-util/cmake/cmake-2.6.0.py
@@ -5,8 +5,10 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['2.4.8'] = 'http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip'
         self.targets['2.6.0'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.0-win32-x86.zip'
+        self.targets['2.6.1'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.1-RC-11-win32-x86.zip'
         self.targetInstSrc['2.4.8'] = 'cmake-2.4.8-win32-x86'
         self.targetInstSrc['2.6.0'] = 'cmake-2.6.0-win32-x86'
+        self.targetInstSrc['2.6.1'] = 'cmake-2.6.1-RC-11-win32-x86'
         self.defaultTarget = '2.6.0'
 
 class subclass(base.baseclass):
