8b2d08020dc2d3a551fa4cdbf0a4ae7aaa70e28a
Patrick Spendrin <ps_ml@gmx.de>
2008-07-29 15:37:19 +0000

    add bsdtar
    svn path=/trunk/kdesupport/emerge/; revision=839238

---
+++ portage/gnuwin32/libarchive/libarchive-2.4.12.py
@@ -0,0 +1,20 @@
+import gnuwin32
+import info
+
+SRC_URI = """
+http://downloads.sourceforge.net/sourceforge/gnuwin32/libarchive-2.4.12-1-bin.zip
+http://downloads.sourceforge.net/sourceforge/gnuwin32/libarchive-2.4.12-1-dep.zip
+"""
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['2.4.12-1'] = SRC_URI
+        self.defaultTarget = '2.4.12-1'
+        
+class subclass(gnuwin32.gnuwin32class):
+  def __init__( self, **args ):
+    gnuwin32.gnuwin32class.__init__( self, SRC_URI )
+    self.subinfo = subinfo()
+
+if __name__ == '__main__':
+    subclass().execute()
