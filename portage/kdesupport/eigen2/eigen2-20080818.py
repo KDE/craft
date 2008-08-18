0cb0d58be437a29419610cfe99637679c6e9fb3a
Patrick Spendrin <ps_ml@gmx.de>
2008-08-18 14:52:51 +0000

    add eigen2 needed for koffice alpha 10 tomorrow - I hope it won't break to much
    svn path=/trunk/kdesupport/emerge/; revision=848841

---
+++ portage/kdesupport/eigen2/eigen2-20080818.py
@@ -0,0 +1,40 @@
+import base
+import utils
+from utils import die
+import sys
+import info
+
+# see http://eigen.tuxfamily.org/ for more informations
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/eigen2'
+        self.defaultTarget = 'svnHEAD'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        # header-only package
+        self.createCombinedPackage = True
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        self.kdeCustomDefines = "-DBUILD_TESTS=ON"
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if self.traditional:
+            self.instdestdir = "kde"
+        return self.doPackaging( "eigen2", utils.cleanPackageName( sys.argv[0], "eigen2" ), True )        
+
+if __name__ == '__main__':
+    subclass().execute()
