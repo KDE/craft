7b4f351ba7f0683f5a79df88ba8d36e09d61095d
Patrick Spendrin <ps_ml@gmx.de>
2008-08-19 13:57:07 +0000

    add the windows kio fish replacement
    svn path=/trunk/kdesupport/emerge/; revision=849400

---
+++ portage/testing/kio-pish/kio-pish-20080819.py
@@ -0,0 +1,34 @@
+import base
+import os
+import utils
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/playground/ioslaves/kio-pish'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+        self.instsrcdir = "kio-pish"
+        
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "kio-pish", "20080819", True )
+		
+if __name__ == '__main__':
+    subclass().execute()
