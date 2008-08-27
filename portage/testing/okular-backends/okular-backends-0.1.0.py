19215be3200e9baf7f086d8c2d18af514c49e019
Patrick Spendrin <ps_ml@gmx.de>
2008-08-27 16:40:06 +0000

    make just one binary package
    svn path=/trunk/kdesupport/emerge/; revision=853384

---
+++ portage/testing/okular-backends/okular-backends-0.1.0.py
@@ -0,0 +1,35 @@
+import base
+import os
+import utils
+import info
+import shutil
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        """ """
+        self.targets['0.1.0'] = """http://winkde.org/pub/kde/ports/win32/repository/win32libs/libgs-GPL-8.63-bin.tar.bz2
+                                  http://winkde.org/pub/kde/ports/win32/repository/win32libs/libgs-GPL-8.63-lib.tar.bz2
+
+                                  http://winkde.org/pub/kde/ports/win32/repository/win32libs/libspectre-0.2.1-bin.tar.bz2
+                                  http://winkde.org/pub/kde/ports/win32/repository/win32libs/libspectre-0.2.1-lib.tar.bz2
+
+                                  http://winkde.org/pub/kde/ports/win32/repository/win32libs/ebook-tools-0.1.0-bin.tar.bz2
+                                  http://winkde.org/pub/kde/ports/win32/repository/win32libs/ebook-tools-0.1.0-lib.tar.bz2
+                                  
+                                  http://winkde.org/pub/kde/ports/win32/repository/win32libs/libzip-0.8.0-bin.tar.bz2
+                                  http://winkde.org/pub/kde/ports/win32/repository/win32libs/libzip-0.8.0-lib.tar.bz2"""
+        self.targetInstSrc['0.1.0'] = ""
+        self.defaultTarget = '0.1.0'
+
+    def setDependencies( self ):
+        """ """
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+        self.createCombinedPackage = True
+
+if __name__ == '__main__':
+    subclass().execute()
