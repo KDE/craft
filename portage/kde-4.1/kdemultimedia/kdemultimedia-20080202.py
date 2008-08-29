675d0c70d494a0a78e041cdbc5dd94a195e94064
Patrick Spendrin <ps_ml@gmx.de>
2008-08-29 21:29:22 +0000

    add kdemultimedia add post-install scripts
    svn path=/trunk/kdesupport/emerge/; revision=854594

--- portage/kde-4.1/kdemultimedia/kdemultimedia-20080202.py
+++ portage/kde-4.1/kdemultimedia/kdemultimedia-20080202.py
@@ -6,14 +6,14 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdemultimedia'
-        for ver in ['60']:
-          self.targets['4.1.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.1.' + ver + '/src/kdemultimedia-4.1.' + ver + '.tar.bz2'
-          self.targetInstSrc['4.1.' + ver] = 'kdemultimedia-4.1.' + ver
+        self.svnTargets['svnHEAD'] = 'branches/KDE/4.1/kdemultimedia'
+        for ver in ['0', '1']:
+          self.targets['4.1.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.1.' + ver + '/src/kdemultimedia-4.1.' + ver + '.tar.bz2'
+          self.targetInstSrc['4.1.' + ver] = 'kdelibs-4.1.' + ver
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
-        self.hardDependencies['kde-4.1/kdelibs'] = 'default'
+        self.hardDependencies['kde/kdelibs'] = 'default'
         self.hardDependencies['kdesupport/taglib'] = 'default'
         
 class subclass(base.baseclass):
