d4a16c8fcf74e7b36ab1dbc69d6a68c7f893b4a5
Patrick Spendrin <ps_ml@gmx.de>
2008-09-04 00:00:38 +0000

    fix libarchive headers so they can be used for ark
    svn path=/trunk/kdesupport/emerge/; revision=856874

--- portage/gnuwin32/libarchive/libarchive-2.4.12.py
+++ portage/gnuwin32/libarchive/libarchive-2.4.12.py
@@ -1,9 +1,9 @@
 import gnuwin32
 import info
+import os
 
 SRC_URI = """
 http://downloads.sourceforge.net/sourceforge/gnuwin32/libarchive-2.4.12-1-bin.zip
-http://downloads.sourceforge.net/sourceforge/gnuwin32/libarchive-2.4.12-1-dep.zip
 http://downloads.sourceforge.net/sourceforge/gnuwin32/libarchive-2.4.12-1-lib.zip
 """
 
@@ -11,11 +11,21 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['2.4.12-1'] = SRC_URI
         self.defaultTarget = '2.4.12-1'
+
+    def setDependencies( self ):
+        self.hardDependencies['dev-util/win32libs'] = 'default'
         
 class subclass(gnuwin32.gnuwin32class):
-  def __init__( self, **args ):
-    gnuwin32.gnuwin32class.__init__( self, SRC_URI )
-    self.subinfo = subinfo()
-
+    def __init__( self, **args ):
+        gnuwin32.gnuwin32class.__init__( self, SRC_URI )
+        self.subinfo = subinfo()
+        
+    def compile( self ):
+        cmd = "cd %s && patch -p0 < %s" % \
+          ( self.workdir, os.path.join( self.packagedir, "libarchive-comp.diff" ) )
+        self.system( cmd )
+        return True
+    
+ 
 if __name__ == '__main__':
     subclass().execute()
