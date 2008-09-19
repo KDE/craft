70a592d7151b18bf2f6240b406efee3c452b1226
Patrick Spendrin <ps_ml@gmx.de>
2008-09-19 16:07:33 +0000

    add koffice alpha10 tag
    svn path=/trunk/kdesupport/emerge/; revision=862680

--- portage/testing/koffice/koffice-1.9.98.20080917.py
+++ portage/testing/koffice/koffice-1.9.98.20080917.py
@@ -9,8 +9,10 @@ class subinfo(info.infoclass):
         self.svnTargets['1.9.95.8'] = "tags/koffice/1.9.95.8/koffice"
         self.svnTargets['1.9.95.9'] = "tags/koffice/1.9.95.9/koffice"
         self.svnTargets['1.9.98.0'] = "tags/koffice/1.9.98.0/koffice"
-        self.targets['1.9.95.10'] = 'ftp://ftp.kde.org/pub/kde/unstable/koffice-1.9.95.10/src/koffice-1.9.95.10.tar.bz2'
-        self.targetInstSrc['1.9.95.10'] = 'koffice-1.9.95.10'
+        self.targets['alpha10'] = 'ftp://ftp.kde.org/pub/kde/unstable/koffice-1.9.95.10/src/koffice-1.9.95.10.tar.bz2'
+        self.targetInstSrc['alpha10'] = 'koffice-1.9.95.10'
+        self.targets['beta1'] = 'ftp://ftp.kde.org/pub/kde/unstable/koffice-1.9.98.0/src/koffice-1.9.98.0.tar.bz2'
+        self.targetInstSrc['beta1'] = 'koffice-1.9.98.0'
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
@@ -18,7 +20,7 @@ class subinfo(info.infoclass):
         self.hardDependencies['kde/kdelibs'] = 'default'
         self.hardDependencies['kde/kdebase-runtime'] = 'default'
 #        self.hardDependencies['kdesupport/eigen'] = 'default'
-#         self.hardDependencies['kdesupport/eigen2'] = 'default'
+        self.hardDependencies['kdesupport/eigen2'] = 'default'
         self.softDependencies['kdesupport/qca'] = 'default'
         self.softDependencies['testing/gsl'] = 'default'
     
@@ -43,7 +45,20 @@ class subclass(base.baseclass):
 #        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_doc=OFF "
 
     def unpack( self ):
-        if self.buildTarget == '1.9.95.10':
+        if self.buildTarget == 'alpha10':
+            if( not base.baseclass.unpack( self ) ):
+                return False
+                
+            src = os.path.join( self.workdir, self.instsrcdir )
+
+            cmd = "cd %s && patch -p0 < %s" % \
+                  ( src, os.path.join( self.packagedir , "koffice-alpha10.diff" ) )
+            if utils.verbose() >= 1:
+                print cmd
+            self.kdeCustomDefines += " -DINCLUDE_INSTALL_DIR=%s" % os.path.join( self.rootdir, "include" )
+            self.system( cmd ) or die( "patch" )
+            return True
+        if self.buildTarget == 'beta1':
             if( not base.baseclass.unpack( self ) ):
                 return False
                 
