6deaa2089ffeeeb1d1ea9d52c770d0c5d9cd4890
Patrick Spendrin <ps_ml@gmx.de>
2008-10-30 12:16:33 +0000

    update to version 0.41
    svn path=/trunk/kdesupport/emerge/; revision=877808

--- portage/win32libs-sources/libical-src/libical-src-0.41.20081030.py
+++ portage/win32libs-sources/libical-src/libical-src-0.41.20081030.py
@@ -7,9 +7,9 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'libical'
-        self.targets['0.40'] = 'http://downloads.sourceforge.net/freeassociation/libical-0.40.tar.gz'
-        self.targetInstSrc['0.40'] = 'libical-0.40'
-        self.defaultTarget = '0.40'
+        self.targets['0.41'] = 'http://downloads.sourceforge.net/freeassociation/libical-0.41.tar.gz'
+        self.targetInstSrc['0.41'] = 'libical-0.41'
+        self.defaultTarget = '0.41'
     
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
@@ -19,11 +19,6 @@ class subclass(base.baseclass):
         base.baseclass.__init__( self, args=args )
         self.createCombinedPackage = True
         self.subinfo = subinfo()
-        if self.compiler <> "mingw":
-            print "error: can only be build with MinGW right now."
-            exit( 1 )
-
-
     
     def unpack( self ):
         print "libical unpack called for %s" % self.subinfo.buildTarget
@@ -56,7 +51,7 @@ class subclass(base.baseclass):
         self.createImportLibs( "libicalss" )
         self.createImportLibs( "libicalvcal" )
 
-        self.doPackaging( "libical", "0.34-0", True )
+        self.doPackaging( "libical", "0.41-0", True )
 
         return True
 
