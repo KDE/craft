13ca15e1a9eb249fdeec110941f0a19450cfda84
Patrick Spendrin <ps_ml@gmx.de>
2008-11-09 12:12:03 +0000

    fix amarok post install (I will try to rebuild the package so these strange errors never will happen again) do not require setting a path or anything - I still wonder how the post-install would work without it.
    svn path=/trunk/kdesupport/emerge/; revision=881942

--- portage/extragear/amarok/post-install-bin.cmd
+++ portage/extragear/amarok/post-install-bin.cmd
@@ -5,12 +5,12 @@ echo package: %0
 echo *******************************************************************************
 
 set quitnow=False
-for %%i in ( kioslave.exe kded4.exe ) DO (
-    tasklist /NH /FI "IMAGENAME eq %%i" | find "%%i" > "Nul" && (
-        echo error: found %%i running. please kill this process!
-        set quitnow=True
-    )
-)
+::for %%i in ( kioslave.exe kded4.exe ) DO (
+::    tasklist /NH /FI "IMAGENAME eq %%i" | find "%%i" > "Nul" && (
+::        echo error: found %%i running. please kill this process!
+::        set quitnow=True
+::    )
+::)
 
 if %quitnow% equ True (
     echo error: one or more programs are running that shouldn't run.
@@ -18,15 +18,15 @@ if %quitnow% equ True (
     goto :eof
 )
 
-if not `"update-mime-database --help"` equ "" (
-    update-mime-database %CD%\share\mime
+if not `"bin\update-mime-database.exe --help"` equ "" (
+    bin\update-mime-database %CD%\share\mime
 ) else (
     echo error: update-mime-database wasn't found.
     echo        please check for correct installation
 )
 
-if not `"kbuildsycoca4 --help"` equ "" (
-    kbuildsycoca4 --noincremental
+if not `"bin\kbuildsycoca4.exe --help"` equ "" (
+    bin\kbuildsycoca4 --noincremental
 ) else (
     echo error: kbuildsycoca4 wasn't found.
     echo        please check for correct installation
