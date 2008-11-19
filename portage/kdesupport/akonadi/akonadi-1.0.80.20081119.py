4f6337ca27eb4b573caa019d5ca1e4c7f6ce4482
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2008-11-19 19:52:25 +0000

    we need 1.0.80 for kde4.2
    svn path=/trunk/kdesupport/emerge/; revision=886658

---
+++ portage/kdesupport/akonadi/akonadi-1.0.80.20081119.py
@@ -0,0 +1,43 @@
+import base
+import utils
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['kdesupport/automoc'] = 'default'
+        self.hardDependencies['libs/qt'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['0.80'] = 'tags/akonadi/0.80'
+        self.svnTargets['0.81'] = 'tags/akonadi/0.81'
+        self.svnTargets['0.82'] = 'tags/akonadi/0.82'
+        self.svnTargets['1.0.0'] = 'tags/akonadi/1.0.0'
+        self.svnTargets['1.0.80'] = 'tags/akonadi/1.0.80'
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/akonadi'
+        self.defaultTarget = 'svnHEAD'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.instsrcdir = "akonadi"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if self.buildTarget == "svnHEAD":
+            return self.doPackaging( "akonadi", utils.cleanPackageName( sys.argv[0], "akonadi" ), True )
+        else:
+            return self.doPackaging( "akonadi", self.buildTarget, True )
+
+if __name__ == '__main__':
+    subclass().execute()
