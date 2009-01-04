e5179f3204e155e01665d6981e72249e36eafcd0
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-01-04 17:05:52 +0000

    bump versions for 4.2.0
    svn path=/trunk/kdesupport/emerge/; revision=905576

--- portage/kdesupport/akonadi/akonadi-1.1.0.20090104.py
+++ portage/kdesupport/akonadi/akonadi-1.1.0.20090104.py
@@ -6,23 +6,23 @@ import info
 class subinfo(info.infoclass):
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['kdesupport/automoc'] = 'default'
         self.hardDependencies['libs/qt'] = 'default'
-        self.hardDependencies['kdesupport/kdewin32'] = 'default'
-        self.hardDependencies['kdesupport/clucene-core'] = 'default'
 
     def setTargets( self ):
-        self.svnTargets['0.5.7'] = 'tags/strigi/strigi/0.5.7'
-        self.svnTargets['0.5.8'] = 'tags/strigi/strigi/0.5.8'
-        self.svnTargets['0.5.9'] = 'tags/strigi/strigi/0.5.9'
-        self.svnTargets['0.5.10'] = 'tags/strigi/strigi/0.5.10'
-        self.svnTargets['0.5.11'] = 'tags/strigi/strigi/0.5.11'
-        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/strigi'
+        self.svnTargets['0.80'] = 'tags/akonadi/0.80'
+        self.svnTargets['0.81'] = 'tags/akonadi/0.81'
+        self.svnTargets['0.82'] = 'tags/akonadi/0.82'
+        self.svnTargets['1.0.0'] = 'tags/akonadi/1.0.0'
+        self.svnTargets['1.0.80'] = 'tags/akonadi/1.0.80'
+        self.svnTargets['1.1.0']  = 'tags/akonadi/1.1.0'
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/akonadi'
         self.defaultTarget = 'svnHEAD'
 
 class subclass(base.baseclass):
     def __init__( self, **args ):
         base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "strigi"
+        self.instsrcdir = "akonadi"
         self.subinfo = subinfo()
 
     def unpack( self ):
@@ -36,9 +36,9 @@ class subclass(base.baseclass):
 
     def make_package( self ):
         if self.buildTarget == "svnHEAD":
-            return self.doPackaging( "strigi", utils.cleanPackageName( sys.argv[0], "strigi" ), True )
+            return self.doPackaging( "akonadi", utils.cleanPackageName( sys.argv[0], "akonadi" ), True )
         else:
-            return self.doPackaging( "strigi", self.buildTarget, True )
+            return self.doPackaging( "akonadi", self.buildTarget, True )
 
 if __name__ == '__main__':
     subclass().execute()
---
+++ portage/kdesupport/strigi/strigi-0.6.2.20090104.py
@@ -0,0 +1,45 @@
+import base
+import utils
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['libs/qt'] = 'default'
+        self.hardDependencies['kdesupport/kdewin32'] = 'default'
+        self.hardDependencies['kdesupport/clucene-core'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['0.5.7'] = 'tags/strigi/strigi/0.5.7'
+        self.svnTargets['0.5.8'] = 'tags/strigi/strigi/0.5.8'
+        self.svnTargets['0.5.9'] = 'tags/strigi/strigi/0.5.9'
+        self.svnTargets['0.5.10'] = 'tags/strigi/strigi/0.5.10'
+        self.svnTargets['0.5.11'] = 'tags/strigi/strigi/0.5.11'
+        self.svnTargets['0.6.2']  = 'tags/strigi/strigi/0.6.2'
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/strigi'
+        self.defaultTarget = 'svnHEAD'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.instsrcdir = "strigi"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if self.buildTarget == "svnHEAD":
+            return self.doPackaging( "strigi", utils.cleanPackageName( sys.argv[0], "strigi" ), True )
+        else:
+            return self.doPackaging( "strigi", self.buildTarget, True )
+
+if __name__ == '__main__':
+    subclass().execute()
