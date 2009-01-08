49cc8fe9e8bcc5e2fc823e17f88a759018c5efef
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-01-08 20:55:46 +0000

    add 4.2.96 for kde4.1.96
    svn path=/trunk/kdesupport/emerge/; revision=907879

--- portage/kdesupport/phonon/phonon-4.2.96.20090108.py
+++ portage/kdesupport/phonon/phonon-4.2.96.20090108.py
@@ -14,9 +14,10 @@ class subinfo(info.infoclass):
         self.svnTargets['4.1.0'] = 'tags/phonon/4.1.0'    # tagged version, also in qt4.4.0
         self.svnTargets['4.2.0'] = 'tags/phonon/4.2.0'    # tagged version
         self.svnTargets['4.2.80'] = 'tags/phonon/4.2.80'
+        self.svnTargets['4.2.96'] = 'tags/phonon/4.2.96'
         self.svnTargets['4.2'] = 'branches/phonon/4.2'
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/phonon'
-        self.defaultTarget = '4.2.80'
+        self.defaultTarget = 'svnHEAD'
 
 class subclass(base.baseclass):
     def __init__( self, **args ):
