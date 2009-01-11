4d2f620e2c1559b8afbe47ff4ca02682e245f82f
Patrick Spendrin <ps_ml@gmx.de>
2009-01-11 23:00:09 +0000

    fix UAC problem with patch.exe on vista
    svn path=/trunk/kdesupport/emerge/; revision=909645

--- portage/gnuwin32/patch/patch-2.5.9-7.py
+++ portage/gnuwin32/patch/patch-2.5.9-7.py
@@ -1,5 +1,7 @@
 import gnuwin32
 import info
+import os
+import shutil
 
 SRC_URI = """
 http://downloads.sourceforge.net/sourceforge/gnuwin32/patch-2.5.9-7-bin.zip
@@ -17,6 +19,18 @@ class subclass(gnuwin32.gnuwin32class):
   def __init__( self, **args ):
     gnuwin32.gnuwin32class.__init__( self, SRC_URI )
     self.subinfo = subinfo()
+
+  def install( self ):
+    gnuwin32.gnuwin32class.install( self )
+
+    manifest = os.path.join( self.packagedir , "patch.exe.manifest" )
+    if self.traditional:
+        manifest_dest = os.path.join( self.imagedir, "gnuwin32", "bin" )
+    else:
+        manifest_dest = os.path.join( self.imagedir, "bin" )
+
+    shutil.copy( manifest, manifest_dest )
+    return True
 
 if __name__ == '__main__':
     subclass().execute()
