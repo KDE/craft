621694ae14a4d2c5017028a55353e2573d0e6a61
Ralf Habacker <ralf.habacker@freenet.de>
2009-01-11 09:31:51 +0000

    there is no 4.7.9 version on the server
    svn path=/trunk/kdesupport/emerge/; revision=909194

---
+++ portage/testing/sip/sip-4.7.7.py
@@ -0,0 +1,57 @@
+import base
+import utils
+import sys
+import info
+import os
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+    def setTargets( self ):
+        self.targets['4.7.7'] = 'http://www.winkde.org/pub/kde/ports/win32/repository/external/sip-4.7.7.zip'
+        self.targetInstSrc['4.7.7'] = 'sip-4.7.7'
+        self.defaultTarget = '4.7.7'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+    def configure( self ):
+		builddir = os.path.join( self.workdir, self.instsrcdir )
+		os.chdir( builddir )
+		if self.buildType == 'Debug':
+			command = "python configure.py -u"
+		else:
+			command = "python configure.py"
+		# add mingw compiler
+		#command += "-p plat"
+		command += " CFLAGS=-I" + os.path.join(self.packagedir)
+		command += " CXXFLAGS=-I" + os.path.join(self.packagedir)
+		print command
+		self.system( command )
+		return True
+
+    def compile( self ):
+		self.configure()
+		builddir = os.path.join( self.workdir, self.instsrcdir )
+		os.chdir( builddir )
+		command = "nmake"
+		self.system( command )
+		return True
+
+    def install( self ):
+		builddir = os.path.join( self.workdir, self.instsrcdir )
+		os.chdir( builddir )
+		self.system( "nmake install" )
+		# fix problem with not copying manifest file 
+		self.system( "copy " + os.path.join(builddir,"sipgen","sip.exe.manifest") + " c:\python25") 
+		# install manifest file too
+		return True
+
+    def make_package( self ):
+        return False
+
+if __name__ == '__main__':
+    subclass().execute()
