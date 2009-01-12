1358fcdd896f993d7cfd1177f5f6d5e9ed436735
Ralf Habacker <ralf.habacker@freenet.de>
2009-01-12 23:15:37 +0000

    updated to the recent version
    svn path=/trunk/kdesupport/emerge/; revision=910284

---
+++ portage/kdesupport/kdewin-installer-static/kdewin-installer-static-0.9.5.py
@@ -0,0 +1,39 @@
+import base
+import utils
+import sys
+import info
+import os.path
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['libs/qt-static'] = 'default'
+        self.hardDependencies['dev-util/upx'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/kdewin-installer'
+        self.defaultTarget = 'svnHEAD'
+
+class subclass(base.baseclass):
+    def __init__(self, **args):
+        base.baseclass.__init__( self, args=args )
+        self.instsrcdir = "kdewin-installer"
+        self.buildType = "Release"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        self.kdeCustomDefines = "-DQT_QMAKE_EXECUTABLE:FILEPATH=%s" \
+            % os.path.join(self.rootdir, "qt-static", "bin", "qmake.exe").replace('\\', '/')
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "kdewin-installer", utils.cleanPackageName( sys.argv[0], "kdewin-installer" ), True )
+
+if __name__ == '__main__':
+    subclass().execute()
