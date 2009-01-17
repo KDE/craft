fed17194525abeaf4a99de0911438f95bd4c521d
Patrick Spendrin <ps_ml@gmx.de>
2009-01-17 01:04:58 +0000

    add a virtual package which contains binaries for kdelibs library dependencies (except those from kdesupport)
    svn path=/trunk/kdesupport/emerge/; revision=912253

---
+++ portage/virtual/kdelibs-base/kdelibs-base-0.1.py
@@ -0,0 +1,34 @@
+import base
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['0.1'] = ""
+        self.defaultTarget = '0.1'
+    
+    def setDependencies( self ):
+        self.hardDependencies['win32libs-bin/aspell']  = 'default'
+#        self.hardDependencies['win32libs-bin/enchant']  = 'default'
+        self.hardDependencies['win32libs-bin/gettext']  = 'default'
+#        self.hardDependencies['win32libs-bin/gssapi']  = 'default'
+#        self.hardDependencies['win32libs-bin/hspell']  = 'default'
+        self.hardDependencies['win32libs-bin/jpeg']  = 'default'
+        self.hardDependencies['win32libs-bin/libbzip2']  = 'default'
+        self.hardDependencies['win32libs-bin/libgif']  = 'default'
+        self.hardDependencies['win32libs-bin/libjasper']  = 'default'
+        self.hardDependencies['win32libs-bin/libpng']  = 'default'
+        self.hardDependencies['win32libs-bin/libxml2']  = 'default'
+        self.hardDependencies['win32libs-bin/libxslt']  = 'default'
+#        self.hardDependencies['win32libs-bin/openexr']  = 'default'
+        self.hardDependencies['win32libs-bin/openssl']  = 'default'
+        self.hardDependencies['win32libs-bin/pcre']  = 'default'
+        self.hardDependencies['win32libs-bin/shared-mime-info']  = 'default'
+        self.hardDependencies['win32libs-bin/zlib']  = 'default'
+    
+class subclass(base.baseclass):
+  def __init__( self, **args ):
+    base.baseclass.__init__( self, args=args )
+    self.subinfo = subinfo()
+
+if __name__ == '__main__':
+    subclass().execute()
