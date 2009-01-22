afbf7a22c5c399abda189c2ca8ec3a6153f7bb87
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-01-22 16:41:56 +0000

    some updates
    svn path=/trunk/kdesupport/emerge/; revision=915251

--- portage/dev-util/mingw/mingw-3.4.5.py
+++ portage/dev-util/mingw/mingw-3.4.5.py
@@ -11,16 +11,17 @@ binutils-2.15.91-20040904-1.tar.gz
 gcc-core-3.4.2-20040916-1.tar.gz
 gcc-g++-3.4.2-20040916-1.tar.gz
 mingw32-make-3.80.0-3.exe
-mingw-runtime-3.7.tar.gz -> 3.9
-w32api-3.2.tar.gz -> 3.6
+mingw-runtime-3.7.tar.gz -> 3.14
+w32api-3.2.tar.gz -> 3.11
 """
 
 SRC_URI = """
-http://downloads.sourceforge.net/sourceforge/mingw/binutils-2.18.50-20080109-2.tar.gz
+http://downloads.sourceforge.net/sourceforge/mingw/binutils-2.19-mingw32-bin.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/gcc-core-3.4.5-20060117-3.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/gcc-g++-3.4.5-20060117-3.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/mingw32-make-3.81-20080326.tar.gz
-http://downloads.sourceforge.net/sourceforge/mingw/mingw-runtime-3.14.tar.gz
+http://downloads.sourceforge.net/sourceforge/mingw/mingwrt-3.15.2-mingw32-dll.tar.gz
+http://downloads.sourceforge.net/sourceforge/mingw/mingwrt-3.15.2-mingw32-dev.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/w32api-3.11.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/gdb-6.8-mingw-3.tar.bz2
 http://downloads.sourceforge.net/sourceforge/mingw/mingw-utils-0.3.tar.gz
