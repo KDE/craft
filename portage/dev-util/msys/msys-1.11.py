1604a54ab55ed3e833d412b23c636a826c406a58
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-02-04 19:44:07 +0000

    add gettext msys packages to allow i18n
    svn path=/trunk/kdesupport/emerge/; revision=921325

--- portage/dev-util/msys/msys-1.11.py
+++ portage/dev-util/msys/msys-1.11.py
@@ -6,6 +6,8 @@ import info
 
 SRC_URI = """
 http://downloads.sourceforge.net/sourceforge/mingw/msysCORE-1.0.11-2007.01.19-1.tar.bz2
+http://downloads.sourceforge.net/sourceforge/mingw/gettext-devel-0.16.1-MSYS-1.0.11-1.tar.bz2
+http://downloads.sourceforge.net/sourceforge/mingw/gettext-0.16.1-MSYS-1.0.11-1.tar.bz2
 """
 
 class subinfo(info.infoclass):
