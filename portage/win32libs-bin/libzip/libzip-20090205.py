917ea91bf82009027b38da3b3d6e57db64c9fd5d
Patrick Spendrin <ps_ml@gmx.de>
2009-02-05 15:05:31 +0000

    add libzip (that is _lib_ zip and not zip!)
    svn path=/trunk/kdesupport/emerge/; revision=921731

---
+++ portage/win32libs-bin/libzip/libzip-20090205.py
@@ -0,0 +1,25 @@
+import base
+import os
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        repoUrl = """http://downloads.sourceforge.net/kde-windows"""
+        
+        for version in ['0.8.0']:
+            self.targets[ version ] = repoUrl + """/libzip-""" + version + """-bin.tar.bz2
+                                """ + repoUrl + """/libzip-""" + version + """-lib.tar.bz2"""
+
+            
+        self.defaultTarget = '0.8.0'
+
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, "" )
+    self.subinfo = subinfo()
+
+if __name__ == '__main__':
+    subclass().execute()
