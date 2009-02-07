60da32dc15c3117772909e1ade4f6a41a7e67ed5
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-02-07 14:39:36 +0000

    update to subversion 1.5.5 - hopefully sparse checkouts are faster now
    svn path=/trunk/kdesupport/emerge/; revision=922716

--- portage/dev-util/subversion/subversion-1.5.5.20090207.py
+++ portage/dev-util/subversion/subversion-1.5.5.20090207.py
@@ -1,24 +1,22 @@
 import base
 import info
 
-SRC_URI = "http://subversion.tigris.org/files/documents/15/41094/svn-win32-1.4.6.zip"
-
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.4.6'] = SRC_URI
+        self.targets['1.4.6'] = "http://subversion.tigris.org/files/documents/15/41094/svn-win32-1.4.6.zip"
         self.targets['1.5.0'] = "http://subversion.tigris.org/files/documents/15/43074/svn-win32-1.5.0.zip"
+        self.targets['1.5.5'] = "http://subversion.tigris.org/files/documents/15/44589/svn-win32-1.5.5.zip"
         self.targetInstSrc['1.4.6'] = "svn-win32-1.4.6"
         self.targetInstSrc['1.5.0'] = "svn-win32-1.5.0"
-        self.defaultTarget = '1.5.0'
-    
+        self.targetInstSrc['1.5.5'] = "svn-win32-1.5.5"
+        self.defaultTarget = '1.5.5'
+
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
-    
+
 class subclass(base.baseclass):
   def __init__( self, **args ):
-    base.baseclass.__init__( self, SRC_URI, args=args )
-    if self.traditional:
-        self.instdestdir = "subversion"
+    base.baseclass.__init__( self, args=args )
     self.subinfo = subinfo()
 
 if __name__ == '__main__':
