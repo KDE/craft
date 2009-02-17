3a911325192f5ebd38b4774b8a944332ac89bfa6
Patrick Spendrin <ps_ml@gmx.de>
2009-02-17 22:02:39 +0000

    eigen has been removed from kdesupport long time ago
    svn path=/trunk/kdesupport/emerge/; revision=927609

--- portage/kdesupport/eigen/eigen-20071019.py
+++ portage/kdesupport/eigen/eigen-20071019.py
@@ -14,7 +14,6 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['1.0.5'] = 'http://download.tuxfamily.org/eigen/eigen-1.0.5.tar.gz'
         self.targetInstSrc['1.0.5'] = "eigen"
-        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/eigen'
         self.defaultTarget = '1.0.5'
 
 class subclass(base.baseclass):
