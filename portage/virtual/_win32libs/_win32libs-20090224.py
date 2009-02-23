ad5c1f2d29d6d9fc249acb0ef4b282054a3be529
Patrick Spendrin <ps_ml@gmx.de>
2009-02-23 23:27:02 +0000

    add a virtual package to install all depend libraries from win32libs-bin instead of using one big file. if you replace occurances of dev-util/win32libs with virtual/_win32libs you should get a slightly faster system.
    svn path=/trunk/kdesupport/emerge/; revision=930656

---
+++ portage/virtual/_win32libs/_win32libs-20090224.py
@@ -0,0 +1,48 @@
+import base
+import os
+import utils
+import shutil
+import info
+
+PACKAGES= """aspell
+libbzip2
+expat
+gettext
+giflib
+gpgme
+iconv
+jasper
+jpeg
+lcms
+libpng
+libxml2
+libxslt
+openslp
+openssl
+redland
+shared-mime-info
+tiff
+zlib
+zip
+boost
+dbus
+pcre
+"""
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['HEAD'] = ""
+        self.defaultTarget = 'HEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+        for package in PACKAGES.split():
+            self.hardDependencies['win32libs-bin/' + package] = 'default'
+    
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, "" )
+    self.subinfo = subinfo()
+
+if __name__ == '__main__':
+    subclass().execute()
