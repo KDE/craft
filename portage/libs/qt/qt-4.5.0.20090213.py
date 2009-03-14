11631bb38635ab6cb6b8cf0b31542e9471e4e3ea
Ralf Habacker <ralf.habacker@freenet.de>
2009-03-14 09:19:14 +0000

    fixed  binary package name
    svn path=/trunk/kdesupport/emerge/; revision=939202

--- portage/libs/qt/qt-4.5.0.20090213.py
+++ portage/libs/qt/qt-4.5.0.20090213.py
@@ -28,7 +28,7 @@ class subclass(base.baseclass):
     def __init__( self, **args ):
         base.baseclass.__init__( self, args=args )
         self.instsrcdir = "qt-win-opensource-src-" + self.compiler
-        self.openssl = "http://downloads.sourceforge.net/kde-windows/openssl-0.9.8g-1-lib.zip"
+        self.openssl = "http://downloads.sourceforge.net/kde-windows/openssl-0.9.8j-1-lib.tar.bz2"
         if self.compiler == "msvc2005" or self.compiler == "msvc2008":
             self.dbuslib = "http://downloads.sourceforge.net/kde-windows/dbus-msvc-1.2.4-1-lib.tar.bz2"
         elif self.compiler == "mingw":
