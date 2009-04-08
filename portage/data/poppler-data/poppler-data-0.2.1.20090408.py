2cac8990eff8dfc9ecdf4c2e0ca9c47c06473ec1
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-04-08 15:33:28 +0000

    update for 0.2.1
    svn path=/trunk/kdesupport/emerge/; revision=951177

---
+++ portage/data/poppler-data/poppler-data-0.2.1.20090408.py
@@ -0,0 +1,35 @@
+import base
+import os
+import utils
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        for v in [ '0.2.0', '0.2.1' ]:
+          self.targets[v] = 'http://poppler.freedesktop.org/poppler-data-' + v + '.tar.gz'
+          self.targetInstSrc[v] = 'poppler-data-' + v
+          self.patchToApply[v] = ( 'poppler-data-cmake.patch', 0 )
+        self.defaultTarget = '0.2.1'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.createCombinedPackage = True
+        self.subinfo = subinfo()
+
+    def compile( self ):
+        self.kdeCompile()
+        return True
+        
+    def install( self ):
+        self.kdeInstall()
+        return True
+        
+    def make_package( self ):
+        # now do packaging with kdewin-packager
+        self.doPackaging( "poppler-data", self.buildTarget, True )
+
+        return True
+
+if __name__ == '__main__':
+    subclass().execute()
