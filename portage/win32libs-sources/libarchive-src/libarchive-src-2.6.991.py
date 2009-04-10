51fa62a4ebe17250787193ae64a6e4e1ab9296a6
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-04-10 08:19:12 +0000

    add libarchive 2.7rc - no binary until 2.7 is released
    svn path=/trunk/kdesupport/emerge/; revision=951780

---
+++ portage/win32libs-sources/libarchive-src/libarchive-src-2.6.991.py
@@ -0,0 +1,44 @@
+import base
+import utils
+import os
+import shutil
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        for v in [ '2.6.991a' ]:
+            self.targets[ v ] = 'http://libarchive.googlecode.com/files/libarchive-' + v + '.tar.gz'
+            self.targetInstSrc[ v ] = 'libarchive-' + v
+        self.defaultTarget = '2.6.991a'
+    
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['win32libs-bin/libbzip2'] = 'default'
+#        self.hardDependencies['win32libs-bin/lzma'] = 'default'
+        self.hardDependencies['win32libs-bin/openssl'] = 'default'
+        self.hardDependencies['win32libs-bin/zlib'] = 'default'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.createCombinedPackage = True
+        self.subinfo = subinfo()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        # auto-create both import libs with the help of pexports
+        #self.createImportLibs( "libical" )
+        #self.createImportLibs( "libicalss" )
+        #self.createImportLibs( "libicalvcal" )
+
+        self.doPackaging( "libarchive", self.buildTarget, True )
+
+        return True
+
+if __name__ == '__main__':
+    subclass().execute()
