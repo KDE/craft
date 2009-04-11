0f32cf9464ddc8bc3aeb0fc715eec4f66f6d3004
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-04-11 14:26:23 +0000

    cmake for cyrus-sasl - not tested on mingw yet - needs some more tweaks (.rc/.res file generation)
    svn path=/trunk/kdesupport/emerge/; revision=952307

--- portage/contributed/cyrus-sasl-src/cyrus-sasl-src-2.1.22.py
+++ portage/contributed/cyrus-sasl-src/cyrus-sasl-src-2.1.22.py
@@ -8,8 +8,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = False
         self.targets['2.1.22'] = 'ftp://ftp.andrew.cmu.edu/pub/cyrus-mail/cyrus-sasl-2.1.22.tar.gz'
+        self.targetInstSrc['2.1.22'] = 'cyrus-sasl-2.1.22'
+        self.patchToApply['2.1.22'] = ( 'cyrus-sasl-2.1.22.patch', 1 )
         self.defaultTarget = '2.1.22'
 
     def setDependencies( self ):
@@ -18,54 +19,13 @@ class subinfo(info.infoclass):
 class subclass(base.baseclass):
     def __init__( self, **args ):
         base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "cyrus-sasl"
         self.subinfo = subinfo()
 
-    def execute( self ):
-        base.baseclass.execute( self )
-        if self.compiler == "mingw":
-            print "error: can only be build with MSVC"
-            exit( 1 )
-
-    def unpack( self ):
-        base.baseclass.unpack( self ) or utils.die( "unpack failed" )
-        os.chdir( self.workdir )
-        shutil.move("cyrus-sasl-2.1.22", self.instsrcdir)
-        self.system( "cd %s && patch -p0 < %s" % ( os.path.join(self.workdir,self.instsrcdir), os.path.join( self.packagedir, "plugins_NTMakefile.patch" ) ) )
-        self.system( "cd %s && patch -p0 < %s" % ( os.path.join(self.workdir,self.instsrcdir), os.path.join( self.packagedir, "utils_NTMakefile.patch" ) ) )
-        # required by rc
-        fc = open( os.path.join(os.environ["TEMP"],"afxres.h"), "w")
-        fc.write("\n")
-        fc.close()
-        fc = open( os.path.join(self.workdir,self.instsrcdir,"plugins","afxres.h"), "w")
-        fc.write("\n")
-        fc.close()
-
-        return True
-
     def compile( self ):
-        os.chdir(os.path.join(self.workdir,self.instsrcdir))
-        dst = os.path.join( self.imagedir, self.instdestdir)
-        # CFG=Release/Debug
-        # VERBOSE=0/1
-        #DB_INCLUDE
-        #DB_LIBPATH
-        #OPENSSL_INCLUDE
-        #OPENSSL_LIBPATH
-        #GSSAPI_INCLUDE
-        #GSSAPI_LIBPATH
-        #SQLITE_INCLUDE
-        #SQLITE_LIBPATH
-        #LDAP_LIB_BASE
-        #LDAP_INCLUDE
-        self.system("nmake /f NTMakefile CFG=Release prefix=%s" % (dst))
-        return True
+        return self.kdeCompile()
 
     def install( self ):
-        os.chdir(os.path.join(self.workdir,self.instsrcdir))
-        dst = os.path.join( self.imagedir, self.instdestdir)
-        os.system("nmake /f NTMakefile  CFG=Release prefix=%s install" % (dst))
-        return True
+        return self.kdeInstall()
 
     def make_package( self ):
         return self.doPackaging( "cyrus-sasl", self.buildTarget, True )
