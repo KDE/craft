1b9dabf44f376c7fcf3b2170633b838e7a97730e
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-04-11 14:42:23 +0000

    add chokoq - compiles fine on windows, saro plz add a beta release :)
    BUG: 189370
    CCMAIL: ps_ml@gmx.de
    
    svn path=/trunk/kdesupport/emerge/; revision=952320

---
+++ portage/extragear/choqok/choqok-20090411.py
@@ -0,0 +1,34 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/extragear/network/choqok'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+    
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        self.buildType = "Debug"
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "ktorrent", self.buildTarget, True )
+
+if __name__ == '__main__':
+    subclass().execute()
