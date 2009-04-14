5c50e48db90a30d95e5bb262085c7ec095a7a3df
Patrick Spendrin <ps_ml@gmx.de>
2009-04-14 21:49:14 +0000

    add correct dependencies.
    svn path=/trunk/kdesupport/emerge/; revision=954004

--- portage/kdesupport/kdewin32/kdewin32-0.3.9.20080328.py
+++ portage/kdesupport/kdewin32/kdewin32-0.3.9.20080328.py
@@ -6,6 +6,8 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
         self.hardDependencies['libs/qt'] = 'default'
+        self.hardDependencies['win32libs-bin/zlib'] = 'default'
+        self.hardDependencies['win32libs-bin/libpng'] = 'default'
 
     def setTargets( self ):
         self.svnTargets['0.3.9'] = 'tags/kdewin32/0.3.9'
