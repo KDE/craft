44d283e2483240abae3a5410e0fdeebefcc2b37b
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-04-18 18:30:24 +0000

    fix package version
    svn path=/trunk/kdesupport/emerge/; revision=955878

--- portage/win32libs-bin/openssl/openssl-0.9.8k.py
+++ portage/win32libs-bin/openssl/openssl-0.9.8k.py
@@ -6,10 +6,10 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         repoUrl = """http://downloads.sourceforge.net/kde-windows"""
         
-        for version in [ '0.9.8j-1', '0.9.8k-1' ]:
+        for version in [ '0.9.8j-1', '0.9.8k-2' ]:
             self.targets[ version ] = repoUrl + """/openssl-""" + version + """-bin.tar.bz2
                                 """ + repoUrl + """/openssl-""" + version + """-lib.tar.bz2"""
-        self.defaultTarget = '0.9.8k-1'
+        self.defaultTarget = '0.9.8k-2'
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
