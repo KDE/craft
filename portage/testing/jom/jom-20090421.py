6f0a3939b67f1768c5072b774bd959aff0fdb7dc
Patrick Spendrin <ps_ml@gmx.de>
2009-04-21 15:51:29 +0000

    a new build tool for windows. Thanks to jobor for doing all the work with it. This version will not build cmake-made Makefiles yet, but I will add a src package later (and hopefully it can be fixed). For more information: http://labs.trolltech.com/blogs/2009/03/27/speeding-up-visual-c-qt-builds/
    svn path=/trunk/kdesupport/emerge/; revision=957214

---
+++ portage/testing/jom/jom-20090421.py
@@ -0,0 +1,35 @@
+import base
+import info
+import shutil
+import os
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+    def setTargets( self ):
+        self.targets['HEAD'] = 'http://winkde.org/pub/kde/ports/win32/repository/other/jom.zip'
+        self.targetInstSrc['HEAD'] = 'jom'
+        self.defaultTarget = 'HEAD'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return base.baseclass.unpack( self )
+
+    def install(self):
+        res = base.baseclass.install( self )
+        srcdir = os.path.join( self.workdir, "jom.exe" )
+        destdir = os.path.join( self.imagedir, "bin" )
+        if not os.path.exists( self.imagedir ):
+            os.mkdir( self.imagedir )
+        if not os.path.exists( destdir ):
+            os.mkdir( destdir )
+        shutil.copy( srcdir, os.path.join( destdir, "jom.exe" ) )
+        return res
+
+if __name__ == '__main__':
+    subclass().execute()
