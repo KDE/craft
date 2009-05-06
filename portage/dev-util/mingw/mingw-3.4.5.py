9833d56d53703ee9b9bdebf2726d5266d0d38b5b
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-05-06 21:37:28 +0000

    updated binutils
    svn path=/trunk/kdesupport/emerge/; revision=964555

--- portage/dev-util/mingw/mingw-3.4.5.py
+++ portage/dev-util/mingw/mingw-3.4.5.py
@@ -7,16 +7,16 @@ import info
 """
 from trolltech mingw package:
 
-binutils-2.15.91-20040904-1.tar.gz
-gcc-core-3.4.2-20040916-1.tar.gz
-gcc-g++-3.4.2-20040916-1.tar.gz
-mingw32-make-3.80.0-3.exe
+binutils-2.15.91-20040904-1.tar.gz -> 2.19.1
+gcc-core-3.4.2-20040916-1.tar.gz -> 3.4.5
+gcc-g++-3.4.2-20040916-1.tar.gz -> 3.4.5
+mingw32-make-3.80.0-3.exe -> 3.81
 mingw-runtime-3.7.tar.gz -> 3.14
 w32api-3.2.tar.gz -> 3.11
 """
 
 SRC_URI = """
-http://downloads.sourceforge.net/sourceforge/mingw/binutils-2.19-mingw32-bin.tar.gz
+http://downloads.sourceforge.net/sourceforge/mingw/binutils-2.19.1-mingw32-bin.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/gcc-core-3.4.5-20060117-3.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/gcc-g++-3.4.5-20060117-3.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/mingw32-make-3.81-20080326-3.tar.gz
--- portage/dev-util/msys/msys-1.11.py
+++ portage/dev-util/msys/msys-1.11.py
@@ -5,7 +5,7 @@ import os
 import info
 
 SRC_URI = """
-http://downloads.sourceforge.net/sourceforge/mingw/msysCORE-1.0.11-2007.01.19-1.tar.bz2
+http://downloads.sourceforge.net/sourceforge/mingw/msysCORE-1.0.11-20080826.tar.gz
 http://downloads.sourceforge.net/sourceforge/mingw/gettext-devel-0.16.1-MSYS-1.0.11-1.tar.bz2
 http://downloads.sourceforge.net/sourceforge/mingw/gettext-0.16.1-MSYS-1.0.11-1.tar.bz2
 """
