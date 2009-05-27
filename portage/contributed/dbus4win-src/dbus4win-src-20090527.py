b141f3309ec5e908a1f5ef5a0246d47fdefdad58
Frank Osterfeld <frank.osterfeld@kdemail.net>
2009-05-27 13:12:53 +0000

    sigh
    svn path=/trunk/kdesupport/emerge/; revision=973511

--- portage/contributed/dbus4win-src/dbus4win-src-20090527.py
+++ portage/contributed/dbus4win-src/dbus4win-src-20090527.py
@@ -7,7 +7,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['dbus4win-frank'] = 'http://www.kdab.net/~frank/dbus4win-20090527-2.zip'
+        self.targets['dbus4win-frank'] = 'http://www.kdab.net/~frank/dbus4win-20090527-3.zip'
         self.defaultTarget = 'dbus4win-frank'
     
     def setDependencies( self ):
@@ -17,7 +17,7 @@ class subinfo(info.infoclass):
 class subclass(base.baseclass):
   def __init__( self, **args ):
     base.baseclass.__init__( self, args=args )
-    self.instsrcdir = os.path.join( "dbus4win-20090527-2", "cmake" )
+    self.instsrcdir = os.path.join( "dbus4win-20090527-3", "cmake" )
     self.subinfo = subinfo()
     self.kdeCustomDefines = "-DDBUS_USE_EXPAT=ON -DDBUS_DISABLE_EXECUTABLE_DEBUG_POSTFIX=ON"
 
