4a540a2923bc59b905277d7eb9d3db536abc5191
Patrick Spendrin <ps_ml@gmx.de>
2009-06-01 02:21:06 +0000

    update to a working git package - it got bigger though
    svn path=/trunk/kdesupport/emerge/; revision=976140

--- portage/dev-util/git/git-1.6.2.2.py
+++ portage/dev-util/git/git-1.6.2.2.py
@@ -1,21 +1,16 @@
-# -*- coding: iso-8859-15 -*-
+# -*- coding: utf-8 -*-
 import base
 import info
 
-PACKAGE_NAME         = "Git"
-PACKAGE_VER          = "1.6.2.2"
-PACKAGE_FULL_VER     = "1.6.2.2"
-PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_FULL_VER)
-
 SRC_URI= """
-http://winkde.org/pub/kde/ports/win32/repository/other/"""  + PACKAGE_FULL_NAME + """.tar.bz2
+http://winkde.org/pub/kde/ports/win32/repository/other/Git-1.6.3-preview20090507-2.tar.bz2
 """
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.6.2.2'] = SRC_URI
-        self.targetInstSrc['1.6.2.2'] = PACKAGE_FULL_NAME
-        self.defaultTarget = '1.6.2.2'
+        self.targets['1.6.3'] = SRC_URI
+        self.targetInstSrc['1.6.3'] = PACKAGE_FULL_NAME
+        self.defaultTarget = '1.6.3'
     
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
