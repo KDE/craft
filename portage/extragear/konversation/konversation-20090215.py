8f886c626c92455722298752acbb90dd472d5367
Patrick Spendrin <ps_ml@gmx.de>
2009-06-02 19:31:06 +0000

    there have been three alpha releases in the last weeks - add them and make them ready for packaging
    svn path=/trunk/kdesupport/emerge/; revision=976840

--- portage/extragear/konversation/konversation-20090215.py
+++ portage/extragear/konversation/konversation-20090215.py
@@ -7,6 +7,9 @@ import sys
 
 class subinfo(info.infoclass):
     def setTargets( self ):
+        for i in ['1', '2', '3']:
+            self.targets['1.2alpha' + i] = 'http://download2.berlios.de/konversation/konversation-1.2-alpha' + i + '.tar.bz2'
+            self.targetInstSrc['1.2alpha' + i] = 'konversation-1.2-alpha' + i
         self.svnTargets['svnHEAD'] = 'trunk/extragear/network/konversation'
         self.defaultTarget = 'svnHEAD'
     
