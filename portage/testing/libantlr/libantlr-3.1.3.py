3d347d2f71ef9c46377555e7b7b821359a9720ca
Patrick Spendrin <ps_ml@gmx.de>
2009-06-05 00:21:46 +0000

    add "binary" package too
    svn path=/trunk/kdesupport/emerge/; revision=977674

---
+++ portage/testing/libantlr/libantlr-3.1.3.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import base
+import os
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        compiler = "vc90"
+        if os.getenv("KDECOMPILER") == "mingw":
+            compiler = "mingw"
+
+        if os.getenv("KDECOMPILER") == "msvc":
+            print "Error: this library has been compiled with a newer compiler, please use the -src package instead!"
+            sys.exit(1)
+
+        repoUrl = """http://downloads.sourceforge.net/kde-windows"""
+        
+        for version in ['3.1.3']:
+            self.targets[ version ] = repoUrl + """/libantlr-""" + compiler + """-""" + version + """-lib.tar.bz2"""
+
+        self.defaultTarget = '3.1.3'
+
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, "" )
+    self.subinfo = subinfo()
+
+if __name__ == '__main__':
+    subclass().execute()
