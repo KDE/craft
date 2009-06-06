3ebd7c4937d4ff901551acef46655d108e51c77a
Patrick Spendrin <ps_ml@gmx.de>
2009-06-06 22:04:04 +0000

    use only the marble subdirectory to compile
    svn path=/trunk/kdesupport/emerge/; revision=978387

--- portage/testing/marble-kde/marble-kde-20080415.py
+++ portage/testing/marble-kde/marble-kde-20080415.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import base
 import utils
 import os
@@ -7,12 +8,13 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/KDE/kdeedu'
+        self.svnTargets['marble-gsoc-2009'] = 'branches/marble/marble-gsoc-2009'
         self.svnTargets['4.1'] = 'branches/KDE/4.1/kdeedu'
         self.svnTargets['4.2'] = 'branches/KDE/4.2/kdeedu'
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
-        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['kde-4.2/kdebase-runtime'] = 'default'
     
 class subclass(base.baseclass):
     def __init__( self, **args ):
@@ -25,30 +27,8 @@ class subclass(base.baseclass):
 
     def compile( self ):
         self.kdeCustomDefines = ""
-        self.kdeCustomDefines += " -DBUILD_blinken=OFF"
-        self.kdeCustomDefines += " -DBUILD_doc=OFF"
-        self.kdeCustomDefines += " -DBUILD_kalzium=OFF"
-        self.kdeCustomDefines += " -DBUILD_kalgebra=OFF"
-        self.kdeCustomDefines += " -DBUILD_kanagram=OFF"
-        self.kdeCustomDefines += " -DBUILD_kbruch=OFF"
-        self.kdeCustomDefines += " -DBUILD_kgeography=OFF"
-        self.kdeCustomDefines += " -DBUILD_khangman=OFF"
-        self.kdeCustomDefines += " -DBUILD_kig=OFF"
-        self.kdeCustomDefines += " -DBUILD_kiten=OFF"
-        self.kdeCustomDefines += " -DBUILD_klettres=OFF"
-        self.kdeCustomDefines += " -DBUILD_kmplot=OFF"
-        self.kdeCustomDefines += " -DBUILD_kpercentage=OFF"
-        self.kdeCustomDefines += " -DBUILD_kstars=OFF"
-        self.kdeCustomDefines += " -DBUILD_ktouch=OFF"
-        self.kdeCustomDefines += " -DBUILD_kturtle=OFF"
-        self.kdeCustomDefines += " -DBUILD_parley=OFF"
-        self.kdeCustomDefines += " -DBUILD_kwordquiz=OFF"
-#        self.kdeCustomDefines += " -DBUILD_marble=OFF"
-        self.kdeCustomDefines += " -DBUILD_step=OFF"
-        self.kdeCustomDefines += " -DBUILD_keduvocdocument=OFF"
-        self.kdeCustomDefines += " -DBUILD_kdeeduui=OFF"
-        self.kdeCustomDefines += " -DBUILD_libscience=OFF"
-        
+        if not self.buildTarget == 'marble-gsoc-2009':
+            self.kde.sourcePath = os.path.join( self.kde.sourcePath, "marble" )
         self.kdeCustomDefines += " -DTILES_AT_COMPILETIME=OFF"
         return self.kdeCompile()
 
