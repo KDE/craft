62e3445f0ef583ca172af627deb203e2fe184c9a
Patrick Spendrin <ps_ml@gmx.de>
2009-06-06 23:08:04 +0000

    add jom source package, being the first package that uses a git repository.
    svn path=/trunk/kdesupport/emerge/; revision=978395

---
+++ portage/testing/jom-src/jom-src-20090601.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import base
+import os
+import utils
+import info
+import sys
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        """ """
+        self.svnTargets['svnHEAD'] = "git://qt.gitorious.org/qt-labs/jom.git"
+        self.defaultTarget = 'svnHEAD'
+
+    def setDependencies( self ):
+        self.hardDependencies['dev-util/msys'] = 'default'
+        self.hardDependencies['dev-util/git'] = 'default'
+        pass
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+        self.createCombinedPackage = True
+
+    def compile( self ):
+        self.kdeCustomDefines = "-DJOM_ENABLE_TESTS=ON"
+        self.kde.sourcePath = self.svndir
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def unittest( self ):
+        return self.kdeTest()
+
+    def make_package( self ):
+        self.kde.sourcePath = self.svndir
+        return True
+
+if __name__ == '__main__':
+    subclass().execute()
