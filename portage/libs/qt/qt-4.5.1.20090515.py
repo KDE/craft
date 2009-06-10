3310be3a30c86a5466fd187195730928125845c6
Ralf Habacker <ralf.habacker@freenet.de>
2009-06-10 08:48:54 +0000

    bug fix
    svn path=/trunk/kdesupport/emerge/; revision=979580

--- portage/libs/qt/qt-4.5.1.20090515.py
+++ portage/libs/qt/qt-4.5.1.20090515.py
@@ -109,12 +109,8 @@ class subclass(base.baseclass):
         else:
             exit( 1 )
 
-        if self.buildTarget == '4.4.3-3':
-          os.environ[ "USERIN" ] = "y"
-          userin = "y"
-        else:
-          os.environ[ "USERIN" ] = "oy"
-          userin = "oy"
+        os.environ[ "USERIN" ] = "y"
+        userin = "y"
         os.chdir( qtbindir )
         command = r"echo %s | %s -platform %s -prefix %s " \
           "-qt-gif -qt-libpng -qt-libjpeg -qt-libtiff " \
