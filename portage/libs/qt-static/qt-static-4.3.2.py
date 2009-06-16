b40cce4a84c128292b05cec3b54b34b9f8f885ed
Patrick Spendrin <ps_ml@gmx.de>
2009-06-16 23:18:06 +0000

    make qt-static use getPackage function too, attention target has been updated to 4.5.1-1
    svn path=/trunk/kdesupport/emerge/; revision=982873

--- portage/libs/qt-static/qt-static-4.3.2.py
+++ portage/libs/qt-static/qt-static-4.3.2.py
@@ -3,22 +3,12 @@ import base
 import os
 import info
 
-SRC_URI= """
-http://www.winkde.org/pub/kde/ports/win32/repository/other/qt-static-msvc-4.3.2-bin.zip
-http://www.winkde.org/pub/kde/ports/win32/repository/other/qt-static-msvc-4.3.2-lib.zip
-http://www.winkde.org/pub/kde/ports/win32/repository/other/qt-static-msvc-4.3.2-doc.zip
-"""
-
-SRC_URI2 = """
-http://www.winkde.org/pub/kde/ports/win32/repository/other/qt-static-msvc-4.5.1-1-bin.tar.bz2
-http://www.winkde.org/pub/kde/ports/win32/repository/other/qt-static-msvc-4.5.1-1-lib.tar.bz2
-"""
-
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['4.3.2'] = SRC_URI
-        self.targets['4.5.1'] = SRC_URI2
-        self.defaultTarget = '4.5.1'
+        repoUrl = "http://www.winkde.org/pub/kde/ports/win32/repository/other"
+        for version in ['4.3.2', '4.5.1-1']:
+            self.targets[ version ] = self.getPackage( repoUrl, "qt-static", version )
+        self.defaultTarget = '4.5.1-1'
     
 class subclass(base.baseclass):
   def __init__(self, **args):
