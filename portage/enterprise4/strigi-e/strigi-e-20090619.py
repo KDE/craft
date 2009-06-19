d47140adc9516495d7513835ff3773dd15153f4f
Allen Winter <winter@kde.org>
2009-06-19 21:37:55 +0000

    strigi-e, from kdesupport/strigi
    svn path=/trunk/kdesupport/emerge/; revision=984075

---
+++ portage/enterprise4/strigi-e/strigi-e-20090619.py
@@ -0,0 +1,51 @@
+import base
+import utils
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['enterprise4/qt-e'] = 'default'
+        self.hardDependencies['kdesupport/clucene-core'] = 'default'
+        self.hardDependencies['win32libs-sources/exiv2-src'] = 'default'
+        self.hardDependencies['win32libs-sources/iconv-src'] = 'default'
+        self.hardDependencies['win32libs-sources/bzip2-src'] = 'default'
+        self.hardDependencies['win32libs-sources/libxml2-src'] = 'default'
+        self.hardDependencies['win32libs-sources/zlib-src'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['0.5.7'] = 'tags/strigi/strigi/0.5.7'
+        self.svnTargets['0.5.8'] = 'tags/strigi/strigi/0.5.8'
+        self.svnTargets['0.5.9'] = 'tags/strigi/strigi/0.5.9'
+        self.svnTargets['0.5.10'] = 'tags/strigi/strigi/0.5.10'
+        self.svnTargets['0.5.11'] = 'tags/strigi/strigi/0.5.11'
+        self.svnTargets['0.6.3']  = 'tags/strigi/strigi/0.6.3'
+        self.svnTargets['0.6.4']  = 'tags/strigi/strigi/strigi-0.6.4'
+        self.svnTargets['0.6.5']  = 'tags/strigi/strigi/0.6.5'
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/strigi'
+        self.defaultTarget = 'svnHEAD'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.instsrcdir = "strigi"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if self.buildTarget == "svnHEAD":
+            return self.doPackaging( "strigi" )
+        else:
+            return self.doPackaging( "strigi", self.buildTarget, True )
+
+if __name__ == '__main__':
+    subclass().execute()
