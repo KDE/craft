d888ed5b4fbb88025b9df628d546c575f40ded7d
Allen Winter <winter@kde.org>
2009-06-19 20:52:44 +0000

    add automoc-e, from kdesupport/automoc
    svn path=/trunk/kdesupport/emerge/; revision=984055

---
+++ portage/enterprise4/automoc-e/automoc-e-20090619.py
@@ -0,0 +1,37 @@
+import base
+import utils
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['enterprise4/qt-e'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/automoc'
+        self.defaultTarget = 'svnHEAD'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.instsrcdir = "automoc"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if self.buildTarget == "svnHEAD":
+            return self.doPackaging( "automoc" )
+        else:
+            return self.doPackaging( "automoc", self.buildTarget, True )
+
+if __name__ == '__main__':
+    subclass().execute()
