5e59d51d17430fd2e5ca1abeb3a0c1e094294b01
Allen Winter <winter@kde.org>
2009-06-21 20:13:26 +0000

    add kdepimlibs-branch
    svn path=/trunk/kdesupport/emerge/; revision=984972

---
+++ portage/enterprise4/kdepimlibs-branch/kdepimlibs-branch-20090621.py
@@ -0,0 +1,47 @@
+import base
+import utils
+import os
+import sys
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'branches/kdepim/enterprise4/kdepimlibs'
+        for ver in ['74', '80', '83']:
+          self.targets['4.0.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.0.' + ver + '/src/kdepimlibs-enterprise4-4.0.' + ver + '.tar.bz2'
+          self.targetInstSrc['4.0.' + ver] = 'kdepimlibs-enterprise4-4.0.' + ver
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['enterprise4/kdelibs-branch'] = 'default'
+        self.hardDependencies['win32libs-bin/cyrus-sasl'] = 'default'
+        self.hardDependencies['win32libs-bin/gpgme'] = 'default'
+        self.hardDependencies['enterprise4/akonadi-e'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        # add env var so that boost headers are found
+        path = os.path.join( self.rootdir, "win32libs" )
+        os.putenv( "BOOST_ROOT", path )
+
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if not self.buildTarget == 'svnHEAD':
+            return self.doPackaging( "kdepimlibs-enterprise4", self.buildTarget, True )
+        else:
+            return self.doPackaging( "kdepimlibs-enterprise4", os.path.basename(sys.argv[0]).replace("kdepimlibs-", "").replace(".py", ""), True )
+
+
+if __name__ == '__main__':
+    subclass().execute()
