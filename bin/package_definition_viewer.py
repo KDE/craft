b5c016bcf85f69c5d88d9e56a34296bcabede5bc
Ralf Habacker <ralf.habacker@freenet.de>
2009-06-28 19:05:55 +0000

    adapted to merge xml schema
    svn path=/trunk/kdesupport/emerge/; revision=988807

--- bin/package_definition_viewer.py
+++ bin/package_definition_viewer.py
@@ -4,55 +4,102 @@ import re
 import os, sys
 
 TAGDOKU = dict()
-TAGDOKU['headline'] = """The tags for kdewin-packager's xml files are the following:"""
-TAGDOKU['module']   = """the root element.  It can have an attribute 'name' which contains the """ \
-                    + """name of the package and thus defines the name of the 'default' package. """ \
-                    + """There has to be one xml file per KDE-module."""
-TAGDOKU['package']  = """defines a single package, which can result in multiple packages (bin, """ \
-                    + """lib, doc, src, debug, ...) with the name given by the (required) """ \
-                    + """attribute 'name'. The package with the special name 'default' will be """ \
-                    + """used to dump the remaining files. It will result in an error, if the """ \
-                    + """attribute 'empty' has been set to 'true', for all other package names """ \
-                    + """the result of the attribute 'empty' is undefined. There can be """ \
-                    + """multiple 'package' tags per 'module'."""
-TAGDOKU['regexp']   = """Is a child tag of 'package' which contains a perl compatible regular """ \
-                    + """expression for the files that should be contained in the package. """ \
-                    + """It is possible to have multiple 'regexp' tags per 'package' tag. They """ \
-                    + """will be executed subsequently."""
-TAGDOKU['ignore']   = """Is a child tag of 'package' which contains a perl compatible regular """ \
-                    + """expression for the files that should not be contained in any package. """ \
-                    + """It is possible to have multiple 'ignore' tags per 'package' tag. They """ \
-                    + """will be executed subsequently. 'ignore' tags will always precede """ \
-                    + """'regexp' tags."""
-TAGDOKU['shortDescription'] = """Is a child tag of 'package' and contains the shortDescription used """\
-                            + """in the Installer. NOTE: there is no other description yet."""
+TAGDOKU['headline'] = """
+The tags for kdewin-packager's xml files are the following:
+"""
+
+TAGDOKU['module']   = """
+the root element.  It can have an attribute 'name' which contains the 
+name of the package and thus defines the name of the 'default' package.
+There has to be one xml file per KDE-module.
+"""
+
+TAGDOKU['package']  = """
+defines a single package, which can have multiple package components
+(called parts). The name of the package is determined by the (required)
+attribute 'name'. There can be multiple 'package' tags per 'module'.
+"""
+
+TAGDOKU['part']  =  """
+defines a package component (called parts). The known package components
+are "runtime", "development", "documentation", "source" and "debug" 
+The generated archive files contains related terms (bin, lib, doc, src, debug)
+in the filename. There can be multiple 'part' tags per 'package'.
+"""
+TAGDOKU['files'] = """
+Is a child tag of 'part' which describes a set of files for this package
+component. There are attributes available for this tag: 
+
+	include="<regexp>" 
+		contains a specification of a perl compatible 
+		regular expression for the files that should be 
+		contained in the package. The tag content should 
+		be empty in this case, which means the tag could 
+		also be defined as <files include=".*"/>
+		
+When not using any attribute one can add relative file pathes 
+separated by ';' or '\n' into the content of this tag. 
+It is possible to have multiple 'files' tags per 'part' tag. They
+will be executed subsequently.
+"""
+
+TAGDOKU['ignore']   = """
+Is a child tag of 'package' which contains a perl compatible regular
+expression for the files that should not be contained in any package.
+It is possible to have multiple 'ignore' tags per 'package' tag. They
+will be executed subsequently. 'ignore' tags will always precede
+regexp' tags."""
+
+TAGDOKU['shortDescription'] = """
+Is a child tag of 'package' and contains the shortDescription used
+in the Installer. NOTE: there is no other description yet.
+"""
+
+TAGDOKU['dependency'] = """
+Is a child tag of 'package' and contains a package dependency of this 
+package. It is possible to have multiple 'dependency' tags per 'package' 
+tag.
+"""
+class Part:
+    def __init__( self, name='' ):
+        self.name = name
+        self.reList = []
+        self.filesList = []
+        self.ignoreList = []
+        self.ignoreFileList = []
+        self.fileList = []
+
+    def printFileList( self ):
+        for fileName in self.fileList:
+            print fileName
+
+    def addFiles( self, include ):
+        if include is None:
+            return
+        for file in include.split('\n'):
+            self.filesList.append(file)
+
+    def addInclude( self, include ):
+        self.reList.append( re.compile( include ) )
+
+    def addIgnore( self, regexp ):
+        self.ignoreList.append( re.compile( regexp ) )
 
 class SinglePackage:
     def __init__( self, name='' ):
         self.description = ''
         self.shortDescription = ''
         self.name = name
-        self.fileList = []
-        self.reList = []
-        self.ignoreList = []
-        self.ignoreFileList = []
         self.isEmpty = False
-
-    def printFileList( self ):
-        for fileName in self.fileList:
-            print fileName
-
-    def addRegExp( self, regexp ):
-        self.reList.append( re.compile( regexp ) )
-
-    def addIgnore( self, regexp ):
-        self.ignoreList.append( re.compile( regexp ) )
+        self.partList = list()
 
     def __repr__( self ):
         return self.name + ' - ' + self.shortDescription
+
         
 class XmlPackager:
     packageList = list()
+    fileList = []
     def __init__( self, document, imagepath ):
         self.imagepath = imagepath
         self._checkTag( document.getElementsByTagName( 'module' )[0] )
@@ -78,11 +125,22 @@ class XmlPackager:
                 package.description = self._checkTag( element.getElementsByTagName( 'description' )[0] )
             if len( element.getElementsByTagName( 'shortDescription' ) ) > 0:
                 package.shortDescription = self._checkTag( element.getElementsByTagName( 'shortDescription' )[0] )
-            for i in range( len( element.getElementsByTagName( 'regexp' ) ) ):
-                package.addRegExp( self._checkTag( element.getElementsByTagName( 'regexp' )[i] ) )
             for i in range( len( element.getElementsByTagName( 'ignore' ) ) ):
                 package.addIgnore( self._checkTag( element.getElementsByTagName( 'ignore' )[i] ) )
+            for part in element.getElementsByTagName( 'part' ):
+                package.partList.append(self._checkTag(part))
             self.packageList.append( package )            
+
+    def handle_partTag( self, element ):
+        if element.hasAttribute( 'name' ):
+            part = Part(element.getAttribute( 'name' ))
+            for i in range( len( element.getElementsByTagName( 'files' ) ) ):
+                elem = element.getElementsByTagName( 'files' )[i]
+                if elem.hasAttribute( 'include' ):
+                    part.addInclude(elem.getAttribute( 'include' ))
+                if len( elem.childNodes ) > 0:
+                    part.addFiles(elem.childNodes[0].nodeValue)
+        return part
 
     def handle_descriptionTag( self, element ):
         if len( element.childNodes ) > 0:
@@ -96,12 +154,6 @@ class XmlPackager:
         else:
             return None
 
-    def handle_regexpTag( self, element ):
-        if len( element.childNodes ) > 0:
-            return element.childNodes[0].nodeValue
-        else:
-            return None
-
     def handle_ignoreTag( self, element ):
         if len( element.childNodes ) > 0:
             return element.childNodes[0].nodeValue
@@ -109,69 +161,67 @@ class XmlPackager:
             return None
 
     def split( self ):
-        fileList = []
         for root, dirs, files in os.walk( self.imagepath ):
             shortRoot = root.replace( self.imagepath + os.sep, '' )
             for fileName in files:
                 name = os.path.join( shortRoot, fileName )
                 name = name.replace( os.sep, '/' )
-                fileList.append( name )
+                self.fileList.append( name )
 
-        defaultPackage = None
         for package in self.packageList:
-            for reg in package.ignoreList:
-                for line in fileList:
-                    regObj = reg.match( line )
-                    if regObj:
-                        package.ignoreFileList.append( line )
-                for z in package.ignoreFileList:
-                    if z in fileList:
-                        fileList.remove( z )
+            for part in package.partList:
+                for reg in part.ignoreList:
+                    for line in self.fileList:
+                        regObj = reg.match( line )
+                        if regObj:
+                            part.ignoreFileList.append( line )
+                    for z in part.ignoreFileList:
+                        if z in self.fileList:
+                            self.fileList.remove( z )
 
-            if package.name == 'default':
-                defaultPackage = package
-                continue
+                for reg in part.reList:
+                    for line in self.fileList:
+                        regObj = reg.match( line )
+                        if regObj:
+                            part.fileList.append( line )
+                    for z in part.fileList:
+                        if z in self.fileList:
+                            self.fileList.remove( z )
 
-            for reg in package.reList:
-                for line in fileList:
-                    regObj = reg.match( line )
-                    if regObj:
-                        package.fileList.append( line )
-                for z in package.fileList:
-                    if z in fileList:
-                        fileList.remove( z )
+                for file in part.filesList:
+                    for line in self.fileList:
+                        if line == file:
+                            part.fileList.append( line )
+                    for z in part.fileList:
+                        if z in self.fileList:
+                            self.fileList.remove( z )
 
-        if len(fileList) > 0:
-            if defaultPackage:
-                if defaultPackage.isEmpty:
-                    print "ERROR: The default package is supposed to be empty but there are files left over!"
-                for name in fileList:
-                    defaultPackage.fileList.append( name )
-            else:
-                print "ERROR: No default package is available but there are one or more files left over!"
-                exit(1)
-                        
+    def printUnusedFiles( self ):
+        if len(self.fileList) > 0:
+            print "unused files"
+            for name in self.fileList:
+                print name 
 
     def printOverview( self ):
         for package in self.packageList:
-            if len( package.ignoreFileList ) == 0:
-                print package, '\tcontaining', len( package.fileList ), 'files'
-            else:
-                print package, '\tcontaining', len( package.fileList ), 'files, ignoring', len( package.ignoreFileList ), 'files'
+            for part in package.partList:
+                if len( part.ignoreFileList ) == 0:
+                    print package.name + ' ' + part.name, '\tcontaining', len( part.fileList ), 'files'
+                else:
+                    print package.name + ' ' + part.name, '\tcontaining', len( part.fileList ), 'files, ignoring', len( package.ignoreFileList ), 'files'
 
-    def printList( self, name ):
+    def printList( self, name="" ):
         for package in self.packageList:
-            if package.name == name:
-                for filename in package.fileList:
-                    print filename
-                if len( package.ignoreFileList ) > 0:
-                    print 'ATTENTION: Ignoring:'
-                    for filename in package.ignoreFileList:
+            print "package  " + package.name
+            if name == "" or package.name == name:
+                for part in package.partList:
+                    print "part " + part.name
+                    for filename in part.fileList:
                         print filename
-
-                return
-        print 'ERROR: No suitable package was found!'
-        exit(1)
+                    if len( part.ignoreFileList ) > 0:
+                        print 'ATTENTION: Ignoring:'
+                        for filename in part.ignoreFileList:
+                            print filename
 
 def usage():
     print 'Syntax:'
@@ -197,12 +247,23 @@ def usage():
         print '- ' + tag
         print TAGDOKU[tag]
         print
-
+        
 if len( sys.argv ) > 2:
-    image = os.path.abspath( sys.argv[1] )
-    filename = sys.argv[2]
-    #image = 'C:\\kde\\kde-msvc\\tmp\\kdegraphics-20080202\\image-msvc2008'
-    #filename = 'C:\\kde\\kde-msvc\\emerge\\portage\\kde\\kdegraphics\\kdegraphics-package.xml'
+    i = 1
+    _printUnused = False
+    _printList = False
+    if sys.argv[i] == "--unused":
+        _printUnused = True
+        i = i+1
+    elif sys.argv[i] == "--list":
+        _printList = True
+        i = i+1
+    image = os.path.abspath( sys.argv[i] )
+    i = i+1
+    filename = sys.argv[i]
+    i = i+1
+    #image = 'C:\\daten\\kde\msvc-root\\tmp\\kdegraphics-20080202\\image-msvc2008'
+    #filename = 'C:\\daten\\kde\msvc-root\\emerge\\portage\\kde\\kdegraphics\\kdegraphics-package.xml'
 
     fileobj = open( filename )
 
@@ -214,7 +275,14 @@ if len( sys.argv ) > 2:
 
     packager = XmlPackager( doc, image )
     packager.split()
-
+    if _printUnused:
+        packager.printUnusedFiles()
+        exit(0)
+        
+    if _printList == 1:
+        packager.printList()
+        exit(0)
+    
     if len( sys.argv ) == 3:
         packager.printOverview()
 
