0ab82e033b214614862747a8c8cd61b065df8732
Patrick Spendrin <ps_ml@gmx.de>
2009-07-02 13:19:22 +0000

    add bugfix release
    svn path=/trunk/kdesupport/emerge/; revision=990395

--- portage/extragear/kipi-plugins/kipi-plugins-0.2.0.20090407.py
+++ portage/extragear/kipi-plugins/kipi-plugins-0.2.0.20090407.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import base
 import os
 import sys
@@ -9,6 +10,8 @@ class subinfo(info.infoclass):
         self.svnTargets['svnHEAD'] = 'trunk/extragear/graphics/kipi-plugins'
         self.targets['0.2.0'] = 'http://digikam3rdparty.free.fr/0.10.x-releases/kipi-plugins-0.2.0.tar.bz2'
         self.targetInstSrc['0.2.0'] = 'kipi-plugins-0.2.0'
+        self.targets['0.3.0'] = 'http://digikam3rdparty.free.fr/0.10.x-releases/kipi-plugins-0.3.0.tar.bz2'
+        self.targetInstSrc['0.3.0'] = 'kipi-plugins-0.3.0'
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
@@ -25,7 +28,7 @@ class subclass(base.baseclass):
         self.subinfo = subinfo()
 
     def unpack( self ):
-        if self.buildTarget in ['0.2.0']:
+        if self.buildTarget in ['0.2.0', '0.3.0']:
             return base.baseclass.unpack( self )
         else:
             return self.kdeSvnUnpack()
