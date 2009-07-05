3f64df71efce83f15bccd13bf0bfd71b9b21830f
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-05 10:45:47 +0000

    used new method patchToApply from info class
    svn path=/trunk/kdesupport/emerge/; revision=991567

--- bin/Source/SourceBase.py
+++ bin/Source/SourceBase.py
@@ -15,8 +15,8 @@ class SourceBase(EmergeBase):
         """ apply patches is available """
         utils.debug( "SourceBase.__applyPatches called", 1 )
 
-        if len( self.subinfo.targets ) and self.subinfo.buildTarget in self.subinfo.patchToApply.keys():
-            ( file, patchdepth ) = self.subinfo.patchToApply[ self.subinfo.buildTarget ]
+        if self.subinfo.hasTarget():
+            ( file, patchdepth ) = self.subinfo.patchToApply()
             patchfile = os.path.join ( self.packagedir, file )
             srcdir = os.path.join ( self.workdir, self.instsrcdir )
             return utils.applyPatch( patchfile, srcdir, patchdepth )
