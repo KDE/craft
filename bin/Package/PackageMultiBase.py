a28dbf81f3c63a434310115bc3b61174d53d5656
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-06 18:19:48 +0000

    removed methods already defined in PackageBase
    svn path=/trunk/kdesupport/emerge/; revision=992377

--- bin/Package/PackageMultiBase.py
+++ bin/Package/PackageMultiBase.py
@@ -34,54 +34,6 @@ class PackageMultiBase (PackageBase):
         
         PackageBase.execute(self)
                     
-    def qmerge( self ):
-        """mergeing the imagedirectory into the filesystem"""
-        for pkgtype in ['bin', 'lib', 'doc', 'src']:
-            script = os.path.join( self.packagedir, "post-install-%s.cmd" ) % pkgtype
-            scriptName = "post-install-%s-%s-%s.cmd" % ( self.package, self.version, pkgtype )
-            destscript = os.path.join( self.imageDir(), "manifest", scriptName )
-            if not os.path.exists( os.path.join( self.imageDir(), "manifest" ) ):
-                os.mkdir( os.path.join( self.imageDir(), "manifest" ) )
-            if os.path.exists( script ):
-                shutil.copyfile( script, destscript )
-                             
-        utils.mergeImageDirToRootDir( self.imageDir(), self.rootdir )
-
-        # run post-install scripts
-        for pkgtype in ['bin', 'lib', 'doc', 'src']:
-            scriptName = "post-install-%s-%s-%s.cmd" % ( self.package, self.version, pkgtype )
-            script = os.path.join( self.rootdir, "manifest", scriptName )
-            if os.path.exists( script ):
-                cmd = "cd %s && %s" % ( self.rootdir, script )
-                utils.system( cmd ) or utils.warning("%s failed!" % cmd )
-        utils.addInstalled( self.category, self.package, self.version )
-        return True
-
-    def unmerge( self ):
-        """unmergeing the files from the filesystem"""
-        if utils.verbose() > 1:
-            print "base unmerge called"
-        utils.unmerge( self.rootdir, self.package, self.forced )
-        utils.remInstalled( self.category, self.package, self.version )
-        return True
-
-    def cleanup( self ):
-        """cleanup before install to imagedir"""
-        if ( os.path.exists( self.imageDir() ) ):
-            utils.debug( "cleaning image dir: %s" % self.imageDir(), 1 )
-            utils.cleanDirectory( self.imageDir() )
-        return True
-        
-    def manifest( self ):
-        """installer compatibility: make the manifest files that make up the installers"""
-        """install database"""
-        print "manifest should be created on qmerge "
-    
-        if utils.verbose() > 1:
-            print "base manifest called"
-        utils.manifestDir( os.path.join( self.workDir(), self.instsrcdir, self.package ), self.imagedir, self.category, self.package, self.version )
-        return True
-
     def fetch(self):
         return self.source.fetch()
         
