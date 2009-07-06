88e480950ce7421976bf820e4bd70f2904e05b27
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-06 04:39:51 +0000

    added new module PackagerFactory, which creates the requested Packager object
    svn path=/trunk/kdesupport/emerge/; revision=991955

---
+++ bin/Packager/PackagerFactory.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from KDEWinPackager import *;
+from CPackPackager import *;
+from MSInstallerPackager import *;
+
+import info
+import utils
+
+def PackagerFactory(packagerType, buildSystem):
+    """ return PackagerBased derived instance """
+    utils.debug( "PackagerFactory called", 1 )
+
+    packager = None
+    if packagerType == 'KDEWin' or packagerType == None:
+        packager = KDEWinPackager()
+    elif packagerType == 'CPack':
+        packager = CPackPackager()
+    else:
+        utils.die("none or unsupported packager set, use self.packagerType='type', where type could be 'KDEWin'")
+
+    packager.subinfo = buildSystem.subinfo
+    packager.buildSystem = buildSystem
+    packager.source = buildSystem.source
+    return packager
