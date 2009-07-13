1845aae6f4032e59795d5f9d4c528188bdac4d59
Patrick Spendrin <ps_ml@gmx.de>
2009-07-13 12:36:53 +0000

    update marble-kde dependencies
    svn path=/trunk/kdesupport/emerge/; revision=995805

--- portage/testing/marble-kde/marble-kde-20080415.py
+++ portage/testing/marble-kde/marble-kde-20080415.py
@@ -9,12 +9,13 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/KDE/kdeedu'
         self.svnTargets['marble-gsoc-2009'] = 'branches/marble/marble-gsoc-2009'
-        self.svnTargets['4.1'] = 'branches/KDE/4.1/kdeedu'
         self.svnTargets['4.2'] = 'branches/KDE/4.2/kdeedu'
+        self.svnTargets['4.3'] = 'branches/KDE/4.3/kdeedu'
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
-        self.hardDependencies['kde-4.2/kdebase-runtime'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+#        self.hardDependencies['kde-4.3/kdebase-runtime'] = 'default'
     
 class subclass(base.baseclass):
     def __init__( self, **args ):
