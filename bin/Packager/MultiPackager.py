0ac70077ab78d0e6976b634fc36182e6cd0e0d1f
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-18 20:39:43 +0000

    adde MultiPackager class
    svn path=/trunk/kdesupport/emerge/; revision=998977

---
+++ bin/Packager/MultiPackager.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from KDEWinPackager import *;
+from CPackPackager import *;
+from MSInstallerPackager import *;
+
+import info
+import utils
+
+class MultiPackager():
+    def __init__(self,packagerType=None):
+        utils.debug( "MultiPackager __init__", 1 )
+        self.packager = None
+        if packagerType == 'KDEWin' or packagerType == 'kdewin' or packagerType == None:
+            self.packager = KDEWinPackager()
+        elif packagerType == 'CPack':
+            self.packager = CPackPackager()
+        else:
+            utils.die("none or unsupported packager set, use self.packagerType='type', where type could be 'KDEWin'")
+
+        self.packager.subinfo = self.subinfo
+        self.packager.parent = self
+
+    def createPackage(self):
+        return self.packager.createPackage()
+        
+    def make_package(self):
+        return self.packager.createPackage()
