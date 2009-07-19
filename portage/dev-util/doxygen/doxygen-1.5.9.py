c6ea5a623d611db8c50e92215ebe765f18cd26d3
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-19 12:00:50 +0000

    added doxygen for emerge docu
    svn path=/trunk/kdesupport/emerge/; revision=999192

---
+++ portage/dev-util/doxygen/doxygen-1.5.9.py
@@ -0,0 +1,26 @@
+import base
+import info
+
+from Source.MultiSource import *
+from BuildSystem.BinaryBuildSystem import *
+from Package.PackageBase import *
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.targets['1.5.9'] = 'http://ftp.stack.nl/pub/users/dimitri/doxygen-1.5.9.windows.bin.zip'
+        self.defaultTarget = '1.5.9'
+        # the zip file does not have a bin dir, so we have to create it  
+        self.targetInstallPath['1.5.9'] = "bin";
+        # merge the package into the dev-utils tree 
+        self.targetMergePath['1.5.9'] = "dev-utils";
+    
+class Package(PackageBase, MultiSource, BinaryBuildSystem):
+    def __init__( self):
+        self.subinfo = subinfo()
+        PackageBase.__init__(self)
+        MultiSource.__init__(self)
+        BinaryBuildSystem.__init__(self)
+        # no packager required 
+
+if __name__ == '__main__':
+    Package().execute()
