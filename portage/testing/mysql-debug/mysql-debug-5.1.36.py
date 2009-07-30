846c4a1a3f32fd9a69564cf73f1fd135e4a0ae48
Patrick Spendrin <ps_ml@gmx.de>
2009-07-30 16:19:12 +0000

    split up the mysql package into one part for the server, one for the embedded server and a debug package
    svn path=/trunk/kdesupport/emerge/; revision=1004626

---
+++ portage/testing/mysql-debug/mysql-debug-5.1.36.py
@@ -0,0 +1,26 @@
+import base
+import os
+import utils
+import info
+import shutil
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        """ """
+        self.targets['5.1.36'] = """
+http://winkde.org/pub/kde/ports/win32/repository/win32libs/mysql-debug-5.1.36-bin.tar.bz2
+"""
+        self.targetInstSrc['5.1.36'] = ""
+        self.defaultTarget = '5.1.36'
+
+    def setDependencies( self ):
+        """ """
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+        
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+if __name__ == '__main__':
+    subclass().execute()
---
+++ portage/testing/mysql-embedded/mysql-embedded-5.1.36.py
@@ -0,0 +1,28 @@
+import base
+import os
+import utils
+import info
+import shutil
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        """ """
+        self.targets['5.1.36'] = """
+http://winkde.org/pub/kde/ports/win32/repository/win32libs/mysql-embedded-5.1.36-bin.tar.bz2
+http://winkde.org/pub/kde/ports/win32/repository/win32libs/mysql-embedded-5.1.36-lib.tar.bz2
+"""
+        self.targetInstSrc['5.1.36'] = ""
+        self.defaultTarget = '5.1.36'
+
+    def setDependencies( self ):
+        """ """
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+        self.hardDependencies['testing/mysql-server'] = 'default' # the include files are used in the mysql-server package already
+        
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.subinfo = subinfo()
+
+if __name__ == '__main__':
+    subclass().execute()
