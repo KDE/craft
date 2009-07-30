c6ab2b685f0285beae2b7611d09fd98cb920a346
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-30 11:30:41 +0000

    migramigrated to refactored emerge
    svn path=/trunk/kdesupport/emerge/; revision=1004510

--- portage/gnuwin32/grep/grep-2.5.4.py
+++ portage/gnuwin32/grep/grep-2.5.4.py
@@ -11,14 +11,22 @@ class subinfo(info.infoclass):
         for t in ( '2.5.1a', '2.5.4' ):
           self.targets[ t ] = SRC_URI % ( t, t )
         self.defaultTarget = '2.5.4'
+        self.targetMergePath['2.5.4'] = "dev-utils";
     
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
-    
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, args=args )
-    self.subinfo = subinfo()
+
+from Source.ArchiveSource import *
+from BuildSystem.BinaryBuildSystem import *
+from Package.PackageBase import *
+
+class Package(PackageBase, ArchiveSource, BinaryBuildSystem):
+    def __init__( self):
+        self.subinfo = subinfo()
+        PackageBase.__init__(self)
+        ArchiveSource.__init__(self)
+        BinaryBuildSystem.__init__(self)
+        # no packager required 
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
