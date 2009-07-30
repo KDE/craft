59d255c5de16428331e85a14584e5482255c7a19
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-30 08:09:39 +0000

    migrated to refactored emerge
    svn path=/trunk/kdesupport/emerge/; revision=1004450

--- portage/dev-util/perl/perl-5.10.0.1004.py
+++ portage/dev-util/perl/perl-5.10.0.1004.py
@@ -7,12 +7,21 @@ class subinfo(info.infoclass):
         self.targets['5.10.0'] = "http://downloads.activestate.com/ActivePerl/Windows/5.10/ActivePerl-5.10.0.1004-MSWin32-x86-287188.zip"
         self.targetInstSrc['5.8.8'] = "ActivePerl-5.8.8.822-MSWin32-x86-280952\\perl"
         self.targetInstSrc['5.10.0'] = "ActivePerl-5.10.0.1004-MSWin32-x86-287188\\perl"
+        self.targetMergePath['5.8.8'] = "dev-utils";
+        self.targetMergePath['5.10.0'] = "dev-utils";
         self.defaultTarget = '5.10.0'
         
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, args=args )
-    self.subinfo = subinfo()
-		
+from Package.PackageBase import *
+from Source.ArchiveSource import *
+from BuildSystem.BinaryBuildSystem import *
+
+class Package(PackageBase, ArchiveSource, BinaryBuildSystem):
+    def __init__( self):
+        self.subinfo = subinfo()
+        PackageBase.__init__(self)
+        ArchiveSource.__init__(self)
+        BinaryBuildSystem.__init__(self)
+        # no packager required 
+
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
