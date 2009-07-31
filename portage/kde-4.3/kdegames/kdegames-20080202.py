1d56643d9b79180a2b34dbc5654211921ed52737
Carlo Segato <brandon.ml@gmail.com>
2009-07-31 18:31:39 +0000

    add dependency to qca here too
    svn path=/trunk/kdesupport/emerge/; revision=1005284

--- portage/kde-4.3/kdegames/kdegames-20080202.py
+++ portage/kde-4.3/kdegames/kdegames-20080202.py
@@ -18,6 +18,7 @@ class subinfo(info.infoclass):
     
     def setDependencies( self ):
         self.hardDependencies['kde-4.3/kdebase-runtime'] = 'default'
+        self.hardDependencies['kdesupport/qca'] = 'default'
         
 class subclass(base.baseclass):
     def __init__( self, **args ):
