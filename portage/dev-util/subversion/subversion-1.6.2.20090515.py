f49f089886e2327b5464cb1da373bfaad2b58fe5
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-31 19:45:12 +0000

    migrated to refactored emerge
    svn path=/trunk/kdesupport/emerge/; revision=1005317

--- portage/dev-util/subversion/subversion-1.6.2.20090515.py
+++ portage/dev-util/subversion/subversion-1.6.2.20090515.py
@@ -3,22 +3,21 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.4.6'] = "http://subversion.tigris.org/files/documents/15/41094/svn-win32-1.4.6.zip"
-        self.targets['1.5.5'] = "http://subversion.tigris.org/files/documents/15/44589/svn-win32-1.5.5.zip"
         self.targets['1.6.1'] = "http://subversion.tigris.org/files/documents/15/45600/svn-win32-1.6.1.zip"
-        self.targetInstSrc['1.4.6'] = "svn-win32-1.4.6"
-        self.targetInstSrc['1.5.5'] = "svn-win32-1.5.5"
-        self.targetInstSrc['1.6.1'] = "svn-win32-1.6.1"
+        # this location affects class SvnSource 
+        self.targetMergePath['1.6.1'] = "dev-utils/svn";
+        self.targetMergeSourcePath['1.6.1'] = "svn-win32-1.6.1";
         self.defaultTarget = '1.6.1'
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
+   
+from Package.BinaryPackageBase import *
 
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, args=args )
-    self.instdestdir = "dev-utils"
-    self.subinfo = subinfo()
+class Package(BinaryPackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        BinaryPackageBase.__init__(self)
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
