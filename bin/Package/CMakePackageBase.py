bcf9e8f19807f3f05ba1d4beae27eb30840be285
Ralf Habacker <ralf.habacker@freenet.de>
2009-07-31 22:53:04 +0000

    added PackageBase for cmake projects - it uses multisource, so any source could be used
    svn path=/trunk/kdesupport/emerge/; revision=1005396

---
+++ bin/Package/CMakePackageBase.py
@@ -0,0 +1,14 @@
+
+from PackageBase import *;
+from Source.MultiSource import *;
+from BuildSystem.CMakeBuildSystem import *;
+from Packager.KDEWinPackager import *;
+
+class CMakePackageBase (PackageBase, MultiSource, CMakeBuildSystem, KDEWinPackager):
+    """provides a base class for cmake packages from any source"""
+    def __init__(self):
+        utils.debug("CMakePackageBase.__init__ called",2)
+        PackageBase.__init__(self)
+        MultiSource.__init__(self)
+        CMakeBuildSystem.__init__(self)
+        KDEWinPackager.__init__(self)
