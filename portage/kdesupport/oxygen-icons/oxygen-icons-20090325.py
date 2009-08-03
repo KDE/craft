f7ee7298d7770d65720a8b38fdc0dc59c62c38a0
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-03 10:02:55 +0000

    migrated to refactored emerge
    svn path=/trunk/kdesupport/emerge/; revision=1006248

--- portage/kdesupport/oxygen-icons/oxygen-icons-20090325.py
+++ portage/kdesupport/oxygen-icons/oxygen-icons-20090325.py
@@ -1,6 +1,3 @@
-import base
-import utils
-import sys
 import info
 
 class subinfo(info.infoclass):
@@ -11,28 +8,16 @@ class subinfo(info.infoclass):
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/oxygen-icons'
         for i in ['4.3.0', '4.3.1', '4.3.2', '4.3.3', '4.3.4', '4.3']:
             self.svnTargets[ i ] = 'tags/kdesupport-for-4.3/kdesupport/oxygen-icons'
+            self.targetInstSrc[ i ] = "oxygen-icons"
+            
         self.defaultTarget = 'svnHEAD'
 
-class subclass(base.baseclass):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "oxygen-icons"
+from Package.CMakePackageBase import *
+        
+class Package(CMakePackageBase):
+    def __init__( self ):
         self.subinfo = subinfo()
-
-    def unpack( self ):
-        return self.kdeSvnUnpack()
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        if self.buildTarget == "svnHEAD":
-            return self.doPackaging( "oxygen-icons" )
-        else:
-            return self.doPackaging( "oxygen-icons", self.buildTarget, True )
+        CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
