af8eecd46e114f1b432c3f583d01d72ad3d9b684
Patrick Spendrin <ps_ml@gmx.de>
2009-08-03 23:32:29 +0000

    fix packaging of oxygen-icons
    svn path=/trunk/kdesupport/emerge/; revision=1006589

--- portage/kdesupport/oxygen-icons/oxygen-icons-20090325.py
+++ portage/kdesupport/oxygen-icons/oxygen-icons-20090325.py
@@ -13,11 +13,14 @@ class subinfo(info.infoclass):
         self.defaultTarget = 'svnHEAD'
 
 from Package.CMakePackageBase import *
-        
-class Package(CMakePackageBase):
+from Packager.KDEWinPackager import *
+
+class Package(CMakePackageBase, KDEWinPackager):
     def __init__( self ):
         self.subinfo = subinfo()
+        self.createCombinedPackage = True
         CMakePackageBase.__init__( self )
+        KDEWinPackager.__init__( self )
 
 if __name__ == '__main__':
     Package().execute()
