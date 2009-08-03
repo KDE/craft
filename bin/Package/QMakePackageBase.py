12cc357d12f2144145d4b2f90bff28c9995fecaa
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-03 08:33:32 +0000

    new file
    svn path=/trunk/kdesupport/emerge/; revision=1006205

---
+++ bin/Package/QMakePackageBase.py
@@ -0,0 +1,14 @@
+
+from PackageBase import *;
+from Source.MultiSource import *;
+from BuildSystem.QMakeBuildSystem import *;
+from Packager.KDEWinPackager import *;
+
+class QMakePackageBase (PackageBase, MultiSource, QMakeBuildSystem, KDEWinPackager):
+    """provides a base class for qmake packages from any source"""
+    def __init__(self):
+        utils.debug("QMakePackageBase.__init__ called",2)
+        PackageBase.__init__(self)
+        MultiSource.__init__(self)
+        QMakeBuildSystem.__init__(self)
+        KDEWinPackager.__init__(self)
