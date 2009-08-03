e219f5e4e4b60a5444b36b2d93a18a1b1e471f44
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-03 13:28:27 +0000

    cleaned up
    svn path=/trunk/kdesupport/emerge/; revision=1006326

--- bin/Source/SourceFactory.py
+++ bin/Source/SourceFactory.py
@@ -15,34 +15,19 @@ def SourceFactory(settings):
     
     if settings.hasTarget():
         url = settings.target()
-        if utils.verbose > 1:
-            print "foung archive target with url=" + url
         source = ArchiveSource()
 
     ## \todo move settings access into info class 
     if settings.hasSvnTarget():
         url = settings.svnTarget()
-        if utils.verbose > 1:
-            print "found svn target with url=" + url
         if url.find("://") == -1: 
-            if utils.verbose > 1:
-                print "found kde svn target with url=" + url
             source = KDESvnSource()
-            
         elif url.find("git:") >= 0:
-            if utils.verbose > 1:
-                print "found git target with url=" + url
             source = GitSource()
-         
         elif url.find("svn:") >= 0 or url.find("https:") >= 0 or url.find("http:") >= 0:
-            if utils.verbose > 1:
-                print "found svn target with url=" + url
             source = SvnSource()
-
         ## \todo complete more cvs access schemes 
         elif url.find("pserver:") >= 0: 
-            if utils.verbose > 1:
-                print "found cvs target with url=" + url
             source = CvsSource()
 
     if source == None:
