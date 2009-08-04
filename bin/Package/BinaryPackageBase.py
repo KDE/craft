b732460744812202be46fa0981ad4fa74f6eb834
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-04 07:38:32 +0000

    added also packager for binary package - it may be required to perform a repackage
    svn path=/trunk/kdesupport/emerge/; revision=1006641

--- bin/Package/BinaryPackageBase.py
+++ bin/Package/BinaryPackageBase.py
@@ -2,11 +2,13 @@
 from PackageBase import *;
 from Source.ArchiveSource import *;
 from BuildSystem.BinaryBuildSystem import *;
+from Packager.KDEWinPackager import *;
 
-class BinaryPackageBase (PackageBase, ArchiveSource, BinaryBuildSystem):
+class BinaryPackageBase (PackageBase, ArchiveSource, BinaryBuildSystem, KDEWinPackager):
     """provides a base class for binary packages"""
     def __init__(self):
         utils.debug("BinaryPackageBase.__init__ called",2)
         PackageBase.__init__(self)
         ArchiveSource.__init__(self)
         BinaryBuildSystem.__init__(self)
+        KDEWinPackager.__init__(self)
