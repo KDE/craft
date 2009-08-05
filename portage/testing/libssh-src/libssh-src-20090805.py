a0b9c5a863e4a2bffd677ef3b5ce77dd3210c484
Patrick Spendrin <ps_ml@gmx.de>
2009-08-05 21:37:48 +0000

    add package for libssh (cf. http://www.libssh.org/)
    svn path=/trunk/kdesupport/emerge/; revision=1007528

---
+++ portage/testing/libssh-src/libssh-src-20090805.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import base
+import utils
+import shutil
+from utils import die
+import os
+import info
+import re
+
+from Source.GitSource import *
+from BuildSystem.CMakeBuildSystem import *
+from Package.PackageBase import *
+from Packager.KDEWinPackager import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = "git://git.libssh.org/projects/libssh/libssh.git"
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+class Package(PackageBase, GitSource, CMakeBuildSystem, KDEWinPackager):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        GitSource.__init__(self)
+        CMakeBuildSystem.__init__(self)
+        PackageBase.__init__(self)
+        KDEWinPackager.__init__(self)
+        
+if __name__ == '__main__':
+    Package().execute()
