00f43e6fa748d1a18142671656edbefb436f73af
Carlo Segato <brandon.ml@gmail.com>
2009-08-05 14:47:04 +0000

    add alpha5 release, add dependencies to qca and kdepimlibs too
    svn path=/trunk/kdesupport/emerge/; revision=1007332

--- portage/extragear/konversation/konversation-20090215.py
+++ portage/extragear/konversation/konversation-20090215.py
@@ -7,7 +7,7 @@ import sys
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for i in ['1', '2', '3']:
+        for i in ['1', '2', '3', '5']:
             self.targets['1.2alpha' + i] = 'http://download2.berlios.de/konversation/konversation-1.2-alpha' + i + '.tar.bz2'
             self.targetInstSrc['1.2alpha' + i] = 'konversation-1.2-alpha' + i
         self.svnTargets['svnHEAD'] = 'trunk/extragear/network/konversation'
@@ -15,6 +15,8 @@ class subinfo(info.infoclass):
     
     def setDependencies( self ):
         self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['kdesupport/qca'] = 'default'
+        self.hardDependencies['kde/kdepimlibs'] = 'default'
     
 class subclass(base.baseclass):
     def __init__( self, **args ):
