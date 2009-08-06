1a2fc65441a187b958f6e2967a21eb3cf5930928
Patrick Spendrin <ps_ml@gmx.de>
2009-08-06 22:29:43 +0000

    fix it and build it from the games folder
    svn path=/trunk/kdesupport/emerge/; revision=1008119

--- portage/testing/kolf-ng/kolf-ng-20090807.py
+++ portage/testing/kolf-ng/kolf-ng-20090807.py
@@ -8,7 +8,7 @@ from Packager.KDEWinPackager import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = "trunk/playground/games/kolf-ng"
+        self.svnTargets['svnHEAD'] = "trunk/playground/games"
         self.defaultTarget = 'svnHEAD'
 
     def setDependencies( self ):
@@ -21,6 +21,32 @@ class Package(PackageBase, SvnSource, CMakeBuildSystem, KDEWinPackager):
         CMakeBuildSystem.__init__(self)
         PackageBase.__init__(self)
         KDEWinPackager.__init__(self)
+        defines = ""
+        defines += " -DBUILD_doc=OFF"
+        defines += " -DBUILD_libksirtet=OFF"
+        defines += " -DBUILD_kbackgammon=OFF"
+        defines += " -DBUILD_kigo=OFF"
+        defines += " -DBUILD_kolf-ng=ON"
+        defines += " -DBUILD_kombination=OFF"
+        defines += " -DBUILD_ksimili=OFF"
+        defines += " -DBUILD_ksirtet=OFF"
+        defines += " -DBUILD_kttt=OFF"
+        defines += " -DBUILD_magazynier=OFF"
+        defines += " -DBUILD_palapeli=OFF"
+        defines += " -DBUILD_pege=OFF"
+        defines += " -DBUILD_superpong=OFF"
+        defines += " -DBUILD_kamala=OFF"
+        defines += " -DBUILD_astrododge=OFF"
+        defines += " -DBUILD_draughts=OFF"
+        defines += " -DBUILD_kollagame=OFF"
+        defines += " -DBUILD_kpicross=OFF"
+        defines += " -DBUILD_kpoker=OFF"
+        defines += " -DBUILD_ktank=OFF"
+        defines += " -DBUILD_market=OFF"
+        defines += " -DBUILD_nonogram=OFF"
+        defines += " -DBUILD_parsek=OFF"
+        defines += " -DBUILD_mancala=OFF"
+        self.subinfo.options.configure.defines = defines
         
 if __name__ == '__main__':
     Package().execute()
