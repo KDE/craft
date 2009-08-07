7f146408317b6889579bcd3be59fa8534ed2e698
Patrick Spendrin <ps_ml@gmx.de>
2009-08-07 01:01:45 +0000

    add dependency to glew
    svn path=/trunk/kdesupport/emerge/; revision=1008166

--- portage/testing/kolf-ng/kolf-ng-20090807.py
+++ portage/testing/kolf-ng/kolf-ng-20090807.py
@@ -6,27 +6,49 @@ from BuildSystem.CMakeBuildSystem import *
 from Package.PackageBase import *
 from Packager.KDEWinPackager import *
 
+class dummySubinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = "trunk/playground/games/cmake"
+        self.defaultTarget = 'svnHEAD'
+        
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = "trunk/playground/games"
+        self.svnTargets['svnHEAD'] = "trunk/playground/games/kolf-ng"
         self.defaultTarget = 'svnHEAD'
+
+    def configurePath( self ):
+        return ".."
+
+    def hasConfigurePath( self ):
+        return True
 
     def setDependencies( self ):
         self.hardDependencies['kde/kdegames'] = 'default'
+        self.hardDependencies['testing/glew'] = 'default'
 
-class Package(PackageBase, SvnSource, CMakeBuildSystem, KDEWinPackager):
+class Package(PackageBase, SvnSource):
+    def __init__( self, **args ):
+        self.subinfo = dummySubinfo()
+        SvnSource.__init__(self)
+        PackageBase.__init__(self)
+        
+    def compile( self ):
+        return True
+
+class MainPackage(PackageBase, SvnSource, CMakeBuildSystem, KDEWinPackager):
     def __init__( self, **args ):
         self.subinfo = subinfo()
+
+        PackageBase.__init__(self)
         SvnSource.__init__(self)
         CMakeBuildSystem.__init__(self)
-        PackageBase.__init__(self)
         KDEWinPackager.__init__(self)
         defines = ""
         defines += " -DBUILD_doc=OFF"
         defines += " -DBUILD_libksirtet=OFF"
         defines += " -DBUILD_kbackgammon=OFF"
         defines += " -DBUILD_kigo=OFF"
-        defines += " -DBUILD_kolf-ng=ON"
+#        defines += " -DBUILD_kolf-ng=OFF"
         defines += " -DBUILD_kombination=OFF"
         defines += " -DBUILD_ksimili=OFF"
         defines += " -DBUILD_ksirtet=OFF"
@@ -47,6 +69,15 @@ class Package(PackageBase, SvnSource, CMakeBuildSystem, KDEWinPackager):
         defines += " -DBUILD_parsek=OFF"
         defines += " -DBUILD_mancala=OFF"
         self.subinfo.options.configure.defines = defines
-        
+
+        self.cmakedummy = portage.getPackageInstance('testing','kolf-ng')
+
+    def execute(self):
+        (command, option) = self.getAction()
+        ## \todo does not work yet see note in PackageBase::getAction()
+        self.runAction(command)
+        self.cmakedummy.runAction(command)
+        return True
+
 if __name__ == '__main__':
-    Package().execute()
+    MainPackage().execute()
