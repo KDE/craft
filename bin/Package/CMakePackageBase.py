4b2c6da87ad5f93d8bd662ab33eddac580d8ec3e
Patrick Spendrin <ps_ml@gmx.de>
2009-08-07 14:11:34 +0000

    since MultiSource simply doesn't work - go back to SvnSource for now
    svn path=/trunk/kdesupport/emerge/; revision=1008440

--- bin/Package/CMakePackageBase.py
+++ bin/Package/CMakePackageBase.py
@@ -1,15 +1,15 @@
 
 from PackageBase import *;
-from Source.MultiSource import *;
+from Source.SvnSource import *;
 from BuildSystem.CMakeBuildSystem import *;
 from Packager.KDEWinPackager import *;
 
 ## \todo rename to CMakePackage
-class CMakePackageBase (PackageBase, MultiSource, CMakeBuildSystem, KDEWinPackager):
+class CMakePackageBase (PackageBase, SvnSource, CMakeBuildSystem, KDEWinPackager):
     """provides a base class for cmake packages from any source"""
     def __init__(self):
         utils.debug("CMakePackageBase.__init__ called",2)
         PackageBase.__init__(self)
-        MultiSource.__init__(self)
+        SvnSource.__init__(self)
         CMakeBuildSystem.__init__(self)
         KDEWinPackager.__init__(self)
