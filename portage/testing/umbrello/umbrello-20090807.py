500d0e3544f68d1a05a378b33cac7e1b96cf8533
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-08 22:48:48 +0000

    added umbrello as single package
    svn path=/trunk/kdesupport/emerge/; revision=1009020

---
+++ portage/testing/umbrello/umbrello-20090807.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import info
+
+from Package.CMakePackageBase import *
+        
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = "trunk/KDE/kdesdk#norecursive;trunk/KDE/kdesdk/umbrello;trunk/KDE/kdesdk/cmake"
+        self.defaultTarget = 'svnHEAD'
+
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['kdesupport/oxygen-icons'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.buildOnlyTargets = 'umbrello' 
+            
+if __name__ == '__main__':
+    Package().execute()
+
+    
