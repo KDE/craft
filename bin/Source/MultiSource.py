5e22ae5e9f0470b7320957fbe1d598de752fb140
Patrick Spendrin <ps_ml@gmx.de>
2009-08-08 19:47:16 +0000

    fix MultiSource - this patch fixes the error that self.category and self.PV are not defined if a MultiSource is used. the problem is that those variables get defined in PackageBase - the pure SvnSource object does not inherit PackageBase though
    svn path=/trunk/kdesupport/emerge/; revision=1008968

--- bin/Source/MultiSource.py
+++ bin/Source/MultiSource.py
@@ -11,7 +11,12 @@ class MultiSource():
     """ provides multi source type api """
     def __init__(self):
         utils.debug( "MultiSource __init__", 2 )
+
         self.source = SourceFactory(self.subinfo)
+        self.source.category = self.category
+        self.source.package = self.package
+        self.source.version = self.version
+        self.source.PV = self.PV
 
     def fetch(self):
         return self.source.fetch()
