2e7f1a0edc6a532fdf876fdfce4a60e736c2a282
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-08 14:38:33 +0000

    uses MultiSource as default again - if there are any problems with using MultiSource contact me
    svn path=/trunk/kdesupport/emerge/; revision=1008871

--- bin/Package/CMakePackageBase.py
+++ bin/Package/CMakePackageBase.py
@@ -1,15 +1,15 @@
 
 from PackageBase import *;
-from Source.SvnSource import *;
+from Source.MultiSource import *;
 from BuildSystem.CMakeBuildSystem import *;
 from Packager.KDEWinPackager import *;
 
 ## \todo rename to CMakePackage
-class CMakePackageBase (PackageBase, SvnSource, CMakeBuildSystem, KDEWinPackager):
+class CMakePackageBase (PackageBase, MultiSource, CMakeBuildSystem, KDEWinPackager):
     """provides a base class for cmake packages from any source"""
     def __init__(self):
         utils.debug("CMakePackageBase.__init__ called",2)
         PackageBase.__init__(self)
-        SvnSource.__init__(self)
+        MultiSource.__init__(self)
         CMakeBuildSystem.__init__(self)
         KDEWinPackager.__init__(self)
