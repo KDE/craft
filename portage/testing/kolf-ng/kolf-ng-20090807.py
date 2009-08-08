134a63e2bbabac3cc6525e1089d0892442ac00c9
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-08 22:17:16 +0000

    uses now new configure option onlyBuildTargets, which makes package much smaller and error less
    svn path=/trunk/kdesupport/emerge/; revision=1009010

--- portage/testing/kolf-ng/kolf-ng-20090807.py
+++ portage/testing/kolf-ng/kolf-ng-20090807.py
@@ -16,33 +16,7 @@ class Package(CMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
-        
-        defines = ""
-        defines += " -DBUILD_doc=OFF"
-        defines += " -DBUILD_libksirtet=OFF"
-        defines += " -DBUILD_kbackgammon=OFF"
-        defines += " -DBUILD_kigo=OFF"
-#        defines += " -DBUILD_kolf-ng=OFF"
-        defines += " -DBUILD_kombination=OFF"
-        defines += " -DBUILD_ksimili=OFF"
-        defines += " -DBUILD_ksirtet=OFF"
-        defines += " -DBUILD_kttt=OFF"
-        defines += " -DBUILD_magazynier=OFF"
-        defines += " -DBUILD_palapeli=OFF"
-        defines += " -DBUILD_pege=OFF"
-        defines += " -DBUILD_superpong=OFF"
-        defines += " -DBUILD_kamala=OFF"
-        defines += " -DBUILD_astrododge=OFF"
-        defines += " -DBUILD_draughts=OFF"
-        defines += " -DBUILD_kollagame=OFF"
-        defines += " -DBUILD_kpicross=OFF"
-        defines += " -DBUILD_kpoker=OFF"
-        defines += " -DBUILD_ktank=OFF"
-        defines += " -DBUILD_market=OFF"
-        defines += " -DBUILD_nonogram=OFF"
-        defines += " -DBUILD_parsek=OFF"
-        defines += " -DBUILD_mancala=OFF"
-        self.subinfo.options.configure.defines = defines
+        self.subinfo.options.configure.onlyBuildTargets = 'kolf-ng'
 
 if __name__ == '__main__':
     Package().execute()
