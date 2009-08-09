446dd8bc3bd4a4788d40849da7e1573878772489
Jarosław Staniek <staniek@kde.org>
2009-08-09 21:59:56 +0000

    upgrade
    svn path=/trunk/kdesupport/emerge/; revision=1009395

---
+++ portage/dev-util/subversion/subversion-1.6.4.20090809.py
@@ -0,0 +1,23 @@
+import base
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['1.6.4'] = "http://subversion.tigris.org/files/documents/15/46471/svn-win32-1.6.4.zip"
+        # this location affects class SvnSource 
+        self.targetMergePath['1.6.4'] = "dev-utils/svn";
+        self.targetMergeSourcePath['1.6.4'] = "svn-win32-1.6.4";
+        self.defaultTarget = '1.6.4'
+
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+   
+from Package.BinaryPackageBase import *
+
+class Package(BinaryPackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        BinaryPackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
