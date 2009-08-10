d209c5db56a05f550e175ca1b78aec3874eec4a5
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-10 15:36:13 +0000

    added generic support for checkout sources from a svn repository the with svn information starting from the svn root repository directory. It detects the svn root by searching the predefined root subdirectories 'trunk', 'branches' and 'tags' which will probably fit for most svn servers. Additional strategies could be added easily.
    CCMAIL:ps_ml@gmx.de
    
    svn path=/trunk/kdesupport/emerge/; revision=1009663

--- bin/Source/SvnSource.py
+++ bin/Source/SvnSource.py
@@ -27,12 +27,66 @@ class SvnSource (VersionSystemSourceBase):
             url = self.repositoryPath(i)
             sourcedir = self.sourceDir(i)
             if self.repositoryPathOptions(i) == 'norecursive':
-                self.__checkout(url,sourcedir,False)
+                self.__tryCheckoutFromRoot(url,sourcedir,False)
             else:
-                self.__checkout(url,sourcedir,True)
+                self.__tryCheckoutFromRoot(url,sourcedir,True)
             i += 1
         return True
 
+    def __splitPath(self, path):
+        """ split a path into a base part and a relative repository url. 
+        The delimiters are currently 'trunk', 'branches' and 'tags'. 
+        """
+        pos=path.find('trunk')
+        if pos == -1:
+            pos=path.find('branches')
+            if pos == -1:
+                pos=path.find('tags')
+        if pos == -1:
+            ret = [path,None]
+        else:
+            ret = [path[:pos-1], path[pos:]]
+        return ret
+
+    def __tryCheckoutFromRoot ( self, url, sourcedir, recursive=True ):
+        """This method checkout source with svn informations from 
+        the svn root repository directory. It detects the svn root 
+        by searching the predefined root subdirectories 'trunk', 'branches' 
+        and 'tags' which will probably fit for most servers
+        """
+        (urlBase,urlPath) = self.__splitPath(url)
+        if urlPath == None:
+            return self.__checkout(url, sourcedir, recursive)
+        
+        (srcBase,srcPath)  = self.__splitPath(sourcedir)
+        if srcPath == None: 
+            return self.__checkout(url, sourcedir, recursive)
+        
+        urlRepo = urlBase
+        srcDir = srcBase
+        urlParts = urlPath.split('/')
+        pathSep = '/'
+        srcParts = srcPath.split(pathSep)
+        
+        # url and source parts not match 
+        if len(urlParts) <> len(srcParts):
+            return self.__checkout(url, sourcedir, recursive)
+        
+        for i in range(0,len(urlParts)-1):
+            urlPart = urlParts[i]
+            srcPart = srcParts[i]
+            if ( urlPart == "" ):
+                continue
+            
+            urlRepo +=  '/' + urlPart
+            srcDir +=  pathSep + srcPart
+            
+            if os.path.exists(srcDir): 
+                continue
+            self.__checkout( urlRepo, srcDir, False )
+            
+        self.__checkout( url, sourcedir, recursive )
+    
     def __checkout( self, url, sourcedir, recursive=True ):
         """internal method for subversion checkout and update"""
         option = ""
