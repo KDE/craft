b7819a326041423dca9fa2e78ba7ec0768de4f98
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-10 06:48:13 +0000

    uses MultiSource - kdewin-packager needs it
    svn path=/trunk/kdesupport/emerge/; revision=1009474

--- bin/Package/BinaryPackageBase.py
+++ bin/Package/BinaryPackageBase.py
@@ -3,16 +3,16 @@
 #
 
 from PackageBase import *;
-from Source.ArchiveSource import *;
+from Source.MultiSource import *;
 from BuildSystem.BinaryBuildSystem import *;
 from Packager.KDEWinPackager import *;
 
 ## \todo rename to BinaryPackage
-class BinaryPackageBase (PackageBase, ArchiveSource, BinaryBuildSystem, KDEWinPackager):
+class BinaryPackageBase (PackageBase, MultiSource, BinaryBuildSystem, KDEWinPackager):
     """provides a base class for binary packages"""
     def __init__(self):
         utils.debug("BinaryPackageBase.__init__ called",2)
         PackageBase.__init__(self)
-        ArchiveSource.__init__(self)
+        MultiSource.__init__(self)
         BinaryBuildSystem.__init__(self)
         KDEWinPackager.__init__(self)
