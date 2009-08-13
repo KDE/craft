4b5b3d61a433a019129f2acbda93a53c2d421a89
Patrick Spendrin <ps_ml@gmx.de>
2009-08-13 08:36:37 +0000

    make compiler independent packages
    svn path=/trunk/kdesupport/emerge/; revision=1010682

--- portage/win32libs-sources/libssh-src/libssh-src-20090805.py
+++ portage/win32libs-sources/libssh-src/libssh-src-20090805.py
@@ -16,6 +16,7 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = "git://git.libssh.org/projects/libssh/libssh.git"
         self.defaultTarget = 'gitHEAD'
+        self.options.package.withCompiler = False
 
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
