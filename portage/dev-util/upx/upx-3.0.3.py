fcefe9e67be68d58e84b4e88bb7386d8f6786cae
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-14 21:29:45 +0000

    migrated to refactored emerge
    svn path=/trunk/kdesupport/emerge/; revision=1011505

--- portage/dev-util/upx/upx-3.0.3.py
+++ portage/dev-util/upx/upx-3.0.3.py
@@ -5,15 +5,15 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['3.0.3'] = "http://www.winkde.org/pub/kde/ports/win32/repository/other/upx-3.0.3-bin.zip"
         self.defaultTarget = '3.0.3'
+        self.targetMergePath['3.0.3'] = "dev-utils";
     
-class subclass(base.baseclass):
-  def __init__(self, **args):
-    base.baseclass.__init__( self, args=args )
-    self.subinfo = subinfo()
+from Package.BinaryPackageBase import *
 
-  def unpack(self):
-    res = base.baseclass.unpack( self )
-    return res
+class Package(BinaryPackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        self.subinfo.options.merge.ignoreBuildType = True
+        BinaryPackageBase.__init__(self)
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
