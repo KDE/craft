010d7fa19ea408ab3b2c894fda222ff3133206f1
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-14 21:29:11 +0000

    bug fix
    svn path=/trunk/kdesupport/emerge/; revision=1011504

--- portage/dev-util/pexports/pexports-0.43.py
+++ portage/dev-util/pexports/pexports-0.43.py
@@ -5,14 +5,14 @@ class subinfo(info.infoclass):
         self.targets['0.43'] = 'http://www.emmestech.com/software/pexports-0.43/pexports-0.43.zip'
         self.targetMergeSourcePath['0.43'] = 'pexports-0.43'
         self.defaultTarget = '0.43'
-        self.options.merge.destinationPath = 'dev-utils'
-        self.options.package.withCompiler = False
 
 from Package.BinaryPackageBase import *        
         
 class Package(BinaryPackageBase):
   def __init__( self ):
     self.subinfo = subinfo()
+    self.subinfo.options.merge.destinationPath = 'dev-utils'
+    self.subinfo.options.package.withCompiler = False
     BinaryPackageBase.__init__( self )
 
 if __name__ == '__main__':
