b4123180142a89f3825ad08c7d99e2bb18d9587c
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-14 21:28:34 +0000

    added todo
    svn path=/trunk/kdesupport/emerge/; revision=1011503

--- portage/dev-util/kdewin-packager/kdewin-packager-0.9.6.py
+++ portage/dev-util/kdewin-packager/kdewin-packager-0.9.6.py
@@ -4,6 +4,7 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['0.9.6'] = "http://www.winkde.org/pub/kde/ports/win32/installer/kdewin-packager.exe"
         self.defaultTarget = '0.9.6'
+        ## \todo specific a target independent install path option
         self.targetInstallPath['0.9.6'] = 'bin'
 
 from Package.BinaryPackageBase import *
