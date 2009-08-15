c09748534925accc0efff652b8b75f406d9c90bc
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-15 16:59:23 +0000

    updated to git 1.6.4
    svn path=/trunk/kdesupport/emerge/; revision=1011728

--- portage/dev-util/git/git-1.6.4.py
+++ portage/dev-util/git/git-1.6.4.py
@@ -4,9 +4,13 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['1.6.3'] = "http://winkde.org/pub/kde/ports/win32/repository/other/Git-1.6.3-preview20090507-2.tar.bz2"
+        self.targets['1.6.4'] = "http://msysgit.googlecode.com/files/PortableGit-1.6.4-preview20090729.7z"
         self.targetInstSrc['1.6.3'] = ""
-        self.targetMergePath['1.6.3'] = "dev-utils/git";
-        self.defaultTarget = '1.6.3'
+        self.defaultTarget = '1.6.4'
+
+    def setDependencies(self):
+        self.hardDependencies['dev-util/7zip']   = 'default'
+
         
 from Package.BinaryPackageBase import *
 
@@ -14,6 +18,7 @@ class Package(BinaryPackageBase):
     def __init__( self):
         self.subinfo = subinfo()
         self.subinfo.options.merge.ignoreBuildType = True
+        self.subinfo.options.merge.destinationPath = "dev-utils/git";
         BinaryPackageBase.__init__(self)
 
 if __name__ == '__main__':
