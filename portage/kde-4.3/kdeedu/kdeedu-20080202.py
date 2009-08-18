6bca54635ef86f419ffbc0b168507785f593f4c1
Ralf Habacker <ralf.habacker@freenet.de>
2009-08-18 18:19:28 +0000

    added save problem patch for parley - r1011613 from 4.3 branch - please update the kdeedu 4.3.0 binary package, many thanks
    CCMAIL:ps_ml@gmx.de
    
    svn path=/trunk/kdesupport/emerge/; revision=1013051

--- portage/kde-4.3/kdeedu/kdeedu-20080202.py
+++ portage/kde-4.3/kdeedu/kdeedu-20080202.py
@@ -10,10 +10,12 @@ class subinfo(info.infoclass):
         for ver in ['91', '95', '98']:
           self.targets['4.2.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.2.' + ver + '/src/kdeedu-4.2.' + ver + '.tar.bz2'
           self.targetInstSrc['4.2.' + ver] = 'kdeedu-4.2.' + ver
+        self.patchToApply['4.2.95'] = ('kdeedu-kmplot.diff', 0)
         for ver in ['0', '1', '2', '3', '4']:
           self.targets['4.3.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.3.' + ver + '/src/kdeedu-4.3.' + ver + '.tar.bz2'
           self.targetInstSrc['4.3.' + ver] = 'kdeedu-4.3.' + ver
-        self.patchToApply['4.2.95'] = ('kdeedu-kmplot.diff', 0)
+        self.patchToApply['4.3.0'] = ('1011613.diff', 0)
+          
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
@@ -25,23 +27,12 @@ class subinfo(info.infoclass):
         self.hardDependencies['win32libs-bin/libnova'] = 'default'
         self.hardDependencies['win32libs-bin/openbabel'] = 'default'
 
-class subclass(base.baseclass):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "kdeedu"
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
         self.subinfo = subinfo()
-
-    def unpack( self ):
-        return self.kdeSvnUnpack()
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        return self.doPackaging( "kdeedu", self.buildTarget, True )
+        CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
