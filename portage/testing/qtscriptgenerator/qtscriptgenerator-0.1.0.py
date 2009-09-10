3297cd0b7291cd1641e68595078f48ec6d2cbb34
Ralf Habacker <ralf.habacker@freenet.de>
2009-09-10 16:37:04 +0000

    migrated to refactored emerge
    svn path=/trunk/kdesupport/emerge/; revision=1022044

--- portage/testing/qtscriptgenerator/qtscriptgenerator-0.1.0.py
+++ portage/testing/qtscriptgenerator/qtscriptgenerator-0.1.0.py
@@ -1,8 +1,3 @@
-import base
-import utils
-import sys
-import os
-import shutil
 import info
 
 class subinfo(info.infoclass):
@@ -13,32 +8,25 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['0.1.0'] = 'http://qtscriptgenerator.googlecode.com/files/qtscriptgenerator-src-0.1.0.tar.gz'
         self.targetInstSrc['0.1.0'] = 'qtscriptgenerator-src-0.1.0'
+        #patches = [ "qtscriptgenerator-cmake.diff",1]
+        #patches.append(["qtscriptgenerator.diff",1])
+        # \todo there is only one patch possible
+        #self.patchToApply['0.1.0'] = patches
         self.defaultTarget = '0.1.0'
+   
+from Package.CMakePackageBase import *
 
-class subclass(base.baseclass):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "qtscriptgenerator"
+class Package(CMakePackageBase):
+    def __init__( self):
         self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
 
-    def unpack( self ):
-        ret = self.kdeSvnUnpack()
-        if self.buildTarget == '0.1.0':
-                self.system( "cd %s && patch -p1 < %s" % ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir, "qtscriptgenerator-cmake.diff" ) ) )
-                self.system( "cd %s && patch -p1 < %s" % ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir, "qtscriptgenerator.diff" ) ) )
-        return ret
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        if self.buildTarget == "svnHEAD":
-            return self.doPackaging( "qtscriptgenerator", utils.cleanPackageName( sys.argv[0], "qtscriptgenerator" ), True )
-        else:
-            return self.doPackaging( "qtscriptgenerator", self.buildTarget, True )
+    def unpack(self):
+        if not CMakePackageBase.unpack(self):
+            return False
+        self.system( "cd %s && patch -p1 < %s" % ( self.sourceDir(), os.path.join( self.packageDir(), "qtscriptgenerator-cmake.diff" ) ) )
+        self.system( "cd %s && patch -p1 < %s" % ( self.sourceDir(), os.path.join( self.packageDir(), "qtscriptgenerator.diff" ) ) )
+        return True
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
