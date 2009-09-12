b56162e9e41d5ad5dcc41e56bb7b2ea2c063ab50
Ralf Habacker <ralf.habacker@freenet.de>
2009-09-12 06:52:06 +0000

    migrated to refactored emerge
    svn path=/trunk/kdesupport/emerge/; revision=1022586

--- portage/testing/kdevplatform/kdevplatform-3.9.91.20090208.py
+++ portage/testing/kdevplatform/kdevplatform-3.9.91.20090208.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-import base
-import utils
-import os
-import sys
 import info
 
 class subinfo(info.infoclass):
@@ -14,26 +9,12 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.hardDependencies['kde/kdebase-runtime'] = 'default'
     
-class subclass(base.baseclass):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "kdevplatform"
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self):
         self.subinfo = subinfo()
-
-    def unpack( self ):
-        return self.kdeSvnUnpack()
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        if not self.buildTarget == 'svnHEAD':
-            return self.doPackaging( "kdevplatform", self.buildTarget, True )
-        else:
-            return self.doPackaging( "kdevplatform" )
+        CMakePackageBase.__init__(self)
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
