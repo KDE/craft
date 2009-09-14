8568fee30aa8f93db4ff02a5536f1d988d5219fc
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-09-14 17:44:21 +0000

    update to 3.20 for kdeedu/kstars
    svn path=/trunk/kdesupport/emerge/; revision=1023398

--- portage/win32libs-sources/cfitsio-src/cfitsio-src-3.20.py
+++ portage/win32libs-sources/cfitsio-src/cfitsio-src-3.20.py
@@ -17,12 +17,15 @@ class subinfo(info.infoclass):
         self.targets['3.08'] = 'ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3080.tar.gz'
         self.targets['3.10'] = 'ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3100.tar.gz'
         self.targets['3.14'] = 'ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3140.tar.gz'
+        self.targets['3.20'] = 'ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3200.tar.gz'
         self.targetInstSrc['3.08'] = "cfitsio"
         self.targetInstSrc['3.10'] = "cfitsio"
         self.targetInstSrc['3.14'] = "cfitsio"
-        self.defaultTarget = '3.14'
+        self.targetInstSrc['3.20'] = "cfitsio"
+        self.defaultTarget = '3.20'
 
     def setDependencies( self ):
+        self.hardDependencies['virtual/base']  = 'default'
         self.hardDependencies['dev-util/msys'] = 'default'
 
 class subclass(base.baseclass):
