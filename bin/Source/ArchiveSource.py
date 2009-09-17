c16c45ad3183557bc155b2da294f4eaae8c8b026
Patrick Spendrin <ps_ml@gmx.de>
2009-09-17 01:36:55 +0000

    add createpatch implementation for archive sources
    svn path=/trunk/kdesupport/emerge/; revision=1024642

--- bin/Source/ArchiveSource.py
+++ bin/Source/ArchiveSource.py
@@ -4,6 +4,7 @@
 
 from Source.SourceBase import *
 import utils
+import shutil
 
 class ArchiveSource(SourceBase):
     """ file download source"""
@@ -62,3 +63,61 @@ class ArchiveSource(SourceBase):
             return False
 
         return self.applyPatches()
+
+    def createPatch( self ):
+        """ unpacking all zipped(gz,zip,bz2) tarballs a second time and making a patch """
+        # get the file paths of the tarballs
+        filenames = self.__localFileNames()
+
+        # if using BinaryBuildSystem the files should be unpacked into imagedir
+        if hasattr( self, 'buildSystemType' ) and self.buildSystemType == 'binary':
+            destdir = self.installDir()
+            utils.debug( "unpacking files into image root %s" % destdir, 1 )
+        else:
+            destdir = self.workDir()
+
+        # it makes no sense to make a diff against nothing
+        if ( not os.path.exists( self.buildDir() ) ):
+            utils.error( "build directory doesn't exist, please run unpack first" )
+            return False
+
+        utils.debug( "unpacking files into work root %s" % destdir, 1 )
+
+
+        # make a temporary directory so the original packages don't overwrite the already existing ones
+        tmpdir = os.path.join( destdir, "tmp" )
+        if ( not os.path.exists( tmpdir ) ):
+            os.mkdir( tmpdir )
+
+        # unpack all packages
+        for filename in filenames:
+            utils.debug( "unpacking this file: %s" % filename, 1 )
+            if ( not utils.unpackFile( self.downloadDir(), filename, tmpdir ) ):
+                return False
+
+        packagelist = os.listdir( tmpdir )
+        
+        # move the packages up and rename them to be different from the original source directory
+        for dir in packagelist:
+            # if the source or dest directory already exists, remove the occurance instead
+            if os.path.exists( os.path.join( destdir, dir + ".orig" ) ):
+                shutil.rmtree( os.path.join( destdir, dir + ".orig" ) )
+            shutil.move( os.path.join( tmpdir, dir ), os.path.join( destdir, dir + ".orig" ) )
+
+        # make one diff per file, even though we aren't able to apply multiple patches per package atm
+        os.chdir( destdir )
+        for dir in packagelist:
+            cmd = "diff -Nru %s.orig %s > %s || echo 0" % ( dir, dir, os.path.join( self.buildRoot(), \
+            "%s-%s.diff" % ( dir, str( datetime.date.today() ).replace('-', '') ) ) )
+            if not self.system( cmd ):
+                return False
+            
+        # remove all directories that are not needed any more after making the patch
+        # disabled for now
+        #for dir in packagelist:
+        #    shutil.rmtree( dir + ".orig" )
+            
+        # remove the temporary directory, it should be empty after all directories have been moved up
+        os.rmdir( tmpdir )
+
+        return True
