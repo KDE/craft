52c7572a3a242ddbadc325468d84051f647255e6
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-09-25 15:50:36 +0000

    fix package name
    svn path=/trunk/kdesupport/emerge/; revision=1028039

--- portage/win32libs-sources/redland-src/redland-src-1.0.8.20090124.py
+++ portage/win32libs-sources/redland-src/redland-src-1.0.8.20090124.py
@@ -4,10 +4,11 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets[ '1.0.8-1' ] = 'http://download.librdf.org/source/redland-1.0.8.tar.gz'
-        self.targetInstSrc[ '1.0.8-1' ] = 'redland-1.0.8'
-        self.patchToApply[ '1.0.8-1' ] = ( 'redland-src_1.0.8.patch', 1 )
-        self.defaultTarget = '1.0.8-1'
+        ver = '1.0.8-2'
+        self.targets[ ver ] = 'http://download.librdf.org/source/redland-1.0.8.tar.gz'
+        self.targetInstSrc[ ver ] = 'redland-1.0.8'
+        self.patchToApply[ ver ] = ( 'redland-src_1.0.8.patch', 1 )
+        self.defaultTarget = ver
 
     def setDependencies( self ):
         self.hardDependencies['win32libs-bin/sqlite'] = 'default'
@@ -21,6 +22,7 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
+        self.subinfo.options.package.packageName = 'redland'
         CMakePackageBase.__init__( self )
         
 if __name__ == '__main__':
