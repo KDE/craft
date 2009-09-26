192af5ef980ce8e6fa1228843ba36b83f234a43a
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-09-26 07:29:45 +0000

    sync name with sf.net/kde-windows
    svn path=/trunk/kdesupport/emerge/; revision=1028189

--- portage/win32libs-sources/freetype-src/freetype-src-2.3.7.20090108.py
+++ portage/win32libs-sources/freetype-src/freetype-src-2.3.7.20090108.py
@@ -6,10 +6,10 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['2.3.5-2'] = "http://downloads.sourceforge.net/freetype/freetype-2.3.5.tar.bz2"
-        self.targets['2.3.7'] = "http://downloads.sourceforge.net/freetype/freetype-2.3.7.tar.bz2"
+        self.targets['2.3.7-2'] = "http://downloads.sourceforge.net/freetype/freetype-2.3.7.tar.bz2"
         self.targetInstSrc['2.3.5-2'] = "freetype-2.3.5"
-        self.targetInstSrc['2.3.7'] = "freetype-2.3.7"
-        self.defaultTarget = '2.3.7'
+        self.targetInstSrc['2.3.7-2'] = "freetype-2.3.7"
+        self.defaultTarget = '2.3.7-2'
     
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
