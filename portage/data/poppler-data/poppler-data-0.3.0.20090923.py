703ab792cea1df553389c3dddfed5e8fe7ee18cd
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2009-09-26 13:12:00 +0000

    fix packaging
    svn path=/trunk/kdesupport/emerge/; revision=1028274

--- portage/data/poppler-data/poppler-data-0.3.0.20090923.py
+++ portage/data/poppler-data/poppler-data-0.3.0.20090923.py
@@ -16,6 +16,7 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
+        self.subinfo.options.package.withCompiler = False
         CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
