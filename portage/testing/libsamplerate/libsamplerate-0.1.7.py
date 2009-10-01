feec986ffbc575f62319de1d931af656a1adea7a
Patrick Spendrin <ps_ml@gmx.de>
2009-10-01 22:21:02 +0000

    update libsamplerate to new class layout, extend the CMake build system and put it into a patch
    svn path=/trunk/kdesupport/emerge/; revision=1030224

--- portage/testing/libsamplerate/libsamplerate-0.1.7.py
+++ portage/testing/libsamplerate/libsamplerate-0.1.7.py
@@ -1,55 +1,19 @@
-import base
-import os
-import shutil
-import utils
 import info
 
-PACKAGE_NAME         = "libsamplerate"
-PACKAGE_VER          = "0.1.7"
-PACKAGE_FULL_VER     = "0.1.7"
-PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_VER )
-PACKAGE_DLL_NAME     = "samplerate"
-
-SRC_URI= """
-http://www.mega-nerd.com/SRC/libsamplerate-0.1.7.tar.gz
-"""
-
-DEPEND = """
-"""
+from Package.CMakePackageBase import *
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets['0.1.7'] = SRC_URI
+        self.targets['0.1.7'] = "http://www.mega-nerd.com/SRC/libsamplerate-0.1.7.tar.gz"
+        self.targetInstSrc['0.1.7'] = 'libsamplerate-0.1.7'
+        self.patchToApply['0.1.7'] = ('libsamplerate-0.1.7-20091002.diff', 1)
         self.defaultTarget = '0.1.7'
         
-class subclass( base.baseclass ):
+class Package(CMakePackageBase):
     def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "libsamplerate-0.1.7"
-        #self.createCombinedPackage = True
         self.subinfo = subinfo()
-
-    def unpack( self ):
-        base.baseclass.unpack( self ) or utils.die( "unpack failed" )
-        os.chdir( self.workdir )
-        shutil.copyfile( os.path.join( self.packagedir, "CMakeLists.txt" ), os.path.join( self.workdir,self.instsrcdir, "CMakeLists.txt" ) )
-        shutil.copyfile(os.path.join( self.packagedir, "config.h"), os.path.join( self.workdir,self.instsrcdir, "Win32/config.h" ) )
-        if( not os.path.exists( self.workdir ) ):
-            os.makedirs( self.workdir )
-        return True
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-
-    # now do packaging with kdewin-packager
-        self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
-
-        return True
+        self.subinfo.options.configure.defines = " -DBUILD_SHARED_LIB=ON -DBUILD_EXAMPLES=ON -DBUILD_TESTS=ON"
+        CMakePackageBase.__init__(self)
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
