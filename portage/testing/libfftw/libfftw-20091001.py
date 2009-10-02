4e686e073d179a1190ad52204bb9ceba31caf22f
Patrick Spendrin <ps_ml@gmx.de>
2009-10-02 01:17:19 +0000

    add a binary package too
    svn path=/trunk/kdesupport/emerge/; revision=1030249

--- portage/testing/libfftw/libfftw-20091001.py
+++ portage/testing/libfftw/libfftw-20091001.py
@@ -1,44 +1,24 @@
 # -*- coding: utf-8 -*-
-import info
+from Package.BinaryPackageBase import *
 import os
-import shutil
-import utils
+import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['3.3.2'] = 'ftp://ftp.fftw.org/pub/fftw/fftw-3.2.2.pl1-dll32.zip'
+        repoUrl = """http://downloads.sourceforge.net/kde-windows"""
+        
+        for version in ['3.3.2']:
+            self.targets[ version ] = self.getUnifiedPackage( repoUrl, "libfftw", version )
+
         self.defaultTarget = '3.3.2'
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
-        
 
-from Package.BinaryPackageBase import *
-        
 class Package(BinaryPackageBase):
-    def __init__( self, **args ):
-        self.subinfo = subinfo()
-        self.subinfo.options.package.packSources = False
-        BinaryPackageBase.__init__(self)
-
-    def __move(self, filenames, destdir):
-        """ check if the destination directory exists and move multiple files """
-        if not os.path.exists(destdir):
-            os.makedirs(destdir)
-        for i in filenames:
-            shutil.move(os.path.join(self.imageDir(), i), os.path.join(destdir, i))
-
-        
-    def unpack(self):
-        BinaryPackageBase.unpack(self)
-        self.__move('bench.exe benchf.exe benchl.exe fftw-wisdom.exe fftwf-wisdom.exe fftwl-wisdom.exe libfftw3-3.dll libfftw3f-3.dll libfftw3l-3.dll'.split(), os.path.join(self.imageDir(), "bin"))
-        self.__move('libfftw3-3.def libfftw3f-3.def libfftw3l-3.def'.split(), os.path.join(self.imageDir(), "lib"))
-        self.__move('fftw3.f fftw3.h'.split(), os.path.join(self.imageDir(), "include"))
-        self.__move('COPYING COPYRIGHT NEWS README README-bench README-WINDOWS'.split(), os.path.join(self.imageDir(), "doc", "fftw"))
-
-        for i in "libfftw3-3 libfftw3f-3 libfftw3l-3".split():
-            utils.createImportLibs(i, self.imageDir())
-        return True
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
 
 if __name__ == '__main__':
     Package().execute()
