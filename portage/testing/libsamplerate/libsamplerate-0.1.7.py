1c41f2c6409d9e029ffd0ae25bf268d1ee0b5b7b
Patrick Spendrin <ps_ml@gmx.de>
2009-10-02 01:44:28 +0000

    make libsamplerate a normal binary package
    svn path=/trunk/kdesupport/emerge/; revision=1030262

--- portage/testing/libsamplerate/libsamplerate-0.1.7.py
+++ portage/testing/libsamplerate/libsamplerate-0.1.7.py
@@ -1,19 +1,23 @@
+from Package.BinaryPackageBase import *
+import os
 import info
 
-from Package.CMakePackageBase import *
-
-class subinfo( info.infoclass ):
+class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['0.1.7'] = "http://www.mega-nerd.com/SRC/libsamplerate-0.1.7.tar.gz"
-        self.targetInstSrc['0.1.7'] = 'libsamplerate-0.1.7'
-        self.patchToApply['0.1.7'] = ('libsamplerate-0.1.7-20091002.diff', 1)
-        self.defaultTarget = '0.1.7'
+        repoUrl = """http://downloads.sourceforge.net/kde-windows"""
         
-class Package(CMakePackageBase):
-    def __init__( self, **args ):
-        self.subinfo = subinfo()
-        self.subinfo.options.configure.defines = " -DBUILD_SHARED_LIB=ON -DBUILD_EXAMPLES=ON -DBUILD_TESTS=ON"
-        CMakePackageBase.__init__(self)
+        for version in ['0.1.7']:
+            self.targets[ version ] = self.getUnifiedPackage( repoUrl, "libsamplerate", version )
+
+        self.defaultTarget = '0.1.7'
+
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
 
 if __name__ == '__main__':
     Package().execute()
