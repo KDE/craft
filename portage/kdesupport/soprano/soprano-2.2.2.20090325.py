afb2fe12b9de48dbd238a947acecbd1989f76c07
Ralf Habacker <ralf.habacker@freenet.de>
2009-10-02 13:51:07 +0000

    we do not like to have a java dependency in kde base libraries
    svn path=/trunk/kdesupport/emerge/; revision=1030542

--- portage/kdesupport/soprano/soprano-2.2.2.20090325.py
+++ portage/kdesupport/soprano/soprano-2.2.2.20090325.py
@@ -32,6 +32,7 @@ class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines="-DSOPRANO_DISABLE_SESAME2_BACKEND=YES"
 
 if __name__ == '__main__':
     Package().execute()
