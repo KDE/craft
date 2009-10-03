ec6c2872cfae32ff564f089448261ab8f81a1adf
Patrick Spendrin <ps_ml@gmx.de>
2009-10-03 19:50:11 +0000

    use second rc
    svn path=/trunk/kdesupport/emerge/; revision=1031026

--- portage/dev-util/cmake/cmake-2.6.4.py
+++ portage/dev-util/cmake/cmake-2.6.4.py
@@ -4,10 +4,10 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['2.4.8'] = 'http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip'
         self.targets['2.6.4'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.4-win32-x86.zip'
-        self.targets['2.8.0-rc1'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.0-rc1-win32-x86.zip'
+        self.targets['2.8.0-rc2'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.0-rc2-win32-x86.zip'
         self.targetMergeSourcePath['2.4.8'] = 'cmake-2.4.8-win32-x86'
         self.targetMergeSourcePath['2.6.4'] = 'cmake-2.6.4-win32-x86'
-        self.targetMergeSourcePath['2.8.0-rc1'] = 'cmake-2.8.0-rc1-win32-x86'
+        self.targetMergeSourcePath['2.8.0-rc2'] = 'cmake-2.8.0-rc2-win32-x86'
         self.defaultTarget = '2.6.4'
 
 from Package.BinaryPackageBase import *
