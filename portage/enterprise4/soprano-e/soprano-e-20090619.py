61969063b8511866cbacac01c0ca0f006f94e48c
Allen Winter <winter@kde.org>
2009-10-06 22:11:15 +0000

    update to more modern soprano
    svn path=/trunk/kdesupport/emerge/; revision=1032119

--- portage/enterprise4/soprano-e/soprano-e-20090619.py
+++ portage/enterprise4/soprano-e/soprano-e-20090619.py
@@ -1,13 +1,11 @@
-import base
-import utils
-import sys
 import info
 
 class subinfo(info.infoclass):
     def setDependencies( self ):
         self.hardDependencies['virtual/base']            = 'default'
-        self.hardDependencies['enterprise4/qt-e']                 = 'default'
+        self.hardDependencies['enterprise4/qt-e']        = 'default'
         self.hardDependencies['kdesupport/clucene-core'] = 'default'
+        self.hardDependencies['win32libs-bin/redland']   = 'default'
 
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/soprano'
@@ -24,28 +22,17 @@ class subinfo(info.infoclass):
         self.svnTargets['2.2']    = 'tags/soprano/2.2'
         self.svnTargets['2.2.1']  = 'tags/soprano/2.2.1'
         self.svnTargets['2.2.2']  = 'tags/soprano/2.2.2'
+        for i in ['4.3.0', '4.3.1', '4.3.2', '4.3.3', '4.3.4', '4.3']:
+            self.svnTargets[ i ] = 'tags/kdesupport-for-4.3/kdesupport/soprano'
         self.defaultTarget = 'svnHEAD'
 
-class subclass(base.baseclass):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "soprano"
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
         self.subinfo = subinfo()
-
-    def unpack( self ):
-        return self.kdeSvnUnpack()
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        if not self.buildTarget == 'svnHEAD':
-            return self.doPackaging( "soprano", self.buildTarget, True )
-        else:
-            return self.doPackaging( "soprano", utils.cleanPackageName( sys.argv[0], "soprano" ), True )
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines="-DSOPRANO_DISABLE_SESAME2_BACKEND=YES"
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
