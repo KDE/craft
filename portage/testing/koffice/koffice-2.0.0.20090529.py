ad15bcea683dde02cdeddf629cf9d1cd97e3b0b6
Patrick Spendrin <ps_ml@gmx.de>
2009-10-06 16:20:54 +0000

    add koffice 2.0.83 release
    svn path=/trunk/kdesupport/emerge/; revision=1032002

--- portage/testing/koffice/koffice-2.0.0.20090529.py
+++ portage/testing/koffice/koffice-2.0.0.20090529.py
@@ -7,76 +7,29 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = "trunk/koffice"
-        self.svnTargets['2.0'] = "branches/koffice/2.0/koffice"
-        self.targets['2.0.0'] = 'ftp://ftp.kde.org/pub/kde/stable/koffice-2.0.0/src/koffice-2.0.0.tar.bz2'
-        self.targetInstSrc['2.0.0'] = 'koffice-2.0.0'
-        self.targets['2.0.1'] = 'ftp://ftp.kde.org/pub/kde/stable/koffice-2.0.1/src/koffice-2.0.1.tar.bz2'
-        self.targetInstSrc['2.0.1'] = 'koffice-2.0.1'
         for ver in ['2.0.82', '2.0.83']:
             self.targets[ver] = 'ftp://ftp.kde.org/pub/kde/unstable/koffice-' + ver + '/src/koffice-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = 'koffice-' + ver
+        self.patchToApply['2.0.0'] = ('koffice-2.0.0.diff', 0)
+        self.patchToApply['2.0.82'] = ('koffice-2.0.82.diff', 0)
+        self.patchToApply['2.0.83'] = ('koffice-2.0.83.diff', 1)
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
         self.hardDependencies['win32libs-bin/lcms'] = 'default'
-        self.hardDependencies['kde-4.3/kdepimlibs'] = 'default'
-        self.hardDependencies['kde-4.3/kdebase-runtime'] = 'default'
+        self.hardDependencies['kde/kdepimlibs'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
 #        self.hardDependencies['kdesupport/eigen'] = 'default'
         self.hardDependencies['kdesupport/eigen2'] = 'default'
         self.softDependencies['kdesupport/qca'] = 'default'
         self.softDependencies['testing/gsl'] = 'default'
     
-class subclass(base.baseclass):
+from Package.CMakePackageBase import *
+        
+class Package(CMakePackageBase):
     def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "koffice"
         self.subinfo = subinfo()
-        self.kdeCustomDefines = ""
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_karbon=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kformula=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kpresenter=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kchart=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kdgantt=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kexi=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kivio=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kounavail=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kplato=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_krita=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kword=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_kspread=OFF "
-#        self.kdeCustomDefines = self.kdeCustomDefines + "-DBUILD_doc=OFF "
+        CMakePackageBase.__init__(self)
 
-    def unpack( self ):
-        if self.buildTarget in ['2.0.0', '2.0.1', '2.0.82', '2.0.83']:
-            if( not base.baseclass.unpack( self ) ):
-                return False
-                
-            src = os.path.join( self.workdir, self.instsrcdir )
-
-            if self.buildTarget == '2.0.82':
-                cmd = "cd %s && patch -p0 < %s" % \
-                      ( src, os.path.join( self.packagedir , "koffice-2.0.82.diff" ) )
-            else:
-                cmd = "cd %s && patch -p0 < %s" % \
-                      ( src, os.path.join( self.packagedir , "koffice-2.0.0.diff" ) )
-            if utils.verbose() >= 1:
-                print cmd
-            self.system( cmd ) or die( "patch" )
-            return True
-        else:
-            return self.kdeSvnUnpack()
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        if not self.buildTarget == 'svnHEAD':
-            return self.doPackaging( "koffice", self.buildTarget, True )
-        else:
-            return self.doPackaging( "koffice" )
-		
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
