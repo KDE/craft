18ca555c0b5e58c84e3451e1c7f0977645a6c55a
Allen Winter <winter@kde.org>
2009-10-06 12:05:46 +0000

    libassuan should be KDE-free now. Thanks Patrick!
    svn path=/trunk/kdesupport/emerge/; revision=1031889

--- portage/enterprise4/kdepim-branch/kdepim-branch-20090622.py
+++ portage/enterprise4/kdepim-branch/kdepim-branch-20090622.py
@@ -14,9 +14,7 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
 	self.hardDependencies['win32libs-bin/sqlite'] = 'default'
         self.hardDependencies['enterprise4/kdebase-runtime-branch'] = 'default'
-# the libassuan CMakeLists.txt is currently dependent on KDE
-# so this must be installed later by-hand. for now.
-#        self.hardDependencies['contributed/libassuan-src'] = 'default'
+        self.hardDependencies['contributed/libassuan-src'] = 'default'
 
 class subclass(base.baseclass):
     def __init__( self, **args ):
