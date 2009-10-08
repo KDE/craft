5a8b17a8ed64e55b9d0e1de0d37a53a60b0f488b
Patrick Spendrin <ps_ml@gmx.de>
2009-10-08 23:09:16 +0000

    fix libssh binary package
    svn path=/trunk/kdesupport/emerge/; revision=1032953

--- portage/win32libs-bin/libssh/libssh-20091008.py
+++ portage/win32libs-bin/libssh/libssh-20091008.py
@@ -6,11 +6,10 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         repoUrl = """http://downloads.sourceforge.net/kde-windows"""
 
-        for version in ['20090813']:
-            self.targets[ version ] = repoUrl + """/libssh-""" + version + """-bin.tar.bz2
-                                """ + repoUrl + """/libssh-""" + version + """-lib.tar.bz2"""
+        for version in ['20090812', '20091008']:
+            self.targets[ version ] = self.getUnifiedPackage(repoUrl, 'libssh', version)
 
-        self.defaultTarget = '20090813'
+        self.defaultTarget = '20091008'
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
