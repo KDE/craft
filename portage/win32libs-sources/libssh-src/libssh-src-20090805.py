d4b73a2b507d99cff1f6b5b1bb67b196d8bb7bcd
Patrick Spendrin <ps_ml@gmx.de>
2009-10-08 20:14:35 +0000

    cleanup a bit
    svn path=/trunk/kdesupport/emerge/; revision=1032888

--- portage/win32libs-sources/libssh-src/libssh-src-20090805.py
+++ portage/win32libs-sources/libssh-src/libssh-src-20090805.py
@@ -1,35 +1,22 @@
 # -*- coding: utf-8 -*-
-import base
-import utils
-import shutil
-from utils import die
-import os
 import info
-import re
-
-from Source.GitSource import *
-from BuildSystem.CMakeBuildSystem import *
-from Package.PackageBase import *
-from Packager.KDEWinPackager import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = "git://git.libssh.org/projects/libssh/libssh.git"
         self.defaultTarget = 'gitHEAD'
         self.options.package.withCompiler = False
+        self.subinfo.options.configure.defines = "-DWITH_STATIC_LIB=ON"
 
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
 
-class Package(PackageBase, GitSource, CMakeBuildSystem, KDEWinPackager):
-    def __init__( self, **args ):
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self):
         self.subinfo = subinfo()
-        GitSource.__init__(self)
-        CMakeBuildSystem.__init__(self)
-        PackageBase.__init__(self)
-        KDEWinPackager.__init__(self)
-        
-        self.subinfo.options.configure.defines = "-DWITH_STATIC_LIB=ON"
+        CMakePackageBase.__init__(self)
         
 if __name__ == '__main__':
     Package().execute()
