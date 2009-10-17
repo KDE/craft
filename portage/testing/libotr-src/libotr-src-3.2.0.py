bf3f9ae2147d0b0a857cde2f25257ab2ec4fe2e0
Patrick Spendrin <ps_ml@gmx.de>
2009-10-17 02:32:13 +0000

    add a library for kopete end-to-end encryption
    svn path=/trunk/kdesupport/emerge/; revision=1036339

---
+++ portage/testing/libotr-src/libotr-src-3.2.0.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import info
+
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['3.2.0'] = 'http://www.cypherpunks.ca/otr/libotr-3.2.0.tar.gz'
+        self.targetInstSrc['3.2.0'] = 'libotr-3.2.0'
+        self.patchToApply['3.2.0'] = ('libotr-3.2.0-20091017.diff', 1)
+        self.defaultTarget = '3.2.0'
+
+    def setDependencies( self ):
+        self.hardDependencies['testing/libgcrypt-src'] = 'default'
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+        
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+        self.buildInSource = True
+
+if __name__ == '__main__':
+    Package().execute()
