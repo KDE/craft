fea1e68bb7ba6727b719998e089fd5fff62f0c5f
Till Adam <adam@kde.org>
2009-10-26 08:58:50 +0000

    Add targets for our akonadi porting work branch.
    svn path=/trunk/kdesupport/emerge/; revision=1040352

---
+++ portage/contributed/akonadi-porting-branch/kdepim/kdepim-20091026.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svn'] = 'branches/work/akonadi-ports/kdepim'
+        self.defaultTarget = 'svn'
+
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['contributed/akonadi-porting-branch/kdepimlibs'] = 'default'
+        self.hardDependencies['win32libs-bin/sqlite'] = 'default'
+        
+from Package.CMakePackageBase import *
+        
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DKLEO_SYNCHRONOUS_API_HOTFIX=ON"
+        #        self.subinfo.options.configure.defines += " -DBUILD_doc=OFF"
+
+if __name__ == '__main__':
+    Package().execute()
+
