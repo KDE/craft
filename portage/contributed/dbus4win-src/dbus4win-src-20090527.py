c38ff6cdc9bf3fbd7af42590a0a82ff096d84cb2
Ralf Habacker <ralf.habacker@freenet.de>
2009-10-31 09:23:56 +0000

    - added git source - upgraded to refactored emerge
    svn path=/trunk/kdesupport/emerge/; revision=1042954

--- portage/contributed/dbus4win-src/dbus4win-src-20090527.py
+++ portage/contributed/dbus4win-src/dbus4win-src-20090527.py
@@ -1,49 +1,28 @@
-# -*- coding: utf-8 -*-
-import base
-import utils
-import os
-import shutil
 import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['dbus4win-frank'] = 'http://www.kdab.net/~frank/dbus4win-20090527-3.zip'
-        self.targets['dbus4win-noncetcp'] = 'http://www.kdab.net/~frank/dbus4win-noncetcp-20090612.zip'
+        self.targets['dbus4win-frank'] = 'ftp://ftp.kdab.net/pub/dbus4win/dbus4win-20090527-3.zip'
+        self.targets['dbus4win-noncetcp'] = 'ftp://ftp.kdab.net/pub/dbus4win/dbus4win-noncetcp-20090612.zip'
         self.targetInstSrc['dbus4win-frank'] = os.path.join( "dbus4win-20090527-3", "cmake" )
         self.targetInstSrc['dbus4win-noncetcp'] = os.path.join( "dbus4win-noncetcp-20090612", "cmake" )
+        self.svnTargets['svnhead'] = 'git://repo.or.cz/dbus4win.git'
+        self.targetConfigurePath['svnhead'] = 'cmake'
+       
+        #self.defaultTarget = 'svnhead'
         self.defaultTarget = 'dbus4win-frank'
     
     def setDependencies( self ):
         self.hardDependencies['win32libs-bin/expat'] = 'default'
         self.hardDependencies['virtual/base'] = 'default'
 
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, args=args )
-    self.subinfo = subinfo()
-    self.kdeCustomDefines = "-DDBUS_USE_EXPAT=ON -DDBUS_DISABLE_EXECUTABLE_DEBUG_POSTFIX=ON"
-
-  def unpack( self ):
-    if( not base.baseclass.unpack( self ) ):
-        return False
-
-    print "dbus4win unpack called for %s" % self.subinfo.buildTarget
-
-    if( not os.path.exists( self.workdir ) ):
-        os.makedirs( self.workdir )
-
-    return True
-
-  def compile( self ):
-    return self.kdeCompile()
-
-  def install( self ):
-    return self.kdeInstall()
-
-  def make_package( self ):
-    self.doPackaging( "dbus4win", self.buildTarget, False )
-
-    return True
+from Package.CMakePackageBase import *        
+        
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON -DDBUS_DISABLE_EXECUTABLE_DEBUG_POSTFIX=ON"
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
