0dc93fab73e3bf3c554a5b5e5c7dea79a8843bd8
Patrick Spendrin <ps_ml@gmx.de>
2009-11-06 01:07:37 +0000

    add kmymoney
    svn path=/trunk/kdesupport/emerge/; revision=1045415

---
+++ portage/testing/kmymoney/kmymoney-20091106.py
@@ -0,0 +1,22 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/playground/office/kmymoney'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdelibs'] = 'default'
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['testing/mysql-server'] = 'default'
+        self.hardDependencies['win32libs-bin/sqlite'] = 'default'
+    
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
