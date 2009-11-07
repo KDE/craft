f7c8a76f56a605a995922cfbbf6ba126adc97d35
Patrick Spendrin <ps_ml@gmx.de>
2009-11-07 19:25:11 +0000

    some fixes for mingw4
    svn path=/trunk/kdesupport/emerge/; revision=1046189

--- portage/win32libs-sources/exiv2-src/exiv2-src-20090504.py
+++ portage/win32libs-sources/exiv2-src/exiv2-src-20090504.py
@@ -1,9 +1,4 @@
-import base
-import utils
-import os
-import shutil
 import info
-
 
 class subinfo(info.infoclass):
     def setTargets( self ):
@@ -13,7 +8,7 @@ class subinfo(info.infoclass):
           self.targetInstSrc[ver] = 'exiv2-%s' % ver
           self.patchToApply[ver]  = ( 'exiv2-%s-cmake.diff' % ver, 0 )
 
-        self.svnTargets['svnHEAD'] = 'unstable'
+        self.svnTargets['svnHEAD'] = 'svn://dev.robotbattle.com/exiv2/branches/'
         self.defaultTarget = '0.18.2'
     
     def setDependencies( self ):
@@ -23,41 +18,12 @@ class subinfo(info.infoclass):
         self.hardDependencies['win32libs-bin/zlib']    = 'default'
         self.hardDependencies['virtual/base']          = 'default'
         
-class subclass(base.baseclass):
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
     def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
         self.subinfo = subinfo()
-        if self.subinfo.buildTarget == 'svnHEAD':
-            self.kde.sourcePath = os.path.join( self.svndir, self.subinfo.svnTargets[ self.subinfo.buildTarget ] )
-
-    def unpack( self ):
-        if not self.buildTarget == 'svnHEAD':
-            if not base.baseclass.unpack( self ):
-                return False
-        else:
-            repo = 'svn://dev.robotbattle.com/exiv2/branches/'
-            if self.subinfo.buildTarget in self.subinfo.svnTargets.keys():
-                self.svnFetch( repo + self.subinfo.svnTargets[ self.subinfo.buildTarget ] )
-            else:
-                return False
-            utils.cleanDirectory( self.workdir )
-        return True
-        
-    def compile( self ):
-        if self.subinfo.buildTarget == 'svnHEAD':
-            self.kde.sourcePath = os.path.join( self.svndir, self.subinfo.svnTargets[ self.subinfo.buildTarget ] )
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        if self.subinfo.buildTarget == 'svnHEAD':
-            self.kde.sourcePath = os.path.join( self.svndir, self.subinfo.svnTargets[ self.subinfo.buildTarget ] )
-            return self.doPackaging( "exiv2", "0.18svn-0", True )
-        else:
-            return self.doPackaging( "exiv2", self.subinfo.buildTarget, True)
-
+        CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
