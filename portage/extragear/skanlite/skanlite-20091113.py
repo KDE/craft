523c34e27bfde031ff8bff7a5eab2e821292262e
Patrick Spendrin <ps_ml@gmx.de>
2009-11-13 12:53:30 +0000

    add a new application
    svn path=/trunk/kdesupport/emerge/; revision=1048493

---
+++ portage/extragear/skanlite/skanlite-20091113.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/extragear/graphics/skanlite'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kde/kdebase-runtime'] = 'default'
+        self.hardDependencies['kde/kdegraphics'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
