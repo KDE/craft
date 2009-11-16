3eb24dcff845d9fc4e4eba73b3743a61dfa0899b
Ralf Habacker <ralf.habacker@freenet.de>
2009-11-16 09:14:21 +0000

    - uses moveFile from utils - installed source files correctly - used default unpack implementation
    svn path=/trunk/kdesupport/emerge/; revision=1049943

--- portage/win32libs-bin/sqlite/sqlite-3.6.2.py
+++ portage/win32libs-bin/sqlite/sqlite-3.6.2.py
@@ -29,15 +29,11 @@ class subinfo(info.infoclass):
         self.hardDependencies['gnuwin32/sed'] = 'default'
     
 class Package(BinaryPackageBase):
-    def __init__( self, **args ):
+    def __init__( self ):
         self.subinfo = subinfo()
         BinaryPackageBase.__init__( self )
         self.createCombinedPackage = True
 
-    def unpack( self ):
-        BinaryPackageBase.unpack( self )
-        return True
-        
     def install( self ):
         dst = os.path.join( self.imageDir(), "bin" )
         utils.cleanDirectory( dst )
@@ -45,28 +41,28 @@ class Package(BinaryPackageBase):
         utils.cleanDirectory( dst )
         dst = os.path.join( self.imageDir(), "lib" )
         utils.cleanDirectory( dst )
+        dst = os.path.join( self.imageDir(), "src" )
+        utils.cleanDirectory( dst )
 
         src = os.path.join( self.imageDir(), PACKAGE_DLL_NAME + ".dll" )
         dst = os.path.join( self.imageDir(), "bin", PACKAGE_DLL_NAME + ".dll" )
-        shutil.copy( src, dst )
+        utils.moveFile( src, dst )
         src = os.path.join( self.imageDir(), PACKAGE_DLL_NAME + ".exe" )
         dst = os.path.join( self.imageDir(), "bin", PACKAGE_DLL_NAME + ".exe" )
-        shutil.copy( src, dst )
+        utils.moveFile( src, dst )
         src = os.path.join( self.imageDir(), PACKAGE_DLL_NAME + ".h" )
         dst = os.path.join( self.imageDir(), "include", PACKAGE_DLL_NAME + ".h" )
-        shutil.copy( src, dst )
+        utils.moveFile( src, dst )
         src = os.path.join( self.imageDir(), PACKAGE_DLL_NAME + "ext.h" )
         dst = os.path.join( self.imageDir(), "include", PACKAGE_DLL_NAME + "ext.h" )
-        shutil.copy( src, dst )
+        utils.moveFile( src, dst )
         src = os.path.join( self.imageDir(), PACKAGE_DLL_NAME + ".def" )
         dst = os.path.join( self.imageDir(), "lib", PACKAGE_DLL_NAME + ".def" )
-        shutil.copy( src, dst )
+        utils.moveFile( src, dst )
+        src = os.path.join( self.imageDir(), PACKAGE_DLL_NAME + ".c" )
+        dst = os.path.join( self.imageDir(), "src", PACKAGE_DLL_NAME + ".c" )
+        utils.moveFile( src, dst )
         utils.createImportLibs( PACKAGE_DLL_NAME, self.imageDir() )
-        return True
-
-    def make_package( self ):
-        self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, False )
-
         return True
 
 if __name__ == '__main__':
