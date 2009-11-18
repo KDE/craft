28a8487d6db27b6429c894557009227dd4cbc6a1
Ralf Habacker <ralf.habacker@freenet.de>
2009-11-18 23:53:40 +0000

    added command line ssh tools
    svn path=/trunk/kdesupport/emerge/; revision=1051198

---
+++ portage/dev-util/putty/putty-0.60.py
@@ -0,0 +1,27 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['0.60'] = """
+            http://the.earth.li/~sgtatham/putty/latest/x86/pscp.exe
+            http://the.earth.li/~sgtatham/putty/latest/x86/plink.exe
+            http://the.earth.li/~sgtatham/putty/latest/x86/psftp.exe
+        """
+        self.targetInstallPath['0.60'] = "bin"
+        self.defaultTarget = '0.60'
+
+from Package.BinaryPackageBase import *        
+        
+class Package(BinaryPackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        self.subinfo.options.merge.ignoreBuildType = True
+        self.subinfo.options.merge.destinationPath = 'dev-utils'
+        self.subinfo.options.package.withCompiler = False
+        BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
+
+
+
