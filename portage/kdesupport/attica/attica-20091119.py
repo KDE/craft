b83feb0900607afb63a3ff3f4d91f40e497237c9
Patrick Spendrin <ps_ml@gmx.de>
2009-11-20 16:41:45 +0000

    add attica which is needed for kdelibs
    svn path=/trunk/kdesupport/emerge/; revision=1052056

---
+++ portage/kdesupport/attica/attica-20091119.py
@@ -0,0 +1,20 @@
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['libs/qt'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/attica'
+        self.defaultTarget = 'svnHEAD'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
