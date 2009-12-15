8ebbeebcc52ab46f6a548a33b18b05bf5f2b6796
Patrick Spendrin <ps_ml@gmx.de>
2009-12-15 20:34:42 +0000

    update to libzip 0.9.0
    svn path=/trunk/kdesupport/emerge/; revision=1062740

--- portage/win32libs-sources/libzip-src/libzip-src-0.8.0.py
+++ portage/win32libs-sources/libzip-src/libzip-src-0.8.0.py
@@ -1,48 +1,23 @@
-import base
-import os
-import utils
+# -*- coding: utf-8 -*-
 import info
-import shutil
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        """ """
-        self.targets['0.8.0'] = "http://www.nih.at/libzip/libzip-0.8.tar.gz"
-        self.targetInstSrc['0.8.0'] = "libzip-0.8"
-        self.defaultTarget = '0.8.0'
+        self.targets['0.9.0'] = "http://www.nih.at/libzip/libzip-0.9.tar.bz2"
+        self.targetInstSrc['0.9.0'] = "libzip-0.9"
+        self.patchToApply['0.9.0'] = ( 'libzip-0.9.diff', 1 )
+        self.defaultTarget = '0.9.0'
+        self.options.package.withCompiler = False
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+from Package.CMakePackageBase import *
         
-class subclass(base.baseclass):
+class Package(CMakePackageBase):
     def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
         self.subinfo = subinfo()
-        self.createCombinedPackage = True
-        
-    def unpack( self ):
-        base.baseclass.unpack( self )
-        srcdir = os.path.join( self.packagedir, "zip_err_str.c" )
-        destdir = os.path.join( self.workdir, "libzip-0.8", "lib" )
-        utils.copySrcDirToDestDir( srcdir, destdir )
-        os.chdir( self.workdir )
-        self.system( "cd %s && patch -p0 < %s" % ( self.workdir, os.path.join( self.packagedir, "libzip.diff" ) ) )
-        return True
-        
+        CMakePackageBase.__init__(self)
 
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-    
-    def make_package( self ):
-        self.instsrcdir = ""
-
-        # auto-create both import libs with the help of pexports
-        self.createImportLibs( "libzip" )
-
-        # now do packaging with kdewin-packager
-        self.doPackaging( "libzip", "0.8.0", True )
-
-        return True
-  
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
