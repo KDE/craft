ce901fef7a7279aedba31f78e1801eccb6ebe140
Patrick Spendrin <ps_ml@gmx.de>
2009-12-18 20:59:59 +0000

    add binary package
    svn path=/trunk/kdesupport/emerge/; revision=1063583

---
+++ portage/win32libs-bin/ebook-tools/ebook-tools-0.1.1.py
@@ -0,0 +1,25 @@
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        repoUrl = """http://downloads.sourceforge.net/kde-windows"""
+        
+        for version in ['0.1.0', '0.1.1']:
+            self.targets[ version ] = repoUrl + """/ebook-tools-""" + version + """-bin.tar.bz2
+                                """ + repoUrl + """/ebook-tools-""" + version + """-lib.tar.bz2"""
+
+            
+        self.defaultTarget = '0.1.1'
+
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
