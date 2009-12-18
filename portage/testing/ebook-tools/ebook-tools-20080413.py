306b14294e99174521f4ac92a61bde21df6c79a4
Patrick Spendrin <ps_ml@gmx.de>
2009-12-18 17:13:39 +0000

    add 0.1.1 release
    svn path=/trunk/kdesupport/emerge/; revision=1063486

--- portage/testing/ebook-tools/ebook-tools-20080413.py
+++ portage/testing/ebook-tools/ebook-tools-20080413.py
@@ -1,45 +1,26 @@
-import base
-import utils
-import os
-import shutil
+# -*- coding: utf-8 -*-
 import info
-
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['0.1.0'] = 'http://downloads.sourceforge.net/ebook-tools/ebook-tools-0.1.0.tar.gz'
-        self.targetInstSrc['0.1.0'] = 'ebook-tools-0.1.0'
+        svnurl = "https://ebook-tools.svn.sourceforge.net/svnroot/ebook-tools/"
+        self.svnTargets['svnHEAD'] = svnurl + 'trunk/ebook-tools'
         self.targets['0.1.1'] = 'http://downloads.sourceforge.net/ebook-tools/ebook-tools-0.1.1.tar.gz'
         self.targetInstSrc['0.1.1'] = 'ebook-tools-0.1.1'
+        self.patchToApply['0.1.1'] = ('ebook-tools-0.1.1.diff', 1)
         self.defaultTarget = '0.1.1'
-    
+        self.options.package.withCompiler = False
+
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
-        self.hardDependencies['testing/libzip-src'] = 'default'
+        self.hardDependencies['win32libs-sources/libzip-src'] = 'default'
+
+from Package.CMakePackageBase import *
         
-class subclass(base.baseclass):
+class Package(CMakePackageBase):
     def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
         self.subinfo = subinfo()
-        self.createCombinedPackage = True
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        self.instsrcdir = ""
-
-        # auto-create both import libs with the help of pexports
-        self.createImportLibs( "libepub" )
-
-        # now do packaging with kdewin-packager
-        self.doPackaging( "ebook-tools", self.buildTarget, True )
-
-        return True
-  
+        CMakePackageBase.__init__(self)
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
