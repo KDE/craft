08e6397926a556b288ed0bae4e384df12eb0712c
Ralf Habacker <ralf.habacker@freenet.de>
2009-12-21 15:19:30 +0000

    added pageant
    svn path=/trunk/kdesupport/emerge/; revision=1064729

--- portage/dev-util/putty/putty-0.60.py
+++ portage/dev-util/putty/putty-0.60.py
@@ -6,6 +6,7 @@ class subinfo(info.infoclass):
             http://the.earth.li/~sgtatham/putty/latest/x86/pscp.exe
             http://the.earth.li/~sgtatham/putty/latest/x86/plink.exe
             http://the.earth.li/~sgtatham/putty/latest/x86/psftp.exe
+            http://the.earth.li/~sgtatham/putty/latest/x86/pageant.exe
         """
         self.targetInstallPath['0.60'] = "bin"
         self.defaultTarget = '0.60'
