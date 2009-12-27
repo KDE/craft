d390fdb5a2c2cb4b4326fdf8f8ebdc09ad33b94d
Patrick Spendrin <ps_ml@gmx.de>
2009-12-27 23:33:08 +0000

    add quassel CCMAIL: patrick.vonreth@gmail.com
    svn path=/trunk/kdesupport/emerge/; revision=1066811

---
+++ portage/testing/quassel/quassel-0.5.1-20091221.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import info
+import os
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://git.quassel-irc.org/quassel.git'
+        self.defaultTarget = 'gitHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['kdesupport/automoc'] = 'default'
+    
+
+class Package( CMakePackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.defines = ""
+        self.subinfo.options.configure.defines += " -DWITH_KDE=ON"
+
+    def unpack( self ):
+        if not CMakePackageBase.unpack( self ):
+            return False
+       #  self.source.shell.execute(os.environ["KDEROOT"],"pexport"sos.path.join( os.environ["SYSTEMROOT"], "System32" , "dbghelp.dll" ) )
+     #Advapi32.dll
+     #Secur32.dll
+    #dbgHelp.h
+        self.source.shell.execute( self.sourceDir(), "git", "reset --hard" )
+        utils.applyPatch( self.sourceDir(), os.path.join( self.packageDir(), "quassel.diff" ), 1 )
+
+        return True
+      
+
+
+if __name__ == '__main__':
+    Package().execute()
