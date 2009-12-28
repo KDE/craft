56bcf61e067b5c558cda2302cfe07a3793ceda26
Patrick Spendrin <ps_ml@gmx.de>
2009-12-28 20:19:53 +0000

    update to new buildsystem API fix build on mingw4
    svn path=/trunk/kdesupport/emerge/; revision=1067054

--- portage/win32libs-sources/openssl-src/openssl-src-0.9.8k.py
+++ portage/win32libs-sources/openssl-src/openssl-src-0.9.8k.py
@@ -8,60 +8,60 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['0.9.8k'] = 'http://www.openssl.org/source/openssl-0.9.8k.tar.gz'
         self.targetInstSrc['0.9.8k'] = 'openssl-0.9.8k'
+        self.patchToApply['0.9.8k'] = ('openssl-0.9.8k.diff', 1)
+        self.options.package.withCompiler = False
         self.defaultTarget = '0.9.8k'
 
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
         self.hardDependencies['dev-util/perl'] = 'default'
+        
+from Package.CMakePackageBase import *
+        
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
 
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, args=args )
-    self.createCombinedPackage = True
-    self.subinfo = subinfo()
-
-  def compile( self ):
-    os.chdir( os.path.join( self.workdir, self.instsrcdir ) )
-    cmd = ""
-    if self.compiler == "mingw" or self.compiler == "mingw4":
-      cmd = "ms\mingw32.bat"
-    else:
-      cmd = "ms\\32all.bat"
+    def compile( self ):
+        os.chdir( self.sourceDir() )
+        cmd = ""
+        if self.compiler() == "mingw" or self.compiler() == "mingw4":
+            cmd = "ms\mingw32.bat"
+        else:
+            cmd = "ms\\32all.bat"
       
-    return self.system( cmd )
+        return self.system( cmd )
 
-  def install( self ):
-    src = os.path.join( self.workdir, self.instsrcdir )
-    dst = self.imagedir
+    def install( self ):
+        src = self.sourceDir()
+        dst = self.imageDir()
 
-    if not os.path.isdir( dst ):
-      os.mkdir( dst )
-    os.mkdir( os.path.join( dst, "bin" ) )
-    os.mkdir( os.path.join( dst, "lib" ) )
-    os.mkdir( os.path.join( dst, "include" ) )
+        if not os.path.isdir( dst ):
+            os.mkdir( dst )
+            os.mkdir( os.path.join( dst, "bin" ) )
+            os.mkdir( os.path.join( dst, "lib" ) )
+            os.mkdir( os.path.join( dst, "include" ) )
 
-    if self.compiler == "mingw" or self.compiler == "mingw4":
-      shutil.copy( os.path.join( src, "libeay32.dll" ) , os.path.join( dst, "bin" ) )
-      shutil.copy( os.path.join( src, "libssl32.dll" ) , os.path.join( dst, "bin", "ssleay32.dll" ) )
-      utils.copySrcDirToDestDir( os.path.join( src, "outinc" ) , os.path.join( dst, "include" ) )
-      shutil.copy( os.path.join( src, "ms", "applink.c" ) , os.path.join( dst, "include", "openssl" ) )
+        if self.compiler() == "mingw" or self.compiler() == "mingw4":
+            shutil.copy( os.path.join( src, "libeay32.dll" ) , os.path.join( dst, "bin" ) )
+            shutil.copy( os.path.join( src, "libssl32.dll" ) , os.path.join( dst, "bin", "ssleay32.dll" ) )
+            utils.copySrcDirToDestDir( os.path.join( src, "outinc" ) , os.path.join( dst, "include" ) )
+            shutil.copy( os.path.join( src, "ms", "applink.c" ) , os.path.join( dst, "include", "openssl" ) )
 
-      # auto-create both import libs with the help of pexports
-      for f in "libeay32 ssleay32".split():
-        self.stripLibs( f )
-        self.createImportLibs( f )
+            # auto-create both import libs with the help of pexports
+            for f in "libeay32 ssleay32".split():
+                self.stripLibs( f )
+                self.createImportLibs( f )
 
-    else:
-      shutil.copy( os.path.join( src, "out32dll", "libeay32.dll" ) , os.path.join( dst, "bin" ) )
-      shutil.copy( os.path.join( src, "out32dll", "ssleay32.dll" ) , os.path.join( dst, "bin" ) )
-      shutil.copy( os.path.join( src, "out32dll", "libeay32.lib" ) , os.path.join( dst, "lib" ) )
-      shutil.copy( os.path.join( src, "out32dll", "ssleay32.lib" ) , os.path.join( dst, "lib" ) )
-      utils.copySrcDirToDestDir( os.path.join( src, "include" ) , os.path.join( dst, "include" ) )
-    
-    return True
+        else:
+            shutil.copy( os.path.join( src, "out32dll", "libeay32.dll" ) , os.path.join( dst, "bin" ) )
+            shutil.copy( os.path.join( src, "out32dll", "ssleay32.dll" ) , os.path.join( dst, "bin" ) )
+            shutil.copy( os.path.join( src, "out32dll", "libeay32.lib" ) , os.path.join( dst, "lib" ) )
+            shutil.copy( os.path.join( src, "out32dll", "ssleay32.lib" ) , os.path.join( dst, "lib" ) )
+            utils.copySrcDirToDestDir( os.path.join( src, "include" ) , os.path.join( dst, "include" ) )
 
-  def make_package( self ):
-    return self.doPackaging( "openssl", self.buildTarget + "-1", False )
+        return True
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
