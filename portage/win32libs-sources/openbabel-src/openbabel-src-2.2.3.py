f51d4714b6fb29a377a3c84e069220023038e906
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-01-04 20:05:17 +0000

    add 2.2.3
    svn path=/trunk/kdesupport/emerge/; revision=1070068

--- portage/win32libs-sources/openbabel-src/openbabel-src-2.2.3.py
+++ portage/win32libs-sources/openbabel-src/openbabel-src-2.2.3.py
@@ -9,29 +9,17 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['2.1.1'] = 'http://downloads.sourceforge.net/sourceforge/openbabel/openbabel-2.1.1.tar.gz'
         self.targets['2.2.0'] = 'http://downloads.sourceforge.net/sourceforge/openbabel/openbabel-2.2.0.tar.gz'
-        self.targetInstSrc['2.1.1'] = 'openbabel-2.1.1'
+        self.targets['2.2.3'] = 'http://downloads.sourceforge.net/sourceforge/openbabel/openbabel-2.2.3.tar.gz'
+        self.patchToApply['2.2.0'] = ('openbabel-2.2.0-cmake.diff', 0)
         self.targetInstSrc['2.2.0'] = 'openbabel-2.2.0'
-        self.defaultTarget = '2.2.0'
+        self.targetInstSrc['2.2.3'] = 'openbabel-2.2.3'
+        self.defaultTarget = '2.2.3'
         
 class subclass(base.baseclass):
     def __init__( self, **args ):
         base.baseclass.__init__( self, args=args )
         self.subinfo = subinfo()
-
-    def unpack( self ):
-        if( not base.baseclass.unpack( self ) ):
-            return False
-
-        patch = "openbabel-%s-cmake.diff" % self.buildTarget
-        cmd = "cd %s && patch -p0 < %s" % \
-              ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir , patch ) )
-        if utils.verbose() > 0:
-            print cmd
-        utils.system( cmd ) or utils.die( "patchin'" )
-        
-        return True
 
     def compile( self ):
         return self.kdeCompile()
