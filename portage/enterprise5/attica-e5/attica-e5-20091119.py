d8f04df99d279a0af28a13ce567698dc69b4bd9c
Till Adam <adam@kde.org>
2010-01-04 13:57:06 +0000

    Add attica build script and dependency.
    svn path=/trunk/kdesupport/emerge/; revision=1069951

---
+++ portage/enterprise5/attica-e5/attica-e5-20091119.py
@@ -0,0 +1,21 @@
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['libs/qt'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['20100101'] = 'tags/kdepim/enterprise5.0.20100101.1068602/kdesupport/attica'
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/attica'
+        self.defaultTarget = '20100101'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
