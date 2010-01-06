ca6ed25f37830695ebf5305d529b78d835ebc62d
Patrick Spendrin <ps_ml@gmx.de>
2010-01-06 03:28:39 +0000

    add Mercurial based version control There is no support for branches yet; the mercurial support is disabled at runtime if no hg can be found in the path. You can easily install mercurial via 'easy_install mercurial' or simply get the corresponding binaries for your python version.
    svn path=/trunk/kdesupport/emerge/; revision=1070578

---
+++ bin/Source/HgSource.py
@@ -0,0 +1,105 @@
+# 
+# copyright (c) 2009 Patrick Spendrin <ps_ml@gmx.de>
+#
+# mercurial support based on the git support
+
+from VersionSystemSourceBase import *
+import os
+import utils
+class HgSource ( VersionSystemSourceBase ):
+    """mercurial support"""
+    def __init__( self ):
+        VersionSystemSourceBase.__init__( self )
+
+        self.enableHg = True
+        try:
+            self.enableHg = self.system("hg help > NUL")
+        except:
+            self.enableHg = False
+
+        # initialize the repository
+        self.repo = None
+
+    def fetch( self, repopath=None ):
+        """try to clone or update the repository"""
+        utils.debug( "HgSource.fetch called", 2 )
+
+        # get the path where the repositories should be stored to
+        if repopath == None:
+            repopath = self.repositoryPath()
+        
+        # in case you need to move from a read only Url to a writeable one, here it gets replaced
+#        repoString = utils.replaceGitUrl( repopath )
+        repopath = repopath.replace("[hg]", "")
+        [repoUrl, repoBranch, repoTag ] = utils.splitGitUrl( repopath )
+        
+
+        if utils.verbose() <= 2:
+            devNull = " > NUL"
+        else:
+            devNull = ""
+        ret = True
+
+        # only run if wanted (e.g. no --offline is given on the commandline) or no hg support is given by the python installation
+        if ( not self.noFetch and self.enableHg ):
+            # question whether mercurial stuff uses these proxies
+            self.setProxy()
+
+            if os.path.exists( self.sourceDir() ):
+                # if directory already exists, simply do an update but obey to offline
+                os.chdir( self.sourceDir() )
+                ret = self.system( "hg update" )
+                
+            else:
+                # it doesn't exist so clone the repo
+                svnsrcdir = self.sourceDir().replace( self.package, "" )
+                if not os.path.exists( svnsrcdir ):
+                    os.makedirs( svnsrcdir )
+
+                os.chdir( svnsrcdir )
+                ret = self.system( "hg clone %s %s" % ( repoUrl, self.package ) )
+        else:
+            utils.debug( "skipping hg fetch (--offline)" )
+        return ret
+    
+    def applyPatches(self):
+        """apply patches to mercurial repository"""
+        utils.debug( "HgSource.applyPatches called", 2 )
+
+        if self.subinfo.hasTarget() or self.subinfo.hasSvnTarget() and self.enableHg:
+            ( file, patchdepth ) = self.subinfo.patchesToApply()
+            if file:
+                patchfile = os.path.join ( self.packageDir(), file )
+                os.chdir( self.sourceDir() )
+                return self.system( "hg import -p %s %s" % (patchdepth, patchfile) )
+        return True
+
+    def createPatch( self ):
+        """create patch file from git source into the related package dir. The patch file is named autocreated.patch"""
+        ret = False
+        if self.enableHg:
+            os.chdir( self.sourceDir() )
+            ret = self.system( self.sourceDir(), "hg diff > %s" % os.path.join( self.packageDir(), "%s-%s.patch" % ( self.package, str( datetime.date.today() ).replace('-', '') ) ) )
+        return ret
+
+    def sourceVersion( self ):
+        """ return the revision of the repository """
+
+        if self.enableHg:
+        
+            # open a temporary file - do not use generic tmpfile because this doesn't give a good file object with python
+            tempfile = open( os.path.join( self.sourceDir().replace('/', '\\'), ".emergehgtip.tmp" ), "wb+" )
+            
+            # run the command
+            ret = utils.system( "hg tip", tempfile )
+            tempfile.seek( os.SEEK_SET )
+
+            # read the temporary file and grab the first line
+            revision = tempfile.readline().replace("changeset:", "").strip()
+            tempfile.close()
+            
+            # print the revision - everything else should be quiet now
+            print revision
+            os.remove( os.path.join( self.sourceDir().replace('/', '\\'), ".emergehgtip.tmp" ) )
+        # always return True to not break something serious
+        return True
