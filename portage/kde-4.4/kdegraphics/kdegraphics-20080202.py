6737b0dd6f036cec24d00c5e1a35e850b77cbacd
Patrick Spendrin <ps_ml@gmx.de>
2010-01-12 18:31:28 +0000

    remove openmp for now as msvc only supports openmp 2.0 (and not the required 2.5)
    svn path=/trunk/kdesupport/emerge/; revision=1073714

--- portage/kde-4.4/kdegraphics/kdegraphics-20080202.py
+++ portage/kde-4.4/kdegraphics/kdegraphics-20080202.py
@@ -9,6 +9,7 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4']:
           self.targets['4.4.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.4.' + ver + '/src/kdegraphics-4.4.' + ver + '.tar.bz2'
           self.targetInstSrc['4.4.' + ver] = 'kdegraphics-4.4.' + ver
+        self.patchToApply['4.3.90'] = ('kdegraphics-4.3.90.diff', 1)
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
