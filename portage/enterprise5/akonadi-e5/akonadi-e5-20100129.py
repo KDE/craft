54bd4b0603dbb4404078846cc8bcfe8fb5b37c01
Thomas McGuire <mcguire@kde.org>
2010-02-05 17:46:51 +0000

    SVN_SILENT revert accidental commit
    svn path=/trunk/kdesupport/emerge/; revision=1085664

---
+++ portage/enterprise5/akonadi-e5/akonadi-e5-20100129.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import base
+import utils
+import sys
+import os
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['win32libs-bin/libxslt'] = 'default'
+        self.hardDependencies['win32libs-bin/shared-mime-info'] = 'default'
+        self.hardDependencies['win32libs-sources/boost-src']   = 'default'
+        self.hardDependencies['enterprise5/automoc-e5'] = 'default'
+        self.hardDependencies['enterprise5/soprano-e5'] = 'default'
+        self.hardDependencies['libs/qt'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['0.80'] = 'tags/akonadi/0.80'
+        self.svnTargets['0.81'] = 'tags/akonadi/0.81'
+        self.svnTargets['0.82'] = 'tags/akonadi/0.82'
+        self.svnTargets['1.0.0'] = 'tags/akonadi/1.0.0'
+        self.svnTargets['1.0.80'] = 'tags/akonadi/1.0.80'
+        self.svnTargets['1.1.0']  = 'tags/akonadi/1.1.0'
+        self.svnTargets['1.1.1']  = 'tags/akonadi/1.1.1'
+        self.svnTargets['1.1.2']  = 'tags/akonadi/1.1.2'
+        self.svnTargets['20091111'] = 'tags/kdepim/pe5.20091111/kdesupport/akonadi'
+        self.svnTargets['20091123'] = 'tags/kdepim/pe5.20091123/kdesupport/akonadi'
+        self.svnTargets['20091201'] = 'tags/kdepim/pe5.20091201/kdesupport/akonadi'
+        self.svnTargets['20100101'] = 'tags/kdepim/enterprise5.0.20100101.1068602/kdesupport/akonadi'
+        self.svnTargets['20100115'] = 'tags/kdepim/enterprise5.0.20100115.1075215/kdesupport/akonadi'
+        self.svnTargets['20100122'] = 'tags/kdepim/enterprise5.0.20100122.1078631/kdesupport/akonadi'
+        self.svnTargets['20100129'] = 'tags/kdepim/enterprise5.0.20100129.1082020/kdesupport/akonadi'
+        self.defaultTarget = '20100129'
+
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.instsrcdir = "akonadi"
+        self.subinfo = subinfo()
+
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        if self.buildTarget == "svnHEAD":
+            return self.doPackaging( "akonadi" )
+        else:
+            return self.doPackaging( "akonadi", self.buildTarget, True )
+
+if __name__ == '__main__':
+    subclass().execute()
