41b9f6f79d621941de431024f8229205378bbaea
Patrick Spendrin <ps_ml@gmx.de>
2010-02-09 11:03:04 +0000

    add the upstream git repo for dbus
    svn path=/trunk/kdesupport/emerge/; revision=1087549

--- portage/win32libs-sources/dbus-src/dbus-src-1.2.4.20091122.py
+++ portage/win32libs-sources/dbus-src/dbus-src-1.2.4.20091122.py
@@ -13,10 +13,13 @@ class subinfo(info.infoclass):
         self.defaultTarget = '1.2.4'
         self.targetInstSrc['1.2.4'] = 'tags/1.2.4'
         self.targetConfigurePath['1.2.4'] = 'cmake'
+        self.svnTargets['gitHEAD'] = 'git://anongit.freedesktop.org/git/dbus/dbus'
+        self.targetConfigurePath['gitHEAD'] = 'cmake'
         self.targetConfigurePath['svnHEAD'] = 'cmake'
     
     def setDependencies( self ):
         self.hardDependencies['win32libs-bin/libxml2'] = 'default'
+        self.hardDependencies['win32libs-bin/expat'] = 'default'
         self.hardDependencies['virtual/base'] = 'default'
 
 from Package.CMakePackageBase import *
@@ -26,12 +29,14 @@ class Package(CMakePackageBase):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
         self.subinfo.options.package.packageName = 'dbus'
+        self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON -DDBUS_DISABLE_EXECUTABLE_DEBUG_POSTFIX=ON"
         self.subinfo.options.make.slnBaseName = 'dbus'
         
     def unpack(self):
         if not CMakePackageBase.unpack(self):
             return False
-        utils.copyFile( os.path.join(self.packageDir(), "wspiapi.h"), os.path.join(self.buildDir(), "wspiapi.h") )
+        if self.buildTarget in ['1.2.1', '1.2.3', '1.2.4', 'svnHEAD']:
+            utils.copyFile( os.path.join(self.packageDir(), "wspiapi.h"), os.path.join(self.buildDir(), "wspiapi.h") )
         return True
 
     
