3cef3f65183a680c4c7e34e2a9fa6c2a2affec54
Patrick von Reth <patrick.vonreth@gmail.com>
2010-02-11 19:47:05 +0000

    added small mingw patch
    svn path=/trunk/kdesupport/emerge/; revision=1088851

--- portage/testing/libplist-src/libplist-src-1.2.py
+++ portage/testing/libplist-src/libplist-src-1.2.py
@@ -17,6 +17,12 @@ class Package(CMakePackageBase):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
 
-
+    def unpack( self ):
+        if not CMakePackageBase.unpack( self ):
+            return False
+        self.source.shell.execute( self.sourceDir(), "git", "reset --hard" )
+        utils.applyPatch( self.sourceDir(), os.path.join( self.packageDir(), "mingw.diff" ), 1 )
+        return True
+        
 if __name__ == '__main__':
     Package().execute()
