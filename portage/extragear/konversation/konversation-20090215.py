2619ad7a4603e63744be805b1a72ae1c248fed6f
Patrick Spendrin <ps_ml@gmx.de>
2010-02-12 20:21:23 +0000

    add 1.2.3 release
    svn path=/trunk/kdesupport/emerge/; revision=1089275

--- portage/extragear/konversation/konversation-20090215.py
+++ portage/extragear/konversation/konversation-20090215.py
@@ -7,6 +7,8 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['1.2.1'] = 'ftp://ftp.kde.org/pub/kde/stable/konversation/1.2.1/src/konversation-1.2.1.tar.bz2'
         self.targetInstSrc['1.2.1'] = 'konversation-1.2.1'
+        self.targets['1.2.3'] = 'ftp://ftp.kde.org/pub/kde/stable/konversation/1.2.3/src/konversation-1.2.3.tar.bz2'
+        self.targetInstSrc['1.2.1'] = 'konversation-1.2.3'
         self.svnTargets['gitHEAD'] = 'git://gitorious.org/konversation/konversation.git'
         self.defaultTarget = 'gitHEAD'
     
