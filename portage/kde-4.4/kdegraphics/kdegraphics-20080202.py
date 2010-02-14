0a3cc529c6963f8290d3cd118f42947d864c683f
Patrick Spendrin <ps_ml@gmx.de>
2010-02-14 14:02:49 +0000

    update patch
    svn path=/trunk/kdesupport/emerge/; revision=1090030

--- portage/kde-4.4/kdegraphics/kdegraphics-20080202.py
+++ portage/kde-4.4/kdegraphics/kdegraphics-20080202.py
@@ -11,6 +11,7 @@ class subinfo(info.infoclass):
           self.targetInstSrc['4.4.' + ver] = 'kdegraphics-4.4.' + ver
         self.patchToApply['4.3.90'] = ('kdegraphics-4.3.90.diff', 1)
         self.patchToApply['4.3.95'] = ('kdegraphics-4.3.95.diff', 1)
+        self.patchToApply['4.4.0'] = ('kdegraphics-4.4.0.diff', 1)
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
