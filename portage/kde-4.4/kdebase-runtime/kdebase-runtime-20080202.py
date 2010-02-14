808e21bcb1a2e9dda42327a627a9e939bce9a33d
Patrick Spendrin <ps_ml@gmx.de>
2010-02-14 13:50:34 +0000

    add build fix
    svn path=/trunk/kdesupport/emerge/; revision=1090028

--- portage/kde-4.4/kdebase-runtime/kdebase-runtime-20080202.py
+++ portage/kde-4.4/kdebase-runtime/kdebase-runtime-20080202.py
@@ -9,6 +9,7 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4']:
           self.targets['4.4.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.4.' + ver + '/src/kdebase-runtime-4.4.' + ver + '.tar.bz2'
           self.targetInstSrc['4.4.' + ver] = 'kdebase-runtime-4.4.' + ver
+        self.patchToApply['4.4.0'] = ('kdebase-runtime-4.4.0.diff', 1)
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
