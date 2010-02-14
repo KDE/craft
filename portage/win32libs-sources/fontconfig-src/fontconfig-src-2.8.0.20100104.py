c6fdc9cd50ee38f54cfebda7594a7c29dec0a8d0
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-02-14 18:34:46 +0000

    update freetype to 2.3.12 use new emerge buildsystem
    svn path=/trunk/kdesupport/emerge/; revision=1090134

--- portage/win32libs-sources/fontconfig-src/fontconfig-src-2.8.0.20100104.py
+++ portage/win32libs-sources/fontconfig-src/fontconfig-src-2.8.0.20100104.py
@@ -1,42 +1,22 @@
-import base
-import os
-import utils
-import shutil
 import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['2.7.3-1'] = "http://fontconfig.org/release/fontconfig-2.7.3.tar.gz"
         self.targets['2.8.0-1'] = "http://fontconfig.org/release/fontconfig-2.8.0.tar.gz"
-        self.patchToApply['2.7.3-1'] = ('fontconfig-2.7.3.diff', 1)
         self.patchToApply['2.8.0-1'] = ('fontconfig-2.8.0.diff', 1)
-        self.targetInstSrc['2.7.3-1'] = "fontconfig-2.7.3"
         self.targetInstSrc['2.8.0-1'] = "fontconfig-2.8.0"
         self.defaultTarget = '2.8.0-1'
     
     def setDependencies( self ):
         self.hardDependencies['kdesupport/kdewin'] = 'default'
         self.hardDependencies['win32libs-sources/freetype-src'] = 'default'
+        self.hardDependencies['win32libs-sources/libxml2-src'] = 'default'
     
-class subclass(base.baseclass):
+from Package.CMakePackageBase import *
+class Package(CMakePackageBase):
     def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.createCombinedPackage = False
         self.subinfo = subinfo()
-
-    def kdeSvnPath( self ):
-        return False
-    
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        self.doPackaging( "fontconfig", self.buildTarget, True )
-
-        return True
+        CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+  Package().execute()
---
+++ portage/win32libs-sources/freetype-src/freetype-src-2.3.12.20100214.py
@@ -0,0 +1,20 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['2.3.12-1'] = "http://downloads.sourceforge.net/freetype/freetype-2.3.12.tar.bz2"
+        self.patchToApply['2.3.12-1'] = ('freetype-2.3.12.diff', 1)
+        self.targetInstSrc['2.3.12-1'] = "freetype-2.3.12"
+        self.defaultTarget = '2.3.12-1'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+from Package.CMakePackageBase import *
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+  Package().execute()
