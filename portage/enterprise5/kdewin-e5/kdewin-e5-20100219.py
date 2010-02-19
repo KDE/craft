96bc242fbf4846c79995a5c705aa9bb6ebd00aee
Thomas McGuire <mcguire@kde.org>
2010-02-19 17:10:33 +0000

    There is no kdewin in the current tag, since there is no kdewin in kdesupport-for-4.4 anymore as well.
    Therefore remove that version from the emerge script.
    
    svn path=/trunk/kdesupport/emerge/; revision=1092891

--- portage/enterprise5/kdewin-e5/kdewin-e5-20100219.py
+++ portage/enterprise5/kdewin-e5/kdewin-e5-20100219.py
@@ -21,8 +21,7 @@ class subinfo(info.infoclass):
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/kdewin'
         self.svnTargets['20100205'] = 'tags/kdepim/enterprise5.0.20100205.1085631/kdesupport/kdewin'
         self.svnTargets['20100212'] = 'tags/kdepim/enterprise5.0.20100212.1089060/kdesupport/kdewin'
-        self.svnTargets['20100219'] = 'tags/kdepim/enterprise5.0.20100219.1092868/kdesupport/kdewin'
-        self.defaultTarget = '20100219'
+        self.defaultTarget = '20100205'
 
 class subclass(base.baseclass):
     def __init__( self, **args ):
