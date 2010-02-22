af991b12c26567534befc927da9e502210e19e12
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-02-22 15:51:53 +0000

    adopt to new classes
    svn path=/trunk/kdesupport/emerge/; revision=1094347

--- portage/win32libs-sources/bzip2-src/bzip2-src-1.0.5.py
+++ portage/win32libs-sources/bzip2-src/bzip2-src-1.0.5.py
@@ -1,53 +1,31 @@
-import base
-import os
-import shutil
 import utils
 import info
-
-PACKAGE_NAME         = "libbzip2"
+from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.0.4-6'] = 'http://www.bzip.org/1.0.4/bzip2-1.0.4.tar.gz'
-        self.targetInstSrc['1.0.4-6'] = "bzip2-1.0.4"
         self.targets['1.0.5-1'] = 'http://www.bzip.org/1.0.5/bzip2-1.0.5.tar.gz'
         self.targetInstSrc['1.0.5-1'] = "bzip2-1.0.5"
+        self.patchToApply['1.0.5-1'] = ("bzip.diff", 0)
         self.defaultTarget = '1.0.5-1'
 
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, args=args )
-    self.createCombinedPackage = True
-    self.buildType = "Release"
+class Package(CMakePackageBase):
+  def __init__( self ):
     self.subinfo = subinfo()
+    CMakePackageBase.__init__(self)
+    self.subinfo.options.package.packageName = 'libbzip2'
+    self.subinfo.options.package.withCompiler = None
+
+  def buildType( self ):
+    return "Release"
 
   def unpack( self ):
-    if( not base.baseclass.unpack( self ) ):
+    if( not CMakePackageBase.unpack( self ) ):
       return False
-    # we have an own cmake script - copy it to the right place
-    cmake_script = os.path.join( self.packagedir , "CMakeLists.txt" )
-    cmake_dest = os.path.join( self.workdir, self.instsrcdir, "CMakeLists.txt" )
-    shutil.copy( cmake_script, cmake_dest )
-
-    bzip2_dir  = os.path.join( self.workdir, self.instsrcdir )
-
-    cmd = "cd %s && patch -p0 < %s" % \
-          ( bzip2_dir, os.path.join( self.packagedir, "bzip.diff" ) )
-    utils.system( cmd )
-
-    return True
-
-  def compile( self ):
-    return self.kdeCompile()
-
-  def install( self ):
-    return self.kdeInstall()
-
-  def make_package( self ):
-    # do packaging with kdewin-packager
-    self.doPackaging( PACKAGE_NAME, self.buildTarget, True )
-
+    # copy CMakeLists.txt
+    utils.copyFile( os.path.join( self.packageDir(), "CMakeLists.txt" ),
+                    os.path.join( self.sourceDir(),  "CMakeLists.txt" ) )
     return True
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
--- portage/win32libs-sources/libpng-src/libpng-src-1.2.40.py
+++ portage/win32libs-sources/libpng-src/libpng-src-1.2.40.py
@@ -1,7 +1,6 @@
-import base
-import os
 import shutil
 import info
+from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
@@ -9,45 +8,42 @@ class subinfo(info.infoclass):
         self.targets[ver] = 'http://downloads.sourceforge.net/libpng/libpng-' + ver + '.tar.gz'
         self.targetInstSrc[ver] = 'libpng-' + ver
         self.defaultTarget = ver
-    
+
     def setDependencies( self ):
         self.hardDependencies['win32libs-bin/zlib'] = 'default'
 
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, args=args )
-    self.createCombinedPackage = True
-    self.buildType = "Release"
-    self.subinfo = subinfo()
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DPNG_TESTS=OFF -DPNG_STATIC=OFF -DPNG_NO_STDIO=OFF"
+        self.subinfo.options.package.packageName = 'libpng'
+        self.subinfo.options.package.withCompiler = None
+        if not (self.compiler == "mingw" or self.compiler == "mingw4"):
+            print "error: can only be build with MinGW (but in the end a MinGW/MSVC combined package is created)"
+            exit( 1 )
 
-  def unpack( self ):
-    if( not self.kdeSvnUnpack() ):
-      return False
-    # the cmake script is in libpng-src/scripts
-    srcdir  = os.path.join( self.workdir, self.instsrcdir, "scripts", "CMakeLists.txt" )
-    destdir = os.path.join( self.workdir, self.instsrcdir,            "CMakeLists.txt" )
-    shutil.copy( srcdir, destdir )
-    
-    return True
+    def buildType( self ):
+        return "Release"
 
-  def compile( self ):
-    self.kdeCustomDefines = "-DPNG_TESTS=OFF -DPNG_STATIC=OFF -DPNG_NO_STDIO=OFF"
-    return self.kdeCompile()
+    def unpack( self ):
+        if( not CMakePackageBase.unpack( self ) ):
+          return False
+        # the cmake script is in libpng-src/scripts
+        srcdir  = os.path.join( self.sourceDir(), "scripts", "CMakeLists.txt" )
+        destdir = os.path.join( self.sourceDir(),            "CMakeLists.txt" )
+        shutil.copy( srcdir, destdir )
 
-  def install( self ):
-    return self.kdeInstall()
+        return True
 
-  def make_package( self ):
-    # auto-create both import libs with the help of pexports
-    self.stripLibs( "libpng12" )
+    def createPackage( self ):
+        # auto-create both import libs with the help of pexports
+        self.stripLibs( "libpng12" )
 
-    # auto-create both import libs with the help of pexports
-    self.createImportLibs( "libpng12" )
-
-    # now do packaging with kdewin-packager
-    self.doPackaging( "libpng", self.buildTarget )
-
-    return True
+        # auto-create both import libs with the help of pexports
+        self.createImportLibs( "libpng12" )
+        
+        return CMakePackageBase.createPackage( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
