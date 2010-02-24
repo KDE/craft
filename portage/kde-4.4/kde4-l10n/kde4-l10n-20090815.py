84c710efd7810c7f3f7df4512691b1eee15be209
Patrick Spendrin <ps_ml@gmx.de>
2010-02-24 23:23:18 +0000

    this should build at least the packaged version now
    svn path=/trunk/kdesupport/emerge/; revision=1095744

--- portage/kde-4.4/kde4-l10n/kde4-l10n-20090815.py
+++ portage/kde-4.4/kde4-l10n/kde4-l10n-20090815.py
@@ -7,8 +7,10 @@ class subinfo(info.infoclass):
         self.defaultTarget = 'svnHEAD'
         self.svnTargets['svnHEAD'] = 'branches/stable/l10n-kde4/%s'
         for ver in ['0', '1', '2', '3', '4']:
-          self.targets['4.4.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.4.' + ver + '/src/kde-l10n/%s.tar.bz2' 
+          self.targets['4.4.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.4.' + ver + '/src/kde-l10n/kde-l10n-%s-4.4.' + ver + '.tar.bz2'
+          self.targetInstSrc['4.4.' + ver] = 'kde-l10n-%s-4.4.' + ver
         self.defaultTarget = 'svnHEAD'
+        
     
     def setDependencies( self ):
         self.hardDependencies['dev-util/cmake'] = 'default'
@@ -22,7 +24,7 @@ class Package(CMakePackageBase):
     def __init__( self  ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        self.language = 'de'
+        self.language = None
         # because of the large amount of packages
         # it is very annoying to restart the build, 
         # wasting several hours, so ignore any errors 
@@ -31,29 +33,61 @@ class Package(CMakePackageBase):
         self.subinfo.options.exitOnErrors = False
         # hardcoded for now
         #self.subinfo.options.package.version = '4.4.63'
+        # overwrite the workDir function that is inherited from SourceBase
+        self.source.workDir = self.workDir.__get__(self, Package)
+        self.source.localFileNames = self.localFileNames.__get__(self, Package)
+
+    def localFileNames(self):
+        filenames = []
+        for i in range( self.repositoryPathCount() ):
+            filenames.append( os.path.basename( self.repositoryPath( i ) ) )
+            print i
+        return filenames
+
+    def configureSourceDir(self):
+        return CMakePackageBase.configureSourceDir( self ) % self.language
 
     def repositoryPath(self,index=0):
         # \todo we cannot use CMakePackageBase here because repositoryPath 
         # then is not be overrideable for unknown reasons 
-        print 'repository path:', self.language
         url = CMakePackageBase.repositoryPath(self,index) % self.language
         return url
 
-    def sourceDir(self,index=0):
-        dir = CMakePackageBase.sourceDir(self,index) % self.language
+    def workDir(self):
+        dir = os.path.join(CMakePackageBase.workDir(self), self.language)
         return dir
-        
-    def buildRoot(self):
-        dir = os.path.join(CMakePackageBase.buildRoot(self), self.language)
-        return dir
+
+    def fetch(self):
+        filenames = self.localFileNames()
+
+        if ( self.source.noFetch ):
+            utils.debug( "skipping fetch (--offline)" )
+            return True
+
+        self.source.setProxy()
+        if self.subinfo.hasTarget():
+            return utils.getFiles( self.subinfo.target() % self.language, self.downloadDir() )
+        else:
+            return utils.getFiles( "", self.downloadDir() )
 
     def unpack(self):
         autogen = os.path.join( self.packageDir() , "autogen.py" )
-        if not CMakePackageBase.unpack(self): 
-            return False
+
+        filenames = self.localFileNames()
+
+        # the following code is taken from ArchiveSource::unpack() function
+        # it will not remove already existing files
+        destdir = self.workDir()
+        utils.debug( "unpacking files into work root %s" % destdir, 1 )
+
+        for filename in filenames:
+            utils.debug( "unpacking this file: %s" % filename, 1 )
+            if ( not utils.unpackFile( self.downloadDir(), filename, destdir ) ):
+                return False
+
         # execute autogen.py and generate the CMakeLists.txt files
-        cmd = "cd %s && python %s %s" % \
-              (self.sourceDir()+'/..', autogen, self.language )
+        cmd = "cd %s && python %s %s %s" % \
+              ('..', autogen, self.sourceDir() % self.language, self.language )
         return self.system( cmd )
 
     def configure(self):
@@ -82,7 +116,7 @@ class MainInfo(info.infoclass):
         self.languages += 'ta te tg th tr uk uz vi wa xh zh_CN zh_HK zh_TW '
 
         #for testing
-        self.languages  = 'de fr'
+        self.languages  = 'fr de'
     
     def setDependencies( self ):
         self.hardDependencies['dev-util/cmake'] = 'default'
@@ -109,7 +143,6 @@ class MainPackage(CMakePackageBase):
 #        else:
         languages = self.subinfo.languages.split()
         found=None
-            
         for language in languages:
             if not found and self.startLanguage:
                 if self.startLanguage <> language:
@@ -118,11 +151,11 @@ class MainPackage(CMakePackageBase):
                     found = True
             
             self.kde4_l10n.language = language
-            print self.kde4_l10n.language
+            utils.debug("current language: %s" % self.kde4_l10n.language, 1)
             self.kde4_l10n.runAction(command)
 #                self.errors["%s-%s" % (language, command)] = 1
 
-        print self.errors
+        utils.debug("Errors that happened while executing last command: %s" % self.errors, 2)
         return True    
         
 if __name__ == '__main__':
