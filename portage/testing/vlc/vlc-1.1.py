c0e4d993deda421b420995be8950c55df02afd7f
Patrick von Reth <patrick.vonreth@gmail.com>
2010-02-26 11:48:00 +0000

    fixed vlc installation, reverted git url for vlc-backend because the gitorious one is outdated
    svn path=/trunk/kdesupport/emerge/; revision=1096251

--- portage/testing/vlc/vlc-1.1.py
+++ portage/testing/vlc/vlc-1.1.py
@@ -7,12 +7,12 @@ import os
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['20100129-1616', '20100219-0002', '20100223-1143']:
+        for ver in ['20100226-0002']:
             self.targets[ ver ]  = "http://nightlies.videolan.org/build/win32/trunk-%s/vlc-1.1.0-git-%s-win32.7z" % (ver , ver )
             self.targetInstSrc[ver] = "vlc-1.1.0-git-%s" % (ver)
         
         
-        self.defaultTarget = '20100223-1143'  
+        self.defaultTarget = '20100226-0002'  
        
 
     def setDependencies( self ):
@@ -28,7 +28,8 @@ class Package(BinaryPackageBase):
     
   def install( self ):
     shutil.move( os.path.join( self.installDir() , self.subinfo.targetInstSrc[ self.subinfo.buildTarget ]) , os.path.join( self.installDir(), "bin" ) )
-    shutil.move( os.path.join( self.installDir() , "bin" , "sdk" ) , os.path.join( self.installDir(),"include"))
+    shutil.move( os.path.join( self.installDir() , "bin" , "sdk" , "include") , os.path.join( self.installDir(), "include" ) ) 
+    shutil.move( os.path.join( self.installDir() , "bin" , "sdk" , "lib") , os.path.join( self.installDir(), "lib" ) ) 
     return True 
     
 if __name__ == '__main__':
