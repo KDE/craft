8d70071b2b529c536de13b76533c660a496f32f2
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-03-04 17:37:23 +0000

    grep is needed too
    svn path=/trunk/kdesupport/emerge/; revision=1098925

--- portage/dev-util/msys/msys-1.0.13.py
+++ portage/dev-util/msys/msys-1.0.13.py
@@ -5,9 +5,13 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         repoUrl = """http://downloads.sourceforge.net/sourceforge/mingw"""
         msysFiles=""
-        for file in ['msysCORE-1.0.13-2-msys-1.0.13-bin.tar.lzma','bash-3.1.17-2-msys-1.0.11-bin.tar.lzma',
-                     'sed-4.2.1-1-msys-1.0.11-bin.tar.lzma','coreutils-5.97-2-msys-1.0.11-bin.tar.lzma',
-                     'gawk-3.1.7-1-msys-1.0.11-bin.tar.lzma','make-3.81-2-msys-1.0.11-bin.tar.lzma']:
+        for file in ['msysCORE-1.0.13-2-msys-1.0.13-bin.tar.lzma',
+                     'bash-3.1.17-2-msys-1.0.11-bin.tar.lzma',
+                     'sed-4.2.1-1-msys-1.0.11-bin.tar.lzma',
+                     'coreutils-5.97-2-msys-1.0.11-bin.tar.lzma',
+                     'gawk-3.1.7-1-msys-1.0.11-bin.tar.lzma',
+                     'make-3.81-2-msys-1.0.11-bin.tar.lzma',
+                     'grep-2.5.4-1-msys-1.0.11-bin.tar.lzma']:
             msysFiles = """%s
                         %s/%s""" % ( msysFiles , repoUrl , file )
         self.targets['1.0.13'] = msysFiles
