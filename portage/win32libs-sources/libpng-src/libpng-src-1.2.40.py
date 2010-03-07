6a47ea8031b1036e70ce8195100b32909065a672
Patrick von Reth <patrick.vonreth@gmail.com>
2010-03-07 01:02:14 +0000

    updated and fixed libpng-src
    svn path=/trunk/kdesupport/emerge/; revision=1100176

--- portage/win32libs-sources/libpng-src/libpng-src-1.2.40.py
+++ portage/win32libs-sources/libpng-src/libpng-src-1.2.40.py
@@ -4,7 +4,7 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = '1.2.40'
+        ver = '1.4.1'
         self.targets[ver] = 'http://downloads.sourceforge.net/libpng/libpng-' + ver + '.tar.gz'
         self.targetInstSrc[ver] = 'libpng-' + ver
         self.defaultTarget = ver
@@ -19,31 +19,9 @@ class Package(CMakePackageBase):
         self.subinfo.options.configure.defines = "-DPNG_TESTS=OFF -DPNG_STATIC=OFF -DPNG_NO_STDIO=OFF"
         self.subinfo.options.package.packageName = 'libpng'
         self.subinfo.options.package.withCompiler = None
-        if not (self.compiler == "mingw" or self.compiler == "mingw4"):
+        if not (self.compiler() == "mingw" or self.compiler() == "mingw4"):
             print "error: can only be build with MinGW (but in the end a MinGW/MSVC combined package is created)"
             exit( 1 )
-
-    def buildType( self ):
-        return "Release"
-
-    def unpack( self ):
-        if( not CMakePackageBase.unpack( self ) ):
-          return False
-        # the cmake script is in libpng-src/scripts
-        srcdir  = os.path.join( self.sourceDir(), "scripts", "CMakeLists.txt" )
-        destdir = os.path.join( self.sourceDir(),            "CMakeLists.txt" )
-        shutil.copy( srcdir, destdir )
-
-        return True
-
-    def createPackage( self ):
-        # auto-create both import libs with the help of pexports
-        self.stripLibs( "libpng12" )
-
-        # auto-create both import libs with the help of pexports
-        self.createImportLibs( "libpng12" )
-        
-        return CMakePackageBase.createPackage( self )
 
 if __name__ == '__main__':
     Package().execute()
