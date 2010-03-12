f80cd5568e87c33cb17190cabf9e1854155a49b9
Patrick von Reth <patrick.vonreth@gmail.com>
2010-03-12 10:43:17 +0000

    removed upstream  patch
    svn path=/trunk/kdesupport/emerge/; revision=1102352

--- portage/testing/libplist-src/libplist-src-1.2.py
+++ portage/testing/libplist-src/libplist-src-1.2.py
@@ -16,13 +16,6 @@ class Package(CMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
-
-    def unpack( self ):
-        if not CMakePackageBase.unpack( self ):
-            return False
-        self.source.shell.execute( self.sourceDir(), "git", "reset --hard" )
-        utils.applyPatch( self.sourceDir(), os.path.join( self.packageDir(), "mingw.diff" ), 1 )
-        return True
         
 if __name__ == '__main__':
     Package().execute()
