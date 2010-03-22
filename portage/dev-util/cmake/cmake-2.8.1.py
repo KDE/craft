4214aa0ec44690b8849b4e9a2ef3a376bc8973e3
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-03-22 20:34:05 +0000

    update to 2.8.1
    svn path=/trunk/kdesupport/emerge/; revision=1106429

--- portage/dev-util/cmake/cmake-2.8.1.py
+++ portage/dev-util/cmake/cmake-2.8.1.py
@@ -4,11 +4,11 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['2.4.8'] = 'http://www.cmake.org/files/v2.4/cmake-2.4.8-win32-x86.zip'
         self.targets['2.6.4'] = 'http://www.cmake.org/files/v2.6/cmake-2.6.4-win32-x86.zip'
-        self.targets['2.8.0'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.0-win32-x86.zip'
+        self.targets['2.8.1'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.1-win32-x86.zip'
         self.targetMergeSourcePath['2.4.8'] = 'cmake-2.4.8-win32-x86'
         self.targetMergeSourcePath['2.6.4'] = 'cmake-2.6.4-win32-x86'
-        self.targetMergeSourcePath['2.8.0'] = 'cmake-2.8.0-win32-x86'
-        self.defaultTarget = '2.8.0'
+        self.targetMergeSourcePath['2.8.1'] = 'cmake-2.8.1-win32-x86'
+        self.defaultTarget = '2.8.1'
 
 from Package.BinaryPackageBase import *
 
