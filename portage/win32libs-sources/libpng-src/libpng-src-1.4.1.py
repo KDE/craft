7a8204ce2171e1d54f2525066c02a6d756a297a9
Patrick von Reth <patrick.vonreth@gmail.com>
2010-03-24 10:34:03 +0000

    readded 1.2.40 and fiexed createPackage
    svn path=/trunk/kdesupport/emerge/; revision=1106961

--- portage/win32libs-sources/libpng-src/libpng-src-1.4.1.py
+++ portage/win32libs-sources/libpng-src/libpng-src-1.4.1.py
@@ -4,10 +4,11 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = '1.4.1'
-        self.targets[ver] = 'http://downloads.sourceforge.net/libpng/libpng-' + ver + '.tar.gz'
-        self.targetInstSrc[ver] = 'libpng-' + ver
-        self.defaultTarget = ver
+        for ver in ['1.4.1','1.2.40']:
+            self.targets[ver] = 'http://downloads.sourceforge.net/libpng/libpng-' + ver + '.tar.gz'
+            self.targetInstSrc[ver] = 'libpng-' + ver
+        
+        self.defaultTarget = '1.4.1'
 
     def setDependencies( self ):
         self.hardDependencies['win32libs-bin/zlib'] = 'default'
@@ -22,6 +23,30 @@ class Package(CMakePackageBase):
         if not (self.compiler() == "mingw" or self.compiler() == "mingw4"):
             print "error: can only be build with MinGW (but in the end a MinGW/MSVC combined package is created)"
             exit( 1 )
+            
+            
+    def unpack( self ):
+        if( not CMakePackageBase.unpack( self ) ):
+          return False 
+        if(self.subinfo.buildTarget.startswith('1.4')):
+          return True
+        # the cmake script is in libpng-src/scripts	 
+        srcdir  = os.path.join( self.sourceDir(), "scripts", "CMakeLists.txt" )	 
+        destdir = os.path.join( self.sourceDir(),            "CMakeLists.txt" )	 
+        shutil.copy( srcdir, destdir )
+        return True
+        
+        
+    def createPackage( self ): 
+        if(self.subinfo.buildTarget.startswith('1.2')):
+           libName="libpng12" 
+        else:
+           libName="libpng14" 
+        self.stripLibs( libName )
+        # auto-create both import libs with the help of pexports	 
+        self.createImportLibs( libName )
+        return CMakePackageBase.createPackage( self )
 
 if __name__ == '__main__':
     Package().execute()
+    
