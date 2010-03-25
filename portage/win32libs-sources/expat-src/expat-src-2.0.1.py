5631bbe3ff348e51ecdd838afbbf8807fc16c09d
Patrick von Reth <patrick.vonreth@gmail.com>
2010-03-25 21:27:52 +0000

    expat-src now realy is a src package
    svn path=/trunk/kdesupport/emerge/; revision=1107515

--- portage/win32libs-sources/expat-src/expat-src-2.0.1.py
+++ portage/win32libs-sources/expat-src/expat-src-2.0.1.py
@@ -1,61 +1,40 @@
+import os
+from shells import MSysShell
 import info
-import utils
+
 
 class subinfo(info.infoclass):
     def setDependencies( self ):
-        self.hardDependencies['dev-util/pexports'] = 'default'
-        self.hardDependencies['gnuwin32/sed'] = 'default'
-        self.hardDependencies['gnuwin32/wget'] = 'default'
+        self.hardDependencies['dev-util/msys'] = 'default'
+        self.hardDependencies['win32libs-sources/zlib-src'] = 'default'
 
     def setTargets( self ):
-        self.targets['2.0.1'] = 'http://downloads.sourceforge.net/sourceforge/expat/expat-win32bin-2.0.1.exe'
+        self.targets['2.0.1'] = 'http://downloads.sourceforge.net/sourceforge/expat/expat-2.0.1.tar.gz'
+     
+        
+        self.targetInstSrc['2.0.1'] = "expat-2.0.1"
+       
+
         self.defaultTarget = '2.0.1'
 
-from Package.BinaryPackageBase import *
+from Package.PackageBase import *
+from Source.MultiSource import *
+from BuildSystem.AutoToolsBuildSystem import *
+from Packager.KDEWinPackager import *;
 
-class Package(BinaryPackageBase):
+class Package( PackageBase, MultiSource, AutoToolsBuildSystem, KDEWinPackager):
     def __init__( self ):
         self.subinfo = subinfo()
-        BinaryPackageBase.__init__( self )
-        # don't use shortcut to unpack into imageDir()
-        self.buildSystemType = 'custom'
-        # create combined package
-        self.subinfo.options.package.withCompiler = None
-
-    def unpack( self ):
-  
-        # hopefully only one...
-        for filename in self.localFileNames():
-            self.system( os.path.join( self.downloadDir(), filename ) + " /DIR=\"" + self.workDir() + "\" /SILENT")
-        return True
-
-    def install( self ):
-        srcdir = self.sourceDir()
-        dstdir = self.installDir()
-
-        utils.cleanDirectory( dstdir )
-        os.makedirs( os.path.join( dstdir, "bin" ) )
-        os.makedirs( os.path.join( dstdir, "include" ) )
-        os.makedirs( os.path.join( dstdir, "lib" ) )
-
-        # expat binaries
-        utils.copyFile( os.path.join( srcdir, "Bin", "libexpat.dll" ),
-                        os.path.join( dstdir, "bin", "libexpat.dll") )
-        # doc can be used from zip package
-        utils.copyDir( os.path.join( srcdir, "doc" ),
-                       os.path.join( dstdir, "doc", self.package + "-" + self.subinfo.buildTarget ) )
-        # expat include dir
-        for f in ( "expat.h", "expat_external.h" ):
-          utils.copyFile( os.path.join( srcdir, "Source", "lib", f ),
-                          os.path.join( dstdir, "include", f ) )
-        # contrib
-        os.makedirs( os.path.join( dstdir, "contrib", self.package + "-" + self.subinfo.buildTarget ) )
-        utils.copyFile( os.path.join( srcdir, "README.txt" ),
-                        os.path.join( dstdir, "contrib", self.package + "-" + self.subinfo.buildTarget, "README.txt" ) )
-        # auto-create both import libs with the help of pexports
-        self.createImportLibs( "libexpat" )
+        PackageBase.__init__(self)
+        MultiSource.__init__(self)
+        AutoToolsBuildSystem.__init__(self)
+        KDEWinPackager.__init__(self)
         
-        return True
+    def createPackage( self ):
+        # auto-create both import libs with the help of pexports
+        self.createImportLibs( "libexpat-1" )
+        return KDEWinPackager.createPackage( self )
 
+           
 if __name__ == '__main__':
-    Package().execute()
+     Package().execute()
