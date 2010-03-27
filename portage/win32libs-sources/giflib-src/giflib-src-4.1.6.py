21ed2366d614450620be32be7912c3dd8881a9f9
Patrick Spendrin <ps_ml@gmx.de>
2010-03-27 16:47:44 +0000

    add a newer giflib version, add a cmake build system so we can build this library ourselves
    svn path=/trunk/kdesupport/emerge/; revision=1108036

---
+++ portage/win32libs-sources/giflib-src/giflib-src-4.1.6.py
@@ -0,0 +1,35 @@
+import shutil
+import info
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        for ver in ['4.1.6']:
+            self.targets[ver] = 'http://downloads.sourceforge.net/sourceforge/giflib/giflib-' + ver + '.tar.bz2'
+            self.targetInstSrc[ver] = 'giflib-' + ver
+        self.patchToApply['4.1.6'] = ("giflib-4.1.6-20100327.diff", 1)
+        
+        self.defaultTarget = '4.1.6'
+
+    def setDependencies( self ):
+        pass
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.package.packageName = 'giflib'
+        self.subinfo.options.configure.defines = "-DBUILD_utils=OFF"
+        self.subinfo.options.package.withCompiler = None
+            
+            
+    def createPackage( self ): 
+        libName="giflib4" 
+        self.stripLibs( libName )
+        # auto-create both import libs with the help of pexports	 
+        self.createImportLibs( libName )
+        return CMakePackageBase.createPackage( self )
+
+if __name__ == '__main__':
+    Package().execute()
+    
