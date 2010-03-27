fc3c889e90553ba3c03f5ca88a05fb14841f09be
Patrick von Reth <patrick.vonreth@gmail.com>
2010-03-27 00:17:55 +0000

    added openssl 1.0.0 beta 5 with x64 support
    svn path=/trunk/kdesupport/emerge/; revision=1107845

---
+++ portage/testing/openssl_beta-src/openssl_beta-src-20100327.py
@@ -0,0 +1,44 @@
+import os
+from shells import MSysShell
+import info
+
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['dev-util/msys'] = 'default'
+        self.hardDependencies['dev-util/perl'] = 'default'
+        self.hardDependencies['win32libs-sources/zlib-src'] = 'default'
+
+    def setTargets( self ):
+        self.targets['1.0.0-b5'] = 'http://www.openssl.org/source/openssl-1.0.0-beta5.tar.gz'
+     
+        
+        self.targetInstSrc['1.0.0-b5'] = "openssl-1.0.0-beta5"
+       
+
+        self.defaultTarget = '1.0.0-b5'
+
+from Package.PackageBase import *
+from Source.MultiSource import *
+from BuildSystem.AutoToolsBuildSystem import *
+from Packager.KDEWinPackager import *;
+
+class Package( PackageBase, MultiSource, AutoToolsBuildSystem, KDEWinPackager):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        PackageBase.__init__(self)
+        MultiSource.__init__(self)
+        AutoToolsBuildSystem.__init__(self)
+        KDEWinPackager.__init__(self)
+        self.buildInSource=True
+        if(self.compiler() == "mingw4"):
+          compiler="mingw"
+          if( os.getenv("EMERGE_ARCHITECTURE")=="x64"):
+            compiler="mingw64"
+        else:
+            print("msvc is not supported");
+        os.putenv("PERL",MSysShell().toNativePath(os.path.join( os.environ.get( "KDEROOT" ) , "dev-utils" , "bin" , "perl.exe" )))
+        self.subinfo.options.configure.defines = """ shared no-zlib """ +compiler 
+           
+if __name__ == '__main__':
+     Package().execute()
