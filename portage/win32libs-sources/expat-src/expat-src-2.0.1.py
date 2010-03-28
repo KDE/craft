b53bd6defa9ddc1f35c4527636f73c520a506331
Patrick Spendrin <ps_ml@gmx.de>
2010-03-28 23:27:34 +0000

    add cmake based build system for expat
    svn path=/trunk/kdesupport/emerge/; revision=1108458

--- portage/win32libs-sources/expat-src/expat-src-2.0.1.py
+++ portage/win32libs-sources/expat-src/expat-src-2.0.1.py
@@ -1,6 +1,6 @@
 import os
-from shells import MSysShell
 import info
+from Package.CMakePackageBase import *
 
 
 class subinfo(info.infoclass):
@@ -10,29 +10,20 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.targets['2.0.1'] = 'http://downloads.sourceforge.net/sourceforge/expat/expat-2.0.1.tar.gz'
-     
-        
-        self.targetInstSrc['2.0.1'] = "expat-2.0.1"
-       
-
+        self.targetInstSrc['2.0.1'] = 'expat-2.0.1'
+        self.patchToApply['2.0.1'] = ('expat-2.0.1-20100329.diff', 1)
         self.defaultTarget = '2.0.1'
 
-from Package.PackageBase import *
-from Source.MultiSource import *
-from BuildSystem.AutoToolsBuildSystem import *
-from Packager.KDEWinPackager import *;
-
-class Package( PackageBase, MultiSource, AutoToolsBuildSystem, KDEWinPackager):
-    def __init__( self ):
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
         self.subinfo = subinfo()
-        PackageBase.__init__(self)
-        MultiSource.__init__(self)
-        AutoToolsBuildSystem.__init__(self)
-        KDEWinPackager.__init__(self)
+        CMakePackageBase.__init__( self )
         
     def createPackage( self ):
-        # auto-create both import libs with the help of pexports
-        self.createImportLibs( "libexpat-1" )
+        libName="expat" 
+        self.stripLibs( libName )
+        # auto-create both import libs with the help of pexports	 
+        self.createImportLibs( libName )
         return KDEWinPackager.createPackage( self )
 
            
