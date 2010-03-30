eb3f6a1132029de3d0aedc006df48af572738f26
Romain Pokrzywka <romain@kdab.net>
2010-03-30 19:56:57 +0000

    and now dbus has WinCE building support too, based on Marcus's work in dbus4win (pending inclusion in upstream)
    svn path=/branches/kdesupport/emerge/wince-cross-compile/; revision=1109217

--- portage/win32libs-sources/dbus-src/dbus-src-1.2.4.20091122.py
+++ portage/win32libs-sources/dbus-src/dbus-src-1.2.4.20091122.py
@@ -9,17 +9,29 @@ class subinfo(info.infoclass):
         self.svnTargets['1.2.1'] = svnurl + 'tags/1.2.1'
         self.svnTargets['1.2.3'] = svnurl + 'tags/1.2.3'
         self.svnTargets['1.2.4'] = svnurl + 'tags/1.2.4'
-        self.svnTargets['svnHEAD'] = svnurl + 'trunk'
-        self.defaultTarget = '1.2.4'
         self.targetInstSrc['1.2.4'] = 'tags/1.2.4'
-        self.targetConfigurePath['1.2.4'] = 'cmake'
+        self.svnTargets['svnHEAD'] = svnurl + 'trunk'
         self.svnTargets['gitHEAD'] = 'git://anongit.freedesktop.org/git/dbus/dbus'
+        self.svnTargets['git-wince'] = 'git://repo.or.cz/dbus4win.git|marcus-dbus-master-wince-upstream'
+        self.targetInstSrc['git-wince'] = 'dbus4win-wince'
+        self.patchToApply['git-wince'] = ('dbus-wince5-msvc.patch', 1)
+
+        self.targetConfigurePath['1.2.4'] = 'cmake'
         self.targetConfigurePath['gitHEAD'] = 'cmake'
         self.targetConfigurePath['svnHEAD'] = 'cmake'
-    
+        self.targetConfigurePath['git-wince'] = 'cmake'
+
+        if self.hasTargetPlatform():
+            self.defaultTarget = 'git-wince'
+        else:
+            self.defaultTarget = '1.2.4'
+
     def setDependencies( self ):
-        self.hardDependencies['win32libs-bin/libxml2'] = 'default'
-        self.hardDependencies['win32libs-bin/expat'] = 'default'
+        if self.hasTargetPlatform():
+            self.hardDependencies['win32libs-sources/libxml2-src'] = 'default'
+        else:
+            self.hardDependencies['win32libs-bin/libxml2'] = 'default'
+            self.hardDependencies['win32libs-bin/expat'] = 'default'
         self.hardDependencies['virtual/base'] = 'default'
 
 from Package.CMakePackageBase import *
@@ -29,8 +41,11 @@ class Package(CMakePackageBase):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
         self.subinfo.options.package.packageName = 'dbus'
-        self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON -DDBUS_DISABLE_EXECUTABLE_DEBUG_POSTFIX=ON"
         self.subinfo.options.make.slnBaseName = 'dbus'
+        if self.hasTargetPlatform():
+            self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=OFF"
+        else:
+            self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON -DDBUS_DISABLE_EXECUTABLE_DEBUG_POSTFIX=ON"
         
     def unpack(self):
         if not CMakePackageBase.unpack(self):
