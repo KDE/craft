1e396bcb3ffa65e7edc7b6a4368284cd60340f65
Stephen Kelly <steveire@gmail.com>
2010-04-09 12:20:50 +0000

    Fix the package to fetch from the correct branch and use the correct options.
    svn path=/trunk/kdesupport/emerge/; revision=1112883

--- portage/kdesupport/grantlee/grantlee-20091123.py
+++ portage/kdesupport/grantlee/grantlee-20091123.py
@@ -3,9 +3,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['0.1'] = "git://gitorious.org/grantlee/grantlee.git"
+        self.svnTargets['gitHEAD'] = "git://gitorious.org/grantlee/grantlee.git"
+        self.svnTargets['0.1'] = "git://gitorious.org/grantlee/grantlee.git|0.1"
         self.defaultTarget = '0.1'
-        self.options.package.withCompiler = False
 
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
@@ -17,9 +17,6 @@ class Package(CMakePackageBase):
     def __init__( self):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
-        if self.buildTarget == '0.1':
-            self.subinfo.options.package.withCompiler = True
-
         
 if __name__ == '__main__':
     Package().execute()
