ffae308750a30bd4f547729ddd4707c984283585
Carlo Segato <brandon.ml@gmail.com>
2010-04-11 14:45:53 +0000

    add libktorrent
    svn path=/trunk/kdesupport/emerge/; revision=1113699

--- portage/extragear/libktorrent/libktorrent-20100411.py
+++ portage/extragear/libktorrent/libktorrent-20100411.py
@@ -4,19 +4,14 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = 'trunk/extragear/network/ktorrent'
-        for version in ['3.2beta1', '3.2rc1', '3.2', '3.2.2', '3.2.3', '3.3beta1']:
-            self.targets[version] = 'http://ktorrent.org/downloads/' + version + '/ktorrent-' + version + '.tar.bz2'
-            self.targetInstSrc[version] = 'ktorrent-' + version
+        self.svnTargets['svnHEAD'] = 'trunk/extragear/network/libktorrent'
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
         self.hardDependencies['kde/kdebase-runtime'] = 'default'
-        self.hardDependencies['kde/kdepimlibs'] = 'default'
         self.hardDependencies['kdesupport/qca'] = 'default'
         self.hardDependencies['win32libs-bin/libgmp'] = 'default'
         self.hardDependencies['dev-util/gettext-tools'] = 'default'
-        self.hardDependencies['extragear/libktorrent'] = 'default'
     
 class Package(CMakePackageBase):
     def __init__( self):
