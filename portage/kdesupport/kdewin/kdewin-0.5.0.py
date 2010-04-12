4a9a4a3d5db5b10ebde225b099f11d6f60256259
Ralf Habacker <ralf.habacker@freenet.de>
2010-04-12 08:37:02 +0000

    update to recent version
    svn path=/trunk/kdesupport/emerge/; revision=1113912

--- portage/kdesupport/kdewin/kdewin-0.5.0.py
+++ portage/kdesupport/kdewin/kdewin-0.5.0.py
@@ -19,6 +19,7 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
+        self.subinfo.options.package.version = '0.5.0'
         CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
