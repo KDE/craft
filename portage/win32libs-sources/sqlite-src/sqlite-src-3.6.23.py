6fde3baf1934e6cd2033de4cd77be9d32276b234
Patrick von Reth <patrick.vonreth@gmail.com>
2010-04-12 22:35:04 +0000

    changed buildsystem of sqllite to cmake
    svn path=/trunk/kdesupport/emerge/; revision=1114182

--- portage/win32libs-sources/sqlite-src/sqlite-src-3.6.23.py
+++ portage/win32libs-sources/sqlite-src/sqlite-src-3.6.23.py
@@ -1,6 +1,8 @@
 import os
-from shells import MSysShell
+import utils
 import info
+from Package.CMakePackageBase import *
+
 
 
 class subinfo(info.infoclass):
@@ -9,23 +11,22 @@ class subinfo(info.infoclass):
         self.targetInstSrc['3.6.23.1'] = "sqlite-3.6.23.1"
         self.defaultTarget = '3.6.23.1'
 
-    def setDependencies( self ):
-        self.hardDependencies['dev-util/msys'] = 'default'
-        self.hardDependencies['dev-util/perl'] = 'default'
-        self.hardDependencies['win32libs-bin/zlib'] = 'default'
+class Package(CMakePackageBase):
+  def __init__( self ):
+    self.subinfo = subinfo()
+    CMakePackageBase.__init__(self)
 
-from Package.PackageBase import *
-from Source.MultiSource import *
-from BuildSystem.AutoToolsBuildSystem import *
-from Packager.KDEWinPackager import *;
 
-class Package( PackageBase, MultiSource, AutoToolsBuildSystem, KDEWinPackager):
-    def __init__( self ):
-        self.subinfo = subinfo()
-        PackageBase.__init__(self)
-        MultiSource.__init__(self)
-        AutoToolsBuildSystem.__init__(self)
-        KDEWinPackager.__init__(self)
+  def unpack( self ):
+    if( not CMakePackageBase.unpack( self ) ):
+      return False
+    # copy CMakeLists.txt
+    utils.copyFile( os.path.join( self.packageDir(), "CMakeLists.txt" ),
+                    os.path.join( self.sourceDir(),  "CMakeLists.txt" ) )
+    return True
+
+
+    return CMakePackageBase.createPackage( self )
 
 if __name__ == '__main__':
-     Package().execute()
+    Package().execute()
