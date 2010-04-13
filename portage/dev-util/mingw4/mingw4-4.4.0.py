d12fdbda9795b9327528020e11cbb8b942f2a079
Ralf Habacker <ralf.habacker@freenet.de>
2010-04-13 12:37:56 +0000

    added sha1 digests for 4.4.0 target
    svn path=/trunk/kdesupport/emerge/; revision=1114396

--- portage/dev-util/mingw4/mingw4-4.4.0.py
+++ portage/dev-util/mingw4/mingw4-4.4.0.py
@@ -40,7 +40,20 @@ class subinfo(info.infoclass):
                                                  'gmp-4.2.4-mingw32-dll.tar.gz',
                                                  'mpfr-2.4.1-mingw32-dll.tar.gz'])
         self.patchToApply['4.4.0']=('STRICT_ANSI.diff',0)
-        
+        self.targetDigests['4.4.0'] = ['8352235ab799e69dc0cfe34dd58193f1003de2dc',
+                                       'c13767263c42d0d964443ccf729499ed05492824',
+                                       '5ecc6db65849cfe2af2ab6226e55bd7ebf704f00',
+                                       '0c562f3b6a89f376b9edba48ccd7388c535f8c8d',
+                                       'f1c81109796c4c87243b074ebb5f85a5552e0219',
+                                       '04be438bb0edae03e5d16bef0ad7fe29302a6f25',
+                                       '7ae32742ece1e89978784634aed824775cf47336',
+                                       'b88b8f3644ca0cdf2c41cd03f820bf7823a8eabb',
+                                       '0372ecf4caf75d0d9fe4a7739ca234f1a3de831b',
+                                       'a87c5bdcab060999455e89f1f01679dc9d9b85ec',
+                                       '210c1fe3a9fb2d4f7baf6e06f3fd8eecb689fa3a',
+                                       'a14dd928382f093f67cb3cd57c140625b1b265bb',
+                                       '43b7ecb2c0c785c44321ff6c4376f51375713a7b']
+
         self.targets['4.5.0'] = self.getPackageList('http://downloads.sourceforge.net/sourceforge/mingw',
                                                  ['binutils-2.20.1-2-mingw32-bin.tar.gz',
                                                  'make-3.81-20090914-mingw32-bin.tar.gz',
