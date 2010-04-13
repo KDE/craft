2126ff0c7af0eac7377ec427f3aeb7dce55894c2
Patrick Spendrin <ps_ml@gmx.de>
2010-04-13 04:40:04 +0000

    add exports so that a shared library can be build under msvc
    svn path=/trunk/kdesupport/emerge/; revision=1114221

--- portage/win32libs-sources/sqlite-src/sqlite-src-3.6.23.py
+++ portage/win32libs-sources/sqlite-src/sqlite-src-3.6.23.py
@@ -9,24 +9,13 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['3.6.23.1'] = 'http://www.sqlite.org/sqlite-amalgamation-3.6.23.1.tar.gz'
         self.targetInstSrc['3.6.23.1'] = "sqlite-3.6.23.1"
+        self.patchToApply['3.6.23.1'] = ( "sqlite-3.6.23.1-20100413.diff", 1 )
         self.defaultTarget = '3.6.23.1'
 
 class Package(CMakePackageBase):
   def __init__( self ):
     self.subinfo = subinfo()
     CMakePackageBase.__init__(self)
-
-
-  def unpack( self ):
-    if( not CMakePackageBase.unpack( self ) ):
-      return False
-    # copy CMakeLists.txt
-    utils.copyFile( os.path.join( self.packageDir(), "CMakeLists.txt" ),
-                    os.path.join( self.sourceDir(),  "CMakeLists.txt" ) )
-    return True
-
-
-    return CMakePackageBase.createPackage( self )
 
 if __name__ == '__main__':
     Package().execute()
