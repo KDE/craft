66e741e87c456bc2ae160f98040505ce2dcd5051
Patrick Spendrin <ps_ml@gmx.de>
2010-04-16 08:48:03 +0000

    7 to go!!! add a cmake build system for lcms
    svn path=/trunk/kdesupport/emerge/; revision=1115380

---
+++ portage/win32libs-sources/lcms-src/lcms-src-1.19.py
@@ -0,0 +1,25 @@
+import os
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['1.19'] = "http://www.littlecms.com/lcms-1.19.tar.gz"
+        self.targetInstSrc['1.19'] = "lcms-1.19"
+        self.patchToApply['1.19'] = ( 'lcms-1.19-20100416.diff', 1 )
+        self.defaultTarget = '1.19'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        # both examples and tests can be run here
+        self.subinfo.options.configure.defines = "-DBUILD_TESTS=OFF -DBUILD_SAMPLES=ON -DBUILD_TOOLS=OFF"
+        self.subinfo.options.package.withCompiler = None
+
+if __name__ == '__main__':
+    Package().execute()
