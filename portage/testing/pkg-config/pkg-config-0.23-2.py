5231bc228ff834ebd9a27575d82b62861be1814a
Patrick von Reth <patrick.vonreth@gmail.com>
2010-04-18 22:09:03 +0000

    added x64 support
    svn path=/trunk/kdesupport/emerge/; revision=1116230

--- portage/testing/pkg-config/pkg-config-0.23-2.py
+++ portage/testing/pkg-config/pkg-config-0.23-2.py
@@ -3,15 +3,18 @@ import os
 import info
 
 class subinfo(info.infoclass):
-    def setTargets( self ):        
-        for version in ['0.23-3']:
-            self.targets[ version ] = """http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/pkg-config_0.23-3_win32.zip"""
+    def setTargets( self ):      
+        arch='32'
+        if( os.getenv('EMERGE_ARCHITECTURE')=="x64"):
+           arch='64'
+        for version in ['0.23-2']: 
+            self.targets[ version ] = "http://ftp.gnome.org/pub/gnome/binaries/win"+ arch+"/dependencies/pkg-config_0.23-2_win"+arch+".zip"
 
-        self.defaultTarget = '0.23-3'
+        self.defaultTarget = '0.23-2'
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
-        self.hardDependencies['testing/glib'] = 'default'
+
 
 class Package(BinaryPackageBase):
   def __init__(self):
