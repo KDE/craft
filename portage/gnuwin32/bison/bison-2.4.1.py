0fa61ac9f514ac5411cab1b352f795afca179aa5
Ralf Habacker <ralf.habacker@freenet.de>
2010-04-18 22:03:31 +0000

    we need the dependencies too
    svn path=/trunk/kdesupport/emerge/; revision=1116228

--- portage/gnuwin32/bison/bison-2.4.1.py
+++ portage/gnuwin32/bison/bison-2.4.1.py
@@ -7,8 +7,10 @@ import utils
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['2.4.1'] = "http://downloads.sourceforge.net/project/gnuwin32/bison/2.4.1/bison-2.4.1-bin.zip"
-        self.targetDigests['2.4.1'] = '2e05049a8519ea6b802553adf7b46da358e05c81'
+        self.targets['2.4.1'] = """http://downloads.sourceforge.net/project/gnuwin32/bison/2.4.1/bison-2.4.1-bin.zip
+                                 http://downloads.sourceforge.net/project/gnuwin32/bison/2.4.1/bison-2.4.1-dep.zip"""
+        self.targetDigests['2.4.1'] = ['2e05049a8519ea6b802553adf7b46da358e05c81',
+                                       '820e44f4d0d87c3fdff00cb823388b9cc9e1941b']                                 
         self.defaultTarget = '2.4.1'
 
 from Package.BinaryPackageBase import *
