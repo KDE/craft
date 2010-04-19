ace03f0c03a4e3b421e75990936f55b72d24acff
Ralf Habacker <ralf.habacker@freenet.de>
2010-04-19 14:10:21 +0000

    msvc build fix
    svn path=/trunk/kdesupport/emerge/; revision=1116446

--- portage/testing/phonon-vlc/phonon-vlc-0.2.py
+++ portage/testing/phonon-vlc/phonon-vlc-0.2.py
@@ -7,6 +7,9 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setDependencies( self ):
       self.hardDependencies['testing/vlc'] = 'default'
+      if COMPILER == "msvc2008":
+        self.hardDependencies['kdesupport/kdewin'] = 'default'
+        self.patchToApply['gitHEAD'] = ['0001-msvc-build-fixes.patch',1]
         
     def setTargets( self ):
       self.svnTargets['gitHEAD'] = 'git://gitorious.org/phonon/phonon-vlc.git'
@@ -17,6 +20,8 @@ class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)       
+        if COMPILER == "msvc2008":
+            self.subinfo.options.configure.defines = ' -DCMAKE_INCUDE_PATH=%s/include/msvc' % self.mergeDestinationDir()
 
 if __name__ == '__main__':
     Package().execute()
