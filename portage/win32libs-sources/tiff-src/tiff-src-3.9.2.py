ab9edf42e48df8f7cca49b586c87d6ac2713470e
Patrick Spendrin <ps_ml@gmx.de>
2010-04-19 13:38:41 +0000

    add tiff package although I have the feeling I was missing something - 6 to go
    svn path=/trunk/kdesupport/emerge/; revision=1116430

---
+++ portage/win32libs-sources/tiff-src/tiff-src-3.9.2.py
@@ -0,0 +1,25 @@
+import os
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['3.9.2'] = "ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.2.tar.gz"
+        self.targetInstSrc['3.9.2'] = "tiff-3.9.2"
+        self.patchToApply['3.9.2'] = ( 'tiff-3.9.2-20100418.diff', 1 )
+        self.defaultTarget = '3.9.2'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        # both examples and tests can be run here
+        self.subinfo.options.configure.defines = "-DBUILD_TESTS=OFF -DBUILD_SAMPLES=ON -DBUILD_TOOLS=OFF"
+        self.subinfo.options.package.withCompiler = None
+
+if __name__ == '__main__':
+    Package().execute()
