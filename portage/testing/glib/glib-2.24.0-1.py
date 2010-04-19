bdf3b3bcade3761f05cb4a68d166fefcaaa7fd3a
Ralf Habacker <ralf.habacker@freenet.de>
2010-04-19 14:22:42 +0000

    added digests
    svn path=/trunk/kdesupport/emerge/; revision=1116449

--- portage/testing/glib/glib-2.24.0-1.py
+++ portage/testing/glib/glib-2.24.0-1.py
@@ -11,6 +11,8 @@ class subinfo(info.infoclass):
             self.targets[ version ] = self.getPackageList("ftp.gnome.org/pub/gnome/binaries/win"+arch+"/glib/2.24",
                                                           ["glib_2.24.0-1_win"+arch+".zip",
                                                           "glib-dev_2.24.0-1_win"+arch+".zip"])
+        self.targetDigests['2.24'] = ['11c52267d9ae06f507a3eee66ae25e35c89b8c77',
+                                      '45bd47470d10e691d29f0e3a9ce1371a0d38f149']                                                          
    
         self.defaultTarget = '2.24'
 
