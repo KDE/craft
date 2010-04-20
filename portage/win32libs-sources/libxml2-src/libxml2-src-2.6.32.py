4b454c0d2e146c68263cd7e0a99e153db113589f
Romain Pokrzywka <romain@kdab.net>
2010-04-20 14:34:24 +0000

    adjust to new functions naming
    svn path=/branches/kdesupport/emerge/wince-cross-compile/; revision=1116905

--- portage/win32libs-sources/libxml2-src/libxml2-src-2.6.32.py
+++ portage/win32libs-sources/libxml2-src/libxml2-src-2.6.32.py
@@ -6,13 +6,15 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['2.7.7'] = "ftp://xmlsoft.org/libxml2/libxml2-sources-2.7.7.tar.gz"
         self.targetInstSrc['2.7.7'] = 'libxml2-2.7.7'
+        self.patchToApply['2.7.7'] = ('libxml2-2.7.7-wince5.patch', 0)
         self.defaultTarget = '2.7.7'
-        
-        if self.hasTargetPlatform():
-            self.patchToApply['2.7.7'] = ('libxml2-2.7.7-wince5.patch', 0)
 
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
 
 from Package.CMakePackageBase import *
 
@@ -51,11 +53,8 @@ class Package(CMakePackageBase):
         
     def make(self):
         os.chdir( os.path.join( self.sourceDir(), "win32" ) )
-        
-        if self.hasTargetPlatform():
-            self.setupCrossToolchain()
-
-        return self.system( "nmake /f Makefile.msvc rebuild" )
+        self.setupTargetToolchain()
+        return self.system( "nmake /f Makefile.msvc all" )
         
     def install(self):
         os.chdir( os.path.join( self.sourceDir(), "win32" ) )
