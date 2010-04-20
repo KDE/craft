42e7525e8a8fb3417adcf94f5ab689433256edc4
Romain Pokrzywka <romain@kdab.net>
2010-04-20 21:16:11 +0000

    adjust to new functions naming, switch to fredesktop.org dbus (master)
    svn path=/branches/kdesupport/emerge/wince-cross-compile/; revision=1117012

--- portage/win32libs-sources/dbus-src/dbus-src-1.2.4.20091122.py
+++ portage/win32libs-sources/dbus-src/dbus-src-1.2.4.20091122.py
@@ -6,32 +6,22 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         svnurl = "https://windbus.svn.sourceforge.net/svnroot/windbus/"
-        self.svnTargets['1.2.1'] = svnurl + 'tags/1.2.1'
-        self.svnTargets['1.2.3'] = svnurl + 'tags/1.2.3'
         self.svnTargets['1.2.4'] = svnurl + 'tags/1.2.4'
         self.targetInstSrc['1.2.4'] = 'tags/1.2.4'
         self.svnTargets['svnHEAD'] = svnurl + 'trunk'
         self.svnTargets['gitHEAD'] = 'git://anongit.freedesktop.org/git/dbus/dbus'
-        self.svnTargets['git-wince'] = 'git://repo.or.cz/dbus4win.git|marcus-dbus-master-wince-upstream'
-        self.targetInstSrc['git-wince'] = 'dbus4win-wince'
 
         self.targetConfigurePath['1.2.4'] = 'cmake'
         self.targetConfigurePath['gitHEAD'] = 'cmake'
         self.targetConfigurePath['svnHEAD'] = 'cmake'
-        self.targetConfigurePath['git-wince'] = 'cmake'
 
-        if self.hasTargetPlatform():
-            self.defaultTarget = 'git-wince'
-        else:
-            self.defaultTarget = '1.2.4'
+        self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
-        if self.hasTargetPlatform():
-            self.hardDependencies['win32libs-sources/libxml2-src'] = 'default'
-        else:
-            self.hardDependencies['win32libs-bin/libxml2'] = 'default'
-            self.hardDependencies['win32libs-bin/expat'] = 'default'
         self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['win32libs-bin/expat'] = 'default'
+        if utils.isCrossCompilingEnabled():
+            self.hardDependencies['win32libs-sources/libxml2-src'] = 'default'
 
 from Package.CMakePackageBase import *
                 
@@ -41,10 +31,10 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__( self )
         self.subinfo.options.package.packageName = 'dbus'
         self.subinfo.options.make.slnBaseName = 'dbus'
-        if self.hasTargetPlatform():
+        if self.isTargetBuild():
             self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=OFF"
         else:
-            self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON -DDBUS_DISABLE_EXECUTABLE_DEBUG_POSTFIX=ON"
+            self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON"
         
     def unpack(self):
         if not CMakePackageBase.unpack(self):
