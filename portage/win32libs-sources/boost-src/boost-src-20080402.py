116ddd7b39f79047b16a2ff0c7d026c5a01b87cb
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-04-20 14:31:49 +0000

    - changed boost-src to use cmake for building
    svn path=/branches/kdesupport/emerge/wince-cross-compile/; revision=1116904

--- portage/win32libs-sources/boost-src/boost-src-20080402.py
+++ portage/win32libs-sources/boost-src/boost-src-20080402.py
@@ -14,13 +14,15 @@ class subinfo(info.infoclass):
         self.targets['1.34.1'] = 'http://downloads.sourceforge.net/boost/boost_1_34_1.tar.bz2'
         self.targets['1.35.0'] = 'http://downloads.sourceforge.net/boost/boost_1_35_0.tar.bz2'
         self.targets['1.37.0'] = 'http://downloads.sourceforge.net/boost/boost_1_37_0.tar.bz2'
+        self.svnTargets['1.41.0'] = "git://gitorious.org/boost/cmake.git"
         self.targetInstSrc['1.34.1'] = 'boost_1_34_1'
         self.targetInstSrc['1.35.0'] = 'boost_1_35_0'
         self.targetInstSrc['1.37.0'] = 'boost_1_37_0'
-        self.defaultTarget = '1.37.0'
+        self.targetInstSrc['1.41.0'] = 'boost_1_41_0'
+        self.defaultTarget = '1.41.0'
     
-    def setDependencies( self ):
-        self.hardDependencies['dev-util/bjam'] = 'default'
+#    def setDependencies( self ):
+#        self.hardDependencies['dev-util/bjam'] = 'default'
         
 from Package.CMakePackageBase import *
 
@@ -29,45 +31,54 @@ class Package(CMakePackageBase):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
 
-    def libsToBuild( self ):
-        libs = " --with-python --with-program_options "
-        return libs
         
-    def configure( self, unused1=None, unused2=""):
-        return True
         
-    def make(self, unused=''):
-        self.enterSourceDir()
+#    def libsToBuild( self ):
+#        libs = " --with-python --with-program_options "
+#        return libs
+#        
+#    def configure( self, unused1=None, unused2=""):
+#        return True
+#        
+#    def make(self, unused=''):
+#        self.enterSourceDir()
+#        
+#        toolset = ""
+#        if self.compiler == "mingw" or self.compiler == "mingw4":
+#            toolset = "gcc"
+#        else:
+#            toolset = "msvc"
+#            
+#        cmd = "bjam --toolset=%s --prefix=%s --build-type=complete install " % \
+#                (toolset,
+#                self.imageDir())
+#                
+#        cmd += self.libsToBuild()
+#        
+#        if self.hasTargetPlatform():
+#            self.setupCrossToolchain()
+            # This is needed to find some wcecompat files (e.g. errno.h) included by some openssl headers
+            # but we make sure to add it at the very end so it doesn't disrupt the rest of the Qt build
+#            os.putenv( "INCLUDE", os.getenv("INCLUDE") + ";" + os.path.join( self.rootdir, "include", "wcecompat" ) )
         
-        toolset = ""
-        if self.compiler == "mingw" or self.compiler == "mingw4":
-            toolset = "gcc"
-        else:
-            toolset = "msvc"
-            
-        cmd = "bjam --toolset=%s --prefix=%s --build-type=complete install " % \
-                (toolset,
-                self.imageDir())
-                
-        cmd += self.libsToBuild()
-        print "command: ", cmd
-        utils.system( cmd )
-        return True
+#        print "command: ", cmd
+#        utils.system( cmd )
+#        return True
         
-    def cleanImage( self ):
-        return True
+#    def cleanImage( self ):
+#        return True
 
-    def install( self ):
+#    def install( self ):
 
         # copy runtime libraries to the bin folder
-        self.enterImageDir()
-        cmd = "mkdir bin && move lib\\*.dll bin"
-        print "command: ", cmd
-        utils.system( cmd )
-        return True
+#        self.enterImageDir()
+#        cmd = "mkdir bin && move lib\\*.dll bin"
+#        print "command: ", cmd
+#        utils.system( cmd )
+#        return True
 
-    def make_package( self ):
-        return self.doPackaging( "boost", self.buildTarget, True )
+#    def make_package( self ):
+#        return self.doPackaging( "boost", self.buildTarget, True )
 
 if __name__ == '__main__':
     Package().execute()
