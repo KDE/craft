d803ae26f4ee966a1cc91c20b729a0227a2f30c5
Patrick von Reth <patrick.vonreth@gmail.com>
2010-04-21 18:04:13 +0000

    reverted rename of pexports-src, because it is a src pakage
    svn path=/trunk/kdesupport/emerge/; revision=1117277

--- portage/dev-util/bjam/bjam-3.1.16-1.py
+++ portage/dev-util/bjam/bjam-3.1.16-1.py
@@ -6,8 +6,8 @@ import shutil
 import utils
 
 PACKAGE_NAME         = "boost-jam"
-PACKAGE_VER          = "3.1.16"
-PACKAGE_FULL_VER     = "3.1.16-1"
+PACKAGE_VER          = "3.1.18"
+PACKAGE_FULL_VER     = "3.1.18-1"
 PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_FULL_VER)
 
 SRC_URI= """
@@ -16,9 +16,9 @@ http://downloads.sourceforge.net/boost/""" + PACKAGE_FULL_NAME + """-ntx86.zip
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['3.1.16-1'] = SRC_URI
-        self.targetInstSrc['3.1.16-1'] = PACKAGE_FULL_NAME + "-ntx86"
-        self.defaultTarget = '3.1.16-1'
+        self.targets[PACKAGE_FULL_VER ] = SRC_URI
+        self.targetInstSrc[PACKAGE_FULL_VER ] = PACKAGE_FULL_NAME + "-ntx86"
+        self.defaultTarget = PACKAGE_FULL_VER 
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
@@ -39,7 +39,7 @@ class Package(BinaryPackageBase):
     def unpack(self):
         if not BinaryPackageBase.unpack(self):
             return False
-        return utils.renameDir(os.path.join(self.imageDir(),'boost-jam-3.1.16-1-ntx86'),os.path.join(self.imageDir(),'bin')) 
+        return utils.renameDir(os.path.join(self.imageDir(),PACKAGE_FULL_NAME + "-ntx86"),os.path.join(self.imageDir(),'bin')) 
         return True
 
 if __name__ == '__main__':
