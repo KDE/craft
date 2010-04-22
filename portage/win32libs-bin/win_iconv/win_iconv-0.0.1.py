85292fcde5c7745db2705175c973f5c6ab0abb3f
Patrick Spendrin <ps_ml@gmx.de>
2010-04-22 00:06:51 +0000

    add the version even though it isn't build yet
    svn path=/trunk/kdesupport/emerge/; revision=1117370

---
+++ portage/win32libs-bin/win_iconv/win_iconv-0.0.1.py
@@ -0,0 +1,23 @@
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        repoUrl = """http://downloads.sourceforge.net/kde-windows"""
+        
+        for version in ['0.0.1']:
+            self.targets[ version ] = self.getUnifiedPackage( repoUrl, "win_iconv", version )
+
+        self.defaultTarget = '0.0.1'
+
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
