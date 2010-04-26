c33228c6390ca50aa5f384543c88f4a9697a4981
Romain Pokrzywka <romain@kdab.net>
2010-04-26 10:28:51 +0000

    switched to our clone of wcecompat on github, which includes the previous patches and which has a CMake build script, thus simplifying the package code and solving the source copy issues.
    svn path=/trunk/kdesupport/emerge/; revision=1118978

--- portage/win32libs-sources/wcecompat-src/wcecompat-src-20100122.py
+++ portage/win32libs-sources/wcecompat-src/wcecompat-src-20100122.py
@@ -5,8 +5,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = 'git://github.com/mauricek/wcecompat.git'
-        self.patchToApply['gitHEAD'] = ('wcecompat-wince5.patch', 0)
+        self.svnTargets['gitHEAD'] = 'git://github.com/kromain/wcecompat.git'
         self.defaultTarget = 'gitHEAD'
     
     def setDependencies( self ):
@@ -23,46 +22,7 @@ class TestPackage(CMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DWCECOMPAT_INSTALL_INC_DIR=include/wcecompat"
 
-    def unpack(self):
-        # FIXME : find a way to do something like svn export or make out-of-source builds work
-        # Currently the emerge fails to clean the build dir due to a .git file which can't be deleted
-        # (filename too long probably), which means only the first build succeeds
-        CMakePackageBase.unpack( self )
-        utils.copySrcDirToDestDir(self.sourceDir(), self.buildDir())
-        return True
-        
-    def configure(self):
-        self.enterBuildDir()
-
-        os.environ["TARGETCPU"] = self.buildArchitecture()
-        if self.buildPlatform() == "WM50":
-            os.environ["OSVERSION"] = "WCE501"
-        elif self.buildPlatform() == "WM60" or self.buildPlatform() == "WM65":
-            os.environ["OSVERSION"] = "WCE502"
-
-        command = "perl config.pl"
-        return self.system( command )
-
-    def make(self):
-        self.enterBuildDir()
-        self.setupTargetToolchain()
-        return self.system( self.makeProgramm ) 
-
-    def install(self):
-        src = self.buildDir()
-        dst = self.imageDir()
-
-        if not os.path.isdir( dst ):
-            os.mkdir( dst )
-            os.mkdir( os.path.join( dst, "lib" ) )
-            os.mkdir( os.path.join( dst, "include" ) )
-
-        utils.copySrcDirToDestDir( os.path.join( src, "include" ) , os.path.join( os.path.join( dst, "include" ), "wcecompat" ) )
-        shutil.copy( os.path.join( src, "lib", "wcecompat.lib" ) , os.path.join( dst, "lib" ) )
-        shutil.copy( os.path.join( src, "lib", "wcecompatex.lib" ) , os.path.join( dst, "lib" ) )
-        
-        return True
-    
 if __name__ == '__main__':
     TestPackage().execute()
