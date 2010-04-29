5c1d8bed2de9bbe1521cc0828c150549e121c4f2
Patrick von Reth <patrick.vonreth@gmail.com>
2010-04-29 15:32:48 +0000

    added x64 target
    svn path=/trunk/kdesupport/emerge/; revision=1120656

--- portage/dev-util/perl/perl-5.10.1.1007.py
+++ portage/dev-util/perl/perl-5.10.1.1007.py
@@ -1,12 +1,17 @@
 import info
+import platform
 
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['5.8.8'] = "http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8.8.822-MSWin32-x86-280952.zip"
         self.targetMergeSourcePath['5.8.8'] = "ActivePerl-5.8.8.822-MSWin32-x86-280952\\perl"
-        self.targets['5.10.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.10.1.1007/ActivePerl-5.10.1.1007-MSWin32-x86-291969.zip"
-        self.targetMergeSourcePath['5.10.1'] = "ActivePerl-5.10.1.1007-MSWin32-x86-291969\\perl"
-        self.targetDigests['5.10.1'] = '9122a828b32d8b8499c73b61972eaec303698961'
+        if platform.buildArchitecture() == 'x64':
+           self.targets['5.10.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.10.1.1007/ActivePerl-5.10.1.1007-MSWin32-x64-291969.zip"
+           self.targetDigests['5.10.1'] = 'b5e9ab83d14e1c3311e280a96d355d491d4d55f5'
+        else:
+           self.targets['5.10.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.10.1.1007/ActivePerl-5.10.1.1007-MSWin32-x86-291969.zip"
+           self.targetDigests['5.10.1'] = '9122a828b32d8b8499c73b61972eaec303698961'
+        self.targetMergeSourcePath['5.10.1'] = "ActivePerl-5.10.1.1007-MSWin32-x86-291969\\perl"       
         self.defaultTarget = '5.10.1'
 
     def setBuildOptions( self ):
