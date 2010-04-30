eec3d558d8d62461599617cfa7050b425d151296
Patrick Spendrin <ps_ml@gmx.de>
2010-04-30 01:04:09 +0000

    rely on new iconv package instead
    svn path=/trunk/kdesupport/emerge/; revision=1120799

--- portage/kdesupport/strigi/strigi-0.7.1.20100107.py
+++ portage/kdesupport/strigi/strigi-0.7.1.20100107.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
         self.hardDependencies['libs/qt'] = 'default'
         self.hardDependencies['kdesupport/clucene-core'] = 'default'
         self.hardDependencies['win32libs-bin/exiv2'] = 'default'
-        self.hardDependencies['win32libs-bin/iconv'] = 'default'
+        self.hardDependencies['win32libs-bin/win_iconv'] = 'default'
         self.hardDependencies['win32libs-bin/libbzip2'] = 'default'
         self.hardDependencies['win32libs-bin/libxml2'] = 'default'
         self.hardDependencies['win32libs-bin/zlib'] = 'default'
