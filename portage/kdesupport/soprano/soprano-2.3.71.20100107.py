f1b01629e8110d4b4a7fcbde7d3b9d9a7e31cea8
Romain Pokrzywka <romain@kdab.net>
2010-04-30 13:37:17 +0000

    restore clucene-core dependency, disable redland for wince
    svn path=/trunk/kdesupport/emerge/; revision=1121134

--- portage/kdesupport/soprano/soprano-2.3.71.20100107.py
+++ portage/kdesupport/soprano/soprano-2.3.71.20100107.py
@@ -1,10 +1,13 @@
 import info
+import platform
 
 class subinfo(info.infoclass):
     def setDependencies( self ):
         self.hardDependencies['virtual/base']            = 'default'
         self.hardDependencies['libs/qt']                 = 'default'
-        self.hardDependencies['win32libs-bin/redland']   = 'default'
+        self.hardDependencies['kdesupport/clucene-core'] = 'default'
+        if not platform.isCrossCompilingEnabled():
+            self.hardDependencies['win32libs-bin/redland']   = 'default'
 
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/soprano'
