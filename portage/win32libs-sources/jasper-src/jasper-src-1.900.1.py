0be07bcd781177234c95246133a6eb7cf0e5d03b
Patrick Spendrin <ps_ml@gmx.de>
2010-04-30 14:18:54 +0000

    move jasper to new build system
    svn path=/trunk/kdesupport/emerge/; revision=1121139

--- portage/win32libs-sources/jasper-src/jasper-src-1.900.1.py
+++ portage/win32libs-sources/jasper-src/jasper-src-1.900.1.py
@@ -1,50 +1,23 @@
-import base
 import os
-import shutil
 import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets[ '1.900.1-2' ] = 'http://www.ece.uvic.ca/~mdadams/jasper/software/jasper-1.900.1.zip'
         self.targetInstSrc[ '1.900.1-2' ] = os.path.join( 'jasper-1.900.1', 'src', 'libjasper' )
+        self.patchToApply[ '1.900.1-2' ] = ( "jasper-1.900.1-20100430.diff", 3 )
         self.defaultTarget = '1.900.1-2'
 
     def setDependencies( self ):
-        self.hardDependencies['win32libs-bin/jpeg'] = 'default'
+        self.hardDependencies['testing/jpeg7-src'] = 'default'
 
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, args=args )
-    self.createCombinedPackage = True
-    self.subinfo = subinfo()
+from Package.CMakePackageBase import *
 
-  def unpack( self ):
-    if( not base.baseclass.unpack( self ) ):
-      return False
-    # we have an own cmake script - copy it to the right place
-    cmake_script = os.path.join( self.packagedir , "CMakeLists.txt" )
-    cmake_dest = os.path.join( self.workdir, self.instsrcdir, "CMakeLists.txt" )
-    shutil.copy( cmake_script, cmake_dest )
-
-    return True
-
-  def compile( self ):
-    return self.kdeCompile()
-
-  def install( self ):
-    return self.kdeInstall()
-
-  def make_package( self ):
-    # auto-create both import libs with the help of pexports
-    self.stripLibs( "libjasper" )
-
-    # auto-create both import libs with the help of pexports
-    self.createImportLibs( "libjasper" )
-
-    # now do packaging with kdewin-packager
-    self.doPackaging( "jasper", self.buildTarget, True )
-
-    return True
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.createCombinedPackage = True
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
