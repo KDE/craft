bdd40ebc6ab387d6fc36b9cb3de09e1ff6d78bb7
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-05-04 07:22:26 +0000

    - fixed sqlite for wince
    svn path=/trunk/kdesupport/emerge/; revision=1122566

--- portage/win32libs-sources/sqlite-src/sqlite-src-3.6.23.py
+++ portage/win32libs-sources/sqlite-src/sqlite-src-3.6.23.py
@@ -9,13 +9,28 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['3.6.23.1'] = 'http://www.sqlite.org/sqlite-amalgamation-3.6.23.1.tar.gz'
         self.targetInstSrc['3.6.23.1'] = "sqlite-3.6.23.1"
-        self.patchToApply['3.6.23.1'] = ( "sqlite-3.6.23.1-20100413.diff", 1 )
+        self.patchToApply['3.6.23.1'] = ( "sqlite-3.6.23.1-20100504.diff", 1 )
         self.defaultTarget = '3.6.23.1'
+        
+        
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        if platform.isCrossCompilingEnabled():
+            self.hardDependencies['win32libs-sources/wcecompat-src'] = 'default'
 
 class Package(CMakePackageBase):
   def __init__( self ):
     self.subinfo = subinfo()
     CMakePackageBase.__init__(self)
+    
+  def make(self, unused=''):
+    if self.isTargetBuild():
+        # Set the include path for the wcecompat files (e.g. errno.h). Setting it through
+        # the Configure script generates errors due to the the backslashes in the path
+        wcecompatincdir = os.path.join( os.path.join( self.mergeDestinationDir(), "include" ), "wcecompat" )
+        os.environ["TARGET_INCLUDE"] = wcecompatincdir + ";" + os.getenv("TARGET_INCLUDE")
+        
+    CMakePackageBase.make( self )
 
 if __name__ == '__main__':
     Package().execute()
