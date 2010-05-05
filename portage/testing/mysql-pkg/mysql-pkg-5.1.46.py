b2af57e6b74ad4e2b5eae5f58253cec8a302acd0
Patrick von Reth <patrick.vonreth@gmail.com>
2010-05-05 08:58:46 +0000

    added mysql-embeded so only one package is needed
    svn path=/trunk/kdesupport/emerge/; revision=1123082

--- portage/testing/mysql-pkg/mysql-pkg-5.1.46.py
+++ portage/testing/mysql-pkg/mysql-pkg-5.1.46.py
@@ -43,12 +43,17 @@ class Package(CMakePackageBase):
     if( not self.cleanImage()):
       return False
     shutil.copytree( os.path.join( self.sourceDir() , "bin" ) , os.path.join( self.installDir(), "bin") , ignore=shutil.ignore_patterns('*.pdb','*.map') )
+    shutil.copy( os.path.join( self.sourceDir() , "Embedded" , "DLL" , "release" , "libmysqld.dll" ) , os.path.join( self.installDir(), "bin" , "libmysqld.dll" ) )
     shutil.copytree( os.path.join( self.sourceDir() , "include" ) , os.path.join( self.installDir(), "include") )
     shutil.copytree( os.path.join( self.sourceDir() , "scripts" ) , os.path.join( self.installDir(), "scripts") )
     shutil.copytree( os.path.join( self.sourceDir() , "share" ) , os.path.join( self.installDir(), "share") )
+    os.makedirs(os.path.join( self.sourceDir() , "share" , "licence" ) )
+    shutil.copy( os.path.join( self.sourceDir() , "COPYING" ) , os.path.join( self.sourceDir() , "share" , "licence" ,  "COPYING"  ) )
+    shutil.copy( os.path.join( self.sourceDir() , "EXCEPTIONS-CLIENT" ) , os.path.join( self.sourceDir() , "share" , "licence" ,  "EXCEPTIONS-CLIENT"  ) )
     shutil.copytree( os.path.join( self.sourceDir() , "data" ) , os.path.join( self.installDir(), "data") )
     shutil.copytree( os.path.join( self.sourceDir() , "Docs" ) , os.path.join( self.installDir(), "Docs") )
     self.createImportLibs( "libmysql")
+    self.createImportLibs( "libmysqld")
     return True 
     
 if __name__ == '__main__':
