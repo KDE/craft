4911638f63bcd94b521ea76e22ea4597d8b5aff0
Patrick Spendrin <ps_ml@gmx.de>
2010-05-11 11:37:55 +0000

    add a 2.8.0 version for windows ce builds. Hopefully this helps
    svn path=/trunk/kdesupport/emerge/; revision=1125437

--- portage/dev-util/cmake-pkg/cmake-pkg-20100421.py
+++ portage/dev-util/cmake-pkg/cmake-pkg-20100421.py
@@ -8,6 +8,9 @@ class subinfo(info.infoclass):
         self.targets['2.8.1'] = "http://www.cmake.org/files/v2.8/cmake-2.8.1.tar.gz"
         self.targetInstSrc['2.8.1'] = "cmake-2.8.1"
         self.patchToApply['2.8.1'] = ("cmake-2.8.1-20100511.diff", 1)
+        self.targets['2.8.0'] = "http://www.cmake.org/files/v2.8/cmake-2.8.0.tar.gz"
+        self.targetInstSrc['2.8.0'] = "cmake-2.8.0"
+        self.patchToApply['2.8.0'] = ("cmake-2.8.0-20100512.diff", 1)
         self.defaultTarget = 'release'
 
     def setDependencies( self ):
