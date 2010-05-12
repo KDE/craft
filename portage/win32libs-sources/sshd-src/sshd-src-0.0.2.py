b6fcbb5b896c2de7f2326706e53e176dfbb6bd61
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-05-12 10:48:14 +0000

    - added sshd for wince -caution the adduser is not compiled, because a dependency of the platform builder is needed. so just take it from the bin package
    svn path=/trunk/kdesupport/emerge/; revision=1125793

---
+++ portage/win32libs-sources/sshd-src/sshd-src-0.0.2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import info
+import utils
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['0.0.2'] = "http://officespace.kdab.com/~andy/CESSH-14454.zip"
+        self.targetInstSrc['0.0.2'] = "SSH"
+        self.patchToApply['0.0.2'] = ( 'SSH-20100512.diff', 1 )
+        self.defaultTarget = '0.0.2'
+
+    def setDependencies( self ):
+        self.hardDependencies['dev-util/cmake'] = 'default'
+        self.hardDependencies['win32libs-sources/openssl-src'] = 'default'
+        self.hardDependencies['win32libs-sources/zlib-src'] = 'default'
+        
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        self.subinfo.options.package.withCompiler = None
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
