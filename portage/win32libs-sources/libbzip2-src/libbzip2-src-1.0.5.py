39a7e1ac7b7bdc45eedc60853f068146c72ae4cd
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-05-17 13:07:09 +0000

    - made libbzip2 wince compatible - no bin build for wince
    svn path=/trunk/kdesupport/emerge/; revision=1127730

--- portage/win32libs-sources/libbzip2-src/libbzip2-src-1.0.5.py
+++ portage/win32libs-sources/libbzip2-src/libbzip2-src-1.0.5.py
@@ -2,11 +2,17 @@ import utils
 import info
 from Package.CMakePackageBase import *
 
+##########################################################################
+############################# ATTENTION ##################################
+##########################################################################
+######### There is no binary build for Windows Ce, just the lib! #########
+##########################################################################
+
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['1.0.5-1'] = 'http://www.bzip.org/1.0.5/bzip2-1.0.5.tar.gz'
         self.targetInstSrc['1.0.5-1'] = "bzip2-1.0.5"
-        self.patchToApply['1.0.5-1'] = ("bzip.diff", 0)
+        self.patchToApply['1.0.5-1'] = ("bzip.diff", 1)
         self.defaultTarget = '1.0.5-1'
 
 class Package(CMakePackageBase):
@@ -18,14 +24,6 @@ class Package(CMakePackageBase):
 
   def buildType( self ):
     return "Release"
-
-  def unpack( self ):
-    if( not CMakePackageBase.unpack( self ) ):
-      return False
-    # copy CMakeLists.txt
-    utils.copyFile( os.path.join( self.packageDir(), "CMakeLists.txt" ),
-                    os.path.join( self.sourceDir(),  "CMakeLists.txt" ) )
-    return True
 
   def createPackage( self ):
     # auto-create both import libs with the help of pexports
