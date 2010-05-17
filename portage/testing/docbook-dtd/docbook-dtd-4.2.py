fca2344ccc2dd4b629bee952acb40293f6016364
Patrick Spendrin <ps_ml@gmx.de>
2010-05-17 08:17:56 +0000

    remove tabs
    svn path=/trunk/kdesupport/emerge/; revision=1127638

--- portage/testing/docbook-dtd/docbook-dtd-4.2.py
+++ portage/testing/docbook-dtd/docbook-dtd-4.2.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.targets['4.2'] = 'http://www.docbook.org/xml/4.2/docbook-xml-4.2.zip'
-		self.targetDigests['4.2'] = '5e3a35663cd028c5c5fbb959c3858fec2d7f8b9e'		
+        self.targetDigests['4.2'] = '5e3a35663cd028c5c5fbb959c3858fec2d7f8b9e'		
         self.defaultTarget = '4.2'
 
 from Package.BinaryPackageBase import *
