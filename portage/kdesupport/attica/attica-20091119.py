abb4d977c7e89deaefa732d593e38ae25fecc986
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-05-17 15:54:53 +0000

    - made attica wince compatible
    svn path=/trunk/kdesupport/emerge/; revision=1127772

--- portage/kdesupport/attica/attica-20091119.py
+++ portage/kdesupport/attica/attica-20091119.py
@@ -17,6 +17,15 @@ class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
+        
+        self.subinfo.options.configure.defines = ""
+        qmake = os.path.join(self.mergeDestinationDir(), "bin", "qmake.exe")
+        if not os.path.exists(qmake):
+            print("<%s>") % qmake
+            utils.die("could not found qmake")
+        ## \todo a standardized way to check if a package is installed in the image dir would be good.
+        self.subinfo.options.configure.defines += "-DQT_QMAKE_EXECUTABLE:FILEPATH=%s " \
+            % qmake.replace('\\', '/')
 
 if __name__ == '__main__':
     Package().execute()
