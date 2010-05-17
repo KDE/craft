26380c038f0cd777e9a4863aebfe0a4d9dfdc217
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-05-17 15:49:23 +0000

    - made phonon wince compatible - disabled host build, because of the directx sdk requirement
    svn path=/trunk/kdesupport/emerge/; revision=1127768

--- portage/kdesupport/phonon/phonon-4.3.1.20090227.py
+++ portage/kdesupport/phonon/phonon-4.3.1.20090227.py
@@ -12,7 +12,11 @@ class subinfo(info.infoclass):
         self.defaultTarget = 'gitHEAD'
         self.options.configure.defines = "-DPHONON_BUILD_EXAMPLES=OFF -DPHONON_BUILD_TESTS=OFF"
         if COMPILER == "mingw4":
-          self.options.configure.defines +=" -DBUILD_PHONON_DS9=OFF"
+            self.options.configure.defines +=" -DBUILD_PHONON_DS9=OFF"
+          
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
 
 from Package.CMakePackageBase import *
 
@@ -20,6 +24,23 @@ class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
+        
+        self.subinfo.options.configure.defines = ""
+        qmake = os.path.join(self.mergeDestinationDir(), "bin", "qmake.exe")
+        if not os.path.exists(qmake):
+            print("<%s>") % qmake
+            utils.die("could not found qmake")
+        ## \todo a standardized way to check if a package is installed in the image dir would be good.
+        self.subinfo.options.configure.defines += "-DQT_QMAKE_EXECUTABLE:FILEPATH=%s " \
+            % qmake.replace('\\', '/')
+            
+        automoc = os.path.join(os.getenv( "KDEROOT" ), "lib", "automoc4", "Automoc4Config.cmake")
+        if not os.path.exists(automoc):
+            print("<%s>") % automoc
+            utils.die("could not found automoc")
+        ## \todo a standardized way to check if a package is installed in the image dir would be good.
+        self.subinfo.options.configure.defines += "-DAUTOMOC4_CONFIG_FILE:FILEPATH=%s " \
+            % automoc.replace('\\', '/')
 
 if __name__ == '__main__':
     Package().execute()
