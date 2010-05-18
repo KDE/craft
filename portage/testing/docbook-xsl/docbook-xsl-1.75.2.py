b7e3ff063a7e161a1a8e56353d327f67c254ed3b
Patrick Spendrin <ps_ml@gmx.de>
2010-05-18 02:00:28 +0000

    add docbook-xsl which is also required for kdelibs
    svn path=/trunk/kdesupport/emerge/; revision=1127914

---
+++ portage/testing/docbook-xsl/docbook-xsl-1.75.2.py
@@ -0,0 +1,30 @@
+import info
+import shutil
+import os
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+
+    def setTargets( self ):
+        self.targets['1.75.2'] = 'http://downloads.sourceforge.net/docbook/docbook-xsl-1.75.2.tar.bz2'
+        self.defaultTarget = '1.75.2'
+
+from Package.BinaryPackageBase import *
+
+class Package(BinaryPackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        BinaryPackageBase.__init__( self )
+        self.subinfo.options.install.installPath = 'share/xml/docbook'
+        
+    def unpack( self ):
+        """rename the directory here"""
+        if not BinaryPackageBase.unpack(self):
+            return False
+        os.rename(os.path.join(self.installDir(), os.path.basename(self.repositoryUrl()).replace(".tar.bz2", "")), 
+                  os.path.join(self.installDir(), "xsl-stylesheets-" + self.subinfo.buildTarget))
+        return True
+
+if __name__ == '__main__':
+    Package().execute()
