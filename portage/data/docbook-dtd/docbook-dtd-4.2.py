7288551aafae1e249137777e9513e99db9199d95
Ralf Habacker <ralf.habacker@freenet.de>
2010-05-18 06:44:19 +0000

    kdelibs is bound to version 4.2, 4.5 will not be detected
    svn path=/trunk/kdesupport/emerge/; revision=1127938

--- portage/data/docbook-dtd/docbook-dtd-4.2.py
+++ portage/data/docbook-dtd/docbook-dtd-4.2.py
@@ -5,8 +5,10 @@ class subinfo(info.infoclass):
         self.hardDependencies['virtual/base'] = 'default'
 
     def setTargets( self ):
+        self.targets['4.2'] = 'http://www.docbook.org/xml/4.2/docbook-xml-4.2.zip'
+        self.targetDigests['4.2'] = '5e3a35663cd028c5c5fbb959c3858fec2d7f8b9e'
         self.targets['4.5'] = 'http://www.docbook.org/xml/4.5/docbook-xml-4.5.zip'
-        self.defaultTarget = '4.5'
+        self.defaultTarget = '4.2'
 
 from Package.BinaryPackageBase import *
 
@@ -14,7 +16,7 @@ class Package(BinaryPackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         BinaryPackageBase.__init__( self )
-        self.subinfo.options.install.installPath = 'share/xml/docbook/schema/dtd/4.5'
+        self.subinfo.options.install.installPath = 'share/xml/docbook/schema/dtd/%s' % self.subinfo.buildTarget
 
 if __name__ == '__main__':
     Package().execute()
