e1a307a2f3818544dd3b07631774d04fa3410ddd
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-05-18 08:23:51 +0000

    - made pcre wince compatible
    svn path=/trunk/kdesupport/emerge/; revision=1128048

--- portage/win32libs-sources/pcre-src/pcre-src-8.02.py
+++ portage/win32libs-sources/pcre-src/pcre-src-8.02.py
@@ -6,6 +6,7 @@ class subinfo(info.infoclass):
         for ver in ['7.9', '8.00', '8.02']:
           self.targets[ver] = 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-' + ver + '.tar.bz2'
           self.targetInstSrc[ver] = 'pcre-' + ver
+        self.patchToApply['8.02'] = ("pcre-8.02-20100518.diff", 1)
         self.defaultTarget = '8.02'
     def setDependencies( self ):
         self.hardDependencies['win32libs-sources/libbzip2-src'] = 'default'
@@ -20,6 +21,9 @@ class Package(CMakePackageBase):
         defines += "-DPCRE_SUPPORT_UNICODE_PROPERTIES=ON "
         defines += "-DPCRE_SUPPORT_UTF8=ON "
         defines += "-DPCRE_EBCDIC=OFF "
+        if self.isTargetBuild():
+            defines += "-DPCRE_BUILD_TESTS=OFF "
+            defines += "-DPCRE_BUILD_PCREGREP=OFF "
         self.subinfo.options.configure.defines = defines
         self.subinfo.options.package.packageName = 'pcre'
 
