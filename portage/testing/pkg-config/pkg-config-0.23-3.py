3d5ffe02a5a24ac4ddbec9d54b423902fb888cbe
Ralf Habacker <ralf.habacker@freenet.de>
2010-05-18 07:21:06 +0000

    updated, the old version is not available any more
    svn path=/trunk/kdesupport/emerge/; revision=1127946

---
+++ portage/testing/pkg-config/pkg-config-0.23-3.py
@@ -0,0 +1,25 @@
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):      
+        arch='32'
+        if( os.getenv('EMERGE_ARCHITECTURE')=="x64"):
+           arch='64'
+        for version in ['0.23-3']: 
+            self.targets[ version ] = "http://ftp.gnome.org/pub/gnome/binaries/win"+ arch+"/dependencies/pkg-config_"+version+"_win"+arch+".zip"
+
+        self.defaultTarget = '0.23-3'
+
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
