7521905723f24694763083c8d84e22805e5e0ad9
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-05-21 07:05:07 +0000

    - startet gettext package, not yet done
    svn path=/trunk/kdesupport/emerge/; revision=1129055

---
+++ portage/testing/gettext-src/gettext-src-0.18.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import info
+import utils
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+       self.targets[ '0.18' ] = 'http://ftp.gnu.org/pub/gnu/gettext/gettext-0.18.tar.gz'
+       self.targetInstSrc[ '0.18' ] = "gettext-0.18"
+       self.patchToApply['0.18'] = ("gettext-0.18-20100521.diff", 1)
+       self.defaultTarget = '0.18'
+
+    def setDependencies( self ):
+        self.hardDependencies['dev-util/cmake'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
