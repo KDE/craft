6bb3da797d1f54c104bb488b92350f8b3e060cd1
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-05-27 09:43:23 +0000

    - started with kdewin wince port
    svn path=/trunk/kdesupport/emerge/; revision=1131091

--- portage/kdesupport/kdewin/kdewin-0.5.2.py
+++ portage/kdesupport/kdewin/kdewin-0.5.2.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/kdewin'
         for i in ['4.3.0', '4.3.1', '4.3.2', '4.3.3', '4.3.4', '4.3']:
             self.svnTargets[ i ] = 'tags/kdesupport-for-4.3/kdesupport/kdewin'
+        self.patchToApply['svnHEAD'] = ("kdewin-20100527.patch", 1)
         self.defaultTarget = 'svnHEAD'
 
 from Package.CMakePackageBase import *
@@ -24,6 +25,15 @@ class Package(CMakePackageBase):
         self.subinfo.options.package.version = '0.5.2'
         self.subinfo.options.configure.defines = '-DBUILD_BASE_LIB_WITH_QT=On -DBUILD_QT_LIB=On -DBUILD_TOOLS=On'
         CMakePackageBase.__init__( self )
+        
+        self.subinfo.options.configure.defines = ""
+        qmake = os.path.join(self.mergeDestinationDir(), "bin", "qmake.exe")
+        if not os.path.exists(qmake):
+            print("<%s>") % qmake
+            utils.die("could not found qmake")
+        ## \todo a standardized way to check if a package is installed in the image dir would be good.
+        self.subinfo.options.configure.defines += "-DQT_QMAKE_EXECUTABLE:FILEPATH=%s " \
+            % qmake.replace('\\', '/')
 
 if __name__ == '__main__':
     Package().execute()
