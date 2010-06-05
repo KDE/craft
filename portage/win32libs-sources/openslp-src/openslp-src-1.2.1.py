e6f63427ad5782c0b6fce25491055caa57e3314c
Patrick Spendrin <ps_ml@gmx.de>
2010-06-05 08:26:01 +0000

    make virtual/base the main dependency
    svn path=/trunk/kdesupport/emerge/; revision=1134769

--- portage/win32libs-sources/openslp-src/openslp-src-1.2.1.py
+++ portage/win32libs-sources/openslp-src/openslp-src-1.2.1.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 import base
 import os
 import shutil
 import re
 import utils
+import info
 from utils import die
 
 PACKAGE_NAME         = "openslp"
@@ -11,71 +13,58 @@ PACKAGE_FULL_VER     = "1.2.1-2"
 PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_VER)
 PACKAGE_DLL_NAME     = "libslp"
 
-SRC_URI= """
-http://mesh.dl.sourceforge.net/sourceforge/openslp/""" + PACKAGE_FULL_NAME + """.tar.gz
-"""
-
-DEPEND = """
-"""
-
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, SRC_URI, args=args )
-    self.instsrcdir = PACKAGE_FULL_NAME
-    self.createCombinedPackage = True
-    self.buildType = "Release"
-
-  def execute( self ):
-    base.baseclass.execute( self )
-    if self.compiler <> "mingw":
-      print "error: can only be build with MinGW (but in the end a \
-             mingw/msvc combined package is created"
-      exit( 1 )
-
-  def unpack( self ):
-    if( not base.baseclass.unpack( self ) ):
-      return False
-    src = os.path.join( self.workdir, self.instsrcdir )
-
-    cmd = "cd %s && patch -p0 < %s" % \
-          ( src, os.path.join( self.packagedir , "openslp-1.2.1.diff" ) )
-    os.system( cmd ) or die
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['1.2.1'] = "http://mesh.dl.sourceforge.net/sourceforge/openslp/" + PACKAGE_FULL_NAME + ".tar.gz"
+        self.targetInstSrc['1.2.1'] = "openslp-1.2.1"
+        self.defaultTarget = '1.2.1'
     
-    # we have an own cmake script - copy it to the right place
-    cmake_script = os.path.join( self.packagedir , "CMakeLists.txt" )
-    cmake_dest = os.path.join( self.workdir, self.instsrcdir, "CMakeLists.txt" )
-    shutil.copy( cmake_script, cmake_dest )
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+    
+class subclass(base.baseclass):
+    def __init__( self, **args ):
+        base.baseclass.__init__( self, args=args )
+        self.createCombinedPackage = True
+        self.subinfo = subinfo()
 
-    return True
+    def execute( self ):
+        if self.compiler <> "mingw":
+            print "error: can only be build with MinGW (but in the end a \
+                   mingw/msvc combined package is created"
+            exit( 1 )
+        base.baseclass.execute( self )
 
-  def kdeDefaultDefines( self ):
-    # adjust some vars for proper compile
-    cmake_src  = os.path.join( self.workdir, self.instsrcdir )
+    def unpack( self ):
+        if( not base.baseclass.unpack( self ) ):
+            return False
+        src = os.path.join( self.workdir, self.instsrcdir )
 
-    options = "%s -DCMAKE_INSTALL_PREFIX=%s " % \
-              ( cmake_src, self.rootdir.replace( '\\', '/' ) )
-    options = options + "-DCMAKE_INCLUDE_PATH=%s " % \
-              os.path.join( self.rootdir, "win32libs", "include" ).replace( "\\", "/" )
+        cmd = "cd %s && patch -p0 < %s" % \
+                ( src, os.path.join( self.packagedir , "openslp-1.2.1.diff" ) )
+        os.system( cmd ) or die
 
-    options = options + "-DCMAKE_LIBRARY_PATH=%s " % \
-              os.path.join( self.rootdir, "win32libs", "lib" ).replace( "\\", "/" )
+        # we have an own cmake script - copy it to the right place
+        cmake_script = os.path.join( self.packagedir , "CMakeLists.txt" )
+        cmake_dest = os.path.join( self.workdir, self.instsrcdir, "CMakeLists.txt" )
+        shutil.copy( cmake_script, cmake_dest )
 
-    return options
+        return True
 
-  def compile( self ):
-    return self.kdeCompile()
+    def compile( self ):
+        return self.kdeCompile()
 
-  def install( self ):
-    return self.kdeInstall()
+    def install( self ):
+        return self.kdeInstall()
 
-  def make_package( self ):
-    # auto-create both import libs with the help of pexports
-    self.createImportLibs( PACKAGE_DLL_NAME )
+    def make_package( self ):
+        # auto-create both import libs with the help of pexports
+        self.createImportLibs( PACKAGE_DLL_NAME )
 
-    # now do packaging with kdewin-packager
-    self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
+        # now do packaging with kdewin-packager
+        self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
 
-    return True
+        return True
 
 if __name__ == '__main__':
     subclass().execute()
