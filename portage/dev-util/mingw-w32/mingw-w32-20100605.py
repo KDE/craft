6a087eb94eebfb761b3b59740d4c3bd69b8c16c3
Patrick von Reth <patrick.vonreth@gmail.com>
2010-06-05 09:08:07 +0000

    added untested mingw-w32 package
    svn path=/trunk/kdesupport/emerge/; revision=1134776

---
+++ portage/dev-util/mingw-w32/mingw-w32-20100605.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import utils
+import shutil
+import os
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['20100527'] = "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/sezero_20100527/mingw-w32-bin_i686-mingw_20100527_sezero.zip"
+        self.defaultTarget = '20100527'
+            
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+        self.hardDependencies['gnuwin32/patch'] = 'default'
+        
+from Package.BinaryPackageBase import *
+
+class Package(BinaryPackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        self.subinfo.options.merge.ignoreBuildType = True
+        BinaryPackageBase.__init__(self)
+
+    def install(self):
+        shutil.move( os.path.join( self.installDir() , "mingw32" ) , os.path.join( self.installDir(), "mingw" ) )
+        shutil.copy(os.path.join( self.installDir() , "mingw" , "bin" , "gmake.exe") , os.path.join( self.installDir() , "mingw64" , "bin" , "mingw32-make.exe") )
+        return True
+
+if __name__ == '__main__':
+    Package().execute()
