deb152655164e477199524797906fe4167843f32
Patrick Spendrin <ps_ml@gmx.de>
2010-06-07 14:50:15 +0000

    update dependency, fix download url
    svn path=/trunk/kdesupport/emerge/; revision=1135542

--- portage/testing/kmymoney/kmymoney-20091106.py
+++ portage/testing/kmymoney/kmymoney-20091106.py
@@ -3,10 +3,6 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/kdereview/kmymoney'
-        #don't use the sf link (http://sourceforge.net/projects/kmymoney2/files/KMyMoney-KDE4/3.98/kmymoney-3.98.1.tar.bz2/download)
-        #since you'll get an 'dont know how to unpack this file: download' error because of the way the filename is extracted from the URL
-        self.targets['3.98.1'] = 'http://ftp.heanet.ie/mirrors/sourceforge/k/project/km/kmymoney2/KMyMoney-KDE4/3.98/kmymoney-3.98.1.tar.bz2'
-        self.targetInstSrc['3.98.1'] = 'kmymoney-3.98.1'
         self.targets['3.98.1'] = 'http://downloads.sourceforge.net/kmymoney2/kmymoney-3.98.1.tar.bz2'
         self.targetInstSrc['3.98.1'] = 'kmymoney-3.98.1'
         self.defaultTarget = 'svnHEAD'
@@ -15,7 +11,7 @@ class subinfo(info.infoclass):
         self.hardDependencies['kde/kdelibs'] = 'default'
         self.hardDependencies['kde/kdepimlibs'] = 'default'
         self.hardDependencies['kde/kdebase-runtime'] = 'default'
-        self.hardDependencies['testing/mysql-server'] = 'default'
+        self.hardDependencies['testing/mysql-pkg'] = 'default'
         self.hardDependencies['win32libs-bin/sqlite'] = 'default'
 #        self.softDependencies['testing/libofx'] = 'default'
         self.hardDependencies['win32libs-bin/gettext'] = 'default'
