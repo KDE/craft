120b42293ced7f3da6a9b7530e9f590a1f99a896
Patrick von Reth <patrick.vonreth@gmail.com>
2010-06-07 22:11:09 +0000

    added msys support for mingw
    svn path=/trunk/kdesupport/emerge/; revision=1135672

--- portage/win32libs-sources/openssl-src/openssl-src-1.0.0.py
+++ portage/win32libs-sources/openssl-src/openssl-src-1.0.0.py
@@ -7,13 +7,16 @@ import platform
 
 class subinfo(info.infoclass):
     def setTargets( self ):   
-        for ver in [ '0.9.8k' , '0.9.8m' ,'1.0.0' ]:
+        for ver in [ '0.9.8k' , '0.9.8m' ,'1.0.0' , '1.0.0a']:
             self.targets[ver] = 'http://www.openssl.org/source/openssl-'+ver+'.tar.gz'
             self.targetInstSrc[ver] = 'openssl-'+ver
-            self.patchToApply[ver] = ('openssl-'+ver+'.diff', 1)
+            if not COMPILER.startswith( "mingw" ):
+              self.patchToApply[ver] = ('openssl-'+ver+'.diff', 1)
             self.targetDigestUrls[ver] = 'http://www.openssl.org/source/openssl-'+ver+'.tar.gz.sha1'      
-
-            self.defaultTarget = '1.0.0'
+            if not COMPILER.startswith( "mingw" ):
+              self.defaultTarget = '1.0.0'
+            else:
+              self.defaultTarget = '1.0.0a'
         
         self.options.package.withCompiler = False
 
@@ -22,6 +25,9 @@ class subinfo(info.infoclass):
             self.hardDependencies['dev-util/perl'] = 'default'
             if platform.isCrossCompilingEnabled():
                 self.hardDependencies['win32libs-sources/wcecompat-src'] = 'default'
+            if COMPILER.startswith( "mingw" ):
+                self.hardDependencies['dev-util/msys'] = 'default'
+                self.hardDependencies['win32libs-bin/zlib'] = 'default'
 
     def setBuildOptions( self ):
         self.disableHostBuild = True
@@ -30,7 +36,7 @@ class subinfo(info.infoclass):
 
 from Package.CMakePackageBase import *
 
-class Package(CMakePackageBase):
+class PackageCMake(CMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
@@ -38,43 +44,34 @@ class Package(CMakePackageBase):
     def compile( self ):       
         os.chdir( self.sourceDir() )
         cmd = ""
-        if self.compiler() == "mingw" or self.compiler() == "mingw4":
-            if self.buildTarget == "1.0.0":
-                if platform.buildArchitecture() == 'x64':
-                    cmd = "ms\mingw64.bat enable-md2 no-asm shared"
-                else:
-                    cmd = "ms\mingw32.bat enable-md2 no-asm shared"
-            else:
-                cmd = "ms\mingw32.bat"
+        if self.isTargetBuild():
+            """WinCE cross-building environment setup"""
+            config = "VC-CE"
+            os.environ["WCECOMPAT"] = self.mergeDestinationDir()
+            os.environ["TARGETCPU"] = self.buildArchitecture() 
+            os.environ["PLATFORM"] = self.buildPlatform()
+            if self.buildPlatform() == "WM50":
+                os.environ["OSVERSION"] = "WCE501"
+            elif self.buildPlatform() == "WM60" or self.buildPlatform() == "WM65":
+                os.environ["OSVERSION"] = "WCE502"
         else:
-            if self.isTargetBuild():
-                """WinCE cross-building environment setup"""
-                config = "VC-CE"
-                os.environ["WCECOMPAT"] = self.mergeDestinationDir()
-                os.environ["TARGETCPU"] = self.buildArchitecture() 
-                os.environ["PLATFORM"] = self.buildPlatform()
-                if self.buildPlatform() == "WM50":
-                    os.environ["OSVERSION"] = "WCE501"
-                elif self.buildPlatform() == "WM60" or self.buildPlatform() == "WM65":
-                    os.environ["OSVERSION"] = "WCE502"
-            else:
-                config = "VC-WIN32"
+            config = "VC-WIN32"
 
-            if not self.system( "perl Configure %s" % config, "configure" ):
-                return False
+        if not self.system( "perl Configure %s" % config, "configure" ):
+            return False
 
-            if not self.system( "ms\do_ms.bat", "configure" ):
-                return False
-                
-            if self.isTargetBuild():
-                self.setupTargetToolchain()
-                # Set the include path for the wcecompat files (e.g. errno.h). Setting it through
-                # the Configure script generates errors due to the the backslashes in the path
-                wcecompatincdir = os.path.join( os.path.join( self.mergeDestinationDir(), "include" ), "wcecompat" )
-                os.putenv( "INCLUDE", wcecompatincdir + ";" + os.getenv("INCLUDE") )
-                cmd = r"nmake -f ms\cedll.mak"
-            else:
-                cmd = r"nmake -f ms\ntdll.mak"
+        if not self.system( "ms\do_ms.bat", "configure" ):
+            return False
+            
+        if self.isTargetBuild():
+            self.setupTargetToolchain()
+            # Set the include path for the wcecompat files (e.g. errno.h). Setting it through
+            # the Configure script generates errors due to the the backslashes in the path
+            wcecompatincdir = os.path.join( os.path.join( self.mergeDestinationDir(), "include" ), "wcecompat" )
+            os.putenv( "INCLUDE", wcecompatincdir + ";" + os.getenv("INCLUDE") )
+            cmd = r"nmake -f ms\cedll.mak"
+        else:
+            cmd = r"nmake -f ms\ntdll.mak"
 
         return self.system( cmd )
 
@@ -91,29 +88,61 @@ class Package(CMakePackageBase):
         if not os.path.isdir( os.path.join( dst, "include" ) ):
             os.mkdir( os.path.join( dst, "include" ) )
 
-        if self.compiler() == "mingw" or self.compiler() == "mingw4":
-            shutil.copy( os.path.join( src, "libeay32.dll" ) , os.path.join( dst, "bin" ) )
-            shutil.copy( os.path.join( src, "libssl32.dll" ) , os.path.join( dst, "bin", "ssleay32.dll" ) )
-            utils.copySrcDirToDestDir( os.path.join( src, "outinc" ) , os.path.join( dst, "include" ) )
-            shutil.copy( os.path.join( src, "ms", "applink.c" ) , os.path.join( dst, "include", "openssl" ) )
+        outdir = "out32dll"
+        if self.isTargetBuild():
+            outdir += "_" + self.buildArchitecture()
 
-            # auto-create both import libs with the help of pexports
-            for f in "libeay32 ssleay32".split():
-                self.stripLibs( f )
-                self.createImportLibs( f )
-
-        else:
-            outdir = "out32dll"
-            if self.isTargetBuild():
-                outdir += "_" + self.buildArchitecture()
-
-            shutil.copy( os.path.join( src, outdir, "libeay32.dll" ) , os.path.join( dst, "bin" ) )
-            shutil.copy( os.path.join( src, outdir, "ssleay32.dll" ) , os.path.join( dst, "bin" ) )
-            shutil.copy( os.path.join( src, outdir, "libeay32.lib" ) , os.path.join( dst, "lib" ) )
-            shutil.copy( os.path.join( src, outdir, "ssleay32.lib" ) , os.path.join( dst, "lib" ) )
-            utils.copySrcDirToDestDir( os.path.join( src, "include" ) , os.path.join( dst, "include" ) )
-
+        shutil.copy( os.path.join( src, outdir, "libeay32.dll" ) , os.path.join( dst, "bin" ) )
+        shutil.copy( os.path.join( src, outdir, "ssleay32.dll" ) , os.path.join( dst, "bin" ) )
+        shutil.copy( os.path.join( src, outdir, "libeay32.lib" ) , os.path.join( dst, "lib" ) )
+        shutil.copy( os.path.join( src, outdir, "ssleay32.lib" ) , os.path.join( dst, "lib" ) )
+        utils.copySrcDirToDestDir( os.path.join( src, "include" ) , os.path.join( dst, "include" ) )
+        
         return True
+        
+        
+from Package.PackageBase import *
+from Source.MultiSource import *
+from BuildSystem.AutoToolsBuildSystem import *
+from Packager.KDEWinPackager import *;
+
+class PackageMSys(PackageBase, MultiSource, AutoToolsBuildSystem, KDEWinPackager):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        PackageBase.__init__(self)
+        MultiSource.__init__(self)
+        AutoToolsBuildSystem.__init__(self)
+        KDEWinPackager.__init__(self)
+        self.subinfo.options.package.packageName = 'openssl'
+        self.subinfo.options.package.withCompiler = False
+        self.subinfo.options.package.packSources = False
+        self.shell = MSysShell()
+        
+        self.buildInSource=True
+        if self.buildArchitecture()=="x64" and COMPILER == "mingw4":
+            compiler="mingw64"
+        elif(COMPILER == "mingw4"):
+            compiler="mingw"
+        else:
+            utils.die("msvc is not supported");
+
+        # target install needs perl with native path on configure time
+        os.putenv("PERL",MSysShell().toNativePath(os.path.join( os.environ.get( "KDEROOT" ) , "dev-utils" , "bin" , "perl.exe" )))
+        self.subinfo.options.configure.defines = " shared enable-md2 zlib-dynamic --with-zlib-lib=libzlib.dll.a --with-zlib-include=%s %s" % (MSysShell().toNativePath(os.path.join( self.mergeDestinationDir() ,"include" )) ,compiler )
+           
+      
+    def install (self):
+      if(not AutoToolsBuildSystem.install(self)):
+	return False
+      self.shell.execute(os.path.join( self.imageDir() , "lib"), "chmod" ,"664 *")
+      self.shell.execute(os.path.join( self.imageDir() , "lib" , "engines" ), "chmod" , "755 *")
+      shutil.move( os.path.join( self.imageDir(),  "lib" , "libcrypto.dll.a" ) , os.path.join( self.imageDir() , "lib" , "libeay32.dll.a" ) )
+      shutil.move( os.path.join( self.imageDir(), "lib" , "libssl.dll.a" ) , os.path.join( self.imageDir() , "lib" , "ssleay32.dll.a" ) )
+      return True
+      
 
 if __name__ == '__main__':
-    Package().execute()
+    if COMPILER.startswith( "mingw" ):
+      PackageMSys().execute()
+    else:
+      PackageCMake().execute()
