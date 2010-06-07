f07dbddb3c9ef414bd89195b6bca64f8c6223d9a
Patrick von Reth <patrick.vonreth@gmail.com>
2010-06-07 16:58:38 +0000

    added correct tag
    svn path=/trunk/kdesupport/emerge/; revision=1135577

--- portage/extragear/amarok/amarok-2.3.1.py
+++ portage/extragear/amarok/amarok-2.3.1.py
@@ -6,7 +6,7 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['2.3.1'] = 'git://gitorious.org/amarok/amarok.git|2.3.1'
+        self.targets['2.3.1'] =  'http://download.kde.org/download.php?url=stable/amarok/2.3.1/src/amarok-2.3.1.tar.bz2'
         self.svnTargets['gitHEAD'] = 'git://gitorious.org/amarok/amarok.git'
         self.defaultTarget = 'gitHEAD'
     
