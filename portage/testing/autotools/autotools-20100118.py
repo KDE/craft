89931e0c464f49ab9a3f565da41a5f74cf7ef99d
Patrick von Reth <patrick.vonreth@gmail.com>
2010-06-08 20:55:32 +0000

    added autotools for testing
    svn path=/trunk/kdesupport/emerge/; revision=1136067

---
+++ portage/testing/autotools/autotools-20100118.py
@@ -0,0 +1,34 @@
+import info
+import shutil
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        ver = "20100118"
+        self.targets[ ver ] = "http://downloads.sourceforge.net/sourceforge/mingw-w64/autotools-"+ver+".tar.bz2"
+        self.targetDigests[ ver ] = 'dfb63c7894e1b373df49120988726bc736ac8ba5'
+        
+        self.defaultTarget = ver
+        
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
+        self.hardDependencies['dev-util/msys'] = 'default'
+        
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+from Package.BinaryPackageBase import *
+
+class Package(BinaryPackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        self.subinfo.options.merge.ignoreBuildType = True
+        BinaryPackageBase.__init__(self)
+        
+        
+    def install(self):
+        shutil.move( os.path.join( self.installDir() , "autotools" ) , os.path.join( self.installDir(), "msys" ) )
+        return True
+
+if __name__ == '__main__':
+    Package().execute()
