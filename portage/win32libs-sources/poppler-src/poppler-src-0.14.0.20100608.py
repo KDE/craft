cdb0ccf151184e97ecf8bcccafef7ab8430de14d
Patrick Spendrin <ps_ml@gmx.de>
2010-06-08 20:51:04 +0000

    update to poppler 0.14.0
    svn path=/trunk/kdesupport/emerge/; revision=1136066

--- portage/win32libs-sources/poppler-src/poppler-src-0.14.0.20100608.py
+++ portage/win32libs-sources/poppler-src/poppler-src-0.14.0.20100608.py
@@ -4,19 +4,20 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for i in ( '0.12.1', '0.12.2', '0.12.3', '0.12.4' ):
+        for i in ( '0.12.1', '0.12.2', '0.12.3', '0.12.4', '0.14.0' ):
             self.targets[ i ] = 'http://poppler.freedesktop.org/poppler-%s.tar.gz' % i
             self.targetInstSrc[ i ] = 'poppler-%s' % i
         self.svnTargets['gitHEAD'] = "git://git.freedesktop.org/git/poppler/poppler|master"
         self.svnTargets['0.12-branch'] = "git://git.freedesktop.org/git/poppler/poppler|poppler-0.12"
+        self.svnTargets['0.14-branch'] = "git://git.freedesktop.org/git/poppler/poppler|poppler-0.14"
 
-        self.defaultTarget = "0.12.4"
+        self.defaultTarget = "0.14.0"
     
     def setDependencies( self ):
-        self.hardDependencies['win32libs-bin/fontconfig'] = 'default'
+        #self.hardDependencies['win32libs-bin/fontconfig'] = 'default'
         self.hardDependencies['win32libs-bin/freetype'] = 'default'
         # does not work with mingw
-        #self.hardDependencies['win32libs-bin/openjpeg'] = 'default'
+        self.hardDependencies['win32libs-bin/openjpeg'] = 'default'
         self.hardDependencies['win32libs-bin/lcms'] = 'default'
         self.hardDependencies['win32libs-bin/zlib'] = 'default'
         self.hardDependencies['win32libs-bin/jpeg'] = 'default'
