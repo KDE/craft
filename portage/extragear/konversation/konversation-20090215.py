740a766f13a36615688847c077fa2aff0a315158
Patrick Spendrin <ps_ml@gmx.de>
2010-06-19 21:06:03 +0000

    also move konversation
    svn path=/trunk/kdesupport/emerge/; revision=1140114

--- portage/extragear/konversation/konversation-20090215.py
+++ portage/extragear/konversation/konversation-20090215.py
@@ -8,7 +8,7 @@ class subinfo(info.infoclass):
         for ver in ['1.2.1', '1.2.2', '1.2.3', '1.3']:
             self.targets[ver] = 'ftp://ftp.kde.org/pub/kde/stable/konversation/' + ver + '/src/konversation-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = 'konversation-' + ver
-        self.svnTargets['gitHEAD'] = 'git://gitorious.org/konversation/konversation.git'
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/konversation/konversation.git'
         self.defaultTarget = 'gitHEAD'
     
     def setDependencies( self ):
