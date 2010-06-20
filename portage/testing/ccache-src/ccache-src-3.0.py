eb836fdc5ef048e65ada66c7bdd52215c3919634
Patrick von Reth <patrick.vonreth@gmail.com>
2010-06-20 23:23:44 +0000

    use SaroEngels 3.0 windows branche
    svn path=/trunk/kdesupport/emerge/; revision=1140555

--- portage/testing/ccache-src/ccache-src-3.0.py
+++ portage/testing/ccache-src/ccache-src-3.0.py
@@ -9,12 +9,9 @@ from Package.PackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['2.4'] = "http://ccache-win32.googlecode.com/svn/trunk/ccache-win32"
-        self.svnTargets['gitHEAD'] = "git://git.samba.org/ccache.git"
-        self.targets['3.0-pre1'] = "http://samba.org/ftp/ccache/ccache-3.0pre1.tar.bz2"
-        self.targetInstSrc['3.0-pre1'] = "ccache-3.0pre1"
+        self.svnTargets['gitHEAD'] = "git://gitorious.org/ccache-win/ccache-win.git|windows"
         self.targetSrcSuffix['gitHEAD'] = 'git'
-        self.defaultTarget = '2.4'
+        self.defaultTarget = 'gitHEAD'
         
 
     def setDependencies( self ):
@@ -27,15 +24,8 @@ class Package(PackageBase, MultiSource, AutoToolsBuildSystem, MultiPackager):
         MultiSource.__init__(self)
         AutoToolsBuildSystem.__init__(self)        
         MultiPackager.__init__(self)
-        self.buildInSource = True
         self.subinfo.options.package.withCompiler = False
-        
-    def configure( self ):
-        if self.buildTarget in ['gitHEAD', '3.0-pre1']:
-            if self.buildTarget in ['gitHEAD']:
-              self.subinfo.options.configure.bootstrap = True
-            return AutoToolsBuildSystem.configure( self )
-        return True
+        self.subinfo.options.configure.bootstrap = True
 
 if __name__ == '__main__':
     Package().execute()
