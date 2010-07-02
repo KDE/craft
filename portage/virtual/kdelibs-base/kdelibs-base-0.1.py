191cec6d7c9f0205e4d7f0da76fd0e1884b895d4
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-07-02 07:26:37 +0000

    - removed some dependencies for wince
    svn path=/trunk/kdesupport/emerge/; revision=1145205

--- portage/virtual/kdelibs-base/kdelibs-base-0.1.py
+++ portage/virtual/kdelibs-base/kdelibs-base-0.1.py
@@ -1,5 +1,6 @@
 import base
 import info
+import platform
 
 class subinfo(info.infoclass):
     def setTargets( self ):
@@ -10,10 +11,12 @@ class subinfo(info.infoclass):
         self.hardDependencies['win32libs-bin/aspell']  = 'default'
 #        self.hardDependencies['win32libs-bin/enchant']  = 'default'
         self.hardDependencies['win32libs-bin/gettext']  = 'default'
-        self.hardDependencies['win32libs-bin/giflib']  = 'default'
+        if not platform.isCrossCompilingEnabled():
+            self.hardDependencies['win32libs-bin/giflib']  = 'default'
 #        self.hardDependencies['win32libs-bin/gssapi']  = 'default'
 #        self.hardDependencies['win32libs-bin/hspell']  = 'default'
-        self.hardDependencies['win32libs-bin/jasper']  = 'default'
+        if not platform.isCrossCompilingEnabled():
+            self.hardDependencies['win32libs-bin/jasper']  = 'default'
         self.hardDependencies['win32libs-bin/jpeg']  = 'default'
         self.hardDependencies['win32libs-bin/libbzip2']  = 'default'
         self.hardDependencies['win32libs-bin/libpng']  = 'default'
@@ -22,7 +25,8 @@ class subinfo(info.infoclass):
 #        self.hardDependencies['win32libs-bin/openexr']  = 'default'
         self.hardDependencies['win32libs-bin/openssl']  = 'default'
         self.hardDependencies['win32libs-bin/pcre']  = 'default'
-        self.hardDependencies['win32libs-bin/shared-mime-info']  = 'default'
+        if not platform.isCrossCompilingEnabled():
+            self.hardDependencies['win32libs-bin/shared-mime-info']  = 'default'
         self.hardDependencies['win32libs-bin/zlib']  = 'default'
     
 class subclass(base.baseclass):
