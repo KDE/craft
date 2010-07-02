b9b1d407fbfefad0e7c4b7a0207208c140e280c4
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-07-02 07:14:33 +0000

    - switched libical to new package system
    svn path=/trunk/kdesupport/emerge/; revision=1145203

--- portage/win32libs-sources/libical-src/libical-src-0.44.py
+++ portage/win32libs-sources/libical-src/libical-src-0.44.py
@@ -1,7 +1,3 @@
-import base
-import utils
-import os
-import shutil
 import info
 
 class subinfo(info.infoclass):
@@ -16,47 +12,14 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
 
-class subclass(base.baseclass):
+from Package.CMakePackageBase import *
+        
+class Package(CMakePackageBase):
     def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.createCombinedPackage = True
         self.subinfo = subinfo()
-    
-    def unpack( self ):
-        print "libical unpack called for %s" % self.subinfo.buildTarget
-        # do the svn fetch/update
-        if self.buildTarget == 'svnHEAD':
-            repo = 'https://freeassociation.svn.sourceforge.net/svnroot/freeassociation/trunk/'
-            if self.subinfo.buildTarget in self.subinfo.svnTargets.keys():
-                self.svnFetch( repo + self.subinfo.svnTargets[ self.subinfo.buildTarget ] )
-            else:
-                return False
-            utils.cleanDirectory( self.workdir )
-        else:
-            return base.baseclass.unpack( self )
-        return True
-
-    def compile( self ):
-        if self.buildTarget == 'svnHEAD':
-            if self.subinfo.buildTarget in self.subinfo.svnTargets.keys():
-                self.kde.sourcePath = os.path.join( self.svndir, self.subinfo.svnTargets[ self.subinfo.buildTarget ] )
-            else:
-                return False
-        self.kdeCustomDefines = "-DUSE_BUILTIN_TZDATA=true -DICAL_UNIX_NEWLINE=false"
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        # auto-create both import libs with the help of pexports
-        self.createImportLibs( "libical" )
-        self.createImportLibs( "libicalss" )
-        self.createImportLibs( "libicalvcal" )
-
-        self.doPackaging( "libical", self.buildTarget, True )
-
-        return True
+        self.subinfo.options.package.withCompiler = None
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = " -DUSE_BUILTIN_TZDATA=true -DICAL_UNIX_NEWLINE=false "
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
