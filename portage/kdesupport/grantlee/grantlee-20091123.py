fb80a1abc9a2b67dfd716609ffebc6e8203033f9
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-07-06 07:58:40 +0000

    - make grantlee compile for wince
    svn path=/trunk/kdesupport/emerge/; revision=1146511

--- portage/kdesupport/grantlee/grantlee-20091123.py
+++ portage/kdesupport/grantlee/grantlee-20091123.py
@@ -16,7 +16,16 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self):
         self.subinfo = subinfo()
+        self.subinfo.options.configure.defines = '-DBUILD_TESTS=OFF'
         CMakePackageBase.__init__(self)
+        
+        qmake = os.path.join(self.mergeDestinationDir(), "bin", "qmake.exe")
+        if not os.path.exists(qmake):
+            print("<%s>") % qmake
+            utils.die("could not found qmake")
+        ## \todo a standardized way to check if a package is installed in the image dir would be good.
+        self.subinfo.options.configure.defines += " -DQT_QMAKE_EXECUTABLE:FILEPATH=%s " \
+            % qmake.replace('\\', '/')
         
 if __name__ == '__main__':
     Package().execute()
