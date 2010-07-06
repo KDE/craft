4463e6037eb8e7c41e67a86f4371fb356ae35b1e
Patrick Spendrin <ps_ml@gmx.de>
2010-07-06 13:36:58 +0000

    use lcms from sourceforge
    svn path=/trunk/kdesupport/emerge/; revision=1146725

--- portage/win32libs-sources/lcms-src/lcms-src-1.19.py
+++ portage/win32libs-sources/lcms-src/lcms-src-1.19.py
@@ -5,7 +5,7 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.19'] = "http://www.littlecms.com/lcms-1.19.tar.gz"
+        self.targets['1.19'] = "http://download.sourceforge.net/lcms/lcms-1.19.tar.gz"
         self.targetInstSrc['1.19'] = "lcms-1.19"
         self.patchToApply['1.19'] = ( 'lcms-1.19-20100416.diff', 1 )
         self.defaultTarget = '1.19'
