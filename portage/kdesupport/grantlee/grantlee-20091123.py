9fc9b43a33c515a2732a0d4403a516fda94980c1
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-07-07 07:21:49 +0000

    - simplified
    svn path=/trunk/kdesupport/emerge/; revision=1146962

--- portage/kdesupport/grantlee/grantlee-20091123.py
+++ portage/kdesupport/grantlee/grantlee-20091123.py
@@ -21,13 +21,5 @@ class Package(CMakePackageBase):
             self.subinfo.options.configure.defines = '-DBUILD_TESTS=OFF'
         CMakePackageBase.__init__(self)
         
-        qmake = os.path.join(self.mergeDestinationDir(), "bin", "qmake.exe")
-        if not os.path.exists(qmake):
-            print("<%s>") % qmake
-            utils.die("could not found qmake")
-        ## \todo a standardized way to check if a package is installed in the image dir would be good.
-        self.subinfo.options.configure.defines += " -DQT_QMAKE_EXECUTABLE:FILEPATH=%s " \
-            % qmake.replace('\\', '/')
-        
 if __name__ == '__main__':
     Package().execute()
