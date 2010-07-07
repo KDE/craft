18bc6ec4cbf1d156f5b00d117abffd5f9320915e
Casper van Donderen <casper.vandonderen@gmail.com>
2010-07-07 16:10:58 +0000

    Add CMake support for libsndfile.
    svn path=/trunk/kdesupport/emerge/; revision=1147198

--- portage/testing/libsndfile-src/libsndfile-src-20100707.py
+++ portage/testing/libsndfile-src/libsndfile-src-20100707.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['1.0.21'] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.21.tar.gz'
         self.targetInstSrc['1.0.21'] = 'libsndfile-1.0.21'
+		self.patchToApply['1.0.21'] = ('libsndfile-1.0.21-20100707.diff', 1)
         self.options.package.withCompiler = False
         self.options.package.packageName = "libsndfile"
         self.defaultTarget = '1.0.21'
@@ -17,7 +18,6 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
-        self.subinfo.options.configure.defines = " -DBUILD_SHARED_LIB=ON"
         CMakePackageBase.__init__(self)
 
 if __name__ == '__main__':
