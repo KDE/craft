4754e3a89610ee5245a6201084f1df346b716905
Casper van Donderen <casper.vandonderen@gmail.com>
2010-07-08 08:32:24 +0000

    Update patch for libsndfile-src.
    svn path=/trunk/kdesupport/emerge/; revision=1147428

--- portage/testing/libsndfile-src/libsndfile-src-20100707.py
+++ portage/testing/libsndfile-src/libsndfile-src-20100707.py
@@ -7,13 +7,15 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['1.0.21'] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.21.tar.gz'
         self.targetInstSrc['1.0.21'] = 'libsndfile-1.0.21'
-        self.patchToApply['1.0.21'] = ('libsndfile-1.0.21-20100707.diff', 1)
+        self.patchToApply['1.0.21'] = ('libsndfile-1.0.21-20100708.diff', 1)
         self.options.package.withCompiler = False
         self.options.package.packageName = "libsndfile"
         self.defaultTarget = '1.0.21'
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
+        self.hardDependencies['testing/libogg-src'] = 'default'
+        self.hardDependencies['testing/libvorbis-src'] = 'default'
         
 class Package(CMakePackageBase):
     def __init__( self, **args ):
