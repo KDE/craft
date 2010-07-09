844f19350088500eef1409dcd32abed6ba4bb134
Thomas McGuire <mcguire@kde.org>
2010-07-09 14:54:47 +0000

    Repair duplicate entries
    svn path=/trunk/kdesupport/emerge/; revision=1148021

--- portage/enterprise5/akonadi-e5/akonadi-e5-20100709.py
+++ portage/enterprise5/akonadi-e5/akonadi-e5-20100709.py
@@ -48,7 +48,6 @@ class subinfo(info.infoclass):
         self.svnTargets['20100618'] = 'tags/kdepim/enterprise5.0.20100618.1139547/kdesupport/akonadi'
         self.svnTargets['20100625'] = 'tags/kdepim/enterprise5.0.20100625.1142603/kdesupport/akonadi'
         self.svnTargets['20100701'] = 'tags/kdepim/enterprise5.0.20100701.1144979/kdesupport/akonadi'
-        self.svnTargets['20100709'] = 'tags/kdepim/enterprise5.0.20100709.1147858/kdesupport/akonadi'
         self.svnTargets['20100709'] = 'tags/kdepim/enterprise5.0.20100709.1148001/kdesupport/akonadi'
         self.defaultTarget = '20100709'
 
