f81c530c4193f8537e2a6eabc06bd93c986d9b9c
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-07-12 13:43:10 +0000

    - pushed patch upstream - didn't push findpng so patch needed for that
    svn path=/trunk/kdesupport/emerge/; revision=1149057

--- portage/kde/kdelibs/kdelibs-20080202.py
+++ portage/kde/kdelibs/kdelibs-20080202.py
@@ -8,8 +8,7 @@ class subinfo(info.infoclass):
         for ver in ['80', '83', '85']:
           self.targets['4.0.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.0.' + ver + '/src/kdelibs-4.0.' + ver + '.tar.bz2'
           self.targetInstSrc['4.0.' + ver] = 'kdelibs-4.0.' + ver
-        if platform.isCrossCompilingEnabled():
-            self.patchToApply['svnHEAD'] = ("kdelibs-20100705.patch", 0)
+        self.patchToApply['svnHEAD'] = ("kdelibs-20100705.patch", 0)
         self.defaultTarget = 'svnHEAD'
 
     
