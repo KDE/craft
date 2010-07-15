be43686313ad2ca057e9bd451a5ddafe27117cac
Andre Heinecke <aheinecke@intevation.de>
2010-07-15 09:21:11 +0000

    Fix building of libssh with mingw4 by using Version 0.4.4
    svn path=/trunk/kdesupport/emerge/; revision=1150169

--- portage/win32libs-sources/libssh-src/libssh-src-20090805.py
+++ portage/win32libs-sources/libssh-src/libssh-src-20090805.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
+import compiler
 import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
+        self.targets['0.4.4'] = "http://www.libssh.org/files/libssh-0.4.4.tar.gz"
+        self.targetDigests['0.4.4'] = 'bde1d4713a86b6256ce2d14e6de6326e52c4da44'
+        self.targetInstSrc['0.4.4'] = "libssh-0.4.4"
         self.svnTargets['gitHEAD'] = "git://git.libssh.org/projects/libssh/libssh.git"
         self.svnTargets['0.4'] = "git://git.libssh.org/projects/libssh/libssh.git|v0-4"
-        self.defaultTarget = '0.4'
+
+        # There is a problem in libssh with mingw building till thats fixed we use the
+        # 0.4.4 release with MinGW
+        if compiler.isMinGW():
+            self.defaultTarget = '0.4.4'
+        else:
+            self.defaultTarget = '0.4'
         self.options.package.withCompiler = False
         self.options.configure.defines = "-DWITH_STATIC_LIB=ON"
 
@@ -13,7 +23,7 @@ class subinfo(info.infoclass):
         self.hardDependencies['virtual/base'] = 'default'
         # should be zlib-src, but it's not a real source package...
         self.hardDependencies['win32libs-bin/zlib'] = 'default'
-        self.hardDependencies['win32libs-bin/openssl'] = 'default'
+        self.hardDependencies['win32libs-sources/openssl-src'] = 'default'
 
 from Package.CMakePackageBase import *
 
