ce16d6bedac4d7c1047d99b904726fb9300182e7
Patrick Spendrin <ps_ml@gmx.de>
2010-07-16 08:39:57 +0000

    make the default package a self compiled one, to also fix the find_package bug for the e5 stuff & the general public.
    svn path=/trunk/kdesupport/emerge/; revision=1150560

--- portage/dev-util/cmake/cmake-2.8.2.py
+++ portage/dev-util/cmake/cmake-2.8.2.py
@@ -8,6 +8,7 @@ class subinfo( info.infoclass ):
         self.targets['2.8.0'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.0-win32-x86.zip'
         self.targets['2.8.1'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.1-win32-x86.zip'
         self.targets['2.8.2'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.2-win32-x86.zip'
+        self.targets['v2.8.2'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-v2.8.2-bin.tar.bz2'
         self.targets['2.8.0-ce'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-2.8.0-2-bin.tar.bz2'
         self.targets['2.8.1-ce'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-2.8.1-bin.tar.bz2'
         self.targetMergeSourcePath['2.4.8'] = 'cmake-2.4.8-win32-x86'
@@ -17,12 +18,13 @@ class subinfo( info.infoclass ):
         self.targetMergeSourcePath['2.8.2'] = 'cmake-2.8.2-win32-x86'
         self.targetDigests['2.8.1'] = '2f3b6f14502f0d5c3b1a8e13633789b9995b4629'
         self.targetDigests['2.8.2'] = '3bc9e0861b8cd5d4b4532a47ded7ca0d6f9c85bc'
+        self.targetDigests['v2.8.2'] = 'de516a570808c7a022139b55e758d5f7b378ec7d'
         self.patchToApply['2.8.0'] = ('cmake-2.8.0-wince-support.patch', 0)
 
         if platform.isCrossCompilingEnabled():
             self.defaultTarget = '2.8.0-ce'
         else:
-            self.defaultTarget = '2.8.2'
+            self.defaultTarget = 'v2.8.2'
             
     def setBuildOptions( self ):
         self.disableHostBuild = False
