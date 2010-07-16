824025d82ce718aa7f89b6f04f8680ade21ede7c
Ralf Habacker <ralf.habacker@freenet.de>
2010-07-16 17:49:51 +0000

    added digests
    svn path=/trunk/kdesupport/emerge/; revision=1150773

--- portage/gnuwin32/sed/sed-4.1.5.py
+++ portage/gnuwin32/sed/sed-4.1.5.py
@@ -8,6 +8,8 @@ http://downloads.sourceforge.net/sourceforge/gnuwin32/sed-4.1.5-dep.zip
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['4.1.5'] = SRC_URI
+        self.targetDigests['4.1.5'] = ['abe52d0be25f1bb44b8a8e7a94e7afa9c15b3ae5',
+                                       '736678616305fab80b4ec1a639d5ff0170183310']        
         self.defaultTarget = '4.1.5'
     
     def setDependencies( self ):
