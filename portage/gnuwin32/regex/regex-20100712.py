0b534a8e5e7045b77d855414cdbb9fbe6350e5c4
Andre Heinecke <aheinecke@intevation.de>
2010-07-19 15:45:43 +0000

    Change the binary package to another build from GNUWin32 that is more up to date
    svn path=/trunk/kdesupport/emerge/; revision=1151807

--- portage/gnuwin32/regex/regex-20100712.py
+++ portage/gnuwin32/regex/regex-20100712.py
@@ -1,17 +1,19 @@
 # Regex:
-# Binary package for GNU regex.
-# see http://gnuwin32.sourceforge.net/packages/regex.htm
-# for more information and the Source Code according to this package
+# Binary package for GNU regex compiled for Win32
+# http://sourceforge.net/projects/gnuwin32/files/regex-Spencer/3.8/
+# for more information and the Source Code according to this package see:
+# http://gnuwin32.sourceforge.net/packages/regex-spencer.htm
 
 import info
 
 class subinfo(info.infoclass):
 
     def setTargets( self ):
-        self.targets['2.7'] = \
-                'http://downloads.sourceforge.net/project/gnuwin32/regex/2.7/regex-2.7-bin.zip'
-        self.targetDigests[ '2.7' ] = "0532975f880a6d6dea71a48968709ee36b934b32"
-        self.defaultTarget = '2.7'
+        version = '3.8'
+        self.targets[version] = \
+                '''http://downloads.sourceforge.net/project/gnuwin32/regex-Spencer/3.8/regex-spencer-3.8-lib.zip
+                   http://downloads.sourceforge.net/project/gnuwin32/regex-Spencer/3.8/regex-spencer-3.8-bin.zip'''
+        self.defaultTarget = '3.8'
 
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
