1ee651ca8678e9d11597b2be9b9435ab9cafb49e
Andre Heinecke <aheinecke@intevation.de>
2010-07-19 15:46:35 +0000

    Add a msys-build based source package for OpenLDAP that compiles with MinGW4
    svn path=/trunk/kdesupport/emerge/; revision=1151808

---
+++ portage/testing/openldap-src/openldap-src-2.4.21-20100719.py
@@ -0,0 +1,73 @@
+# OpenLDAP source package for MinGW builds
+#
+# This package provides the OpenLDAP libraries.
+# Building of the OpenLDAP server is disabled.
+
+# TODO: (for integration into win32libs-sources)
+#      Provide a bin package for MSVC
+
+import base
+import os
+import shutil
+import utils
+import info
+import platform
+import compiler
+from Package.PackageBase import *
+from Source.MultiSource import *
+from BuildSystem.AutoToolsBuildSystem import *
+from Packager.KDEWinPackager import *;
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        for ver in [ '2.4.23' , '2.4.21']:
+            self.targets[ver] = 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-' +\
+                                 ver + '.tgz'
+            self.targetInstSrc[ver] = 'openldap-' + ver
+           # self.targetDigestUrls[ver] = 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/' +\
+           #                              'openldap-'+ver+'.sha1'
+            self.defaultTarget = '2.4.21'
+
+        if compiler.isMSVC():
+            utils.die ( "MSVC Build of openldap-src is unsupported" )
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['dev-util/msys'] = 'default'
+        self.hardDependencies['gnuwin32/regex'] = 'default'
+        self.hardDependencies['win32libs-bin/gettext'] = 'default'
+
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class PackageMSys(PackageBase, MultiSource, AutoToolsBuildSystem, KDEWinPackager):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        PackageBase.__init__(self)
+        MultiSource.__init__(self)
+        AutoToolsBuildSystem.__init__(self)
+        KDEWinPackager.__init__(self)
+        self.subinfo.options.package.packageName = 'openldap'
+        self.subinfo.options.package.withCompiler = False
+        self.subinfo.options.package.packSources = True
+        self.subinfo.options.install.useDestDir = False
+        self.shell = MSysShell()
+
+        self.buildInSource=True
+
+        # Only build the client and libraries and not the OpenLDAP server
+        self.subinfo.options.configure.defines = "--disable-slapd"
+
+    def install (self):
+        self.enterSourceDir()
+        self.shell.execute(self.sourceDir(), self.makeProgram, "install" )
+        return True
+
+class Package(PackageMSys):
+    def __init__( self ):
+        PackageMSys.__init__( self )
+
+if __name__ == '__main__':
+      Package().execute()
