14f8769ff0a12b02cebf2e4ceb8171fa08a7213a
Ralf Habacker <ralf.habacker@freenet.de>
2010-07-19 09:08:08 +0000

    - removed obsolate dbus-master-pending patch, it is upstream - removed obsolate dbus-1.3.1-20100710 patch by using dbus-1.3.1.tar.bz2 from dbus git snapshots for 1.3.1 target; the released file did not have the cmake sub dir by accident - applied scope patch to gitHEAD - fixed session bus address settings - wspiapi.h is not required for upstream dbus
    svn path=/trunk/kdesupport/emerge/; revision=1151649

--- portage/win32libs-sources/dbus-src/dbus-src-1.3.1.py
+++ portage/win32libs-sources/dbus-src/dbus-src-1.3.1.py
@@ -12,23 +12,27 @@ class subinfo(info.infoclass):
         self.targetInstSrc['1.2.4'] = 'tags/1.2.4'
         self.targetConfigurePath['1.2.4'] = 'cmake'
         
-        self.targets['1.3.1'] = 'http://dbus.freedesktop.org/releases/dbus/dbus-1.3.1.tar.gz'
-        self.targetInstSrc['1.3.1'] = 'dbus-1.3.1'
-        self.targetConfigurePath['1.3.1'] = 'cmake'
-        self.patchToApply['1.3.1'] = ('dbus-1.3.1-20100710.diff', 1)
-        #self.patchToApply['1.3.1'] = ('dbus-scopes.diff', 1)
-
         self.svnTargets['svnHEAD'] = svnurl + 'trunk'
         self.targetConfigurePath['svnHEAD'] = 'cmake'
-
+        
+        # dbus-1.3.1.tar.gz is missing the cmake sub dir and because 
+        # emerge is not able yet to apply more than one patch we the 
+        # 1.3.1 snapshot took for now
+        #self.targets['1.3.1'] = 'http://dbus.freedesktop.org/releases/dbus/dbus-1.3.1.tar.gz'
+        #self.targetDigests['1.3.1'] = '83c27e15ba79d4a84a10b123ff382233cc77773b'
+        self.targets['1.3.1'] = 'http://cgit.freedesktop.org/dbus/dbus/snapshot/dbus-1.3.1.tar.bz2'
+        self.targetDigests['1.3.1'] = 'e8fa74ad6f2294bdf7d22aed25896d8943287c32'
+        self.targetInstSrc['1.3.1'] = 'dbus-1.3.1'
+        self.targetConfigurePath['1.3.1'] = 'cmake'
+        self.patchToApply['1.3.1'] = ('dbus-scopes.diff', 1)
+        
         self.svnTargets['gitHEAD'] = 'git://anongit.freedesktop.org/git/dbus/dbus'
         self.targetSrcSuffix['gitHEAD'] = 'git'
         self.targetConfigurePath['gitHEAD'] = 'cmake'
-        self.patchToApply['gitHEAD'] = ('dbus-master-pending.diff', 1)
+        self.patchToApply['gitHEAD'] = ('dbus-scopes.diff', 1)
 
         self.defaultTarget = 'gitHEAD'
         self.options.package.version = '1.3.1'
-
         
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
@@ -42,13 +46,16 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__( self )
         self.subinfo.options.package.packageName = 'dbus'
         self.subinfo.options.make.slnBaseName = 'dbus'
-        self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON -DDBUS_SESSION_BUS_DEFAULT_ADDRESS:STRING=autolaunch:scope=install-path"
+        if self.buildTarget == '1.3.1' or self.buildTarget == 'gitHEAD':
+            self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON -DDBUS_SESSION_BUS_DEFAULT_ADDRESS:STRING=autolaunch:scope=install-path"
+        else:
+            self.subinfo.options.configure.defines = "-DDBUS_USE_EXPAT=ON"
         
     def unpack(self):
         if not CMakePackageBase.unpack(self):
             return False      
         if compiler.isMinGW32():
-          if self.buildTarget in ['1.2.1', '1.2.3', '1.2.4', 'svnHEAD', 'gitHEAD']:
+          if self.buildTarget in ['1.2.1', '1.2.3', '1.2.4', 'svnHEAD']:
               utils.copyFile( os.path.join(self.packageDir(), "wspiapi.h"), os.path.join(self.buildDir(), "wspiapi.h") )
         return True
 
