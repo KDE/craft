28ce9226ba6678ef0dc1cfa59e3199aaa03f9259
Patrick Spendrin <ps_ml@gmx.de>
2010-07-20 22:59:10 +0000

    include romains patch
    svn path=/trunk/kdesupport/emerge/; revision=1152404

--- portage/dev-util/cmake/cmake-2.8.2.py
+++ portage/dev-util/cmake/cmake-2.8.2.py
@@ -9,7 +9,7 @@ class subinfo( info.infoclass ):
         self.targets['2.8.1'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.1-win32-x86.zip'
         self.targets['2.8.2'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.2-win32-x86.zip'
         self.targets['v2.8.2'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-v2.8.2-bin.tar.bz2'
-        self.targets['2.8.0-ce'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-2.8.0-3-bin.tar.bz2'
+        self.targets['2.8.0-ce'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-2.8.0-4-bin.tar.bz2'
         self.targets['2.8.1-ce'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-2.8.1-bin.tar.bz2'
         self.targetMergeSourcePath['2.4.8'] = 'cmake-2.4.8-win32-x86'
         self.targetMergeSourcePath['2.6.4'] = 'cmake-2.6.4-win32-x86'
