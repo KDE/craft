60af0579ba4ec20fad4919eab157f4ab8ceff586
Ralf Habacker <ralf.habacker@freenet.de>
2010-07-23 10:59:54 +0000

    migrated to class Package
    svn path=/trunk/kdesupport/emerge/; revision=1153490

--- portage/testing/gsl/gsl-1.8.py
+++ portage/testing/gsl/gsl-1.8.py
@@ -1,13 +1,23 @@
-import base
+import info
 
-SRC_URI= """
-http://downloads.sourceforge.net/gnuwin32/gsl-1.8-bin.zip
-http://downloads.sourceforge.net/gnuwin32/gsl-1.8-lib.zip
-"""
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['1.8'] = """http://downloads.sourceforge.net/gnuwin32/gsl-1.8-bin.zip
+                                 http://downloads.sourceforge.net/gnuwin32/gsl-1.8-lib.zip
+                                 """
+        self.targetDigests['1.8'] = ['91b42ab9fadc7da5eda18eb2b8f295cd89259df8',
+                                     '20e597659098829eb6f46c9923c25bf2c7a47397']
+        self.defaultTarget = '1.8'
+            
+    def setDependencies( self ):
+        self.hardDependencies['gnuwin32/wget'] = 'default'
 
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-    base.baseclass.__init__( self, SRC_URI, args=args )
+from Package.BinaryPackageBase import *
+        
+class Package(BinaryPackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        BinaryPackageBase.__init__( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
--- portage/testing/juk/juk-20080630.py
+++ portage/testing/juk/juk-20080630.py
@@ -6,7 +6,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdemultimedia'
+        self.svnTargets['svnHEAD'] = "trunk/KDE/kdemultimedia#norecursive;trunk/KDE/kdemultimedia/juk;trunk/KDE/kdemultimedia/cmake"
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
@@ -14,33 +14,14 @@ class subinfo(info.infoclass):
         self.hardDependencies['kdesupport/phonon'] = 'default'
         self.hardDependencies['kdesupport/taglib'] = 'default'
         
-class subclass(base.baseclass):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
+from Package.CMakePackageBase import *
+        
+class Package(CMakePackageBase):
+    def __init__( self ):
         self.subinfo = subinfo()
-
-    def unpack( self ):
-        return self.kdeSvnUnpack()
-
-    def compile( self ):
-        self.kdeCustomDefines = "-DBUILD_dragonplayer=OFF"
-        self.kdeCustomDefines += " -DBUILD_kscd=OFF"
-        self.kdeCustomDefines += " -DBUILD_kcompactdisc=OFF"
-#        self.kdeCustomDefines += " -DBUILD_juk=OFF"
-        self.kdeCustomDefines += " -DBUILD_strigi-analyzer=OFF"
-        self.kdeCustomDefines += " -DBUILD_kioslave=OFF"        # audiocd kioslave
-        self.kdeCustomDefines += " -DBUILD_kmix=OFF"
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        if not self.buildTarget == 'svnHEAD':
-            return self.doPackaging( "kdemultimedia", self.buildTarget, True )
-        else:
-            return self.doPackaging( "kdemultimedia", os.path.basename(sys.argv[0]).replace("kdepimlibs-", "").replace(".py", ""), True )
-
+        CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
+
+    
