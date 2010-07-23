97a5d28f96118bd43a92f3aef419f68c42201fe2
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-07-23 07:01:58 +0000

    - fixed phonon to find automoc in wince builds
    svn path=/trunk/kdesupport/emerge/; revision=1153421

--- portage/kdesupport/phonon/phonon-4.3.1.20090227.py
+++ portage/kdesupport/phonon/phonon-4.3.1.20090227.py
@@ -25,22 +25,16 @@ class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        
-        qmake = os.path.join(self.mergeDestinationDir(), "bin", "qmake.exe")
-        if not os.path.exists(qmake):
-            print("<%s>") % qmake
-            utils.die("could not found qmake")
-        ## \todo a standardized way to check if a package is installed in the image dir would be good.
-        self.subinfo.options.configure.defines += "-DQT_QMAKE_EXECUTABLE:FILEPATH=%s " \
-            % qmake.replace('\\', '/')
+        self.subinfo.options.configure.defines = ""
             
-        automoc = os.path.join(self.mergeDestinationDir(), "lib", "automoc4", "Automoc4Config.cmake")
-        if not os.path.exists(automoc):
-            print("<%s>") % automoc
-            utils.die("could not found automoc")
-        ## \todo a standardized way to check if a package is installed in the image dir would be good.
-        self.subinfo.options.configure.defines += "-DAUTOMOC4_CONFIG_FILE:FILEPATH=%s " \
-            % automoc.replace('\\', '/')
+        if self.isTargetBuild():
+            automoc = os.path.join(self.rootdir, "lib", "automoc4", "Automoc4Config.cmake")
+            if not os.path.exists(automoc):
+                print("<%s>") % automoc
+                utils.die("could not found automoc")
+            ## \todo a standardized way to check if a package is installed in the image dir would be good.
+            self.subinfo.options.configure.defines += "-DAUTOMOC4_CONFIG_FILE:FILEPATH=%s " \
+                % automoc.replace('\\', '/')
 
 if __name__ == '__main__':
     Package().execute()
