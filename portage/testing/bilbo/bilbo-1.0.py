445d53df992644db5d22e23c14b08ecca9abb34d
Ralf Habacker <ralf.habacker@freenet.de>
2010-07-23 10:45:47 +0000

    migrated to class Package
    svn path=/trunk/kdesupport/emerge/; revision=1153486

--- portage/testing/bilbo/bilbo-1.0.py
+++ portage/testing/bilbo/bilbo-1.0.py
@@ -1,7 +1,3 @@
-import base
-import utils
-import os
-import sys
 import info
 
 class subinfo( info.infoclass ):
@@ -9,34 +5,19 @@ class subinfo( info.infoclass ):
         self.svnTargets['svnHEAD'] = False
         self.targets['1.0'] = 'http://bilbo.gnufolks.org/packages/bilbo-1.0-src.tar.gz'
         self.targetInstSrc['1.0'] = 'bilbo'
+        self.targetDigests['1.0'] = '91a285337b4fdd0a938d79edfbddda2f13142995'        
         self.defaultTarget = '1.0'
     
     def setDependencies( self ):
         self.hardDependencies['kde/kdepimlibs'] = 'default'
         self.hardDependencies['dev-util/gettext-tools'] = 'default'
         
-class subclass( base.baseclass ):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
+from Package.CMakePackageBase import *
+                
+class Package(CMakePackageBase):
+    def __init__( self ):
         self.subinfo = subinfo()
-        self.instsrcdir = "bilboblogger"
-        
-    def unpack( self ):
-        print "BilboBlogger unpack called"
-        # do the svn fetch/update
-        if not base.baseclass.unpack( self ):
-            return False
-
-        return True
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        return self.doPackaging( "bilboblogger", os.path.basename(sys.argv[0]).replace("bilbo-", "").replace(".py", ""), True )
-
+        CMakePackageBase.__init__(self)        
+        	
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
