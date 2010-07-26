31e252777757eb0eb94bc746d64760ce566e50ad
Ralf Habacker <ralf.habacker@freenet.de>
2010-07-26 12:22:41 +0000

    reverted digests for now - emerge need to be extended to support digests for dynamic urls returned by self.getPackage()
    svn path=/trunk/kdesupport/emerge/; revision=1154870

--- portage/win32libs-bin/boost/boost-1.37.0.py
+++ portage/win32libs-bin/boost/boost-1.37.0.py
@@ -7,8 +7,9 @@ class subinfo(info.infoclass):
         for version in ['1.37.0-1']:
             self.targets[ version ] = self.getPackage( repoUrl, "boost", version )
 
-        self.targetDigests['1.37.0-1'] = ['e789822568e9ed4e217d9f962e3388643ebda473',
-                                          '66f29eff14ccdabdb4d5dfea1f722eea4fe8202d']            
+        ## @todo url's returned from self.getPackage are compiler specific, hard coded digests does not work yet
+        #self.targetDigests['1.37.0-1-vc90'] = ['e789822568e9ed4e217d9f962e3388643ebda473',
+        #                                       '66f29eff14ccdabdb4d5dfea1f722eea4fe8202d']            
         self.defaultTarget = '1.37.0-1'
 
     def setDependencies( self ):
