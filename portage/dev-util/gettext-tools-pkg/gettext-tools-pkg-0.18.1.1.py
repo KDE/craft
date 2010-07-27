a332503c4a2383810ce86aabc85748599e345687
Patrick von Reth <patrick.vonreth@gmail.com>
2010-07-27 10:19:36 +0000

    the msys source package for gettext-tools, for a complete static build some trix are required
    svn path=/trunk/kdesupport/emerge/; revision=1155426

---
+++ portage/dev-util/gettext-tools-pkg/gettext-tools-pkg-0.18.1.1.py
@@ -0,0 +1,42 @@
+# -*- coding: iso-8859-15 -*-
+import base
+import os
+import shutil
+import utils
+import info
+from shells import *
+print """The use of this package is not recommed, use the binary package instead
+To be shure that gettext-tools is linked completly static I recomed a clean setup and libiconv has to be build in a static way"""
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['0.18.1.1'] = 'http://ftp.gnu.org/pub/gnu/gettext/gettext-0.18.1.1.tar.gz'
+        self.targetDigests['0.18.1.1'] = '5009deb02f67fc3c59c8ce6b82408d1d35d4e38f'
+        self.targetInstSrc['0.18.1.1'] = 'gettext-0.18.1.1'
+        self.defaultTarget = '0.18.1.1'
+        
+    def setDependencies( self ):
+        self.hardDependencies['dev-util/perl'] = 'default' # buildtime dependency
+        self.hardDependencies['dev-util/msys'] = 'default' # buildtime dependency
+        self.hardDependencies['win32libs-bin/iconv'] = 'default'
+
+
+from Package.PackageBase import *
+from Source.MultiSource import *
+from BuildSystem.AutoToolsBuildSystem import *
+from Packager.KDEWinPackager import *;
+
+class Package( PackageBase, MultiSource, AutoToolsBuildSystem, KDEWinPackager):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        PackageBase.__init__(self)
+        MultiSource.__init__(self)
+        AutoToolsBuildSystem.__init__(self)
+        KDEWinPackager.__init__(self)
+        self.subinfo.options.configure.defines = "--disable-java --disable-csharp --disable-shared --enable-static --with-gettext-tools "
+        self.subinfo.options.package.withCompiler = None
+        self.subinfo.options.package.packageName = 'gettext-tools'
+
+
+if __name__ == '__main__':
+     Package().execute()
