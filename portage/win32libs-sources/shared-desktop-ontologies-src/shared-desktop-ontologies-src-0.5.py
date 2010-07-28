b059bc3e6a83e8f87275cd76b6894bfb86f6c1f9
Patrick Spendrin <ps_ml@gmx.de>
2010-07-28 16:10:31 +0000

    update to 0.5 version
    svn path=/trunk/kdesupport/emerge/; revision=1156135

--- portage/win32libs-sources/shared-desktop-ontologies-src/shared-desktop-ontologies-src-0.5.py
+++ portage/win32libs-sources/shared-desktop-ontologies-src/shared-desktop-ontologies-src-0.5.py
@@ -4,12 +4,12 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['0.1', '0.2','0.3', '0.4']:
+        for ver in ['0.1', '0.2','0.3', '0.4', '0.5']:
             self.targets[ ver ] = 'http://downloads.sourceforge.net/sourceforge/oscaf/shared-desktop-ontologies-' + ver + '.tar.bz2'
             self.targetInstSrc[ ver ] = 'shared-desktop-ontologies-' + ver
         self.targetDigests['0.4'] = '7ca3522dd9d58329966f46a3b18fa57b0b2280a8'
         self.options.package.withCompiler = False
-        self.defaultTarget = '0.4'
+        self.defaultTarget = '0.5'
 
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
