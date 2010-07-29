1d8e5eb1cc5fc2d6837f21138a03405f873f5e99
Patrick Spendrin <ps_ml@gmx.de>
2010-07-29 12:00:41 +0000

    add a digest
    svn path=/trunk/kdesupport/emerge/; revision=1156624

--- portage/gnuwin32/gawk/gawk-3.1.6-1.py
+++ portage/gnuwin32/gawk/gawk-3.1.6-1.py
@@ -7,6 +7,7 @@ http://downloads.sourceforge.net/sourceforge/gnuwin32/gawk-3.1.6-1-bin.zip
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['3.1.6-1'] = SRC_URI
+        self.targetDigests['3.1.6-1'] = 'bda507655eb3d15059d8a55a0daf6d697a15f632'
         self.defaultTarget = '3.1.6-1'
     
     def setDependencies( self ):
