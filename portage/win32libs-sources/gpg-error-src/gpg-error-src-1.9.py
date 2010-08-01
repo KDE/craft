cb069a054b9be626e634f9480c80b81a17bfc903
Patrick Spendrin <ps_ml@gmx.de>
2010-08-01 20:59:16 +0000

    update to version 1.9 (and also reflect that in the filename) add a first patch to make gpg-error compile on msvc, some minor issues are still left
    svn path=/trunk/kdesupport/emerge/; revision=1157995

---
+++ portage/win32libs-sources/gpg-error-src/gpg-error-src-1.9.py
@@ -0,0 +1,29 @@
+import utils
+import os
+import info
+import platform
+import compiler
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['1.9'] = 'ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.9.tar.bz2'
+        self.patchToApply['1.9'] = ('libgpg-error-1.9-20100801.diff', 1)
+        self.targetInstSrc['1.9'] = 'libgpg-error-1.9'
+        self.targetDigests['1.9'] = '6836579e42320b057a2372bbcd0325130fe2561e'
+        self.defaultTarget = '1.9'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['gnuwin32/grep'] = 'default'
+        self.hardDependencies['gnuwin32/gawk'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+
+if __name__ == '__main__':
+    Package().execute()
