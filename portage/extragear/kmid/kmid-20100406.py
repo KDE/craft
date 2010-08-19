885442f83e0f2a328968851a999ddc79a8cb683f
Pedro Lopez-Cabanillas <pedro.lopez.cabanillas@gmail.com>
2010-08-19 10:17:45 +0000

    Latest published release KMid-2.4.0
    svn path=/trunk/kdesupport/emerge/; revision=1165409

--- portage/extragear/kmid/kmid-20100406.py
+++ portage/extragear/kmid/kmid-20100406.py
@@ -5,10 +5,10 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/extragear/multimedia/kmid'
-        for ver in ['2.3.0', '2.3.1']:
+        for ver in ['2.3.0', '2.3.1', '2.4.0']:
           self.targets[ver] = 'http://downloads.sourceforge.net/kmid2/kmid-' + ver + '.tar.bz2'
           self.targetInstSrc[ver] = 'kmid-' + ver
-        self.defaultTarget = '2.3.1'
+        self.defaultTarget = '2.4.0'
     
     def setDependencies( self ):
         self.hardDependencies['kde/kdebase-runtime'] = 'default'
