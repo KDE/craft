ec3d0854c2ec6a81c8d29a8616bcac9738814690
Patrick Spendrin <ps_ml@gmx.de>
2010-08-20 13:29:03 +0000

    make the default value depend on an environment variable
    svn path=/trunk/kdesupport/emerge/; revision=1165948

--- bin/options.py
+++ bin/options.py
@@ -102,7 +102,10 @@ class OptionsCMake:
         ## use IDE for configuring msvc2008 projects, open IDE in make action instead of running command line orientated make
         self.openIDE = False
         ## use CTest instead of the make utility
-        self.useCTest = False
+        if "EMERGE_USECTEST" in os.environ and os.getenv("EMERGE_USECTEST") in ["True", "1"]:
+            self.useCTest = True
+        else:
+            self.useCTest = False
         
 ## main option class
 class Options:
