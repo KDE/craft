84bfc71cf5fff2f5832a05ab87b2f23ab640db31
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-08-24 14:59:08 +0000

    - added wincetools to testing
    svn path=/trunk/kdesupport/emerge/; revision=1167416

---
+++ portage/testing/wincetools/wincetools-0.01.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import utils
+import os
+import info
+import platform
+import compiler
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://cvs.gnupg.org/wincetools.git'
+        self.targetConfigurePath['gitHEAD'] = 'loader'
+        self.defaultTarget = 'gitHEAD'
+        
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
+
+from Package.CMakePackageBase import *
+                
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+    
+if __name__ == '__main__':
+    Package().execute()
