e8bef7e17d59b4e81d994710ac7fc0699c0a605a
Cristian Oneț <onet.cristian@gmail.com>
2010-09-04 19:25:44 +0000

    Added a target to build kmymoney-4.5.0.
    svn path=/trunk/kdesupport/emerge/; revision=1171659

--- portage/extragear/kmymoney/kmymoney-20091106.py
+++ portage/extragear/kmymoney/kmymoney-20091106.py
@@ -3,6 +3,8 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/extragear/office/kmymoney'
+        self.targets['4.5.0'] = 'http://downloads.sourceforge.net/kmymoney2/kmymoney-4.5.tar.bz2'
+        self.targetInstSrc['4.5.0'] = 'kmymoney-4.5'
         self.targets['3.98.1'] = 'http://downloads.sourceforge.net/kmymoney2/kmymoney-3.98.1.tar.bz2'
         self.targetInstSrc['3.98.1'] = 'kmymoney-3.98.1'
         self.patchToApply['3.98.1'] = ('kmymoney-3.98.1.diff', 1)
