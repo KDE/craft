44f6ff383c000224e49379fdcbb03afd74131f16
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-09-13 15:16:35 +0000

    - make kdeqmlplugin static
    svn path=/trunk/kdesupport/emerge/; revision=1174916

--- portage/kde/kdepim/kdepim-20080202.py
+++ portage/kde/kdepim/kdepim-20080202.py
@@ -37,6 +37,7 @@ class Package(CMakePackageBase):
             self.subinfo.options.configure.defines += " -DTEMPLATEPARSER_NO_WEBKIT=ON "
             self.subinfo.options.configure.defines += " -DIMAPRESOURCE_NO_SOLID=ON "
             self.subinfo.options.configure.defines += " -DRUNTIME_PLUGINS_STATIC=ON "
+            self.subinfo.options.configure.defines += " -DKDEQMLPLUGIN_STATIC=ON "
         
         self.subinfo.options.configure.defines += "-DHOST_BINDIR=%s " \
             % os.path.join(ROOTDIR, "bin")
