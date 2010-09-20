5c7ca066dd16bc9a355fb0c61547f1c600a478cc
Ralf Habacker <ralf.habacker@freenet.de>
2010-09-20 15:01:50 +0000

    added new dependency for kdegraphics based applications like digikam
    svn path=/trunk/kdesupport/emerge/; revision=1177555

---
+++ portage/kde/kdegraphicslibs/kdegraphicslibs-20080202.py
@@ -0,0 +1,24 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdegraphics#norecursive;trunk/KDE/kdegraphics/libs'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.hardDependencies['win32libs-bin/expat'] = 'default'
+        self.hardDependencies['win32libs-bin/jpeg'] = 'default'
+        self.hardDependencies['win32libs-bin/exiv2'] = 'default'
+        self.hardDependencies['win32libs-bin/lcms'] = 'default'
+
+from Package.CMakePackageBase import *
+        
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.onlyBuildTargets='libs'
+        self.subinfo.options.configure.defines='-DBUILD_LIBS=On'
+
+if __name__ == '__main__':
+    Package().execute()
