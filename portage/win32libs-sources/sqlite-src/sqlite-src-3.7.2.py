b1a60b44d6665a12877ab30249733723d5ec2a50
Andre Heinecke <aheinecke@intevation.de>
2010-09-23 13:48:52 +0000

    Update to version 3.7.2
    svn path=/trunk/kdesupport/emerge/; revision=1178680

--- portage/win32libs-sources/sqlite-src/sqlite-src-3.7.2.py
+++ portage/win32libs-sources/sqlite-src/sqlite-src-3.7.2.py
@@ -3,16 +3,16 @@ import utils
 import info
 from Package.CMakePackageBase import *
 
-
-
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['3.6.23.1'] = 'http://www.sqlite.org/sqlite-amalgamation-3.6.23.1.tar.gz'
+        self.targets['3.7.2'] = 'http://www.sqlite.org/sqlite-amalgamation-3_7_2.zip'
+        self.targetDigests['3.7.2'] = "e37304dffaf6e1b38357113ee9845cadff05e958"
+        self.patchToApply['3.7.2']    = ("sqlite_cmake_and_wince_20100923.diff", 1)
+        self.patchToApply['3.6.23.1'] = ("sqlite_cmake_and_wince_20100923.diff", 1)
         self.targetInstSrc['3.6.23.1'] = "sqlite-3.6.23.1"
-        self.patchToApply['3.6.23.1'] = ( "sqlite-3.6.23.1-20100504.diff", 1 )
-        self.defaultTarget = '3.6.23.1'
-        
-        
+        self.defaultTarget = '3.7.2'
+
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
         if platform.isCrossCompilingEnabled():
@@ -24,16 +24,15 @@ class Package(CMakePackageBase):
     CMakePackageBase.__init__(self)
     if platform.isCrossCompilingEnabled() and self.isTargetBuild():
         self.subinfo.options.configure.defines = "-DSTATIC_LIBRARY=ON"
-    
+
   def make(self, unused=''):
     if self.isTargetBuild():
         # Set the include path for the wcecompat files (e.g. errno.h). Setting it through
         # the Configure script generates errors due to the the backslashes in the path
         wcecompatincdir = os.path.join( os.path.join( self.mergeDestinationDir(), "include" ), "wcecompat" )
         os.environ["TARGET_INCLUDE"] = wcecompatincdir + ";" + os.getenv("TARGET_INCLUDE")
-        
-    return CMakePackageBase.make( self )
 
+    return CMakePackageBase.make( self )
 
 if __name__ == '__main__':
     Package().execute()
