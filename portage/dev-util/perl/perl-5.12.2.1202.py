6ac46e8ed6a6caa4f95669270b4e4f3908ec5135
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-09-24 18:44:07 +0000

    updat to perl 5.12.2
    svn path=/trunk/kdesupport/emerge/; revision=1179181

--- portage/dev-util/perl/perl-5.12.2.1202.py
+++ portage/dev-util/perl/perl-5.12.2.1202.py
@@ -9,11 +9,17 @@ class subinfo(info.infoclass):
            self.targets['5.10.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.10.1.1007/ActivePerl-5.10.1.1007-MSWin32-x64-291969.zip"
            self.targetDigests['5.10.1'] = 'b5e9ab83d14e1c3311e280a96d355d491d4d55f5'
            self.targetMergeSourcePath['5.10.1'] = "ActivePerl-5.10.1.1007-MSWin32-x64-291969\\perl"   
+           self.targets['5.12.2'] = "http://downloads.activestate.com/ActivePerl/releases/5.12.2.1202/ActivePerl-5.12.2.1202-MSWin32-x64-293621.zip"
+           #self.targetDigests['5.12.2'] = 'b5e9ab83d14e1c3311e280a96d355d491d4d55f5'
+           self.targetMergeSourcePath['5.12.2'] = "ActivePerl-5.12.2.1202-MSWin32-x64-291969\\perl"
         else:
            self.targets['5.10.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.10.1.1007/ActivePerl-5.10.1.1007-MSWin32-x86-291969.zip"
            self.targetDigests['5.10.1'] = '9122a828b32d8b8499c73b61972eaec303698961'
-           self.targetMergeSourcePath['5.10.1'] = "ActivePerl-5.10.1.1007-MSWin32-x86-291969\\perl"       
-        self.defaultTarget = '5.10.1'
+           self.targetMergeSourcePath['5.10.1'] = "ActivePerl-5.10.1.1007-MSWin32-x86-291969\\perl"
+           self.targets['5.12.2'] = "http://downloads.activestate.com/ActivePerl/releases/5.12.2.1202/ActivePerl-5.12.2.1202-MSWin32-x86-293621.zip"
+           self.targetDigests['5.12.2'] = '6110dc6915902738c5b67c30c39227916d189fe2'
+           self.targetMergeSourcePath['5.12.2'] = "ActivePerl-5.12.2.1202-MSWin32-x86-291969\\perl"
+        self.defaultTarget = '5.12.2'
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
