ae50ae749b2b9eb0c3951dea5bfc0ab34384763d
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-09-26 14:11:13 +0000

    update to 0.14.3 use src dependency packages when building from source
    svn path=/trunk/kdesupport/emerge/; revision=1179919

--- portage/win32libs-sources/poppler-src/poppler-src-0.14.3.py
+++ portage/win32libs-sources/poppler-src/poppler-src-0.14.3.py
@@ -4,25 +4,27 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for i in ( '0.12.1', '0.12.2', '0.12.3', '0.12.4', '0.14.0', '0.14.1' ):
+        for i in ( '0.12.1', '0.12.2', '0.12.3', '0.12.4', '0.14.0', '0.14.1', '0.14.3' ):
             self.targets[ i ] = 'http://poppler.freedesktop.org/poppler-%s.tar.gz' % i
             self.targetInstSrc[ i ] = 'poppler-%s' % i
+        self.patchToApply['0.14.3'] = ( 'poppler-src-0.14.3.patch', 1 )
+        self.targetDigests['0.14.3'] = 'b5e0db722625d92a025e62c3e2fe7a4fc6318b32'
         self.svnTargets['gitHEAD'] = "git://git.freedesktop.org/git/poppler/poppler|master"
         self.svnTargets['0.12-branch'] = "git://git.freedesktop.org/git/poppler/poppler|poppler-0.12"
         self.svnTargets['0.14-branch'] = "git://git.freedesktop.org/git/poppler/poppler|poppler-0.14"
 
-        self.defaultTarget = "0.14-branch"
+        self.defaultTarget = "0.14.3"
     
     def setDependencies( self ):
         #self.hardDependencies['win32libs-bin/fontconfig'] = 'default'
-        self.hardDependencies['win32libs-bin/freetype'] = 'default'
+        self.hardDependencies['win32libs-sources/freetype-src'] = 'default'
         # does not work with mingw
-        self.hardDependencies['win32libs-bin/openjpeg'] = 'default'
-        self.hardDependencies['win32libs-bin/lcms'] = 'default'
-        self.hardDependencies['win32libs-bin/zlib'] = 'default'
-        self.hardDependencies['win32libs-bin/jpeg'] = 'default'
-        self.hardDependencies['win32libs-bin/libpng'] = 'default'
-        self.hardDependencies['win32libs-bin/libxml2'] = 'default'
+        self.hardDependencies['win32libs-sources/openjpeg-src'] = 'default'
+        self.hardDependencies['win32libs-sources/lcms-src'] = 'default'
+        self.hardDependencies['win32libs-sources/zlib-src'] = 'default'
+        self.hardDependencies['win32libs-sources/jpeg-src'] = 'default'
+        self.hardDependencies['win32libs-sources/libpng-src'] = 'default'
+        self.hardDependencies['win32libs-sources/libxml2-src'] = 'default'
         self.hardDependencies['data/poppler-data'] = 'default'
         self.hardDependencies['libs/qt'] = 'default'
     
