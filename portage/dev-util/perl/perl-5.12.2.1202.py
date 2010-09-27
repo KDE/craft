44ff91b8fb6e97bb2e0e198ecb39772607907368
Patrick Spendrin <ps_ml@gmx.de>
2010-09-27 07:44:22 +0000

    fix perl on 64bit
    svn path=/trunk/kdesupport/emerge/; revision=1180148

--- portage/dev-util/perl/perl-5.12.2.1202.py
+++ portage/dev-util/perl/perl-5.12.2.1202.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
            self.targetMergeSourcePath['5.10.1'] = "ActivePerl-5.10.1.1007-MSWin32-x64-291969\\perl"   
            self.targets['5.12.2'] = "http://downloads.activestate.com/ActivePerl/releases/5.12.2.1202/ActivePerl-5.12.2.1202-MSWin32-x64-293621.zip"
            #self.targetDigests['5.12.2'] = 'b5e9ab83d14e1c3311e280a96d355d491d4d55f5'
-           self.targetMergeSourcePath['5.12.2'] = "ActivePerl-5.12.2.1202-MSWin32-x86-293621\\perl"
+           self.targetMergeSourcePath['5.12.2'] = "ActivePerl-5.12.2.1202-MSWin32-x64-293621\\perl"
         else:
            self.targets['5.10.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.10.1.1007/ActivePerl-5.10.1.1007-MSWin32-x86-291969.zip"
            self.targetDigests['5.10.1'] = '9122a828b32d8b8499c73b61972eaec303698961'
