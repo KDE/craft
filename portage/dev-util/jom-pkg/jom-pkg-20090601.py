c3b40561a28de99505dcf5ee4fb5a5ae5aba61f6
Patrick Spendrin <ps_ml@gmx.de>
2010-10-01 13:36:12 +0000

    add the temporary mingw repository for jom
    svn path=/trunk/kdesupport/emerge/; revision=1181612

--- portage/dev-util/jom-pkg/jom-pkg-20090601.py
+++ portage/dev-util/jom-pkg/jom-pkg-20090601.py
@@ -1,56 +1,25 @@
 # -*- coding: utf-8 -*-
-import base
-import os
-import utils
 import info
-import sys
+from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
         """ """
         self.svnTargets['svnHEAD'] = "git://qt.gitorious.org/qt-labs/jom.git"
+        self.svnTargets['mingw'] = "git://gitorious.org/~saroengels/qt-labs/jom-mingw.git"
         self.svnTargets['static'] = "git://qt.gitorious.org/qt-labs/jom.git"
+        
         self.defaultTarget = 'svnHEAD'
 
     def setDependencies( self ):
-        self.hardDependencies['dev-util/msys'] = 'default'
-        self.hardDependencies['dev-util/git'] = 'default'
+        self.hardDependencies['virtual/base'] = 'default'
         self.hardDependencies['testing/libantlr'] = 'default'
-        pass
         
-    def setBuildOptions( self ):
-        self.disableHostBuild = False
-        self.disableTargetBuild = True
-
-class subclass(base.baseclass):
+class Package(CMakePackageBase):
     def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
         self.subinfo = subinfo()
-        if self.buildTarget == "static":
-            self.buildType = "Release"
-
-        self.createCombinedPackage = True
-
-    def compile( self ):
-        if self.buildTarget == "static":
-            self.kdeCustomDefines += " -DQT_QMAKE_EXECUTABLE:FILEPATH=%s " % \
-            os.path.join(self.rootdir, "qt-static", "bin", "qmake.exe").replace('\\', '/')
-        else:
-            self.kdeCustomDefines = "-DJOM_ENABLE_TESTS=ON"
-
-        self.kde.sourcePath = self.svndir
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def unittest( self ):
-        return self.kdeTest()
-
-    def make_package( self ):
-        self.kde.sourcePath = self.svndir
-        self.doPackaging("jom")
-        return True
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DJOM_ENABLE_TESTS=ON"
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
