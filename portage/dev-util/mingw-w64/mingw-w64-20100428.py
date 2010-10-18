4623ba4231873e303a9bb08c20d42fee01279962
Patrick von Reth <patrick.vonreth@gmail.com>
2010-10-18 10:11:04 +0000

    updated version
    svn path=/trunk/kdesupport/emerge/; revision=1187111

--- portage/dev-util/mingw-w64/mingw-w64-20100428.py
+++ portage/dev-util/mingw-w64/mingw-w64-20100428.py
@@ -5,7 +5,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):        
-        ver = "20100914"
+        ver = "20101003"
         self.targets[ver] = "http://downloads.sourceforge.net/sourceforge/mingw-w64/mingw-w64-bin_x86_64-mingw_"+ver+"_sezero.zip"
         self.defaultTarget = ver
             
