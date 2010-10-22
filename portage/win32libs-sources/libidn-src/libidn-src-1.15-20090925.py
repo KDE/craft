0a8e094145d8cc6b1aa23bcc7c6437a48bf9f201
Patrick von Reth <patrick.vonreth@gmail.com>
2010-10-22 10:46:03 +0000

    updated version and emerge file
    svn path=/trunk/kdesupport/emerge/; revision=1188418

--- portage/win32libs-sources/libidn-src/libidn-src-1.15-20090925.py
+++ portage/win32libs-sources/libidn-src/libidn-src-1.15-20090925.py
@@ -6,65 +6,31 @@ import shutil
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = '1.15'
-        self.targets[ver]       = 'ftp://alpha.gnu.org/pub/gnu/libidn/libidn-%s.tar.gz' % ver
-        self.targetInstSrc[ver] = 'libidn-%s' % ver
+        ver = '1.19'
+        self.targets[ ver ]       = 'ftp://ftp.gnu.org/gnu/libidn/libidn-%s.tar.gz' % ver
+        self.targetDigests[ ver ] = '2b6dcb500e8135a9444a250d7df76f545915f25f'
+        self.targetInstSrc[ ver ] = 'libidn-%s' % ver
+        self.patchToApply[ ver ] = ("libidn-1.19-20101022.diff", 1)
         self.defaultTarget = ver
 
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
 
 
-class subclass(base.baseclass):
-  def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.createCombinedPackage = True
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
         self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
 
-  def execute( self ):
-    base.baseclass.execute( self )
-    if not (self.compiler == "mingw" or self.compiler == "mingw4"):
-      print "error: can only be build with MinGW (but in the end a \
-             mingw/msvc combined package is created"
-      exit( 1 )
+    def createPackage( self ): 
 
-  def unpack( self ):    
-    if( not base.baseclass.unpack( self ) ):
-      return False
+        self.stripLibs( "libidn-11" )
+        self.createImportLibs( "libidn-11" )
 
-    # we have an own cmake script - copy it to the right place
-    src = os.path.join( self.packagedir , "CMakeLists.txt" )
-    dst = os.path.join( self.workdir, self.instsrcdir, "CMakeLists.txt" )
-    shutil.copy( src, dst )
-
-    src = os.path.join( self.packagedir , "config.h.cmake" )
-    dst = os.path.join( self.workdir, self.instsrcdir, "config.h.cmake" )
-    shutil.copy( src, dst )
-
-    src = os.path.join( self.packagedir , "idn-int.h.cmake" )
-    dst = os.path.join( self.workdir, self.instsrcdir, "idn-int.h.cmake" )
-    shutil.copy( src, dst )
-
-    return True
-
-  def compile( self ):
-      return self.kdeCompile()
-
-  def install( self ):
-      return self.kdeInstall()
-
-  def make_package( self ):
-    # clean directory
-    dst = os.path.join( self.imagedir, self.instdestdir, "lib" )
-    utils.cleanDirectory( dst )
-
-    self.stripLibs( "libidn-11" )
-    self.createImportLibs( "libidn-11" )
-
-    # now do packaging with kdewin-packager
-    self.doPackaging( "libidn", self.buildTarget, True )
-
-    return True
+        # now do packaging with kdewin-packager
+        return CMakePackageBase.createPackage( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+     Package().execute()
