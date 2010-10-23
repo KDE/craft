4f5f0104a27ce43c9c2b90733ab1ac1c5a585f7a
Patrick von Reth <patrick.vonreth@gmail.com>
2010-10-23 22:51:13 +0000

    updated fixed glib
    svn path=/trunk/kdesupport/emerge/; revision=1189073

--- portage/testing/glib/glib-2.26.0-1.py
+++ portage/testing/glib/glib-2.26.0-1.py
@@ -8,9 +8,9 @@ class subinfo(info.infoclass):
         if( os.getenv('EMERGE_ARCHITECTURE')=="x64"):
            arch='64'
         for version in ['2.24.0-2']:
-            self.targets[ version ] = self.getPackageList("ftp.gnome.org/pub/gnome/binaries/win"+arch+"/glib/2.24",
-                                                          ["glib_2.24.0-2_win"+arch+".zip",
-                                                          "glib-dev_2.24.0-2_win"+arch+".zip"])
+            self.targets[ version ] = self.getPackageList("http://ftp.acc.umu.se/pub/gnome/binaries/win"+arch+"/glib/2.26/",
+                                                          ["glib_2.26.0-1_win"+arch+".zip",
+                                                          "glib-dev_2.26.0-1_win"+arch+".zip"])
         self.targetDigests['2.24.0-2'] = ['0efd5f86f526bc3ec63eebe1b31709918708f0d6',
                                           'b41715b4c1379a0172c47a8b54b3208ece20f14e']   
         self.defaultTarget = '2.24.0-2'
