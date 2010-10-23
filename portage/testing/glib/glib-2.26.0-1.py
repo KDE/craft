363879d49b182026e3f2aaa578d805cebcb96fdf
Patrick von Reth <patrick.vonreth@gmail.com>
2010-10-23 23:03:25 +0000

    removed digets cant use digets for multi architecture packages
    svn path=/trunk/kdesupport/emerge/; revision=1189075

--- portage/testing/glib/glib-2.26.0-1.py
+++ portage/testing/glib/glib-2.26.0-1.py
@@ -11,9 +11,7 @@ class subinfo(info.infoclass):
             self.targets[ version ] = self.getPackageList("http://ftp.acc.umu.se/pub/gnome/binaries/win"+arch+"/glib/2.26/",
                                                           ["glib_2.26.0-1_win"+arch+".zip",
                                                           "glib-dev_2.26.0-1_win"+arch+".zip"])
-                                                          
-        self.targetDigests['2.24.0-2'] = ['4a2b4effeba93a9fdb5f591c856845c328239efa',
-                                          'f54e9766e19a007e21535dc2a28c16babc827865']
+
 
         self.defaultTarget = '2.24.0-2'
 
