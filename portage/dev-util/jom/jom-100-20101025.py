3a0fa93d7cad3acf6c9641946da37fc96118f135
Patrick von Reth <patrick.vonreth@gmail.com>
2010-10-25 09:16:41 +0000

    updeated version
    svn path=/trunk/kdesupport/emerge/; revision=1189559

--- portage/dev-util/jom/jom-100-20101025.py
+++ portage/dev-util/jom/jom-100-20101025.py
@@ -12,9 +12,9 @@ class subinfo(info.infoclass):
         self.targets['HEAD'] = 'ftp://ftp.qt.nokia.com/jom/jom.zip'
         self.targets['094'] = 'ftp://ftp.qt.nokia.com/jom/jom094.zip'
         self.targetDigests['094'] = '1f946283866cd6f40a5888088f6c7d840b62af2d'
-        self.targetInstallPath['094'] = "bin"
-        self.targetInstallPath['HEAD'] = "bin"
-        self.defaultTarget = '094'
+        self.targets['100'] = 'ftp://ftp.qt.nokia.com/jom/jom100.zip'
+        self.targetDigests['100'] = '545e964c606d28edce582f167574298589970fb4'
+        self.defaultTarget = '100'
 
     def setBuildOptions( self ):
         self.disableHostBuild = False
@@ -26,7 +26,7 @@ class Package(BinaryPackageBase):
     def __init__( self):
         self.subinfo = subinfo()
         self.subinfo.options.merge.ignoreBuildType = True
-        self.subinfo.options.merge.destinationPath = "dev-utils"
+        self.subinfo.options.merge.destinationPath = os.path.join("dev-utils","bin")
         BinaryPackageBase.__init__(self)
 
 if __name__ == '__main__':
