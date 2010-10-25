bf953525a09fc1b625b2b3f5852e599631f2a2e0
Patrick von Reth <patrick.vonreth@gmail.com>
2010-10-25 19:36:52 +0000

    build libfftw our self
    svn path=/trunk/kdesupport/emerge/; revision=1189757

--- portage/testing/libfftw-src/libfftw-src-20091001.py
+++ portage/testing/libfftw-src/libfftw-src-20091001.py
@@ -6,41 +6,23 @@ import utils
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['3.3.2'] = 'ftp://ftp.fftw.org/pub/fftw/fftw-3.2.2.pl1-dll32.zip'
+        self.targets['3.3.2'] = 'http://www.fftw.org/fftw-3.2.2.tar.gz'
+        self.targetDigests['3.3.2'] = 'd43b799eedfb9408f62f9f056f5e8a645618467b'
+        self.targetInstSrc['3.3.2'] = "fftw-3.2.2"
+
         self.defaultTarget = '3.3.2'
-        self.options.package.packSources = False
-        self.options.package.withCompiler = False
-        self.options.package.packageName = "libfftw"
 
     def setDependencies( self ):
         self.hardDependencies['gnuwin32/wget'] = 'default'
-        
+        self.hardDependencies['dev-util/msys'] = 'default'
 
-from Package.BinaryPackageBase import *
-        
-class Package(BinaryPackageBase):
-    def __init__( self, **args ):
+from Package.AutoToolsPackageBase import *
+
+class Package( AutoToolsPackageBase):
+    def __init__( self ):
         self.subinfo = subinfo()
-        BinaryPackageBase.__init__(self)
-
-    def __move(self, filenames, destdir):
-        """ check if the destination directory exists and move multiple files """
-        if not os.path.exists(destdir):
-            os.makedirs(destdir)
-        for i in filenames:
-            shutil.move(os.path.join(self.imageDir(), i), os.path.join(destdir, i))
-
+        AutoToolsPackageBase.__init__(self)
+        self.subinfo.options.package.withCompiler = False
         
-    def unpack(self):
-        BinaryPackageBase.unpack(self)
-        self.__move('bench.exe benchf.exe benchl.exe fftw-wisdom.exe fftwf-wisdom.exe fftwl-wisdom.exe libfftw3-3.dll libfftw3f-3.dll libfftw3l-3.dll'.split(), os.path.join(self.imageDir(), "bin"))
-        self.__move('libfftw3-3.def libfftw3f-3.def libfftw3l-3.def'.split(), os.path.join(self.imageDir(), "lib"))
-        self.__move('fftw3.f fftw3.h'.split(), os.path.join(self.imageDir(), "include"))
-        self.__move('COPYING COPYRIGHT NEWS README README-bench README-WINDOWS'.split(), os.path.join(self.imageDir(), "doc", "fftw"))
-
-        for i in "libfftw3-3 libfftw3f-3 libfftw3l-3".split():
-            utils.createImportLibs(i, self.imageDir())
-        return True
-
 if __name__ == '__main__':
-    Package().execute()
+     Package().execute()
