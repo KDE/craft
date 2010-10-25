ae60d7b63f68e384e655500b8b469a2d128220b9
Patrick von Reth <patrick.vonreth@gmail.com>
2010-10-25 18:08:49 +0000

    make patch aply
    svn path=/trunk/kdesupport/emerge/; revision=1189735

--- portage/testing/libsamplerate-src/libsamplerate-src-0.1.7.py
+++ portage/testing/libsamplerate-src/libsamplerate-src-0.1.7.py
@@ -5,6 +5,7 @@ from Package.CMakePackageBase import *
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['0.1.7'] = "http://www.mega-nerd.com/SRC/libsamplerate-0.1.7.tar.gz"
+        self.targetDigests['0.1.7'] = 'f3f803ec5feae5a3fdb0fa3937277669e854386e'
         self.targetInstSrc['0.1.7'] = 'libsamplerate-0.1.7'
         self.patchToApply['0.1.7'] = ('libsamplerate-0.1.7-20091002.diff', 1)
         self.options.package.withCompiler = False
