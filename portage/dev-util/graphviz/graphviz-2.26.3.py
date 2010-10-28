49b512ed8ece5a33a97f22573eb1634775b73a22
Ralf Habacker <ralf.habacker@freenet.de>
2010-10-28 13:54:09 +0000

    completed graphviz package
    svn path=/trunk/kdesupport/emerge/; revision=1190667

--- portage/dev-util/graphviz/graphviz-2.26.3.py
+++ portage/dev-util/graphviz/graphviz-2.26.3.py
@@ -3,7 +3,11 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['2.26.3'] = 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.26.3.msi'
+        self.targetDigests['2.26.3'] = '0ce70fcd7ce880ee19ecce6ef8e943d48a1a5374'
         self.defaultTarget = '2.26.3'
+        # the zip file does not have a bin dir, so we have to create it  
+        # This attribute is in prelimary state
+        #self.targetInstallPath['1.5.9'] = "bin"
         
 from Package.BinaryPackageBase import *
 
@@ -11,6 +15,7 @@ class Package(BinaryPackageBase):
     def __init__( self):
         self.subinfo = subinfo()
         self.subinfo.options.merge.ignoreBuildType = True
+        self.subinfo.options.merge.destinationPath = "dev-utils"
         BinaryPackageBase.__init__(self)
 
 if __name__ == '__main__':
