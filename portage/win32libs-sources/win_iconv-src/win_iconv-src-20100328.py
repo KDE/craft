0afa43919e9dcb8f06edd6ebe6d3cc35146dd59c
Patrick Spendrin <ps_ml@gmx.de>
2010-10-28 11:43:56 +0000

    add new release 0.0.2 and make it default for everybody again
    svn path=/trunk/kdesupport/emerge/; revision=1190630

--- portage/win32libs-sources/win_iconv-src/win_iconv-src-20100328.py
+++ portage/win32libs-sources/win_iconv-src/win_iconv-src-20100328.py
@@ -3,14 +3,15 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'http://win-iconv.googlecode.com/svn/trunk'
-        self.targets['0.0.1'] = 'http://win-iconv.googlecode.com/files/win-iconv-0.0.1.tar.bz2'
-        self.targetDigests['0.0.1'] = 'faf4f1f311f92f2a80afe275f43fabb047f23308'
-        self.targetInstSrc['0.0.1'] = 'win-iconv-0.0.1'
-        if platform.isCrossCompilingEnabled():
-            self.defaultTarget = 'svnHEAD'
-        else:
-            self.defaultTarget = '0.0.1'
+        for ver in ['0.0.1', '0.0.2']:
+            self.targets[ver] = 'http://win-iconv.googlecode.com/files/win-iconv-' + ver + '.tar.bz2'
+            self.targetInstSrc[ver] = 'win-iconv-' + ver
 
+        self.targetDigests['0.0.1'] = 'faf4f1f311f92f2a80afe275f43fabb047f23308'
+        self.targetDigests['0.0.2'] = 'd61714a4708d76537600782eb72ccb3cbc89b4b5'
+
+        self.defaultTarget = '0.0.2'
+    
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
         if platform.isCrossCompilingEnabled():
