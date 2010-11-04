0d9749eb8e99098eeab135620273f558e33d2f5f
Patrick Spendrin <ps_ml@gmx.de>
2010-11-04 01:58:44 +0000

    add libechonest
    svn path=/trunk/kdesupport/emerge/; revision=1192804

---
+++ portage/testing/libechonest/libechonest-20101104.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import info
+import os
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+      self.hardDependencies['libs/qt'] = 'default'
+        
+    def setTargets( self ):
+      self.svnTargets['gitHEAD'] = 'git://git.kde.org/libechonest'
+      self.defaultTarget = 'gitHEAD'
+         
+
+class Package( CMakePackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)       
+
+if __name__ == '__main__':
+    Package().execute()
