052c6b4e93903598729c0eb4196776ad52ed1cc2
Patrick Spendrin <ps_ml@gmx.de>
2010-11-04 03:15:55 +0000

    move some stuff around one of the tests still fails, so we might not currently want it to switch of the tests
    svn path=/trunk/kdesupport/emerge/; revision=1192812

--- portage/testing/libechonest/libechonest-20101104.py
+++ portage/testing/libechonest/libechonest-20101104.py
@@ -4,19 +4,21 @@ import os
 from Package.CMakePackageBase import *
 
 
-class subinfo(info.infoclass):
+class subinfo( info.infoclass ):
     def setDependencies( self ):
-      self.hardDependencies['libs/qt'] = 'default'
+      self.hardDependencies[ 'libs/qt' ] = 'default'
+      self.hardDependencies[ 'kdesupport/qjson' ] = 'default'
         
     def setTargets( self ):
-      self.svnTargets['gitHEAD'] = 'git://git.kde.org/libechonest'
+      self.svnTargets[ 'gitHEAD' ] = 'git://git.kde.org/libechonest'
       self.defaultTarget = 'gitHEAD'
          
 
 class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
-        CMakePackageBase.__init__(self)       
+        CMakePackageBase.__init__( self )
+#        self.subinfo.options.configure.defines = "-DECHONEST_BUILD_TESTS=OFF"
 
 if __name__ == '__main__':
     Package().execute()
