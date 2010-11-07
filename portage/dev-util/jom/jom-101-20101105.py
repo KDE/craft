e92a1b105b5bcdb23a53b62adfb84f4283b8d4b9
Patrick Spendrin <ps_ml@gmx.de>
2010-11-07 21:50:21 +0000

    add a new patched jom 101 binaries provided by chehrlic
    svn path=/trunk/kdesupport/emerge/; revision=1194009

--- portage/dev-util/jom/jom-101-20101105.py
+++ portage/dev-util/jom/jom-101-20101105.py
@@ -15,6 +15,8 @@ class subinfo(info.infoclass):
         self.targetDigests['094'] = '1f946283866cd6f40a5888088f6c7d840b62af2d'
         self.targetDigests['100'] = '545e964c606d28edce582f167574298589970fb4'
         self.targetDigests['101'] = '3cbc2750a8a0b8c736a10c7445b0b92ecf247292'
+        self.targets['101-patched'] = 'http://www.winkde.org/pub/kde/ports/win32/repository/other/jom101-patched.7z'
+        self.targetDigests['101-patched'] = '5f878e50cdd05f390b2737d4050a740edd48337f'
         self.defaultTarget = '094'
 
     def setBuildOptions( self ):
