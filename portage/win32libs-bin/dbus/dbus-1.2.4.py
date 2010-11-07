521a02bf5ef31bde87d05e7f04fa058fd76e260a
Patrick Spendrin <ps_ml@gmx.de>
2010-11-07 20:24:34 +0000

    few whitespace changes first
    svn path=/trunk/kdesupport/emerge/; revision=1193993

--- portage/win32libs-bin/dbus/dbus-1.2.4.py
+++ portage/win32libs-bin/dbus/dbus-1.2.4.py
@@ -2,24 +2,24 @@ from Package.BinaryPackageBase import *
 import os
 import info
 
-class subinfo(info.infoclass):
+class subinfo( info.infoclass ):
     def setTargets( self ):
         repoUrl = """http://downloads.sourceforge.net/kde-windows"""
 
-        for version in ['1.2.4-1']:
+        for version in [ '1.2.4-1' ]:
             self.targets[ version ] = self.getPackage( repoUrl, "dbus", version )
 
         self.defaultTarget = '1.2.4-1'
 
     def setDependencies( self ):
-        self.hardDependencies['gnuwin32/wget'] = 'default'
+        self.hardDependencies[ 'gnuwin32/wget' ] = 'default'
         
     def setBuildOptions( self ):
         self.disableHostBuild = False
         self.disableTargetBuild = True
 
-class Package(BinaryPackageBase):
-  def __init__(self):
+class Package( BinaryPackageBase ):
+  def __init__( self ):
     self.subinfo = subinfo()
     BinaryPackageBase.__init__( self )
 
