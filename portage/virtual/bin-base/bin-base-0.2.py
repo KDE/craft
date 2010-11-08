48ea3a87428fced0fdd55d6b007b49f94e249e7a
Ralf Habacker <ralf.habacker@freenet.de>
2010-11-08 11:28:53 +0000

    Recent binary packages includes gnuwin32/wget as implicid dependency to the development system. Adding this base package makes it able to control binary packages dependencies in on place as done with base package for source packages. When the implicid development dependency feature is completed the base and bin-base packages will not contain development package dependencies any more.
    svn path=/trunk/kdesupport/emerge/; revision=1194236

---
+++ portage/virtual/bin-base/bin-base-0.2.py
@@ -0,0 +1,20 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['0.2'] = ""
+        self.defaultTarget = '0.2'
+    
+    def setDependencies( self ):
+        if not os.getenv('EMERGE_ENABLE_IMPLICID_BUILDTIME_DEPENDENCIES'):
+            self.hardDependencies['gnuwin32/wget'] = 'default'
+
+from Package.BinaryPackageBase import *
+        
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__(self)
+    
+if __name__ == '__main__':
+    Package().execute()
