02504800f4c9fdf40f234b377b6b211209686749
Patrick Spendrin <ps_ml@gmx.de>
2010-11-09 12:38:38 +0000

    remove circular dependency for implicit dependencies
    svn path=/trunk/kdesupport/emerge/; revision=1194664

--- portage/dev-util/7zip/7zip-9.16.py
+++ portage/dev-util/7zip/7zip-9.16.py
@@ -1,11 +1,10 @@
-import base
 import info
 
-class subinfo(info.infoclass):
+class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets['9.16'] = "http://downloads.sourceforge.net/sourceforge/sevenzip/7za916.zip"
-        self.targetDigests['9.16'] = 'b389a6e2f93c18daae20393532af0e4e85ebe6f4'
-        self.targetInstallPath['9.16'] = "bin"
+        self.targets[ '9.16' ] = "http://downloads.sourceforge.net/sourceforge/sevenzip/7za916.zip"
+        self.targetDigests[ '9.16' ] = 'b389a6e2f93c18daae20393532af0e4e85ebe6f4'
+        self.targetInstallPath[ '9.16' ] = "bin"
         self.defaultTarget = '9.16'
     
     def setBuildOptions( self ):
@@ -13,16 +12,34 @@ class subinfo(info.infoclass):
         self.disableTargetBuild = True
     
     def setDependencies( self ):
-        self.hardDependencies['gnuwin32/wget']       = 'default'
+        self.hardDependencies[ 'gnuwin32/wget' ]       = 'default'
 
-from Package.BinaryPackageBase import *
+from Source.SourceBase import *
+from Package.PackageBase import *
+from BuildSystem.BinaryBuildSystem import *
 
-class Package(BinaryPackageBase):
-    def __init__( self):
+class Package( PackageBase, SourceBase, BinaryBuildSystem ):
+    def __init__( self ):
         self.subinfo = subinfo()
         self.subinfo.options.merge.ignoreBuildType = True
         self.subinfo.options.merge.destinationPath = "dev-utils"
-        BinaryPackageBase.__init__(self)
+        self.subinfo.options.install.installPath = "bin"
+        SourceBase.__init__( self )
+        PackageBase.__init__( self )
+        BinaryBuildSystem.__init__( self )
+        
+    def fetch( self ):
+        filenames = [ os.path.basename( self.subinfo.target() ) ]
+
+        if ( self.noFetch ):
+            utils.debug( "skipping fetch (--offline)" )
+            return True
+
+        self.setProxy()
+        return utils.getFiles( self.subinfo.target(), self.downloadDir() )
+
+    def unpack( self ):
+        return utils.unpackFiles( self.downloadDir(), [ os.path.basename( self.subinfo.target() ) ], self.imageDir() )
 
 if __name__ == '__main__':
     Package().execute()
