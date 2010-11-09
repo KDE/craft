4e40cc4411f5d66618e2caabaf3138b2fe4ba8be
Patrick Spendrin <ps_ml@gmx.de>
2010-11-09 12:45:00 +0000

    remove circular dependency for implicit dependencies
    svn path=/trunk/kdesupport/emerge/; revision=1194668

--- portage/dev-util/kdewin-packager/kdewin-packager-0.9.6.py
+++ portage/dev-util/kdewin-packager/kdewin-packager-0.9.6.py
@@ -7,14 +7,19 @@ class subinfo(info.infoclass):
         ## \todo specific a target independent install path option
         self.targetInstallPath['0.9.6'] = 'bin'
 
-from Package.BinaryPackageBase import *
-        
-class Package(BinaryPackageBase):
-    def __init__(self ):
+from Source.FileSource import *
+from Package.PackageBase import *
+from BuildSystem.BinaryBuildSystem import *
+
+class Package( PackageBase, FileSource, BinaryBuildSystem ):
+    def __init__( self ):
         self.subinfo = subinfo()
         self.subinfo.options.merge.ignoreBuildType = True
-        self.subinfo.options.merge.destinationPath = 'dev-utils'
-        BinaryPackageBase.__init__( self )
+        self.subinfo.options.merge.destinationPath = "dev-utils"
+        self.subinfo.options.install.installPath = "bin"
+        FileSource.__init__( self )
+        PackageBase.__init__( self )
+        BinaryBuildSystem.__init__( self )
 
 if __name__ == '__main__':
     Package().execute()
