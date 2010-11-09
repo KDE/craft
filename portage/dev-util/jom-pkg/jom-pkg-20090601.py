8534861310a6856d5b47a10f0ce3ca0aa2994b91
Patrick Spendrin <ps_ml@gmx.de>
2010-11-09 01:41:03 +0000

    build jom with cmake again, and also make a usable binary with a static Qt
    svn path=/trunk/kdesupport/emerge/; revision=1194469

--- portage/dev-util/jom-pkg/jom-pkg-20090601.py
+++ portage/dev-util/jom-pkg/jom-pkg-20090601.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import info
+import os
 from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
@@ -9,20 +10,24 @@ class subinfo(info.infoclass):
         self.svnTargets['mingw'] = "git://gitorious.org/~saroengels/qt-labs/jom-mingw.git"
         self.svnTargets['cmake'] = "git://gitorious.org/~saroengels/qt-labs/jom-cmake.git"
         self.svnTargets['static'] = "git://qt.gitorious.org/qt-labs/jom.git"
+        self.svnTargets['static-cmake'] = "git://gitorious.org/~saroengels/qt-labs/jom-cmake.git"
         self.targetSrcSuffix['cmake'] = "cmake"
         self.targetSrcSuffix['mingw'] = "mingw"
         
         self.defaultTarget = 'svnHEAD'
 
     def setDependencies( self ):
-        self.hardDependencies['virtual/base'] = 'default'
-        self.hardDependencies['testing/libantlr'] = 'default'
+        self.buildDependencies['virtual/base'] = 'default'
+        self.buildDependencies['dev-util/msys'] = 'default'  # for flex
+        self.buildDependencies['dev-util/qlalr'] = 'default'
         
 class Package(CMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        self.subinfo.options.configure.defines = "-DJOM_ENABLE_TESTS=ON"
+        self.subinfo.options.configure.defines = "-DFLEX_EXECUTABLE=" + os.path.join( os.getenv( "KDEROOT" ), "msys", "bin", "flex.exe" ) + " -DJOM_ENABLE_TESTS=ON"
+        if self.buildTarget.startswith( "static" ):
+            self.subinfo.options.configure.defines += " -DQT_QMAKE_EXECUTABLE=" + os.path.join( os.getenv( "KDEROOT" ), "qt-static", "bin", "qmake.exe" )
 
 if __name__ == '__main__':
     Package().execute()
