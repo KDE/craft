7e4a608ca5e02b8e1669746e5abe8313499f0ffa
Patrick von Reth <patrick.vonreth@gmail.com>
2010-11-09 10:01:15 +0000

    use correct base
    svn path=/trunk/kdesupport/emerge/; revision=1194537

--- portage/win32libs-sources/libbfd-src/libbfd-src-2.20.1.py
+++ portage/win32libs-sources/libbfd-src/libbfd-src-2.20.1.py
@@ -2,11 +2,6 @@
 import info
 import os
 
-from Source.MultiSource import *
-from BuildSystem.AutoToolsBuildSystem import *
-from Packager.MultiPackager import *
-from Package.PackageBase import *
-
 
 class subinfo(info.infoclass):
     def setTargets( self ):
@@ -19,14 +14,15 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.hardDependencies['virtual/base'] = 'default'
         self.hardDependencies['dev-util/msys'] = 'default'
+
         
-class Package(PackageBase, MultiSource, AutoToolsBuildSystem,  KDEWinPackager):
+        
+from Package.AutoToolsPackageBase import *
+
+class Package(AutoToolsPackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
-        PackageBase.__init__(self)
-        MultiSource.__init__(self)
-        AutoToolsBuildSystem.__init__(self)        
-        KDEWinPackager.__init__(self)
+        AutoToolsPackageBase.__init__(self)
 
 if __name__ == '__main__':
     Package().execute()
