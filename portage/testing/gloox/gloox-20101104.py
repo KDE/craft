449ca6bcf9c291c6f414fafa71d4b74e2c94db73
Patrick Spendrin <ps_ml@gmx.de>
2010-11-10 23:12:42 +0000

    add gloox, another dependency of tomahawk
    svn path=/trunk/kdesupport/emerge/; revision=1195380

---
+++ portage/testing/gloox/gloox-20101104.py
@@ -0,0 +1,27 @@
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['libs/qt'] = 'default'
+
+    def setTargets( self ):
+        for ver in ['1.0']:
+            self.targets[ ver ] = "http://camaya.net/download/gloox-" + ver + ".tar.bz2"
+            self.targetInstSrc[ ver ] = "gloox-" + ver
+        self.patchToApply['1.0'] = [('gloox-1.0-20101111.diff', 1)]
+            
+        self.targetDigests['1.0'] = '8c788738f72b05fae7c05c744a67859419ffa09c'
+
+        self.defaultTarget = '1.0'
+        self.options.configure.defines = ""
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        
+if __name__ == '__main__':
+    Package().execute()
