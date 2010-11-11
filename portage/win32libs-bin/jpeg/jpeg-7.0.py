bd82cad36a92121cd5d9544a5b0f862a839b2c60
Patrick Spendrin <ps_ml@gmx.de>
2010-11-11 10:12:21 +0000

    use automatically updated binaries from sourceforge
    svn path=/trunk/kdesupport/emerge/; revision=1195489

---
+++ portage/win32libs-bin/jpeg/jpeg-7.0.py
@@ -0,0 +1,32 @@
+# This package-script is automatically updated by the script win32libsupdater.py
+# which can be found in your emerge/bin folder. To update this package, run
+# win32libsupdater.py (and commit the results)
+# based on revision 1
+
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        repoUrl = 'http://downloads.sourceforge.net/kde-windows'
+
+        for version in [ '6.b-5', '7.0' ]:
+            self.targets[ version ] = self.getPackage( repoUrl, 'jpeg', version )
+
+        self.defaultTarget = '7.0'
+
+    def setDependencies( self ):
+        self.hardDependencies[ 'gnuwin32/wget' ] = 'default'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
