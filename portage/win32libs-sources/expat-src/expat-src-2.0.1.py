53ec96ee40be35ca22c2c3406604db1c0378c97a
Patrick Spendrin <ps_ml@gmx.de>
2010-11-12 00:56:42 +0000

    fix runtimeDependency
    svn path=/trunk/kdesupport/emerge/; revision=1195800

--- portage/win32libs-sources/expat-src/expat-src-2.0.1.py
+++ portage/win32libs-sources/expat-src/expat-src-2.0.1.py
@@ -5,7 +5,7 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setDependencies( self ):
-        self.hardDependencies['virtual/base'] = 'default'
+        self.buildDependencies['virtual/base'] = 'default'
 
     def setTargets( self ):
         self.targets['2.0.1'] = 'http://downloads.sourceforge.net/sourceforge/expat/expat-2.0.1.tar.gz'
