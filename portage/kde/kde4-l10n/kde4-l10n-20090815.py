6c85e172b2e0b3c597ea41d71f7292cf4a73428f
Ralf Habacker <ralf.habacker@freenet.de>
2010-11-15 14:47:20 +0000

    - upgraded to newest emerge api - fixed available languages
    svn path=/trunk/kdesupport/emerge/; revision=1197364

--- portage/kde/kde4-l10n/kde4-l10n-20090815.py
+++ portage/kde/kde4-l10n/kde4-l10n-20090815.py
@@ -3,25 +3,25 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = 'trunk/l10n-kde4/%s'
+        #for ver in ['4.0', '4.1', '4.2', '4.3', '4.4']:
+        #  self.targets['4.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.' + ver + '/src/kde-l10n/kde-l10n-%s-4.' + ver + '.tar.bz2'
+        #  self.targetInstSrc['4.' + ver] = 'kde-l10n-%s-4.' + ver
+
+        self.svnTargets['svnHEAD'] = 'branches/stable/l10n-kde4/%s'
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
-        self.hardDependencies['dev-util/cmake'] = 'default'
-        self.hardDependencies['dev-util/gettext-tools'] = 'default'
-        self.hardDependencies['kde/kdelibs'] = 'default'
-    
+        self.buildDependencies['dev-util/cmake'] = 'default'
+        self.buildDependencies['dev-util/gettext-tools'] = 'default'
+   
 
 from Package.CMakePackageBase import *
 
-class Package(PackageBase, SvnSource, CMakeBuildSystem, KDEWinPackager):
+class Package(CMakePackageBase):
     def __init__( self  ):
         self.subinfo = subinfo()
-        PackageBase.__init__( self )
-        SvnSource.__init__( self )
-        CMakeBuildSystem.__init__( self )
-        KDEWinPackager.__init__( self )
-        self.language = 'de'
+        CMakePackageBase.__init__( self )
+        self.language = None
         # because of the large amount of packages
         # it is very annoying to restart the build, 
         # wasting several hours, so ignore any errors 
@@ -29,84 +29,97 @@ class Package(PackageBase, SvnSource, CMakeBuildSystem, KDEWinPackager):
         self.subinfo.options.make.ignoreErrors = True
         self.subinfo.options.exitOnErrors = False
         # hardcoded for now
-        #self.subinfo.options.package.version = '4.3.63'
+        #self.subinfo.options.package.version = '4.4.63'
+        # overwrite the workDir function that is inherited from SourceBase
+        self.source.workDir = self.workDir.__get__(self, Package)
+        self.source.localFileNames = self.localFileNames.__get__(self, Package)
+
+    def localFileNames(self):
+        filenames = []
+        for i in range( self.repositoryUrlCount() ):
+            filenames.append( os.path.basename( self.repositoryUrl( i ) ) )
+        return filenames
+
+    def configureSourceDir(self):
+        return CMakePackageBase.configureSourceDir( self ) % self.language
+        
+    def sourceDir(self):
+        return CMakePackageBase.sourceDir( self ) % self.language
 
     def repositoryUrl(self,index=0):
         # \todo we cannot use CMakePackageBase here because repositoryPath 
         # then is not be overrideable for unknown reasons 
-        url = SvnSource.repositoryUrl(self,index) % self.language
+        url = CMakePackageBase.repositoryUrl(self,index) % self.language
         return url
 
-    def checkoutDir(self,index=0):
-        dir = SvnSource.checkoutDir(self,index) % self.language
-        return dir
-        
-    def buildRoot(self):
-        dir = os.path.join(PackageBase.buildRoot(self), self.language)
+    def workDir(self):
+        dir = os.path.join(CMakePackageBase.workDir(self), self.language)
         return dir
 
-    def qmerge(self):
-        ''' When crosscompiling install l10n files
-            also into the targets directory '''
-        ret = PackageBase.qmerge(self)
-        if emergePlatform.isCrossCompilingEnabled():
-            utils.copyDir(self.imageDir(),
-                    os.path.join(self.rootdir,
-                    os.environ["EMERGE_TARGET_PLATFORM"]))
-        return ret
+    def installDir(self):
+        dir = os.path.join(CMakePackageBase.installDir(self), self.language)
+        return dir
+
+    def fetch(self):
+        filenames = self.localFileNames()
+
+        if ( self.source.noFetch ):
+            utils.debug( "skipping fetch (--offline)" )
+            return True
+
+        self.source.setProxy()
+        if self.subinfo.hasTarget():
+            return utils.getFiles( self.subinfo.target() % self.language, self.downloadDir() )
+        else:
+            return utils.getFiles( "", self.downloadDir() )
 
     def unpack(self):
         autogen = os.path.join( self.packageDir() , "autogen.py" )
-        if not SvnSource.unpack(self):
-            return False
-        # execute autogen.py and generate the CMakeLists.txt files
-        # Possible autogen options are --disable-messages,
-        # --disable-docs, --disable-data and --disable-scripts
-        cmd = [ "python", autogen, self.language ]
-        if emergePlatform.isCrossCompilingEnabled():
-            cmd.append( "--disable-docs" )
-        return self.system( cmd, cwd=os.path.join( self.sourceDir(), ".." ) )
 
-    def configure(self):
-        if not os.path.exists(os.path.join(self.buildDir(),"CMakeCache.txt")):
-            return CMakeBuildSystem.configure(self)
-        return True
-        
+        filenames = self.localFileNames()
+
+        # the following code is taken from ArchiveSource::unpack() function
+        # it will not remove already existing files
+        destdir = self.workDir()
+        utils.debug( "unpacking files into work root %s" % destdir, 1 )
+
+        if not utils.unpackFiles( self.downloadDir(), filenames, destdir ):
+            return False
+
+        # execute autogen.py and generate the CMakeLists.txt files
+        cmd = "cd %s && python %s %s %s" % \
+              ('..', autogen, self.sourceDir(), self.language )
+        return self.system( cmd )
+
     def createPackage(self):
-        self.subinfo.options.package.packageName = 'l10n-kde4-%s' % self.language
+        self.subinfo.options.package.packageName = 'kde4-l10n-%s' % self.language
         self.subinfo.options.package.withCompiler = False
-        return KDEWinPackager.createPackage(self)
+        return CMakePackageBase.createPackage(self)
         
         
 class MainInfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/l10n-kde4/scripts'
         self.defaultTarget = 'svnHEAD'
-        # all targets in svn
-        self.languages  = 'af ar be bg bn bn_IN br ca cs csb cy da de '
-        self.languages += 'el en_GB eo es et eu fa fi fr fy ga gl gu '
-        self.languages += 'ha he hi hr hsb hu hy is it ja ka kk km kn ko ku '
-        self.languages += 'lb lt lv mk ml ms mt nb nds ne nl nn nso oc '
-        self.languages += 'pa pl pt pt_BR ro ru rw se sk sl sr sv '
-        self.languages += 'ta te tg th tr uk uz vi wa xh zh_CN zh_HK zh_TW '
+        self.languages = dict()
+
+        self.languages['svnHEAD'] = 'ar bg ca ca@valencia cs csb da de el en_GB eo es et eu fi fr fy ga'
+        self.languages['svnHEAD'] += ' gl gu he hi hr hu id is it ja kk km kn ko lt lv mai mk ml nb nds' 
+        self.languages['svnHEAD'] += ' nl nn pa pl pt pt_BR ro ru si sk sl sr sv tg tr uk wa zh_CN zh_TW'
 
         #for testing
-        self.languages  = 'de'
+        #self.languages  = 'de'
     
     def setDependencies( self ):
-        self.hardDependencies['dev-util/cmake'] = 'default'
-        self.hardDependencies['dev-util/gettext-tools'] = 'default'
-        self.hardDependencies['kde/kdelibs'] = 'default'
-
-    def setBuildOptions( self ):
-        self.disableHostBuild = False
-        self.disableTargetBuild = True
-
-class MainPackage(PackageBase):
+        self.buildDependencies['dev-util/cmake'] = 'default'
+        self.buildDependencies['dev-util/gettext-tools'] = 'default'
+    
+class MainPackage(CMakePackageBase):
     def __init__( self  ):
         self.subinfo = MainInfo()
-        PackageBase.__init__( self )
-        self.kde4_l10n = portage.getPackageInstance('kde','kde4-l10n')
+        CMakePackageBase.__init__( self )
+        self.kde4_l10n = Package()
+
         # set to any language to start building from 
         ## \todo when emerge.py is able to provide command line options to us
         # it would be possible to set this from command line 
@@ -114,15 +127,17 @@ class MainPackage(PackageBase):
         
     def execute(self):
         (command, option) = self.getAction()
-        if self.isTargetBuild(): return True
         self.errors = dict()
         ## \todo does not work yet see note in PackageBase::getAction()
-        if option <> None:
-            languages = option.split()
+#        if option <> None:
+#            languages = option.split()
+#        else:
+        if self.subinfo.languages[self.buildTarget]: 
+            languages = self.subinfo.languages[self.buildTarget].split()
         else:
-            languages = self.subinfo.languages.split()
+            languages = self.subinfo.languages['svnHEAD'].split()
+        
         found=None
-            
         for language in languages:
             if not found and self.startLanguage:
                 if self.startLanguage <> language:
@@ -131,10 +146,12 @@ class MainPackage(PackageBase):
                     found = True
             
             self.kde4_l10n.language = language
-            if not self.kde4_l10n.runAction(command):
-                self.errors["%s-%s" % (language, command)] = 1
+            utils.debug("current language: %s current command: %s" % (self.kde4_l10n.language,command), 1)
+            self.kde4_l10n.runAction(command)
+#                self.errors["%s-%s" % (language, command)] = 1
 
-        print self.errors
+        if self.errors:
+            utils.debug("Errors that happened while executing last command: %s" % self.errors, 2)
         return True    
         
 if __name__ == '__main__':
