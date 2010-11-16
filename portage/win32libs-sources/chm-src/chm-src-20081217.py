9a4b90c13f43a981f3bc0fb15d8f267c7425ac4f
Patrick Spendrin <ps_ml@gmx.de>
2010-11-16 09:13:41 +0000

    add chm-src (library for reading windows chm files) to win32libs builds
    svn path=/trunk/kdesupport/emerge/; revision=1197602

--- portage/win32libs-sources/chm-src/chm-src-20081217.py
+++ portage/win32libs-sources/chm-src/chm-src-20081217.py
@@ -1,47 +1,25 @@
-# -*- coding: utf-8 -*-
-import base
-import utils
-import os
-import shutil
 import info
-
 
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['0.40'] = 'http://www.jedrea.com/chmlib/chmlib-0.40.tar.bz2'
+        self.patchToApply['0.40'] = ('chm-cmake.diff', 0)
         self.targetInstSrc['0.40'] = 'chmlib-0.40'
+        self.targetDigests['0.40'] = '5231d7531e8808420d7f89fd1e4fdbac1ed7a167'
         self.defaultTarget = '0.40'
     
     def setDependencies( self ):
-        self.hardDependencies['virtual/base'] = 'default'
+        self.buildDependencies['virtual/base'] = 'default'
         
-class subclass(base.baseclass):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
         self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
 
-    def unpack( self ):
-        if not base.baseclass.unpack( self ):
-            return False
-
-        os.chdir( self.workdir )
-        print os.path.join( self.workdir, self.instsrcdir )
-        if self.buildTarget == '0.40':
-            self.system( "cd %s && patch -p0 < %s" % ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir, "chm-cmake.diff" ) ) )
-        
-        return True
-        
-    def compile( self ):
         # building examples and debugging tools
-        self.kdeCustomDefines = "-DBUILD_examples=OFF"
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-        return self.doPackaging( "chm", self.buildTarget, True )
-
+        self.subinfo.options.configure.defines = "-DBUILD_examples=OFF"
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
