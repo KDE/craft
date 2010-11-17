4044b348c823f7c0e725e565b8b1a3f435bfcb6e
Patrick Spendrin <ps_ml@gmx.de>
2010-11-17 03:16:41 +0000

    update libspectre version, fix building & make it work spectre-test.exe has QImage based output now, and shows that spectre works!
    svn path=/trunk/kdesupport/emerge/; revision=1197931

---
+++ portage/testing/libspectre-src/libspectre-src-0.2.6.py
@@ -0,0 +1,26 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        """ """
+        for ver in ['0.2.1', '0.2.6']:
+            self.targets[ver] = "http://libspectre.freedesktop.org/releases/libspectre-" + ver + ".tar.gz"
+            self.targetInstSrc[ver] = "libspectre-" + ver
+        self.patchToApply["0.2.1"] = ("spectre-0.2.1-cmake.diff", 1)
+        self.patchToApply["0.2.6"] = ("libspectre-0.2.6-20101117.diff", 1)
+        self.targetDigests['0.2.6'] = '819475c7e34a1e9bc2e876110fee530b42aecabd'
+        self.defaultTarget = '0.2.6'
+
+    def setDependencies( self ):
+        #self.hardDependencies['testing/libgs'] = 'default'
+        pass
+        
+from Package.CMakePackageBase import *
+        
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
