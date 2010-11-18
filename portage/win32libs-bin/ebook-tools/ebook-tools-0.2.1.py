786f16af25be94e5aa1854e001f2c771065c2c22
Patrick Spendrin <ps_ml@gmx.de>
2010-11-18 01:57:42 +0000

    update ebook-tools
    svn path=/trunk/kdesupport/emerge/; revision=1198273

---
+++ portage/win32libs-bin/ebook-tools/ebook-tools-0.2.1.py
@@ -0,0 +1,38 @@
+# This package-script is automatically updated by the script win32libsupdater.py
+# which can be found in your emerge/bin folder. To update this package, run
+# win32libsupdater.py (and commit the results)
+# based on revision 1
+
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        repoUrl = 'http://downloads.sourceforge.net/kde-windows'
+
+        for version in [ '0.1.0', '0.1.1', '0.2.1' ]:
+            self.targets[ version ]          = self.getPackage( repoUrl, 'ebook-tools', version )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'ebook-tools', version , '.tar.bz2.sha1' )
+
+        self.defaultTarget = '0.2.1'
+
+
+    def setDependencies( self ):
+        if not os.getenv( 'EMERGE_ENABLE_IMPLICID_BUILDTIME_DEPENDENCIES' ):
+            self.buildDependencies[ 'gnuwin32/wget' ] = 'default'
+        self.runtimeDependencies[ 'win32libs-bin/libzip' ] = 'default'
+        self.runtimeDependencies[ 'win32libs-bin/libxml2' ] = 'default'
+
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
