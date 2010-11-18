284bfe0821bc98376da8f41159bea712220ad40b
Patrick Spendrin <ps_ml@gmx.de>
2010-11-18 02:21:57 +0000

    fix dependencies
    svn path=/trunk/kdesupport/emerge/; revision=1198277

--- portage/testing/mysql-pkg/mysql-pkg-5.1.46.py
+++ portage/testing/mysql-pkg/mysql-pkg-5.1.46.py
@@ -25,7 +25,7 @@ class subinfo(info.infoclass):
        
 
     def setDependencies( self ):
-        self.hardDependencies['virtual/bin-base'] = 'default'
+        self.buildDependencies['virtual/bin-base'] = 'default'
         
         
 class Package(CMakePackageBase):
