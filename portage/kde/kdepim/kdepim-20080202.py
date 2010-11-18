ef10790e0be17882a70ebbadfab75bc88aff973c
Andreas Holzammer <andreas.holzammer@kdab.com>
2010-11-18 16:17:38 +0000

    - enable build kleopatra for wince
    svn path=/trunk/kdesupport/emerge/; revision=1198480

--- portage/kde/kdepim/kdepim-20080202.py
+++ portage/kde/kdepim/kdepim-20080202.py
@@ -45,7 +45,7 @@ class Package(CMakePackageBase):
             self.subinfo.options.configure.defines += " -DBUILD_kmail=ON "
             self.subinfo.options.configure.defines += " -DKDEPIM_NO_NEPOMUK=ON "
             self.subinfo.options.configure.defines += " -DKDE4_BUILD_TESTS=OFF "
-            # self.subinfo.options.configure.defines += " -DBUILD_kleopatra=ON "
+            self.subinfo.options.configure.defines += " -DBUILD_kleopatra=ON "
 
         self.subinfo.options.configure.defines += "-DHOST_BINDIR=%s " \
             % os.path.join(ROOTDIR, "bin")
