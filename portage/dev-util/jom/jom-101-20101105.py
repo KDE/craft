e08839ad30d1ebbea4692d0ef6f3536637cb6c5d
Patrick Spendrin <ps_ml@gmx.de>
2010-11-19 09:45:18 +0000

    add jom version 102 - it should contain the fixes for mingw, please try this out! CCMAIL: kde-windows@kde.org
    svn path=/trunk/kdesupport/emerge/; revision=1198692

--- portage/dev-util/jom/jom-101-20101105.py
+++ portage/dev-util/jom/jom-101-20101105.py
@@ -10,11 +10,12 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.targets['HEAD'] = 'ftp://ftp.qt.nokia.com/jom/jom.zip'
-        for ver in ['094', '100', '101']:
+        for ver in ['094', '100', '101', '102']:
             self.targets[ver] = 'ftp://ftp.qt.nokia.com/jom/jom' + ver + '.zip'
         self.targetDigests['094'] = '1f946283866cd6f40a5888088f6c7d840b62af2d'
         self.targetDigests['100'] = '545e964c606d28edce582f167574298589970fb4'
         self.targetDigests['101'] = '3cbc2750a8a0b8c736a10c7445b0b92ecf247292'
+        self.targetDigests['102'] = '9cda65f2de954a3236d79f6bc72f5561da32163f'
         self.targets['101-patched'] = 'http://www.winkde.org/pub/kde/ports/win32/repository/other/jom101-patched.7z'
         self.targetDigests['101-patched'] = '5f878e50cdd05f390b2737d4050a740edd48337f'
         self.defaultTarget = '094'
