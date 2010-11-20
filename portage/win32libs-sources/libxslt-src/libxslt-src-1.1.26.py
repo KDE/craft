7147386bb6de99c21df67c39e545931f4ed98b79
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-11-20 21:23:43 +0000

    use source deps for source packages
    svn path=/trunk/kdesupport/emerge/; revision=1199154

--- portage/win32libs-sources/libxslt-src/libxslt-src-1.1.26.py
+++ portage/win32libs-sources/libxslt-src/libxslt-src-1.1.26.py
@@ -14,7 +14,7 @@ class subinfo(info.infoclass):
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
-        self.dependencies['win32libs-bin/libxml2'] = 'default'
+        self.dependencies['win32libs-sources/libxml2-src'] = 'default'
 
     def setBuildOptions( self ):
         self.disableHostBuild = False
