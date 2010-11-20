d7423090cc3d5298d16ce1751fa251d25dfb578a
Patrick Spendrin <ps_ml@gmx.de>
2010-11-20 01:01:04 +0000

    add assuan2 and gpgme builds from ppo
    svn path=/trunk/kdesupport/emerge/; revision=1198866

---
+++ portage/win32libs-sources/assuan2-src/assuan2-src-2.0.0.py
@@ -0,0 +1,28 @@
+import utils
+import os
+import info
+import platform
+import compiler
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['2.0.1'] = 'ftp://ftp.gnupg.org/gcrypt/libassuan/libassuan-2.0.1.tar.bz2'
+        self.targetInstSrc['2.0.1'] = 'libassuan-2.0.1'
+        self.targetDigests['2.0.1'] = 'b7e9dbd41769cc20b1fb7db9f2ecdf276ffc352c'
+        self.patchToApply['2.0.1'] = [ ('assuan-381-head.diff', 0), ('libassuan-2.0.1-20101029.diff', 1) ]
+        self.defaultTarget = '2.0.1'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['win32libs-sources/gpg-error-src'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+
+if __name__ == '__main__':
+    Package().execute()
---
+++ portage/win32libs-sources/gpgme-src/gpgme-src-1.3.0.py
@@ -0,0 +1,29 @@
+import utils
+import os
+import info
+import platform
+import compiler
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['1.3.0'] = 'ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-1.3.0.tar.bz2'
+        self.targetInstSrc['1.3.0'] = 'gpgme-1.3.0'
+        self.targetDigests['1.3.0'] = '0db69082abfbbbaf86c3ab0906f5137de900da73'
+        self.patchToApply['1.3.0'] = [('gpgme-1447-HEAD.diff', 0), ('gpgme-1.3.0-cmake.diff', 1), ('gpgme-1.3.0-20101102.diff', 1)]
+        self.defaultTarget = '1.3.0'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.hardDependencies['win32libs-sources/gpg-error-src'] = 'default'
+        self.hardDependencies['win32libs-sources/assuan2-src'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+
+if __name__ == '__main__':
+    Package().execute()
