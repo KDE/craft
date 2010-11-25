a72027192557974d023e059ae9b83c57e263fa9e
Patrick Spendrin <ps_ml@gmx.de>
2010-11-25 14:17:40 +0000

    backport from libfftw-src
    svn path=/trunk/kdesupport/emerge/; revision=1200692

--- portage/win32libs-sources/shared-mime-info-src/CheckMingwVersion.cmake
+++ portage/win32libs-sources/shared-mime-info-src/CheckMingwVersion.cmake
@@ -7,15 +7,20 @@
 #
 if (NOT MINGW32 AND NOT MINGW64)
     exec_program(
-        gcc
-        ARGS -dumpmachine
+        ${CMAKE_C_COMPILER}
+        ARGS ${CMAKE_C_COMPILER_ARG1} -dumpmachine
         OUTPUT_VARIABLE _machine
     )
     if (_machine STREQUAL mingw32)
         set (MINGW32 1)
         message(STATUS "found mingw 32 bit compiler")
+    elseif (_machine STREQUAL i686-w64-mingw32)
+        set (MINGW32 1)
+        set (MINGW_W32 1)
+        message(STATUS "found mingw 64 bit compiler")
     elseif (_machine STREQUAL x86_64-w64-mingw32)
         set (MINGW64 1)
+        set (MINGW_W64 1)
         message(STATUS "found mingw 64 bit compiler")
     endif (_machine STREQUAL mingw32)
 endif (NOT MINGW32 AND NOT MINGW64)
