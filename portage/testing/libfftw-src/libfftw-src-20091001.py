36e9edd5cddb8c237703aa9e5dba8806c602a912
Patrick Spendrin <ps_ml@gmx.de>
2010-11-25 02:07:09 +0000

    fix it at least for mingw32, for msvc a linker error will happen if you build benchmarks.
    svn path=/trunk/kdesupport/emerge/; revision=1200456

--- portage/testing/libfftw-src/libfftw-src-20091001.py
+++ portage/testing/libfftw-src/libfftw-src-20091001.py
@@ -19,6 +19,7 @@ class Package( CMakePackageBase ):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DFFTW_SINGLE=ON -DFFTW_DOUBLE=OFF -DBUILD_BENCHMARKS=OFF"
         
 if __name__ == '__main__':
      Package().execute()
