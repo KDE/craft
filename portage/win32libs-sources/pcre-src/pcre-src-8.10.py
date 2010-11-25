e55bba3a15fa1e2b83d85cc9c6be95c99820aa32
Patrick Spendrin <ps_ml@gmx.de>
2010-11-25 23:05:51 +0000

    correct man page installation destination
    svn path=/trunk/kdesupport/emerge/; revision=1200839

--- portage/win32libs-sources/pcre-src/pcre-src-8.10.py
+++ portage/win32libs-sources/pcre-src/pcre-src-8.10.py
@@ -1,20 +1,24 @@
 import info
 from Package.CMakePackageBase import *
 
-class subinfo(info.infoclass):
+class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in ['7.9', '8.00', '8.02', '8.10']:
-          self.targets[ver] = 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-' + ver + '.tar.bz2'
-          self.targetInstSrc[ver] = 'pcre-' + ver
+        for ver in [ '7.9', '8.00', '8.02', '8.10' ]:
+            self.targets[ ver ] = 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-' + ver + '.tar.bz2'
+            self.targetInstSrc[ ver ] = 'pcre-' + ver
+        self.patchToApply[ '8.10' ] = [ ( "pcre-8.10-20101125.diff", 1 ) ]
         if emergePlatform.isCrossCompilingEnabled():
-          self.patchToApply['8.10'] = ("pcre-8.02-20100518.diff", 1)
-        self.defaultTarget = '8.10'
-    def setDependencies( self ):
-        self.buildDependencies['virtual/base'] = 'default'
-        self.dependencies['win32libs-bin/libbzip2'] = 'default'
-        self.dependencies['win32libs-bin/zlib'] = 'default'
+            self.patchToApply[ '8.10' ].append( ( "pcre-8.02-20100518.diff", 1 ) )
 
-class Package(CMakePackageBase):
+        self.targetDigests[ '8.10' ] = '8b345da0f835b2caabff071b0b5bab40564652be'
+        self.defaultTarget = '8.10'
+
+    def setDependencies( self ):
+        self.buildDependencies[ 'virtual/base' ] = 'default'
+        self.dependencies[ 'win32libs-bin/libbzip2' ] = 'default'
+        self.dependencies[ 'win32libs-bin/zlib' ] = 'default'
+
+class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
@@ -27,7 +31,6 @@ class Package(CMakePackageBase):
             defines += "-DPCRE_BUILD_TESTS=OFF "
             defines += "-DPCRE_BUILD_PCREGREP=OFF "
         self.subinfo.options.configure.defines = defines
-        self.subinfo.options.package.packageName = 'pcre'
 
 if __name__ == '__main__':
     Package().execute()
