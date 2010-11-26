2e7bc6487d056fe379819627c6ff77c99992740a
Patrick Spendrin <ps_ml@gmx.de>
2010-11-26 18:25:35 +0000

    add binaries for fftw, lastfm and samplerate
    svn path=/trunk/kdesupport/emerge/; revision=1201092

---
+++ portage/win32libs-bin/libfftw/libfftw-3.2.2.py
@@ -0,0 +1,36 @@
+# This package-script is automatically updated by the script win32libsupdater.py
+# which can be found in your emerge/bin folder. To update this package, run
+# win32libsupdater.py (and commit the results)
+# based on revision 1
+
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        repoUrl = 'http://downloads.sourceforge.net/kde-windows'
+
+        for version in [ '3.2.2' ]:
+            self.targets[ version ]          = self.getPackage( repoUrl, 'libfftw', version )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'libfftw', version , '.tar.bz2.sha1' )
+
+        self.defaultTarget = '3.2.2'
+
+
+    def setDependencies( self ):
+        if not os.getenv( 'EMERGE_ENABLE_IMPLICID_BUILDTIME_DEPENDENCIES' ):
+            self.buildDependencies[ 'gnuwin32/wget' ] = 'default'
+
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
---
+++ portage/win32libs-bin/liblastfm/liblastfm-gitHEAD.py
@@ -0,0 +1,39 @@
+# This package-script is automatically updated by the script win32libsupdater.py
+# which can be found in your emerge/bin folder. To update this package, run
+# win32libsupdater.py (and commit the results)
+# based on revision 1
+
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        repoUrl = 'http://downloads.sourceforge.net/kde-windows'
+
+        for version in [ 'gitHEAD' ]:
+            self.targets[ version ]          = self.getPackage( repoUrl, 'liblastfm', version )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'liblastfm', version , '.tar.bz2.sha1' )
+
+        self.defaultTarget = 'gitHEAD'
+
+
+    def setDependencies( self ):
+        if not os.getenv( 'EMERGE_ENABLE_IMPLICID_BUILDTIME_DEPENDENCIES' ):
+            self.buildDependencies[ 'gnuwin32/wget' ] = 'default'
+        self.runtimeDependencies[ 'win32libs-bin/libsamplerate' ] = 'default'
+        self.runtimeDependencies[ 'libs/qt' ] = 'default'
+        self.runtimeDependencies[ 'win32libs-bin/libfftw' ] = 'default'
+
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
