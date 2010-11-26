da634f75489dc945829befb6b428439a67937b0c
Patrick Spendrin <ps_ml@gmx.de>
2010-11-26 23:44:32 +0000

    fix typo
    svn path=/trunk/kdesupport/emerge/; revision=1201155

--- portage/win32libs-sources/libsndfile-src/libsndfile-src-20100707.py
+++ portage/win32libs-sources/libsndfile-src/libsndfile-src-20100707.py
@@ -4,20 +4,20 @@ from Package.CMakePackageBase import *
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets['1.0.21'] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.21.tar.gz'
-        self.targetInstSrc['1.0.21'] = 'libsndfile-1.0.21'
-        self.patchToApply['1.0.21'] = ( 'libsndfile-1.0.21-20100708.diff', 1 )
+        self.targets[ '1.0.21' ] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.21.tar.gz'
+        self.targetInstSrc[ '1.0.21' ] = 'libsndfile-1.0.21'
+        self.patchToApply[ '1.0.21' ] = ( 'libsndfile-1.0.21-20100708.diff', 1 )
         self.defaultTarget = '1.0.21'
 
     def setDependencies( self ):
-        self.buildDependencies['virtual/base'] = 'default'
-        self.dependencies['win32libs-bin/libogg'] = 'default'
-        self.dependencies['win32libs-bin/libvorbis'] = 'default'
+        self.buildDependencies[ 'virtual/base' ] = 'default'
+        self.dependencies[ 'win32libs-bin/libogg' ] = 'default'
+        self.dependencies[ 'win32libs-bin/libvorbis' ] = 'default'
         
 class Package( CMakePackageBase ):
     def __init__( self ):
         self.subinfo = subinfo()
-        CMakePackageBase.__init__( sel f)
+        CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
     Package().execute()
