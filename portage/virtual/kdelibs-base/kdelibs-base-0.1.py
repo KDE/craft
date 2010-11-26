0095252668baeba30a1da205acac6d6d1c4f578b
Patrick Spendrin <ps_ml@gmx.de>
2010-11-26 16:09:21 +0000

    update dependencies
    svn path=/trunk/kdesupport/emerge/; revision=1201052

--- portage/virtual/kdelibs-base/kdelibs-base-0.1.py
+++ portage/virtual/kdelibs-base/kdelibs-base-0.1.py
@@ -8,32 +8,29 @@ class subinfo(info.infoclass):
         self.defaultTarget = '0.1'
     
     def setDependencies( self ):
-        self.hardDependencies['win32libs-bin/aspell']  = 'default'
-#        self.hardDependencies['win32libs-bin/enchant']  = 'default'
-        self.hardDependencies['win32libs-bin/gettext']  = 'default'
-        self.hardDependencies['win32libs-bin/giflib']  = 'default'
-#        self.hardDependencies['win32libs-bin/gssapi']  = 'default'
-#        self.hardDependencies['win32libs-bin/hspell']  = 'default'
+        self.dependencies['win32libs-bin/aspell']  = 'default'
+#        self.dependencies['win32libs-bin/enchant']  = 'default'
+        self.dependencies['win32libs-bin/gettext']  = 'default'
+        self.dependencies['win32libs-bin/giflib']  = 'default'
+#        self.dependencies['win32libs-bin/gssapi']  = 'default'
+#        self.dependencies['win32libs-bin/hspell']  = 'default'
         if not emergePlatform.isCrossCompilingEnabled():
-            self.hardDependencies['win32libs-bin/jasper']  = 'default'
-        self.hardDependencies['win32libs-bin/jpeg']  = 'default'
-        self.hardDependencies['win32libs-bin/libbzip2']  = 'default'
-        self.hardDependencies['win32libs-bin/libpng']  = 'default'
-        self.hardDependencies['win32libs-bin/libxml2']  = 'default'
-        self.hardDependencies['win32libs-bin/libxslt']  = 'default'
-#        self.hardDependencies['win32libs-bin/openexr']  = 'default'
-        self.hardDependencies['win32libs-bin/openssl']  = 'default'
-        self.hardDependencies['win32libs-bin/pcre']  = 'default'
-        self.hardDependencies['win32libs-bin/shared-mime-info']  = 'default'
-        self.hardDependencies['win32libs-bin/zlib']  = 'default'
+            self.dependencies['win32libs-bin/jasper']  = 'default'
+        self.dependencies['win32libs-bin/jpeg']  = 'default'
+        self.dependencies['win32libs-bin/libbzip2']  = 'default'
+        self.dependencies['win32libs-bin/libpng']  = 'default'
+        self.dependencies['win32libs-bin/libxml2']  = 'default'
+        self.dependencies['win32libs-bin/libxslt']  = 'default'
+#        self.dependencies['win32libs-bin/openexr']  = 'default'
+        self.dependencies['win32libs-bin/openssl']  = 'default'
+        self.dependencies['win32libs-bin/pcre']  = 'default'
+        self.dependencies['win32libs-bin/shared-mime-info']  = 'default'
+        self.dependencies['win32libs-bin/zlib']  = 'default'
     
 class Package( VirtualPackageBase ):
     def __init__( self ):
         self.subinfo = subinfo()
         VirtualPackageBase.__init__( self )
-
-    def unpack( self ):
-        return True
 
 if __name__ == '__main__':
     Package().execute()
