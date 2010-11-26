379f93d360a41f3d6a0e8edee9dbe8c69668646b
Patrick Spendrin <ps_ml@gmx.de>
2010-11-26 23:15:00 +0000

    move libogg and libvorbis, fix dependencies for libsndfile
    svn path=/trunk/kdesupport/emerge/; revision=1201144

--- portage/win32libs-sources/libogg-src/libogg-src-20100707.py
+++ portage/win32libs-sources/libogg-src/libogg-src-20100707.py
@@ -5,17 +5,16 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.0.21'] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.21.tar.gz'
-        self.targetInstSrc['1.0.21'] = 'libsndfile-1.0.21'
-        self.patchToApply['1.0.21'] = ('libsndfile-1.0.21-20100708.diff', 1)
+        self.targets['1.2.0'] = 'http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz'
+        self.targetInstSrc['1.2.0'] = 'libogg-1.2.0'
+        self.patchToApply['1.2.0'] = ('libogg-1.2.0-20100707.diff', 1)
+        self.targetDigests['1.2.0'] = '135fb812282e08833295c91e005bd0258fff9098'
         self.options.package.withCompiler = False
-        self.options.package.packageName = "libsndfile"
-        self.defaultTarget = '1.0.21'
+        self.options.package.packageName = "libogg"
+        self.defaultTarget = '1.2.0'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
-        self.dependencies['win32libs-bin/libogg'] = 'default'
-        self.dependencies['win32libs-bin/libvorbis'] = 'default'
         
 class Package(CMakePackageBase):
     def __init__( self, **args ):
