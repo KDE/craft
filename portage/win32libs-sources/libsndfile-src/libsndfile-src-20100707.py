3741f326063872e26eba5a23688fd378e1beb166
Patrick Spendrin <ps_ml@gmx.de>
2010-11-27 02:24:31 +0000

    cleanup libsndfile so that it builds more or less with mingw4
    svn path=/trunk/kdesupport/emerge/; revision=1201184

--- portage/win32libs-sources/libsndfile-src/libsndfile-src-20100707.py
+++ portage/win32libs-sources/libsndfile-src/libsndfile-src-20100707.py
@@ -6,7 +6,8 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets[ '1.0.21' ] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.21.tar.gz'
         self.targetInstSrc[ '1.0.21' ] = 'libsndfile-1.0.21'
-        self.patchToApply[ '1.0.21' ] = ( 'libsndfile-1.0.21-20100708.diff', 1 )
+        self.patchToApply[ '1.0.21' ] = ( 'libsndfile-1.0.21-20101127.diff', 1 )
+        self.targetDigests[ '1.0.21' ] = '136845a8bb5679e033f8f53fb98ddeb5ee8f1d97'
         self.defaultTarget = '1.0.21'
 
     def setDependencies( self ):
