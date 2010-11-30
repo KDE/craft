4ecbcd071d4427c3ad573a9e856dd1cb12194b0c
Patrick Spendrin <ps_ml@gmx.de>
2010-11-30 09:15:47 +0000

    add digest
    svn path=/trunk/kdesupport/emerge/; revision=1202248

--- portage/win32libs-sources/libarchive-src/libarchive-src-2.7.0.py
+++ portage/win32libs-sources/libarchive-src/libarchive-src-2.7.0.py
@@ -5,6 +5,7 @@ class subinfo( info.infoclass ):
         for v in [ '2.7.0', '2.8.4' ]:
             self.targets[ v ] = 'http://libarchive.googlecode.com/files/libarchive-' + v + '.tar.gz'
             self.targetInstSrc[ v ] = 'libarchive-' + v
+        self.targetDigests['2.8.4'] = 'b9cc3bbd20bd71f996be9ec738f19fda8653f7af'
         self.shortDescription = "C library and command-line tools for reading and writing tar, cpio, zip, ISO, and other archive formats"
         self.defaultTarget = '2.8.4'
     
