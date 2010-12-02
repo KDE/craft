447f4882441101cfecb2ba69c84760d6bd698503
Patrick von Reth <patrick.vonreth@gmail.com>
2010-12-02 15:46:33 +0000

    use 4.4.3 as default tag, also fixed cmake defines
    svn path=/trunk/kdesupport/emerge/; revision=1202874

--- portage/kdesupport/phonon/phonon-4.4.3.20101202.py
+++ portage/kdesupport/phonon/phonon-4.4.3.20101202.py
@@ -11,11 +11,11 @@ class subinfo(info.infoclass):
         self.svnTargets['gitHEAD'] = 'git://gitorious.org/phonon/phonon.git'
         self.svnTargets['4.4'] = 'git://gitorious.org/phonon/phonon.git|4.4'
         self.patchToApply['gitHEAD'] = ("phonon-20100915.diff", 1)
-        self.defaultTarget = 'gitHEAD'
+        self.targets['4.4.3'] = 'http://download.kde.org/download.php?url=stable/phonon/4.4.3/phonon-4.4.3.tar.bz2'
+        self.targetInstSrc['4.4.3'] = 'phonon-4.4.3'
+        self.targetDigests['4.4.3'] = '50262d590beb648be9dcad6b913b920db19a84f8'
+        self.defaultTarget = '4.4.3'
         
-        self.options.configure.defines = "-DPHONON_BUILD_EXAMPLES=OFF -DPHONON_BUILD_TESTS=OFF"
-        if COMPILER == "mingw4":
-            self.options.configure.defines +=" -DBUILD_PHONON_DS9=OFF "
           
     def setBuildOptions( self ):
         self.disableHostBuild = False
@@ -27,7 +27,7 @@ class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        self.subinfo.options.configure.defines = ""
+        self.subinfo.options.configure.defines = "-DPHONON_BUILD_EXAMPLES=OFF -DPHONON_BUILD_TESTS=OFF "
             
         if self.isTargetBuild():
             automoc = os.path.join(self.rootdir, "lib", "automoc4", "Automoc4Config.cmake")
