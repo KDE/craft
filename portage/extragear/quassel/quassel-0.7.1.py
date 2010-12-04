b8706ae8612dfe0822e1fd3f9a5a41f3f2574544
Patrick von Reth <patrick.vonreth@gmail.com>
2010-12-04 13:58:27 +0000

    added 0.7.1 target
    svn path=/trunk/kdesupport/emerge/; revision=1203537

--- portage/extragear/quassel/quassel-0.7.1.py
+++ portage/extragear/quassel/quassel-0.7.1.py
@@ -8,6 +8,9 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = 'git://gitorious.org/quassel/quassel.git'
         self.svnTargets['0.6'] = 'git://gitorious.org/quassel/quassel.git|0.6|'
+        self.targets['0.7.1'] = 'http://quassel-irc.org/pub/quassel-0.7.1.tar.bz2'
+        self.targetDigests['0.7.1'] = '791086da977033a1bbee3effa317668b3726bd7f'
+        self.targetInstSrc['0.7.1'] = 'quassel-0.7.1'
         self.defaultTarget = 'gitHEAD'
     
     def setDependencies( self ):
