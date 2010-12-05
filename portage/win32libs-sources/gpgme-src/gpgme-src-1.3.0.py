273d99a5a8e9e169d2ac8cee688e7e798506446f
Patrick Spendrin <ps_ml@gmx.de>
2010-12-05 00:26:40 +0000

    base our build on the latest trunk tarball
    svn path=/trunk/kdesupport/emerge/; revision=1203659

--- portage/win32libs-sources/gpgme-src/gpgme-src-1.3.0.py
+++ portage/win32libs-sources/gpgme-src/gpgme-src-1.3.0.py
@@ -13,6 +13,12 @@ class subinfo(info.infoclass):
                                       ('gpgme-1.3.0-cmake.diff', 1), 
                                       ('gpgme-1.3.0-20101102.diff', 1), 
                                       ('gpgme-1.3.0-mingw.diff', 1)]
+
+        self.targets['1510'] = "download.sourceforge.net/kde-windows/gpgme-r1510.tar.bz2"
+        self.targetInstSrc['1510'] = "gpgme-r1510"
+        self.targetDigests['1510'] = 'bb67afb24dc95795ac2a369aafd7ef99438b90b6'
+        self.patchToApply['1510'] = [('gpgme-r1510-cmake.diff', 1), ('gpgme-r1510-20101205.diff', 1)]
+        
         self.shortDescription = "GnuPG cryptography support library (runtime)"
         self.defaultTarget = '1.3.0'
 
