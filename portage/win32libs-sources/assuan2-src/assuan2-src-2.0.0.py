257c16f08ab7b9ecb0da6a37aec8d4166126ce33
Patrick Spendrin <ps_ml@gmx.de>
2010-12-05 01:36:23 +0000

    add a target that is based on the current trunk
    svn path=/trunk/kdesupport/emerge/; revision=1203665

--- portage/win32libs-sources/assuan2-src/assuan2-src-2.0.0.py
+++ portage/win32libs-sources/assuan2-src/assuan2-src-2.0.0.py
@@ -9,7 +9,11 @@ class subinfo(info.infoclass):
         self.targets['2.0.1'] = 'ftp://ftp.gnupg.org/gcrypt/libassuan/libassuan-2.0.1.tar.bz2'
         self.targetInstSrc['2.0.1'] = 'libassuan-2.0.1'
         self.targetDigests['2.0.1'] = 'b7e9dbd41769cc20b1fb7db9f2ecdf276ffc352c'
-        self.patchToApply['2.0.1'] = [ ('assuan-381-head.diff', 0), ('libassuan-2.0.1-20101029.diff', 1) ]
+        self.patchToApply['2.0.1'] = [ ('libassuan-cmake.diff', 1), ('assuan-381-head.diff', 0), ('libassuan-2.0.1-20101029.diff', 1) ]
+        self.targets['400'] = "http://downloads.sourceforge.net/kde-windows/libassuan-r400.tar.bz2"
+        self.targetInstSrc['400'] = "libassuan-r400"
+        self.targetDigests['400'] = '91d85d50ccdc40b5353abe8190d6bd5ee9fb0be4'
+        self.patchToApply['400'] = [ ('libassuan-2.0.1-20101029.diff', 1), ('libassuan-cmake.diff', 1) ]
         self.shortDescription = "an IPC library used by some of the other GnuPG related packages"
         self.defaultTarget = '2.0.1'
 
