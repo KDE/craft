571c03c91256f3a07db7824add502dd7bf63f9bf
Andre Heinecke <aheinecke@intevation.de>
2010-12-06 17:14:56 +0000

    Add aspell codepages
    svn path=/trunk/kdesupport/emerge/; revision=1204302

--- portage/data/aspell-data/aspell-data-20090428.py
+++ portage/data/aspell-data/aspell-data-20090428.py
@@ -6,8 +6,9 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         repoUrl = """http://winkde.org/pub/kde/ports/win32/repository/aspell"""
         
-        self.targets[ '0.60' ] = repoUrl + """/aspell-en-6.0-41-bin.tar.bz2
-                           """ + repoUrl + """/aspell-de-0.60.20030222.1-10-bin.tar.bz2"""
+        self.targets[ '0.60' ] = (repoUrl + """/aspell-en-6.0-41-bin.tar.bz2 """
+                                + repoUrl + """/aspell-de-0.60.20030222.1-10-bin.tar.bz2 """
+                                + """ http://files.kolab.org/local/windows-ce/aspell-0.60.6-codepages.zip""")
 
         self.defaultTarget = '0.60'
             
