0f84f944a58865db3bd8b04d2e72ff6179a25803
Patrick Spendrin <ps_ml@gmx.de>
2010-12-10 09:15:28 +0000

    special name for kdebase-apps
    svn path=/trunk/kdesupport/emerge/; revision=1205157

--- portage/kde-4.5/kdebase-apps/kdebase-apps-20080202.py
+++ portage/kde-4.5/kdebase-apps/kdebase-apps-20080202.py
@@ -4,8 +4,8 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'branches/KDE/4.5/kdebase/apps'
         for ver in ['0', '1', '2', '3', '4']:
-            self.targets['4.5.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.5.' + ver + '/src/kdebase-apps-4.5.' + ver + '.tar.bz2'
-            self.targetInstSrc['4.5.' + ver] = 'kdebase-apps-4.5.' + ver
+            self.targets['4.5.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.5.' + ver + '/src/kdebase-4.5.' + ver + '.tar.bz2'
+            self.targetInstSrc['4.5.' + ver] = 'kdebase-4.5.' + ver
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
