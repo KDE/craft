9b95a06880458730175767668d1e4c2d0a5e0524
Patrick von Reth <patrick.vonreth@gmail.com>
2010-12-12 00:24:31 +0000

    build tags by default
    svn path=/trunk/kdesupport/emerge/; revision=1205650

--- portage/win32libs-sources/taglib-extras-src/taglib-extras-src-1.0.1-20090921.py
+++ portage/win32libs-sources/taglib-extras-src/taglib-extras-src-1.0.1-20090921.py
@@ -13,9 +13,7 @@ class subinfo(info.infoclass):
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/taglib-extras'
         self.targets['1.0.1'] = 'http://kollide.net/~jefferai/taglib-extras-1.0.1.tar.gz'
         self.targetInstSrc['1.0.1'] = 'taglib-extras-1.0.1'
-        for i in ['4.3.0', '4.3.1', '4.3.2', '4.3.3', '4.3.4', '4.3']:
-            self.svnTargets[ i ] = 'tags/kdesupport-for-4.3/kdesupport/taglib-extras'
-        self.defaultTarget = 'svnHEAD'
+        self.defaultTarget = '1.0.1'
 
 
 from Package.CMakePackageBase import *
--- portage/win32libs-sources/taglib-src/taglib-src-1.6.1-20100107.py
+++ portage/win32libs-sources/taglib-src/taglib-src-1.6.1-20100107.py
@@ -9,11 +9,7 @@ class subinfo(info.infoclass):
           self.targets[ v ] = 'http://developer.kde.org/~wheeler/files/src/taglib-%s.tar.gz' % v
           self.targetInstSrc[ v ] = 'taglib-%s' % v
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/taglib'
-        for i in ['4.3.0', '4.3.1', '4.3.2', '4.3.3', '4.3.4', '4.3']:
-            self.svnTargets[ i ] = 'tags/kdesupport-for-4.3/kdesupport/taglib'
-        for i in ['4.4.0', '4.4.1', '4.4.2', '4.4.3', '4.4.4', '4.4']:
-            self.svnTargets[ i ] = 'tags/kdesupport-for-4.4/taglib'
-        self.defaultTarget = 'svnHEAD'
+        self.defaultTarget = '1.6.3'
 
 from Package.CMakePackageBase import *
 
