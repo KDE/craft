9d4d2ad292b5a990afcf4839502626bbe0721ca0
Patrick Spendrin <ps_ml@gmx.de>
2010-12-14 17:39:11 +0000

    fix kdebase-workspace that makes at least two beer in Berlin (for me and Pau) or one free commit for me with #define public private in kdelibs
    svn path=/trunk/kdesupport/emerge/; revision=1206505

--- portage/kde-4.5/kdebase-workspace/kdebase-workspace-20080202.py
+++ portage/kde-4.5/kdebase-workspace/kdebase-workspace-20080202.py
@@ -7,7 +7,7 @@ class subinfo(info.infoclass):
             self.targets['4.5.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.5.' + ver + '/src/kdebase-workspace-4.5.' + ver + '.tar.bz2'
             self.targetInstSrc['4.5.' + ver] = 'kdebase-workspace-4.5.' + ver
         self.targetDigests['4.5.4'] = 'e362bceff622f39bf6949657d1e629ef541c6ae2'
-        self.patchToApply['4.5.4'] = [('kdebase-workspace-4.5-20101212.diff', 1)]
+        self.patchToApply['4.5.4'] = [('kdebase-workspace-4.5.4-20101214.diff', 1)]
         self.defaultTarget = 'svnHEAD'
     
     def setDependencies( self ):
