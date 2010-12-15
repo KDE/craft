b35a136e93693a195a0087b0167856cd5df2dff4
Patrick Spendrin <ps_ml@gmx.de>
2010-12-15 01:21:44 +0000

    make 0.21 default.
    svn path=/trunk/kdesupport/emerge/; revision=1206580

--- portage/win32libs-sources/exiv2-src/exiv2-src-0.21-20101215.py
+++ portage/win32libs-sources/exiv2-src/exiv2-src-0.21-20101215.py
@@ -1,6 +1,6 @@
 import info
 
-class subinfo(info.infoclass):
+class subinfo( info.infoclass ):
     def setTargets( self ):
         for f in ( '16', '17', '18', '18.1', '18.2', '19', '21' ):
           ver = '0.' + f
@@ -9,7 +9,7 @@ class subinfo(info.infoclass):
           self.patchToApply[ver]  = ( 'exiv2-%s-cmake.diff' % ver, 0 )
         self.svnTargets['svnHEAD'] = 'svn://dev.robotbattle.com/exiv2/branches/'
         self.shortDescription = "an image metadata library"
-        self.defaultTarget = '0.19'
+        self.defaultTarget = '0.21'
     
     def setDependencies( self ):
         self.dependencies['win32libs-bin/win_iconv']    = 'default'
@@ -20,7 +20,7 @@ class subinfo(info.infoclass):
         
 from Package.CMakePackageBase import *
 
-class Package(CMakePackageBase):
+class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
