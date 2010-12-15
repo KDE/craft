7372171b0044f4375756559a3dc8c2d559e1fbbc
Patrick Spendrin <ps_ml@gmx.de>
2010-12-15 01:26:30 +0000

    update binary package for exiv2
    svn path=/trunk/kdesupport/emerge/; revision=1206584

--- portage/win32libs-bin/exiv2/exiv2-0.21.py
+++ portage/win32libs-bin/exiv2/exiv2-0.21.py
@@ -1,7 +1,7 @@
 # This package-script is automatically updated by the script win32libsupdater.py
 # which can be found in your emerge/bin folder. To update this package, run
 # win32libsupdater.py (and commit the results)
-# based on revision 1
+# based on revision svn1206583
 
 from Package.BinaryPackageBase import *
 import os
@@ -11,11 +11,11 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         repoUrl = 'http://downloads.sourceforge.net/kde-windows'
 
-        for version in [ '0.18.1', '0.18.2', '0.19', '0.18' ]:
+        for version in [ '0.18.1', '0.18.2', '0.19', '0.18', '0.21' ]:
             self.targets[ version ]          = self.getPackage( repoUrl, 'exiv2', version )
             self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'exiv2', version , '.tar.bz2.sha1' )
 
-        self.defaultTarget = '0.19'
+        self.defaultTarget = '0.21'
 
 
     def setDependencies( self ):
