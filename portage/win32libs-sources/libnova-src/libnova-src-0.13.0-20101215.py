c7d0632418f81db20e748901901c9d39370eddab
Patrick Spendrin <ps_ml@gmx.de>
2010-12-15 02:13:15 +0000

    rename and fix for mingw-w32
    svn path=/trunk/kdesupport/emerge/; revision=1206598

--- portage/win32libs-sources/libnova-src/libnova-src-0.13.0-20101215.py
+++ portage/win32libs-sources/libnova-src/libnova-src-0.13.0-20101215.py
@@ -16,11 +16,11 @@ import compiler
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['0.13.0+svn270'] = 'download.sourceforge.net/kde-windows/libnova-0.13.0+svn270.tar.bz2'
+        self.targetInstSrc['0.13.0+svn270'] = 'libnova'
         self.targetDigests['0.13.0+svn270'] = '1d618a5a1f4282e531b2a3d434407bac941cd700'
-        self.patchToApply['0.13.0+svn270'] = ('libnova-svn.diff', 0)
+        self.patchToApply['0.13.0+svn270'] = ('libnova-20101215.diff', 1)
         self.shortDescription = "a Celestial Mechanics, Astrometry and Astrodynamics library"
         self.defaultTarget = '0.13.0+svn270'
-        self.targetConfigurePath['0.13.0+svn270'] = 'libnova'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
