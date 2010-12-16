48a08c8ec7059ebddba206a77b15b65dcb428438
Patrick Spendrin <ps_ml@gmx.de>
2010-12-16 21:36:11 +0000

    update libnova and libidn binary packages
    svn path=/trunk/kdesupport/emerge/; revision=1207082

--- portage/win32libs-bin/libnova/libnova-0.13.0+svn270.py
+++ portage/win32libs-bin/libnova/libnova-0.13.0+svn270.py
@@ -1,7 +1,7 @@
 # This package-script is automatically updated by the script win32libsupdater.py
 # which can be found in your emerge/bin folder. To update this package, run
 # win32libsupdater.py (and commit the results)
-# based on revision svn1206223
+# based on revision svn1206583
 
 from Package.BinaryPackageBase import *
 import os
@@ -11,11 +11,11 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         repoUrl = 'http://downloads.sourceforge.net/kde-windows'
 
-        for version in [ '1.14', '1.15', '1.12', '1.13', '1.9', '1.19', '1.19-1', '1.19-3' ]:
-            self.targets[ version ]          = self.getPackage( repoUrl, 'libidn', version )
-            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'libidn', version , '.tar.bz2.sha1' )
+        for version in [ '0.12.3', '0.13.0+svn270' ]:
+            self.targets[ version ]          = self.getPackage( repoUrl, 'libnova', version )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'libnova', version , '.tar.bz2.sha1' )
 
-        self.defaultTarget = '1.19-3'
+        self.defaultTarget = '0.13.0+svn270'
 
 
     def setDependencies( self ):
