fe1a02a28e102292fe3c512cfa7076e34b09abb5
Patrick Spendrin <ps_ml@gmx.de>
2010-12-17 15:02:18 +0000

    update to patchlevel 5
    svn path=/trunk/kdesupport/emerge/; revision=1207331

--- portage/win32libs-bin/libidn/libidn-1.19-5.py
+++ portage/win32libs-bin/libidn/libidn-1.19-5.py
@@ -1,7 +1,7 @@
 # This package-script is automatically updated by the script win32libsupdater.py
 # which can be found in your emerge/bin folder. To update this package, run
 # win32libsupdater.py (and commit the results)
-# based on revision svn1207078
+# based on revision svn1207325
 
 from Package.BinaryPackageBase import *
 import os
@@ -11,11 +11,11 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         repoUrl = 'http://downloads.sourceforge.net/kde-windows'
 
-        for version in [ '1.14', '1.15', '1.12', '1.13', '1.9', '1.19', '1.19-3', '1.19-1', '1.19-4' ]:
+        for version in [ '1.14', '1.15', '1.12', '1.13', '1.9', '1.19', '1.19-3', '1.19-1', '1.19-4', '1.19-5' ]:
             self.targets[ version ]          = self.getPackage( repoUrl, 'libidn', version )
             self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'libidn', version , '.tar.bz2.sha1' )
 
-        self.defaultTarget = '1.19-4'
+        self.defaultTarget = '1.19-5'
 
 
     def setDependencies( self ):
