e50e91ae04aeeb52475d38b3169a3651d4e3c15e
Patrick Spendrin <ps_ml@gmx.de>
2010-12-22 13:32:22 +0000

    add a kdepim-runtime package
    svn path=/trunk/kdesupport/emerge/; revision=1208599

---
+++ portage/kde/kdepim-runtime/kdepim-runtime-20101222.py
@@ -0,0 +1,58 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/kdepim-runtime'
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        if not emergePlatform.isCrossCompilingEnabled():
+            self.dependencies['kde/kdebase-runtime'] = 'default'
+        else:
+            self.dependencies['kdesupport/oxygen-icons'] = 'default'
+        self.dependencies['kde/kdepimlibs'] = 'default'
+        if not emergePlatform.isCrossCompilingEnabled():
+            self.dependencies['kdesupport/grantlee'] = 'default'
+        self.dependencies['win32libs-bin/sqlite'] = 'default'
+        
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
+
+from Package.CMakePackageBase import *
+        
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DKLEO_SYNCHRONOUS_API_HOTFIX=ON "
+
+        if emergePlatform.isCrossCompilingEnabled():
+            self.subinfo.options.configure.defines += "-DDISABLE_ALL_OPTIONAL_SUBDIRECTORIES=TRUE "
+            self.subinfo.options.configure.defines += " -DKDEPIM_MOBILE_UI=TRUE "
+            self.subinfo.options.configure.defines += " -DBUILD_mobile=ON -DBUILD_messagecomposer=ON  -DBUILD_runtime=ON -DBUILD_strigi-analyzer=ON "
+            self.subinfo.options.configure.defines += " -DMESSAGEVIEWER_NO_WEBKIT=ON "
+            self.subinfo.options.configure.defines += " -DTEMPLATEPARSER_NO_WEBKIT=ON "
+            #self.subinfo.options.configure.defines += " -DIMAPRESOURCE_NO_SOLID=ON "
+            self.subinfo.options.configure.defines += " -DRUNTIME_PLUGINS_STATIC=ON "
+            self.subinfo.options.configure.defines += " -DKDEQMLPLUGIN_STATIC=ON "
+            self.subinfo.options.configure.defines += " -DACCOUNTWIZARD_NO_GHNS=ON "
+            self.subinfo.options.configure.defines += " -DBUILD_kmail=ON "
+            self.subinfo.options.configure.defines += " -DKDEPIM_NO_NEPOMUK=ON "
+            self.subinfo.options.configure.defines += " -DKDE4_BUILD_TESTS=OFF "
+            self.subinfo.options.configure.defines += " -DBUILD_kleopatra=ON "
+            self.subinfo.options.configure.defines += " -DBUILD_korganizer=ON "
+            self.subinfo.options.configure.defines += " -DKDEPIM_ENTERPRISE_BUILD=ON "
+            self.subinfo.options.configure.defines += " -DKORGAC_AKONADI_AGENT=ON "
+            self.subinfo.options.configure.defines += " -DBUILD_NEW_MAIL_NOTIFIER_AGENT=ON "
+            self.subinfo.options.configure.defines += " -DAKONADI_USE_STRIGI_SEARCH=ON "
+            self.subinfo.options.configure.defines += " -DKDEPIM_INPROCESS_LDAP=ON "
+        else:
+            self.subinfo.options.configure.defines += " -DKDEPIM_BUILD_MOBILE=FALSE "
+
+        self.subinfo.options.configure.defines += "-DHOST_BINDIR=%s " \
+            % os.path.join(ROOTDIR, "bin")
+
+
+if __name__ == '__main__':
+    Package().execute()
