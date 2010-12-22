fa1fb67b4531830a5a586a41f18d74dd0d2e218f
Patrick Spendrin <ps_ml@gmx.de>
2010-12-22 11:39:38 +0000

    add a 2.8.3 package with the fixes for kdepimlibs stuff
    svn path=/trunk/kdesupport/emerge/; revision=1208582

--- portage/dev-util/cmake-pkg/cmake-pkg-20100421.py
+++ portage/dev-util/cmake-pkg/cmake-pkg-20100421.py
@@ -6,14 +6,12 @@ class subinfo(info.infoclass):
         self.svnTargets['gitHEAD'] = "git://cmake.org/cmake.git"
         self.svnTargets['release'] = "git://cmake.org/cmake.git|release|"
         self.svnTargets['v2.8.2'] = "git://cmake.org/cmake.git|release|v2.8.2"
-        self.targets['2.8.1'] = "http://www.cmake.org/files/v2.8/cmake-2.8.1.tar.gz"
-        self.targets['2.8.2'] = "http://www.cmake.org/files/v2.8/cmake-2.8.2.tar.gz"
-        self.targetInstSrc['2.8.1'] = "cmake-2.8.1"
-        self.targetInstSrc['2.8.2'] = "cmake-2.8.2"
-        self.patchToApply['2.8.1'] = ("cmake-2.8.1-20100511.diff", 1)
-        self.targets['2.8.0'] = "http://www.cmake.org/files/v2.8/cmake-2.8.0.tar.gz"
-        self.targetInstSrc['2.8.0'] = "cmake-2.8.0"
+        for ver in ['2.8.0', '2.8.1', '2.8.2', '2.8.3']:
+            self.targets[ ver ] = "http://www.cmake.org/files/v2.8/cmake" + ver + ".tar.gz"
+            self.targetInstSrc[ ver ] = "cmake-" + ver
         self.patchToApply['2.8.0'] = ("cmake-2.8.0-20100512.diff", 1)
+        self.patchToApply['2.8.1'] = ("cmake-2.8.1-20100511.diff", 1)
+        self.patchToApply['2.8.3'] = ("cmake-2.8.3-20101222.diff", 1)
         self.defaultTarget = 'release'
 
     def setDependencies( self ):
