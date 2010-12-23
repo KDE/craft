905a570c154e6b6890a9838adf187a0853827a46
Patrick von Reth <patrick.vonreth@gmail.com>
2010-12-23 16:44:40 +0000

    updated url
    svn path=/trunk/kdesupport/emerge/; revision=1208905

--- portage/testing/rekonq/rekonq-20091023.py
+++ portage/testing/rekonq/rekonq-20091023.py
@@ -4,7 +4,7 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = 'git://gitorious.org/rekonq/mainline.git'
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/rekonq'
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
