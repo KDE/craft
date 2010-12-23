2ede2e57c12646ba26c31118cc1ed491eef21505
Ralf Habacker <ralf.habacker@freenet.de>
2010-12-23 12:26:21 +0000

    migrated 0.3.9 tag
    svn path=/trunk/kdesupport/emerge/; revision=1208828

--- portage/kdesupport/kdewin/kdewin-0.5.4-20101223.py
+++ portage/kdesupport/kdewin/kdewin-0.5.4-20101223.py
@@ -13,7 +13,7 @@ class subinfo(info.infoclass):
         self.dependencies['win32libs-bin/libpng'] = 'default'
 
     def setTargets( self ):
-        self.svnTargets['0.3.9'] = 'tags/kdewin32/0.3.9'
+        self.svnTargets['0.3.9'] = 'http://gitweb.kde.org/kdewin.git/snapshot/fc116df1dc204d8a06dc5c874a4cdecc335115ec.tar.gz'
         self.svnTargets['gitHEAD'] = 'git://git.kde.org/kdewin'
         for i in ['4.3.0', '4.3.1', '4.3.2', '4.3.3', '4.3.4', '4.3']:
             self.svnTargets[ i ] = 'tags/kdesupport-for-4.3/kdesupport/kdewin'
