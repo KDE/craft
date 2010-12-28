0a157557e0c150973bacc5a63e83945ab9b59d21
Patrick Spendrin <ps_ml@gmx.de>
2010-12-28 22:09:38 +0000

    add installer generation package for kdevelop
    svn path=/trunk/kdesupport/emerge/; revision=1209991

---
+++ portage/package/kdevelop-package/kdevelop-package-20101221.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.VirtualPackageBase import *
+from Packager.NullsoftInstallerPackager import *
+
+# This is an example package for building 
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets[ '4.1.1' ] = ""
+        self.defaultTarget = '4.1.1'
+    
+    def setDependencies( self ):
+        self.dependencies[ 'extragear/kdevelop' ] = 'default'
+    
+class Package( NullsoftInstallerPackager, VirtualPackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+#        whitelists = [ 'whitelist.txt' ]
+        blacklists = [ NSIPackagerLists.runtimeBlacklist, 'blacklist.txt', 'blacklist-mysql.txt', 'blacklist-virtuoso.txt' ]
+        NullsoftInstallerPackager.__init__( self, blacklists=blacklists )
+        VirtualPackageBase.__init__( self )
+        self.defines[ "executable" ] = "bin\\kdevelop.exe"
+
+if __name__ == '__main__':
+    Package().execute()
