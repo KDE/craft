80cb6d227d04477572af0267caed1091c953a964
Andrius da Costa Ribas <andriusmao@gmail.com>
2010-12-30 02:12:28 +0000

    Add KRegExpEditor to portage.
    svn path=/trunk/kdesupport/emerge/; revision=1210221

---
+++ portage/extragear/kregexpeditor/kregexpeditor-20101229.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/playground/utils/kregexpeditor'
+        self.defaultTarget = 'svnHEAD'
+    
+    def setDependencies( self ):
+        self.dependencies['virtual/kdebase-runtime'] = 'default'
+    
+class Package(CMakePackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
