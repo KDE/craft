8f95fd07c39fe5ed9d0d97a9f1ed90f137601f29
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2011-01-04 23:05:00 +0000

    update to 0.4.7, add digests
    svn path=/trunk/kdesupport/emerge/; revision=1211773

--- portage/win32libs-sources/libssh-src/libssh-src-0.4.7-20110104.py
+++ portage/win32libs-sources/libssh-src/libssh-src-0.4.7-20110104.py
@@ -4,14 +4,17 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['0.4.4', '0.4.6']:
+        for ver in ['0.4.4', '0.4.6', '0.4.7']:
             self.targets[ ver ] = "http://www.libssh.org/files/0.4/libssh-" + ver + ".tar.gz"
             self.targetInstSrc[ ver ] = "libssh-" + ver
         self.targetDigests['0.4.4'] = 'bde1d4713a86b6256ce2d14e6de6326e52c4da44'
+        self.targetDigests['0.4.6'] = '52e7e68590fbcd835bc4a2eceb51e672641efb69'
+        self.targetDigests['0.4.7'] = '5e31736ff906e745e6da508828685d8389e37954'
+
         self.svnTargets['gitHEAD'] = "git://git.libssh.org/projects/libssh/libssh.git"
         self.svnTargets['0.4'] = "git://git.libssh.org/projects/libssh/libssh.git|v0-4"
         self.shortDescription = "a working SSH implementation by the mean of a library"
-        self.defaultTarget = '0.4.6'
+        self.defaultTarget = '0.4.7'
         self.options.configure.defines = "-DWITH_STATIC_LIB=ON"
 
     def setDependencies( self ):
