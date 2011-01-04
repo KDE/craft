1440eb27cc4c6fb460d9c7f36117d32961b13b50
Patrick Spendrin <ps_ml@gmx.de>
2011-01-04 18:26:14 +0000

    use patch in libopensp and rename already
    svn path=/trunk/kdesupport/emerge/; revision=1211716

---
+++ portage/testing/libopensp-src/libopensp-src-1.5.2.py
@@ -0,0 +1,23 @@
+import info
+
+SRC_URI= """
+http://surfnet.dl.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz
+"""
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.targets['1.5.2'] = SRC_URI
+        self.targetInstSrc['1.5.2'] = "OpenSP-1.5.2"
+        self.patchToApply['1.5.2'] = ( "OpenSP-1.5.2-20110104.diff", 1 )
+        self.defaultTarget = '1.5.2'
+        
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )        
+
+if __name__ == '__main__':
+    Package().execute()
+
