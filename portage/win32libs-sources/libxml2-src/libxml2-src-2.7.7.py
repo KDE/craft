b8e981f5f32321a28bbb6f0fccb16b819d406141
Pau Garcia i Quiles <pgquiles@elpauer.org>
2011-01-05 18:12:46 +0000

    Add libxml2 2.7.8, keep 2.7.7 as default
    svn path=/trunk/kdesupport/emerge/; revision=1212100

--- portage/win32libs-sources/libxml2-src/libxml2-src-2.7.7.py
+++ portage/win32libs-sources/libxml2-src/libxml2-src-2.7.7.py
@@ -4,11 +4,13 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['2.7.7']:
+        for ver in ['2.7.7', '2.7.8']:
             self.targets[ver] = 'ftp://xmlsoft.org/libxml2/libxml2-' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'libxml2-' + ver
         self.patchToApply['2.7.7'] = ("libxml2-2.7.7-20101103.diff", 1)
         self.targetDigests['2.7.7'] = '8592824a2788574a172cbddcdc72f734ff87abe3'
+        self.patchToApply['2.7.8'] = ("libxml2-2.7.8-20110105.diff", 1)
+        self.targetDigests['2.7.8'] = '859dd535edbb851cc15b64740ee06551a7a17d40'
         self.shortDescription = "XML C parser and toolkit (runtime and applications)"
         
         self.defaultTarget = '2.7.7'
