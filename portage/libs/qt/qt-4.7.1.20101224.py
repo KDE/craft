7db8090bdb9ac21182b6f89d9ebecd5c38ece10a
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-01-06 21:45:51 +0000

    add another qml fix
    svn path=/trunk/kdesupport/emerge/; revision=1212408

--- portage/libs/qt/qt-4.7.1.20101224.py
+++ portage/libs/qt/qt-4.7.1.20101224.py
@@ -68,7 +68,8 @@ class subinfo(info.infoclass):
             ('patches/4.7/Override-new-in-qt-dlls-to-use-dlmalloc.patch', 1),
             ('patches/4.7/Replace-qeventdispatcher.patch', 1),
             ('patches/4.7/fix-build-with-QT_NO_SVG.patch', 1),
-            ('patches/4.7/fix-QSortFilterProxyModel.patch', 1) ]
+            ('patches/4.7/fix-QSortFilterProxyModel.patch', 1),
+            ('patches/4.7/Repaint-when-text-color-changes.patch', 1) ]
         self.patchToApply['wince-4.7'] =  winceVersionIndependentPatches + [
             ('patches/4.7/Override-new-in-qt-dlls-to-use-dlmalloc.patch-for-4-7-branch', 1),
             ('patches/4.7/Replace-qeventdispatcher-and-add-wcecompat-dep-for-4-7-branch.patch', 1),
