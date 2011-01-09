563eb46823bcd9021e9ee7e638ba8d952c9ff531
Ralf Habacker <ralf.habacker@freenet.de>
2011-01-09 11:34:10 +0000

    moved emerge doc generating into separate package emerge-doc. The emerge package is intended to update the source only
    svn path=/trunk/kdesupport/emerge/; revision=1213158

---
+++ portage/dev-util/emerge-doc/emerge-doc-svn.py
@@ -0,0 +1,75 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/emerge'
+        self.defaultTarget = 'svnHEAD'
+        
+    def setDependencies( self ):
+        self.dependencies['dev-util/doxygen'] = 'default'
+        self.dependencies['dev-util/emerge'] = 'default'
+
+from Package.PackageBase import *
+from Source.SvnSource import *
+from BuildSystem.BuildSystemBase import *
+from datetime import date
+
+class Package(PackageBase,SvnSource,BuildSystemBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        self.subinfo.options.merge.destinationPath = "dev-utils"
+        PackageBase.__init__(self)
+        SvnSource.__init__(self)
+        BuildSystemBase.__init__(self,"")
+
+    def checkoutDir(self, index=0 ): 
+        return os.path.join(ROOTDIR,"emerge")
+
+    def configure(self):
+        doxygenSourcePathes = "%s %s" % (os.path.join(self.checkoutDir(),'bin'),os.path.join(self.checkoutDir(),'doc'))
+
+        # copy and patch template
+        sourceDoxyFile = os.path.join(self.checkoutDir(),'doc','Doxyfile')
+        destPath = self.buildDir()
+        destDoxyFile = os.path.join(destPath,'Doxyfile')
+
+        if not os.path.exists(destPath):
+            utils.createDir(destPath)
+        
+        fin = open (sourceDoxyFile, "r")
+        fout = open (destDoxyFile, "w")
+        for line in fin:
+            if line.find("@INPUT@") <> -1:
+                line = line.replace("@INPUT@",doxygenSourcePathes)
+            elif line.find("@PROJECT_NUMBER@") <> -1:
+                line = line.replace("@PROJECT_NUMBER@",date.today().isoformat())
+            fout.write(line) 
+        fin.close()
+        fout.close()
+        return True
+
+    def make(self):
+        buildPath = self.buildDir()
+        doxyFile = os.path.join(buildPath,'Doxyfile')
+        os.chdir(buildPath)
+        utils.system("doxygen %s" % doxyFile)
+        return True
+        
+    def install(self):
+        srcdir = os.path.join(self.buildDir(),'html')    
+        destdir = os.path.join(self.imageDir(),'share','doc','emerge','html')
+        utils.copyDir(srcdir,destdir)
+        return True
+
+    def qmerge(self):
+        if not PackageBase.qmerge(self):
+            return False
+        destdir = os.path.join(self.mergeDestinationDir(),'share','doc','emerge','html')
+        utils.system("start %s/index.html" % destdir)
+        return True
+
+    def createPackage(self):
+        return True
+
+if __name__ == '__main__':
+    Package().execute()
--- portage/dev-util/emerge/emerge-svn.py
+++ portage/dev-util/emerge/emerge-svn.py
@@ -9,7 +9,6 @@ class subinfo(info.infoclass):
         
     def setDependencies( self ):
         self.dependencies['virtual/base'] = 'default'
-        self.dependencies['dev-util/doxygen'] = 'default'
 
 from Package.PackageBase import *
 from Source.SvnSource import *
@@ -22,39 +21,14 @@ class Package(PackageBase,SvnSource,BuildSystemBase):
         PackageBase.__init__(self)
         SvnSource.__init__(self)
         BuildSystemBase.__init__(self,"")
-        print self.subinfo.dependencies
 
     def checkoutDir(self, index=0 ): 
         return os.path.join(ROOTDIR,"emerge")
 
     def configure(self):
-        doxygenSourcePathes = "%s %s" % (os.path.join(self.checkoutDir(),'bin'),os.path.join(self.checkoutDir(),'doc'))
-
-        # copy and patch template
-        sourceDoxyFile = os.path.join(self.checkoutDir(),'doc','Doxyfile')
-        destPath = os.path.join(self.buildDir(),'doc')
-        destDoxyFile = os.path.join(destPath,'Doxyfile')
-
-        if not os.path.exists(destPath):
-            utils.createDir(destPath)
-        
-        fin = open (sourceDoxyFile, "r")
-        fout = open (destDoxyFile, "w")
-        for line in fin:
-            if line.find("@INPUT@") <> -1:
-                line = line.replace("@INPUT@",doxygenSourcePathes)
-            elif line.find("@PROJECT_NUMBER@") <> -1:
-                line = line.replace("@PROJECT_NUMBER@",date.today().isoformat())
-            fout.write(line) 
-        fin.close()
-        fout.close()
         return True
 
     def make(self):
-        buildPath = os.path.join(self.buildDir(),'doc')
-        doxyFile = os.path.join(buildPath,'Doxyfile')
-        os.chdir(buildPath)
-        utils.system("doxygen %s" % doxyFile)
         return True
         
     def install(self): 
