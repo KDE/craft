dec28169a30f48ed3324271b0fd5ec5792b5e601
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2011-01-15 15:02:39 +0000

    fix one more case of wrong indentation
    svn path=/trunk/kdesupport/emerge/; revision=1214613

--- bin/portage_versions.py
+++ bin/portage_versions.py
@@ -137,13 +137,13 @@ def vercmp(ver1, ver2, silent=1):
             # it's possible that the s(1|2)[1] == ''
             # in such a case, fudge it.
             try:
-               r1 = int(s1[1])
+                r1 = int(s1[1])
             except ValueError:
-               r1 = 0
+                r1 = 0
             try:
-               r2 = int(s2[1])
+                r2 = int(s2[1])
             except ValueError:
-               r2 = 0
+                r2 = 0
             return r1 - r2
 
     # the suffix part is equal to, so finally check the revision
