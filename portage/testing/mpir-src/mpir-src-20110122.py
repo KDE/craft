820a9593bf68811987db9f6c50cf15736487b323
Cristian Oneț <onet.cristian@gmail.com>
2011-01-22 23:44:20 +0000

    Added a libgmp fork which supposedly can be build with msvc out of the box (the ebuild needs to be enhanced for that to happen).
    svn path=/trunk/kdesupport/emerge/; revision=1216385

---
+++ portage/testing/mpir-src/mpir-src-20110122.py
@@ -0,0 +1,30 @@
+import os
+from shells import MSysShell
+import info
+
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.buildDependencies['dev-util/msys'] = 'default'
+
+    def setTargets( self ):
+        self.targets['2.2.1'] = 'http://www.mpir.org/mpir-2.2.1.tar.bz2'
+        self.targetInstSrc['2.2.1'] = "mpir-2.2.1"
+        self.patchToApply['2.2.1'] = ('mpir-2.2.1-20110122.diff', 1)
+
+        self.options.package.withCompiler = False
+        self.shortDescription = "Library for arbitrary precision integer arithmetic derived from version 4.2.1 of gmp"
+        self.defaultTarget = '2.2.1'
+
+from Package.AutoToolsPackageBase import *
+from Package.VirtualPackageBase import *
+ 
+class Package(AutoToolsPackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        AutoToolsPackageBase.__init__(self)
+        self.subinfo.options.configure.defines = "--enable-shared --disable-static --enable-gmpcompat"
+
+if __name__ == '__main__':
+      Package().execute()
