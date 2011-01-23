c92dc66acda818b0df054acb28f7906f54f74252
Andrius da Costa Ribas <andriusmao@gmail.com>
2011-01-23 16:29:15 +0000

    Fix obsolete calls.
    svn path=/trunk/kdesupport/emerge/; revision=1216476

--- portage/testing/libdvdcss/libdvdcss-1.2.10.py
+++ portage/testing/libdvdcss/libdvdcss-1.2.10.py
@@ -1,3 +1,4 @@
+from Package.BinaryPackageBase import *
 import base
 import os
 import shutil
@@ -19,35 +20,10 @@ class subinfo( info.infoclass ):
         self.targets['1.2.10'] = SRC_URI
         self.defaultTarget = '1.2.10'
 
-class subclass( base.baseclass ):
-    def __init__( self, **args ):
-        base.baseclass.__init__( self, args=args )
-        self.instsrcdir = "libdvdcss-1.2.10"
-        #self.createCombinedPackage = True
-        self.subinfo = subinfo()
-
-    def unpack( self ):
-        base.baseclass.unpack( self ) or utils.die( "unpack failed" )
-        os.chdir( self.workdir )
-        shutil.copyfile( os.path.join( self.packagedir, "CMakeLists.txt" ), os.path.join( self.workdir,self.instsrcdir, "CMakeLists.txt" ) )
-        shutil.copyfile( os.path.join( self.packagedir, "config.h" ), os.path.join( self.workdir,self.instsrcdir, "config.h" ) )
-        shutil.copyfile( os.path.join( self.packagedir, "common.h" ), os.path.join( self.workdir,self.instsrcdir, "src/common.h" ) )
-        if( not os.path.exists( self.workdir ) ):
-            os.makedirs( self.workdir )
-        return True
-
-    def compile( self ):
-        return self.kdeCompile()
-
-    def install( self ):
-        return self.kdeInstall()
-
-    def make_package( self ):
-
-    # now do packaging with kdewin-packager
-        self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
-
-        return True
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
 
 if __name__ == '__main__':
-    subclass().execute()
+    Package().execute()
