f0f295ee10bd80c5deaf1510a03d69e9fccd4420
Cristian Oneț <onet.cristian@gmail.com>
2011-01-23 09:16:02 +0000

    Added packages for AqBanking and Gwenhywfar plus gnutls which is a Gwenhywfar dependency (see http://www.aquamaniac.de/sites/aqbanking/index.php). The packages are only buildable on mingw for now and are needed for HBCI (German online banking standard) support in KMyMoney (provided by a plugin). The packages still have the following issues so someone with more mingw knowledge could provide some improvements: - gnutls will not compile because the existing gpg-error-src package (used via libgcrypt) does not install the gpg-error-config script - libgcrypt will also need some love - I had to make a nasty hack in gwenhywfar's configure to find qt4libs which will only work in debug ("d4" suffix - need some help here)
    svn path=/trunk/kdesupport/emerge/; revision=1216417

---
+++ portage/testing/gnutls-src/gnutls-src-2.8.6.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import info
+
+from Source.MultiSource import *
+from BuildSystem.AutoToolsBuildSystem import *
+from Packager.MultiPackager import *
+from Package.PackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['2.8.6'] = 'ftp://ftp.gnupg.org/gcrypt/gnutls/gnutls-2.8.6.tar.bz2'
+        self.targetInstSrc['2.8.6'] = 'gnutls-2.8.6'
+        self.patchToApply['2.8.6'] = ('gnutls-2.8.6-20110118.diff', 1)
+        self.defaultTarget = '2.8.6'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/bin-base'] = 'default'
+        self.hardDependencies['testing/libgcrypt'] = 'default'
+        if compiler.isMinGW():
+                self.buildDependencies['dev-util/msys'] = 'default'
+
+class PackageMinGW(AutoToolsBuildSystem):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        AutoToolsBuildSystem.__init__(self)        
+        self.subinfo.options.configure.defines = "--disable-openpgp-authentication --disable-guile"
+
+if compiler.isMinGW():
+    class Package(PackageMinGW):
+        def __init__( self ):
+            PackageMinGW.__init__( self )
+else:
+    class Package(VirtualPackageBase):
+        def __init__( self ):
+            self.subinfo = subinfo()
+            VirtualPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
---
+++ portage/testing/gwenhywfar-src/gwenhywfar-src-20110104.py
@@ -0,0 +1,48 @@
+import os
+from shells import MSysShell
+import info
+import utils
+
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.buildDependencies['testing/gnutls-src'] = 'default'
+        if compiler.isMinGW():
+                self.buildDependencies['dev-util/msys'] = 'default'
+
+    def setTargets( self ):
+        self.targets['4.0.3'] = 'gwenhywfar-4.0.3.tar.gz'
+        self.targetInstSrc['4.0.3'] = "gwenhywfar-4.0.3"       
+        self.patchToApply['4.0.3'] = ('gwenhywfar-4.0.3-20110122.diff', 1)
+
+        self.options.package.withCompiler = False
+        self.shortDescription = "A multi-platform helper library for other libraries"
+        self.defaultTarget = '4.0.3'
+
+from Package.AutoToolsPackageBase import *
+from Package.VirtualPackageBase import *
+ 
+class PackageMinGW(AutoToolsPackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        AutoToolsPackageBase.__init__(self)
+        mergeroot = self.shell.toNativePath( self.mergeDestinationDir() )
+        self.subinfo.options.configure.defines = "--enable-shared --disable-static --with-guis=qt4 --with-qt4-includes=" + mergeroot + "/include --with-qt4-libs=" + mergeroot + "/lib --with-qt4-moc=" + mergeroot + "/bin/moc.exe --with-qt4-uic=" + mergeroot + "/bin/uic.exe"
+
+    def fetch(self):
+        utils.wgetFile('"http://www.aquamaniac.de/sites/download/download.php?package=01&release=56&file=01&dummy=gwenhywfar-4.0.3.tar.gz"' , self.downloadDir() , "gwenhywfar-4.0.3.tar.gz")
+        return True
+
+if compiler.isMinGW():
+    class Package(PackageMinGW):
+        def __init__( self ):
+            PackageMinGW.__init__( self )
+else:
+    class Package(VirtualPackageBase):
+        def __init__( self ):
+            self.subinfo = subinfo()
+            VirtualPackageBase.__init__( self )
+
+if __name__ == '__main__':
+      Package().execute()
