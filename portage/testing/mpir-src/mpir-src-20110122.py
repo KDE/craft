8c22cd37edfacb65f363dd5d21410e928df69c12
Cristian Oneț <onet.cristian@gmail.com>
2011-01-23 08:46:19 +0000

    Make the package a NOOP on msvc for now (see the vc10/readme folder in the sources for an actual implementation of the build on msvc).
    svn path=/trunk/kdesupport/emerge/; revision=1216414

--- portage/testing/mpir-src/mpir-src-20110122.py
+++ portage/testing/mpir-src/mpir-src-20110122.py
@@ -6,7 +6,8 @@ import info
 class subinfo(info.infoclass):
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
-        self.buildDependencies['dev-util/msys'] = 'default'
+        if compiler.isMinGW():
+                self.buildDependencies['dev-util/msys'] = 'default'
 
     def setTargets( self ):
         self.targets['2.2.1'] = 'http://www.mpir.org/mpir-2.2.1.tar.bz2'
@@ -19,12 +20,22 @@ class subinfo(info.infoclass):
 
 from Package.AutoToolsPackageBase import *
 from Package.VirtualPackageBase import *
- 
-class Package(AutoToolsPackageBase):
+
+class PackageMinGW(AutoToolsPackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.configure.defines = "--enable-shared --disable-static --enable-gmpcompat"
+
+if compiler.isMinGW():
+    class Package(PackageMinGW):
+        def __init__( self ):
+            PackageMinGW.__init__( self )
+else:
+    class Package(VirtualPackageBase):
+        def __init__( self ):
+            self.subinfo = subinfo()
+            VirtualPackageBase.__init__( self )
 
 if __name__ == '__main__':
       Package().execute()
