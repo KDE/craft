76252e171f7c8ec3fe43878ed1e5f67e03b8d5d4
Andrius da Costa Ribas <andriusmao@gmail.com>
2011-01-25 14:06:25 +0000

    Add GLee library to portage.
    svn path=/trunk/kdesupport/emerge/; revision=1217047

---
+++ portage/win32libs-sources/glee/glee-5.4.py
@@ -0,0 +1,22 @@
+import os
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets[ '5.4' ] = 'http://elf-stone.com/downloads/GLee/GLee5_4.zip'
+        self.patchToApply[ '5.4' ] = ( "glee-5.4.diff", 1 )
+        self.shortDescription = "GLee (GL Easy Extension library) is a free cross-platform extension loading library for OpenGL"
+        self.defaultTarget = '5.4'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
