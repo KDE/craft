b68f5963dd9918152ba68d5856c7f9d2de9967ac
Patrick Spendrin <ps_ml@gmx.de>
2011-01-25 00:39:47 +0000

    move yakuake to extragear & update it
    svn path=/trunk/kdesupport/emerge/; revision=1216823

---
+++ portage/extragear/yakuake/yakuake-20090627.py
@@ -0,0 +1,19 @@
+import info
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/yakuake'
+		self.shortDescription = "a drop-down terminal emulator based on KDE Konsole technology"
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.dependencies['virtual/kdebase-apps'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
