0772e2d827148643a3b8e02aa921b98526dde4a9
Andre Heinecke <aheinecke@intevation.de>
2011-01-26 16:43:21 +0000

    Add two additional memory management patches for wince qt.
    Those really have to be put in a qt clone,.. 
    
    svn path=/trunk/kdesupport/emerge/; revision=1217313

--- portage/libs/qt/qt-4.7.1.20101224.py
+++ portage/libs/qt/qt-4.7.1.20101224.py
@@ -66,7 +66,9 @@ class subinfo(info.infoclass):
             ('patches/4.7/Add-gpgLogging.patch', 1),
             ('patches/4.7/allow-more-then-one-instance-of-a-wince-application.patch', 1),
             ('patches/4.7/Add-qCalloc-to-qmalloc.patch', 1),
-            ('patches/4.7/Use-dlmalloc-in-QScript4.dll.patch', 1)]
+            ('patches/4.7/Use-dlmalloc-in-QScript4.dll.patch', 1),
+            ('patches/4.7/Use-dlmalloc-for-javascript-garbage-collector.patch', 1),
+            ('patches/4.7/Use-qRealloc-instead-of-realloc-in-qimage.patch', 1)]
         self.patchToApply['wince']     =  winceVersionIndependentPatches + [
             ('patches/4.7/fix-qml-alignment.patch', 1), # Upstream in 4.7
             ('patches/4.7/Override-new-in-qt-dlls-to-use-dlmalloc.patch', 1),
