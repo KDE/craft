1df71ec4d6482c610e50c803a3db4e8b30f52e6f
Patrick von Reth <patrick.vonreth@gmail.com>
2011-01-26 19:37:07 +0000

    fixed build of phonon-backends from gitHEAD
    svn path=/trunk/kdesupport/emerge/; revision=1217334

--- portage/kdesupport/phonon-mplayer/phonon-mplayer-0.1.py
+++ portage/kdesupport/phonon-mplayer/phonon-mplayer-0.1.py
@@ -17,6 +17,8 @@ class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.defines = '-DPHONON_BUILDSYSTEM_DIR=\"%s\" ' % os.path.join(os.getenv('KDEROOT'),'share','phonon-buildsystem').replace('\\','/')
+
 
 if __name__ == '__main__':
     Package().execute()
--- portage/kdesupport/phonon-vlc/phonon-vlc-0.3.1-20110104.py
+++ portage/kdesupport/phonon-vlc/phonon-vlc-0.3.1-20110104.py
@@ -26,6 +26,7 @@ class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.defines = '-DPHONON_BUILDSYSTEM_DIR=\"%s\" ' % os.path.join(os.getenv('KDEROOT'),'share','phonon-buildsystem').replace('\\','/')
 
 if __name__ == '__main__':
     Package().execute()
