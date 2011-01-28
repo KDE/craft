2df96d0678b38daa0f33c4bd540d0d5bbde33cb3
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-01-28 19:42:25 +0000

    kdepimslibs migrated to git
    svn path=/trunk/kdesupport/emerge/; revision=1217811

--- portage/kde/kdepimlibs/kdepimlibs-20080202.py
+++ portage/kde/kdepimlibs/kdepimlibs-20080202.py
@@ -2,12 +2,11 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['4.0.0'] = 'tags/KDE/4.0.0/kdepimlibs'
-        self.svnTargets['svnHEAD'] = 'trunk/KDE/kdepimlibs'
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/kdepimlibs'
         for ver in ['80', '83', '85']:
           self.targets['4.0.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.0.' + ver + '/src/kdepimlibs-4.0.' + ver + '.tar.bz2'
           self.targetInstSrc['4.0.' + ver] = 'kdepimlibs-4.0.' + ver
-        self.defaultTarget = 'svnHEAD'
+        self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.dependencies['kde/kdelibs'] = 'default'
