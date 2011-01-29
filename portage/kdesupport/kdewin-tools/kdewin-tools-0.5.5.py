7c5139db7c45cc3f666da6eda6a488622105ed9f
Ralf Habacker <ralf.habacker@freenet.de>
2011-01-29 12:19:45 +0000

    splitted from kdewin package
    svn path=/trunk/kdesupport/emerge/; revision=1217890

---
+++ portage/kdesupport/kdewin-tools/kdewin-tools-0.5.5.py
@@ -0,0 +1,31 @@
+import info
+import emergePlatform
+import utils
+import compiler
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.dependencies['libs/qt'] = 'default'
+        self.dependencies['win32libs-bin/zlib'] = 'default'
+        self.dependencies['win32libs-bin/libpng'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/kdewin'
+        self.shortDescription = "kde supplementary tools package for win32"
+        self.defaultTarget = 'gitHEAD'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        # required for package generating because we build from gitHEAD by default
+        self.subinfo.options.package.version = '0.5.5'
+        self.subinfo.options.configure.defines = '-DBUILD_BASE_LIB_WITH_QT=OFF -DBUILD_BASE_LIB=OFF -DBUILD_TOOLS=ON -DBUILD_QT_LIB=OFF'
+        if compiler.isMinGW_W32():
+          self.subinfo.options.configure.defines += ' -DMINGW_W32=ON '
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
