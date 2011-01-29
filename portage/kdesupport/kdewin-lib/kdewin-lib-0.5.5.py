be6b92e732a526e5a1d77f8e599e4b8337400b24
Ralf Habacker <ralf.habacker@freenet.de>
2011-01-29 12:41:36 +0000

    added kdewin-qt and kdewin-lib with is a separation of kdewin
    svn path=/trunk/kdesupport/emerge/; revision=1217892

---
+++ portage/kdesupport/kdewin-lib/kdewin-lib-0.5.5.py
@@ -0,0 +1,34 @@
+import info
+import emergePlatform
+import utils
+import compiler
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['0.3.9'] = 'http://gitweb.kde.org/kdewin.git/snapshot/fc116df1dc204d8a06dc5c874a4cdecc335115ec.tar.gz'
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/kdewin'
+        self.shortDescription = "kde supplementary package for win32"
+        self.defaultTarget = 'gitHEAD'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        # required for package generating because we build from svnHEAD by default
+        self.subinfo.options.package.version = '0.5.5'
+        self.subinfo.options.configure.defines = '-DBUILD_BASE_LIB=ON -DBUILD_TOOLS=OFF -DBUILD_QT_LIB=OFF -DBUILD_BASE_LIB_WITH_QT=OFF '
+        if compiler.isMinGW_W32():
+          self.subinfo.options.configure.defines += ' -DMINGW_W32=ON '
+        CMakePackageBase.__init__( self )
+
+    def make(self ):
+        if self.isTargetBuild():
+            os.environ["TARGET_INCLUDE"] = "%s;%s" % (os.path.join(self.mergeDestinationDir(), "include", "wcecompat"), os.getenv("TARGET_INCLUDE"))
+        return CMakePackageBase.make( self )
+
+if __name__ == '__main__':
+    Package().execute()
