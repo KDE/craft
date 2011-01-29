8e36bdd7ef92eb5536e54c6f38f33f82511f47f4
Ralf Habacker <ralf.habacker@freenet.de>
2011-01-29 18:12:24 +0000

    kdepimlibs has been moved to git
    svn path=/trunk/kdesupport/emerge/; revision=1217935

---
+++ portage/kde/kdepimlibs/kdepimlibs-20110129.py
@@ -0,0 +1,45 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/kdepimlibs'
+        for ver in ['80', '83', '85']:
+          self.targets['4.0.' + ver] = 'ftp://ftp.kde.org/pub/kde/unstable/4.0.' + ver + '/src/kdepimlibs-4.0.' + ver + '.tar.bz2'
+          self.targetInstSrc['4.0.' + ver] = 'kdepimlibs-4.0.' + ver
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.dependencies['kde/kdelibs'] = 'default'
+        self.dependencies['kdesupport/akonadi'] = 'default'
+        self.dependencies['win32libs-bin/cyrus-sasl'] = 'default'
+        self.dependencies['win32libs-bin/libical'] = 'default'
+        self.dependencies['win32libs-bin/boost'] = 'default'
+        if not emergePlatform.isCrossCompilingEnabled():
+            self.dependencies['win32libs-bin/gpgme'] = 'default'
+        else:
+            self.dependencies['contributed/gpg4win-dev'] = 'default'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.boost = portage.getPackageInstance('win32libs-bin','boost')
+        path = self.boost.installDir()
+        os.putenv( "BOOST_ROOT", path )
+
+        self.subinfo.options.configure.defines = "-DHOST_BINDIR=%s " \
+            % os.path.join(ROOTDIR, "bin")
+
+        if emergePlatform.isCrossCompilingEnabled():
+            if self.isTargetBuild():
+                self.subinfo.options.configure.defines += "-DKDEPIM_NO_KRESOURCES=ON -DMAILTRANSPORT_INPROCESS_SMTP=ON "
+        self.subinfo.options.configure.defines += "-DBUILD_doc=OFF "
+
+if __name__ == '__main__':
+    Package().execute()
