494d679596255cd193d8c6bbaf9125e2d242bad0
Ralf Habacker <ralf.habacker@freenet.de>
2011-01-30 13:13:53 +0000

    added packages according to the repository name
    svn path=/trunk/kdesupport/emerge/; revision=1218002

---
+++ portage/kde/kde-baseapps/kde-baseapps-20110130.py
@@ -0,0 +1,24 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'git://git.kde.org/kde-baseapps'
+        self.defaultTarget = 'svnHEAD'
+
+    def setDependencies( self ):
+        self.dependencies['kde/kde-runtime'] = 'default'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DBUILD_doc=OFF "
+
+if __name__ == '__main__':
+    Package().execute()
---
+++ portage/kde/kde-workspace/kde-workspace-20110130.py
@@ -0,0 +1,39 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/kde-workspace'
+        self.svnTargets['komobranch'] = 'branches/work/komo/kdebase/workspace'
+        if emergePlatform.isCrossCompilingEnabled():
+            self.defaultTarget = 'komobranch'
+        else:
+            self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.dependencies['kde/kde-runtime'] = 'default'
+#        if not emergePlatform.isCrossCompilingEnabled():
+#            self.dependencies['win32libs-bin/fontconfig'] = 'default'
+        self.dependencies['win32libs-bin/freetype'] = 'default'
+        self.dependencies['win32libs-bin/boost'] = 'default'
+        self.dependencies['kdesupport/akonadi'] = 'default'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = ""
+        self.subinfo.options.configure.defines += "-DHOST_BINDIR=%s " \
+            % os.path.join(ROOTDIR, "bin")
+        if self.isTargetBuild():
+            self.subinfo.options.configure.defines += "-DDISABLE_ALL_OPTIONAL_SUBDIRECTORIES=TRUE "
+        self.subinfo.options.configure.defines += "-DBUILD_doc=OFF "
+
+if __name__ == '__main__':
+    Package().execute()
+
