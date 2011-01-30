5c009297f0a08af122286d6ffde021a366ce5008
Patrick von Reth <patrick.vonreth@gmail.com>
2011-01-30 21:57:48 +0000

    adde SaroEngels cmd wrapper
    svn path=/trunk/kdesupport/emerge/; revision=1218053

---
+++ portage/testing/kcwsh/kcwsh-20110130.py
@@ -0,0 +1,21 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/scratch/sengels/kcwsh'
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.shortDescription = "a cmd wrapper for konsole"
+        self.buildDependencies['virtual/base'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DBUILD_GUI_TEST=OFF "
+
+if __name__ == '__main__':
+    Package().execute()
