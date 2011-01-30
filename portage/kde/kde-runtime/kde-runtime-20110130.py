653e86e1ce49699c71a3caa4709e39583d218b58
Ralf Habacker <ralf.habacker@freenet.de>
2011-01-30 12:32:56 +0000

    added new package to reflect repository name
    svn path=/trunk/kdesupport/emerge/; revision=1217999

---
+++ portage/kde/kde-runtime/kde-runtime-20110130.py
@@ -0,0 +1,43 @@
+import info
+import compiler
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://git.kde.org/kde-runtime'
+        self.svnTargets['komobranch'] = 'branches/work/komo/kdebase/runtime'
+        if emergePlatform.isCrossCompilingEnabled():
+            self.defaultTarget = 'komobranch'
+        else:
+            self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.dependencies['kde/kdelibs'] = 'default'
+        self.dependencies['kdesupport/oxygen-icons'] = 'default'
+        if not emergePlatform.isCrossCompilingEnabled():
+            self.dependencies['win32libs-bin/libssh'] = 'default'
+        if compiler.isMinGW_WXX():
+#            self.dependencies['win32libs-bin/libbfd'] = 'default'
+            self.dependencies['win32libs-sources/libbfd-src'] = 'default'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = True
+        self.disableTargetBuild = False
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = ""
+        if emergePlatform.isCrossCompilingEnabled():
+            self.subinfo.options.configure.defines += "-DDISABLE_ALL_OPTIONAL_SUBDIRECTORIES=TRUE "
+
+        self.subinfo.options.configure.defines += "-DHOST_BINDIR=%s " \
+            % os.path.join(ROOTDIR, "bin")
+
+        if self.isTargetBuild():
+            self.subinfo.options.configure.defines += "-DKDEBASE_DISABLE_MULTIMEDIA=ON "
+        self.subinfo.options.configure.defines += "-DBUILD_doc=OFF "
+
+if __name__ == '__main__':
+    Package().execute()
