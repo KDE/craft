81f5c0b162e2533b881a8e611b81561767dffd90
Patrick von Reth <patrick.vonreth@gmail.com>
2011-01-30 13:50:12 +0000

    removed patch from git head
    svn path=/trunk/kdesupport/emerge/; revision=1218009

--- portage/kdesupport/phonon/phonon-4.4.3.20101202.py
+++ portage/kdesupport/phonon/phonon-4.4.3.20101202.py
@@ -10,7 +10,6 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = 'git://git.kde.org/phonon'
         self.svnTargets['4.4'] = 'git://git.kde.org/phonon|4.4'
-        self.patchToApply['gitHEAD'] = ("phonon-20100915.diff", 1)
         self.targets['4.4.3'] = 'http://download.kde.org/download.php?url=stable/phonon/4.4.3/phonon-4.4.3.tar.bz2'
         self.targetInstSrc['4.4.3'] = 'phonon-4.4.3'
         self.targetDigests['4.4.3'] = '50262d590beb648be9dcad6b913b920db19a84f8'
