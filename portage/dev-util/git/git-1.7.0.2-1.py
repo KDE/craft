0a06c907837074cd6dfd834e52abf2339c4be50c
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2011-02-01 12:39:57 +0000

    add missing dependency needed for the new git config commands in qmerge
    svn path=/trunk/kdesupport/emerge/; revision=1218230

--- portage/dev-util/git/git-1.7.0.2-1.py
+++ portage/dev-util/git/git-1.7.0.2-1.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         self.buildDependencies['dev-util/7zip']   = 'default'
+        self.buildDependencies['dev-util/msys']   = 'default'
 
     def setBuildOptions( self ):
         self.disableHostBuild = False
