e25aaf3df57b922ada48852f7a3cac2db5c63f0a
Ralf Habacker <ralf.habacker@freenet.de>
2011-02-03 21:27:34 +0000

    repository has been moved to git
    svn path=/trunk/kdesupport/emerge/; revision=1218704

--- portage/kde-4.6/kdebase-runtime/kdebase-runtime-20080202.py
+++ portage/kde-4.6/kdebase-runtime/kdebase-runtime-20080202.py
@@ -2,7 +2,7 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = 'branches/KDE/4.6/kdebase/runtime'
+        self.svnTargets['svnHEAD'] = '[git]kde:kde-runtime|KDE/4.6|'
         for ver in ['0', '1', '2', '3', '4']:
             self.targets['4.6.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.6.' + ver + '/src/kdebase-runtime-4.6.' + ver + '.tar.bz2'
             self.targetInstSrc['4.6.' + ver] = 'kdebase-runtime-4.6.' + ver
