64ac9c2111f63f479066b6e4d9d9a3024e4778d4
Ralf Habacker <ralf.habacker@freenet.de>
2011-02-10 21:33:27 +0000

    added digest
    svn path=/trunk/kdesupport/emerge/; revision=1219764

--- portage/testing/virtuoso/virtuoso-20100330.py
+++ portage/testing/virtuoso/virtuoso-20100330.py
@@ -16,6 +16,7 @@ class subinfo(info.infoclass):
           arch = 'win64'
         self.targets[ '20100330' ] =  'http://downloads.sourceforge.net/project/virtuoso/virtuoso/6.1.1/vos6-' + arch + '-20100330.zip'
         self.targetInstSrc[ '20100330' ] = "virtuoso-opensource"
+        self.targetDigests['20100330'] = 'f93f7606a636beefa4c669e8fc5d0100217d85c4'
         self.shortDescription = "a middleware and database engine hybrid for RDBMS, ORDBMS, virtual database, RDF, XML, free-text, web application server and file server functionality"
         self.defaultTarget = '20100330'
 
