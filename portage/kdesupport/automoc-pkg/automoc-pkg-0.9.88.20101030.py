43e02b474464c1b575a87bd22d70a46270c8f73b
Ralf Habacker <ralf.habacker@freenet.de>
2011-02-11 22:52:09 +0000

    added option to build automoc using non static qt library, the static option do not compile with qt-static
    svn path=/trunk/kdesupport/emerge/; revision=1219966

--- portage/kdesupport/automoc-pkg/automoc-pkg-0.9.88.20101030.py
+++ portage/kdesupport/automoc-pkg/automoc-pkg-0.9.88.20101030.py
@@ -1,10 +1,14 @@
 import info
 
+static = False
+
 class subinfo( info.infoclass ):
     def setDependencies( self ):
         self.buildDependencies[ 'virtual/base' ] = 'default'
-#        self.dependencies[ 'libs/qt' ] = 'default'
-        self.buildDependencies[ 'libs/qt-static' ] = 'default'
+        if static: 
+            self.buildDependencies[ 'libs/qt-static' ] = 'default'
+        else:
+            self.dependencies[ 'libs/qt' ] = 'default'
         self.buildDependencies[ 'dev-util/upx' ] = 'default'
 
     def setTargets( self ):
@@ -21,13 +25,14 @@ class Package( CMakePackageBase ):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        self.qtstatic = portage.getPackageInstance( 'libs', 'qt-static' )
-        qmake = os.path.join( self.qtstatic.installDir(), "bin", "qmake.exe" )
-        if not os.path.exists( qmake ):
-            utils.warning( "could not find qmake in <%s>" % qmake )
-        ## \todo a standardized way to check if a package is installed in the image dir would be good.
-        self.subinfo.options.configure.defines = "-DQT_QMAKE_EXECUTABLE:FILEPATH=%s" \
-            % qmake.replace( '\\', '/' )
+        if static: 
+            self.qtstatic = portage.getPackageInstance( 'libs', 'qt-static' )
+            qmake = os.path.join( self.qtstatic.installDir(), "bin", "qmake.exe" )
+            if not os.path.exists( qmake ):
+                utils.warning( "could not find qmake in <%s>" % qmake )
+            ## \todo a standardized way to check if a package is installed in the image dir would be good.
+            self.subinfo.options.configure.defines = "-DAUTOMOC_STATIC -DQT_QMAKE_EXECUTABLE:FILEPATH=%s" \
+                % qmake.replace( '\\', '/' )
 
 if __name__ == '__main__':
     Package().execute()
