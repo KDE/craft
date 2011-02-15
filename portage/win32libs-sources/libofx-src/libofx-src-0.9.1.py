e0d4f7b9fca6d522ece9a378ca88c640e647eba5
Cristian Oneț <onet.cristian@gmail.com>
2011-02-15 19:21:54 +0000

    Added libofx version 0.9.2.
    svn path=/trunk/kdesupport/emerge/; revision=1220928

--- portage/win32libs-sources/libofx-src/libofx-src-0.9.1.py
+++ portage/win32libs-sources/libofx-src/libofx-src-0.9.1.py
@@ -1,21 +1,24 @@
 import info
 import compiler
 
-SRC_URI= """
-http://downloads.sourceforge.net/project/libofx/libofx/0.9.1/libofx-0.9.1.tar.gz
-"""
-#http://sourceforge.net/projects/libofx/files/libofx/0.9.1/libofx-0.9.1.tar.gz/download
-
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets['0.9.1'] = SRC_URI
+        self.targets['0.9.1'] = "http://downloads.sourceforge.net/project/libofx/libofx/0.9.1/libofx-0.9.1.tar.gz"
         self.targetInstSrc['0.9.1'] = "libofx-0.9.1"
         self.patchToApply['0.9.1'] = []
         if compiler.isMSVC():
             self.patchToApply['0.9.1'].append(("ofx-msvc.diff", 1))
         self.patchToApply['0.9.1'].append(("libofx-0.9.1-20110107.diff", 1))
+
+        self.targets['0.9.2'] = "http://downloads.sourceforge.net/project/libofx/libofx/0.9.2/libofx-0.9.2.tar.gz"
+        self.targetInstSrc['0.9.2'] = "libofx-0.9.2"
+        self.patchToApply['0.9.2'] = []
+        if compiler.isMSVC():
+            self.patchToApply['0.9.2'].append(("ofx-msvc.diff", 1))
+        self.patchToApply['0.9.2'].append(("libofx-0.9.2-20110215.diff", 1))
+
         self.shortDescription = "a parser and an API for the OFX (Open Financial eXchange) specification"
-        self.defaultTarget = '0.9.1'
+        self.defaultTarget = '0.9.2'
 
     def setDependencies( self ):
         self.dependencies['win32libs-bin/libopensp'] = 'default'
