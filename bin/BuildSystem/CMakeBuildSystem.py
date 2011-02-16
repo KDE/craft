KDE/craft

commit b1c7aff405a26c09b542bd3d3e8dbdaa66626852

review 6492: use BuildSystemBase.makeOptions() instead of duplicating its code
this changes behaviour in that there are more places now where VERBOSE=1
might be appended

svn path=/trunk/kdesupport/emerge/; revision=1221089

bin/BuildSystem/CMakeBuildSystem.py
@@ -149,16 +149,7 @@ class CMakeBuildSystem(BuildSystemBase):
             self.system( self.makeProgramm + " clean", "make clean" )
             command = "ctest -M " + "Nightly" + " -T Start -T Update -T Configure -T Build -T Submit"
         else:
-            command = self.makeProgramm
-
-            if utils.verbose() > 1:
-                command += " VERBOSE=1"
-
-            if self.subinfo.options.make.ignoreErrors:
-                command += " -i"
-
-            if self.subinfo.options.make.makeOptions:
-                command += " %s" % self.subinfo.options.make.makeOptions
+            command = ' '.join(self.makeProgramm, self.makeOptions())
 
         if self.isTargetBuild():
             self.setupTargetToolchain()

bin/BuildSystem/MakeFileBuildSystem.py
@@ -25,16 +25,7 @@ class MakeFileBuildSystem(BuildSystemBase):
         self.enterBuildDir()
         utils.prependPath(self.rootdir, self.envPath)
 
-        command = self.makeProgramm
-
-        if utils.verbose() > 1:
-            command += " VERBOSE=1"
-
-        if self.subinfo.options.make.ignoreErrors:
-            command += " -i"
-
-        if self.subinfo.options.make.makeOptions:
-            command += " %s" % self.subinfo.options.make.makeOptions
+        command = ' '.join(self.makeProgramm, self.makeOptions())
 
         return self.system( command, "make" )
 

bin/BuildSystem/QMakeBuildSystem.py
@@ -66,9 +66,7 @@ class QMakeBuildSystem(BuildSystemBase):
         """implements the make step for Qt projects"""
         self.enterBuildDir()
 
-        command = self.makeProgramm
-
-        command += " %s" % self.makeOptions(options)
+        command = ' '.join(self.makeProgramm, self.makeOptions(options))
 
         return self.system( command, "make" )
 

bin/BuildSystem/AutoToolsBuildSystem.py
@@ -69,16 +69,9 @@ class AutoToolsBuildSystem(BuildSystemBase):
             self.enterBuildDir()
 
         command = self.makeProgram
-        args = ""
-        if self.subinfo.options.make.ignoreErrors:
-            args += " -i"
-
-        if self.subinfo.options.make.makeOptions:
-            args += " %s" % self.subinfo.options.make.makeOptions
+        args = self.makeOptions()
 
         # adding Targets later
-        if utils.verbose() > 1:
-            args += " VERBOSE=1"
         if self.buildInSource:
             if not self.shell.execute(self.sourceDir(), command, args ):
                 utils.die( "while Make'ing. cmd: %s" % command )

bin/BuildSystem/BuildSystemBase.py
@@ -91,8 +91,10 @@ class BuildSystemBase(EmergeBase):
         """return options for make command line"""
         if self.subinfo.options.make.ignoreErrors:
             defines += " -i"
-        if self.subinfo.options.make.makeOptions != None:
+        if self.subinfo.options.make.makeOptions:
             defines += " %s" % self.subinfo.options.make.makeOptions
+        if utils.verbose() > 1:
+            defines += " VERBOSE=1"
         return defines
 
     def setupTargetToolchain(self):

commit 563472a76936b80495eaffcfc6dd28488aecb237

regression from r1221089: fix usage of str.join
svn path=/trunk/kdesupport/emerge/; revision=1221118

bin/BuildSystem/CMakeBuildSystem.py
@@ -149,7 +149,7 @@ class CMakeBuildSystem(BuildSystemBase):
             self.system( self.makeProgramm + " clean", "make clean" )
             command = "ctest -M " + "Nightly" + " -T Start -T Update -T Configure -T Build -T Submit"
         else:
-            command = ' '.join(self.makeProgramm, self.makeOptions())
+            command = ' '.join([self.makeProgramm, self.makeOptions()])
 
         if self.isTargetBuild():
             self.setupTargetToolchain()

bin/BuildSystem/MakeFileBuildSystem.py
@@ -25,7 +25,7 @@ class MakeFileBuildSystem(BuildSystemBase):
         self.enterBuildDir()
         utils.prependPath(self.rootdir, self.envPath)
 
-        command = ' '.join(self.makeProgramm, self.makeOptions())
+        command = ' '.join([self.makeProgramm, self.makeOptions()])
 
         return self.system( command, "make" )
 

bin/BuildSystem/QMakeBuildSystem.py
@@ -66,7 +66,7 @@ class QMakeBuildSystem(BuildSystemBase):
         """implements the make step for Qt projects"""
         self.enterBuildDir()
 
-        command = ' '.join(self.makeProgramm, self.makeOptions(options))
+        command = ' '.join([self.makeProgramm, self.makeOptions(options)])
 
         return self.system( command, "make" )
 