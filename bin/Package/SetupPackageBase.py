0fcc4be3fadc498566f10c2d2424e021495c655b
Ralf Habacker <ralf.habacker@freenet.de>
2011-03-04 07:41:49 +0000

    added new base package SetupPackageBase for 3rdparty installers
    svn path=/trunk/kdesupport/emerge/; revision=1223694

---
+++ bin/Package/SetupPackageBase.py
@@ -0,0 +1,18 @@
+#
+# copyright (c) 2011 Ralf Habacker <ralf.habacker@freenet.de>
+#
+
+from Package.PackageBase import *
+from Source.FileSource import *
+from BuildSystem.BuildSystemBase import *
+from Packager.PackagerBase import *
+
+class SetupPackageBase (PackageBase, FileSource, BuildSystemBase, PackagerBase):
+    """provides a base class for 3rd party installers or msi packages"""
+    def __init__(self):
+        utils.debug("SetupPackageBase.__init__ called", 2)
+        PackageBase.__init__(self)
+        FileSource.__init__(self)
+        BuildSystemBase.__init__(self)
+        PackagerBase.__init__(self)
+        self.subinfo.options.unpack.runInstaller = True
