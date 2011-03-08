d4e2ab13cd309de00fa63fc31e737e4076e28f9f
Andrius da Costa Ribas <andriusmao@gmail.com>
2011-03-08 22:38:13 +0000

    diff filename has been changed.
    svn path=/trunk/kdesupport/emerge/; revision=1224195

--- portage/kdesupport/libqtsoap/libqtsoap-2.7._1.py
+++ portage/kdesupport/libqtsoap/libqtsoap-2.7._1.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.targets['2.7_1'] = 'http://get.qt.nokia.com/qt/solutions/lgpl/qtsoap-2.7_1-opensource.zip'
-        self.patchToApply['2.7_1'] = ('qtsoap-2.7_1-opensource-20101108.diff',1)
+        self.patchToApply['2.7_1'] = ('qtsoap-2.7_1-opensource-20110308.diff',1)
         self.targetDigests['2.7_1'] = '933ffd4215052af7eed48fc9492a1cd6996c7641'
         self.targetInstSrc['2.7_1'] = 'qtsoap-2.7_1-opensource'
         self.defaultTarget = '2.7_1'
