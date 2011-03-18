3df4279abc1d3906e1f7bbea41ba24d2818b85b2
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2011-03-18 08:47:15 +0000

    make pykde find the pyqt4 libs. Warning: Wild hack - must be fixed
    svn path=/trunk/kdesupport/emerge/; revision=1225217

--- portage/testing/pykde/pykde-20110131.py
+++ portage/testing/pykde/pykde-20110131.py
@@ -24,6 +24,16 @@ class Package(CMakePackageBase):
         haveLocalPython = os.path.exists(localPythonPath)
         if haveLocalPython:
             self.subinfo.options.merge.destinationPath = "emerge/python"
+        else:
+            # PyQt4 does not install itself in C:\Python27\Lib\site-packages
+            # so how is python supposed to be able to import it?
+            # TODO: this is a wild hack...
+            pyqtDir = os.path.join(self.rootdir,'build','testing','pyqt-4.8.3',
+                    'image-mingw4-Debug-4.8.3','Lib','site-packages')
+            if pyqtDir not in os.getenv("PYTHONPATH").split(';'):
+                newPath = ";".join([os.getenv("PYTHONPATH"), pyqtDir])
+                os.environ["PYTHONPATH"] = newPath
+                utils.debug('special PYTHONPATH for pykde, so we can import PyQt4: %s' % newPath)
 
 if __name__ == '__main__':
     Package().execute()
