0e126305fbf510f2156561334563a73a29084afe
Andre Heinecke <aheinecke@intevation.de>
2011-04-06 10:28:12 +0000

    Add target for nepomukshell, a debugging tool for nepomuk
    svn path=/trunk/kdesupport/emerge/; revision=1227214

---
+++ portage/extragear/nepomukshell/nepomukshell-20110604.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import utils
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['gitHEAD'] = '[git]kde:nepomukshell'
+        self.defaultTarget = 'gitHEAD'
+        self.shortDescription =(
+                "NepomukShell is a maintenance and debugging "
+                "tool intended for developers. It allows to browse, "
+                "query, and edit Nepomuk resources.")
+
+    def setDependencies(self):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.dependencies['libs/qt'] = 'default'
+        self.dependencies['kde/kdelibs'] = 'default'
+        self.dependencies['kdesupport/soprano'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
