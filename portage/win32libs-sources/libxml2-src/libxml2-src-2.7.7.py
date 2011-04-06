149678f493b051e42d067a82303c79c3afc9db1c
Ralf Habacker <ralf.habacker@freenet.de>
2011-04-06 10:12:56 +0000

    added patch from http://mail.gnome.org/archives/xml/2011-April/msg00010.html provided by Wolfgang Rohdewald - this should fix the annoying meinproc4 crash for now
    svn path=/trunk/kdesupport/emerge/; revision=1227213

--- portage/win32libs-sources/libxml2-src/libxml2-src-2.7.7.py
+++ portage/win32libs-sources/libxml2-src/libxml2-src-2.7.7.py
@@ -7,7 +7,7 @@ class subinfo(info.infoclass):
         for ver in ['2.7.7', '2.7.8']:
             self.targets[ver] = 'ftp://xmlsoft.org/libxml2/libxml2-' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'libxml2-' + ver
-        self.patchToApply['2.7.7'] = ("libxml2-2.7.7-20101103.diff", 1)
+        self.patchToApply['2.7.7'] = ("libxml2-2.7.7-20110406.diff", 1)
         self.targetDigests['2.7.7'] = '8592824a2788574a172cbddcdc72f734ff87abe3'
         self.patchToApply['2.7.8'] = ("libxml2-2.7.8-20110105.diff", 1)
         self.targetDigests['2.7.8'] = '859dd535edbb851cc15b64740ee06551a7a17d40'
