c9af06fb1992acd8ed36c26775b56453026ae6ca
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2011-04-08 04:39:19 +0000

    rename ActionBase to ArgBase
    svn path=/trunk/kdesupport/emerge/; revision=1227357

--- bin/action.py
+++ bin/action.py
@@ -26,17 +26,17 @@ class AutoNumberingType(type):
         setattr(cls,'number', classNumber)
  
 def __init__():
-    if not ActionBase.defined:
+    if not ArgBase.defined:
         actionClasses = []
         for v in globals().values():
             if hasattr(v, "__mro__"):
-                if len(v.__mro__) > 2 and  v.__mro__[-2] == ActionBase:
+                if len(v.__mro__) > 2 and  v.__mro__[-2] == ArgBase:
                     action = v()
                     actionClasses.append(action)
-    ActionBase.defined = sorted(actionClasses, key=lambda x: x.number)
+    ArgBase.defined = sorted(actionClasses, key=lambda x: x.number)
     
 
-class ActionBase(object):
+class ArgBase(object):
     """this class defines available actions"""
     __metaclass__ = AutoNumberingType
     defined = []
@@ -51,53 +51,53 @@ class ActionBase(object):
         helpString = self.__class__.__doc__
         parser.add_argument( '--'+self.argName(), default=False, action='store_true', help=m18n(helpString), dest=self.destString())
 
-class ActionFetch( ActionBase ):
+class ActionFetch( ArgBase ):
     """retrieve package sources either from files, archives or version control systems."""
 
-class ActionUnpack( ActionBase ):
+class ActionUnpack( ArgBase ):
     """unpack package sources and make up the build directory"""
 
-class ActionCompile( ActionBase ):
+class ActionCompile( ArgBase ):
     """configure and make the package"""
 
-class ActionConfigure( ActionBase ):
+class ActionConfigure( ArgBase ):
     """configure the package"""
 
-class ActionMake( ActionBase ):
+class ActionMake( ArgBase ):
     """run compiler and buildsystem specific make tool"""
 
-class ActionInstall( ActionBase ):
+class ActionInstall( ArgBase ):
     """install the generated files into the image 
        directory of the related package"""
 
-class ActionMerge( ActionBase ):
+class ActionMerge( ArgBase ):
     """merge the image directory into the merge root"""
 
-class ActionUnmerge( ActionBase ):
+class ActionUnmerge( ArgBase ):
     """uninstalls a package from the merge root
 (requires a working manifest directory). Unmerge only delete unmodified files by default.
 You may use the -f or --force option to let unmerge delete all files unconditional."""
 
-class ActionManifest( ActionBase ):
+class ActionManifest( ArgBase ):
     """creates the files contained in the manifest dir"""
 
-class ActionTest( ActionBase ):
+class ActionTest( ArgBase ):
     """run the unittests if they are present"""
 
-class ActionPackage( ActionBase ):
+class ActionPackage( ArgBase ):
     """create an installable package from the image directory"""
 
-class ActionPrint_revision( ActionBase ):
+class ActionPrint_revision( ArgBase ):
     """print the revision that the source repository
 of this package currently has or nothing if there is no repository"""
 
-class ActionPrint_targets( ActionBase ):
+class ActionPrint_targets( ArgBase ):
     """print all the different targets one package
             can contain: different releases might have different tags that are build as targets of a
             package. As an example: You could build the latest amarok sources with the target 'svnHEAD'
             the previous '1.80' release would be contained as target '1.80'."""
 
-class ActionDumpdeps( ActionBase ):
+class ActionDumpdeps( ArgBase ):
     """alternative name for dump-deps"""
 
 class ActionDump_deps(ActionDumpdeps):
@@ -108,32 +108,32 @@ class ActionDump_deps(ActionDumpdeps):
     def destString(self):
         return 'dumpdeps'
 
-class ActionCleanallbuilds( ActionBase ):
+class ActionCleanallbuilds( ArgBase ):
     """clean complete build directory"""
 
-class ActionCleanbuild( ActionBase ):
+class ActionCleanbuild( ArgBase ):
     """clean build directory and image directories for the specified package"""
 
-class ActionCheckdigest( ActionBase ):
+class ActionCheckdigest( ArgBase ):
     """check digest for the specified package.
             If no digest is available calculate and print digests"""
 
-class ActionCleanimage( ActionBase ):
+class ActionCleanimage( ArgBase ):
     """clean image directory for the specified
             package and target"""
 
-class ActionCreatepatch( ActionBase ):
+class ActionCreatepatch( ArgBase ):
     """create source patch file for the specific
             package based on the original archive file or checkout revision of
             the used software revision control system"""
 
-class ActionDisableBuildhost( ActionBase ):
+class ActionDisableBuildhost( ArgBase ):
     """disable the building for the host"""
 
-class ActionDisableBuild_target( ActionBase ):
+class ActionDisableBuild_target( ArgBase ):
     """disable the building for the target"""
 
-class ActionInstall_deps( ActionBase ):
+class ActionInstall_deps( ArgBase ):
     """fetch and install all required 
             dependencies for the specified package"""
             
@@ -142,7 +142,7 @@ __init__()
 # for debugging
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description='package build tool')
-    for a in ActionBase.defined:
+    for a in ArgBase.defined:
         a.insertIntoParser( p )
     args = p.parse_args()
     print args
