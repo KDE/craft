7b67037eca3ebec751f50ea508884ff8fee98855
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2011-04-08 04:39:21 +0000

    implement argument groups and some other things
    svn path=/trunk/kdesupport/emerge/; revision=1227358

--- bin/action.py
+++ bin/action.py
@@ -5,11 +5,10 @@
 # (c) copyright 2011 Ralf Habacker <ralf.habacker@freenet.de>
 # (c) copyright 2011 Wolfgang Rohdewald <wolfgang@rohdewald.de>
 #
-# @TODO review help texts 
-# @TODO fix parameter ordering (order of class list below)
+# @TODO review help texts
 #
 #
-import argparse
+import sys, argparse
 
 def m18n(s):
     """prelimary function for internationalization support"""
@@ -24,126 +23,207 @@ class AutoNumberingType(type):
         global classNumber
         classNumber = classNumber + 1
         setattr(cls,'number', classNumber)
- 
+
 def __init__():
     if not ArgBase.defined:
-        actionClasses = []
+        argClasses = []
         for v in globals().values():
             if hasattr(v, "__mro__"):
-                if len(v.__mro__) > 2 and  v.__mro__[-2] == ArgBase:
-                    action = v()
-                    actionClasses.append(action)
-    ArgBase.defined = sorted(actionClasses, key=lambda x: x.number)
-    
+                if v.__name__.endswith('Arg'):
+                    argClass = v()
+                    argClasses.append(argClass)
+    ArgBase.defined = sorted(argClasses, key=lambda x: x.number)
+    for argClass in ArgBase.defined:
+        argClass.setup()
 
 class ArgBase(object):
     """this class defines available actions"""
     __metaclass__ = AutoNumberingType
     defined = []
+    argGroupName = None
+    argGroups = {}
+    parser = None
+    alternativeNames = []
+    classes = {}
 
-    def argName(self):
-        return self.__class__.__name__[6:].lower().replace('_','-')
+    def setup(self):
+        """we do not want to do this in __init__ because invocation order matters
+           otherwise the groups would not be ordered as they appear in this file"""
+        self.argValue = None
+        if not ArgBase.parser:
+            ArgBase.parser = argparse.ArgumentParser(
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+                description="""
+emerge [[ command and flags ] [ singletarget ]
+        [ command and flags ] [ singletarget ]
+        ... ]
 
-    def destString(self):
-        return self.argName()
+where singletarget can be of the form:
+    category
+    package
+    category/package
 
-    def insertIntoParser( self, parser ):
-        helpString = self.__class__.__doc__
-        parser.add_argument( '--'+self.argName(), default=False, action='store_true', help=m18n(helpString), dest=self.destString())
+Emerge is a tool for building KDE-related software under Windows. emerge
+automates it, looks for the dependencies and fetches them automatically.
+Some options should be used with extreme caution since they will
+make your kde installation unusable in 999 out of 1000 cases.""")
+        argGroupName = self.argGroupName
+        if argGroupName not in self.argGroups:
+            self.argGroups[argGroupName] = self.parser.add_argument_group(argGroupName)
+        self.argGroup = self.argGroups[argGroupName]
+        self.helpString = self.__class__.__doc__
+        self.argName = self.__class__.__name__[:-3].lower().replace('_','-')
+        self.classes[self.argName] = self
 
-class ActionFetch( ArgBase ):
-    """retrieve package sources either from files, archives or version control systems."""
+    def argStrings(self):
+        result = ['--' + self.argName]
+        for name in self.alternativeNames:
+            if len(name) == 1:
+                result.insert(0, '-' + name)
+            else:
+                result.append('--' + name)
+        return result
 
-class ActionUnpack( ArgBase ):
-    """unpack package sources and make up the build directory"""
+    def insertIntoParser( self ):
+        """default for parameters without value"""
+        self.argGroup.add_argument(*self.argStrings(),
+                action='store_true', help=m18n(self.helpString), dest=self.argName)
 
-class ActionCompile( ArgBase ):
-    """configure and make the package"""
+class CommandArgBase(ArgBase):
+    """for command arguments"""
 
-class ActionConfigure( ArgBase ):
-    """configure the package"""
+class GeneralCommandArgBase(CommandArgBase):
+    """for non package related arguments without value"""
+    argGroupName = "Commands without packagename"
 
-class ActionMake( ArgBase ):
-    """run compiler and buildsystem specific make tool"""
+class PackageCommandArgBase(CommandArgBase):
+    """for package related arguments with package value"""
+    argGroupName = "Commands (must have a packagename). Only one such command is allowed"
 
-class ActionInstall( ArgBase ):
-    """install the generated files into the image 
-       directory of the related package"""
+    def insertIntoParser( self ):
+        self.argGroup.add_argument(*self.argStrings(), default=False,
+                help=m18n(self.helpString),
+                metavar='PACKAGE',
+                dest=self.argName)
 
-class ActionMerge( ArgBase ):
-    """merge the image directory into the merge root"""
+class FlagArgBase(ArgBase):
+    """for flag arguments"""
+    argGroupName = "Flags"
 
-class ActionUnmerge( ArgBase ):
-    """uninstalls a package from the merge root
-(requires a working manifest directory). Unmerge only delete unmodified files by default.
-You may use the -f or --force option to let unmerge delete all files unconditional."""
+class InternalArgBase(object):
+    """mixin for internal arguments. Use this as first parent class"""
+    argGroupName = "Internal options or options that aren't fully implemented yet"
 
-class ActionManifest( ArgBase ):
-    """creates the files contained in the manifest dir"""
+class ObsoleteArgBase(object):
+    """mixin for obsolete arguments. Use this as first parent class"""
+    argGroupName = "Obsolete"
 
-class ActionTest( ArgBase ):
-    """run the unittests if they are present"""
-
-class ActionPackage( ArgBase ):
-    """create an installable package from the image directory"""
-
-class ActionPrint_revision( ArgBase ):
-    """print the revision that the source repository
-of this package currently has or nothing if there is no repository"""
-
-class ActionPrint_targets( ArgBase ):
-    """print all the different targets one package
-            can contain: different releases might have different tags that are build as targets of a
-            package. As an example: You could build the latest amarok sources with the target 'svnHEAD'
-            the previous '1.80' release would be contained as target '1.80'."""
-
-class ActionDumpdeps( ArgBase ):
-    """alternative name for dump-deps"""
-
-class ActionDump_deps(ActionDumpdeps):
-    """print all the different targets one
-            package can contain: different releases might have different tags that are build as targets
-            of a package. As an example: You could build the latest amarok sources with the target
-            'svnHEAD' the previous '1.80' release would be contained as target '1.80'."""
-    def destString(self):
-        return 'dumpdeps'
-
-class ActionCleanallbuilds( ArgBase ):
+class CleanallbuildsArg( GeneralCommandArgBase ):
     """clean complete build directory"""
 
-class ActionCleanbuild( ArgBase ):
+class FetchArg( PackageCommandArgBase ):
+    """retrieve package sources either from files,
+       archives or version control systems."""
+
+class UnpackArg( PackageCommandArgBase ):
+    """unpack package sources and make up the build directory"""
+
+class CompileArg( PackageCommandArgBase ):
+    """configure and make the package"""
+
+class ConfigureArg( PackageCommandArgBase ):
+    """configure the package"""
+
+class MakeArg( PackageCommandArgBase ):
+    """run compiler and buildsystem specific make tool"""
+
+class InstallArg( PackageCommandArgBase ):
+    """install the generated files into the image
+       directory of the related package"""
+
+class MergeArg( PackageCommandArgBase ):
+    """merge the image directory into the merge root"""
+
+class UnmergeArg( PackageCommandArgBase ):
+    """uninstalls a package from the merge root
+       (requires a working manifest directory).
+       Unmerge only delete unmodified files by default.
+       You may use the -f or --force option to let unmerge
+       delete all files unconditional."""
+
+class ManifestArg( PackageCommandArgBase ):
+    """creates the files contained in the manifest dir"""
+
+class TestArg( PackageCommandArgBase ):
+    """run the unittests if they are present"""
+
+class PackageArg( PackageCommandArgBase ):
+    """create an installable package from the image directory"""
+
+class Print_revisionArg( PackageCommandArgBase ):
+    """print the revision that the source repository
+       of this package currently has or nothing if there is no repository"""
+
+class Print_targetsArg( PackageCommandArgBase ):
+    """print all the different targets one package
+       can contain: different releases might have different tags that are build as targets of a
+       package. As an example: You could build the latest amarok sources with the target 'svnHEAD'
+       the previous '1.80' release would be contained as target '1.80'."""
+
+class DumpdepsArg( PackageCommandArgBase ):
+    """print all the different targets one
+       package can contain: different releases might have different tags that are build as targets
+       of a package. As an example: You could build the latest amarok sources with the target
+       'svnHEAD' the previous '1.80' release would be contained as target '1.80'."""
+    alternativeNames = ['dump-deps']
+
+class CleanbuildArg( PackageCommandArgBase ):
     """clean build directory and image directories for the specified package"""
 
-class ActionCheckdigest( ArgBase ):
+class CheckdigestArg( PackageCommandArgBase ):
     """check digest for the specified package.
-            If no digest is available calculate and print digests"""
+       If no digest is available calculate and print digests"""
 
-class ActionCleanimage( ArgBase ):
+class CleanimageArg( PackageCommandArgBase ):
     """clean image directory for the specified
-            package and target"""
+       package and target"""
 
-class ActionCreatepatch( ArgBase ):
+class CreatepatchArg( PackageCommandArgBase ):
     """create source patch file for the specific
-            package based on the original archive file or checkout revision of
-            the used software revision control system"""
+       package based on the original archive file or checkout revision of
+       the used software revision control system"""
 
-class ActionDisableBuildhost( ArgBase ):
+class DisableBuildhostArg( FlagArgBase ):
     """disable the building for the host"""
 
-class ActionDisableBuild_target( ArgBase ):
+class DisableBuild_targetArg( FlagArgBase ):
     """disable the building for the target"""
 
-class ActionInstall_deps( ArgBase ):
-    """fetch and install all required 
-            dependencies for the specified package"""
-            
+class Install_depsArg( PackageCommandArgBase ):
+    """fetch and install all required
+       dependencies for the specified package"""
+
+class Version_dirArg(ObsoleteArgBase, PackageCommandArgBase):
+    """whatever this does"""
+
+class ContinueArg(InternalArgBase, FlagArgBase):
+    """after having done the package command, continue with
+       the next commands this package normally gets"""
+    alternativeNames = ['c']
+
 __init__()
 
 # for debugging
 if __name__ == "__main__":
-    p = argparse.ArgumentParser(description='package build tool')
     for a in ArgBase.defined:
-        a.insertIntoParser( p )
-    args = p.parse_args()
-    print args
+        a.insertIntoParser()
+    args = ArgBase.parser.parse_args()
+
+    # put the values into their respective classes:
+    for name in args.__dict__:
+        ArgBase.classes[name].argValue = args.__dict__[name]
+    if len([x for x in ArgBase.classes.values()
+            if isinstance(x, PackageCommandArgBase) and x.argValue]) > 1:
+        print 'only one command with PACKAGE may be given'
+        sys.exit(2)
 
