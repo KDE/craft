671383dd4cae4f7e829b4727469f6258469b9bf3
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-04-15 09:04:15 +0000

    fix mingw debug builds
    svn path=/trunk/kdesupport/emerge/; revision=1228082

--- portage/enterprise5/kdelibs-e5/kdelibs-e5-20110117.py
+++ portage/enterprise5/kdelibs-e5/kdelibs-e5-20110117.py
@@ -9,7 +9,8 @@ class subinfo(info.infoclass):
                     'ftp://ftp.kde.org/pub/kde/stable/4.6.' + ver + \
                     '/src/kdelibs-4.6.' + ver + '.tar.bz2'
             self.targetInstSrc['4.6.' + ver] = 'kdelibs-4.6.' + ver
-        self.patchToApply['4.6.2'] = ( 'damn-you-meinproc.diff', 1 )
+        self.patchToApply['4.6.2'] = [( 'damn-you-meinproc.diff', 1 ),
+                                      ( 'fix_debug_mingw_build.diff', 1 )
 
         self.svnTargets['20091111'] = 'tags/kdepim/pe5.20091111/kdelibs'
         self.svnTargets['20091123'] = 'tags/kdepim/pe5.20091123/kdelibs'
