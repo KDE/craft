9bf02984e06618e244ebde8704d29824b503172d
Ralf Habacker <ralf.habacker@freenet.de>
2011-05-05 07:01:47 +0000

    added standalone package for packager
    svn path=/trunk/kdesupport/emerge/; revision=1230456

---
+++ portage/kdesupport/kdewin-packager-static/kdewin-packager-static-0.9.8.py
@@ -0,0 +1,34 @@
+import info
+import os
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['HEAD'] = '[git]kde:kdewin-installer'
+        self.defaultTarget = 'HEAD'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.buildDependencies['libs/qt-static'] = 'default'
+        self.buildDependencies['dev-util/upx'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.noDefaultInclude = True
+        self.subinfo.options.configure.noDefaultLib = True
+
+        self.qtstatic = portage.getPackageInstance('libs','qt-static')
+        self.qtstatic.setBuildTarget('4.5.2-patched')
+        qmake = os.path.join(self.qtstatic.installDir(), "bin", "qmake.exe")
+        if not os.path.exists(qmake):
+            utils.warning("could not find qmake in <%s>" % qmake)
+        ## \todo a standardized way to check if a package is installed in the image dir would be good.
+        self.subinfo.options.configure.defines = "-DQT_QMAKE_EXECUTABLE:FILEPATH=%s" \
+            % qmake.replace('\\', '/')
+        self.subinfo.options.configure.defines += " -DBUILD_PACKAGER_ONLY=ON"
+
+if __name__ == '__main__':
+    Package().execute()
---
+++ portage/kdesupport/kdewin-packager/kdewin-packager-0.9.8.py
@@ -0,0 +1,22 @@
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.dependencies['libs/qt'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['HEAD'] = '[git]kde:kdewin-installer'
+        self.defaultTarget = 'HEAD'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.make.supportsMultijob = False
+        self.subinfo.options.configure.defines = " -DBUILD_PACKAGER_ONLY=ON"
+
+if __name__ == '__main__':
+    Package().execute()
