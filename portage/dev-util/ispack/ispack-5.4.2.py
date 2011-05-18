4975e41150ca1c702b39b5c63aa999518afaca96
Ralf Habacker <ralf.habacker@freenet.de>
2011-05-18 06:02:02 +0000

    added ispack used by InnoSetupPackager
    svn path=/trunk/kdesupport/emerge/; revision=1232498

---
+++ portage/dev-util/ispack/ispack-5.4.2.py
@@ -0,0 +1,16 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.targets['5.4.2'] = 'http://www.xs4all.nl/~mlaan2/ispack/ispack-5.4.2.exe'
+        self.defaultTarget = '5.4.2'
+
+from Package.SetupPackageBase import *
+
+class Package(SetupPackageBase):
+    def __init__( self):
+        self.subinfo = subinfo()
+        SetupPackageBase.__init__(self)
+            
+if __name__ == '__main__':
+    Package().execute()
