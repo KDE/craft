e8441ab157d4d6ddd82a33ff4bbdeede41ca7ece
Patrick Spendrin <ps_ml@gmx.de>
2011-05-20 22:30:12 +0000

    fix building apr (the apache portage runtime) including apr-iconv and apr-util
    svn path=/trunk/kdesupport/emerge/; revision=1232895

--- portage/testing/apr-src/apr-src-1.4.4.py
+++ portage/testing/apr-src/apr-src-1.4.4.py
@@ -3,11 +3,16 @@ import compiler
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in ['1.4.4']:
-            self.targets[ ver ] = 'http://apache.mirror.digionline.de/apr/apr-' + ver + '.tar.bz2'
-            self.targetInstSrc[ ver ] = 'apr-' + ver
-        self.targetDigests['1.4.4'] = 'd05cd65ec169c06174ca7c8978179289777f8dae'
-        self.defaultTarget = '1.4.4'
+        self.targets[ '1.3.11' ] = ['http://apache.mirror.digionline.de/apr/apr-util-1.3.11.tar.bz2',
+                                   'http://apache.mirror.digionline.de/apr/apr-1.4.4.tar.bz2',
+                                   'http://apache.mirror.digionline.de/apr/apr-iconv-1.2.1.tar.bz2'
+                                   ]
+        self.targetDigests['1.3.11'] = ['dc71af7684f76d56f06b4fc8763a8606bd861e84',
+                                        'd05cd65ec169c06174ca7c8978179289777f8dae',
+                                        'c4707c92472dace3d96dd9d5d161d078b9797608']
+        self.patchToApply['1.3.11'] = [('apr-iconv-1.2.1-20110521.diff', 0), ('apr-util-1.3.11-20110521.diff', 0)]
+        self.defaultTarget = '1.3.11'
+        self.options.make.supportsMultijob = False
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -19,11 +24,19 @@ class Package( CMakePackageBase ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
 
+    def unpack( self ):
+        CMakePackageBase.unpack( self )
+        names = ['apr-util', 'apr', 'apr-iconv']
+        for i in range(3):
+            src = os.path.join( self.sourceDir(), os.path.basename(self.subinfo.targetAt(i)).replace(".tar.bz2", "") )
+            dst = os.path.join( self.sourceDir(), names[i] )
+            shutil.move( src, dst )
+        return True
+
     def configure( self ):
         return True
 
-    def make( self ):
-        self.enterSourceDir()
+    def _getConfig( self ):
         cfg = "Win32"
         # for x64:
         # cfg = "x64"
@@ -31,13 +44,19 @@ class Package( CMakePackageBase ):
             cfg += " Release"
         else:
             cfg += " Debug"
+        return cfg
 
-        self.system( "%s -f Makefile.win USEMAK=1 CFG=\"%s\" PREFIX=\"%s\" buildall" % ( self.makeProgramm, cfg, self.imageDir() ) )
+    def make( self ):
+        self.enterSourceDir()
+        os.chdir("apr-util")
+
+        self.system( "%s -f Makefile.win USEMAK=1 ARCH=\"%s\" PREFIX=\"%s\" buildall" % ( self.makeProgramm, self._getConfig(), self.imageDir() ) )
         return True
 
     def install( self ):
         self.enterSourceDir()
-        self.system( "%s -f Makefile.win PREFIX=%s install" % ( self.makeProgramm, self.imageDir() ) )
+        os.chdir("apr-util")
+        self.system( "%s -f Makefile.win USEMAK=1 ARCH=\"%s\" PREFIX=%s install" % ( self.makeProgramm, self._getConfig(), self.imageDir() ) )
         return True
 
 if __name__ == '__main__':
