5ad07e62cbb31ebe65cc835b4e5b12d87c29bd84
Patrick Spendrin <ps_ml@gmx.de>
2011-05-23 23:33:04 +0000

    update versions to apr 1.4.5 and apr-util 1.3.12
    svn path=/trunk/kdesupport/emerge/; revision=1233297

--- portage/testing/apr-src/apr-src-1.4.5.py
+++ portage/testing/apr-src/apr-src-1.4.5.py
@@ -3,15 +3,15 @@ import compiler
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets[ '1.4.4' ] = ['http://apache.mirror.digionline.de/apr/apr-util-1.3.11.tar.bz2',
-                                   'http://apache.mirror.digionline.de/apr/apr-1.4.4.tar.bz2',
+        self.targets[ '1.4.5' ] = ['http://apache.mirror.digionline.de/apr/apr-util-1.3.12.tar.bz2',
+                                   'http://apache.mirror.digionline.de/apr/apr-1.4.5.tar.bz2',
                                    'http://apache.mirror.digionline.de/apr/apr-iconv-1.2.1.tar.bz2'
                                    ]
-        self.targetDigests['1.4.4'] = ['dc71af7684f76d56f06b4fc8763a8606bd861e84',
-                                        'd05cd65ec169c06174ca7c8978179289777f8dae',
-                                        'c4707c92472dace3d96dd9d5d161d078b9797608']
-        self.patchToApply['1.4.4'] = [('apr-iconv-1.2.1-20110521.diff', 0), ('apr-util-1.3.11-20110521.diff', 0)]
-        self.defaultTarget = '1.4.4'
+        self.targetDigests['1.4.5'] = ['4902165fc5f2f077afbcc7ddf7ebbf61556a1cda',
+                                       '517de5e3cc1e3be810d9bc95508ab66bb8ebe7cb',
+                                       'c4707c92472dace3d96dd9d5d161d078b9797608']
+        self.patchToApply['1.4.5'] = [('apr-iconv-1.2.1-20110521.diff', 0), ('apr-util-1.3.12-20110524.diff', 0)]
+        self.defaultTarget = '1.4.5'
         self.options.make.supportsMultijob = False
 
     def setDependencies( self ):
@@ -30,7 +30,7 @@ class Package( CMakePackageBase ):
         for i in range(3):
             src = os.path.join( self.sourceDir(), os.path.basename(self.subinfo.targetAt(i)).replace(".tar.bz2", "") )
             dst = os.path.join( self.sourceDir(), names[i] )
-            shutil.move( src, dst )
+#            shutil.move( src, dst )
         return True
 
     def configure( self ):
