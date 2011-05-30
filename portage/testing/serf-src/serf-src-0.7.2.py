b0707ed3547fda2f5636b88127551ec359506fb5
Patrick Spendrin <ps_ml@gmx.de>
2011-05-30 08:59:52 +0000

    build a fixed version of serf
    svn path=/trunk/kdesupport/emerge/; revision=1234217

---
+++ portage/testing/serf-src/serf-src-0.7.2.py
@@ -0,0 +1,23 @@
+import info
+import compiler
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.targets['0.7.2'] = 'http://serf.googlecode.com/files/serf-0.7.2.tar.bz2'
+        self.targetInstSrc['0.7.2'] = 'serf-0.7.2'
+        self.targetDigests['0.7.2'] = '132fbb13d50c4f849231eee79dcada8cde3ecad2'
+        self.patchToApply['0.7.2'] = [("select-poll.patch", 0), ("serf-0.7.2-20110522.diff", 1)]
+        self.defaultTarget = '0.7.2'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
