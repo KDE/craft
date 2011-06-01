30b3aaa7dabfb8ce67dc8c4244ac50dbcb396491
Patrick Spendrin <ps_ml@gmx.de>
2011-06-01 22:40:08 +0000

    add a way to install nsis without directly installing it. We must still adapt the NSISPackager to also detect this version of nsis.
    svn path=/trunk/kdesupport/emerge/; revision=1234615

---
+++ portage/dev-util/nsis/nsis-20110601.py
@@ -0,0 +1,38 @@
+import info
+import os
+import shutil
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.buildDependencies['virtual/bin-base'] = 'default'
+
+    def setTargets( self ):
+        self.targets['2.46'] = 'http://downloads.sourceforge.net/sourceforge/nsis/nsis-2.46.zip'
+        self.targetDigests['2.46'] = 'adeff823a1f8af3c19783700a6b8d9054cf0f3c2'
+        self.defaultTarget = '2.46'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+from Package.BinaryPackageBase import *
+
+class Package(BinaryPackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        self.subinfo.options.merge.ignoreBuildType = True
+        self.subinfo.options.merge.destinationPath = os.path.join("dev-utils")
+        BinaryPackageBase.__init__(self)
+
+    def unpack( self ):
+        if not BinaryPackageBase.unpack(self):
+            return False
+        for f in os.listdir(os.path.join(self.imageDir(), "nsis-2.46")):
+            shutil.move(os.path.join(self.imageDir(), "nsis-2.46", f), self.imageDir())
+        os.rmdir(os.path.join(self.imageDir(), "nsis-2.46"))
+        for f in ['makensis', 'makensisw', 'nsis']:
+            shutil.copy(os.path.join(self.packageDir(), "wrapper.bat"), os.path.join(self.imageDir(), "bin", f + ".bat"))
+        return True
+
+if __name__ == '__main__':
+    Package().execute()
---
+++ portage/dev-util/nsis/wrapper.bat
@@ -0,0 +1,2 @@
+@echo off
+%~dp0..\%~n0.exe %*
