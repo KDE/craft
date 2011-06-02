cc0683895be8b1ae2f6d1dd98ff13b00aa7880ee
Patrick Spendrin <ps_ml@gmx.de>
2011-06-02 21:10:34 +0000

    add cwrsync since the cwrsync .zip/installer combination requires admin rights to install, I repackaged it here
    svn path=/trunk/kdesupport/emerge/; revision=1234869

---
+++ portage/dev-util/cwrsync/cwrsync-4.1.0.py
@@ -0,0 +1,30 @@
+import info
+import os
+
+# since the cwrsync .zip/installer combination requires admin privileges to install
+# cwrsync is repackaged here
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.targets['4.1.0'] = 'http://downloads.sourceforge.net/sourceforge/kde-windows/cwrsync-4.1.0.7z'
+        self.targetDigests['4.1.0'] = '7db69d0191aacf5bd0fd64a7b665f55ae78a50ad'
+        self.defaultTarget = '4.1.0'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/bin-base'] = 'default'
+
+from Package.BinaryPackageBase import *
+
+class Package( BinaryPackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        self.subinfo.options.merge.ignoreBuildType = True
+        self.subinfo.options.merge.destinationPath = os.path.join( "dev-utils", "rsync" )
+        BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
