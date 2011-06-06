1ef2134dbec2cfc3affcc39073c4d4453b01a7ef
Patrick Spendrin <ps_ml@gmx.de>
2011-06-06 21:03:40 +0000

    add another way to build a standalone automoc this will become part of cmake sooner or later
    svn path=/trunk/kdesupport/emerge/; revision=1235561

---
+++ portage/win32libs-sources/automoc-src/automoc-src-0.9.90.20110606.py
@@ -0,0 +1,24 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setDependencies( self ):
+        self.buildDependencies[ 'virtual/base' ] = 'default'
+        self.buildDependencies[ 'dev-util/upx' ] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets[ 'gitHEAD' ] = '[git]kde:automoc|no-qt|'
+        self.defaultTarget = 'gitHEAD'
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
