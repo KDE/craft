ff29f396a3c9d8566c34fcd536a0c19300f0dffd
Patrick von Reth <patrick.vonreth@gmail.com>
2011-06-27 18:24:12 +0000

    added some more packages
    svn path=/trunk/kdesupport/emerge/; revision=1238526

--- portage/kde/kdegraphics/kdegraphics-20100627.py
+++ portage/kde/kdegraphics/kdegraphics-20100627.py
@@ -1,4 +1,5 @@
 import info
+import base
 
 class subinfo(info.infoclass):
     def setTargets( self ):
@@ -11,8 +12,21 @@ class subinfo(info.infoclass):
         self.dependencies['kde/kolourpaint'] = 'default'
         self.dependencies['kde/kruler'] = 'default'
         self.dependencies['kde/ksnapshot'] = 'default'
+        self.dependencies['kde/svgpart'] = 'default'
+        self.dependencies['kde/ksaneplugin'] = 'default'
+        self.dependencies['kde/kdegraphics-thumbnailers'] = 'default'
+        
+        #doesnt work because of missing dependency gphoto2
+        #self.dependencies['kde/kamera'] = 'default'
+        
 
 from Package.VirtualPackageBase import *
 
+class Package( VirtualPackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        VirtualPackageBase.__init__( self )
+
+
 if __name__ == '__main__':
-    Package(subinfo()).execute()
+    Package().execute()
