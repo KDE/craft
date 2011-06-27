78a1b0ba91984965425bfa12550d8239568775f8
Patrick Spendrin <ps_ml@gmx.de>
2011-06-27 08:54:11 +0000

    fix weird naming of eigen2 tarball
    svn path=/trunk/kdesupport/emerge/; revision=1238488

--- portage/kdesupport/eigen2/eigen2-2.0.16-20110627.py
+++ portage/kdesupport/eigen2/eigen2-2.0.16-20110627.py
@@ -14,6 +14,8 @@ class subinfo(info.infoclass):
         for ver in ['2.0.10','2.0.15', '2.0.16']:
             self.targets[ver] = 'http://bitbucket.org/eigen/eigen/get/' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = 'eigen'
+        self.targetInstSrc['2.0.16'] = 'eigen-eigen-9ca09dbd70ce'
+        self.targetDigests['2.0.16'] = 'ec0d6e6716b7e3ce916ca0ff378a346d710c8db2'
         self.defaultTarget = '2.0.16'
 
 from Package.CMakePackageBase import *
