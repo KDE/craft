317546feebc4dce4f5cd79a22497d2a843c89938
Patrick Spendrin <ps_ml@gmx.de>
2011-07-04 06:56:40 +0200

    fix pexports under x64 (cherry picked from commit a975b244907ab1cf36f3f1ca83a2b54aa08d6491)

--- portage/dev-util/pexports/pexports-0.44.py
+++ portage/dev-util/pexports/pexports-0.44.py
@@ -3,7 +3,8 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['0.44'] =  self.getUnifiedPackage( 'http://downloads.sourceforge.net/kde-windows' , "pexports" , '0.44' ,packagetypes=['bin'] )
-        self.targetDigests['0.44'] = 'dd9497260e89c25e51f120de41949a09f08ee54f'
+		if emergePlatform.buildArchitecture() <> "x64":
+			self.targetDigests['0.44'] = 'dd9497260e89c25e51f120de41949a09f08ee54f'
         self.defaultTarget = '0.44'
 
     def setBuildOptions( self ):
