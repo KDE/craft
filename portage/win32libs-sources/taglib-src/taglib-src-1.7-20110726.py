0a62fd6dc75c3d6afa9001588d48057c639b7d80
Patrick von Reth <patrick.vonreth@gmail.com>
2011-07-26 21:36:19 +0200

    updated taglib

--- portage/win32libs-sources/taglib-src/taglib-src-1.7-20110726.py
+++ portage/win32libs-sources/taglib-src/taglib-src-1.7-20110726.py
@@ -7,13 +7,14 @@ class subinfo(info.infoclass):
         self.shortDescriptions = "audio meta-data library"
 
     def setTargets( self ):
-        for v in ['1.4', '1.5', '1.6', '1.6.1' , '1.6.3' ]:
+        for v in ['1.4', '1.5', '1.6', '1.6.1' , '1.6.3','1.7' ]:
           self.targets[ v ] = 'http://developer.kde.org/~wheeler/files/src/taglib-%s.tar.gz' % v
           self.targetInstSrc[ v ] = 'taglib-%s' % v
         self.svnTargets['svnHEAD'] = 'trunk/kdesupport/taglib'
         self.patchToApply[ '1.6.3' ] = [("taglib-1.6.3-20101213.diff", 1)]
+        self.targetDigests['1.7'] = '5138e1665182bc2171e298ff31518c9ad72ddf23'
         self.shortDescription = "audio metadata library"
-        self.defaultTarget = '1.6.3'
+        self.defaultTarget = '1.7'
 
 from Package.CMakePackageBase import *
 
