7531aec6fac1d63231ef998ec3bf62908eca31f8
Patrick von Reth <patrick.vonreth@gmail.com>
2011-07-26 20:58:16 +0200

    updated jpeg target

--- portage/win32libs-sources/jpeg-src/jpeg-src-8.0c.py
+++ portage/win32libs-sources/jpeg-src/jpeg-src-8.0c.py
@@ -5,14 +5,20 @@ class subinfo(info.infoclass):
         self.targets['7.0'] = "http://www.ijg.org/files/jpegsrc.v7.tar.gz"
         self.targetInstSrc['7.0'] = "jpeg-7"
         self.targets['8.0'] = "http://www.ijg.org/files/jpegsrc.v8.tar.gz"
+        self.targets['8.0c'] = "http://www.ijg.org/files/jpegsrc.v8c.tar.gz"
+        self.targetDigests['8.0c'] = 'f0a3b88ac4db19667798bee971537eeed552bce9'
+        
         self.targetInstSrc['8.0'] = "jpeg-8"
+        self.targetInstSrc['8.0c'] = "jpeg-8c"
+        
         self.patchToApply['7.0'] = ( 'jpeg7.diff', 1 )
         self.patchToApply['8.0'] = ( 'jpeg8.diff', 1 )
+        self.patchToApply['8.0c'] = ( 'jpeg8.diff', 1 )
 
         self.shortDescription = 'A library for manipulating JPEG image format files'
         self.categoryName = 'win32libs'
 
-        self.defaultTarget = '8.0'
+        self.defaultTarget = '8.0c'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
