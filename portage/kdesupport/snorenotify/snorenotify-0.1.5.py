4ccfdfd72ef2e3c54e9f63103c437a7dc1d12ea5
Patrick von Reth <patrick.vonreth@gmail.com>
2011-07-31 19:31:03 +0200

    updated dependency to bin packages

--- portage/kdesupport/snorenotify/snorenotify-0.1.5.py
+++ portage/kdesupport/snorenotify/snorenotify-0.1.5.py
@@ -8,8 +8,8 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
       self.shortDescription = "An application to show kde notifications with Snarl/Growl"
       self.hardDependencies['libs/qt'] = 'default'
-      self.hardDependencies['win32libs-sources/boost-src'] = 'default'
-      self.hardDependencies['win32libs-sources/boost-thread-src'] = 'default'
+      self.hardDependencies['win32libs-bin/boost-system'] = 'default'
+      self.hardDependencies['win32libs-bin/boost-thread'] = 'default'
       self.hardDependencies['kde/kdelibs'] = 'default'
       self.buildDependencies['testing/cryptopp-src'] = 'default'
 
