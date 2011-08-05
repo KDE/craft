96ad8cd9900a1743467af8d0d79dd8f959e57bb5
Patrick von Reth <patrick.vonreth@gmail.com>
2011-08-05 15:21:51 +0200

    fix

--- package.bat
+++ package.bat
@@ -5,6 +5,7 @@ rem     it should be used to run the package.py script
 rem     to set up an automatic build, run this file whenever you need with the packagelist as single parameter
 rem     by Patrick Spendrin <ps_ml@gmx.de>
 
+cd %~dp0
 set EMERGE_WAIT_FOR_APPLICATION=True
 call %~dp0\kdeenv.bat python %~dp0\server\package.py %1
 timeout /T 60
