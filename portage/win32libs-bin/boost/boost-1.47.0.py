31d2961b9cd1a5fc8beda4856550e9250e17c7b8
Patrick von Reth <patrick.vonreth@gmail.com>
2011-08-14 21:40:21 +0200

    binary package for it

--- portage/win32libs-bin/boost/boost-1.47.0.py
+++ portage/win32libs-bin/boost/boost-1.47.0.py
@@ -1,7 +1,7 @@
 # This package-script is automatically updated by the script win32libsupdater.py
 # which can be found in your emerge/bin folder. To update this package, run
 # win32libsupdater.py (and commit the results)
-# based on revision git9f6bf2a6095d43643eda8af62d13b41cf2deb643
+# based on revision gitc4a14690afafd13c48c2f3f2b68c4b3b1e34fdda
 
 from Package.BinaryPackageBase import *
 import os
@@ -11,11 +11,11 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         repoUrl = 'http://downloads.sourceforge.net/kde-windows'
 
-        for version in [ '1.37.0-1', '1.44.0', '1.44.0-2', '1.44.0-1', '1.44.0-3' ]:
+        for version in [ '1.44.0', '1.37.0-1', '1.44.0-1', '1.44.0-2', '1.44.0-3', '1.47.0' ]:
             self.targets[ version ]          = self.getPackage( repoUrl, 'boost', version )
             self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'boost', version, '.tar.bz2.sha1' )
 
-        self.defaultTarget = '1.44.0-3'
+        self.defaultTarget = '1.47.0'
 
 
     def setDependencies( self ):
