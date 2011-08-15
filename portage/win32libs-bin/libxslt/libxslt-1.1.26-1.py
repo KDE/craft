4effd01fc1132881ad349cb49dd4d7674cc4cb5f
Patrick von Reth <patrick.vonreth@gmail.com>
2011-08-16 00:44:18 +0200

    rebuild libxslt, because it depends on a broken xml

--- portage/win32libs-bin/libxslt/libxslt-1.1.26-1.py
+++ portage/win32libs-bin/libxslt/libxslt-1.1.26-1.py
@@ -1,7 +1,7 @@
 # This package-script is automatically updated by the script win32libsupdater.py
 # which can be found in your emerge/bin folder. To update this package, run
 # win32libsupdater.py (and commit the results)
-# based on revision 1
+# based on revision git89b768c373d41fe154c8aff2c686289ac51d8aa7
 
 from Package.BinaryPackageBase import *
 import os
@@ -11,11 +11,11 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         repoUrl = 'http://downloads.sourceforge.net/kde-windows'
 
-        for version in [ '1.1.23-3', '1.1.26' ]:
+        for version in [ '1.1.23-3', '1.1.26', '1.1.26-1' ]:
             self.targets[ version ]          = self.getPackage( repoUrl, 'libxslt', version )
-            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'libxslt', version , '.tar.bz2.sha1' )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'libxslt', version, '.tar.bz2.sha1' )
 
-        self.defaultTarget = '1.1.26'
+        self.defaultTarget = '1.1.26-1'
 
 
     def setDependencies( self ):
