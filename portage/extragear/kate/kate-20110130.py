23447e4623c8451442927ff134833636199a8598
Patrick Spendrin <ps_ml@gmx.de>
2011-08-15 16:04:36 +0200

    add kate tarballs

--- portage/extragear/kate/kate-20110130.py
+++ portage/extragear/kate/kate-20110130.py
@@ -3,6 +3,9 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:kate|KDE/4.7|'
+        for ver in ['0', '1', '2', '3', '4']:
+            self.targets['4.7.' + ver] = "http://download.kde.org/stable/4.7." + ver + "/src/kate-4.7." + ver + ".tar.bz2"
+            self.targetInstSrc['4.7.' + ver] = 'kate-4.7.' + ver
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
