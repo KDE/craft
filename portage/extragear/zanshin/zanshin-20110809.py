0d3787b9b795e0119a47543d83ef5b7fe37939f1
Patrick Spendrin <ps_ml@gmx.de>
2011-08-15 16:12:50 +0200

    fix whitespaces

--- portage/extragear/zanshin/zanshin-20110809.py
+++ portage/extragear/zanshin/zanshin-20110809.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:zanshin'
         self.svnTargets['0.2-beta1'] = 'http://files.kde.org/zanshin/zanshin-0.1.81.tar.bz2'
-		self.description = "a powerful yet simple application for managing your day to day actions"
+        self.description = "a powerful yet simple application for managing your day to day actions"
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
