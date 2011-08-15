7e9783dcfbad6e241bad276db9f287fecabec99e
Michael Drueing <michael@drueing.de>
2011-08-15 22:21:20 +0200

    Fix git branch name

--- portage/kde/kdegraphics/okular/okular-20110627.py
+++ portage/kde/kdegraphics/okular/okular-20110627.py
@@ -2,7 +2,7 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:okular|KDE/4.7|'
+        self.svnTargets['gitHEAD'] = '[git]kde:okular|4.7|'
         for ver in ['0', '1', '2', '3', '4']:
             self.targets['4.7.' + ver] = "ftp://ftp.kde.org/pub/kde/stable/4.7." + ver + "/src/okular-4.7." + ver + ".tar.bz2"
             self.targetInstSrc['4.7.' + ver] = 'okular-4.7.' + ver
