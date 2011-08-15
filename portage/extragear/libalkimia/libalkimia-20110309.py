5b44efd260a8c627ab9399fd677cdb3341620308
Patrick Spendrin <ps_ml@gmx.de>
2011-08-15 17:16:57 +0200

    update libalkimia to 4.3.1

--- portage/extragear/libalkimia/libalkimia-20110309.py
+++ portage/extragear/libalkimia/libalkimia-20110309.py
@@ -3,8 +3,10 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/extragear/office/alkimia/libalkimia'
-        self.targets['4.3.0'] = 'libalkimia-4.3.0.tar.bz2'
-        self.targetInstSrc['4.3.0'] = "libalkimia-4.3.0"
+        for ver in ['4.3.0', '4.3.1']:
+            self.targets[ ver ] = 'http://kde-apps.org/CONTENT/content-files/137323-libalkimia-' + ver + '.tar.bz2'
+            self.targetInstSrc[ ver ] = "libalkimia-" + ver
+        self.targetDigests['4.3.1'] = 'a8381bf4def252425aca31d0929e31b1aa82d0b5'
         self.defaultTarget = 'svnHEAD'
 
     def setDependencies( self ):
@@ -17,13 +19,6 @@ class Package(CMakePackageBase):
     def __init__( self):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
-
-    def fetch(self):
-        if self.buildTarget == 'svnHEAD':
-                CMakePackageBase.fetch(self)
-        else:
-                utils.wgetFile('"http://kde-apps.org/CONTENT/content-files/137323-libalkimia-4.3.0.tar.bz2"' , self.downloadDir() , "libalkimia-4.3.0.tar.bz2")
-        return True
 
 if __name__ == '__main__':
     Package().execute()
