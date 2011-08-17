8a9058d85d235c23b5f80b6a847e21864843e581
Patrick Spendrin <ps_ml@gmx.de>
2011-08-17 19:38:27 +0200

    fix tarball & trigger rebuild

--- portage/kde/kdegraphics/kolourpaint/kolourpaint-20110817.py
+++ portage/kde/kdegraphics/kolourpaint/kolourpaint-20110817.py
@@ -4,8 +4,8 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:kolourpaint|KDE/4.7|'
         for ver in ['0', '1', '2', '3', '4']:
-            self.targets['4.7.' + ver] = "ftp://ftp.kde.org/pub/kde/stable/4.7." + ver + "/src/kdelibs-4.7." + ver + ".tar.bz2"
-            self.targetInstSrc['4.7.' + ver] = 'kdelibs-4.7.' + ver
+            self.targets['4.7.' + ver] = "ftp://ftp.kde.org/pub/kde/stable/4.7." + ver + "/src/kolourpaint-4.7." + ver + ".tar.bz2"
+            self.targetInstSrc['4.7.' + ver] = 'kolourpaint-4.7.' + ver
         self.shortDescription = "KolourPaint is an easy-to-use paint program"
         self.defaultTarget = 'gitHEAD'
 
