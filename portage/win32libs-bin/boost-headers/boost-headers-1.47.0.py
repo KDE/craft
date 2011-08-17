0bc0539188b0db369c1cf6fde9bf92ff6c432705
Patrick von Reth <patrick.vonreth@gmail.com>
2011-08-18 00:56:45 +0200

    added binary packages

---
+++ portage/win32libs-bin/boost-headers/boost-headers-1.47.0.py
@@ -0,0 +1,36 @@
+# This package-script is automatically updated by the script win32libsupdater.py
+# which can be found in your emerge/bin folder. To update this package, run
+# win32libsupdater.py (and commit the results)
+# based on revision git886de0cc9b787069382ac2e9c21bfdd0f31d215d
+
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        repoUrl = 'http://downloads.sourceforge.net/kde-windows'
+
+        for version in [ '1.47.0' ]:
+            self.targets[ version ]          = self.getPackage( repoUrl, 'boost-headers', version )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'boost-headers', version, '.tar.bz2.sha1' )
+
+        self.defaultTarget = '1.47.0'
+
+
+    def setDependencies( self ):
+        if not utils.envAsBool( 'EMERGE_ENABLE_IMPLICID_BUILDTIME_DEPENDENCIES' ):
+            self.buildDependencies[ 'gnuwin32/wget' ] = 'default'
+
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
