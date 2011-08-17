d807e107d4a5c7157d809713ade494e206ad5f91
Patrick Spendrin <ps_ml@gmx.de>
2011-08-17 20:48:59 +0200

    add opencv which is a dependency of digikam

---
+++ portage/win32libs-sources/opencv-src/opencv-src-2.3.py
@@ -0,0 +1,24 @@
+import info
+import os
+from Package.CMakePackageBase import *
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        for ver in [ '2.3' ]:
+            self.targets[ ver ] = 'http://downloads.sourceforge.net/opencvlibrary/OpenCV-2.3.0-win-src.zip'
+            self.targetInstSrc[ ver ] = 'OpenCV-2.3.0'
+        self.patchToApply['2.3'] = ('OpenCV-2.3.0-20110817.diff', 1)
+        self.targetDigests['2.3'] = '126787da5a3d71e80eb6e8d3bed126391e0549c9'
+        self.defaultTarget = '2.3'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+
+
+class Package( CMakePackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
