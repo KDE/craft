c956b93ac60e2e3b6e087f3564e3105987baa55f
Patrick Spendrin <ps_ml@gmx.de>
2011-08-18 15:14:54 +0200

    fix msvc build of amarok under windows (cherry picked from commit 18ecae31d87d0951f5f0c9b681eae21c7b04679a)

--- portage/extragear/amarok/amarok-2.4.3-20110110.py
+++ portage/extragear/amarok/amarok-2.4.3-20110110.py
@@ -10,6 +10,7 @@ class subinfo(info.infoclass):
             self.targets[ver] = 'http://download.kde.org/download.php?url=stable/amarok/' + ver + '/src/amarok-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = 'amarok-' + ver
         self.patchToApply[ '2.3.2' ] = [ ( 'amarok-2.3.2-20110110.diff', 1 ) ]
+        self.patchToApply[ '2.4.3' ] = [ ( 'amarok-2.4.3-20110818.diff', 1 ) ]
         self.targetDigests['2.4.3'] = '8a46fca6a550a4ca403a9be7f595728c819641c4'
 
         self.svnTargets['gitHEAD'] = '[git]kde:amarok.git'
