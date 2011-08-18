f77ab074de3402b2342f898d57cceada97c00399
Patrick Spendrin <ps_ml@gmx.de>
2011-08-19 01:16:32 +0200

    fix libksane as I already did in the repo

--- portage/kde/kdegraphics/libksane/libksane-20110819.py
+++ portage/kde/kdegraphics/libksane/libksane-20110819.py
@@ -6,6 +6,7 @@ class subinfo( info.infoclass ):
         for ver in ['0', '1', '2', '3', '4']:
             self.targets['4.7.' + ver] = "ftp://ftp.kde.org/pub/kde/stable/4.7." + ver + "/src/libksane-4.7." + ver + ".tar.bz2"
             self.targetInstSrc['4.7.' + ver] = 'libksane-4.7.' + ver
+        self.patchToApply['4.7.0'] = ("libksane-4.7.0-20110819.diff", 1)
         self.shortDescription = "libksane is an image scanning library that provides a QWidget that contains all the logic needed to interface a sacanner."
         self.defaultTarget = 'gitHEAD'
 
