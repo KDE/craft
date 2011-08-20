dcfdf5553613c45e95b9dab7dce09cecd2be586c
Patrick von Reth <patrick.vonreth@gmail.com>
2011-08-20 22:33:28 +0200

    binary package for opencv

---
+++ portage/win32libs-bin/opencv/opencv-2.3.py
@@ -0,0 +1,36 @@
+# This package-script is automatically updated by the script win32libsupdater.py
+# which can be found in your emerge/bin folder. To update this package, run
+# win32libsupdater.py (and commit the results)
+# based on revision git6bc02a779b18f516b3624d2331e735deadd2fe98
+
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        repoUrl = 'http://downloads.sourceforge.net/kde-windows'
+
+        for version in [ '2.3' ]:
+            self.targets[ version ]          = self.getPackage( repoUrl, 'opencv', version )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'opencv', version, '.tar.bz2.sha1' )
+
+        self.defaultTarget = '2.3'
+
+
+    def setDependencies( self ):
+        if not utils.envAsBool( 'EMERGE_ENABLE_IMPLICID_BUILDTIME_DEPENDENCIES' ):
+            self.buildDependencies[ 'gnuwin32/wget' ] = 'default'
+
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
