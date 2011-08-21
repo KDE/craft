a242e46cb81651e5d0e4f22338c40992e90bc017
Patrick von Reth <patrick.vonreth@gmail.com>
2011-08-21 21:30:36 +0200

    updated quassel

--- portage/extragear/quassel/quassel-0.7.2.py
+++ portage/extragear/quassel/quassel-0.7.2.py
@@ -8,10 +8,12 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = 'git://gitorious.org/quassel/quassel.git'
         self.svnTargets['0.6'] = 'git://gitorious.org/quassel/quassel.git|0.6|'
-        self.targets['0.7.1'] = 'http://quassel-irc.org/pub/quassel-0.7.1.tar.bz2'
+        for ver in ['0.7.1','0.7.2']:
+            self.targets[ver] = 'http://quassel-irc.org/pub/quassel-%s.tar.bz2' % ver
+            self.targetInstSrc[ver] = 'quassel-%s' % ver
         self.targetDigests['0.7.1'] = '791086da977033a1bbee3effa317668b3726bd7f'
-        self.targetInstSrc['0.7.1'] = 'quassel-0.7.1'
-        self.defaultTarget = 'gitHEAD'
+
+        self.defaultTarget = '0.7.2'
 
     def setDependencies( self ):
         self.dependencies['virtual/kde-runtime'] = 'default'
