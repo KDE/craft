05e14982317e8cad4f060b1b15ec4be2f6222c60
Patrick Spendrin <ps_ml@gmx.de>
2011-08-22 07:19:08 +0200

    fix libalkimia and hopefully also kmymoney

--- portage/extragear/libalkimia/libalkimia-20110309.py
+++ portage/extragear/libalkimia/libalkimia-20110309.py
@@ -6,6 +6,7 @@ class subinfo(info.infoclass):
         for ver in ['4.3.0', '4.3.1']:
             self.targets[ ver ] = 'http://kde-apps.org/CONTENT/content-files/137323-libalkimia-' + ver + '.tar.bz2'
             self.targetInstSrc[ ver ] = "libalkimia-" + ver
+        self.patchToApply['4.3.1'] = ("libalkimia-4.3.1-20110822.diff", 1)
         self.targetDigests['4.3.1'] = 'a8381bf4def252425aca31d0929e31b1aa82d0b5'
         self.defaultTarget = 'svnHEAD'
 
