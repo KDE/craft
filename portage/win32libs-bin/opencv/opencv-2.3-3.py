6e1f7c0c4c94422fecd2531355983c0222ed49cd
Patrick Spendrin <ps_ml@gmx.de>
2011-08-25 00:37:34 +0200

    also update the binary package (cherry picked from commit aaa5118d249c582e3352549a3d9b9d7d3ecf00d3)

--- portage/win32libs-bin/opencv/opencv-2.3-3.py
+++ portage/win32libs-bin/opencv/opencv-2.3-3.py
@@ -1,7 +1,7 @@
 # This package-script is automatically updated by the script win32libsupdater.py
 # which can be found in your emerge/bin folder. To update this package, run
 # win32libsupdater.py (and commit the results)
-# based on revision gite34e7c75543d1ca1db2ac9dfdd860ea1ab46f785
+# based on revision git475c7d8bede9822dd317bc9ae7868544e04a7cf7
 
 from Package.BinaryPackageBase import *
 import os
@@ -11,11 +11,11 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         repoUrl = 'http://downloads.sourceforge.net/kde-windows'
 
-        for version in [ '2.3', '2.3-1', '2.3-2' ]:
+        for version in [ '2.3', '2.3-1', '2.3-2', '2.3-3' ]:
             self.targets[ version ]          = self.getPackage( repoUrl, 'opencv', version )
             self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'opencv', version, '.tar.bz2.sha1' )
 
-        self.defaultTarget = '2.3-2'
+        self.defaultTarget = '2.3-3'
 
 
     def setDependencies( self ):
