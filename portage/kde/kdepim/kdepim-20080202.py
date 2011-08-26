eea6732a977f3a6aa76f54d74c57534eed4ef53e
Andre Heinecke <aheinecke@intevation.de>
2011-08-26 17:04:22 +0200

    Do not overwrite the 4.7.0 patch

--- portage/kde/kdepim/kdepim-20080202.py
+++ portage/kde/kdepim/kdepim-20080202.py
@@ -6,11 +6,12 @@ class subinfo( info.infoclass ):
         for ver in ['0', '1', '2', '3', '4']:
             self.targets['4.7.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.7.' + ver + '/src/kdepim-4.7.' + ver + '.tar.bz2'
             self.targetInstSrc['4.7.' + ver] = 'kdepim-4.7.' + ver
-        self.patchToApply['4.7.0'] = ("kdepim-4.7.0-20110823.diff", 1)
         self.defaultTarget = 'gitHEAD'
 
         for target in self.targets:
             self.patchToApply[target] = [('disable-crypto-backend.patch', 1)]
+
+        self.patchToApply['4.7.0'] += [("kdepim-4.7.0-20110823.diff", 1)]
 
     def setDependencies( self ):
         self.runtimeDependencies['kde/kdepim-runtime'] = 'default'
