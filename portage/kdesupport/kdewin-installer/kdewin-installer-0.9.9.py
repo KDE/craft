833ad71bdca8c11115d0fa754ecdf25c269c7eb1
Ralf Habacker <ralf.habacker@freenet.de>
2011-08-28 01:16:24 +0200

    updated

---
+++ portage/kdesupport/kdewin-installer/kdewin-installer-0.9.9.py
@@ -0,0 +1,27 @@
+import info
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.dependencies['libs/qt'] = 'default'
+        self.dependencies['win32libs-bin/libbzip2'] = 'default'
+
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = '[git]kde:kdewin-installer'
+        self.defaultTarget = 'gitHEAD'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.make.supportsMultijob = False
+
+    def configure(self):
+        if self.buildTarget == 'amarokHEAD':
+            self.subinfo.configure.defines = " -DBUILD_FOR_AMAROK=ON"
+        return CMakePackageBase.configure(self)
+
+if __name__ == '__main__':
+    Package().execute()
