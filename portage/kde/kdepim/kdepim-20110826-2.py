969fbce619c33c428de492664df9e13a4498e4cf
Patrick Spendrin <ps_ml@gmx.de>
2011-08-27 19:48:04 +0200

    also fix application icons for other apps

--- portage/kde/kdepim/kdepim-20110826-2.py
+++ portage/kde/kdepim/kdepim-20110826-2.py
@@ -11,7 +11,8 @@ class subinfo( info.infoclass ):
         for target in self.targets:
             self.patchToApply[target] = [('disable-crypto-backend.patch', 1),
                         ('Add-kde4_app_icon-macro-for-mobile-targets.patch',1),
-                        ('remove-old-icon-way.diff', 1)]
+                        ('remove-old-icon-way.diff', 1),
+                        ('broken-app-icons-fix.diff', 1)]
 
         self.patchToApply['4.7.0'] += [("kdepim-4.7.0-20110823.diff", 1)]
 
