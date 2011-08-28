c4aa5f84caa2a0970bb75d3d6902b6548bed500a
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-08-28 10:59:23 +0200

    enable building kontact touch

--- portage/kde/kdepim/kdepim-20110826-2.py
+++ portage/kde/kdepim/kdepim-20110826-2.py
@@ -31,6 +31,7 @@ class Package( CMakePackageBase ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
         self.subinfo.options.configure.defines = "-DKLEO_SYNCHRONOUS_API_HOTFIX=ON "
+        self.subinfo.options.configure.defines += " -DKDEPIM_MOBILE_UI=TRUE "
 
 if __name__ == '__main__':
     Package().execute()
