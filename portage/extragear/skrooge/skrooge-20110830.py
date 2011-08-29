531f21ba3f864fa21d9cebb1935805b214ad0587
Patrick Spendrin <ps_ml@gmx.de>
2011-08-30 00:18:33 +0200

    fix skrooge icon & sqlite loading error

--- portage/extragear/skrooge/skrooge-20110830.py
+++ portage/extragear/skrooge/skrooge-20110830.py
@@ -3,7 +3,10 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:skrooge'
-        self.svnTargets['0.9.1'] = "[git]kde:skrooge|master|4c9e641"
+        self.targets['0.9.1'] = "http://www.winkde.org/pub/kde/ports/win32/repository/other/skrooge-4c9e641.tar.bz2"
+        self.targetInstSrc['0.9.1'] = "skrooge-4c9e641"
+        self.patchToApply['0.9.1'] = ("skrooge-4c9e641-20110830.diff", 1)
+        self.shortDescription = "a personal finance manager for KDE"
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
