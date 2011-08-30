8d113e098825c71c6cee2a45cfae87396090b739
Patrick Spendrin <ps_ml@gmx.de>
2011-08-31 00:47:12 +0200

    add 1.3.1 release and extend cmake buildsystem with tests

--- portage/win32libs-sources/gpgme-src/gpgme-src-1510-2.py
+++ portage/win32libs-sources/gpgme-src/gpgme-src-1510-2.py
@@ -6,18 +6,21 @@ import compiler
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.3.0'] = 'ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-1.3.0.tar.bz2'
-        self.targetInstSrc['1.3.0'] = 'gpgme-1.3.0'
-        self.targetDigests['1.3.0'] = '0db69082abfbbbaf86c3ab0906f5137de900da73'
+        for ver in ['1.3.0', '1.3.1']:
+            self.targets[ver] = 'ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-' + ver + '.tar.bz2'
+            self.targetInstSrc[ver] = 'gpgme-' + ver
         self.patchToApply['1.3.0'] = [('gpgme-1447-HEAD.diff', 0),
                                       ('gpgme-1.3.0-cmake.diff', 1),
                                       ('gpgme-1.3.0-20101102.diff', 1),
                                       ('gpgme-1.3.0-mingw.diff', 1)]
+        self.patchToApply['1.3.1'] = [('gpgme-1.3.1-20110830.diff', 1), ("gpgme-1.3.1-cmake.diff", 1)]
 
         self.targets['1510'] = "download.sourceforge.net/kde-windows/gpgme-r1510.tar.bz2"
         self.targetInstSrc['1510'] = "gpgme-r1510"
-        self.targetDigests['1510'] = 'bb67afb24dc95795ac2a369aafd7ef99438b90b6'
         self.patchToApply['1510'] = [('gpgme-r1510-cmake.diff', 1), ('gpgme-r1510-20101205.diff', 1)]
+        self.targetDigests['1510'] = 'bb67afb24dc95795ac2a369aafd7ef99438b90b6'
+        self.targetDigests['1.3.0'] = '0db69082abfbbbaf86c3ab0906f5137de900da73'
+        self.targetDigests['1.3.1'] = '7d19a95a2239da13764dad7f97541be884ec5a37'
 
         self.shortDescription = "GnuPG cryptography support library (runtime)"
         self.defaultTarget = '1510'
