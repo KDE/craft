b027334f28ce2a11d576927780753e6381f9be60
Patrick Spendrin <ps_ml@gmx.de>
2011-09-04 23:14:51 +0200

    include missing dlls in runtime package CCBUG:281344

--- portage/libs/runtime/runtime-multi.py
+++ portage/libs/runtime/runtime-multi.py
@@ -3,9 +3,9 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         # not used  yet only for reference
-        self.targets[ '0.0.1' ] = "http://download.microsoft.com/download/1/1/1/1116b75a-9ec3-481a-a3c8-1777b5381140/vcredist_x86.exe"
+        self.targets[ '0.0.2' ] = "http://download.microsoft.com/download/1/1/1/1116b75a-9ec3-481a-a3c8-1777b5381140/vcredist_x86.exe"
         self.shortDescription = "the compiler runtime package"
-        self.defaultTarget = '0.0.1'
+        self.defaultTarget = '0.0.2'
 
     def setDependencies( self ):
         self.buildDependencies[ 'virtual/base' ] = 'default'
@@ -42,10 +42,10 @@ class Package( BinaryPackageBase ):
                 files = [ 'mingwm10.dll', 'libgcc_s_dw2-1.dll' ]
             elif compiler.isMinGW_W32():
                 srcdir = os.path.join( self.rootdir, "mingw", "bin" )
-                files = [ 'libgcc_s_sjlj-1.dll' ]
+                files = [ 'libgcc_s_sjlj-1.dll', 'libgomp-1.dll' ]
             elif compiler.isMinGW_W64():
                 srcdir = os.path.join( self.rootdir, "mingw64", "bin" )
-                files = [ 'libgcc_s_sjlj-1.dll' ]
+                files = [ 'libgcc_s_sjlj-1.dll', 'libgomp-1.dll' ]
 #        elif compiler.isMSVC2008():
 #            if self.buildType() == "Debug":
 #                srcdir = os.path.join( self.packageDir(), "redist", "Debug_NonRedist", "x86", "Microsoft.VC90.DebugCRT" )
@@ -55,10 +55,28 @@ class Package( BinaryPackageBase ):
 #                files = [ "Microsoft.VC90.CRT.manifest", "msvcr90.dll", "msvcp90.dll", "msvcm90.dll" ]
         elif compiler.isMSVC2010():
             srcdir = os.path.join( os.environ["windir"], "system32" ) 
-            files = [ "msvcr100%s.dll" % postfix, "msvcp100%s.dll" % postfix]
+            files = [ "msvcr100%s.dll" % postfix, "msvcp100%s.dll" % postfix ]
 
         for file in files:
             utils.copyFile( os.path.join( srcdir, file ), os.path.join( destdir, file ) )
+
+        # extract pthread package.
+        if compiler.isMinGW_WXX():
+            tmpdir = os.getenv( "TEMP" )
+
+            if compiler.isMinGW_W32(): _ext = 32
+            elif compiler.isMinGW_W64(): _ext = 64
+            else: utils.die( "unknown flavor of mingw-wXX" )
+
+            pthreadPackageName = os.path.join( self.rootdir, "mingw", "pthreads-w%s.zip" ) % _ext
+            pthreadDll = "pthreadGC2-w%s.dll" % _ext
+
+            utils.unZip( pthreadPackageName, tmpdir )
+
+            srcdir = os.path.join( tmpdir, "bin" )
+            files = [ pthreadDll ]
+            for file in files:
+                utils.copyFile( os.path.join( srcdir, file ), os.path.join( destdir, file ) )
         return True
 
 if __name__ == '__main__':
