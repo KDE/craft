262c39fdc5357d97e36aee2d92a26a4dfdc9ef77
Patrick Spendrin <ps_ml@gmx.de>
2011-09-06 14:59:35 +0200

    update binary packages

--- portage/win32libs-bin/gpgme/gpgme-1510-2.py
+++ portage/win32libs-bin/gpgme/gpgme-1510-2.py
@@ -1,7 +1,7 @@
 # This package-script is automatically updated by the script win32libsupdater.py
 # which can be found in your emerge/bin folder. To update this package, run
 # win32libsupdater.py (and commit the results)
-# based on revision git41c767438a800596a95aefa2814825efba193318
+# based on revision git9e68ba20b63f582f00614117ddfd24524f758f1c
 
 from Package.BinaryPackageBase import *
 import os
@@ -11,11 +11,13 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         repoUrl = 'http://downloads.sourceforge.net/kde-windows'
 
-        for version in [ '1.1.4-3', '1.3.0', '1510', '1510-1' ]:
+        for version in [ '1.1.4-3', '1.3.0', '1510', '1510-1', '1510-2' ]:
             self.targets[ version ]          = self.getPackage( repoUrl, 'gpgme', version )
             self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'gpgme', version, '.tar.bz2.sha1' )
 
-        self.defaultTarget = '1510-1'
+        self.shortDescription = '''GnuPG cryptography support library (runtime)'''
+
+        self.defaultTarget = '1510-2'
 
 
     def setDependencies( self ):
