b73e57176888ef86cb3ede455d9c40f4fb52a0b3
Patrick Spendrin <ps_ml@gmx.de>
2011-10-09 21:46:35 +0200

    add skrooge 1.0.0 release

--- portage/extragear/skrooge/skrooge-20111009.py
+++ portage/extragear/skrooge/skrooge-20111009.py
@@ -6,6 +6,8 @@ class subinfo( info.infoclass ):
         self.targets['0.9.1'] = "http://www.winkde.org/pub/kde/ports/win32/repository/other/skrooge-4c9e641.tar.bz2"
         self.targetInstSrc['0.9.1'] = "skrooge-4c9e641"
         self.patchToApply['0.9.1'] = ("skrooge-4c9e641-20110830.diff", 1)
+        self.targets['1.0.0'] = "http://skrooge.org/files/skrooge-1.0.0.tar.bz2"
+        self.targetInstSrc['1.0.0'] = "skrooge-1.0.0"
         self.shortDescription = "a personal finance manager for KDE"
         self.defaultTarget = 'gitHEAD'
 
