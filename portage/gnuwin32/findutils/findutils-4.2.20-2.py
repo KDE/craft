b023930c9884984a1c2cc9c95914e8a468b5de3d
Ralf Habacker <ralf.habacker@freenet.de>
2011-10-13 09:04:04 +0200

    added digest

--- portage/gnuwin32/findutils/findutils-4.2.20-2.py
+++ portage/gnuwin32/findutils/findutils-4.2.20-2.py
@@ -8,6 +8,7 @@ http://downloads.sourceforge.net/project/gnuwin32/findutils/4.2.20-2/findutils-4
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['4.2.20-2'] = SRC_URI
+        self.targetDigests['4.2.20-2'] = '69ea9bdff90bbe3ee7e30d23058b12ece8f403a2'
         self.defaultTarget = '4.2.20-2'
 
 from Package.BinaryPackageBase import *
