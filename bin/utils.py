0d1c3c965bff5fe1967eb796d15feeeb4183665a
Patrick von Reth <patrick.vonreth@gmail.com>
2011-10-30 20:17:53 +0100

    fixed a remaining typo in PortablePackager, added new amarok scripts

--- bin/utils.py
+++ bin/utils.py
@@ -199,15 +199,10 @@ def getFiles( urls, destdir, suffix='' , filenames = ''):
         
     dlist = zip( urlList , filenameList )
     
-    if len(filenameList) == 0:
-        for url in urlList:
-            if ( not getFile( url + suffix, destdir ) ):
-                return False
-    else:
-        for url,filename in dlist:
-            if ( not getFile( url + suffix, destdir , filename ) ):
-                return False
-    
+    for url,filename in dlist:
+        if ( not getFile( url + suffix, destdir , filename ) ):
+            return False
+
     return True
 
 def getFile( url, destdir , filename='' ):
@@ -438,34 +433,17 @@ def unTar( fileName, destdir ):
     """unpack tar file specified by 'file' into 'destdir'"""
     debug( "unTar called. file: %s, destdir: %s" % ( fileName, destdir ), 1 )
     ( shortname, ext ) = os.path.splitext( fileName )
-
-    mode = "r"
-    if ( ext == ".gz" ):
-        mode = "r:gz"
-    elif ( ext == ".bz2" ):
-        mode = "r:bz2"
-    elif( ext == ".lzma" or ext == ".xz" ):
-        un7zip( fileName, os.getenv("TMP") )
-        _, tarname = os.path.split( shortname )
-        fileName = os.path.join( os.getenv("TMP"), tarname )
+  
+    un7zip( fileName, os.getenv("TMP") )
+    _, tarname = os.path.split( shortname )
+    fileName = os.path.join( os.getenv("TMP"), tarname )
 
     if not os.path.exists( fileName ):
         error( "couldn't find file %s" % fileName )
         return False
-
-    try:
-        with tarfile.open( fileName, mode ) as tar:
-        # FIXME how to handle errors here ?
-            for fileName in tar:
-                try:
-                    tar.extract(fileName, destdir )
-                except tarfile.TarError:
-                    error( "couldn't extract file %s to directory %s" % ( fileName, destdir ) )
-                    return False
-        return True
-    except tarfile.TarError:
-        error( "could not open existing tar archive: %s" % fileName )
-        return False
+    un7zip(fileName, destdir )
+    os.remove(fileName)
+    return True
 
 
 def unZip( fileName, destdir ):
