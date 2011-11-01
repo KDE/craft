c62c7f85023aaf4d7dfab59334a846677cf9d137
Patrick von Reth <patrick.vonreth@gmail.com>
2011-11-02 00:26:03 +0100

    fixed one of the worst buildsystems on the world, it now no longer installs everything into src/deploy so that on our buildserver the image of all compilers got mixed (cherry picked from commit 72d22dea8e05a8565ae0c15c50a7521c2bb303da)

--- portage/kdesupport/hupnp/hupnp-1.0.0-20111102.py
+++ portage/kdesupport/hupnp/hupnp-1.0.0-20111102.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
     def setTargets( self ):        
         self.shortDescription = "Herqq UPnP (HUPnP) is a software library for building UPnP devices and control points conforming to the UPnP Device Architecture version 1.1"
         self.svnTargets['svnHEAD'] = "https://hupnp.svn.sourceforge.net/svnroot/hupnp/trunk/herqq"
-        self.patchToApply['svnHEAD'] = ("HUpnp1.diff", 0)
+        #self.patchToApply['svnHEAD'] = ("HUpnp1.diff", 0)
         for ver in ["1.0.0"]:
             self.targets[ver] = "http://downloads.sourceforge.net/sourceforge/hupnp/herqq-%s.zip" % ver
             self.targetInstSrc[ver] = "herqq-%s" % ver
@@ -18,10 +18,6 @@ class subinfo(info.infoclass):
         self.dependencies['libs/qt'] = 'default'
         self.dependencies['kdesupport/libqtsoap'] = 'default'
 
-    def setBuildOptions( self ):
-        self.options.configure.defines = ' "CONFIG += DISABLE_TESTAPP"'
-        self.options.configure.defines += ' "CONFIG += DISABLE_AVTESTAPP"'
-        self.options.configure.defines += ' "CONFIG += DISABLE_QTSOAP"'
 
 from Package.QMakePackageBase import *
 
@@ -29,25 +25,23 @@ class Package( QMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         QMakePackageBase.__init__( self )
-
-        
-    def unpack( self ):
-        if not QMakePackageBase.unpack( self ):
-            return False
-        return True
+        self.subinfo.options.configure.defines = ' "PREFIX = %s" ' % self.imageDir().replace("\\","/")
+        self.subinfo.options.configure.defines += ' "CONFIG += DISABLE_TESTAPP"'
+        self.subinfo.options.configure.defines += ' "CONFIG += DISABLE_AVTESTAPP"'
+        self.subinfo.options.configure.defines += ' "CONFIG += DISABLE_QTSOAP"'
         
     def install( self ):
         if not QMakePackageBase.install( self ):
             return False
-        #sic.: the .lib file is placed under bin dir in hupnp
-        utils.copyDir( os.path.join( self.sourceDir(), "hupnp", "deploy" ) , self.installDir() )
-        utils.copyDir( os.path.join( self.sourceDir(), "hupnp_av", "deploy" ) , self.installDir() )
+        #sic.: the .lib file is placed under bin dir in hupnp)
         os.mkdir( os.path.join( self.installDir(), "bin" ) )
         # copy over dlls as required by KDE convention
         for file in os.listdir( os.path.join( self.installDir(), "lib" ) ):
             if file.endswith( ".dll" ):
                 utils.copyFile( os.path.join( self.installDir(), "lib" , file ), os.path.join( self.installDir(), "bin" , file ) )
         return True
+        
+
         
 if __name__ == '__main__':
     Package().execute()
