5b668bad581b41a5ea58371dea51cb6a99bacb7b
Patrick von Reth <patrick.vonreth@gmail.com>
2011-11-08 15:05:20 +0100

    fixed url for 4.5.1 (cherry picked from commit dde364ccbc61198e15a720f03f8b550fdcf18f8b)

--- portage/kdesupport/phonon/phonon-4.5.1.20110130.py
+++ portage/kdesupport/phonon/phonon-4.5.1.20110130.py
@@ -9,8 +9,11 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:phonon'
-        for ver in ['4.4.3', '4.4.4', '4.5.0','4.5.1']:
+        for ver in ['4.4.3', '4.4.4', '4.5.0']:
             self.targets[ver] = 'ftp://ftp.kde.org/pub/kde/stable/phonon/%s/src/phonon-%s.tar.bz2' % (ver ,ver)
+            self.targetInstSrc[ver] = 'phonon-%s' % ver
+        for ver in ['4.5.1']:
+            self.targets[ver] = 'ftp://ftp.kde.org/pub/kde/stable/phonon/%s/src/phonon-%s.tar.xz' % (ver ,ver)
             self.targetInstSrc[ver] = 'phonon-%s' % ver
         if ver in ['4.4.3', '4.4.4']: self.patchToApply[ver] = ("phonon-20100915.diff", 1)
         self.targetDigests['4.4.3'] = '50262d590beb648be9dcad6b913b920db19a84f8'
