de05a992f42c5357070bb139d69c0896193d0349
Patrick von Reth <patrick.vonreth@gmail.com>
2011-11-08 13:17:17 +0100

    updated targets (cherry picked from commit c5b9a4e422cb18a6c7ad4e102af3931505e7f9ca)

--- portage/kdesupport/phonon/phonon-4.5.0.20110130.py
+++ portage/kdesupport/phonon/phonon-4.5.0.20110130.py
@@ -9,7 +9,7 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:phonon'
-        for ver in ['4.4.3', '4.4.4', '4.5.0']:
+        for ver in ['4.4.3', '4.4.4', '4.5.0','4.5.1']:
             self.targets[ver] = 'ftp://ftp.kde.org/pub/kde/stable/phonon/%s/src/phonon-%s.tar.bz2' % (ver ,ver)
             self.targetInstSrc[ver] = 'phonon-%s' % ver
         if ver in ['4.4.3', '4.4.4']: self.patchToApply[ver] = ("phonon-20100915.diff", 1)
