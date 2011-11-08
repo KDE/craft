b38ecce3de3cb6fe9e51a6c36b68cc6689f3d3f4
Patrick von Reth <patrick.vonreth@gmail.com>
2011-11-08 13:12:30 +0100

    filename=target

--- portage/extragear/kmymoney/kmymoney-4.6.1-20110918.py
+++ portage/extragear/kmymoney/kmymoney-4.6.1-20110918.py
@@ -13,7 +13,7 @@ class subinfo(info.infoclass):
         self.patchToApply['4.5.2'] = ('kmymoney-4.5.2-20110112.diff', 1)
         self.patchToApply['4.5.3'] = ('kmymoney-4.5.2-20110112.diff', 1)
         self.patchToApply['4.6.0'] = ('kmymoney-4.6.0-20110822.diff', 1)
-        self.defaultTarget = 'svnHEAD'
+        self.defaultTarget = '4.6.1'
 
     def setDependencies( self ):
         self.dependencies['virtual/kdepimlibs'] = 'default'
