26e5f285acf9cd743e2db33a68fe43856906f618
Patrick von Reth <patrick.vonreth@gmail.com>
2011-11-16 18:36:14 +0100

    updated simon dependencies (cherry picked from commit 0ed288211bf7187a2ad90088e7db7598df6d9b2c)

--- portage/testing/simon/simon-111115.py
+++ portage/testing/simon/simon-111115.py
@@ -5,19 +5,20 @@ from Package.CMakePackageBase import *
 
 
 class subinfo(info.infoclass):
-    def setDependencies( self ):
-      self.hardDependencies['libs/qt'] = 'default'
-      self.hardDependencies['gnuwin32/flex'] = 'default'
-      self.hardDependencies['gnuwin32/bison'] = 'default'
-      self.hardDependencies['win32libs-bin/gettext'] = 'default'
-      self.hardDependencies['dev-util/gettext-tools'] = 'default'
-      self.hardDependencies['kdesupport/qwt6'] = 'default'
-      self.hardDependencies['win32libs-bin/libsamplerate'] = 'default'
-
-        
     def setTargets( self ):
       self.svnTargets['gitHEAD'] = 'git://speech2text.git.sourceforge.net/gitroot/speech2text/speech2text||windows'
       self.defaultTarget = 'gitHEAD'
+      
+    def setDependencies( self ):
+      self.buildDependencies['gnuwin32/flex'] = 'default'
+      self.buildDependencies['gnuwin32/bison'] = 'default'
+      self.buildDependencies['dev-util/gettext-tools'] = 'default'
+      self.hardDependencies['libs/qt'] = 'default'
+      self.hardDependencies['win32libs-bin/gettext'] = 'default'
+      self.hardDependencies['kdesupport/qwt6'] = 'default'
+      self.hardDependencies['win32libs-bin/libsamplerate'] = 'default'
+      self.hardDependencies['kde/kdepimlibs'] = 'default'
+
          
 
 class Package( CMakePackageBase ):
