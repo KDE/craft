dc0d3f8c2600ffadc59f790ca93c16ccb656a6d6
Michael Drueing <michael@drueing.de>
2011-12-19 21:59:55 +0100

    Add preliminary build recipe for libvncserver
    It currently only builds libvncclient successfully, the server part is tricky...

---
+++ portage/testing/libvncserver-src/libvncserver-src-20111219.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import info
+
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['0.9.8.2'] = 'http://downloads.sourceforge.net/project/libvncserver/libvncserver/0.9.8.2/LibVNCServer-0.9.8.2.tar.gz'
+        self.targetInstSrc['0.9.8.2'] = 'LibVNCServer-0.9.8.2'
+        self.patchToApply['0.9.8.2'] = ('libvncserver-0.9.8.2-build-fixes.diff', 1)
+        self.options.package.packageName = "libvncserver"
+        self.defaultTarget = '0.9.8.2'
+
+    def setDependencies( self ):
+        self.hardDependencies['virtual/bin-base'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
+
