ae1002de0cb3b383e564b1c64bd28650c032a0a0
Patrick von Reth <patrick.vonreth@gmail.com>
2011-12-20 23:22:02 +0100

    str cast

--- portage/dev-util/git/git-1.7.4.py
+++ portage/dev-util/git/git-1.7.4.py
@@ -49,7 +49,7 @@ class Package(BinaryPackageBase):
             stdout=tmpFile, stderr=tmpFile  )
         tmpFile.seek( 0 )
         for line in tmpFile:
-            if line.find("kde:")>-1:
+            if str(line).find("kde:")>-1:
                 return True
         utils.debug( "adding kde related settings to global git config file",1 )
         utils.system( "%s config --global url.git://anongit.kde.org/.insteadOf kde:" % git)
