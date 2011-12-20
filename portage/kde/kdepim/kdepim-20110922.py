7696e72a05d076bb3923c7649aada62f524568f3
Patrick Spendrin <ps_ml@gmx.de>
2011-12-20 22:50:17 +0100

    fix building of kdepim on hopefully all platforms

--- portage/kde/kdepim/kdepim-20110922.py
+++ portage/kde/kdepim/kdepim-20110922.py
@@ -17,8 +17,10 @@ class subinfo( info.infoclass ):
                                        ('broken-app-icons-fix.diff', 1),
                                        ('fix-running-kontact-touch-running-under-windows.patch', 1)]
         self.patchToApply['4.7.4'] += [("kdepim-soap-4.7.4.diff", 1),
-                                       ("kdepim-calendar-4.7.4.diff", 1),
-                                       ("kdepim-incidenceeditor-ng-4.7.4.diff", 1)]
+                                       ("kdepim-incidenceeditor-ng-4.7.4.diff", 1),
+                                       ("kdepim-kleo-x64-4.7.4.diff", 1),
+                                       ("kdepim-messagecomposer-4.7.4.diff", 1)
+                                       ]
 
     def setDependencies( self ):
         self.runtimeDependencies['kde/kdepim-runtime'] = 'default'
