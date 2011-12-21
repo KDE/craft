9fd9ad6771550967ce4d19bd47a2e0176384fe4f
Patrick Spendrin <ps_ml@gmx.de>
2011-12-21 23:05:57 +0100

    update konversation and also add 1.4 target

--- portage/extragear/konversation/konversation-20111221.py
+++ portage/extragear/konversation/konversation-20111221.py
@@ -8,6 +8,9 @@ class subinfo(info.infoclass):
         for ver in ['1.2.1', '1.2.2', '1.2.3', '1.3', '1.3.1']:
             self.targets[ver] = 'ftp://ftp.kde.org/pub/kde/stable/konversation/' + ver + '/src/konversation-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = 'konversation-' + ver
+        for ver in ['1.4']:
+            self.targets[ver] = 'ftp://ftp.kde.org/pub/kde/stable/konversation/' + ver + '/src/konversation-' + ver + '.tar.xz'
+            self.targetInstSrc[ver] = 'konversation-' + ver
         self.patchToApply['1.3.1'] = ("konversation-1.3.1-20110822.diff", 1)
         self.svnTargets['gitHEAD'] = '[git]kde:konversation'
         self.defaultTarget = 'gitHEAD'
