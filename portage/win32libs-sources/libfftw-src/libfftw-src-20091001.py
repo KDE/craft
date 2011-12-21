85798409a57fffb6c11af283517d8899f7b381cc
Patrick von Reth <patrick.vonreth@gmail.com>
2011-12-21 12:49:16 +0100

    make it possible to build with double enabled on mingw

--- portage/win32libs-sources/libfftw-src/libfftw-src-20091001.py
+++ portage/win32libs-sources/libfftw-src/libfftw-src-20091001.py
@@ -6,7 +6,7 @@ class subinfo( info.infoclass ):
         self.targets[ '3.2.2' ] = 'http://www.fftw.org/fftw-3.2.2.tar.gz'
         self.targetDigests[ '3.2.2' ] = 'd43b799eedfb9408f62f9f056f5e8a645618467b'
         self.targetInstSrc[ '3.2.2' ] = "fftw-3.2.2"
-        self.patchToApply[ '3.2.2' ] = [ ( 'fftw-3.2.2-20101125.diff', 1 ) ]
+        self.patchToApply[ '3.2.2' ] = [ ( 'fftw-3.2.2-20111221.diff', 1 ) ]
         self.shortDescription = "a C subroutine library for computing the discrete Fourier transform (DFT)"
 
         self.defaultTarget = '3.2.2'
