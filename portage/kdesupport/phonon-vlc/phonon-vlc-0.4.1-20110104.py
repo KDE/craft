c857d3e57ed43dbcd011f14ec9d162e92a31c20c
Patrick von Reth <patrick.vonreth@gmail.com>
2011-12-23 11:02:22 +0100

    backport of 9b294fa2, fixed playback of non ascii files

--- portage/kdesupport/phonon-vlc/phonon-vlc-0.4.1-20110104.py
+++ portage/kdesupport/phonon-vlc/phonon-vlc-0.4.1-20110104.py
@@ -19,6 +19,7 @@ class subinfo(info.infoclass):
       self.patchToApply['0.3.1'] = ("phonon-backend-vlc-0.3.1-20101223.diff", 1)
       self.targets['0.4.1'] = "http://download.kde.org/download.php?url=stable/phonon/phonon-backend-vlc/0.4.1/phonon-backend-vlc-0.4.1.tar.xz"
       self.targetInstSrc['0.4.1'] = "phonon-backend-vlc-0.4.1"
+      self.patchToApply['0.4.1'] = ("phonon-backend-vlc-0.4.1-20111223.diff", 1)
       self.targetDigests['0.4.1'] = 'b2957b70e1722f08a231b9e64acfafb799b52d11'
       
       self.svnTargets['gitHEAD'] = '[git]kde:phonon-vlc'
