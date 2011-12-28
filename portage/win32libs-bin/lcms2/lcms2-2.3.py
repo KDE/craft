509b15886a28dfe26d2f4b61f92e8988f7614128
Patrick Spendrin <ps_ml@gmx.de>
2011-12-28 15:37:22 +0100

    update win32libs (cherry picked from commit f6ad83ac5d344fd7d4e1ee73fed833da7450e209)

---
+++ portage/win32libs-bin/lcms2/lcms2-2.3.py
@@ -0,0 +1,38 @@
+# This package-script is automatically updated by the script win32libsupdater.py
+# which can be found in your emerge/bin folder. To update this package, run
+# win32libsupdater.py (and commit the results)
+# based on revision git2307c0b44f07114f16134d0d5f4db2e94b481eb4
+
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        repoUrl = 'http://downloads.sourceforge.net/kde-windows'
+
+        for version in [ '2.3' ]:
+            self.targets[ version ]          = self.getPackage( repoUrl, 'lcms2', version )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'lcms2', version, '.tar.bz2.sha1' )
+
+        self.shortDescription = '''${description}'''
+
+        self.defaultTarget = '2.3'
+
+
+    def setDependencies( self ):
+        if not utils.envAsBool( 'EMERGE_ENABLE_IMPLICID_BUILDTIME_DEPENDENCIES' ):
+            self.buildDependencies[ 'virtual/bin-base' ] = 'default'
+
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
