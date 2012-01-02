4a27093bde899f716b54289b092399abc5a49114
Patrick Spendrin <ps_ml@gmx.de>
2012-01-02 17:28:17 +0100

    add rubenvb compiler (4.6) (cherry picked from commit 8ed5288e22c6093cd8cb37fa22dc73f9da48f5d7)

--- portage/dev-util/mingw-w64/mingw-w64-20111031.py
+++ portage/dev-util/mingw-w64/mingw-w64-20111031.py
@@ -7,6 +7,8 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         for ver in [ "20111031", "20111101" ]:
             self.targets[ver] = "http://downloads.sourceforge.net/sourceforge/mingw-w64/mingw-w64-bin_x86_64-mingw_"+ver+"_sezero.zip"
+        for ver in [ "4.6.3-1" ]:
+            self.targets[ver] = "http://downloads.sourceforge.net/sourceforge/mingw-w64/x86_64-w64-mingw32-gcc-"+ver+"_rubenvb.7z"
         self.defaultTarget = "20111031"
 
     def setDependencies( self ):
@@ -22,7 +24,7 @@ class Package(BinaryPackageBase):
 
     def install(self):
 #        utils.applyPatch( self.imageDir(), os.path.join( self.packageDir(), "gcc_Exit.diff"), 1 )
-        shutil.copy(os.path.join( self.installDir() , "mingw64" , "bin" , "gmake.exe") , os.path.join( self.installDir() , "mingw64" , "bin" , "mingw32-make.exe") )
+#        shutil.copy(os.path.join( self.installDir() , "mingw64" , "bin" , "gmake.exe") , os.path.join( self.installDir() , "mingw64" , "bin" , "mingw32-make.exe") )
         return True
 
 if __name__ == '__main__':
