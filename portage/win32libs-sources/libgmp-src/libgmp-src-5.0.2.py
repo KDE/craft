929397fc9b0affbd564fbbb8aaea74cd81f1d0fd
Patrick von Reth <patrick.vonreth@gmail.com>
2012-01-03 14:49:01 +0100

    updated libgmp which is needed with rubenvb mingw build (cherry picked from commit 82f938181a6ae804e3495b376cac897b0044a909)

---
+++ portage/win32libs-sources/libgmp-src/libgmp-src-5.0.2.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import info
+import os
+import compiler
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['5.0.2'] = "ftp://ftp.gmplib.org/pub/gmp-5.0.2/gmp-5.0.2.tar.bz2"
+        self.targetDigests['5.0.2'] = '2968220e1988eabb61f921d11e5d2db5431e0a35'
+        self.targetInstSrc['5.0.2'] = "gmp-5.0.2"
+        self.defaultTarget = '5.0.2'
+
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.buildDependencies['dev-util/autotools'] = 'default'
+
+
+from Package.AutoToolsPackageBase import *
+from Package.VirtualPackageBase import *
+
+class PackageMinGW(AutoToolsPackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        AutoToolsPackageBase.__init__(self)
+        self.subinfo.options.package.withCompiler = False
+        self.subinfo.options.configure.defines = "--disable-static --enable-shared --enable-cxx --build=x86_64-w64-mingw32"
+        self.buildInSource = True
+        
+    def configure( self):
+        return AutoToolsPackageBase.configure( self, cflags="-std=c99 ", ldflags="")
+        
+
+if compiler.isMinGW():
+    class Package(PackageMinGW):
+        def __init__( self ):
+            PackageMinGW.__init__( self )
+else:
+    class Package(VirtualPackageBase):
+        def __init__( self ):
+            self.subinfo = subinfo()
+            VirtualPackageBase.__init__( self )
+
+if __name__ == '__main__':
+      Package().execute()
+
+    
