a97ae6d5de8a3070850bc2eb594f7a9cb0319a48
Patrick Spendrin <ps_ml@gmx.de>
2012-01-12 00:29:44 +0100

    fix kdemultimedia and add 4.8

--- portage/kde/kdemultimedia/kdemultimedia-20080202.py
+++ portage/kde/kdemultimedia/kdemultimedia-20080202.py
@@ -3,9 +3,14 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/KDE/kdemultimedia'
+        for ver in ['0', '1', '2', '3', '4']:
+            self.targets['4.8.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.8.' + ver + '/src/kdemultimedia-4.8.' + ver + '.tar.bz2'
+            self.targetInstSrc['4.8.' + ver] = 'kdemultimedia-4.8.' + ver
+        self.shortDescription = "KDE multimedia applications (jux, kmix, kmixctrl, kscd)"
         self.defaultTarget = 'svnHEAD'
 
     def setDependencies( self ):
+        self.runtimeDependencies['kde/kde-runtime'] = 'default'
         self.dependencies['kde/kdelibs'] = 'default'
         self.dependencies['win32libs-bin/taglib'] = 'default'
         self.dependencies['win32libs-bin/libogg'] = 'default'
@@ -17,7 +22,6 @@ class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        self.subinfo.options.configure.defines = "-DBUILD_doc=OFF "
 
 if __name__ == '__main__':
     Package().execute()
