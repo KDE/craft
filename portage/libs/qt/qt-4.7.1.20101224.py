3b1c2b6481300c59d966cea028427d4f352ba2ef
Andre Heinecke <aheinecke@intevation.de>
2012-01-12 11:50:24 +0100

    Correct typo in patch name

--- portage/libs/qt/qt-4.7.1.20101224.py
+++ portage/libs/qt/qt-4.7.1.20101224.py
@@ -52,7 +52,7 @@ class subinfo(info.infoclass):
             ('patches/4.7/add-pdbs-on-msvc.diff', 1) , 
             ("patches/4.7/detect-windows-8-as-windows-7.patch", 1 ),
             ("patches/4.7.4/0001-fixed-error-generating-wrong-introspection-string-in.patch", 1 ),
-            ("patches/4.7.4/Fix-regular-expression-llokup-in-qsscertificate-from.patch",1)
+            ("patches/4.7.4/Fix-regular-expression-lookup-in-qsslcertificate-from.patch", 1)
         ]
 
         self.shortDescription = "a cross-platform application framework"
