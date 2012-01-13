fe8716c591069a5f3656999b16c78df5fe210fe7
Patrick Spendrin <ps_ml@gmx.de>
2012-01-13 17:45:03 +0100

    add analitza as a dependency to cantor and kalgebra

--- portage/kde/kdeedu/analitza/analitza-20120113.py
+++ portage/kde/kdeedu/analitza/analitza-20120113.py
@@ -2,13 +2,15 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:cantor'
-        self.shortDescription = 'a KDE frontend for mathematical software'
+        self.svnTargets['gitHEAD'] = '[git]kde:analitza|KDE/4.8|'
+        for ver in ['0', '1', '2', '3', '4']:
+            self.targets['4.8.' + ver] = "ftp://ftp.kde.org/pub/kde/stable/4.8." + ver + "/src/analitza-4.8." + ver + ".tar.bz2"
+            self.targetInstSrc['4.8.' + ver] = 'analitza-4.8.' + ver
+        self.shortDescription = 'a library for mathematical features'
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.dependencies['kde/kde-runtime'] = 'default'
-        self.dependencies['kde/analitza'] = 'default'
 
 from Package.CMakePackageBase import *
 
