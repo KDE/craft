504e807f2187a28494961a18f856dbc7d0b3258e
Patrick Spendrin <ps_ml@gmx.de>
2012-01-13 11:33:09 +0100

    add 4.8.0 release with cleaner patches

--- portage/libs/qt/qt-4.7.1.20101224.py
+++ portage/libs/qt/qt-4.7.1.20101224.py
@@ -57,9 +57,8 @@ class subinfo(info.infoclass):
         ]
         self.patchToApply['4.8.0'] = [
             ('patches/4.7/out-of-source-build.patch', 1), 
-            ('patches/4.7/add-pdbs-on-msvc.diff', 1) , 
-            ("patches/4.7/detect-windows-8-as-windows-7.patch",1),
-            ("patches/4.7.4/0001-fixed-error-generating-wrong-introspection-string-in.patch",1)
+            ('patches/4.8/add-pdbs-on-msvc.diff', 1),
+            ("patches/4.8/detect-windows-8-as-windows-7.patch", 1)
         ]
 
         self.shortDescription = "a cross-platform application framework"
