a4a3f274b6b275023c9f0e3f3e22ad9d8425b299
Patrick von Reth <patrick.vonreth@gmail.com>
2012-01-15 20:03:06 +0100

    disabled kdelibs dependency hupnp, as amarok upnp support doesnt work yet

--- portage/kde/kdelibs/kdelibs-20110130.py
+++ portage/kde/kdelibs/kdelibs-20110130.py
@@ -15,7 +15,7 @@ class subinfo(info.infoclass):
         self.buildDependencies['virtual/base'] = 'default'
         self.buildDependencies['dev-util/perl'] = 'default'
         self.buildDependencies['win32libs-bin/automoc'] = 'default'
-        self.dependencies['kdesupport/hupnp'] = 'default'
+        #self.dependencies['kdesupport/hupnp'] = 'default'
         self.dependencies['kdesupport/kdewin'] = 'default'
         self.dependencies['kdesupport/phonon'] = 'default'
         self.dependencies['kdesupport/attica'] = 'default'
