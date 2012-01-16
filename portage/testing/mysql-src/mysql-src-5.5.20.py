1e3b7c52fccdb19c56280bc9897e4e5fa0b7176c
Patrick von Reth <patrick.vonreth@gmail.com>
2012-01-16 16:30:07 +0100

    its possible to build mysql with msvc10 from src

---
+++ portage/testing/mysql-src/mysql-src-5.5.20.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import info
+import os
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+        self.hardDependencies['virtual/base'] = 'default'
+        self.buildDependencies["gnuwin32/bison"] = "default"
+
+        
+    def setTargets( self ):
+        self.targets['5.5.20'] = 'ftp://ftp.fu-berlin.de/unix/databases/mysql/Downloads/MySQL-5.5/mysql-5.5.20.tar.gz'
+        self.targetDigests['5.5.20'] = 'd5066327c41ac5a338ca0bb748e50bc4e1902442'
+        self.targetInstSrc[ '5.5.20' ] = "mysql-5.5.20"
+        self.patchToApply['5.5.20'] = ("mysql-5.5.20-20120116.diff",1)
+        self.defaultTarget = '5.5.20'
+         
+
+class Package( CMakePackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.defines = "-DWITH_UNIT_TESTS=OFF -DENABLED_PROFILING=OFF"
+        #self.subinfo.options.cmake.openIDE = True
+
+if __name__ == '__main__':
+    Package().execute()
