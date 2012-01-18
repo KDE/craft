2e87c2a518bb2972c57caffdb50ea289d0e7720e
Patrick von Reth <patrick.vonreth@gmail.com>
2012-01-18 20:23:09 +0100

    jom has  become colorfull (cherry picked from commit 95062d18aae4283ed1886e226ab0356c32c4b6bb)

--- portage/dev-util/jom/jom-108-20120122.py
+++ portage/dev-util/jom/jom-108-20120122.py
@@ -10,7 +10,7 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.targets['HEAD'] = 'ftp://ftp.qt.nokia.com/jom/jom.zip'
-        for ver in ['094', '100', '101', '102', '103', '104', '105']:
+        for ver in ['094', '100', '101', '102', '103', '104', '105','106','107','108']:
             self.targets[ver] = 'ftp://ftp.qt.nokia.com/jom/jom' + ver + '.zip'
         self.targetDigests['094'] = '1f946283866cd6f40a5888088f6c7d840b62af2d'
         self.targetDigests['100'] = '545e964c606d28edce582f167574298589970fb4'
@@ -18,9 +18,10 @@ class subinfo(info.infoclass):
         self.targetDigests['102'] = '9cda65f2de954a3236d79f6bc72f5561da32163f'
         self.targetDigests['103'] = 'bb4c0a1db803a3ed8c31da29b1ad672bbcc636cb'
         self.targetDigests['105'] = '47c0b3fb58a753bb32b707528adc10bebf2b028a'
+        self.targetDigests['108'] = '5efb1ada73d2886722ce0473b1b79de0bef38e1d'
         self.targets['101-patched'] = 'http://www.winkde.org/pub/kde/ports/win32/repository/other/jom101-patched.7z'
         self.targetDigests['101-patched'] = '5f878e50cdd05f390b2737d4050a740edd48337f'
-        self.defaultTarget = '105'
+        self.defaultTarget = '108'
 
     def setBuildOptions( self ):
         self.disableHostBuild = False
