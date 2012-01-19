af334fef576bb957d9fe55a3ad05bc756efc303d
Patrick von Reth <patrick.vonreth@gmail.com>
2012-01-19 15:10:06 +0100

    try to provide ccache bin

--- portage/win32libs-sources/ccache-src/ccache-src-gitHEAD.py
+++ portage/win32libs-sources/ccache-src/ccache-src-gitHEAD.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 import info
 import os
-
-os.putenv("CXX","g++")
-os.putenv("CC","gcc")
+import compiler
 
 class subinfo(info.infoclass):
     def setTargets( self ):
@@ -14,16 +12,30 @@ class subinfo(info.infoclass):
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
-        self.buildDependencies['dev-util/autotools'] = 'default'
+        if compiler.isMinGW():
+            self.buildDependencies['dev-util/autotools'] = 'default'
 
 
 from Package.AutoToolsPackageBase import *
+from Package.VirtualPackageBase import *
 
-class Package(AutoToolsPackageBase):
+class PackageMinGW(AutoToolsPackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.package.withCompiler = False
+        os.putenv("CXX","g++")
+        os.putenv("CC","gcc")
+
+if compiler.isMinGW():
+    class Package(PackageMinGW):
+        def __init__( self ):
+            PackageMinGW.__init__( self )
+else:
+    class Package(VirtualPackageBase):
+        def __init__( self ):
+            self.subinfo = subinfo()
+            VirtualPackageBase.__init__( self )
 
 if __name__ == '__main__':
-    Package().execute()
+      Package().execute()
