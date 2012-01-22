210422ece85f5a3334aaf876a2d7f32092e3e0ed
Andrius da Costa Ribas <andriusmao@gmail.com>
2012-01-22 19:28:12 -0200

    Add libface and libkface, needed for Digikam build

---
+++ portage/extragear/libkface/libkface-1.0.0.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import info
+
+# deprecated class
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        print "setTargets"
+        self.svnTargets['gitHEAD'] = '[git]kde:libkface'
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.dependencies['kdesupport/libface'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
