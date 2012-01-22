629a2307b4456c2964fb259d64f3f96362f6f8cc
Patrick von Reth <patrick.vonreth@gmail.com>
2012-01-22 16:14:42 +0100

    added new binaries (cherry picked from commit 1bfb20106cf499e6462223c173bae7a6db555c1d)

---
+++ portage/win32libs-bin/boost-regex/boost-regex-1.48.0.py
@@ -0,0 +1,38 @@
+# This package-script is automatically updated by the script win32libsupdater.py
+# which can be found in your emerge/bin folder. To update this package, run
+# win32libsupdater.py (and commit the results)
+# based on revision gita7c2a11bea1203153d7f09b40c407a5c63b464d3
+
+from Package.BinaryPackageBase import *
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        repoUrl = 'http://downloads.sourceforge.net/kde-windows'
+
+        for version in [ '1.48.0' ]:
+            self.targets[ version ]          = self.getPackage( repoUrl, 'boost-regex', version )
+            self.targetDigestUrls[ version ] = self.getPackage( repoUrl, 'boost-regex', version, '.tar.bz2.sha1' )
+
+        self.shortDescription = '''${description}'''
+
+        self.defaultTarget = '1.48.0'
+
+
+    def setDependencies( self ):
+        if not utils.envAsBool( 'EMERGE_ENABLE_IMPLICID_BUILDTIME_DEPENDENCIES' ):
+            self.buildDependencies[ 'virtual/bin-base' ] = 'default'
+
+
+    def setBuildOptions( self ):
+        self.disableHostBuild = False
+        self.disableTargetBuild = True
+
+class Package(BinaryPackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    BinaryPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
