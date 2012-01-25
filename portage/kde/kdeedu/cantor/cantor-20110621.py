4a4829361e31d8bb72849cbd2c93ee480aedb154
Patrick Spendrin <ps_ml@gmx.de>
2012-01-25 17:24:56 +0100

    fix cantor on msvc

--- portage/kde/kdeedu/cantor/cantor-20110621.py
+++ portage/kde/kdeedu/cantor/cantor-20110621.py
@@ -6,6 +6,7 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4']:
             self.targets['4.8.' + ver] = "ftp://ftp.kde.org/pub/kde/stable/4.8." + ver + "/src/cantor-4.8." + ver + ".tar.bz2"
             self.targetInstSrc['4.8.' + ver] = 'cantor-4.8.' + ver
+        self.patchToApply['4.8.0'] = [("cantor-4.8.0-20120125.diff", 1)]
         self.shortDescription = 'a KDE frontend for mathematical software'
         self.defaultTarget = 'gitHEAD'
 
