c69380f164fdc8a35b84f108cba2c3a51c88410b
Patrick Spendrin <ps_ml@gmx.de>
2012-01-28 00:38:44 +0100

    also enable docs for krita

--- portage/testing/calligra/calligra-20101207.py
+++ portage/testing/calligra/calligra-20101207.py
@@ -24,7 +24,6 @@ class Package( CMakePackageBase ):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        self.subinfo.options.configure.defines = "-DBUILD_doc=OFF "
 
 if __name__ == '__main__':
     Package().execute()
