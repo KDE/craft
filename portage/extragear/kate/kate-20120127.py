20daa1694f93df002c51968a8ae585e4639c52bd
Patrick Spendrin <ps_ml@gmx.de>
2012-01-27 12:56:29 +0100

    add 4.8.0 release - missed that before

--- portage/extragear/kate/kate-20120127.py
+++ portage/extragear/kate/kate-20120127.py
@@ -2,7 +2,10 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:kate'
+        self.svnTargets['gitHEAD'] = '[git]kde:kate|KDE/4.8|'
+        for ver in ['0', '1', '2', '3', '4']:
+            self.targets['4.8.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.8.' + ver + '/src/kate-4.8.' + ver + '.tar.bz2'
+            self.targetInstSrc['4.8.' + ver] = 'kate-4.8.' + ver
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
