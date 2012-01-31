3b915dd373ec382faf60011079ad142e988437c3
Patrick Spendrin <ps_ml@gmx.de>
2012-02-01 00:05:34 +0100

    use argparse for repackage.py

--- bin/repackage.py
+++ bin/repackage.py
@@ -1,25 +1,36 @@
 import sys
 import os
+import argparse
 
-def usage():
-    print()
-    print("can be used to repackage a packagelist")
-    print("Syntax:")
-    print("\t", sys.argv[0], "packagelist.txt")
 
+usage = "%(prog)s -f list.txt\n" \
+        "   or: %(prog)s --file=list.txt \n"
+parser = argparse.ArgumentParser(prog=sys.argv[0], usage=usage)
+
+parser.add_argument("-f", "--file", dest = "filename", metavar = "list.txt",
+                    help="the filename for a packagelist")
+parser.add_argument("-t", "--type", action = "store", default = OUTPUT_DOT,
+        help="Change the output format type possible values: xml kwi, dot")
+
+args, rest = parser.parse_known_args()
 
 if len(sys.argv) == 1:
-    usage()
+    parser.print_help()
     exit(1)
 
-if not os.path.exists(sys.argv[1]):
-    print("error: couldn't find packagelist file", sys.argv[1])
-    usage()
+if not hasattr(args, "filename") or args.filename != None or not os.path.exists(args.filename):
+    print("error: couldn't find packagelist or can't read it")
+    parser.print_help()
     exit(1)
+
+filename = args.filename
 
 cmdtemplate = "emerge --package%s %s/%s"
 targettemplate = " --target=%s"
 patchleveltemplate = " --patchlevel=%s"
+
+
+
 with open(sys.argv[1], 'r') as f:
     for line in f:
         if line.startswith('#'):
