961a205c503c8cf79c13bd36eedad5fe3ebd8e32
Patrick von Reth <patrick.vonreth@gmail.com>
2012-02-11 13:11:42 +0100

    updated qewitter packaging scripts

--- portage/package/qewitter-portable-package/qewitter-portable-package-20110804.py
+++ portage/package/qewitter-portable-package/qewitter-portable-package-20110804.py
@@ -2,31 +2,29 @@
 import info
 from Package.VirtualPackageBase import *
 from Packager.NullsoftInstallerPackager import *
+from Packager.PortablePackager import *
 
-# This is an example package for building 
+# This is an example package for building
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
         _,gitVersion = portage.getPackageInstance('qt-apps','qewitter').getPackageVersion() 
         self.svnTargets[ 'git-' + gitVersion  ] = ""
-        self.svnTargets[ '0.11pre' ] = ""
-        self.defaultTarget = '0.11pre'
-    
+        self.svnTargets[ '0.11' ] = ""
+        self.defaultTarget = '0.11'
+
     def setDependencies( self ):
         self.dependencies[ 'qt-apps/qewitter' ] = 'default'
+        self.dependencies[ 'kdesupport/snorenotify' ] = 'default'
         self.dependencies[ 'libs/runtime' ] = 'default'
-    
-class Package( NullsoftInstallerPackager, VirtualPackageBase ):
+        
+class Package( PortablePackager, VirtualPackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         whitelists = [ 'whitelist.txt' ]
         blacklists = [ NSIPackagerLists.runtimeBlacklist, 'blacklist.txt' ]
+        PortablePackager.__init__( self, whitelists ,blacklists )
         VirtualPackageBase.__init__( self )
-        NullsoftInstallerPackager.__init__( self, whitelists,blacklists )
-        
-        self.defines[ "executable" ] = "bin\\qewitter.exe"
-        self.defines[ "icon" ] = os.path.join(portage.getPackageInstance('qt-apps','qewitter').sourceDir(),"data","qewitter.ico")
-        
 
 if __name__ == '__main__':
     Package().execute()
