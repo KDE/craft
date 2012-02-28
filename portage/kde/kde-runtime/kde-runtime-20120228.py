ca484948f8216b592e0b9fe2a91f4b65a5788844
Patrick Spendrin <ps_ml@gmx.de>
2012-02-28 01:23:18 +0100

    also reenable nepomuk in kde-runtime, even if that won't help a lot

--- portage/kde/kde-runtime/kde-runtime-20120228.py
+++ portage/kde/kde-runtime/kde-runtime-20120228.py
@@ -6,6 +6,7 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4']:
             self.targets['4.8.' + ver] = "ftp://ftp.kde.org/pub/kde/stable/4.8." + ver + "/src/kde-runtime-4.8." + ver + ".tar.bz2"
             self.targetInstSrc['4.8.' + ver] = 'kde-runtime-4.8.' + ver
+        self.patchToApply['4.8.0'] = [("reenable-nepomuk-on-kde-runtime.diff", 1)]
         self.shortDescription = "Plugins and applications necessary for the running of KDE applications"
         self.defaultTarget = 'gitHEAD'
 
