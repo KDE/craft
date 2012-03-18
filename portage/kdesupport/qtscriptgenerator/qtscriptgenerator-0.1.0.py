9d3908a3baca1af3427d2c3f0c2a2cbc0fbd1008
Patrick von Reth <patrick.vonreth@gmail.com>
2012-03-18 18:17:27 +0100

    added qt4.8 patch

--- portage/kdesupport/qtscriptgenerator/qtscriptgenerator-0.1.0.py
+++ portage/kdesupport/qtscriptgenerator/qtscriptgenerator-0.1.0.py
@@ -9,7 +9,10 @@ class subinfo( info.infoclass ):
         self.targets['0.1.0'] = 'http://qtscriptgenerator.googlecode.com/files/qtscriptgenerator-src-0.1.0.tar.gz'
         self.targetInstSrc['0.1.0'] = 'qtscriptgenerator-src-0.1.0'
         self.targetDigests['0.1.0'] = 'eeae733106369e289f257b754822bc372fd6ba75'
-        self.patchToApply['0.1.0'] = [("qtscriptgenerator-cmake.diff", 1), ("qtscriptgenerator.diff", 1), ("qtscriptgenerator.gcc-4.4.diff", 1)]
+        self.patchToApply['0.1.0'] = [("qtscriptgenerator-cmake.diff", 1),
+                                      ("qtscriptgenerator.diff", 1),
+                                      ("qtscriptgenerator.gcc-4.4.diff", 1),
+                                      ("qtscriptgenerator-0.1.0-qt48.diff",1)]
         self.shortDescription = "a tool that generates Qt bindings for Qt Script"
         self.defaultTarget = '0.1.0'
 
