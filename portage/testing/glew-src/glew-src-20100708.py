06e1cdd419d1e05c20f3aa5fb8f88102a78075b7
Patrick Spendrin <ps_ml@gmx.de>
2012-03-20 23:10:53 +0100

    update glew-src to 1.7.0 (cherry picked from commit 2486080352c34afa9ed2486d88296dd055a00d71)

--- portage/testing/glew-src/glew-src-20100708.py
+++ portage/testing/glew-src/glew-src-20100708.py
@@ -5,12 +5,13 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.5.4'] = 'http://downloads.sourceforge.net/project/glew/glew/1.5.4/glew-1.5.4.zip'
-        self.targetInstSrc['1.5.4'] = 'glew-1.5.4'
+        for ver in ['1.5.4', '1.7.0']:
+            self.targets[ ver ] = 'http://downloads.sourceforge.net/project/glew/glew/' + ver + '/glew-'+ ver + '.zip'
+            self.targetInstSrc[ ver ] = 'glew-' + ver
         self.patchToApply['1.5.4'] = ('glew-1.5.4-20100708.diff', 1)
-        self.options.package.withCompiler = False
-        self.options.package.packageName = "glew"
-        self.defaultTarget = '1.5.4'
+        self.patchToApply['1.7.0'] = ('glew-1.7.0-20120320.diff', 1)
+        self.targetDigests['1.7.0'] = '107c155ff5b69d97b9c530b40e4e8da571aaf729'
+        self.defaultTarget = '1.7.0'
 
     def setDependencies( self ):
         self.hardDependencies['virtual/bin-base'] = 'default'
