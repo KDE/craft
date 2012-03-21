0bf2b00ca34ceefae8663f8a74257270b1bb6802
Patrick von Reth <patrick.vonreth@gmail.com>
2012-03-21 11:16:35 +0100

    updated quassel targets

--- portage/extragear/quassel/quassel-0.8.0.py
+++ portage/extragear/quassel/quassel-0.8.0.py
@@ -8,11 +8,12 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = 'git://gitorious.org/quassel/quassel.git'
         self.svnTargets['0.6'] = 'git://gitorious.org/quassel/quassel.git|0.6|'
-        for ver in ['0.7.1','0.7.2','0.7.3']:
+        for ver in ['0.7.1','0.7.2','0.7.3','0.8.0']:
             self.targets[ver] = 'http://quassel-irc.org/pub/quassel-%s.tar.bz2' % ver
             self.targetInstSrc[ver] = 'quassel-%s' % ver
         self.targetDigests['0.7.1'] = '791086da977033a1bbee3effa317668b3726bd7f'
-
+        self.targetDigests['0.8.0'] = 'b74967fa9f19b5d7c708279075cc0ef3a3dbbe8b'
+        
         self.defaultTarget = 'gitHEAD'
 
 
