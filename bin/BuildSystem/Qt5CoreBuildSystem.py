86aec228d1f59c71abc81f18c668e235bc21ea8c
Patrick von Reth <parick.vonreth@gmail.com>
2012-04-14 13:37:21 +0200

    ugly workaround for the missing 5 in libname when linking problem

--- bin/BuildSystem/Qt5CoreBuildSystem.py
+++ bin/BuildSystem/Qt5CoreBuildSystem.py
@@ -7,6 +7,7 @@ import os
 import utils
 import compiler
 import shutil
+import re
 
 from BuildSystem.QMakeBuildSystem import *
 
@@ -16,11 +17,38 @@ class Qt5CoreBuildSystem(QMakeBuildSystem):
         utils.putenv( "QMAKESPEC", os.path.join(os.getenv("KDEROOT"), 'mkspecs', self.platform ))
         os.putenv("INSTALL_ROOT",os.path.join(self.imageDir(),"bin"))
 
+        
+    def configure( self, configureDefines="" ):
+        moduleDir =  os.path.join(self.sourceDir(),"modules")
+        if not os.path.exists(moduleDir):
+          moduleDir =  os.path.join(self.sourceDir(),"src","modules")
+        if os.path.exists(moduleDir):
+            pattern = re.compile("^(QT.* = Qt.*)+(?<!5)$")
+            filePattern = re.compile(".*\.pri")
+            for module in os.listdir(moduleDir):
+                if filePattern.match(module):
+                  f_in = open(os.path.join(moduleDir,module),"rt+")
+                  in_lines = f_in.readlines()
+                  out_lines = list()
+                  f_in.close()                  
+                  changed = False
+                  for line in in_lines:
+                      if pattern.match(line):
+                          changed = True
+                          line = line.strip() + "5\n"
+                      out_lines.append(line)
+                      
+                  if changed:
+                      f_out = open(os.path.join(moduleDir,module),"wt+")
+                      f_out.writelines(out_lines)
+                      f_out.close()
+        return QMakeBuildSystem.configure(self,configureDefines)
+              
 
 
     def install( self, options=None ):
        """implements the make step for Qt projects"""
-       if not QMakeBuildSystem.install( self ):
+       if not QMakeBuildSystem.install( self ,options):
            return False
        if not  os.path.exists(os.path.join( self.installDir(), "bin" )):
           os.mkdir( os.path.join( self.installDir(), "bin" ) )
