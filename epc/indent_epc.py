78a8ca44765a53d7c8b0abe84dcb726b12149687
Patrick von Reth <vonreth@kde.org>
2012-04-17 10:11:25 +0200

    added epc indent tool, indented epcs

---
+++ epc/indent_epc.py
@@ -0,0 +1,13 @@
+import json
+import sys
+
+if len(sys.argv)<2:
+    sys.exit(1)
+sys.argv.pop(0)
+inFile = sys.argv.pop(0)
+epc = open(inFile,"rt+")
+tmp = json.load(epc)
+epc.close()
+epc = open(inFile,"wt+",newline='\n')
+epc.write(json.dumps(tmp,indent = 2))
+epc.close()
