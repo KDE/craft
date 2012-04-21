78469e9e4480a8977ff17d4babdc0c8c661054ae
Patrick Spendrin <ps_ml@gmx.de>
2012-04-21 12:51:57 +0200

    add a 2.8.8 release for testing purposes (cherry picked from commit 59b7e85796b4ae10fdf3a556ac370ccec3c035c1)

--- portage/dev-util/cmake-pkg/cmake-pkg-20100421.py
+++ portage/dev-util/cmake-pkg/cmake-pkg-20100421.py
@@ -6,12 +6,13 @@ class subinfo(info.infoclass):
         self.svnTargets['gitHEAD'] = "git://cmake.org/cmake.git"
         self.svnTargets['release'] = "git://cmake.org/cmake.git|release|"
         self.svnTargets['v2.8.2'] = "git://cmake.org/cmake.git|release|v2.8.2"
-        for ver in ['2.8.0', '2.8.1', '2.8.2', '2.8.3']:
+        for ver in ['2.8.0', '2.8.1', '2.8.2', '2.8.3', '2.8.8']:
             self.targets[ ver ] = "http://www.cmake.org/files/v2.8/cmake-" + ver + ".tar.gz"
             self.targetInstSrc[ ver ] = "cmake-" + ver
         self.patchToApply['2.8.0'] = ("cmake-2.8.0-20100512.diff", 1)
         self.patchToApply['2.8.1'] = ("cmake-2.8.1-20100511.diff", 1)
         self.patchToApply['2.8.3'] = ("cmake-2.8.3-20101222.diff", 1)
+        self.patchToApply['2.8.8'] = ("cmake-2.8.3-20101222.diff", 1)
         self.defaultTarget = 'release'
 
     def setDependencies( self ):
@@ -23,6 +24,7 @@ class Package(CMakePackageBase):
     def __init__( self):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
+        self.subinfo.options.merge.destinationPath = "dev-utils"
 
 if __name__ == '__main__':
     Package().execute()
