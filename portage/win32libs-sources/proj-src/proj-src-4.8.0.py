3e99acf4cc909ce09582df67b9a1ceeb2e5a8bb0
Ralf Habacker <ralf.habacker@freenet.de>
2012-04-24 09:13:07 +0200

    added proj library

---
+++ portage/win32libs-sources/proj-src/proj-src-4.8.0.py
@@ -0,0 +1,23 @@
+import info
+from Package.CMakePackageBase import *
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        for ver in ['4.8.0']:
+            self.targets[ver] = 'http://download.osgeo.org/proj/proj-%s.zip' % ver
+            self.targetInstSrc[ver] = 'proj-' + ver
+        self.patchToApply[ '4.8.0' ] = [ ( "proj-4.8.0-20120424.diff", 1 ) ]
+        self.targetDigests['4.8.0'] = '15f51318b0314f107919b83bdab7b03f31193b75'
+        self.shortDescription = "Projection library"
+        self.defaultTarget = '4.8.0'
+
+    def setDependencies( self ):
+        self.buildDependencies[ 'virtual/base' ] = 'default'
+
+class Package( CMakePackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
