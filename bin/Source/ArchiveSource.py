9afd0bf5dc2fc33168c2835bc97df45043e91292
Ralf Habacker <ralf.habacker@freenet.de>
2012-04-24 09:07:20 +0200

    fixed generating patch filename when no patches are used

--- bin/Source/ArchiveSource.py
+++ bin/Source/ArchiveSource.py
@@ -218,9 +218,10 @@ class ArchiveSource(SourceBase):
                     utils.debug( "applying patch %s with patchlevel: %s" % ( fileName, patchdepth ) )
                     if not self.applyPatch( fileName, patchdepth, os.path.join( tmpdir, packagelist[ 0 ] ) ):
                         return False
-                patchName = os.path.join( self.buildRoot(), patches[-1][0] )
-            else:
-                patchName = os.path.join( self.buildRoot(), patches[0] )
+                if patches[-1][0]:
+                    patchName = os.path.join( self.buildRoot(), patches[-1][0] )
+            elif patches[-1]:
+                patchName = os.path.join( self.buildRoot(), patches[-1] )
 
         # move the packages up and rename them to be different from the original source directory
         for directory in packagelist:
