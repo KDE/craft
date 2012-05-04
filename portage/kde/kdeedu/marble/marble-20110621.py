d56a2dfd4c6fc5e0c4329c2bbac100681e7fabec
Ralf Habacker <ralf.habacker@freenet.de>
2012-05-04 13:38:25 +0200

    updated and added more patches

--- portage/kde/kdeedu/marble/marble-20110621.py
+++ portage/kde/kdeedu/marble/marble-20110621.py
@@ -3,12 +3,16 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:marble'
-        self.patchToApply[ 'gitHEAD' ] = [ ( "0001-first-version-of-flightgear-position-provider-plugin.patch_", 1 ) ]
+        self.patchToApply[ 'gitHEAD' ] = [("0001-first-version-of-flightgear-position-provider-plugin.patch_", 1),
+                                          ("0003-compile-fix.patch_", 1),
+                                          ("0004-listen-on-any-address-to-support-mapping-over-networ.patch_", 1),
+                                         ]
         self.shortDescription = 'the desktop globe'
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.dependencies['kde/kde-runtime'] = 'default'
+        # TODO: how to limit to gitHEAD for now
         self.buildDependencies['win32libs-sources/nmealib-src'] = 'default'
 
 from Package.CMakePackageBase import *
