bd9932600b92fd65373669a1bdde5236e822a495
Patrick von Reth <vonreth@kde.org>
2012-05-22 12:37:01 +0200

    added vsd (cherry picked from commit 07fba0f0b840d0f72b8e4a3ba169875dc00a0cef)

---
+++ portage/dev-util/vsd/vsd-20120520.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import info
+import os
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setDependencies( self ):
+      self.buildDependencies['virtual/base'] = 'default'
+
+        
+    def setTargets( self ):
+      self.svnTargets['gitHEAD'] = 'git://gitorious.org/vsd/vsd.git'
+      self.defaultTarget = 'gitHEAD'
+         
+
+class Package( CMakePackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__(self)
+
+if __name__ == '__main__':
+    Package().execute()
