b29e292388496718a7529f0f19b704912a86d2fe
Patrick von Reth <vonreth@kde.org>
2012-05-23 20:50:43 +0200

    added libxml2.8.0 the patch isnt clean and some parts are removed so please have a look on it

--- portage/win32libs-sources/libxml2-src/libxml2-src-2.7.7-1.py
+++ portage/win32libs-sources/libxml2-src/libxml2-src-2.7.7-1.py
@@ -4,13 +4,15 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['2.7.7', '2.7.8']:
+        for ver in ['2.7.7', '2.7.8','2.8.0']:
             self.targets[ver] = 'ftp://xmlsoft.org/libxml2/libxml2-' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'libxml2-' + ver
         self.patchToApply['2.7.7'] = ("libxml2-2.7.7-20110406.diff", 1)
         self.targetDigests['2.7.7'] = '8592824a2788574a172cbddcdc72f734ff87abe3'
         self.patchToApply['2.7.8'] = ("libxml2-2.7.8-20110105.diff", 1)
         self.targetDigests['2.7.8'] = '859dd535edbb851cc15b64740ee06551a7a17d40'
+        self.patchToApply['2.8.0'] = ("libxml2-2.8.0-20110105.diff", 1)
+        self.targetDigests['2.8.0'] = 'a0c553bd51ba79ab6fff26dc700004c6a41f5250'
         self.shortDescription = "XML C parser and toolkit (runtime and applications)"
 
         self.defaultTarget = '2.7.7'
