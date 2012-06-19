1baaa0db153017dff2032090d7ebf636e478c090
Patrick von Reth <vonreth@kde.org>
2012-06-19 20:28:00 +0200

    updated mysql,generate mingw importlibs, thx to SaroEngles who found out that was the reason for the x64 mysql crash

---
+++ portage/testing/mysql-pkg/mysql-pkg-5.5.25.py
@@ -0,0 +1,78 @@
+from Package.CMakePackageBase import *
+import info
+import shutil
+import os
+import re
+import urllib.request, urllib.parse, urllib.error
+import emergePlatform
+
+# currently only needed from kdenetwork
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        #http://ftp.gwdg.de/pub/misc/mysql/Downloads/MySQL-5.5/mysql-5.5.25-win32.zip
+        self.baseURL = "ftp://ftp.gwdg.de/pub/misc/mysql/Downloads/"
+        if( emergePlatform.buildArchitecture() == 'x64' ):
+          self.targets[ '5.1.63'] = self.baseURL+"MySQL-5.1/mysql-noinstall-5.1.63-winx64.zip"
+          self.targetInstSrc[ '5.1.56' ] = "mysql-5.1.63-winx64"
+          self.targets[ '5.5.25'] = self.baseURL+"MySQL-5.5/mysql-5.5.25-winx64.zip"
+          self.targetInstSrc[ '5.5.25' ] = "mysql-5.5.25-winx64"
+        else:
+          self.targets[ '5.1.63'] = self.baseURL+"MySQL-5.1/mysql-noinstall-5.1.63-win32.zip"
+          self.targetInstSrc[ '5.1.63' ] = "mysql-5.1.63-win32"          
+          self.targetDigests['5.1.63'] = '37827259cb5319d91940ff4f825e23165364305d'
+          self.targets[ '5.5.25'] = self.baseURL+"MySQL-5.5/mysql-5.5.25-win32.zip"
+          self.targetInstSrc[ '5.5.25' ] = "mysql-5.5.25-win32"
+          self.targetDigests['5.5.25'] = 'd4e937bb12c1e934b4db0b517019a22ef7ace6ef'
+        self.shortDescription = "MySql database server and embedded library"
+        self.defaultTarget = '5.5.25'
+
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/bin-base'] = 'default'
+
+
+class Package(CMakePackageBase):
+  def __init__(self):
+    self.subinfo = subinfo()
+    self.subinfo.options.package.packSources = False
+    self.subinfo.options.package.disableStriping = True
+    CMakePackageBase.__init__( self )
+
+
+  def compile( self ):
+    return True
+
+  def install( self ):
+    if( not self.cleanImage()):
+      return False
+    if self.subinfo.buildTarget.startswith("5.1"):
+        shutil.copytree( os.path.join( self.sourceDir() , "bin" ) , os.path.join( self.installDir(), "bin") , ignore=shutil.ignore_patterns('*.pdb','*.map','*test.exe','mysqld-debug.exe') )
+        # do not create lib files, just take the given ones, because of the stdcall problem
+        os.mkdir( os.path.join( self.installDir(), "lib"  ) )
+        shutil.copy( os.path.join( self.sourceDir() , "lib" , "opt" , "libmysql.lib" ) , os.path.join( self.installDir(), "lib" , "libmysql.lib" ) )
+        os.mkdir(os.path.join( self.installDir(), "lib" , "plugin"))
+        shutil.copy( os.path.join( self.sourceDir() , "lib" , "plugin" , "ha_innodb_plugin.dll" ) , os.path.join( self.installDir(), "lib" , "plugin" , "ha_innodb_plugin.dll" ) )
+        shutil.copy( os.path.join( self.sourceDir() , "Embedded" , "DLL" , "release" , "libmysqld.dll" ) , os.path.join( self.installDir(), "bin" , "libmysqld.dll" ) )
+        shutil.copy( os.path.join( self.sourceDir() , "Embedded" , "DLL" , "release" , "libmysqld.lib" ) , os.path.join( self.installDir(), "lib" , "libmysqld.lib" ) )
+        shutil.copytree( os.path.join( self.sourceDir() , "include" ) , os.path.join( self.installDir(), "include" ) ,  ignore=shutil.ignore_patterns('*.def') )
+        shutil.copytree( os.path.join( self.sourceDir() , "scripts" ) , os.path.join( self.installDir(), "scripts" ) )
+        shutil.copytree( os.path.join( self.sourceDir() , "share" ) , os.path.join( self.installDir(), "share" ) , ignore=shutil.ignore_patterns('Makefile*') )
+        shutil.copytree( os.path.join( self.sourceDir() , "data" ) , os.path.join( self.installDir(), "data" ) )
+    else:
+        shutil.copytree( os.path.join( self.sourceDir() , "bin" ) , os.path.join( self.installDir(), "bin") , ignore=shutil.ignore_patterns('*.pdb','*.map','*test*','mysqld-debug.exe','*.pl','debug*') )    
+        shutil.copy( os.path.join( self.sourceDir() , "lib" , "libmysqld.dll" ) , os.path.join( self.installDir(), "bin" , "libmysqld.dll" ) )
+        shutil.copy( os.path.join( self.sourceDir() , "lib" , "libmysql.dll" ) , os.path.join( self.installDir(), "bin" , "libmysql.dll" ) )
+        shutil.copytree( os.path.join( self.sourceDir() , "lib" ) , os.path.join( self.installDir(), "lib") , ignore=shutil.ignore_patterns('*.pdb','*.map','debug*','libmysqld.dll','libmysql.dll','mysql*') ) 
+        if compiler.isMinGW():
+            utils.createImportLibs( "libmysqld" , self.installDir() )
+            utils.createImportLibs( "libmysql" , self.installDir() )        
+        shutil.copytree( os.path.join( self.sourceDir() , "include" ) , os.path.join( self.installDir(), "include" ) ,  ignore=shutil.ignore_patterns('*.def') )
+        shutil.copytree( os.path.join( self.sourceDir() , "scripts" ) , os.path.join( self.installDir(), "scripts" ) )
+        shutil.copytree( os.path.join( self.sourceDir() , "share" ) , os.path.join( self.installDir(), "share" ) , ignore=shutil.ignore_patterns('Makefile*') )
+        shutil.copytree( os.path.join( self.sourceDir() , "data" ) , os.path.join( self.installDir(), "data" ) )
+    return True
+
+if __name__ == '__main__':
+    Package().execute()
