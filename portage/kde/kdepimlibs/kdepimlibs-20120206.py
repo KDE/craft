69ffd48c6d62319ba5612d45b62adb6e7bd0b5e9
Patrick von Reth <vonreth@kde.org>
2012-06-27 10:11:15 +0200

    fixed package url

--- portage/kde/kdepimlibs/kdepimlibs-20120206.py
+++ portage/kde/kdepimlibs/kdepimlibs-20120206.py
@@ -3,15 +3,20 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:kdepimlibs|KDE/4.8|'
-        for ver in ['0', '1', '2', '3', '4']:
-            self.targets['4.8.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.8.' + ver + '/src/kdepimlibs-4.8.' + ver + '.tar.bz2'
-            self.targetInstSrc['4.8.' + ver] = 'kdepimlibs-4.8.' + ver
+        
+        self.targets['4.8.0'] = 'ftp://ftp.kde.org/pub/kde/stable/4.8.0/src/kdepimlibs-4.8.0.tar.bz2'
+        self.targetInstSrc['4.8.0'] = 'kdepimlibs-4.8.0'
         self.patchToApply['4.8.0'] = [("kdepimlibs-4.8.0-20120206.diff", 1)]
+        
+        for ver in ['1', '2', '3', '4']:
+            self.targets['4.8.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.8.' + ver + '/src/kdepimlibs-4.8.' + ver + '.tar.xz'
+            self.targetInstSrc['4.8.' + ver] = 'kdepimlibs-4.8.' + ver
+        
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.dependencies['kde/kdelibs'] = 'default'
-        self.dependencies['kdesupport/akonadi'] = 'default'
+        #self.dependencies['kdesupport/akonadi'] = 'default'
         self.dependencies['win32libs-bin/cyrus-sasl'] = 'default'
         self.dependencies['win32libs-bin/libical'] = 'default'
         self.dependencies['win32libs-bin/gpgme'] = 'default'
