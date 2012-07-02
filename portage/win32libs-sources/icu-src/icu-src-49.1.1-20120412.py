6eadd04389fc717f24a010c4578adcd5891b8d2c
Patrick von Reth <vonreth@kde.org>
2012-07-02 15:30:00 +0300

    make icu build on mingw

--- portage/win32libs-sources/icu-src/icu-src-49.1.1-20120412.py
+++ portage/win32libs-sources/icu-src/icu-src-49.1.1-20120412.py
@@ -8,8 +8,9 @@ import compiler
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['49.1.1'] = 'http://download.icu-project.org/files/icu4c/49.1.1/icu4c-49_1_1-src.tgz'
-        self.targetInstSrc['49.1.1'] = "icu"
+        self.targetInstSrc['49.1.1'] = "icu/source"
         self.targetDigests['49.1.1'] = 'f407d7e2808b76e3a6ca316aab896aef74bf6722'
+        self.patchToApply[ '49.1.1' ] = ('icu-20120702.diff', 1)
         self.defaultTarget = '49.1.1'
 
     def setDependencies( self ):
@@ -17,7 +18,7 @@ class subinfo(info.infoclass):
 
 from Package.CMakePackageBase import *
 
-class Package(CMakePackageBase):
+class PackageCMake(CMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
@@ -34,6 +35,22 @@ class Package(CMakePackageBase):
         utils.copyDir(os.path.join(self.sourceDir(),"include") , os.path.join(self.imageDir(),"include"))
         utils.copyDir(os.path.join(self.sourceDir(),"lib") , os.path.join(self.imageDir(),"lib"))
         return True
+        
+from Package.AutoToolsPackageBase import *
+
+class PackageMSys(AutoToolsPackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        AutoToolsPackageBase.__init__(self)
+        
+if compiler.isMinGW():
+    class Package(PackageMSys):
+        def __init__( self ):
+            PackageMSys.__init__( self )
+else:
+    class Package(PackageCMake):
+        def __init__( self ):
+            PackageCMake.__init__( self )
 
 if __name__ == '__main__':
-    Package().execute()
+      Package().execute()
