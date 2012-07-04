ded94d0bb9ec5a761d324071be7565ef7017deb8
Patrick von Reth <vonreth@kde.org>
2012-07-04 11:24:53 +0300

    added 1.0.0 target (cherry picked from commit f61ace46850ebc3554161fac60ad9035efdc3aec)

--- portage/kdesupport/liblastfm/liblastfm-20111012.py
+++ portage/kdesupport/liblastfm/liblastfm-20111012.py
@@ -8,6 +8,9 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets[ 'gitHEAD' ] = "git://github.com/TheOneRing/liblastfm.git"
         self.targetSrcSuffix['gitHEAD'] = "theo"
+        self.targets['1.0.0'] = 'http://cdn.last.fm/client/liblastfm-1.0.0.tar.gz'
+        self.targetDigests['1.0.0'] = '1947b1a6397ea188151572da33edc7699bf10164'
+        self.targetInstSrc['1.0.0'] = 'liblastfm-1.0.0'
         self.shortDescription = "a C++/Qt4 library provided by Last.fm for use with their web services"
         self.defaultTarget = 'gitHEAD'
 
@@ -22,6 +25,7 @@ class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DBUILD_FINGERPRINT=ON"
 
 
 if __name__ == '__main__':
