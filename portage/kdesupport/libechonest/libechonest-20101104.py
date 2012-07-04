cb17ed2b404407421ed03d8d5cab22c344581eb4
Patrick von Reth <vonreth@kde.org>
2012-07-04 12:01:57 +0300

    build 1.2.1 by default (cherry picked from commit b13376a3ce82a7ed74119ab711c0d09abe71b4da)

--- portage/kdesupport/libechonest/libechonest-20101104.py
+++ portage/kdesupport/libechonest/libechonest-20101104.py
@@ -11,7 +11,10 @@ class subinfo( info.infoclass ):
 
     def setTargets( self ):
       self.svnTargets[ 'gitHEAD' ] = '[git]kde:libechonest'
-      self.defaultTarget = 'gitHEAD'
+      self.targets['1.2.1'] = 'http://pwsp.cleinias.com/libechonest-1.2.1.tar.bz2'
+      self.targetInstSrc['1.2.1'] = 'libechonest-1.2.1'
+      self.targetDigests['1.2.1'] = '5ad5897c91c365b32840e75e806c9725c89b4522'
+      self.defaultTarget = '1.2.1'
 
 
 class Package( CMakePackageBase ):
