9ed9c0009f2b5bd0d6daf3cb7b6e80ad9c18f793
Patrick von Reth <vonreth@kde.org>
2012-07-25 11:38:35 +0200

    added a updated 4.7.1 crt2.0.4 version

--- portage/dev-util/mingw-w64/mingw-w64-20111031.py
+++ portage/dev-util/mingw-w64/mingw-w64-20111031.py
@@ -7,10 +7,8 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         for ver in [ "20111031", "20111101" ]:
             self.targets[ver] = "http://downloads.sourceforge.net/sourceforge/mingw-w64/mingw-w64-bin_x86_64-mingw_"+ver+"_sezero.zip"
-        for ver in [ "4.6.4" ]:
-            self.targets["4.6.4"] = "http://downloads.sourceforge.net/sourceforge/mingw-w64/x86_64-w64-mingw32-gcc-4.6.4_rubenvb.7z"
+        self.targets["4.7.1"] = "http://winkde.org/~pvonreth/downloads/%20x86_64-w64-mingw32-gcc-4.7.1-release-win64_rubenvb-crt-2.0.4.7z"
 
-        self.targetDigests['4.6.4'] = '3a811d891a9b376b05ae5ee372c4211100aeaaa6'
         self.defaultTarget = "20111031"
 
     def setDependencies( self ):
