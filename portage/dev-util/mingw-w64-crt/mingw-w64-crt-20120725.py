04b5f66ffbfda9de412c8d9019cef48051021525
Patrick von Reth <vonreth@kde.org>
2012-07-25 19:56:16 +0200

    added crt and header image to be able to use trunk versions of mingw-w64

---
+++ portage/dev-util/mingw-w64-crt/mingw-w64-crt-20120725.py
@@ -0,0 +1,33 @@
+import info
+from shells import *
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets['svnHEAD'] = 'https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64/trunk/mingw-w64-crt'
+  
+        self.defaultTarget = 'svnHEAD'
+
+    def setDependencies( self ):
+        self.buildDependencies['dev-util/msys'] = 'default'
+        self.dependencies['dev-util/mingw-w64-headers'] = 'default'
+
+
+from Package.AutoToolsPackageBase import *
+
+class Package( AutoToolsPackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        AutoToolsPackageBase.__init__( self )
+        if os.getenv("EMERGE_ARCHITECTURE") == "x64":
+            target = "x86_64-w64-mingw32"
+            disable = "--disable-lib32"
+            self.subinfo.options.merge.destinationPath = 'mingw64/x86_64-w64-mingw32'
+        else:
+            target = "i686-w64-mingw32"
+            disable = "--disable-lib64"
+            self.subinfo.options.merge.destinationPath = 'mingw/i686-w64-mingw32'
+        self.subinfo.options.configure.defines = " --with-sysroot=%s  %s  --target=%s --host=%s" % (MSysShell().toNativePath(self.mergeDestinationDir()),disable, target,target)
+
+
+if __name__ == '__main__':
+     Package().execute()
