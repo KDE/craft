d8087b9baed1de0614654c8986ecc8ae5f15d71b
Patrick von Reth <vonreth@kde.org>
2012-07-28 17:28:31 +0200

    updated l10n

--- portage/kde/kde4-l10n/kde4-l10n-wa/kde4-l10n-wa-4.9.0.py
+++ portage/kde/kde4-l10n/kde4-l10n-wa/kde4-l10n-wa-4.9.0.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in ['4.9.1', '4.9.2', '4.9.3', '4.9.4']:
+        for ver in ['4.9.0', '4.9.1', '4.9.2', '4.9.3', '4.9.4']:
           self.targets[ ver] = 'ftp://ftp.kde.org/pub/kde/stable/%s/src/kde-l10n/kde-l10n-wa-%s.tar.xz' % (ver , ver )
           self.targetInstSrc[ ver] = 'kde-l10n-wa-%s'  % ver
 
