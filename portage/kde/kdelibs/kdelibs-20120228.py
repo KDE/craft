680c6183f165f06a960b31e6a12452e7c1d7e4e0
Patrick von Reth <vonreth@kde.org>
2012-08-03 15:51:34 +0200

    new gcc patch

--- portage/kde/kdelibs/kdelibs-20120228.py
+++ portage/kde/kdelibs/kdelibs-20120228.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4']:
             self.targets['4.9.' + ver] = "ftp://ftp.kde.org/pub/kde/stable/4.9." + ver + "/src/kdelibs-4.9." + ver + ".tar.xz"
             self.targetInstSrc['4.9.' + ver] = 'kdelibs-4.9.' + ver
+            self.patchToApply['4.9.' + ver] = [("kdelibs-4.9.0-20120803.diff", 1)]
         self.shortDescription = "The KDE Library"
         self.defaultTarget = 'gitHEAD'
 
