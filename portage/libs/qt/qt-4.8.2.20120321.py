0a52b578ebaeeec013160d82bfc981a4214fb554
Andre Heinecke <aheinecke@intevation.de>
2012-08-04 13:10:08 +0000

    Add workaround for qt bug 22829
        This is about a moc error together with boost that first
        occured in kdepim/kleopatra/models/keylistmodel

--- portage/libs/qt/qt-4.8.2.20120321.py
+++ portage/libs/qt/qt-4.8.2.20120321.py
@@ -81,7 +81,8 @@ class subinfo(info.infoclass):
             ('patches/4.8/add-pdbs-on-msvc.diff', 1),
             ('patches/4.8/detect-windows-8-as-windows-7.patch', 1),
             ('patches/4.8/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
-            ('patches/4.8.1/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1)
+            ('patches/4.8.1/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1),
+            ('patches/4.8/moc-boost-fix-bug-22829.diff', 1)
         ]
 
         self.shortDescription = "a cross-platform application framework"
