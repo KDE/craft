5d5f67f3a0fee991a2391d5d4438dbb6795a50cd
Andre Heinecke <aheinecke@intevation.de>
2012-08-04 13:24:32 +0000

    KLEO_SYNCHRONOUS_API_HOTFIX define no longer exists

--- portage/kde/kdepim-runtime/kdepim-runtime-20101222.py
+++ portage/kde/kdepim-runtime/kdepim-runtime-20101222.py
@@ -18,7 +18,6 @@ class Package( CMakePackageBase ):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        self.subinfo.options.configure.defines = "-DKLEO_SYNCHRONOUS_API_HOTFIX=ON "
 
     def install( self ):
         if not CMakePackageBase.install( self ):
