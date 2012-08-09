ad0802226032f20c910c4c188dc34086e2aebf22
Boudewijn Rempt <boud@valdyas.org>
2012-08-09 10:19:18 +0200

    set calligra by default to the stable 2.5

--- portage/kdeapps/calligra/calligra-20120805.py
+++ portage/kdeapps/calligra/calligra-20120805.py
@@ -3,7 +3,8 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = "[git]kde:calligra"
-        self.defaultTarget = 'gitHEAD'
+        self.svnTargets['2.5'] = "[git]kde:calligra||calligra/2.5"
+        self.defaultTarget = '2.5'
         self.shortDescription = "The Calligra Suite of Applications"
 
     def setDependencies( self ):
@@ -27,10 +28,8 @@ class Package( CMakePackageBase ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
         defines = ""
-        defines += "-DBUILD_kexi=OFF "
-        defines += "-DBUILD_plan=OFF "
-        defines += "-DBUILD_doc=OFF "
-        defines += "-DMEMORY_LEAK_TRACKER=OFF"
+        defines += "-DCREATIVEONLY=ON "
+        defines += "-DMEMORY_LEAK_TRACKER=OFF "
 
         self.subinfo.options.configure.defines = defines
 
