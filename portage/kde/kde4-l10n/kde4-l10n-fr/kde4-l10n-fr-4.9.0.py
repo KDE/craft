885d9f8b0765064794f5758d10b40ad6cec9c413
Patrick von Reth <vonreth@kde.org>
2012-08-13 20:28:54 +0200

    updated epc generated l10n packages

---
+++ portage/kde/kde4-l10n/kde4-l10n-fr/kde4-l10n-fr-4.9.0.py
@@ -0,0 +1,28 @@
+import os
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        for ver in ['4.9.0', '4.9.1', '4.9.2', '4.9.3', '4.9.4']:
+          self.targets[ ver] = 'ftp://ftp.kde.org/pub/kde/stable/%s/src/kde-l10n/kde-l10n-fr-%s.tar.xz' % (ver , ver )
+          self.targetInstSrc[ ver] = 'kde-l10n-fr-%s'  % ver
+
+        self.defaultTarget = '4.9.0'
+
+
+    def setDependencies( self ):
+        self.buildDependencies['dev-util/cmake'] = 'default'
+        self.buildDependencies['kde/kdelibs'] = 'default'
+        self.buildDependencies['dev-util/gettext-tools'] = 'default'
+
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+
+if __name__ == '__main__':
+    Package().execute()
