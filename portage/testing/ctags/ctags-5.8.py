128a689b409920bc92847927056740a0f3f829ef
Patrick Spendrin <ps_ml@gmx.de>
2012-08-28 23:20:08 +0200

    add a tool for indexing source files (cherry picked from commit 07da655e5f697467d779f58697e19fb937c7ed10)

---
+++ portage/testing/ctags/ctags-5.8.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import utils
+import info
+
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['5.8'] = 'http://downloads.sourceforge.net/ctags/ctags-5.8.tar.gz'
+        self.patchToApply['5.8'] = [('ctags-5.8-20120828.diff', 1), ('ctags-cmake.diff', 1)]
+        self.targetDigests['5.8'] = '482da1ecd182ab39bbdc09f2f02c9fba8cd20030'
+        self.targetInstSrc['5.8'] = 'ctags-5.8'
+        self.defaultTarget = '5.8'
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        # we use subinfo for now too
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
+
