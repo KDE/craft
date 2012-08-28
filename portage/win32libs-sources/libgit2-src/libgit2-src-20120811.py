c1d754f5861dfad4f1f74fd6941f986cf251d75c
Patrick Spendrin <ps_ml@gmx.de>
2012-08-28 15:24:47 +0200

    add a library for manipulating git repositories

---
+++ portage/win32libs-sources/libgit2-src/libgit2-src-20120811.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'https://github.com/libgit2/libgit2.git'
+
+
+        self.shortDescription = "a portable C library for accessing git repositories"
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+#        self.subinfo.options.configure.defines = "-DDBUS_REPLACE_LOCAL_DIR=ON "
+
+if __name__ == '__main__':
+    Package().execute()
