8b813ec6f486bd21ebe7c7027e5ccf9761a61dcb
Andre Heinecke <aheinecke@intevation.de>
2012-09-03 18:31:56 +0200

    Add new extragear libkgapi
    Necessary for the akonadi google ressource

---
+++ portage/extragear/libkgapi/libkgapi-20120903.py
@@ -0,0 +1,19 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = '[git]kde:libkgapi'
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.dependencies['kde/kdepimlibs'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
