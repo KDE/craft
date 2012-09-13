8fd2af7e92b1f22009ded510959b5c934048304c
Patrick von Reth <vonreth@kde.org>
2012-09-13 12:44:00 +0200

    updated phonon-vlc target (cherry picked from commit 009136f74d8001ab355b8767c658ad6d6af558ee)

--- portage/kdesupport/phonon-vlc/phonon-vlc-0.5.0-1.py
+++ portage/kdesupport/phonon-vlc/phonon-vlc-0.5.0-1.py
@@ -24,17 +24,20 @@ class subinfo(info.infoclass):
       self.targets['0.5.0'] = "http://download.kde.org/download.php?url=stable/phonon/phonon-backend-vlc/0.5.0/src/phonon-backend-vlc-0.5.0.tar.xz"
       self.targetInstSrc['0.5.0'] = "phonon-backend-vlc-0.5.0"
       self.patchToApply['0.5.0'] = [("0001-Revert-stop-leaking-video-audio-abstraction.patch",1)]
+      self.targets['0.6.0'] = "http://download.kde.org/download.php?url=stable/phonon/phonon-backend-vlc/0.6.0/src/phonon-backend-vlc-0.6.0.tar.xz"
+      self.targetDigests['0.6.0'] = 'f66a70cd27ad49dc98eb6526d0566cfe0802774b'
+      self.targetInstSrc['0.6.0'] = "phonon-backend-vlc-0.6.0"
       
       self.svnTargets['gitHEAD'] = '[git]kde:phonon-vlc'
       self.shortDescription = "the vlc based phonon multimedia backend"
-      self.defaultTarget = '0.5.0'
+      self.defaultTarget = '0.6.0'
 
 
 class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.defines = '-DPHONON_BUILDSYSTEM_DIR=\"%s;%s\" ' % (os.path.join(os.getenv('KDEROOT'),'share','phonon','buildsystem').replace('\\','/'),os.path.join(os.getenv('KDEROOT'),'share','phonon-buildsystem').replace('\\','/'))
+        self.subinfo.options.configure.defines = '-DCMAKE_CXX_FLAGS=-DWIN32 -DPHONON_BUILDSYSTEM_DIR=\"%s;%s\" ' % (os.path.join(os.getenv('KDEROOT'),'share','phonon','buildsystem').replace('\\','/'),os.path.join(os.getenv('KDEROOT'),'share','phonon-buildsystem').replace('\\','/'))
 
 if __name__ == '__main__':
     Package().execute()
