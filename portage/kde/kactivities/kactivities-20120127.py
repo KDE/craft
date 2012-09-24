36e673ca24fa4a0683993cce6f6e7198d9b4c472
Patrick Spendrin <ps_ml@gmx.de>
2012-09-25 00:00:53 +0200

    kactivities also has a 0 release

--- portage/kde/kactivities/kactivities-20120127.py
+++ portage/kde/kactivities/kactivities-20120127.py
@@ -3,7 +3,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:kactivities|KDE/4.9|'
-        for ver in ['1', '2', '3', '4']:
+        for ver in ['0', '1', '2', '3', '4']:
             self.targets['4.9.' + ver] = 'ftp://ftp.kde.org/pub/kde/stable/4.9.' + ver + '/src/kactivities-4.9.' + ver + '.tar.xz'
             self.targetInstSrc['4.9.' + ver] = 'kactivities-4.9.' + ver
         self.defaultTarget = 'gitHEAD'
