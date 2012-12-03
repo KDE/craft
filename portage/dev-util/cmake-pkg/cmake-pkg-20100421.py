ab3a4a0a7e3b36bb99cf38f9bebf12b903a4b0b4
Patrick von Reth <vonreth@kde.org>
2012-12-03 14:01:39 +0100

    cmake 2.8.10 added, 2.8 got a strange bug with finding boost

--- portage/dev-util/cmake-pkg/cmake-pkg-20100421.py
+++ portage/dev-util/cmake-pkg/cmake-pkg-20100421.py
@@ -6,13 +6,15 @@ class subinfo(info.infoclass):
         self.svnTargets['gitHEAD'] = "git://cmake.org/cmake.git"
         self.svnTargets['release'] = "git://cmake.org/cmake.git|release|"
         self.svnTargets['v2.8.2'] = "git://cmake.org/cmake.git|release|v2.8.2"
-        for ver in ['2.8.0', '2.8.1', '2.8.2', '2.8.3', '2.8.8']:
+        for ver in ['2.8.0', '2.8.1', '2.8.2', '2.8.3', '2.8.8','2.8.10']:
             self.targets[ ver ] = "http://www.cmake.org/files/v2.8/cmake-" + ver + ".tar.gz"
             self.targetInstSrc[ ver ] = "cmake-" + ver
         self.patchToApply['2.8.0'] = ("cmake-2.8.0-20100512.diff", 1)
         self.patchToApply['2.8.1'] = ("cmake-2.8.1-20100511.diff", 1)
         self.patchToApply['2.8.3'] = ("cmake-2.8.3-20101222.diff", 1)
         self.patchToApply['2.8.8'] = ("cmake-2.8.3-20101222.diff", 1)
+        self.patchToApply['2.8.10'] = ("cmake-2.8.10.diff", 1)
+        self.targetDigests['2.8.10'] = '6384f2295d01f04d017e2701447378598b4c7223'
         self.defaultTarget = 'release'
 
     def setDependencies( self ):
