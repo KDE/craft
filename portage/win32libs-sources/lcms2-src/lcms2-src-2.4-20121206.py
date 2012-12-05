0434636ee282ba4c4997ef1e81d3c74ea28a3917
Boudewijn Rempt <boud@valdyas.org>
2012-12-05 12:46:27 +0000

    Use LCMS 2.4 instread of 2.3

--- portage/win32libs-sources/lcms2-src/lcms2-src-2.4-20121206.py
+++ portage/win32libs-sources/lcms2-src/lcms2-src-2.4-20121206.py
@@ -5,12 +5,12 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['2.3'] = "http://download.sourceforge.net/lcms/lcms2-2.3.tar.gz"
-        self.targetInstSrc['2.3'] = "lcms2-2.3"
-        self.patchToApply['2.3'] = ( 'lcms2-2.3-20120302.diff', 1 )
-        self.targetDigests['2.3'] = '67d5fabda2f5777ca8387766539b9c871d993133'
+        self.targets['2.4'] = "http://download.sourceforge.net/lcms/lcms2-2.4.tar.gz"
+        self.targetInstSrc['2.4'] = "lcms2-2.4"
+        self.patchToApply['2.4'] = ( 'lcms2-2.4-20121205.diff', 1 )
+        self.targetDigests['2.4'] = '9944902864283af49e4e21a1ca456db4e04ea7c2'
         self.shortDescription = "A small-footprint, speed optimized color management engine"
-        self.defaultTarget = '2.3'
+        self.defaultTarget = '2.4'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
