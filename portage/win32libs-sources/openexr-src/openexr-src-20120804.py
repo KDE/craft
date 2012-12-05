e31f1e5adc6eb755050c44937bfa12f941672441
Boudewijn Rempt <boud@valdyas.org>
2012-12-05 12:47:12 +0000

    fix number of diff file

--- portage/win32libs-sources/openexr-src/openexr-src-20120804.py
+++ portage/win32libs-sources/openexr-src/openexr-src-20120804.py
@@ -6,7 +6,7 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets[ '1.7.0' ] = 'http://download.savannah.nongnu.org/releases/openexr/openexr-1.7.0.tar.gz'
         self.targetInstSrc[ '1.7.0' ] = 'openexr-1.7.0'
-        self.patchToApply[ '1.0.24' ] = [( 'openexr-1.7.0-20120804.diff', 1 )]
+        self.patchToApply[ '1.7.0' ] = [( 'openexr-1.7.0-20120804.diff', 1 )]
         self.targetDigests['1.7.0'] = '91d0d4e69f06de956ec7e0710fc58ec0d4c4dc2b'
         self.shortDescription = "a file format library for HDR images"
         self.defaultTarget = '1.7.0'
