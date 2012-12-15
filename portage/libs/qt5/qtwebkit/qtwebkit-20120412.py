1826b6c8f9f4dbd823b7a4bc97b3b6844fc6400d
Patrick von Reth <vonreth@kde.org>
2012-12-15 12:40:53 +0100

    changed qt5 branch

--- portage/libs/qt5/qtwebkit/qtwebkit-20120412.py
+++ portage/libs/qt5/qtwebkit/qtwebkit-20120412.py
@@ -4,7 +4,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):       
-        self.svnTargets['gitHEAD'] = "[git]git://gitorious.org/qtwebkit/qt5-module.git" 
+        self.svnTargets['gitHEAD'] = "[git]git://gitorious.org/qtwebkit/qt5-module.git|stable" 
 
         self.defaultTarget = "gitHEAD"
 
