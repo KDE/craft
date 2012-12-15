d6b94719fadb46a40a2d23acea8d57e638bd69fb
Patrick von Reth <vonreth@kde.org>
2012-12-15 14:20:41 +0100

    added some more modules to qt5, doc and qtactiveqt wont build

---
+++ portage/libs/qt5/qtgraphicaleffects/qtgraphicaleffects-20120412.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):       
+        self.svnTargets['gitHEAD'] = "[git]git://gitorious.org/qt/qtgraphicaleffects.git|stable" 
+
+        self.defaultTarget = "gitHEAD"
+
+    def setDependencies( self ):
+        self.dependencies['libs/qtbase'] = 'default'
+
+
+from Package.Qt5CorePackageBase import *
+
+class Package( Qt5CorePackageBase ):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        Qt5CorePackageBase.__init__( self )        
+       
+
+        
+if __name__ == '__main__':
+    Package().execute()
