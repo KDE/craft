841ddbfbe3ebd843d07bc1cf74f3a41e035624ad
Patrick von Reth <vonreth@kde.org>
2013-01-07 22:10:43 +0100

    fixed build with gcc 4.7

--- portage/win32libs-sources/cryptopp-src/cryptopp-src-5.6.1-1.py
+++ portage/win32libs-sources/cryptopp-src/cryptopp-src-5.6.1-1.py
@@ -9,7 +9,7 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets[ '561' ] = 'http://www.cryptopp.com/cryptopp561.zip'  
         self.patchToApply[ '561' ] = [('cmake.diff', 1),
-                                      ('workaround_ice_31690.diff', 1)]
+                                      ('workaround_ice_31690.diff', 1),("cryptopp-5.6.1-gcc-4.7.0.patch",1)]
         self.targetDigests['561'] = '31dbb456c21f50865218c57b7eaf4c955a222ba1'       
         self.shortDescription = "Crypto++ Library is a free C++ class library of cryptographic schemes"
         self.defaultTarget = '561'
