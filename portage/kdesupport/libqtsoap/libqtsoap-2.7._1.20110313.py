d38483a7e1b18a1d86f21cb7bf2ea0e5aa03fa01
Patrick von Reth <vonreth@kde.org>
2013-01-09 14:44:42 +0100

    ported libqtsoap for testing to qt5

--- portage/kdesupport/libqtsoap/libqtsoap-2.7._1.20110313.py
+++ portage/kdesupport/libqtsoap/libqtsoap-2.7._1.20110313.py
@@ -6,12 +6,12 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setDependencies( self ):
-        self.dependencies['libs/qt'] = 'default'
+        self.dependencies['libs/qtbase'] = 'default'
 
 
     def setTargets( self ):
         self.targets['2.7_1'] = 'http://get.qt.nokia.com/qt/solutions/lgpl/qtsoap-2.7_1-opensource.zip'
-        self.patchToApply['2.7_1'] = ('qtsoap-2.7_1-opensource-20110308.diff',1)
+        self.patchToApply['2.7_1'] = [('qtsoap-2.7_1-opensource-20110308.diff',1)]
         self.targetDigests['2.7_1'] = '933ffd4215052af7eed48fc9492a1cd6996c7641'
         self.targetInstSrc['2.7_1'] = 'qtsoap-2.7_1-opensource'
         self.defaultTarget = '2.7_1'
