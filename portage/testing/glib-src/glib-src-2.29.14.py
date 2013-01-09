e91c456a17f9d835550cd3cd15ff54b4f9de602c
Ananta Palani <anantapalani@gmail.com>
2013-01-09 11:13:42 +0000

    Support release/debug and win32/x64 builds. Fix build and install problems (cherry picked from commit 9ea5685e5c7d826525aae596176f76069680687e)

--- portage/testing/glib-src/glib-src-2.29.14.py
+++ portage/testing/glib-src/glib-src-2.29.14.py
@@ -5,7 +5,7 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets["2.29.14"] = "http://ftp.acc.umu.se/pub/GNOME/sources/glib/2.29/glib-2.29.14.tar.bz2"
         self.targetInstSrc["2.29.14"] = "glib-2.29.14"
-        self.patchToApply["2.29.14"] = ('glib-2.29.14-20110723.diff', '1')
+        self.patchToApply["2.29.14"] = ('glib-2.29.14-20121010.diff', '1')
         self.targetDigests['2.29.14'] = 'bd993994e9d6262c19d241f6a6f781f11b840831'
         self.shortDescription = "The Glib libraries: glib, gio, gthread, gmodule, gobject"
         self.defaultTarget = "2.29.14"
@@ -29,14 +29,26 @@ class Package(CMakePackageBase):
 
     def compile( self ):
         self.enterSourceDir()
-        cmd = "msbuild build\\win32\\vs10\\glib.sln"
+        os.putenv( "INSTALLDIR", self.imageDir() )
+        configuration = "Release"
+        if self.buildType() == "Debug":
+            configuration = "Debug"
+        platform = "Win32"
+        if self.buildArchitecture() == "x64":
+            platform = "x64"
+        cmd = "msbuild /p:configuration=\""+configuration+"\" /p:platform=\""+platform+"\" build\\win32\\vs10\\glib.sln"
         return self.system( cmd )
 
     def install( self ):
         self.enterSourceDir()
-        os.putenv("INSTALLDIR", self.imageDir() )
-        print(self.imageDir())
-        cmd = "msbuild /target:install build\\win32\\vs10\\glib.sln"
+        os.putenv( "INSTALLDIR", self.imageDir() )
+        configuration = "Release"
+        if self.buildType() == "Debug":
+            configuration = "Debug"
+        platform = "Win32"
+        if self.buildArchitecture() == "x64":
+            platform = "x64"
+        cmd = "msbuild /p:configuration=\""+configuration+"\" /p:platform=\""+platform+"\" /t:install build\\win32\\vs10\\glib.sln"
         return self.system( cmd )
 
 if __name__ == '__main__':
