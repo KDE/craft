9be9c7be1c7df18fcf71b7797f5d1c06e3eb37af
Ananta Palani <anantapalani@gmail.com>
2013-01-14 00:20:17 +0000

    Add LensFun library

---
+++ portage/win32libs-sources/lensfun-src/lensfun-src-0.2.6.py
@@ -0,0 +1,40 @@
+import os
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        for ver in ['0.2.6']:
+            self.targets[ver] = "http://sourceforge.net/projects/lensfun.berlios/files/lensfun-%s.tar.bz2/download/lensfun-%s.tar.bz2" % (ver, ver)
+            self.targetInstSrc[ver] = "lensfun-%s" % ver
+        
+        self.targetDigests['0.2.6'] = '0d7ffbae5c54159308114f69a9e2bc5f4d24d836'
+        
+        self.patchToApply['0.2.6'] = ( 'lensfun-0.2.6.diff', 1 )
+        
+        self.options.configure.defines = "-DBUILD_STATIC=OFF"
+        self.options.configure.defines += " -DBUILD_TESTS=OFF"
+        self.options.configure.defines += " -DBUILD_AUXFUN=OFF"
+        self.options.configure.defines += " -DBUILD_FOR_SSE=ON"
+        self.options.configure.defines += " -DBUILD_FOR_SSE2=ON"
+        self.options.configure.defines += " -DBUILD_DOC=OFF"
+        
+        self.shortDescription = "a library to perform image correction based on lens profiles"
+        self.defaultTarget = '0.2.6'
+    
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        # self.buildDependencies['dev-util/doxygen'] = 'default' # only needed if building docs
+        self.dependencies['testing/glib'] = 'default'
+        # self.dependencies['win32libs-bin/zlib']  = 'default' # only needed if building auxfun and tests
+        # self.dependencies['win32libs-bin/libpng'] = 'default' # only needed if building auxfun and tests
+        
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
