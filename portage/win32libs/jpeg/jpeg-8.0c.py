3c594c93d3f2ab95031e1495592775ca76baa060
Patrick Spendrin <ps_ml@gmx.de>
2013-01-23 16:29:35 +0100

    update jpeg to version 9.0

--- portage/win32libs/jpeg/jpeg-8.0c.py
+++ portage/win32libs/jpeg/jpeg-8.0c.py
@@ -6,19 +6,22 @@ class subinfo(info.infoclass):
         self.targetInstSrc['7.0'] = "jpeg-7"
         self.targets['8.0'] = "http://www.ijg.org/files/jpegsrc.v8.tar.gz"
         self.targets['8.0c'] = "http://www.ijg.org/files/jpegsrc.v8c.tar.gz"
+        self.targets['9.0'] = "http://www.ijg.org/files/jpegsrc.v9.tar.gz"
         self.targetDigests['8.0c'] = 'f0a3b88ac4db19667798bee971537eeed552bce9'
         
         self.targetInstSrc['8.0'] = "jpeg-8"
         self.targetInstSrc['8.0c'] = "jpeg-8c"
+        self.targetInstSrc['9.0'] = "jpeg-9"
         
         self.patchToApply['7.0'] = ( 'jpeg7.diff', 1 )
         self.patchToApply['8.0'] = ( 'jpeg8.diff', 1 )
         self.patchToApply['8.0c'] = ( 'jpeg8.diff', 1 )
+        self.patchToApply['9.0'] = [( 'jpeg9.diff', 1 )]
 
         self.shortDescription = 'A library for manipulating JPEG image format files'
         self.categoryName = 'win32libs'
 
-        self.defaultTarget = '8.0c'
+        self.defaultTarget = '9.0'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
