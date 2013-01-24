d5fa5f9ca2b1c765ef6963bc2d38a1e7b2c20ed8
Patrick Spendrin <ps_ml@gmx.de>
2013-01-24 23:26:46 +0100

    update pcre to version 8.32

--- portage/win32libs/pcre/pcre-8.21.py
+++ portage/win32libs/pcre/pcre-8.21.py
@@ -3,20 +3,22 @@ from Package.CMakePackageBase import *
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in [ '7.9', '8.00', '8.02', '8.10' ,'8.12', '8.21' ]:
+        for ver in [ '7.9', '8.00', '8.02', '8.10' ,'8.12', '8.21', '8.32' ]:
             self.targets[ ver ] = 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-' + ver + '.tar.bz2'
             self.targetInstSrc[ ver ] = 'pcre-' + ver
         self.patchToApply[ '8.10' ] = [ ( "pcre-8.10-20101125.diff", 1 ) ]
         self.patchToApply[ '8.12' ] = [ ( "pcre-8.10-20101125.diff", 1 ) ]
         self.patchToApply[ '8.21' ] = [ ( "pcre-8.10-20101125.diff", 1 ) ]
+        self.patchToApply[ '8.32' ] = [ ( "pcre-8.10-20101125.diff", 1 ) ]
         if emergePlatform.isCrossCompilingEnabled():
             self.patchToApply[ '8.10' ].append( ( "pcre-8.02-20100518.diff", 1 ) )
 
         self.targetDigests['8.10'] = '8b345da0f835b2caabff071b0b5bab40564652be'
         self.targetDigests['8.12'] = '2219b372bff53ee29a7e44ecf5977ad15df01cea'
         self.targetDigests['8.21'] = '52abf655d94f5208377258ffff27c7b35c53af39'
+        self.targetDigests['8.32'] = 'dbd44267cf4d7c6464391003908d5a4342726700'
         self.shortDescription = "Perl-Compatible Regular Expressions"
-        self.defaultTarget = '8.21'
+        self.defaultTarget = '8.32'
 
     def setDependencies( self ):
         self.buildDependencies[ 'virtual/base' ] = 'default'
