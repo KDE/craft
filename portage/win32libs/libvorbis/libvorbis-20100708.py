e7b3c63b01b7f452ddaa6d5225f0293d346af46c
Patrick Spendrin <ps_ml@gmx.de>
2013-01-24 12:02:58 +0100

    update libvorbis to 1.3.3

--- portage/win32libs/libvorbis/libvorbis-20100708.py
+++ portage/win32libs/libvorbis/libvorbis-20100708.py
@@ -4,10 +4,13 @@ from Package.CMakePackageBase import *
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets['1.3.1'] = 'http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.gz'
-        self.targetInstSrc['1.3.1'] = 'libvorbis-1.3.1'
-        self.patchToApply['1.3.1'] = ( 'libvorbis-1.3.1-20100708.diff', 1 )
+        for ver in ['1.3.1', '1.3.3']:
+            self.targets[ ver ] = 'http://downloads.xiph.org/releases/vorbis/libvorbis-' + ver + '.tar.gz'
+            self.targetInstSrc[ ver ] = 'libvorbis-' + ver
+        self.patchToApply['1.3.1'] = [( 'libvorbis-1.3.1-20100708.diff', 1 )]
+        self.patchToApply['1.3.3'] = [( 'libvorbis-1.3.1-20100708.diff', 1 )]
         self.targetDigests['1.3.1'] = '0874dd08699240b868b22979da4c95ae6325006b'
+        self.targetDigests['1.3.3'] = '8dae60349292ed76db0e490dc5ee51088a84518b'
         self.shortDescription = "reference implementation for the vorbis video file format"
         self.defaultTarget = '1.3.1'
 
