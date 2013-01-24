dc9efc5dae1d28fd5cdbcef8167c791d6662ba64
Patrick Spendrin <ps_ml@gmx.de>
2013-01-24 13:47:38 +0100

    update cfitsio to 3.31

--- portage/win32libs/cfitsio/cfitsio-3.20.py
+++ portage/win32libs/cfitsio/cfitsio-3.20.py
@@ -2,12 +2,13 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in [ '3.08', '3.10', '3.14', '3.20' ]:
+        for ver in [ '3.08', '3.10', '3.14', '3.20', '3.31' ]:
             self.targets[ ver ] = 'ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio' + ver.replace(".", "") + '0.tar.gz'
             self.targetInstSrc[ ver ] = "cfitsio"
         self.targetDigests['3.20'] = 'f200fe0acba210e88e230add6a4e68d80ad3d4f2'
-        self.patchToApply['3.20'] = ("cfitsio-20101130.diff", 1)
-        self.shortDescription = "library for the FITS (Flexible Image Transport System) file format"
+        self.targetDigests['3.31'] = '35360dccc69dc5f12efb6fc9096ad951b59244d5'
+        self.patchToApply['3.20'] = [("cfitsio-20101130.diff", 1)]
+        self.patchToApply['3.31'] = [("cfitsio-20130124.diff", 1)]
         self.shortDescription = "library for the FITS (Flexible Image Transport System) file format"
         self.defaultTarget = '3.20'
 
