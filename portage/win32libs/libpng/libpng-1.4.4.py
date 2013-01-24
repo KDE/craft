d5f511e81ad62bd98065e7b137d326dfc889c3ed
Patrick Spendrin <ps_ml@gmx.de>
2013-01-24 15:03:04 +0100

    update libpng to version 1.5.14

--- portage/win32libs/libpng/libpng-1.4.4.py
+++ portage/win32libs/libpng/libpng-1.4.4.py
@@ -4,9 +4,10 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['1.4.4','1.2.43']:
+        for ver in ['1.4.4','1.2.43', '1.5.14']:
             self.targets[ver] = 'http://downloads.sourceforge.net/libpng/libpng-' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'libpng-' + ver
+        # patch is only required for wince, update if necessary
         self.patchToApply['1.4.4'] = ("libpng-1.4.4-20100517.diff", 1)
         self.targetDigests['1.4.4'] = '245490b22086a6aff8964b7d32383a17814d8ebf'
 
