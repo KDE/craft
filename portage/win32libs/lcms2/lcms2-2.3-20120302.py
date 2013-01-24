27b08cf38c44918397a256ffaaf1cd47ece8308b
Patrick Spendrin <ps_ml@gmx.de>
2013-01-24 11:44:46 +0100

    add 2.4 release for lcms2

--- portage/win32libs/lcms2/lcms2-2.3-20120302.py
+++ portage/win32libs/lcms2/lcms2-2.3-20120302.py
@@ -5,10 +5,13 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['2.3'] = "http://download.sourceforge.net/lcms/lcms2-2.3.tar.gz"
-        self.targetInstSrc['2.3'] = "lcms2-2.3"
-        self.patchToApply['2.3'] = ( 'lcms2-2.3-20120302.diff', 1 )
+        for ver in ['2.3', '2.4']:
+            self.targets[ ver ] = "http://download.sourceforge.net/lcms/lcms2-" + ver + ".tar.gz"
+            self.targetInstSrc[ ver ] = "lcms2-" + ver
+        self.patchToApply['2.3'] = [( 'lcms2-2.3-20120302.diff', 1 )]
+        self.patchToApply['2.4'] = [( 'lcms2-2.4-20130124.diff', 1 )]
         self.targetDigests['2.3'] = '67d5fabda2f5777ca8387766539b9c871d993133'
+        self.targetDigests['2.4'] = '9944902864283af49e4e21a1ca456db4e04ea7c2'
         self.shortDescription = "A small-footprint, speed optimized color management engine"
         self.defaultTarget = '2.3'
 
