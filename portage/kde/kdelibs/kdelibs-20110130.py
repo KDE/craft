5136479e8468cb477eae51e827afcef9ba615119
Patrick von Reth <vonreth@kde.org>
2013-02-19 12:47:00 +0100

    apply the dammned patch

--- portage/kde/kdelibs/kdelibs-20110130.py
+++ portage/kde/kdelibs/kdelibs-20110130.py
@@ -8,7 +8,7 @@ class subinfo(info.infoclass):
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
 
-#        self.patchToApply["4.10.0"] = ("kdelibs-4.10.0-20130219.diff",1)
+        self.patchToApply["4.10.0"] = ("kdelibs-4.10.0-20130219.diff",1)
         self.shortDescription = "The KDE Library"
         self.defaultTarget = 'gitHEAD'
 
