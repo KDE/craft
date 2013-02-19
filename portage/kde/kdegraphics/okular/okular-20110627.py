c8f23aeecc15faeb93b6639fe7b2ea47b5e56d87
Patrick von Reth <vonreth@kde.org>
2013-02-19 12:25:20 +0100

    added kate as runtime dep

--- portage/kde/kdegraphics/okular/okular-20110627.py
+++ portage/kde/kdegraphics/okular/okular-20110627.py
@@ -24,6 +24,7 @@ class subinfo(info.infoclass):
         self.dependencies['win32libs/zlib'] = 'default'
         self.dependencies['win32libs/freetype'] = 'default'
         self.dependencies['win32libs/libspectre'] = 'default'
+        self.runtimeDependencies['extragear/kate'] = 'default'
         self.runtimeDependencies['kde/kde-runtime'] = 'default'
 
 from Package.CMakePackageBase import *
