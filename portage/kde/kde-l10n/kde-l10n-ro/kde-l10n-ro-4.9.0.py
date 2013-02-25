1f70678ff5100d7ca5d6fe595052f29963dc7ead
Patrick Spendrin <ps_ml@gmx.de>
2013-02-25 14:49:48 +0100

    fix urls and default targets

--- portage/kde/kde-l10n/kde-l10n-ro/kde-l10n-ro-4.9.0.py
+++ portage/kde/kde-l10n/kde-l10n-ro/kde-l10n-ro-4.9.0.py
@@ -4,10 +4,10 @@ import kdedefaults as kd
 class subinfo(info.infoclass):
     def setTargets( self ):
         for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/kde-l10n" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
+            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/kde-l10n/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
 
-        self.defaultTarget = kde.kdeversion + '0'
+        self.defaultTarget = kd.kdeversion + '0'
 
     def setDependencies( self ):
         self.buildDependencies['dev-util/cmake'] = 'default'
