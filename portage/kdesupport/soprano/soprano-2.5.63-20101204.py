197f3f597a9802868d6d0a05adba1052880d1875
Patrick Spendrin <ps_ml@gmx.de>
2013-02-27 15:16:21 +0100

    use stdcall callback on Windows

--- portage/kdesupport/soprano/soprano-2.5.63-20101204.py
+++ portage/kdesupport/soprano/soprano-2.5.63-20101204.py
@@ -19,6 +19,7 @@ class subinfo(info.infoclass):
         for ver in ['v2.8.0', 'v2.9.0']:
             self.targets[ ver ] = 'http://downloads.sourceforge.net/soprano/soprano-' + ver.replace('v', '') + '.tar.bz2'
             self.targetInstSrc[ ver ] = 'soprano-' + ver[1:]
+        self.patchToApply['v2.9.0'] = [("soprano-redland-callback.diff", 1)]
         self.shortDescription = "a RDF storage solutions library"
         self.defaultTarget = 'gitHEAD'
 
