23ad7d98bc0a6e6c10af1087dfd6e92c5156f7db
Patrick Spendrin <ps_ml@gmx.de>
2013-02-28 22:51:06 +0100

    move kate to kde category, it is a KDE project after all

--- portage/kde/kate/kate-20110130.py
+++ portage/kde/kate/kate-20110130.py
@@ -2,7 +2,11 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:kate'
+        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
+        for ver in ['0', '1', '2', '3', '4', '5']:
+            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
+            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
+
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
