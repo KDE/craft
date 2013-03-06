339fe4172472a7304305b56748fff6baeeb423e2
Patrick Spendrin <ps_ml@gmx.de>
2013-03-06 01:24:03 +0100

    fix kstars on mingw-w64

--- portage/kde/kdeedu/kstars/kstars-20110621.py
+++ portage/kde/kdeedu/kstars/kstars-20110621.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4', '5']:
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
+        self.patchToApply['4.10.1'] = [("kstars-4.10.1-20130306.diff", 1)]
 
         self.shortDescription = 'a desktop planetarium'
         self.defaultTarget = 'gitHEAD'
