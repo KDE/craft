459a7524bbce09696641d62d50b38525876d9754
Patrick Spendrin <ps_ml@gmx.de>
2013-03-06 23:21:13 +0100

    fix building libkgapi release 0.4.4

--- portage/extragear/libkgapi/libkgapi-20120903.py
+++ portage/extragear/libkgapi/libkgapi-20120903.py
@@ -7,7 +7,9 @@ class subinfo( info.infoclass ):
         for ver in ['0.4.4']:
             self.targets[ ver ] = "http://download.kde.org/stable/libkgapi/" + ver + "/src/libkgapi-" + ver + ".tar.bz2"
             self.targetInstSrc[ ver ] = "libkgapi-" + ver
-        self.defaultTarget = '0.4'
+        self.targetDigests['0.4.4'] = 'bccc767ec189d44912744e2b4c59c2d960c334c6'
+        self.patchToApply['0.4.4'] = [("libkgapi-0.4.4-20130306.diff", 1)]
+        self.defaultTarget = '0.4.4'
 
     def setDependencies( self ):
         self.dependencies['kde/kdepimlibs'] = 'default'
