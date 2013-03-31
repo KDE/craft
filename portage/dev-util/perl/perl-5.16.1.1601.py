b9d0994dafd2ef612f9b388835fdc64eda80f046
Andrius da Costa Ribas <andriusmao@gmail.com>
2013-03-31 11:34:14 -0300

    Update perl version CCBUG: 317622

--- portage/dev-util/perl/perl-5.16.1.1601.py
+++ portage/dev-util/perl/perl-5.16.1.1601.py
@@ -18,6 +18,9 @@ class subinfo(info.infoclass):
            self.targets['5.16.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.16.1.1601/ActivePerl-5.16.1.1601-MSWin32-x64-296175.zip"
            self.targetDigests['5.16.1'] = '2993227966574afb227640dcd139b088fee43879'
            self.targetMergeSourcePath['5.16.1'] = "ActivePerl-5.16.1.1601-MSWin32-x64-296175\\perl"
+           self.targets['5.16.3'] = "http://downloads.activestate.com/ActivePerl/releases/5.16.3.1603/ActivePerl-5.16.3.1603-MSWin32-x64-296746.zip"
+           self.targetDigests['5.16.3'] = '9cae0a0e24a86a26814206ff1bf02a7e7cfea4ba'
+           self.targetMergeSourcePath['5.16.3'] = "ActivePerl-5.16.3.1603-MSWin32-x64-296746\\perl"
         else:
            self.targets['5.10.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.10.1.1007/ActivePerl-5.10.1.1007-MSWin32-x86-291969.zip"
            self.targetDigests['5.10.1'] = '9122a828b32d8b8499c73b61972eaec303698961'
@@ -31,7 +34,10 @@ class subinfo(info.infoclass):
            self.targets['5.16.1'] = "http://downloads.activestate.com/ActivePerl/releases/5.16.1.1601/ActivePerl-5.16.1.1601-MSWin32-x86-296175.zip"
            self.targetDigests['5.16.1'] = 'e638f101c46ca8f0ecbfd7a07772d434d7c22c56'
            self.targetMergeSourcePath['5.16.1'] = "ActivePerl-5.16.1.1601-MSWin32-x86-296175\\perl"
-        self.defaultTarget = '5.16.1'
+           self.targets['5.16.3'] = "http://downloads.activestate.com/ActivePerl/releases/5.16.3.1603/ActivePerl-5.16.3.1603-MSWin32-x86-296746.zip"
+           self.targetDigests['5.16.3'] = 'c85063396993a6bf4989970585b0d3a21c7a2a8c'
+           self.targetMergeSourcePath['5.16.3'] = "ActivePerl-5.16.3.1603-MSWin32-x86-296746\\perl"
+        self.defaultTarget = '5.16.3'
     def setDependencies( self ):
         self.buildDependencies['virtual/bin-base'] = 'default'
 
