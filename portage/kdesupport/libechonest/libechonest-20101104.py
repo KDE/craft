43debd3914d5a02a11199bad28ab7ef2844c6233
Patrick von Reth <vonreth@kde.org>
2013-04-19 15:09:05 +0200

    disable -Werror on windows for the same reason as on arm

--- portage/kdesupport/libechonest/libechonest-20101104.py
+++ portage/kdesupport/libechonest/libechonest-20101104.py
@@ -11,13 +11,15 @@ class subinfo( info.infoclass ):
 
     def setTargets( self ):
       self.svnTargets[ 'gitHEAD' ] = '[git]kde:libechonest'
-      for ver in ['1.2.1','2.0.1','2.0.2']:
+      for ver in ['1.2.1','2.0.1','2.0.2','2.0.3']:
         self.targets[ver] = 'http://files.lfranchi.com/libechonest-%s.tar.bz2' % ver
         self.targetInstSrc[ver] = 'libechonest-%s' % ver
       self.targetDigests['1.2.1'] = '5ad5897c91c365b32840e75e806c9725c89b4522'
       self.targetDigests['2.0.1'] = '5dd98ffb370e0e199e37ece4a1775a05594f3dcb'
       self.targetDigests['2.0.2'] = '346eba6037ff544f84505941832604668c1e5b2b'
-      self.defaultTarget = '2.0.2'
+      self.targetDigests['2.0.3'] = '10ada8aced6dce3c0d206afcfbd4b05313bd4d04'
+      self.patchToApply['2.0.3'] = ('libechonest-2.0.3-20130419.diff',1)
+      self.defaultTarget = '2.0.3'
 
 
 class Package( CMakePackageBase ):
