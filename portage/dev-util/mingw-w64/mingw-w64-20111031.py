a019f6d72ededbc8a08e859a0b2c6e2d2ba86f58
Patrick von Reth <vonreth@kde.org>
2013-04-29 11:39:08 +0200

    updated 4.8

--- portage/dev-util/mingw-w64/mingw-w64-20111031.py
+++ portage/dev-util/mingw-w64/mingw-w64-20111031.py
@@ -6,13 +6,13 @@ import emergePlatform
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets["4.7.2"] = "http://downloads.sourceforge.net/sourceforge/mingwbuilds/%s-4.7.2-release-posix-sjlj-rev3.7z" % emergePlatform.buildArchitecture() 
+        ver = "4.8.0"
+        rev = "2"
         if emergePlatform.buildArchitecture() == 'x64':
-            self.targets["4.8.0-1"] = "http://downloads.sourceforge.net/sourceforge/mingwbuilds/x64-4.8.0-release-posix-seh-rev1.7z"
-            self.targetDigests['4.7.2'] = 'e4cc0963bbfe632fd4f7170767f5654ee6adb3c9'
+            self.targets[ "%s-%s" % ( ver, rev ) ] = "http://downloads.sourceforge.net/sourceforge/mingwbuilds/x64-%s-release-posix-seh-rev%s.7z" % ( ver, rev )
         else:
-            self.targets["4.8.0-1"] = "http://downloads.sourceforge.net/sourceforge/mingwbuilds/x32-4.8.0-release-posix-sjlj-rev1.7z"
-        self.defaultTarget = "4.8.0-1"
+            self.targets[ "%s-%s" % ( ver, rev )] = "http://downloads.sourceforge.net/sourceforge/mingwbuilds/x32-%s-release-posix-sjlj-rev%s.7z" % ( ver, rev )
+        self.defaultTarget = "%s-%s" % ( ver, rev )
 
     def setDependencies( self ):
         self.buildDependencies['virtual/bin-base'] = 'default'
