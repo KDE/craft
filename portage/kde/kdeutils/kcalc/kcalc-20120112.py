cb3f4a82db7466895584b53ef485a298e6e275ff
Patrick Spendrin <ps_ml@gmx.de>
2013-05-05 20:52:09 +0200

    fix dependency on mpir
    BUG:319365

--- portage/kde/kdeutils/kcalc/kcalc-20120112.py
+++ portage/kde/kdeutils/kcalc/kcalc-20120112.py
@@ -17,6 +17,7 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.runtimeDependencies['kde/kde-runtime'] = 'default'
         self.dependencies['kde/kdelibs'] = 'default'
+        self.dependencies['win32libs/mpir'] = 'default'
 
 from Package.CMakePackageBase import *
 
