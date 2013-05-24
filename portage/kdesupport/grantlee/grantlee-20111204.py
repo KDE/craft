68f0ad9a0855a0c856cbd33c2265be4e6143503b
Andrius da Costa Ribas <andriusmao@gmail.com>
2013-05-24 16:06:17 -0300

    Fix patchToApply getting overriden in grantlee recipe.

--- portage/kdesupport/grantlee/grantlee-20111204.py
+++ portage/kdesupport/grantlee/grantlee-20111204.py
@@ -10,10 +10,10 @@ class subinfo(info.infoclass):
         for ver in ['0.3.0']:
             self.targets[ ver ] = "http://downloads.grantlee.org/grantlee-%s.tar.gz" % ver
             self.targetInstSrc[ ver ] = "grantlee-%s" % ver
-        self.patchToApply['0.3.0'] = [("0001-add-plugin-path-depending-on-executable-location.patch", 1)]
+        self.patchToApply['0.3.0'] = [("0001-add-plugin-path-depending-on-executable-location.patch", 1),
+                                      ("patches/0.3.0/fix-exports-generator-for-intel-compiler.diff", 1)]
         self.shortDescription = 'libraries for a template system similar to django\'s'
         self.defaultTarget = '0.3.0'
-        self.patchToApply['0.3.0'] = [ ("patches/0.3.0/fix-exports-generator-for-intel-compiler.diff", 1) ]
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
