5ed6fd78cf36eeda8ac195705008b40887e18c41
Patrick Spendrin <ps_ml@gmx.de>
2013-05-24 13:52:32 +0200

    fix download location of libqtsoap

--- portage/kdesupport/libqtsoap/libqtsoap-2.7._1.20110313.py
+++ portage/kdesupport/libqtsoap/libqtsoap-2.7._1.20110313.py
@@ -10,7 +10,7 @@ class subinfo(info.infoclass):
 
 
     def setTargets( self ):
-        self.targets['2.7_1'] = 'http://get.qt.nokia.com/qt/solutions/lgpl/qtsoap-2.7_1-opensource.zip'
+        self.targets['2.7_1'] = 'http://www.winkde.org/pub/kde/ports/win32/repository/other/qtsoap-2.7_1-opensource.zip'
         self.patchToApply['2.7_1'] = ('qtsoap-2.7_1-opensource-20110308.diff',1)
         self.targetDigests['2.7_1'] = '933ffd4215052af7eed48fc9492a1cd6996c7641'
         self.targetInstSrc['2.7_1'] = 'qtsoap-2.7_1-opensource'
