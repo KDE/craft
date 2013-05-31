137ba96e42faad0dc0c10129f2fa0ed0b363eaeb
Patrick Spendrin <ps_ml@gmx.de>
2013-05-31 15:29:46 +0200

    fix issues that result in removal of toLower()

--- portage/kde/kdelibs/kdelibs-20110130.py
+++ portage/kde/kdelibs/kdelibs-20110130.py
@@ -13,7 +13,9 @@ class subinfo(info.infoclass):
         self.patchToApply["4.10.2"] = [("kdelibs-4.10.2-20130430.diff", 1), 
                                        ("kde.conf-extended-fix.diff", 1),
                                        ("fix-kdoctools.diff", 1),
-                                       ("fix-casing-of-realFilePath.diff", 1),
+                                       ("extended-fix-casing-of-realFilePath.diff", 1),
+                                       ("fix-dir-separator.diff", 1),
+                                       ("fix-krun-url-case-sensitivity.diff", 1),
                                        ("add-new-solid-win-backend.diff", 1)]
         self.shortDescription = "The KDE Library"
         self.defaultTarget = 'gitHEAD'
