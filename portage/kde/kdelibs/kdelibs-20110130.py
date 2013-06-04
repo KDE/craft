c1b494c195b6840143286deec077455dcf2eb10a
Patrick von Reth <vonreth@kde.org>
2013-06-04 13:05:32 +0200

    added another patch

--- portage/kde/kdelibs/kdelibs-20110130.py
+++ portage/kde/kdelibs/kdelibs-20110130.py
@@ -16,7 +16,8 @@ class subinfo(info.infoclass):
                                        ("extended-fix-casing-of-realFilePath.diff", 1),
                                        ("fix-dir-separator.diff", 1),
                                        ("fix-krun-url-case-sensitivity.diff", 1),
-                                       ("add-new-solid-win-backend.diff", 1)]
+                                       ("add-new-solid-win-backend.diff", 1),
+                                       ("0003-fixed-atatch-to-attachToConsole.patch", 1)]
         self.shortDescription = "The KDE Library"
         self.defaultTarget = 'gitHEAD'
 
