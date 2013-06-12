ff7c46c887edf560acaab328a690dc28dfa106fc
Patrick Spendrin <ps_ml@gmx.de>
2013-06-13 00:39:22 +0200

    fix soprano and use the local socket

--- portage/kdesupport/soprano/soprano-2.5.63-20101204.py
+++ portage/kdesupport/soprano/soprano-2.5.63-20101204.py
@@ -19,7 +19,11 @@ class subinfo(info.infoclass):
         for ver in ['v2.8.0', 'v2.9.0', 'v2.9.2']:
             self.targets[ ver ] = 'http://downloads.sourceforge.net/soprano/soprano-' + ver.replace('v', '') + '.tar.bz2'
             self.targetInstSrc[ ver ] = 'soprano-' + ver[1:]
-        self.patchToApply['v2.9.0'] = [("soprano-redland-callback.diff", 1)]
+        self.patchToApply['v2.9.0'] = [("soprano-redland-callback.diff", 1),
+                                       ("0001-test-if-virtuoso-executable-exists-first.patch", 1),
+                                       ("0002-use-QLocalSocket-on-Windows-since-this-is-what-the-o.patch", 1)]
+        self.patchToApply['v2.9.0'] = [("0001-test-if-virtuoso-executable-exists-first.patch", 1),
+                                       ("0002-use-QLocalSocket-on-Windows-since-this-is-what-the-o.patch", 1)]
         self.shortDescription = "a RDF storage solutions library"
         self.defaultTarget = 'v2.9.2'
 
