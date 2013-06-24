9459fd3e86c86527cf7ec098a240c50e5c57f5fa
Patrick Spendrin <ps_ml@gmx.de>
2013-06-25 01:00:38 +0200

    split kdenetwork into several packages and rebase the patches

--- portage/kde/kdenetwork/kdenetwork-20080202.py
+++ portage/kde/kdenetwork/kdenetwork-20080202.py
@@ -1,40 +1,27 @@
 import info
-import kdedefaults as kd
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
-            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-        self.patchToApply['4.10.1'] = [("kdenetwork-4.8.0-20120125.diff", 1)]
-        self.patchToApply['4.10.2'] = [("kdenetwork-4.10.2-fix-MSVC2010-compile.patch", 1), 
-                                       ("kdenetwork-4.10.2-fix-yahoo-mingw.diff", 1), 
-                                       ("kdenetwork-4.10.2-fix-jabber-mingw.diff", 1),
-                                       ("kdenetwork-krdc-app-icon.diff", 1)]
-
-        if kd.kdebranch == 'master':
-            self.svnTargets['svnHEAD'] = 'trunk/KDE/%s' % self.package
-        else:
-            self.svnTargets['svnHEAD'] = 'branches/KDE/%s/%s' % (kd.kdeversion[:-1], self.package)
-
-        self.defaultTarget = 'svnHEAD'
-
-    def setDependencies( self ):
-        self.dependencies['kde/kde-runtime'] = 'default'
-        self.dependencies['kde/kdepimlibs'] = 'default'
-        self.dependencies['extragear/libktorrent'] = 'default'
-        self.dependencies['kdesupport/qca'] = 'default'
-        self.dependencies['win32libs/libidn'] = 'default'
-        self.dependencies['win32libs/libmsn'] = 'default'
-        self.dependencies['win32libs/mpir'] = 'default'
+        self.svnTargets['gitHEAD'] = ''
+        self.defaultTarget = 'gitHEAD'
         self.shortDescription = "KDE Networking applications (Kopete, KGet)"
 
-from Package.CMakePackageBase import *
+    def setDependencies( self ):
+#        self.dependencies['kde/kdenetwork-filesharing'] = 'default'
+        self.dependencies['kde/kdenetwork-strigi-analyzers'] = 'default'
+        self.dependencies['kde/kdnssd'] = 'default' # this builds fine, but is it really useful?
+        self.dependencies['kde/kget'] = 'default'
+        self.dependencies['kde/kopete'] = 'default'
+#        self.dependencies['kde/kppp'] = 'default' # doesn't build on windows
+        self.dependencies['kde/krdc'] = 'default' # this builds fine, but is it really useful?
+#        self.dependencies['kde/krfb'] = 'default' # doesn't build on windows
 
-class Package(CMakePackageBase):
+from Package.VirtualPackageBase import *
+
+class Package(VirtualPackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
-        CMakePackageBase.__init__( self )
+        VirtualPackageBase.__init__( self )
 
 if __name__ == '__main__':
     Package().execute()
