c4eccb92166e8adf0a4b53f6d083d9b007fcbbb0
Patrick von Reth <vonreth@kde.org>
2013-06-24 12:01:05 +0200

    added 1.1 target

--- portage/win32libs/shared-mime-info/shared-mime-info-0.71.20101219.py
+++ portage/win32libs/shared-mime-info/shared-mime-info-0.71.20101219.py
@@ -14,17 +14,22 @@ http://ftp.gnome.org/pub/gnome/sources/glib/2.24/glib-2.24.0.tar.bz2
 """, '1.0': """
 http://people.freedesktop.org/~hadess/shared-mime-info-1.0.tar.xz
 http://ftp.gnome.org/pub/gnome/sources/glib/2.24/glib-2.24.0.tar.bz2
+""", '1.1': """
+http://people.freedesktop.org/~hadess/shared-mime-info-1.1.tar.xz
+http://ftp.gnome.org/pub/gnome/sources/glib/2.24/glib-2.24.0.tar.bz2
 """}
 GLIB_VER = "2.24.0"
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['0.71', '1.0']:
+        for ver in ['0.71', '1.0','1.1']:
             self.targets[ ver ] = SRC_URI[ver]
             self.targetInstSrc[ ver ] = "shared-mime-info-" + ver
         self.targetDigests['0.71'] = ['6f3d3379662857646b0c5b95d5d26e47c0b6250a',
                                       '32714e64fff52d18db5f077732910215790e0c5b']
         self.targetDigests['1.0'] = ['146dbad62f5450116b0353f88bb8e700f0034013',
+                                     '32714e64fff52d18db5f077732910215790e0c5b']
+        self.targetDigests['1.1'] = ['752668b0cc5729433c99cbad00f21241ec4797ef',
                                      '32714e64fff52d18db5f077732910215790e0c5b']
         self.shortDescription = "common mimetype library"
         self.defaultTarget = '0.71'
