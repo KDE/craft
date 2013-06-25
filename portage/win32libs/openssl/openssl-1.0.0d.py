569d55064e1ea1e698dab1c1a21eb49c218756bf
Patrick von Reth <vonreth@kde.org>
2013-06-25 17:54:32 +0200

    fix

--- portage/win32libs/openssl/openssl-1.0.0d.py
+++ portage/win32libs/openssl/openssl-1.0.0d.py
@@ -149,7 +149,7 @@ class PackageMSys(AutoToolsPackageBase):
         self.subinfo.options.package.packageName = 'openssl'
         self.subinfo.options.package.packSources = False
         AutoToolsPackageBase.__init__(self)
-        self.shell = MSysShell()
+        self.platform = ""
 
         self.buildInSource=True
 
