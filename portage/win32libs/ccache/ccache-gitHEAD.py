8a91bf4fc67493ce61f40509f327739b1f6e8123
Patrick von Reth <vonreth@kde.org>
2013-06-29 16:02:05 +0200

    fixed dependency

--- portage/win32libs/ccache/ccache-gitHEAD.py
+++ portage/win32libs/ccache/ccache-gitHEAD.py
@@ -14,7 +14,7 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
         if compiler.isMinGW():
-            self.buildDependencies['dev-util/autotools'] = 'default'
+            self.buildDependencies['dev-util/msys'] = 'default'
 
 
 from Package.AutoToolsPackageBase import *
