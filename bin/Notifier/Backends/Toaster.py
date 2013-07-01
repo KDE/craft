4df2dbde868920c54c2b01db0d211d7503fb1da0
Patrick von Reth <vonreth@kde.org>
2013-07-01 11:19:45 +0200

    use my own implementation

--- bin/Notifier/Backends/Toaster.py
+++ bin/Notifier/Backends/Toaster.py
@@ -9,5 +9,5 @@ class Toaster(NotificationInterface):
         
     def notify(self,title,message,alertClass):
         path = os.path.dirname(os.path.realpath(__file__))
-        subprocess.Popen( "%s -t \"%s\" -m \"%s\" -p \"%s\"" % (os.path.join( path, "..", "Libs", "toaster", "toast.exe") ,  title , message , os.path.join(path, "..", "kde-logo.png" )))
+        subprocess.Popen( "%s -t \"%s\" -m \"%s\" -p \"%s\"" % (os.path.join( path, "..", "Libs", "toaster", "SnoreToast.exe") ,  title , message , os.path.join(path, "..", "kde-logo.png" )))
         
