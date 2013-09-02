f036939c47c62e05993a8c5f4104d342a8f8bca2
Nico Kruber <nico.kruber@gmail.com>
2013-09-02 03:56:52 +0200

    add libgcrypt version 1.5.3 and set as default

--- portage/win32libs/gcrypt/gcrypt-1.5.0.py
+++ portage/win32libs/gcrypt/gcrypt-1.5.0.py
@@ -4,15 +4,21 @@ import compiler
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in ['1.5.0']:
-            self.targets[ ver ] = 'ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-' + ver + '.tar.gz'
+        for ver in ['1.5.0', '1.5.3']:
+            self.targets[ ver ] = 'ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-' + ver + '.tar.bz2'
             self.targetInstSrc[ ver ] = 'libgcrypt-' + ver
 
         self.patchToApply['1.5.0'] = [('libgcrypt-1.5.0-20110831.diff', 1), 
                                       ('libgcrypt-1.5.0-cmake.diff', 1),
                                       ('libgcrypt-win64.diff', 1)]
-        self.targetDigests['1.5.0'] = 'e6508315b76eaf3d0df453f67371b106654bd4fe'
-        self.defaultTarget = '1.5.0'
+        self.patchToApply['1.5.3'] = [('libgcrypt-1.5.0-20110831.diff', 1), 
+                                      ('libgcrypt-1.5.3-cmake.diff', 1),
+                                      ('libgcrypt-win64.diff', 1)]
+        self.targetDigests['1.5.0'] = '3e776d44375dc1a710560b98ae8437d5da6e32cf'
+        self.targetDigests['1.5.3'] = '2c6553cc17f2a1616d512d6870fe95edf6b0e26e'
+
+        self.shortDescription = " General purpose crypto library based on the code used in GnuPG."
+        self.defaultTarget = '1.5.3'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
