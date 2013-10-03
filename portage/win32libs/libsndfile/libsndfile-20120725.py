d67b41418aefe3edb9b590c2f69f97d0b819dc79
Nico Kruber <nico.kruber@gmail.com>
2013-10-03 04:21:34 +0200

    fix version in CMakefile of libsndfile

--- portage/win32libs/libsndfile/libsndfile-20120725.py
+++ portage/win32libs/libsndfile/libsndfile-20120725.py
@@ -6,9 +6,10 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets[ '1.0.24' ] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.24.tar.gz'
         self.targetInstSrc[ '1.0.24' ] = 'libsndfile-1.0.24'
-        self.patchToApply[ '1.0.24' ] = [( 'libsndfile-1.0.21-20101127.diff', 1 )]
-        self.shortDescription = "a C library for reading and writing files containing sampled sound"
+        self.patchToApply[ '1.0.24' ] = [( 'libsndfile-1.0.24-20131003.diff', 1 )]
         self.targetDigests['1.0.24'] = 'ade2dad272b52f61bb58aca3a4004b28549ee0f8'
+
+        self.shortDescription = "a C library for reading and writing files containing sampled sound"
         self.defaultTarget = '1.0.24'
 
     def setDependencies( self ):
