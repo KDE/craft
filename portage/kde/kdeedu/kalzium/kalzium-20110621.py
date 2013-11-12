12fce38af27af85894bea7eb904918381c0a7614
Nico Kruber <nico.kruber@gmail.com>
2013-11-12 23:15:03 +0100

    fix kalzium not linking to openbabel with msvc2010

--- portage/kde/kdeedu/kalzium/kalzium-20110621.py
+++ portage/kde/kdeedu/kalzium/kalzium-20110621.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4', '5']:
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
+            self.patchToApply[kd.kdeversion + ver] = [('kalzium-4.11.3-20131112.diff', 1)]
 
         self.shortDescription = 'periodic table of elements'
         self.defaultTarget = 'gitHEAD'
