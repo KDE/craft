5374bda2761be3b3b702d85210faa9c4f402be8f
Nico Kruber <nico.kruber@gmail.com>
2013-11-12 23:37:00 +0100

    adapt the 4.10 kcalc patch for 4.11

--- portage/kde/kdeutils/kcalc/kcalc-20120112.py
+++ portage/kde/kdeutils/kcalc/kcalc-20120112.py
@@ -7,9 +7,10 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4', '5']:
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-        self.patchToApply['4.10.0'] = [("kcalc-4.10.0-20130301.diff", 1)]
-        self.patchToApply['4.10.1'] = [("kcalc-4.10.0-20130301.diff", 1)]
-        self.patchToApply['4.10.2'] = [("kcalc-4.10.0-20130301.diff", 1)]
+            if kd.kdeversion == '4.10.':
+                self.patchToApply[kd.kdeversion + ver] = [("kcalc-4.10.0-20130301.diff", 1)]
+            if kd.kdeversion == '4.11.':
+                self.patchToApply[kd.kdeversion + ver] = [("kcalc-4.11.3-20131112.diff", 1)]
 
         self.shortDescription = "KDE's calculator"
         self.defaultTarget = 'gitHEAD'
