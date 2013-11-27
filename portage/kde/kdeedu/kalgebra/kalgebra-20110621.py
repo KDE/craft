529b9f44174287f24755cb5c55ba85c74d1ab016
Nico Kruber <nico.kruber@gmail.com>
2013-11-27 22:50:14 +0100

    kalgebra: add missing dependency to libkdeedu

--- portage/kde/kdeedu/kalgebra/kalgebra-20110621.py
+++ portage/kde/kdeedu/kalgebra/kalgebra-20110621.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.dependencies['kde/kde-runtime'] = 'default'
         self.dependencies['kde/analitza'] = 'default'
+        self.dependencies['kde/libkdeedu'] = 'default'
 
 from Package.CMakePackageBase import *
 
