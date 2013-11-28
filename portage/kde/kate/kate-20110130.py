ac71b089f00aff883c6d78c78183e5c57414ff67
Nico Kruber <nico.kruber@gmail.com>
2013-11-28 10:59:40 +0100

    kate: remove old patches
    (they have been pushed upstream a while ago)

--- portage/kde/kate/kate-20110130.py
+++ portage/kde/kate/kate-20110130.py
@@ -7,11 +7,8 @@ class subinfo( info.infoclass ):
         for ver in ['0', '1', '2', '3', '4', '5']:
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-        self.patchToApply['4.10.0'] = [("kate-4.10.0.diff", 1)]#upstream
-        self.patchToApply['4.10.1'] = [("kate-4.10.1-20130304.diff", 1)]#upstream
-        self.patchToApply['4.10.2'] = [("kate-4.10.1-20130304.diff", 1)]#upstream
-        self.shortDescription = "the KDE text editor"
 
+        self.shortDescription = "the KDE text editor"
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
