8b5abd405df4669753532a87415d854d2b49b105
Nico Kruber <nico.kruber@gmail.com>
2013-11-30 00:14:00 +0100

    kapptemplate: push patch upstream

--- portage/kde/kdesdk/kapptemplate/kapptemplate-20130225.py
+++ portage/kde/kdesdk/kapptemplate/kapptemplate-20130225.py
@@ -7,7 +7,6 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4', '5']:
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-        self.patchToApply['4.10.2'] = [("kapptemplate-4.10.2-20130502.diff", 1)]
 
         self.defaultTarget = 'gitHEAD'
 
