3d2e84b1bc75dd5a16ccd79658c2958cfda41185
Patrick Spendrin <ps_ml@gmx.de>
2013-11-30 00:10:48 +0100

    add new wip branch

--- portage/kdeapps/konsole/konsole-20110130.py
+++ portage/kdeapps/konsole/konsole-20110130.py
@@ -3,12 +3,14 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:konsole'
-        self.svnTargets['winport'] = '[git]kde:konsole|winport|'
+        self.svnTargets['winport'] = '[git]kde:konsole|sengels/winport|'
         self.defaultTarget = 'winport'
 
     def setDependencies( self ):
         self.dependencies['kde/kdelibs'] = 'default'
         self.dependencies['testing/kcwsh'] = 'default'
+	self.runtimeDependencies['kde/kde-runtime'] = 'default'
+
 
 from Package.CMakePackageBase import *
 
