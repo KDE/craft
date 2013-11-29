a97a0c983b6cfd86c8a12d2ee8679b8ecde99ae6
Patrick von Reth <vonreth@kde.org>
2013-11-29 16:37:14 +0100

    removed upstreamed patch

--- portage/kde/kde-workspace/kde-workspace-20110130.py
+++ portage/kde/kde-workspace/kde-workspace-20110130.py
@@ -7,10 +7,6 @@ class subinfo(info.infoclass):
         for ver in ['0', '1', '2', '3', '4', '5']:
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-            self.patchToApply[kd.kdeversion + ver] = [("build_kinfocenter.diff", 1)]
-
-        # on reviewboard: https://git.reviewboard.kde.org/r/111342/
-        self.patchToApply['gitHEAD'] = [("build_kinfocenter.diff", 1)]
 
         self.shortDescription = 'the KDE workspace including the oxygen style'
         self.defaultTarget = 'gitHEAD'
