e6af10d3983e84e9c5a8f3c7c431d3cd6984f2e7
Patrick von Reth <vonreth@kde.org>
2013-12-05 11:31:40 +0100

    updated sqlite

--- portage/win32libs/sqlite/sqlite-3.7.2.py
+++ portage/win32libs/sqlite/sqlite-3.7.2.py
@@ -9,22 +9,26 @@ class subinfo(info.infoclass):
         self.targets['3.7.2'] = 'http://www.sqlite.org/sqlite-amalgamation-3_7_2.zip'
         self.targets['3.7.15.2'] = 'http://www.sqlite.org/sqlite-amalgamation-3071502.zip'
         self.targets['3.8.0.1'] = 'https://www.sqlite.org/2013/sqlite-amalgamation-3080001.zip'
+        self.targets['3.8.1.0'] = 'http://sqlite.org/2013/sqlite-amalgamation-3080100.zip'
 
         self.targetDigests['3.7.15.2'] = '1312016fdbb1dd6cb5a38bc05d1d051d9dc0e93e'
         self.targetDigests['3.7.2'] = 'e37304dffaf6e1b38357113ee9845cadff05e958'
         self.targetDigests['3.8.0.1'] = '73b2c4ae0cbbd630163f974cf1551bbacbfffa6f'
+        self.targetDigests['3.8.1.0'] = '75a1ab154e796d2d1b391a2c7078679e15512bda'
 
         self.patchToApply['3.6.23.1'] = [("sqlite_cmake_and_wince_20100923.diff", 1)]
         self.patchToApply['3.7.2']    = [("sqlite_cmake_and_wince_20100923.diff", 1)]
         self.patchToApply['3.7.15.2'] = [("sqlite_cmake_and_wince_20130124.diff", 1)]
         self.patchToApply['3.8.0.1'] = [("sqlite_cmake_and_wince_20130124.diff", 1)]
+        self.patchToApply['3.8.1.0'] = [("sqlite_cmake_and_wince_20130124.diff", 1)]
         
         self.targetInstSrc['3.6.23.1'] = "sqlite-3.6.23.1"
         self.targetInstSrc['3.7.15.2'] = "sqlite-amalgamation-3071502"
         self.targetInstSrc['3.8.0.1'] = "sqlite-amalgamation-3080001"
+        self.targetInstSrc['3.8.1.0'] = "sqlite-amalgamation-3080100"
         
         self.shortDescription = "a library providing a self-contained, serverless, zero-configuration, transactional SQL database engine"
-        self.defaultTarget = '3.8.0.1'
+        self.defaultTarget = '3.8.1.0'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
