adcd1fbdd910b6f2ca9d8bcf3691c54cfb485846
Patrick Spendrin <ps_ml@gmx.de>
2013-12-23 16:58:28 +0100

    fix libraw on msvc

--- portage/win32libs/libraw/libraw-0.16.py
+++ portage/win32libs/libraw/libraw-0.16.py
@@ -8,6 +8,7 @@ class subinfo(info.infoclass):
         self.targets['0.16.0-Beta1'] = 'http://www.libraw.org/data/LibRaw-0.16.0-Beta1.tar.gz'
         self.targetInstSrc['0.16.0-Beta1'] = "LibRaw-0.16.0-Beta1"
         self.targetDigests['0.16.0-Beta1'] = '1bda8b14098b1897264c19728c147bdb43afda60'
+        self.patchToApply['0.16.0-Beta1'] = [('LibRaw-0.16.0-Beta1-20131223.diff', 1)]
         
         self.shortDescription = "LibRaw is a library for reading RAW files obtained from digital photo cameras (CRW/CR2, NEF, RAF, DNG, and others)."
         self.defaultTarget = '0.16.0-Beta1'
