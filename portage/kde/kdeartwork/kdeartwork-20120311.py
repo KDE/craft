99e4d6061ce87bbca2da10c452023a7c2da20b6d
Patrick Spendrin <ps_ml@gmx.de>
2013-12-23 17:03:01 +0100

    add tarballs for kdeartwork

--- portage/kde/kdeartwork/kdeartwork-20120311.py
+++ portage/kde/kdeartwork/kdeartwork-20120311.py
@@ -1,8 +1,12 @@
 import info
+import kdedefaults as kd
 
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['svnHEAD'] = 'trunk/KDE/kdeartwork'
+        for ver in ['0', '1', '2', '3', '4', '5']:
+            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
+            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
         self.defaultTarget = 'svnHEAD'
 
     def setDependencies( self ):
