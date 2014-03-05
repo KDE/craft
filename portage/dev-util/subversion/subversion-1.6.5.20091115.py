a008b226936433a7c53536aafaec2cfc8f078a95
Nico Kruber <nico.kruber@gmail.com>
2014-03-06 00:39:49 +0100

    subversion: update to 1.8.5

--- portage/dev-util/subversion/subversion-1.6.5.20091115.py
+++ portage/dev-util/subversion/subversion-1.6.5.20091115.py
@@ -3,16 +3,17 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['1.6.5'] = "http://subversion.tigris.org/files/documents/15/47906/svn-win32-1.6.5.zip"
-        for ver in ['1.7.6', '1.8.1']:
+        for ver in ['1.7.6', '1.8.1', '1.8.5']:
             self.targets[ver] = 'http://downloads.sourceforge.net/win32svn/' + ver + '/apache22/svn-win32-' + ver + '.zip'
-        for ver in ['1.6.5', '1.7.6', '1.8.1']:
+        for ver in ['1.6.5', '1.7.6', '1.8.5']:
             # this location affects class SvnSource
             self.targetMergePath[ver] = "dev-utils/svn";
             self.targetMergeSourcePath[ver] = "svn-win32-" + ver;
         self.targetDigests['1.6.5'] = '0df7b20e0bf0fa82ca3a9ededb9207ba50df063e'
         self.targetDigests['1.7.6'] = '3a18decdf971268abdec34ab459147a8139241db'
         self.targetDigests['1.8.1'] = 'ecc4f3b05322641b68f4285236fb58add227cc71'
-        self.defaultTarget = '1.8.1'
+        self.targetDigests['1.8.5'] = 'e62aaf75c48d2ff98e5fbdf6ddc4d60f8f3f7e29'
+        self.defaultTarget = '1.8.5'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/bin-base'] = 'default'
