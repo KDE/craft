d6f1a926e8e2bdee331c8ba0e3089d9f117ba6d4
Patrick von Reth <vonreth@kde.org>
2014-03-10 16:42:35 +0100

    Cleanup qt build recipe
    - Remove old versions before 4.8.4
    - Remove wince support
    - Use the correct branch for master target
    - Use original configure.exe for master
    - Use tag tarballs instead of cloning the whole qt.git into a new folder
    for each tag checkout

--- portage/libs/qt/qt-all.py
+++ portage/libs/qt/qt-all.py
@@ -19,106 +19,40 @@ from Package.QMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        # the static version uses one of the stable versions
-        self.svnTargets['static'] = "[git]kde:qt-kde|4.5.2-patched|"
-        # this is the upcoming 4.7 version with the KDE patches.
-        self.svnTargets['master'] = "[git]kde:qt-kde"
-        # this version contains the patches against the 4.5.3 release and is recommended for KDE 4.3.X
-        self.svnTargets['4.5.3'] = "[git]kde:qt-kde|4.5.3-patched|"
-        # this branch contains all the patches and follows the 4.6-stable branch on qt.git - it updates daily
-        self.svnTargets['4.6'] = "[git]kde:qt-kde|4.6-stable-patched|"
-        # those are the stable releases with the KDE patches applied on top
-        self.svnTargets['4.6.0'] = "[git]kde:qt-kde|4.6.0-patched|"
-        self.svnTargets['4.6.1'] = "[git]kde:qt-kde|4.6.1-patched|"
-        self.svnTargets['4.6.2'] = "[git]kde:qt-kde|4.6.2-patched|"
-        self.svnTargets['4.6.3'] = "[git]kde:qt-kde|4.6.3-patched|"
-        self.svnTargets['4.7.0'] = "[git]kde:qt-kde|4.7.0-patched|"
-        self.svnTargets['4.7.1'] = "[git]kde:qt-kde|4.7.1-patched|"
-        self.svnTargets['4.7.4'] = "[git]kde:qt||v4.7.4"
-        self.svnTargets['4.8.0'] = "[git]kde:qt||v4.8.0"
-        self.svnTargets['4.8.1'] = "[git]kde:qt||v4.8.1"
-        self.svnTargets['4.8.2'] = "[git]kde:qt||v4.8.2"
-        self.svnTargets['4.8.3'] = "[git]kde:qt||v4.8.3"
-        self.svnTargets['4.8.4'] = "[git]kde:qt||v4.8.4"
-        self.svnTargets['4.8.5'] = "[git]kde:qt||v4.8.5"
-        self.svnTargets['4.7'] = "git://gitorious.org/qt/qt.git|4.7|"
-        self.svnTargets['wince'] = "git://gitorious.org/qt/qt.git|4.7|235d1d687dcc2d21860cd753c9d67964c5270be2"
-        self.svnTargets['wince-4.7'] = self.svnTargets["4.7"]
-        self.targetSrcSuffix['wince'] = "4.7"
-        self.targetSrcSuffix['wince-4.7'] = "4.7"
-        self.targetSrcSuffix['4.7'] = "4.7"
-        self.targetSrcSuffix['4.7.4'] = "4.7.4"
-        self.targetSrcSuffix['4.8.0'] = "4.8.0"
-        self.targetSrcSuffix['4.8.1'] = "4.8.1"
-        self.targetSrcSuffix['4.8.2'] = "4.8.2"
-        self.targetSrcSuffix['4.8.3'] = "4.8.3"
-        self.targetSrcSuffix['4.8.4'] = "4.8.4"
-        self.targetSrcSuffix['4.8.5'] = "4.8.5"
-        self.patchToApply['4.6.3'] = ('patches/4.6.3/fixed_export_macro_for_QtDbus.patch', 1)
-        self.patchToApply['4.7'] = [
-            ('patches/4.7/out-of-source-build.patch', 1),
-            ('patches/4.7/webkit-fixes.patch', 1) ]
-        self.patchToApply['4.7.1'] = [('patches/4.7.1/buildfix-for-mingw64.patch', 1)]
-        self.patchToApply['4.7.4'] = [
-            ('patches/4.7/out-of-source-build.patch', 1), 
-            ('patches/4.7/add-pdbs-on-msvc.diff', 1) , 
-            ("patches/4.7/detect-windows-8-as-windows-7.patch", 1 ),
-            ("patches/4.7.4/0001-fixed-error-generating-wrong-introspection-string-in.patch", 1 ),
-            ("patches/4.7.4/Fix-regular-expression-lookup-in-qsslcertificate-from.patch", 1)
-        ]
-        self.patchToApply['4.8.0'] = [
-            ('patches/4.7/out-of-source-build.patch', 1),
-            ('patches/4.8/add-pdbs-on-msvc.diff', 1),
-            ('patches/4.8/detect-windows-8-as-windows-7.patch', 1),
-            ('patches/4.8/fixed-win32-detection.patch',1),
-            ('patches/4.8/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
-            ('patches/4.8.0/0001-Remove-implicit-const-char-QString-cast-from-QDBusSe.patch', 1)
-        ]
-        self.patchToApply['4.8.1'] = [
-            ('patches/4.7/out-of-source-build.patch', 1),
-            ('patches/4.8/add-pdbs-on-msvc.diff', 1),
-            ('patches/4.8/detect-windows-8-as-windows-7.patch', 1),
-            ('patches/4.8/fixed-win32-detection.patch',1),
-            ('patches/4.8/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
-            ('patches/4.8.1/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1)
-        ]
-        self.patchToApply['4.8.2'] = [
-            ('patches/4.7/out-of-source-build.patch', 1),
-            ('patches/4.8/add-pdbs-on-msvc.diff', 1),
-            ('patches/4.8/detect-windows-8-as-windows-7.patch', 1),
-            ('patches/4.8/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
-            ('patches/4.8.1/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1),
-            ('patches/4.8/moc-boost-fix-bug-22829.diff', 1)
-        ]
-        self.patchToApply['4.8.3'] = [
-            ('patches/4.7/out-of-source-build.patch', 1),
-            ('patches/4.8/add-pdbs-on-msvc.diff', 1),
-            ('patches/4.8/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
-            ('patches/4.8.1/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1),
-            ('patches/4.8/moc-boost-fix-bug-22829.diff', 1),
-            ('patches/4.8.3/fix-MinGW-w64-compilation.patch', 1),
-            ("patches/4.8.3/don't set QT_BUILD_DIR as a side effect.patch",1)
-        ]
-        
+        self.svnTargets['master'] = "[git]kde:qt|4.8"
+        self.targets['4.8.4'] = "http://download.qt-project.org/official_releases/qt/4.8/4.8.4/qt-everywhere-opensource-src-4.8.4.tar.gz"
+        self.targets['4.8.5'] = "http://download.qt-project.org/official_releases/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.tar.gz"
+
         self.patchToApply['4.8.4'] = [
-            ('patches/4.7/out-of-source-build.patch', 1),
-            ('patches/4.8/add-pdbs-on-msvc.diff', 1),
-            ('patches/4.8/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
-            ('patches/4.8.1/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1),
-            ('patches/4.8/moc-boost-fix-bug-22829.diff', 1),
+            ('patches/4.8.4/out-of-source-build.patch', 1),
+            ('patches/4.8.4/add-pdbs-on-msvc.diff', 1),
+            ('patches/4.8.4/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
+            ('patches/4.8.4/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1),
+            ('patches/4.8.4/moc-boost-fix-bug-22829.diff', 1),
             ('patches/4.8.4/fix-defined-defined-and-do-not-include-inttypes-for-intel-compiler.patch', 1),
             ('patches/4.8.4/fix-Q_CORE_EXPORT_INLINE-for-intel-compiler.patch', 1),
         ]
         
         self.patchToApply['4.8.5'] = [
-            ('patches/4.7/out-of-source-build.patch', 1),
-            ('patches/4.8/add-pdbs-on-msvc.diff', 1),
-            ('patches/4.8/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
-            ('patches/4.8.1/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1),
+            ('patches/4.8.4/out-of-source-build.patch', 1),
+            ('patches/4.8.4/add-pdbs-on-msvc.diff', 1),
+            ('patches/4.8.4/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
+            ('patches/4.8.4/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1),
             ('patches/4.8.5/fix-defined-defined-and-do-not-include-inttypes-for-intel-compiler.patch', 1),
             ('patches/4.8.4/fix-Q_CORE_EXPORT_INLINE-for-intel-compiler.patch', 1),
             ('patches/4.8.5/0001-fixed-build-with-new-mingw.patch', 1)
         ]
+        
+        self.patchToApply['master'] = [
+            ('patches/4.8.4/out-of-source-build.patch', 1),
+            ('patches/4.8.4/add-pdbs-on-msvc.diff', 1),
+            ('patches/4.8.4/fix-debug-webkit-linkage-QTBUG-20556.patch', 0),
+            ('patches/4.8.4/Use-windows-path-for-pkgconfig-mkdir_p_asstring.patch', 1),
+            ('patches/4.8.5/fix-defined-defined-and-do-not-include-inttypes-for-intel-compiler.patch', 1),
+            ('patches/4.8.4/fix-Q_CORE_EXPORT_INLINE-for-intel-compiler.patch', 1),
+            ('patches/4.8.5/0001-fixed-build-with-new-mingw.patch', 1)
+        ]
+        
 
         self.shortDescription = "a cross-platform application framework"
         # If you change the default target here please do not forget to rename the portage file
@@ -128,40 +62,6 @@ class subinfo(info.infoclass):
         self.options.package.packageName = 'qt'
         self.options.package.specialMode = True
 
-        # WinCE specific part
-        winceVersionIndependentPatches = self.patchToApply['4.7'] + [
-            ('patches/4.7/custom-flags-for-wince.patch', 1),
-            ('patches/4.7/fix-build-uitools-for-wince.patch', 1),
-            ('patches/4.7/exchange-malloc-against-dlmalloc-for-wince.patch', 1),
-            ('patches/4.7/fix-endless-loop-in-qProcess-for-wince.patch', 1),
-            ('patches/4.7/Replace-malloc-in-qimage.patch', 1),
-            ('patches/4.7/Enable-Softkeyboard-wince.patch', 1),
-            ('patches/4.7/comboboxes-wrong-direction_wince.patch', 1),
-            ('patches/4.7/Add-gpgLogging.patch', 1),
-            ('patches/4.7/allow-more-then-one-instance-of-a-wince-application.patch', 1),
-            ('patches/4.7/Add-qCalloc-to-qmalloc.patch', 1),
-            ('patches/4.7/Use-dlmalloc-in-QScript4.dll.patch', 1),
-            ('patches/4.7/Use-dlmalloc-for-javascript-garbage-collector.patch', 1),
-            ('patches/4.7/Use-dlmalloc-in-qpaintengine.patch', 1),
-            ('patches/4.7/Use-qCalloc-instead-of-qt_wince_calloc-impl.patch', 1),
-            ('patches/4.7/Use-qRealloc-instead-of-realloc-in-qimage.patch', 1),
-            ('patches/4.7/fix-calloc.patch', 1)]
-        self.patchToApply['wince']     =  winceVersionIndependentPatches + [
-            ('patches/4.7/fix-qml-alignment.patch', 1), # Upstream in 4.7
-            ('patches/4.7/Override-new-in-qt-dlls-to-use-dlmalloc.patch', 1),
-            ('patches/4.7/Replace-qeventdispatcher.patch', 1),
-            ('patches/4.7/fix-build-with-QT_NO_SVG.patch', 1),
-            ('patches/4.7/fix-QSortFilterProxyModel.patch', 1),
-            ('patches/4.7/Repaint-when-text-color-changes.patch', 1),
-            ('patches/4.7/fix-map-to-global-calculations.patch', 1),
-            ('patches/4.7/fix-widget-creation.patch', 1)]
-        self.patchToApply['wince-4.7'] =  winceVersionIndependentPatches + [
-            ('patches/4.7/Override-new-in-qt-dlls-for-4-7-branch.patch', 1),
-            ('patches/4.7/Replace-qeventdispatcher-and-add-wcecompat-dep-for-4-7-branch.patch', 1),
-            ('patches/4.7/fix-build-with-QT_NO_SVG-for-4-7-branch.patch', 1)]
-
-        if emergePlatform.isCrossCompilingEnabled():
-            self.defaultTarget = 'wince'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -169,15 +69,12 @@ class subinfo(info.infoclass):
         self.dependencies['win32libs/openssl'] = 'default'
         self.dependencies['win32libs/dbus'] = 'default'
         self.dependencies['win32libs/sqlite'] = 'default'
-        if not emergePlatform.isCrossCompilingEnabled():
-            self.dependencies['binary/mysql-pkg'] = 'default'
-        else:
-            self.dependencies['win32libs/wcecompat'] = 'default'
+        self.dependencies['binary/mysql-pkg'] = 'default'
 
-class Package(PackageBase, GitSource, QMakeBuildSystem, KDEWinPackager):
+class Package(QMakePackageBase):
     def __init__( self, **args ):
         self.subinfo = subinfo()
-        PackageBase.__init__(self)
+        QMakePackageBase.__init__(self)
         if not self.subinfo.options.useShortPathes \
                 and self.compiler() == "mingw4" and len(self.rootdir) > 10:
             # mingw4 cannot compile qt if the command line arguments
@@ -185,38 +82,23 @@ class Package(PackageBase, GitSource, QMakeBuildSystem, KDEWinPackager):
             utils.warning('for mingw4, rootdir %s is too long for full path names.'
                 ' Using short path names.' % self.rootdir)
             self.subinfo.options.useShortPathes = True
-        GitSource.__init__(self)
-        QMakeBuildSystem.__init__(self)
-        KDEWinPackager.__init__(self)
         # get instance of dbus and openssl package
         self.openssl = portage.getPackageInstance('win32libs', 'openssl')
         self.dbus = portage.getPackageInstance('win32libs', 'dbus')
         self.sqlite = portage.getPackageInstance('win32libs', 'sqlite')
-        if not emergePlatform.isCrossCompilingEnabled():
-            self.mysql_server = portage.getPackageInstance('binary', 'mysql-pkg')
-        else:
-            self.wcecompat = portage.getPackageInstance('win32libs', 'wcecompat')
-
+        self.mysql_server = portage.getPackageInstance('binary', 'mysql-pkg')
+        
     def unpack( self ):
-        if not GitSource.unpack( self ): return False
-        utils.copyFile( os.path.join( self.packageDir(), "configure.exe" ),
-                os.path.join( self.sourceDir(), "configure.exe" ) )
+        if not QMakePackageBase.unpack( self ): return False
+        if self.subinfo.buildTarget != "master":
+            utils.copyFile( os.path.join( self.packageDir(), "configure.exe" ),
+                    os.path.join( self.sourceDir(), "configure.exe" ) )
         return True
 
     def configure( self, unused1=None, unused2=""):
         self.enterBuildDir()
         self.setPathes()
 
-        xplatform = ""
-        if self.isTargetBuild():
-            if self.buildPlatform() == "WM60":
-                xplatform = "wincewm60professional-%s" % self.compiler()
-            elif self.buildPlatform() == "WM65":
-                xplatform = "wincewm65professional-%s" % self.compiler()
-            elif self.buildPlatform() == "WM50":
-                xplatform = "wincewm50pocket-%s" % self.compiler()
-            else:
-                exit( 1 )
 
         utils.putenv( "USERIN", "y")
         userin = "y"
@@ -237,35 +119,16 @@ class Package(PackageBase, GitSource, QMakeBuildSystem, KDEWinPackager):
         if self.isTargetBuild():
             incdirs += " -I \"" + os.path.join( self.wcecompat.installDir(), "include" ) + "\""
             libdirs += " -L \"" + os.path.join( self.wcecompat.installDir(), "lib" ) + "\""
-        if not emergePlatform.isCrossCompilingEnabled():
-            incdirs += " -I \"" + os.path.join( self.mysql_server.installDir(), "include" ) + "\""
-            libdirs += " -L \"" + os.path.join( self.mysql_server.installDir(), "lib" ) + "\""
-            libdirs += " -l libmysql "
-        else:
-            utils.copyFile( os.path.join( self.packageDir(), "sources", "qconfig-kde-wince.h" ),
-                    os.path.join( self.sourceDir(), "src", "corelib" , "global", "qconfig-kde-wince.h" ) )
-            utils.copyFile( os.path.join( self.packageDir(), "sources", "new.cpp" ),
-                    os.path.join( self.sourceDir(), "src", "corelib" , "global", "new.cpp" ) )
-            utils.copyFile( os.path.join( self.packageDir(), "sources", "gpglogger_wince.cpp" ),
-                    os.path.join( self.sourceDir(), "src", "corelib" , "global", "gpglogger_wince.cpp" ) )
-            utils.copyFile( os.path.join( self.packageDir(), "sources", "gpglogger_wince.h" ),
-                    os.path.join( self.sourceDir(), "src", "corelib" , "global", "gpglogger_wince.h" ) )
+
+        incdirs += " -I \"" + os.path.join( self.mysql_server.installDir(), "include" ) + "\""
+        libdirs += " -L \"" + os.path.join( self.mysql_server.installDir(), "lib" ) + "\""
+        libdirs += " -l libmysql "
 
         configure = os.path.join( self.sourceDir(), "configure.exe" ).replace( "/", "\\" )
         command = r"echo %s | %s -opensource -prefix %s -platform %s " % ( userin, configure, self.installDir(), self.platform )
-        if emergePlatform.isCrossCompilingEnabled():
-            if self.isTargetBuild():
-                command += "-xplatform %s -qconfig kde-wince " % xplatform
-                command += "-no-exceptions -no-stl -no-rtti "
-            if self.isHostBuild():
-                command += "-no-xmlpatterns -no-declarative -no-opengl "
-            command += "-no-qt3support -no-multimedia -no-scripttools -no-accessibility -no-libmng -no-libtiff -no-gif -no-webkit "
-
-        if not emergePlatform.isCrossCompilingEnabled():
-            # non-cc builds only
-            command += "-plugin-sql-odbc -plugin-sql-mysql -system-sqlite "
-            command += "-qt-style-windowsxp -qt-style-windowsvista "
-            command += "-qt-libpng -qt-libjpeg -qt-libtiff "
+        command += "-plugin-sql-odbc -plugin-sql-mysql -system-sqlite "
+        command += "-qt-style-windowsxp -qt-style-windowsvista "
+        command += "-qt-libpng -qt-libjpeg -qt-libtiff "
 
         # WebKit won't link properly with LTCG in a 32-bit MSVC environment
         if emergePlatform.buildArchitecture() == "x86" and compiler.isMSVC2008():
@@ -341,7 +204,7 @@ class Package(PackageBase, GitSource, QMakeBuildSystem, KDEWinPackager):
         utils.copyFile( os.path.join( self.packageDir(), "qt.conf" ), os.path.join( self.installDir(), "bin", "qt.conf" ) )
 
         # install msvc debug files if available
-        if self.compiler() == "msvc2005" or self.compiler() == "msvc2008" or self.compiler() == "msvc2010" or self.compiler() == "intel":
+        if compiler.isMSVC() or compiler.isIntel():
             srcdir = os.path.join( self.buildDir(), "lib" )
             destdir = os.path.join( self.installDir(), "lib" )
 
