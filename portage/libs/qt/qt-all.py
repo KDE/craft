a1a3daaae1fd4e20e4dfd93ff3a0026ff5c62e70
Patrick von Reth <vonreth@kde.org>
2014-03-11 11:13:20 +0100

    Add a virtual qt-all.py which pulls in qt5

---
+++ portage/libs/qt/qt-all.py
@@ -0,0 +1,24 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = ''
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.dependencies['libs/qt5'] = 'default'
+        
+        
+        
+
+from Package.VirtualPackageBase import *
+
+class Package( VirtualPackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        VirtualPackageBase.__init__( self )
+
+
+if __name__ == '__main__':
+    Package().execute()
