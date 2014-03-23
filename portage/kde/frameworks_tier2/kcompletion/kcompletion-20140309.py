c3f9e53a13ba63c677437c1d1db4c523797a851b
Andrius da Costa Ribas <andriusmao@gmail.com>
2014-03-23 14:30:30 -0300

    Add kcompletion, kcrash, kdnssd - tier2 frameworks.

---
+++ portage/kde/frameworks_tier2/kcompletion/kcompletion-20140309.py
@@ -0,0 +1,34 @@
+import info
+import kdedefaults as kd
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets["gitHEAD"] = "[git]kde:%s|%s|" % (self.package, kd.kdebranch)
+        self.shortDescription = "KCompletion"
+        self.defaultTarget = "gitHEAD"
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.buildDependencies["win32libs/automoc"] = "default"
+        self.dependencies["kde/kconfig"] = "default"
+        self.dependencies["kde/kwidgetsaddons"] = "default"
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = ""
+        if compiler.isMinGW():
+          self.subinfo.options.configure.defines += " -DKDE_DISTRIBUTION_TEXT=\"MinGW %s\" " % compiler.getMinGWVersion()
+        elif compiler.isMSVC():
+          self.subinfo.options.configure.defines += " -DKDE_DISTRIBUTION_TEXT=\"%s\" " % compiler.getVersion()
+
+    def install( self ):
+        if not CMakePackageBase.install( self ):
+            return False
+        return True
+
+if __name__ == "__main__":
+    Package().execute()
