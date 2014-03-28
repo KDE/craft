726d2bc9312bc742b24bf81979c0f4ef984fa121
Patrick von Reth <vonreth@kde.org>
2014-03-28 19:50:23 +0100

    disable digests for procexp

--- portage/dev-util/procexp/procexp-15.3.py
+++ portage/dev-util/procexp/procexp-15.3.py
@@ -4,7 +4,7 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['15.3'] = 'http://download.sysinternals.com/files/ProcessExplorer.zip'
         self.defaultTarget = '15.3'
-        self.targetDigests['15.3'] = '4a1c3964e624254cc301f7c014f9d211d97e37f3'
+        #self.targetDigests['15.3'] = '4a1c3964e624254cc301f7c014f9d211d97e37f3'
         # the zip file does not have a bin dir, so we have to create it
         # This attribute is in prelimary state
         self.targetInstallPath['15.3'] = "bin"
