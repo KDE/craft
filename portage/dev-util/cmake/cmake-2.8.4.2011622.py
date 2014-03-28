b13253cbd6d32fc70031bf13d7fc0fb45e619b5b
Patrick von Reth <vonreth@kde.org>
2014-03-28 15:18:17 +0100

    updated cmake

--- portage/dev-util/cmake/cmake-2.8.4.2011622.py
+++ portage/dev-util/cmake/cmake-2.8.4.2011622.py
@@ -9,6 +9,7 @@ class subinfo( info.infoclass ):
         self.targets['2.8.5'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.5-win32-x86.zip'
         self.targets['2.8.8'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.8-win32-x86.zip'
         self.targets['2.8.12.1'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.12.1-win32-x86.zip'
+        self.targets['2.8.12.2'] = 'http://www.cmake.org/files/v2.8/cmake-2.8.12.2-win32-x86.zip'
         self.targets['v2.8.2'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-v2.8.2-bin.tar.bz2'
         self.targets['2.8.0-ce'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-2.8.0-6-bin.tar.bz2'
         self.targets['2.8.1-ce'] = 'http://downloads.sourceforge.net/kde-windows/cmake-vc90-2.8.1-bin.tar.bz2'
@@ -22,6 +23,7 @@ class subinfo( info.infoclass ):
         self.targetMergeSourcePath['2.8.5'] = 'cmake-2.8.5-win32-x86'
         self.targetMergeSourcePath['2.8.8'] = 'cmake-2.8.8-win32-x86'
         self.targetMergeSourcePath['2.8.12.1'] = 'cmake-2.8.12.1-win32-x86'
+        self.targetMergeSourcePath['2.8.12.2'] = 'cmake-2.8.12.2-win32-x86'
         self.targetDigests['v2.8.2'] = 'de516a570808c7a022139b55e758d5f7b378ec7d'
         self.targetDigests['2.8.3-2'] = 'cba746303abb825c8549e6621da35757c039fa9e'
         self.targetDigests['2.8.4'] = '539ce250521d964a8770e0a7362db196dbc97fbc'
@@ -30,6 +32,7 @@ class subinfo( info.infoclass ):
         self.targetDigests['v2.8.8'] = 'd63da3b1790b64729e357c157ad9103e1bcfa267'
         self.targetDigests['v2.8.10.2'] = '56a5d0820e92c01c39ae2d9be38d3dfe459d4281'
         self.targetDigests['2.8.12.1'] = '6a27d8fcf887774e56fa165eddd5242e1c350464'
+        self.targetDigests['2.8.12.2'] = '0d778fe630e623c881c14e1fef7b6ad40f68055c'
         self.patchToApply['v2.8.2'] = ( 'findtiff.diff', 0 )
         self.patchToApply['v2.8.10.2'] = ( 'findpng16.diff', 0 )
 
@@ -38,7 +41,7 @@ class subinfo( info.infoclass ):
         else:
             # 2.8.4 is broken when using short paths and
             # windres fails even in old projects like zlib
-            self.defaultTarget = '2.8.12.1'
+            self.defaultTarget = '2.8.12.2'
 
     def setBuildOptions( self ):
         self.disableHostBuild = False
