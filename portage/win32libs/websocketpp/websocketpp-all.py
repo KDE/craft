be8b4e99dae3d5056731248f6bd3e09ecc50e770
Patrick von Reth <vonreth@kde.org>
2014-03-31 17:54:41 +0200

    added websocketpp

---
+++ portage/win32libs/websocketpp/websocketpp-all.py
@@ -0,0 +1,23 @@
+import shutil
+import info
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets["gitHEAD"] = 'https://github.com/zaphoyd/websocketpp.git'
+
+
+        self.shortDescription = 'WebSocket++ is a header only C++ library that implements RFC6455 The WebSocket Protocol'
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
+
