2cb9f406de900f73401fe985d19a88b82bcbf950
Patrick von Reth <vonreth@kde.org>
2014-03-31 10:24:41 +0200

    added libgsasl

---
+++ portage/win32libs/libgsasl/libgsasl-1.8.0.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import info
+import os
+from Package.CMakePackageBase import *
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.targets[ '1.8.0' ] = "ftp://ftp.gnu.org/gnu/gsasl/libgsasl-1.8.0.tar.gz"
+        self.targetDigests['1.8.0'] = '08fd5dfdd3d88154cf06cb0759a732790c47b4f7'
+        self.targetInstSrc[ '1.8.0' ] = "libgsasl-1.8.0"
+        self.shortDescription = "GNU SASL is an implementation of the Simple Authentication and Security Layer framework and a few common SASL mechanisms"
+        self.homepage = "http://www.gnu.org/software/gsasl"
+        self.defaultTarget = '1.8.0'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+
+from Package.AutoToolsPackageBase import *
+
+class Package( AutoToolsPackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        AutoToolsPackageBase.__init__(self)
+        self.subinfo.options.configure.defines = " --enable-static --disable-shared "
+        
+        
+if __name__ == '__main__':
+      Package().execute()
