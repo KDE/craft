90a511e1476cba5de465313ea860953defc0d6ef
Patrick von Reth <vonreth@kde.org>
2014-03-31 11:01:15 +0200

    updated quazip

--- portage/qt-libs/quazip/quazip-0.4.4.py
+++ portage/qt-libs/quazip/quazip-0.4.4.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import info
 import os
-from Package.QMakePackageBase import *
+from Package.CMakePackageBase import *
 
 
 class subinfo( info.infoclass ):
@@ -11,18 +11,18 @@ class subinfo( info.infoclass ):
 
     def setTargets( self ):
       self.svnTargets[ 'svnHEAD' ] = 'https://quazip.svn.sourceforge.net/svnroot/quazip/trunk/quazip'
-      self.patchToApply['svnHEAD'] = ('quazip-0.4.4.diff',1)
+      self.patchToApply['svnHEAD'] = ('quazip-20140331.patch',1)
       self.targets['0.4.4'] = 'http://heanet.dl.sourceforge.net/project/quazip/quazip/0.4.4/quazip-0.4.4.zip'
       self.targetDigests['0.4.4'] = 'cfc5ca35ff157e77328fc55de40b73591f425592'
       self.targetInstSrc['0.4.4'] = 'quazip-0.4.4'
       self.patchToApply['0.4.4'] = ('quazip-0.4.4.diff',1)
-      self.defaultTarget = '0.4.4'
+      self.defaultTarget = 'svnHEAD'
 
 
-class Package( QMakePackageBase ):
+class Package( CMakePackageBase ):
     def __init__( self, **args ):
         self.subinfo = subinfo()
-        QMakePackageBase.__init__( self )
+        CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
     Package().execute()
