e23c50f0ca40668c6ea92ad58b2c4fbbc2b8154c
Patrick von Reth <vonreth@kde.org>
2014-03-31 17:55:06 +0200

    changed qt5 target

--- portage/kdesupport/qjson/qjson-20100331.py
+++ portage/kdesupport/qjson/qjson-20100331.py
@@ -16,8 +16,7 @@ class subinfo(info.infoclass):
         self.patchToApply['0.7.1'] = ("qjson-20100517.diff", 1)
         self.targetDigests['0.8.1'] = '197ccfd533f17bcf40428e68a82e6622047ed4ab'
         self.shortDescription = "a qt-based library that maps JSON data to Qt objects"
-        self.defaultTarget = '0.8.1'
-        self.options.configure.defines = ""
+        self.defaultTarget = 'gitHEAD'
 
 from Package.CMakePackageBase import *
 
@@ -25,14 +24,6 @@ class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-
-        self.subinfo.options.configure.defines = ""
-        qmake = os.path.join(self.mergeDestinationDir(), "bin", "qmake.exe")
-        if not os.path.exists(qmake):
-            utils.warning("could not find qmake in <%s>" % qmake)
-        ## \todo a standardized way to check if a package is installed in the image dir would be good.
-        self.subinfo.options.configure.defines += "-DQT_QMAKE_EXECUTABLE:FILEPATH=%s " \
-            % qmake.replace('\\', '/')
 
 if __name__ == '__main__':
     Package().execute()
