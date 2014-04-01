513a13c5bb7bc1e8080bc42f073f3ca80582096d
Patrick von Reth <vonreth@kde.org>
2014-04-01 19:11:27 +0200

    fixed dep

--- portage/kde/kdelibs/kdelibs-20110130.py
+++ portage/kde/kdelibs/kdelibs-20110130.py
@@ -35,7 +35,6 @@ class subinfo(info.infoclass):
         # if self.options.features.nepomuk:
             # self.dependencies['kdesupport/soprano'] = 'default'
             #self.dependencies['kdesupport/strigi'] = 'default'
-        self.dependencies['virtual/kdelibs-base'] = 'default'
         self.dependencies['data/docbook-xsl'] = 'default'
         self.dependencies['data/shared-desktop-ontologies'] = 'default'
         if self.options.features.phononBackend.vlc:
