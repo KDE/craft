22fff77b216d79b7aba0b3d1fbf4e30cabf91211
Patrick von Reth <vonreth@kde.org>
2014-04-01 14:54:56 +0200

    added virtual package for kf5

---
+++ portage/kde/frameworks_tier1/frameworks_tier1-all.py
@@ -0,0 +1,41 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = ''
+        self.defaultTarget = 'gitHEAD'
+        self.shortDescription = 'kf5 tier1'
+
+    def setDependencies( self ):
+        self.dependencies['kde/attica'] = 'default'
+        self.dependencies['kde/karchive'] = 'default'
+        self.dependencies['kde/kcodecs'] = 'default'
+        self.dependencies['kde/kconfig'] = 'default'
+        self.dependencies['kde/kcoreaddons'] = 'default'
+        self.dependencies['kde/kdbusaddons'] = 'default'
+        self.dependencies['kde/kf5umbrella'] = 'default'
+        self.dependencies['kde/kglobalaccel'] = 'default'
+        self.dependencies['kde/kguiaddons'] = 'default'
+        self.dependencies['kde/ki18n'] = 'default'
+        self.dependencies['kde/kidletime'] = 'default'
+        self.dependencies['kde/kimageformats'] = 'default'
+        self.dependencies['kde/kitemmodels'] = 'default'
+        self.dependencies['kde/kitemviews'] = 'default'
+        self.dependencies['kde/kjs'] = 'default'
+        self.dependencies['kde/kplotting'] = 'default'
+        self.dependencies['kde/kwidgetsaddons'] = 'default'
+        self.dependencies['kde/kwindowsystem'] = 'default'
+        self.dependencies['kde/solid'] = 'default'
+        self.dependencies['kde/sonnet'] = 'default'
+        self.dependencies['kde/threadweaver'] = 'default'
+
+
+from Package.VirtualPackageBase import *
+
+class Package( VirtualPackageBase ):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        VirtualPackageBase.__init__( self )
+
+if __name__ == '__main__':
+    Package().execute()
