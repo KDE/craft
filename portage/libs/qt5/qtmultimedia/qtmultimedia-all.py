63e0e44243cb1c0df85e180cf0b2ca5168bf7622
Patrick von Reth <vonreth@kde.org>
2014-04-10 10:07:33 +0200

    added another qt module

--- portage/libs/qt5/qtmultimedia/qtmultimedia-all.py
+++ portage/libs/qt5/qtmultimedia/qtmultimedia-all.py
@@ -1,8 +1,8 @@
+# -*- coding: utf-8 -*-
 import info
 
-
 class subinfo(info.infoclass):
-    def setTargets( self ):
+    def setTargets( self ):       
         self.versionInfo.setupDefaultVersions(__file__)
         for ver in self.versionInfo.tarballs():
             self.targets[ver] = 'http://download.qt-project.org/official_releases/qt/%s/%s/submodules/%s-opensource-src-%s.zip' % ( ver[0:3], ver, self.versionInfo.packageName(), ver)
@@ -16,33 +16,19 @@ class subinfo(info.infoclass):
             self.svnTargets[ver] = '[git]git://gitorious.org/qt/%s.git||%s' % ( self.versionInfo.packageName(), ver)
             
 
+
         self.defaultTarget = self.versionInfo.defaultTarget()
 
     def setDependencies( self ):
         self.dependencies['libs/qtbase'] = 'default'
-        self.dependencies['libs/qtactiveqt'] = 'default'
-        self.dependencies['libs/qtdeclarative'] = 'default'
-        self.dependencies['libs/qtgraphicaleffects'] = 'default'
-        self.dependencies['libs/qtimageformats'] = 'default'
-        self.dependencies['libs/qtmultimedia'] = 'default'
-        self.dependencies['libs/qtquick1'] = 'default'
-        self.dependencies['libs/qtscript'] = 'default'
-        self.dependencies['libs/qtsvg'] = 'default'
-        self.dependencies['libs/qttools'] = 'default'
-        self.dependencies['libs/qtwebkit'] = 'default'
-        self.dependencies['libs/qtxmlpatterns'] = 'default'
-        self.dependencies['libs/qtwinextras'] = 'default'
-        
-        
-        
 
-from Package.VirtualPackageBase import *
 
-class Package( VirtualPackageBase ):
-    def __init__( self ):
+from Package.Qt5CorePackageBase import *
+
+class Package( Qt5CorePackageBase ):
+    def __init__( self, **args ):
         self.subinfo = subinfo()
-        VirtualPackageBase.__init__( self )
-
-
+        Qt5CorePackageBase.__init__( self )
+        
 if __name__ == '__main__':
     Package().execute()
