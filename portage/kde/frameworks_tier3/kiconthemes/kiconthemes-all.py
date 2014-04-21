60fc75153054012d3cb978af065762f4222b93e7
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-04-21 12:23:29 -0300

    Add KIconThemes framework.

---
+++ portage/kde/frameworks_tier3/kiconthemes/kiconthemes-all.py
@@ -0,0 +1,38 @@
+import info
+import kdedefaults as kd
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets["gitHEAD"] = "[git]kde:%s|%s|" % (self.package, kd.kdebranch)
+        self.shortDescription = "TODO"
+        self.defaultTarget = "gitHEAD"
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.buildDependencies["libs/qtbase"] = "default"
+        self.buildDependencies["libs/qtsvg"] = "default"
+        self.buildDependencies["kde/ki18n"] = "default"
+        self.buildDependencies["kde/kconfigwidgets"] = "default"
+        self.buildDependencies["kde/kwidgetsaddons"] = "default"
+        self.buildDependencies["kde/kitemviews"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = ""
+        if compiler.isMinGW():
+          self.subinfo.options.configure.defines += " -DKDE_DISTRIBUTION_TEXT=\"MinGW %s\" " % compiler.getMinGWVersion()
+        elif compiler.isMSVC():
+          self.subinfo.options.configure.defines += " -DKDE_DISTRIBUTION_TEXT=\"%s\" " % compiler.getVersion()
+
+    def install( self ):
+        if not CMakePackageBase.install( self ):
+            return False
+        return True
+
+if __name__ == "__main__":
+    Package().execute()
