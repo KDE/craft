bacac1afe6c46fc0927487d6b0263ad473a6b021
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-04-22 00:10:46 -0300

    Add KNotifications and KWallet frameworks.

---
+++ portage/kde/frameworks_tier3/knotifications/knotifications-all.py
@@ -0,0 +1,39 @@
+import info
+import kdedefaults as kd
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets["gitHEAD"] = "[git]kde:%s|%s|" % (self.package, kd.kdebranch)
+        self.shortDescription = "TODO"
+        self.defaultTarget = "gitHEAD"
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.dependencies["kde/kwindowsystem"] = "default"
+        self.dependencies["kde/kservice"] = "default"
+        self.dependencies["kde/kconfig"] = "default"
+        self.dependencies["kde/kiconthemes"] = "default"
+        self.dependencies["kde/kcodecs"] = "default"
+        self.dependencies["kde/kcoreaddons"] = "default"
+        self.dependencies["kdesupport/phonon"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        self.subinfo = subinfo()
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = ""
+        if compiler.isMinGW():
+          self.subinfo.options.configure.defines += " -DKDE_DISTRIBUTION_TEXT=\"MinGW %s\" " % compiler.getMinGWVersion()
+        elif compiler.isMSVC():
+          self.subinfo.options.configure.defines += " -DKDE_DISTRIBUTION_TEXT=\"%s\" " % compiler.getVersion()
+
+    def install( self ):
+        if not CMakePackageBase.install( self ):
+            return False
+        return True
+
+if __name__ == "__main__":
+    Package().execute()
