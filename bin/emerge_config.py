b034e0b05a5ecef0620a2f07e826faf9a75b7e14
Patrick von Reth <vonreth@kde.org>
2014-04-25 14:31:49 +0200

    added a debugging setting to dump settings state

--- bin/emerge_config.py
+++ bin/emerge_config.py
@@ -30,23 +30,31 @@ class EmergeConfig( object ):
     def __init__( self ):
         self.args = None
         self._config = None
-        iniPath = os.path.join( etcDir(), "kdesettings.ini" )
-        if os.path.exists( iniPath ):
-            self._config = configparser.ConfigParser( )
-            self._config.optionxform = str
-            self._config.read( iniPath )
-            clean = False
-            #replace possible vatiables within a section
-            patern = re.compile("\${.*}")
-            while not clean:
-                clean = True
-                for section in self._config.keys():
-                    for key in self._config[section]:
-                        val = self._config[section][key]
-                        if patern.match(val):
-                            clean = False
-                            match = patern.findall(val)[0]
-                            self._config[section][key] = val.replace(match, self._config[section][match[2:-1]])
+        self.iniPath = os.path.join( etcDir(), "kdesettings.ini" )
+        self._readSettings()
+
+        self.setDefault("General","DUMP_SETTINGS", "False")
+
+    def _readSettings(self):
+        if not os.path.exists( self.iniPath ):
+            print("Could not find %s" % self.iniPath)
+            exit(1)
+        self._config = configparser.ConfigParser( )
+        self._config.optionxform = str
+        self._config.read( self.iniPath )
+        clean = False
+        #replace possible vatiables within a section
+        patern = re.compile("\${.*}")
+        while not clean:
+            clean = True
+            for section in self._config.keys():
+                for key in self._config[section]:
+                    val = self._config[section][key]
+                    if patern.match(val):
+                        clean = False
+                        match = patern.findall(val)[0]
+                        self._config[section][key] = val.replace(match, self._config[section][match[2:-1]])
+
 
     def __contains__( self, key ):
         return self._config and self._config.has_section( key[ 0 ] ) and key[ 1 ] in self._config[ key[ 0 ] ]
@@ -55,15 +63,22 @@ class EmergeConfig( object ):
         if self.__contains__( (group, key) ):
             return self._config[ group ][ key ]
         if default != "_INVALID":
+            self.set(group,key, default)
             return default
         self._config[ group ][ key ]
 
     def set( self, group, key, value ):
         self._config[ group ][ key ] = value
+        print((group ,key ,value))
+        if self.get("General","DUMP_SETTINGS", "False") == "True":
+            print("write " +self.iniPath + ".dump")
+            with open(self.iniPath + ".dump", 'wt+') as configfile:
+                self._config.write(configfile)
+
 
 
     def setDefault( self, group, key, value ):
-        if not self.contains( group, key ):
+        if not ( group, key ) in self:
             self.set( group, key, value )
 
 
