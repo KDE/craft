8b0ef9183e3273e32a261e5d5e81be0852c2b16a
Patrick von Reth <vonreth@kde.org>
2014-04-28 17:53:07 +0200

    use versioninfo for boost

--- portage/win32libs/boost/boost-random/boost-random.py
+++ portage/win32libs/boost/boost-random/boost-random.py
@@ -1,10 +1,7 @@
 import info
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['1.44.0', '1.47.0', '1.48.0', '1.49.0', '1.50.0', '1.52.0', '1.54.0','1.55.0']:
-            self.targets[ver] = ''
-            self.targetInstSrc[ver] = 'random'
-        self.defaultTarget = '1.55.0'
+        self.versionInfo.setDefaultVersions("", tarballInstallSrc = self.package.replace("boost-","").replace("-", "_"))
 
 
         self.homepage = 'http://www.boost.org/'
--- portage/win32libs/boost/boost.py
+++ portage/win32libs/boost/boost.py
@@ -2,9 +2,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['1.44.0', '1.47.0', '1.48.0', '1.49.0', '1.50.0', '1.52.0', '1.54.0','1.55.0']:
-            self.svnTargets[ver] = ''
-        self.defaultTarget = '1.55.0'
+        self.versionInfo.setDefaultVersions("")
 
         self.homepage = 'http://www.boost.org/'
 
