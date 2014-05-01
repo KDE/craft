981ac49aec354a9101e424401229e6ef61941031
Alex Richardson <arichardson.kde@gmail.com>
2014-05-02 00:39:52 +0200

    add missing build dependency libs/qtbase to attica

--- portage/kde/frameworks_tier1/attica/attica-20140309.py
+++ portage/kde/frameworks_tier1/attica/attica-20140309.py
@@ -10,6 +10,7 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.buildDependencies["virtual/base"] = "default"
         self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.buildDependencies["libs/qtbase"] = "default"
         self.buildDependencies["win32libs/automoc"] = "default"
 
 from Package.CMakePackageBase import *
