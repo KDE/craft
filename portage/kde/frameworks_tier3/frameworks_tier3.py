22e2e546a0b7728f332f339522c315aa6f398362
Kevin Funk <kfunk@kde.org>
2014-05-05 19:55:48 +0200

    Update frameworks_tier3 pkg

--- portage/kde/frameworks_tier3/frameworks_tier3.py
+++ portage/kde/frameworks_tier3/frameworks_tier3.py
@@ -4,25 +4,27 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = ''
         self.defaultTarget = 'gitHEAD'
-        self.shortDescription = 'kf5 tier1'
+        self.shortDescription = 'kf5 tier3'
 
     def setDependencies( self ):
         self.dependencies['kde/kbookmarks'] = 'default'
+        self.dependencies['kde/kcmutils'] = 'default'
         self.dependencies['kde/kconfigwidgets'] = 'default'
         self.dependencies['kde/kiconthemes'] = 'default'
+        self.dependencies['kde/kinit'] = 'default'
         self.dependencies['kde/kio'] = 'default'
+        self.dependencies['kde/knewstuff'] = 'default'
         self.dependencies['kde/knotifications'] = 'default'
+        self.dependencies['kde/knotifyconfig'] = 'default'
         self.dependencies['kde/kparts'] = 'default'
         self.dependencies['kde/kservice'] = 'default'
+        self.dependencies['kde/ktexteditor'] = 'default'
         self.dependencies['kde/ktextwidgets'] = 'default'
         self.dependencies['kde/kwallet'] = 'default'
         self.dependencies['kde/kxmlgui'] = 'default'
-        
-
 
 from Package.VirtualPackageBase import *
 
 class Package( VirtualPackageBase ):
     def __init__( self ):
         VirtualPackageBase.__init__( self )
-
