21ed591bc0a47a2caec2f8c1cf220f468e5fe2d0
Kevin Funk <kfunk@kde.org>
2014-05-06 00:46:05 +0200

    kdevplatform: Base on frameworks branch

--- portage/extragear/kdevplatform/kdevplatform.py
+++ portage/extragear/kdevplatform/kdevplatform.py
@@ -2,15 +2,23 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets[ 'gitHEAD' ] = '[git]kde:kdevplatform'
+        self.svnTargets[ 'gitHEAD' ] = '[git]kde:kdevplatform|frameworks'
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
-        self.dependencies[ 'kde/kde-runtime' ] = 'default'
-        self.dependencies[ 'win32libs/boost' ] = 'default'
-        self.dependencies[ 'kdesupport/qjson' ] = 'default'
-        self.buildDependencies[ 'dev-util/gettext-tools' ] = 'default'
-        self.buildDependencies[ 'dev-util/zip' ] = 'default'
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.buildDependencies["libs/qtbase"] = "default"
+        self.dependencies[ 'kde/karchive' ] = 'default'
+        self.dependencies[ 'kde/kcmutils' ] = 'default'
+        self.dependencies[ 'kde/kconfig' ] = 'default'
+        self.dependencies[ 'kde/kguiaddons' ] = 'default'
+        self.dependencies[ 'kde/knewstuff' ] = 'default'
+        self.dependencies[ 'kde/knotifyconfig' ] = 'default'
+        self.dependencies[ 'kde/ki18n' ] = 'default'
+        self.dependencies[ 'kde/kitemmodels' ] = 'default'
+        self.dependencies[ 'kde/kitemviews' ] = 'default'
+        self.dependencies[ 'kde/kxmlgui' ] = 'default'
 
 from Package.CMakePackageBase import *
 
