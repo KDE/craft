0851e5b5e4cffabd46df5ade1c754f8f99b2b7c4
Patrick von Reth <vonreth@kde.org>
2014-05-06 14:18:17 +0200

    use setDefaultVersions for kate

--- portage/kde/kate/kate.py
+++ portage/kde/kate/kate.py
@@ -1,17 +1,15 @@
 import info
-import kdedefaults as kd
 from EmergeConfig import *
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|frameworks|' % (self.package)
-        for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
-            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-            self.targetDigestUrls[ kd.kdeversion + ver  ] = 'http://download.kde.org/stable/' + kd.kdeversion + ver + '/src/' + self.package + '-' + kd.kdeversion + ver + '.tar.xz.sha1'
+        self.versionInfo.setDefaultVersions("http://download.kde.org/unstable/frameworks/${VERSION}/${PACKAGE_NAME}-${VERSION}.tar.xz",
+                                            "http://download.kde.org/unstable/frameworks/${VERSION}/${PACKAGE_NAME}-${VERSION}.tar.xz.sha1",
+                                            "${PACKAGE_NAME}-${VERSION}",
+                                            "[git]kde:${PACKAGE_NAME}" )
 
         self.shortDescription = "the KDE text editor"
-        self.defaultTarget = 'gitHEAD'
+        self.defaultTarget = 'frameworks'
 
     def setDependencies( self ):
         self.buildDependencies["virtual/base"] = "default"
