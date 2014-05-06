b4c6a822af1ffab580407e72eb04d94c7fa32403
Patrick von Reth <vonreth@kde.org>
2014-05-06 11:23:32 +0200

    there is no qtdbus package

--- portage/kde/frameworks_tier3/kiconthemes/kiconthemes.py
+++ portage/kde/frameworks_tier3/kiconthemes/kiconthemes.py
@@ -15,7 +15,6 @@ class subinfo(info.infoclass):
         self.buildDependencies["virtual/base"] = "default"
         self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
         self.dependencies["libs/qtbase"] = "default"
-        self.dependencies["libs/qtdbus"] = "default"
         self.dependencies["libs/qtsvg"] = "default"
         self.dependencies["kde/ki18n"] = "default"
         self.dependencies["kde/kconfigwidgets"] = "default"
