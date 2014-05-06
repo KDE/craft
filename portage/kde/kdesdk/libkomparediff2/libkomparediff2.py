e462075af1c8a1843600e8221ab51bb98817656c
Kevin Funk <kfunk@kde.org>
2014-05-06 09:24:39 +0200

    Port libkomparediff2, hardcode frameworks branch

--- portage/kde/kdesdk/libkomparediff2/libkomparediff2.py
+++ portage/kde/kdesdk/libkomparediff2/libkomparediff2.py
@@ -4,7 +4,7 @@ from EmergeConfig import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
+        self.svnTargets['gitHEAD'] = '[git]kde:%s|frameworks|' % (self.package)
         for ver in ['0', '1', '2', '3', '4', '5']:
             # let's see if this is the URL from 4.12 on...
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
@@ -12,14 +12,21 @@ class subinfo(info.infoclass):
             self.targetDigestUrls[ kd.kdeversion + ver  ] = 'http://download.kde.org/stable/' + kd.kdeversion + ver + '/src/' + self.package + '-' + kd.kdeversion + ver + '.tar.xz.sha1'
 
         self.defaultTarget = 'gitHEAD'
+        self.shortDescription = "Library to compare files and strings, used in Kompare and KDevelop"
 
     def setDependencies( self ):
-        self.dependencies['kde/kdelibs'] = 'default'
-        self.shortDescription = "Library to compare files and strings, used in Kompare and KDevelop"
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.buildDependencies["win32libs/automoc"] = "default"
+        self.dependencies["libs/qtbase"] = "default"
+        self.dependencies["kde/kcodecs"] = "default"
+        self.dependencies["kde/kcoreaddons"] = "default"
+        self.dependencies["kde/ki18n"] = "default"
+        self.dependencies["kde/kio"] = "default"
+        self.dependencies["kde/kxmlgui"] = "default"
 
 from Package.CMakePackageBase import *
 
 class Package(CMakePackageBase):
     def __init__( self ):
         CMakePackageBase.__init__(self)
-
