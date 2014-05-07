e35f4801725c5d5f0618847692c361134fbbe146
Patrick von Reth <vonreth@kde.org>
2014-05-07 16:07:17 +0200

    revert update of pkg-config as the new packages causes problems

--- portage/dev-util/pkg-config/pkg-config.py
+++ portage/dev-util/pkg-config/pkg-config.py
@@ -1,20 +1,19 @@
 from Package.BinaryPackageBase import *
 import info
 
-
-class subinfo( info.infoclass ):
+class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets[ "0.28" ] = "http://downloads.sourceforge.net/msys2/mingw-w64-i686-pkgconf-0.9.5-1-any.pkg.tar.xz"
-        self.targetDigests[ "0.28" ] = "8fe89c3ac4217beffbb942a2e9da3597d30226e8"
-        self.targetInstSrc[ "0.28" ] = "mingw32"
-        self.defaultTarget = "0.28"
+        #from http://kemovitra.blogspot.com/2009/07/mingw-building-static-pkg-config.html
+        self.targets[ '0.23' ]          = 'http://downloads.sourceforge.net/kde-windows/pkg-config.exe'
+        self.targetDigests['0.23'] = '4a00011073670fc68817d730db0f2187f6343d8c'
+        self.defaultTarget = '0.23'
 
     def setDependencies( self ):
-        self.dependencies[ 'virtual/bin-base' ] = 'default'
+        self.dependencies['virtual/bin-base'] = 'default'
 
 
-class Package( BinaryPackageBase ):
-    def __init__( self ):
-        BinaryPackageBase.__init__( self )
-        self.subinfo.options.merge.destinationPath = 'dev-utils'
+class Package(BinaryPackageBase):
+  def __init__(self):
+    BinaryPackageBase.__init__( self )
+    self.subinfo.options.merge.destinationPath = 'dev-utils/bin'
 
