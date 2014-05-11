5a439b0bb5db74c2f44607122b7e31f4cf78585e
Patrick von Reth <vonreth@kde.org>
2014-05-11 18:37:03 +0200

    updated ffmpge

--- portage/win32libs/ffmpeg/ffmpeg.py
+++ portage/win32libs/ffmpeg/ffmpeg.py
@@ -7,14 +7,15 @@ import compiler
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = "git://git.videolan.org/ffmpeg.git"
-        for ver in ["0.8.6", "0.11.2",  "1.1.3", "2.0.1"]:
+        for ver in ["0.8.6", "0.11.2",  "1.1.3", "2.0.1", "2.2.2"]:
                 self.targets[ ver ] = "http://ffmpeg.org/releases/ffmpeg-%s.tar.bz2" % ver 
                 self.targetInstSrc[ ver ] = "ffmpeg-%s" % ver
         self.targetDigests['0.8.6'] = 'ad7eaefa5072ca3c11778f9186fab35558a04478'
         self.targetDigests['0.11.2'] = '5d98729b8368df8145472ae6955ef8d6b9ed0efb'
         self.targetDigests['1.1.3'] = 'd82d6f53c5130ee21dcb87f76bdbdf768d3f0db9'
+        self.targetDigests['2.2.2'] = '8a4f282ccb5efbec31a9747d12c8d7b07c481f2e'
         
-        self.defaultTarget = "2.0.1"
+        self.defaultTarget = "2.2.2"
 
 
     def setDependencies( self ):
@@ -30,26 +31,21 @@ class subinfo(info.infoclass):
 from Package.AutoToolsPackageBase import *
 from Package.VirtualPackageBase import *
 
-class PackageMinGW(AutoToolsPackageBase):
+class Package(AutoToolsPackageBase):
     def __init__( self, **args ):
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.package.withCompiler = False
         self.platform = ""
         self.subinfo.options.configure.defines = " --disable-static --enable-shared --enable-gpl --enable-libvorbis  --disable-doc  --enable-libmp3lame --enable-libopus --enable-w32threads"
+        if compiler.isMSVC():
+            self.subinfo.options.configure.defines += " --toolchain=msvc"
         
     def configure( self):
-        return AutoToolsPackageBase.configure( self, cflags="-std=c99 ", ldflags="")
+        cflags =""
+        if compiler.isMinGW():
+            cflags="-std=c99 "
+        return AutoToolsPackageBase.configure( self, cflags = cflags , ldflags="")
         
     def ccacheOptions(self):
         return " --cc='ccache gcc' --cxx='ccache g++' "
-
-
-if compiler.isMinGW():
-    class Package(PackageMinGW):
-        def __init__( self ):
-            PackageMinGW.__init__( self )
-else:
-    class Package(VirtualPackageBase):
-        def __init__( self ):
-            VirtualPackageBase.__init__( self )
 
