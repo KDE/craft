a593925209b537bf118e3c2dcba5bca8267c7b30
Patrick Spendrin <ps_ml@gmx.de>
2014-05-12 23:40:44 +0200

    split patches and cleanup

--- portage/win32libs/liblzma/liblzma.py
+++ portage/win32libs/liblzma/liblzma.py
@@ -12,7 +12,7 @@ class subinfo(info.infoclass):
         ver = '5.0.5'
         self.targets[ver] = 'http://tukaani.org/xz/xz-' + ver + '.tar.xz'
         self.targetInstSrc[ver] = 'xz-' + ver
-        self.patchToApply[ver] = ('xz-5.0.5.diff',1)
+        self.patchToApply[ver] = [('xz-5.0.5.diff', 1), ('xz-cmake-5.0.5.diff', 1)]
         self.targetDigests['5.0.5'] = '56f1d78117f0c32bbb1cfd40117aa7f55bee8765'
 
         self.shortDescription = "free general-purpose data compression software with high compression ratio"
@@ -27,8 +27,3 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self ):
         CMakePackageBase.__init__( self )
-        self.subinfo.options.package.withCompiler = False
-
-    def createPackage( self ):
-        shutil.copy( os.path.join( self.imageDir() , "lib" ,"liblzma.dll.a" ) , os.path.join( self.imageDir() , "lib" ,"liblzma.lib" ))
-        return KDEWinPackager.createPackage( self )
