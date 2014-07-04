7bfc35ef906028b5f730a7c46217bad6f1a9b34e
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-07-04 15:27:54 -0300

    Fix build of GLEW library.
    MSVC2012 and later complain about the string literals in the .rc files
    being too long. I added a patch that splits the string literals into
    smaller pieces. This was partially fixed upstream and will be in 1.10.1.

--- portage/win32libs/glew/glew.py
+++ portage/win32libs/glew/glew.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
         self.patchToApply['1.5.4'] = [('glew-1.5.4-20100708.diff', 1)]
         self.patchToApply['1.7.0'] = [('glew-1.7.0-20120320.diff', 1)]
         self.patchToApply['1.9.0'] = [('glew-1.9.0-20130124.diff', 1)]
-        self.patchToApply['1.10.0'] = [('glew-1.9.0-20130124.diff', 1)]
+        self.patchToApply['1.10.0'] = [('glew-1.9.0-20130124.diff', 1),('split-string-literals-in-rc-files.diff',1)]
         self.targetDigests['1.7.0'] = '107c155ff5b69d97b9c530b40e4e8da571aaf729'
         self.targetDigests['1.9.0'] = '6c0dd8d6af14db93868bb5482b55e784b2dc1127'
         self.targetDigests['1.10.0'] = 'da45a883ca9b2a8e8fc1a642bd043f251ad69151'
