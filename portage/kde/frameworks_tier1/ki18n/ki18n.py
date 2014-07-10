d0b3b877a4daded644cdc9df5d2e135d422472b3
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-07-09 22:02:43 -0300

    Add missing dependency to ki18n: gettext library.

--- portage/kde/frameworks_tier1/ki18n/ki18n.py
+++ portage/kde/frameworks_tier1/ki18n/ki18n.py
@@ -11,9 +11,10 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.buildDependencies["virtual/base"] = "default"
         self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
-        self.buildDependencies[ "dev-util/gettext-tools" ] = "default"
+        self.buildDependencies["dev-util/gettext-tools"] = "default"
         self.dependencies["libs/qtbase"] = "default"
         self.dependencies["libs/qtscript"] = "default"
+        self.dependencies["win32libs/gettext"] = "default"
 
 from Package.CMakePackageBase import *
 
