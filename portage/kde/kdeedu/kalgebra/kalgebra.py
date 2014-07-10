133039066b21b8b975b8f8f18f0e6873bc22f6d6
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-07-10 16:23:02 -0300

    Update KAlgebra package for the frameworks branch.
    Removed kde-runtime and libkdeedu deps, added Qt and KF5.
    Removed tarball URLs, used setDefaultValues instead.

--- portage/kde/kdeedu/kalgebra/kalgebra.py
+++ portage/kde/kdeedu/kalgebra/kalgebra.py
@@ -4,19 +4,21 @@ from EmergeConfig import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
-        for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
-            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-            self.targetDigestUrls[ kd.kdeversion + ver  ] = 'http://download.kde.org/stable/' + kd.kdeversion + ver + '/src/' + self.package + '-' + kd.kdeversion + ver + '.tar.xz.sha1'
+        self.versionInfo.setDefaultValues()
 
         self.shortDescription = 'a graph calculator'
-        self.defaultTarget = 'gitHEAD'
+        self.defaultTarget = 'frameworks'
 
     def setDependencies( self ):
-        self.dependencies['kde/kde-runtime'] = 'default'
+        self.dependencies['libs/qtbase'] = 'default'
+        self.dependencies['libs/qtdeclarative'] = 'default'
+        self.dependencies['libs/qtwebkit'] = 'default'
         self.dependencies['kde/analitza'] = 'default'
-        self.dependencies['kde/libkdeedu'] = 'default'
+        self.dependencies['kde/ki18n'] = 'default'
+        self.dependencies['kde/kconfigwidgets'] = 'default'
+        self.dependencies['kde/kwidgetsaddons'] = 'default'
+        self.dependencies['kde/kio'] = 'default'
+        self.dependencies['kde/kdoctools'] = 'default'
 
 from Package.CMakePackageBase import *
 
