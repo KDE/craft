6043b2d2076a152675ff87cfce4abb80a0fcea2f
Patrick von Reth <vonreth@kde.org>
2014-07-16 18:07:00 +0200

    update

--- portage/win32libs/cryptopp/cryptopp.py
+++ portage/win32libs/cryptopp/cryptopp.py
@@ -3,12 +3,16 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets[ '561' ] = 'http://www.cryptopp.com/cryptopp561.zip'  
+        self.targets[ '561' ] = 'http://www.cryptopp.com/cryptopp561.zip'
         self.patchToApply[ '561' ] = [('cmake.diff', 1),
                                       ('workaround_ice_31690.diff', 1),("cryptopp-5.6.1-gcc-4.7.0.patch",1)]
-        self.targetDigests['561'] = '31dbb456c21f50865218c57b7eaf4c955a222ba1'       
+        self.targets[ '562' ] = 'http://www.cryptopp.com/cryptopp562.zip'
+        self.patchToApply[ '562' ] = [('cmake.diff', 1)]
+        self.targetDigests['561'] = '31dbb456c21f50865218c57b7eaf4c955a222ba1'
+        self.targetDigests['562'] = 'ddc18ae41c2c940317cd6efe81871686846fa293'
         self.shortDescription = "Crypto++ Library is a free C++ class library of cryptographic schemes"
-        self.defaultTarget = '561'
+        self.homepage = "http://www.cryptopp.com/"
+        self.defaultTarget = '562'
 
 
     def setDependencies( self ):
