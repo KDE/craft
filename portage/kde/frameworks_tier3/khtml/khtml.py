5d98e2268522a03a34380444469382135095a0aa
Cristian Oneț <onet.cristian@gmail.com>
2014-07-19 01:22:45 +0300

    Add khtml since it's needed by KMyMoney.
    REVIEW: 119361

---
+++ portage/kde/frameworks_tier3/khtml/khtml.py
@@ -0,0 +1,35 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "KHTML APIs"
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.dependencies["win32libs/giflib"] = "default"
+        self.dependencies["win32libs/jpeg"] = "default"
+        self.dependencies["libs/qtbase"] = "default"
+        self.dependencies["kde/karchive"] = "default"
+        self.dependencies["kde/kcodecs"] = "default"
+        self.dependencies["kde/kglobalaccel"] = "default"
+        self.dependencies["kde/kiconthemes"] = "default"
+        self.dependencies["kde/ki18n"] = "default"
+        self.dependencies["kde/kio"] = "default"
+        self.dependencies["kde/kjs"] = "default"
+        self.dependencies["kde/knotifications"] = "default"
+        self.dependencies["kde/kparts"] = "default"
+        self.dependencies["kde/ktextwidgets"] = "default"
+        self.dependencies["kde/kwallet"] = "default"
+        self.dependencies["kde/kwidgetsaddons"] = "default"
+        self.dependencies["kde/sonnet"] = "default"
+        self.dependencies["kde/kxmlgui"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
