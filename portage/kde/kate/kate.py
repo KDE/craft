4de05e845374d4df9e4b053dfa04ab732a370d6a
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-07-18 21:25:26 -0300

    Update dependencies of kate.

--- portage/kde/kate/kate.py
+++ portage/kde/kate/kate.py
@@ -12,7 +12,7 @@ class subinfo( info.infoclass ):
         self.buildDependencies["virtual/base"] = "default"
         self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
         self.buildDependencies["libs/qtbase"] = "default"
-        self.dependencies["kde/kconfigwidgets"] = "default"
+        self.dependencies["kde/kconfig"] = "default"
         self.dependencies["kde/kdoctools"] = "default"
         self.dependencies["kde/kguiaddons"] = "default"
         self.dependencies["kde/ki18n"] = "default"
@@ -23,6 +23,8 @@ class subinfo( info.infoclass ):
         self.dependencies["kde/ktexteditor"] = "default"
         self.dependencies["kde/kwindowsystem"] = "default"
         self.dependencies["kde/kxmlgui"] = "default"
+        self.dependencies["kde/kdbusaddons"] = "default"
+        self.dependencies["kde/kitemmodels"] = "default"
 
 from Package.CMakePackageBase import *
 
