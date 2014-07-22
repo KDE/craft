bed0d30d15b8a18ae7a560d72d85d12e8515d5e8
Patrick von Reth <vonreth@kde.org>
2014-07-22 16:45:07 +0200

    updated crt too

--- portage/dev-util/mingw-w64-crt/mingw-w64-crt.py
+++ portage/dev-util/mingw-w64-crt/mingw-w64-crt.py
@@ -3,9 +3,8 @@ from shells import *
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets['svnHEAD'] = 'https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64/trunk/mingw-w64-crt'
-  
-        self.defaultTarget = 'svnHEAD'
+        self.svnTargets['gitHEAD'] = 'git://git.code.sf.net/p/mingw-w64/mingw-w64'
+        self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.buildDependencies['dev-util/msys'] = 'default'
@@ -17,18 +16,13 @@ from Package.AutoToolsPackageBase import *
 class Package( AutoToolsPackageBase ):
     def __init__( self ):
         AutoToolsPackageBase.__init__( self )
+        self.supportsCCACHE = False
         if compiler.isX64():
-            disable = "--disable-lib32"
+            disable = "--disable-lib32 --enable-lib64"
             self.subinfo.options.merge.destinationPath = 'mingw64/x86_64-w64-mingw32'
         else:
-            disable = "--disable-lib64"
+            disable = "--disable-lib64 --enable-lib32"
             self.subinfo.options.merge.destinationPath = 'mingw/i686-w64-mingw32'
-        self.subinfo.options.configure.defines = " --with-sysroot=%s  %s " % (MSysShell().toNativePath(self.mergeDestinationDir()),disable)
+        self.subinfo.options.configure.defines = " --with-sysroot=%s --enable-wildcard --without-headers  %s  " % (self.shell.toNativePath(self.mergeDestinationDir()),disable)
 
-    def install( self ):
-        if not AutoToolsPackageBase.install( self ):
-            return False
-        if compiler.isX64():
-            shutil.move( os.path.join( self.installDir() , "lib64" ) , os.path.join( self.installDir(), "lib" ) )
-        return True
 
