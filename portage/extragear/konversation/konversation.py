a4734f96e2af84c1953b01190840c07d7ce863d0
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-07-31 12:55:08 -0300

    Update konversation package for KF5.

--- portage/extragear/konversation/konversation.py
+++ portage/extragear/konversation/konversation.py
@@ -5,25 +5,28 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['1.2.1', '1.2.2', '1.2.3', '1.3', '1.3.1']:
-            self.targets[ver] = 'http://download.kde.org/stable/konversation/' + ver + '/src/konversation-' + ver + '.tar.bz2'
-            self.targetInstSrc[ver] = 'konversation-' + ver
-        for ver in ['1.4', '1.5']:
-            self.targets[ver] = 'http://download.kde.org/stable/konversation/' + ver + '/src/konversation-' + ver + '.tar.xz'
-            self.targetInstSrc[ver] = 'konversation-' + ver
-        for ver in ['1.5-d938500c']:
-            self.targets[ver] = 'http://www.winkde.org/pub/kde/ports/win32/repository/other/konversation-' + ver + '.tar.xz'
-            self.targetInstSrc[ver] = 'konversation-' + ver
-        self.patchToApply['1.3.1'] = ("konversation-1.3.1-20110822.diff", 1)
-        self.patchToApply['1.4'] = [("konversation-1.4-20130305.diff", 1)]
-        self.patchToApply['1.5-d938500c'] = [("konversation-1.5-d938500c-20130421.diff", 1), ("fix-opening-of-urls-on-windows.diff", 1)]
-        self.svnTargets['gitHEAD'] = '[git]kde:konversation'
-        self.defaultTarget = 'gitHEAD'
+        self.svnTargets['frameworks'] = '[git]kde:konversation|frameworks'
+        self.defaultTarget = 'frameworks'
 
     def setDependencies( self ):
-        self.dependencies['kde/kde-runtime'] = 'default'
+        self.dependencies['libs/qtbase'] = 'default'
+        self.dependencies['kde/karchive'] = 'default'
+        self.dependencies['kde/kbookmarks'] = 'default'
+        self.dependencies['kde/kconfig'] = 'default'
+        self.dependencies['kde/kconfigwidgets'] = 'default'
+        self.dependencies['kde/kemoticons'] = 'default'
+        self.dependencies['kde/ki18n'] = 'default'
+        self.dependencies['kde/kidletime'] = 'default'
+        self.dependencies['kde/knotifyconfig'] = 'default'
+        self.dependencies['kde/kdelibs4support'] = 'default'
+        self.dependencies['kde/kio'] = 'default'
+        self.dependencies['kde/kparts'] = 'default'
+        self.dependencies['kde/solid'] = 'default'
+        self.dependencies['kde/sonnet'] = 'default'
+        self.dependencies['kde/kwallet'] = 'default'
+        self.dependencies['kde/kwidgetsaddons'] = 'default'
+        self.dependencies['kdesupport/phonon'] = 'default'
         self.dependencies['kdesupport/qca'] = 'default'
-        self.dependencies['kde/kdepimlibs'] = 'default'
         self.shortDescription = "a KDE based irc client"
 
 class Package(CMakePackageBase):
