2e7bcdd08c9a481ce8ec7175a327e8a8d2a47811
Patrick von Reth <vonreth@kde.org>
2014-08-11 17:23:35 +0200

    updated deps

--- portage/kdesupport/snorenotify/snorenotify.py
+++ portage/kdesupport/snorenotify/snorenotify.py
@@ -5,18 +5,22 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setDependencies( self ):
-      self.shortDescription = "An application to show kde notifications with Snarl/Growl"
-      self.dependencies['libs/qtbase'] = 'default'
+        self.shortDescription = "An application to show kde notifications with Snarl/Growl"
+        self.dependencies['libs/qtbase'] = 'default'
+        self.dependencies['kde/ki18n'] = 'default'
+        self.dependencies['kde/kiconthemes'] = 'default'
+        self.dependencies['kde/kcoreaddons'] = 'default'
+
 
 
     def setTargets( self ):
-      self.svnTargets['gitHEAD'] = 'https://github.com/Snorenotify/Snorenotify.git'
-      for ver in ['0.2','0.3','0.4-beta1','0.4-beta2']:
-          self.targets[ver] = 'https://github.com/TheOneRing/Snorenotify/archive/v%s.tar.gz' % ver
-          self.archiveNames[ver] = "snorenotify-%s.tar.gz" % ver
-          self.targetInstSrc[ver] = 'Snorenotify-%s' % ver
-      self.targetDigests['0.4-beta1'] = '76996e2ebd23fa7b99b68e743c880b3fa0d724af'
-      self.defaultTarget = 'gitHEAD'
+        self.svnTargets['gitHEAD'] = 'https://github.com/Snorenotify/Snorenotify.git'
+        for ver in ['0.2','0.3','0.4-beta1','0.4-beta2']:
+            self.targets[ver] = 'https://github.com/TheOneRing/Snorenotify/archive/v%s.tar.gz' % ver
+            self.archiveNames[ver] = "snorenotify-%s.tar.gz" % ver
+            self.targetInstSrc[ver] = 'Snorenotify-%s' % ver
+        self.targetDigests['0.4-beta1'] = '76996e2ebd23fa7b99b68e743c880b3fa0d724af'
+        self.defaultTarget = 'gitHEAD'
 
 
 class Package( CMakePackageBase ):
