f9f56fd196f955cffd7e46ffda30671fe7ed817e
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-08-12 00:08:07 +0200

    Update KGeography for KF5.

--- portage/kde/kdeedu/kgeography/kgeography.py
+++ portage/kde/kdeedu/kgeography/kgeography.py
@@ -4,17 +4,22 @@ from EmergeConfig import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
-        for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
-            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-            self.targetDigestUrls[ kd.kdeversion + ver  ] = 'http://download.kde.org/stable/' + kd.kdeversion + ver + '/src/' + self.package + '-' + kd.kdeversion + ver + '.tar.xz.sha1'
+        self.svnTargets['frameworks'] = '[git]kde:kgeography|frameworks'
 
         self.shortDescription = 'a geography trainer'
-        self.defaultTarget = 'gitHEAD'
+        self.defaultTarget = 'frameworks'
 
     def setDependencies( self ):
-        self.dependencies['kde/kde-runtime'] = 'default'
+        self.dependencies['libs/qtbase'] = 'default'
+        self.dependencies['kde/kxmlgui'] = 'default'
+        self.dependencies['kde/kwidgetsaddons'] = 'default'
+        self.dependencies['kde/kcoreaddons'] = 'default'
+        self.dependencies['kde/kconfigwidgets'] = 'default'
+        self.dependencies['kde/ki18n'] = 'default'
+        self.dependencies['kde/kitemviews'] = 'default'
+        self.dependencies['kde/kiconthemes'] = 'default'
+        self.dependencies['kde/kservice'] = 'default'
+        self.dependencies['kde/kdoctools'] = 'default'
 
 from Package.CMakePackageBase import *
 
