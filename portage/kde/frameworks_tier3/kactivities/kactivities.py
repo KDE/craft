2904956c44b7ad72f367a01bdd265b61a437a1f8
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-08-11 11:11:45 +0200

    Build only the KActivities library, not the service or QML imports.
    Fixes build on MSVC, and I think we don't really need the service on
    Windows.

--- portage/kde/frameworks_tier3/kactivities/kactivities.py
+++ portage/kde/frameworks_tier3/kactivities/kactivities.py
@@ -23,6 +23,7 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self ):
         CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DKACTIVITIES_LIBRARY_ONLY=YES"
 
 
     
