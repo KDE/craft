cab6e3eae0af417c123dae280d10e91564c9a202
Patrick von Reth <vonreth@kde.org>
2014-08-11 17:20:54 +0200

    updated options

--- portage/kdesupport/snorenotify/snorenotify.py
+++ portage/kdesupport/snorenotify/snorenotify.py
@@ -22,5 +22,6 @@ class subinfo(info.infoclass):
 class Package( CMakePackageBase ):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.defines = ' -DWITH_KDE=OFF -DWITH_QT4=OFF -DWITH_FREEDESKTOP_FRONTEND=ON -DWITH_SNORE_DEAMON=ON '
+        self.subinfo.options.configure.defines = '-DWITH_QT4=OFF -DWITH_FREEDESKTOP_FRONTEND=ON -DWITH_SNORE_DEAMON=ON '
+        self.subinfo.options.fetch.checkoutSubmodules = True
 
