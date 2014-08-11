634a32991951afc04a2957a701202d5e39aac9c6
Cristian Oneț <onet.cristian@gmail.com>
2014-08-11 15:20:28 +0300

    Add the latest mpir target and remove the old ones.

--- portage/win32libs/mpir/mpir.py
+++ portage/win32libs/mpir/mpir.py
@@ -2,21 +2,21 @@ import os
 
 import info
 
+import utils
+
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['2.3.0', '2.5.0', '2.6.0']:
+        for ver in ['2.7.0-alpha10']:
             self.targets[ver] = 'http://www.mpir.org/mpir-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = "mpir-" + ver
-        self.targetDigests['2.3.0'] = 'e359e8d7417e4ffef40ba975409c306ac4c381e5'
-        self.patchToApply['2.3.0'] = ('mpir-2.3.0-20110310.diff', 1)
-        self.targetDigests['2.5.0'] = 'de6adf9c5318dfba52b29d1700812069cfb6be39'
-        self.patchToApply['2.5.0'] = ('mpir-2.5.0-20120201.diff', 1)
         self.targetDigests['2.6.0'] = '28a91eb4d2315a9a73dc39771acf2b99838b9d72'
         self.patchToApply['2.6.0'] = ('mpir-2.6.0-20131003.diff', 1)
+        self.targetDigests['2.7.0-alpha10'] = '815fee928c9ba5f444457144dd037300cb8f4ca4'
+        self.targetInstSrc['2.7.0-alpha10'] = 'mpir-2.7.0'
 
         self.shortDescription = "Library for arbitrary precision integer arithmetic derived from version 4.2.1 of gmp"
-        self.defaultTarget = '2.6.0'
+        self.defaultTarget = '2.7.0-alpha10'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -42,34 +42,47 @@ class PackageMSVC(MakeFilePackageBase):
             MakeFilePackageBase.__init__( self )
             
     def configure( self ):
-        os.chdir( os.path.join( self.sourceDir(), 'build.vc10') )
+        os.chdir( os.path.join( self.sourceDir(), 'build.vc12') )
         os.putenv('YASMPATH', os.path.join(self.rootdir, 'dev-utils', 'bin'))
-        self.system("configure.bat --enable-shared --cpu-none --ABI32")
         return True
 
     def make( self ):
-        os.chdir( os.path.join( self.sourceDir(), 'build.vc10') )
-        self.system("make.bat")
-        return True
+        os.chdir( os.path.join( self.sourceDir(), 'build.vc12') )
+        if self.buildType() == "Debug":
+            bt = "Debug"
+        else:
+            bt = "Release"
+
+        toolsetSwitches = ""
+        if compiler.isMSVC2012():
+            toolsetSwitches = "/property:PlatformToolset=v110"
+        elif compiler.isMSVC2013():
+            toolsetSwitches = "/tv:12.0 /property:PlatformToolset=v120"
+
+        return utils.system("msbuild /target:lib_mpir_gc \"%s\" /p:Configuration=%s %s" %
+                (os.path.join(self.sourceDir(), "build.vc12", "mpir.sln"), bt, toolsetSwitches)
+        ) and utils.system("msbuild /target:dll_mpir_gc \"%s\" /p:Configuration=%s %s" %
+                (os.path.join(self.sourceDir(), "build.vc12", "mpir.sln"), bt, toolsetSwitches)
+        )
 
     def install( self ):
         if not os.path.isdir( os.path.join( self.installDir() , "bin" ) ):
                 os.makedirs( os.path.join( self.installDir() , "bin" ) )
-        shutil.move(os.path.join( self.sourceDir(), 'build.vc10', 'dll', 'Win32', 'Release', 'mpir.dll'), os.path.join( self.installDir() , "bin" , "mpir.dll") )
+        utils.copyFile(os.path.join( self.sourceDir(), 'dll', 'Win32', 'Release', 'mpir.dll'), os.path.join( self.installDir() , "bin" , "mpir.dll") )
         
         if not os.path.isdir( os.path.join( self.installDir() , "lib" ) ):
                 os.makedirs( os.path.join( self.installDir() , "lib" ) )
-        shutil.move(os.path.join( self.sourceDir(), 'build.vc10', 'dll', 'Win32', 'Release', 'mpir.lib'), os.path.join( self.installDir() , "lib" , "mpir.lib") )
+        utils.copyFile(os.path.join( self.sourceDir(), 'dll', 'Win32', 'Release', 'mpir.lib'), os.path.join( self.installDir() , "lib" , "mpir.lib") )
         # a dirty workaround the fact that FindGMP.cmake will only look for gmp.lib
-        shutil.copy(os.path.join( self.installDir() , "lib" , "mpir.lib"), os.path.join( self.installDir() , "lib" , "gmp.lib") )
+        utils.copyFile(os.path.join( self.installDir() , "lib" , "mpir.lib"), os.path.join( self.installDir() , "lib" , "gmp.lib") )
 
         if not os.path.isdir( os.path.join( self.installDir() , "include" ) ):
                 os.makedirs( os.path.join( self.installDir() , "include" ) )
-        shutil.move(os.path.join( self.sourceDir(), 'build.vc10', 'dll', 'Win32', 'Release', 'gmp.h'), os.path.join( self.installDir() , "include" , "gmp.h") )
-        shutil.move(os.path.join( self.sourceDir(), 'build.vc10', 'dll', 'Win32', 'Release', 'gmpxx.h'), os.path.join( self.installDir() , "include" , "gmpxx.h") )
-        shutil.move(os.path.join( self.sourceDir(), 'build.vc10', 'dll', 'Win32', 'Release', 'mpir.h'), os.path.join( self.installDir() , "include" , "mpir.h") )
-        shutil.move(os.path.join( self.sourceDir(), 'build.vc10', 'dll', 'Win32', 'Release', 'mpirxx.h'), os.path.join( self.installDir() , "include" , "mpirxx.h") )
-        shutil.move(os.path.join( self.sourceDir(), 'build.vc10', 'dll', 'Win32', 'Release', 'gmp-mparam.h'), os.path.join( self.installDir() , "include" , "gmp-mparam.h") )
+        utils.copyFile(os.path.join( self.sourceDir(), 'dll', 'Win32', 'Release', 'gmp.h'), os.path.join( self.installDir() , "include" , "gmp.h") )
+        utils.copyFile(os.path.join( self.sourceDir(), 'dll', 'Win32', 'Release', 'gmpxx.h'), os.path.join( self.installDir() , "include" , "gmpxx.h") )
+        utils.copyFile(os.path.join( self.sourceDir(), 'dll', 'Win32', 'Release', 'mpir.h'), os.path.join( self.installDir() , "include" , "mpir.h") )
+        utils.copyFile(os.path.join( self.sourceDir(), 'dll', 'Win32', 'Release', 'mpirxx.h'), os.path.join( self.installDir() , "include" , "mpirxx.h") )
+        utils.copyFile(os.path.join( self.sourceDir(), 'dll', 'Win32', 'Release', 'gmp-mparam.h'), os.path.join( self.installDir() , "include" , "gmp-mparam.h") )
         
         return True
 
