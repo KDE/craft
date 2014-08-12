b36039abaa3a4cc8eed88dcb6755aa787cf88182
Patrick von Reth <vonreth@kde.org>
2014-08-12 21:46:10 +0200

    added oxygen \m/

---
+++ portage/kde/frameworks_tier4/frameworkintegration/frameworkintegration.py
@@ -0,0 +1,28 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.dependencies["kde/kconfig"] = 'default'
+        self.dependencies["kde/ki18n"] = 'default'
+        self.dependencies["kde/kiconthemes"] = 'default'
+        self.dependencies["kde/kio"] = 'default'
+        self.dependencies["kde/knotifications"] = 'default'
+        self.dependencies["kde/kwidgetsaddons"] = 'default'
+
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
+
+
+    
+
---
+++ portage/kdesupport/oxygen/oxygen.py
@@ -0,0 +1,26 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = '[git]kde:oxygen'
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.dependencies['libs/qtbase'] = 'default'
+        self.dependencies['kde/ki18n'] = 'default'
+        self.dependencies['kde/kconfig'] = 'default'
+        self.dependencies['kde/kguiaddons'] = 'default'
+        self.dependencies["kde/kwidgetsaddons"] = 'default'
+        self.dependencies['kde/kservice'] = 'default'
+        self.dependencies['kde/kcompletion'] = 'default'
+        self.dependencies['kde/frameworkintegration'] = 'default'
+        self.dependencies['kde/kwindowsystem'] = 'default'
+
+
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
+
