9826a505dcf8aa6cdb25521ac063d78e1e2c4b54
Patrick von Reth <vonreth@kde.org>
2014-08-13 18:41:39 +0200

    updated grantlee

--- portage/kdesupport/grantlee/grantlee.py
+++ portage/kdesupport/grantlee/grantlee.py
@@ -4,17 +4,8 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = "git://gitorious.org/grantlee/grantlee.git"
-        for ver in ['0.1', '0.2', '0.3']:
-            self.svnTargets[ ver ] = "git://gitorious.org/grantlee/grantlee.git|%s" % ver
-        for ver in ['0.3.0']:
-            self.targets[ ver ] = "http://downloads.grantlee.org/grantlee-%s.tar.gz" % ver
-            self.targetInstSrc[ ver ] = "grantlee-%s" % ver
-        self.patchToApply['0.3.0'] = [("0001-add-plugin-path-depending-on-executable-location.patch", 1),
-                                      ("patches/0.3.0/fix-exports-generator-for-intel-compiler.diff", 1)]
-        self.targetDigests['0.3.0'] = 'dbfd4370d48f10731b638a73abc848bb25602a35'
-        self.shortDescription = 'libraries for a template system similar to django\'s'
-        self.defaultTarget = '0.3.0'
+        self.svnTargets['gitHEAD'] = "git://gitorious.org/grantlee/grantlee.git| volatile_qt5_port"
+        self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -25,5 +16,5 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.defines = '-DBUILD_TESTS=OFF'
+        #self.subinfo.options.configure.defines = '-DBUILD_TESTS=OFF'
 
