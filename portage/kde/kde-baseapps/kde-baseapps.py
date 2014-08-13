7e586a6ff33888397b5480649d1657e93ea47d4a
Patrick von Reth <vonreth@kde.org>
2014-08-13 18:17:34 +0200

    frameworks adoptions

--- portage/kde/kde-baseapps/kde-baseapps.py
+++ portage/kde/kde-baseapps/kde-baseapps.py
@@ -4,18 +4,11 @@ from EmergeConfig import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
-        for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
-            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-            self.targetDigestUrls[ kd.kdeversion + ver  ] = 'http://download.kde.org/stable/' + kd.kdeversion + ver + '/src/' + self.package + '-' + kd.kdeversion + ver + '.tar.xz.sha1'
-
-        self.defaultTarget = 'gitHEAD'
+        self.versionInfo.setDefaultValues()
+        self.defaultTarget = "frameworks"
 
     def setDependencies( self ):
-        self.dependencies['kde/kde-runtime'] = 'default'
-        if self.options.features.nepomuk:
-            self.dependencies['kde/nepomuk-widgets'] = 'default'
+        self.dependencies['libs/qtbase'] = 'default'
         self.shortDescription = "KDE base applications (Konqueror, Dolphin)"
 
 from Package.CMakePackageBase import *
