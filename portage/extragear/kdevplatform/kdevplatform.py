9ba1c42711c29d5f41c0fdf85cc31249614c74d4
Cristian Oneț <onet.cristian@gmail.com>
2014-08-22 23:00:53 +0300

    Add 'zip' as a build dependency of kdevplatform since a test needs it.

--- portage/extragear/kdevplatform/kdevplatform.py
+++ portage/extragear/kdevplatform/kdevplatform.py
@@ -9,6 +9,7 @@ class subinfo( info.infoclass ):
     def setDependencies( self ):
         self.buildDependencies["virtual/base"] = "default"
         self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.buildDependencies["dev-util/zip"] = "default"
         self.dependencies["libs/qtdeclarative"] = "default"
         self.dependencies["libs/qtscript"] = "default"
         self.dependencies["libs/qtwebkit"] = "default"
