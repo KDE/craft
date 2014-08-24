5d4ace2aaae3217a6ddc5e1c33288a27215ecf05
Patrick Spendrin <ps_ml@gmx.de>
2014-08-24 23:07:36 +0200

    add newer libkolab version

--- portage/extragear/libkolab/libkolab-0.4.1.py
+++ portage/extragear/libkolab/libkolab-0.4.1.py
@@ -5,14 +5,15 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in [ '0.4.1', '0.5.0' ]:
+        for ver in [ '0.4.1', '0.5.0', '0.5.2' ]:
             self.targets[ ver ] = 'http://git.kolab.org/libkolab/snapshot/libkolab-' + ver + '.tar.gz'
             self.targetInstSrc[ ver ] = "libkolab-" + ver
         self.patchToApply['0.4.1'] = [("libkolab-fixes.diff", 1)]
         self.patchToApply['0.5.0'] = [("libkolab-0.5.0-fixes.diff", 1)]
+        self.patchToApply['0.5.2'] = [("libkolab-0.5.2-fixes.diff", 1)]
 
         self.shortDescription = ''
-        self.defaultTarget = '0.5.0'
+        self.defaultTarget = '0.5.2'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
