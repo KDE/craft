de3aba32429190d17661c240b764271761ba1361
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-08-30 15:10:24 -0300

    taglib: remove portage code and patches for old versions.

--- portage/win32libs/taglib/taglib.py
+++ portage/win32libs/taglib/taglib.py
@@ -7,14 +7,11 @@ class subinfo(info.infoclass):
         self.shortDescriptions = "audio meta-data library"
 
     def setTargets( self ):
-        for v in ['1.4', '1.5', '1.6', '1.6.1' , '1.6.3','1.7', '1.8' ]:
-          self.targets[ v ] = 'http://github.com/downloads/taglib/taglib/taglib-%s.tar.gz' % v
-          self.targetInstSrc[ v ] = 'taglib-%s' % v
-        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/taglib'
-        self.patchToApply[ '1.6.3' ] = [("taglib-1.6.3-20101213.diff", 1)]
+        self.targets["1.8"] = 'http://github.com/downloads/taglib/taglib/taglib-1.8.tar.gz'
+        self.targetInstSrc["1.8"] = 'taglib-1.8'
         self.patchToApply['1.8'] = [("taglib-1.8-20130307.diff", 1)]
-        self.targetDigests['1.7'] = '5138e1665182bc2171e298ff31518c9ad72ddf23'
         self.targetDigests['1.8'] = 'bdbfd746fde42401d3a77cd930c7802d374a692d'
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/taglib'
         self.shortDescription = "audio metadata library"
         self.defaultTarget = '1.8'
 
