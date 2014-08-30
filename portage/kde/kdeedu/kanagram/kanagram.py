a68c7cb325dea253637ac9e6c05a5ab125a020ca
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-08-30 00:23:47 -0300

    Add package for KAnagram.

--- portage/kde/kdeedu/kanagram/kanagram.py
+++ portage/kde/kdeedu/kanagram/kanagram.py
@@ -4,17 +4,24 @@ from EmergeConfig import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
-        for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
-            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-            self.targetDigestUrls[ kd.kdeversion + ver  ] = 'http://download.kde.org/stable/' + kd.kdeversion + ver + '/src/' + self.package + '-' + kd.kdeversion + ver + '.tar.xz.sha1'
+        self.svnTargets['frameworks'] = '[git]kde:kanagram|master'
 
         self.shortDescription = 'a letter order game'
-        self.defaultTarget = 'gitHEAD'
+        self.defaultTarget = 'frameworks'
 
     def setDependencies( self ):
-        self.dependencies['kde/libkdeedu'] = 'default'
+        self.dependencies['libs/qtbase'] = 'default'
+        self.dependencies['libs/qtdeclarative'] = 'default'
+        self.dependencies['kde/ki18n'] = 'default'
+        self.dependencies['kde/kcrash'] = 'default'
+        self.dependencies['kde/sonnet'] = 'default'
+        self.dependencies['kde/kconfig'] = 'default'
+        self.dependencies['kde/kconfigwidgets'] = 'default'
+        self.dependencies['kde/kdeclarative'] = 'default'
+        self.dependencies['kde/kio'] = 'default'
+        self.dependencies['kde/knewstuff'] = 'default'
+        self.dependencies['kde/kdelibs4support'] = 'default'
+        self.dependencies['kde/libkeduvocdocument'] = 'default'
 
 from Package.CMakePackageBase import *
 
