a13b038106a2750cdd010d8ed008df6d8a5a9248
Patrick von Reth <vonreth@kde.org>
2014-09-07 16:32:21 +0200

    move libechonest to win32libs

--- portage/qt-apps/tomahawk/tomahawk.py
+++ portage/qt-apps/tomahawk/tomahawk.py
@@ -14,7 +14,7 @@ class subinfo(info.infoclass):
         self.dependencies['qt-libs/phonon'] = 'default'
         self.dependencies['qt-libs/phonon-vlc'] = 'default'
         self.dependencies['kdesupport/qca'] = 'default'
-        self.dependencies['kdesupport/libechonest'] = 'default'
+        self.dependencies['qt-libs/libechonest'] = 'default'
         self.dependencies['kde/attica'] = 'default'
         self.dependencies['qt-libs/liblastfm'] = 'default'
         self.dependencies['win32libs/clucene-core'] = 'default'
