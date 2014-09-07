7dae27ba81d66e0260a12ae3b0b9e0279e39e97a
Patrick von Reth <vonreth@kde.org>
2014-09-07 15:17:09 +0200

    use tags for phonon again

--- portage/kdesupport/phonon-vlc/phonon-vlc.py
+++ portage/kdesupport/phonon-vlc/phonon-vlc.py
@@ -18,12 +18,13 @@ class subinfo(info.infoclass):
         self.targets[ ver ] = "http://download.kde.org/stable/phonon/phonon-backend-vlc/%s/src/phonon-backend-vlc-%s.tar.xz" % ( ver ,ver )
         self.targetInstSrc[ ver ] = "phonon-backend-vlc-%s" % ver 
         
-      for ver in ['0.7.1']:
+      for ver in ['0.7.1', '0.8.0']:
         self.targets[ ver ] = "http://download.kde.org/stable/phonon/phonon-backend-vlc/%s/phonon-backend-vlc-%s.tar.xz" % ( ver ,ver )
         self.targetInstSrc[ ver ] = "phonon-backend-vlc-%s" % ver 
         
       self.targetDigests['0.7.0'] = '8ebf032d7a87064e1307ff3f421aef2b07088681'
       self.targetDigests['0.7.1'] = '6f8e8abae12fdafc63b911b185cb335c65d59450'
+      self.targetDigests['0.8.0'] = '978f6b15539475e698533b0aeeb988b285a85894'
       
       self.patchToApply['0.7.0'] = [("use-kdewin-also-for-intel-compiler.diff", 1), # upstream
                                     ("0002-FindLIBVLC-Detect-libvlc-s-version-even-if-pkg-confi.patch", 1)] # upstream
@@ -31,7 +32,7 @@ class subinfo(info.infoclass):
       self.svnTargets['gitHEAD'] = '[git]kde:phonon-vlc'
       
       self.shortDescription = "the vlc based phonon multimedia backend"
-      self.defaultTarget = 'gitHEAD'
+      self.defaultTarget = '0.8.0'
 
 
 class Package( CMakePackageBase ):
--- portage/kdesupport/phonon/phonon.py
+++ portage/kdesupport/phonon/phonon.py
@@ -10,15 +10,16 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:phonon'        
-        for ver in ['4.7.0','4.7.1']:
+        for ver in ['4.7.0','4.7.1', '4.8.0']:
             self.targets[ver] = 'http://download.kde.org/stable/phonon/%s/phonon-%s.tar.xz' % (ver ,ver)
             self.targetInstSrc[ver] = 'phonon-%s' % ver
         self.patchToApply['4.7.0'] = ("phonon-4.7.0-fix-dll-linkage.diff", 1) # upstream
         self.targetDigests['4.7.0'] = 'feda28afe016fe38eb253f2be01973fc0226d10f'
         self.targetDigests['4.7.1'] = 'f1d3214a752d97028dc4ed910a832c1272951522'
+        self.targetDigests['4.8.0'] = 'b01da88ddba0d2d501bf1b6bb86abbff61ab6a12'
         
         self.shortDescription = "a Qt based multimedia framework"
-        self.defaultTarget = 'gitHEAD'
+        self.defaultTarget = '4.8.0'
 
 from Package.CMakePackageBase import *
 
