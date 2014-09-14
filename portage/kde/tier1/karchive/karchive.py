10f249a571b91ac8279fad2e387b4dbbcbf89fd9
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-09-14 13:55:14 -0300

    KArchive: don't use LZMA on MSVC2010, since liblzma doesn't compile.

--- portage/kde/tier1/karchive/karchive.py
+++ portage/kde/tier1/karchive/karchive.py
@@ -13,8 +13,10 @@ class subinfo(info.infoclass):
         self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
         self.dependencies["libs/qtbase"] = "default"
         self.dependencies["win32libs/libbzip2"] = "default"
-        self.dependencies["win32libs/liblzma"] = "default"
         self.dependencies["win32libs/zlib"] = "default"
+        if not compiler.isMSVC2010():
+            self.dependencies["win32libs/liblzma"] = "default"
+
 from Package.CMakePackageBase import *
 
 class Package(CMakePackageBase):
