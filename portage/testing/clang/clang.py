da9ff35a939fa76b02899dcb8ed4f642f1745037
Patrick Spendrin <ps_ml@gmx.de>
2014-09-16 14:08:30 +0200

    no need for old workaround anymore

--- portage/testing/clang/clang.py
+++ portage/testing/clang/clang.py
@@ -16,6 +16,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
-        self.llvm = portage.getPackageInstance("testing", "llvm")
-        self.subinfo.options.configure.defines = "-DCLANG_PATH_TO_LLVM_BUILD=" + self.llvm.imageDir()
 
