17d20e97e48a50091293d5726790f6fac44d96da
Patrick von Reth <vonreth@kde.org>
2014-09-18 13:47:13 +0200

    update okular

--- portage/kde/kdegraphics/okular/okular.py
+++ portage/kde/kdegraphics/okular/okular.py
@@ -4,18 +4,10 @@ from EmergeConfig import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
-        for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
-            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-            self.targetDigestUrls[ kd.kdeversion + ver  ] = 'http://download.kde.org/stable/' + kd.kdeversion + ver + '/src/' + self.package + '-' + kd.kdeversion + ver + '.tar.xz.sha1'
-            self.patchToApply[ver] = [('0001-fix-parttest-not-linking-against-okularpart-on-msvc.patch', 1)]
-
-        # on reviewboard - see https://git.reviewboard.kde.org/r/114191/
-        self.patchToApply['gitHEAD'] = [('0001-fix-parttest-not-linking-against-okularpart-on-msvc.patch', 1)]
-        
+        self.svnTargets['frameworks'] = '[git]kde:okular|frameworks'
+        self.shortDescription = 'Framework to build IDE-like applications'
         self.shortDescription = "KDE document viewer"
-        self.defaultTarget = 'gitHEAD'
+        self.defaultTarget = 'frameworks'
 
     def setDependencies( self ):
         self.buildDependencies['win32libs/chm'] = 'default'
@@ -30,6 +22,8 @@ class subinfo(info.infoclass):
         self.dependencies['win32libs/ebook-tools'] = 'default'
         self.buildDependencies['win32libs/libspectre'] = 'default'
         self.dependencies['binary/ghostscript'] = 'default'
+        self.dependencies['kde/khtml'] = 'default'
+        self.dependencies['kde/kactivities'] = 'default'
 
 from Package.CMakePackageBase import *
 
