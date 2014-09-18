7ccbd1a2ee5fe88e8f59ff152bd7e46389f47cd0
Patrick von Reth <vonreth@kde.org>
2014-09-18 13:45:57 +0200

    update astyle

--- portage/dev-util/astyle/astyle.py
+++ portage/dev-util/astyle/astyle.py
@@ -3,7 +3,9 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['1.22'] = "http://www.winkde.org/pub/kde/ports/win32/repository/other/astyle-1.22-bin.zip"
-        self.defaultTarget = '1.22'
+        self.targets["2.0.4"] = "http://downloads.sourceforge.net/sourceforge/astyle/AStyle_2.04_windows.zip"
+        self.targetMergeSourcePath["2.0.4"] = "AStyle"
+        self.defaultTarget = '2.0.4'
 
 from Package.BinaryPackageBase import *
 
