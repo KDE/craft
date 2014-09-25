03f3bc3255ea507765aec88d41b17a187c42078a
Patrick von Reth <vonreth@kde.org>
2014-09-25 15:57:34 +0200

    fixed snore without dbus

--- portage/qt-libs/snorenotify/snorenotify.py
+++ portage/qt-libs/snorenotify/snorenotify.py
@@ -26,6 +26,7 @@ class subinfo(info.infoclass):
 class Package( CMakePackageBase ):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.defines = '-DWITH_QT4=OFF -DWITH_FREEDESKTOP_FRONTEND=ON -DWITH_FRONTENDS=ON -DWITH_SNORE_DEAMON=ON '
-        self.subinfo.options.fetch.checkoutSubmodules = True
+        self.subinfo.options.configure.defines = '-DWITH_QT4=OFF -DWITH_FRONTENDS=ON -DWITH_SNORE_DEAMON=ON '
+        if self.subinfo.options.isActive("win32libs/dbus"):
+            self.subinfo.options.configure.defines += '-DWITH_FREEDESKTOP_FRONTEND=ON '
 
