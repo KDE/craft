8541e7ed4fd93dfe74b642479035ca048ddb0f3b
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-09-28 23:32:21 -0300

    Fix build of karchive tarballs.

--- portage/frameworks/tier1/karchive/karchive.py
+++ portage/frameworks/tier1/karchive/karchive.py
@@ -17,6 +17,9 @@ class subinfo(info.infoclass):
         if not compiler.isMSVC2010() and not compiler.isMSVC2012():
             self.dependencies["win32libs/liblzma"] = "default"
 
+        for ver in ('5.0.0', '5.1.0', '5.2.0'):
+            self.patchToApply[ver] = [('get-rid-of-VLAs.diff', 1)]
+
 from Package.CMakePackageBase import *
 
 class Package(CMakePackageBase):
