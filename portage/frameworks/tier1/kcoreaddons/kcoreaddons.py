62a577e8d1df20e130ba4e0c70fa75a6871cea05
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-09-28 22:34:12 -0300

    Fix build of kcoreaddons 5.2.0 tarball.

--- portage/frameworks/tier1/kcoreaddons/kcoreaddons.py
+++ portage/frameworks/tier1/kcoreaddons/kcoreaddons.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues( )
 
         self.shortDescription = "KCoreAddons"
-        
+        self.patchToApply["5.2.0"] = [('std-function-build-fix.diff', 1)]
 
     def setDependencies( self ):
         self.buildDependencies["virtual/base"] = "default"
