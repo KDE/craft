15e987a6a712aff45d8952e1500ed1c3e580a1d4
Patrick von Reth <vonreth@kde.org>
2014-10-02 15:40:57 +0200

    add new quassel version

--- portage/extragear/quassel/quassel.py
+++ portage/extragear/quassel/quassel.py
@@ -9,12 +9,14 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = 'git://gitorious.org/quassel/quassel.git'
         self.svnTargets['0.6'] = 'git://gitorious.org/quassel/quassel.git|0.6|'
-        for ver in ['0.7.1','0.7.2','0.7.3','0.8.0','0.9.0','0.9.1','0.9.2','0.9.3', '0.10.0']:
+        for ver in ['0.7.1','0.7.2','0.7.3','0.8.0','0.9.0','0.9.1','0.9.2','0.9.3', '0.10.0', '0.11.0']:
             self.targets[ver] = 'http://quassel-irc.org/pub/quassel-%s.tar.bz2' % ver
             self.targetInstSrc[ver] = 'quassel-%s' % ver
         self.targetDigests['0.7.1'] = '791086da977033a1bbee3effa317668b3726bd7f'
         self.targetDigests['0.8.0'] = 'b74967fa9f19b5d7c708279075cc0ef3a3dbbe8b'
         self.targetDigests['0.10.0'] = '305d56774b1af2a891775a5637174d9048d875a7'
+        self.targetDigests['0.11.0'] = 'd7b31f8e1ee4465ec33dd77f689fec59f4b78a36'
+        self.patchToApply['0.11.0'] = ('quassel-0.11.0-20141002.diff',1)
         self.defaultTarget = 'gitHEAD'
 
 
