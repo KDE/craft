4d6685172b8d0cea7a15892468b89ebef77ac05b
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-10-04 18:09:46 -0300

    Update CMake to 3.0.2.

--- portage/dev-util/cmake/cmake.py
+++ portage/dev-util/cmake/cmake.py
@@ -3,13 +3,14 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets['3.0.0'] = 'http://www.cmake.org/files/v3.0/cmake-3.0.0-win32-x86.zip'
-        self.targetMergeSourcePath['3.0.0'] = 'cmake-3.0.0-win32-x86'
+        self.targets['3.0.2'] = 'http://www.cmake.org/files/v3.0/cmake-3.0.2-win32-x86.zip'
+        self.targetMergeSourcePath['3.0.2'] = 'cmake-3.0.0-win32-x86'
+        self.targetDigests['3.0.2'] = '8b562caca5ede42969e6e7393da961005f092c0c'
 
         self.shortDescription = "CMake, the cross-platform, open-source build system."
         self.homepage = "http://www.cmake.org/"
 
-        self.defaultTarget = '3.0.0'
+        self.defaultTarget = '3.0.2'
 
 
     def setDependencies( self ):
