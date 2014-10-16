d64b46558276a982a2ba775969b123180cd38a5d
Patrick von Reth <vonreth@kde.org>
2014-10-16 16:17:56 +0200

    update snore

--- portage/qt-libs/snorenotify/snorenotify.py
+++ portage/qt-libs/snorenotify/snorenotify.py
@@ -14,13 +14,14 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = 'https://github.com/Snorenotify/Snorenotify.git'
-        for ver in ['0.5.1']:
+        for ver in ['0.5.1', '0.5.2']:
             self.targets[ver] = 'https://github.com/Snorenotify/Snorenotify/archive/v%s.tar.gz' % ver
             self.archiveNames[ver] = "snorenotify-%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'Snorenotify-%s' % ver
         self.targetDigests['0.5.1'] = 'eb83e0b7bccfc1c307a8457265dc4a5607a8b877'
+        self.targetDigests['0.5.2'] = '9aa4409422872dd32bd5f831a6201820994065a1'
         self.shortDescription = "An application to show kde notifications with Win8, Snarl or Growl"
-        self.defaultTarget = '0.5.1'
+        self.defaultTarget = '0.5.2'
 
 
 class Package( CMakePackageBase ):
