7d818809a420029117b84fce477b95383cdce93b
Patrick von Reth <vonreth@kde.org>
2014-10-31 09:51:20 +0100

    updated phonon

--- portage/qt-libs/phonon/phonon.py
+++ portage/qt-libs/phonon/phonon.py
@@ -9,16 +9,17 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:phonon'        
-        for ver in ['4.7.0','4.7.1', '4.8.0']:
+        for ver in ['4.7.0','4.7.1', '4.8.0', '4.8.1' ]:
             self.targets[ver] = 'http://download.kde.org/stable/phonon/%s/phonon-%s.tar.xz' % (ver ,ver)
             self.targetInstSrc[ver] = 'phonon-%s' % ver
         self.patchToApply['4.7.0'] = ("phonon-4.7.0-fix-dll-linkage.diff", 1) # upstream
         self.targetDigests['4.7.0'] = 'feda28afe016fe38eb253f2be01973fc0226d10f'
         self.targetDigests['4.7.1'] = 'f1d3214a752d97028dc4ed910a832c1272951522'
         self.targetDigests['4.8.0'] = 'b01da88ddba0d2d501bf1b6bb86abbff61ab6a12'
+        self.targetDigests['4.8.1'] = '652872be8144c88974aa1992de5ae705a90e3be1'
         
         self.shortDescription = "a Qt based multimedia framework"
-        self.defaultTarget = '4.8.0'
+        self.defaultTarget = '4.8.1'
 
 from Package.CMakePackageBase import *
 
