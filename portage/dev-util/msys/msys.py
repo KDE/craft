2fb1843649b33f8d25e166f983b7e66fc12e3872
Patrick von Reth <vonreth@kde.org>
2014-11-03 16:56:04 +0100

    update msys base

--- portage/dev-util/msys/msys.py
+++ portage/dev-util/msys/msys.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = "20140910"
+        ver = "20141003"
         if compiler.isX86():
             self.targets[ "base" ] = "http://downloads.sourceforge.net/sourceforge/msys2/msys2-base-i686-%s.tar.xz" % ver
         else:
