0be2d160fa540281a6b106c15f9a6793cf81f9aa
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2014-11-22 13:11:57 +0100

    - RKWard repo is now in git on git.kde.org - Add dependency on okular - Add more stable releases

--- portage/kdeapps/rkward/rkward-20121211.py
+++ portage/kdeapps/rkward/rkward-20121211.py
@@ -3,15 +3,16 @@ import os
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets[ 'svnHEAD' ] = 'http://svn.code.sf.net/p/rkward/code/trunk/rkward'
-        for ver in ['0.5.7', '0.6.0']:
+        self.gitTargets[ 'gitHEAD' ] = '[git]kde:rkward'
+        for ver in ['0.5.7', '0.6.0', '0.6.1', '0.6.2']:
             self.targets[ver] = 'http://downloads.sourceforge.net/rkward/rkward-' + ver + '.tar.gz'
             self.targetInstSrc[ ver] = 'rkward-' + ver
-        self.defaultTarget = 'svnHEAD'
+        self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.dependencies[ 'testing/r-base' ] = 'default'
-        self.dependencies[ 'kde/kate' ] = 'default'  # provides katepart
+        self.dependencies[ 'kde/kate' ] = 'default'  # provides katepart; runtime
+        self.dependencies[ 'kde/okular' ] = 'default'  # not strictly required, but useful for print preview; runtime
 
 from Package.CMakePackageBase import *
 
