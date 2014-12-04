e42d05f70fd317bb56edc95ca14e2e230e4babc9
Patrick von Reth <vonreth@kde.org>
2014-12-04 10:43:20 +0100

    added a release

--- portage/qt-libs/qtkeychain/qtkeychain.py
+++ portage/qt-libs/qtkeychain/qtkeychain.py
@@ -5,11 +5,16 @@ from Package.CMakePackageBase import *
 
 class subinfo( info.infoclass ):
     def setDependencies( self ):
-      self.dependencies[ 'libs/qt' ] = 'default'
+        self.dependencies[ 'libs/qt' ] = 'default'
 
     def setTargets( self ):
-      self.svnTargets[ 'gitHEAD' ] = 'git://github.com/frankosterfeld/qtkeychain.git'
-      self.defaultTarget = 'gitHEAD'
+        self.svnTargets[ 'gitHEAD' ] = 'git://github.com/frankosterfeld/qtkeychain.git'
+        for ver in ["0.4.0"]:
+            self.targets[ ver ] = "https://github.com/frankosterfeld/qtkeychain/archive/v%s.tar.gz" % ver
+            self.archiveNames[ ver ] = "qtkeychain-v%s.tar.gz" % ver
+            self.targetInstSrc[ ver ] = 'qtkeychain-%s' % ver
+        self.targetDigests['0.4.0'] = '869ed20d15cc78ab3903701faf3100d639c3da57'
+        self.defaultTarget = '0.4.0'
 
 
 class Package( CMakePackageBase ):
