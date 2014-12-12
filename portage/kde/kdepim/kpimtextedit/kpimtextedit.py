1097241963157a22356fd52b4ca20476f3736123
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-12-12 14:16:13 -0300

    More kdepim libraries.

---
+++ portage/kde/kdepim/kpimtextedit/kpimtextedit.py
@@ -0,0 +1,29 @@
+import info
+import kdedefaults as kd
+from EmergeConfig import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues()
+
+        self.shortDescription = ""
+        self.defaultTarget = 'master'
+
+    def setDependencies( self ):
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.dependencies["libs/qtbase"] = "default"
+        self.dependencies["frameworks/kcoreaddons"] = "default"
+        self.dependencies["frameworks/kemoticons"] = "default"
+        self.dependencies["frameworks/sonnet"] = "default"
+        self.dependencies["frameworks/ktextwidgets"] = "default"
+        self.dependencies["frameworks/kwidgetsaddons"] = "default"
+        self.dependencies["frameworks/kio"] = "default"
+        self.dependencies["frameworks/kcodecs"] = "default"
+        self.dependencies["frameworks/kdelibs4support"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        CMakePackageBase.__init__(self)
+
