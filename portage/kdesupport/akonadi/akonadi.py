b64cc189fa1918894653f185068e1786b31bd02e
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-12-15 11:31:58 -0300

    Update akonadi server package for KF5.
    I removed the old tarballs because they are for Qt4, which have different
    deps.

--- portage/kdesupport/akonadi/akonadi.py
+++ portage/kdesupport/akonadi/akonadi.py
@@ -4,37 +4,14 @@ import info
 
 class subinfo(info.infoclass):
     def setDependencies( self ):
-        self.buildDependencies['virtual/base'] = 'default'
-        self.buildDependencies['win32libs/automoc'] = 'default'
-        if self.options.features.nepomuk:
-            self.dependencies['kdesupport/soprano'] = 'default'
-        else:
-            self.dependencies['kdesupport/strigi'] = 'default'
-        self.dependencies['win32libs/boost-program-options']   = 'default'
+        self.buildDependencies['dev-util/extra-cmake-modules'] = 'default'
         self.dependencies['win32libs/libxslt'] = 'default'
-        self.dependencies['libs/qt'] = 'default'
+        self.dependencies['libs/qtbase'] = 'default'
         self.dependencies['win32libs/sqlite'] = 'default'
         self.dependencies['win32libs/shared-mime-info'] = 'default'
 
     def setTargets( self ):
-        baseurl = 'http://download.kde.org/stable/akonadi/src/akonadi-%s.tar.bz2'
-        for ver in ['1.10.2','1.10.80', '1.12.1']:
-            self.targets[ver] = baseurl % ver
-            self.targetInstSrc[ver] = 'akonadi-' + ver
-        for ver in ['1.10.3']:
-            self.targets[ver] = baseurl % (ver + "-1")
-            self.targetInstSrc[ver] = 'akonadi-' + ver
-
-        self.targetDigests['1.10.2'] = '97660e2a4fc8797ae86ac2981490d3868c6085ff'
-        self.targetDigests['1.10.3'] = '701fbdde01a2787ec47fc085da02ad6238cf3b92'
-        self.targetDigests['1.10.80'] = '016ff1d137af37dc1a295958e612cfd92075c3f8'
-
-        self.patchToApply['1.10.2'] = [("akonadi-kde.conf-fix.diff", 1)]
-        self.patchToApply['1.10.3'] = [("akonadi-kde.conf-fix.diff", 1)]
-        self.patchToApply['1.10.80'] = [("akonadi-kde.conf-fix-1.10.80.diff", 1)]
-#        self.patchToApply['1.12.1'] = [("akonadi-kde.conf-fix-1.10.80.diff", 1)]
-        self.patchToApply['1.12.1'] = [("akonadi-1.12.1-20140419.diff", 1)]
-        self.patchToApply['gitHEAD'] = [("akonadi-kde.conf-fix-1.10.80.diff", 1)]
+        #self.patchToApply['gitHEAD'] = [("akonadi-kde.conf-fix-1.10.80.diff", 1)]
 
         self.svnTargets['gitHEAD'] = '[git]kde:akonadi.git'
         self.shortDescription = "a storage service for PIM data and meta data"
