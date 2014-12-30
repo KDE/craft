01f87055525b0a4fa940455a552bb5e382775b47
Kevin Funk <kfunk@kde.org>
2014-12-30 12:38:56 +0100

    Add kpackage framework

---
+++ portage/frameworks/tier2/kpackage/kpackage.py
@@ -0,0 +1,21 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues()
+        self.shortDescription = "Documentation generation from docbook "
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.dependencies["libs/qtbase"] = "default"
+        self.dependencies["frameworks/karchive"] = "default"
+        self.dependencies["frameworks/ki18n"] = "default"
+        self.dependencies["frameworks/kcoreaddons"] = "default"
+        self.dependencies["frameworks/kconfig"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
