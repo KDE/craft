196eafe13d67ce96e1a1b97ad11d714125708d8c
Patrick von Reth <vonreth@kde.org>
2015-01-23 13:18:31 +0100

    update exiv2 to 0.24

--- portage/win32libs/exiv2/exiv2.py
+++ portage/win32libs/exiv2/exiv2.py
@@ -2,38 +2,29 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for f in ( '16', '17', '18', '18.1', '18.2', '19' ):
-          ver = '0.' + f
-          self.targets[ver]       = 'http://www.exiv2.org/exiv2-%s.tar.gz' % ver
-          self.targetInstSrc[ver] = 'exiv2-%s' % ver
-          self.patchToApply[ver]  = [( 'exiv2-%s-cmake.diff' % ver, 0 )]
-        for f in ( '21', '22' ):
-          ver = '0.' + f
-          self.targets[ver]       = 'http://www.exiv2.org/exiv2-%s.tar.gz' % ver
-          self.targetInstSrc[ver] = 'exiv2-%s' % ver
-          self.patchToApply[ver]  = [( 'exiv2-%s-cmake.diff' % ver, 1 )]
-        self.patchToApply['0.21'].append(('exiv2-0.21-20101223.diff', 1))
-        self.patchToApply['0.22'].append(('exiv2-0.22-20120117.diff', 1))
-        # 0.23+ will use svn because cmake support are not included in the tarball but are in the repository
-        # exiv2 will eventually fully support cmake
-        for f in ( '23', ):
-          ver = '0.' + f
-          self.svnTargets[ver]       = 'svn://dev.exiv2.org/svn/tags/%s' % ver
+
+        for ver in ['0.24']:
+            self.targets[ver]       = 'http://www.exiv2.org/exiv2-%s.tar.gz' % ver
+            self.targetInstSrc[ver] = 'exiv2-%s' % ver
+        self.targetDigests['0.24'] = '2f19538e54f8c21c180fa96d17677b7cff7dc1bb'
+        self.patchToApply['0.24'] = ('exiv2-0.22-20120117.diff', 1)
+            
         self.svnTargets['svnHEAD'] = 'svn://dev.exiv2.org/svn/trunk'
 
         self.shortDescription = "an image metadata library"
-        self.defaultTarget = '0.22'
+        self.defaultTarget = '0.24'
 
     def setDependencies( self ):
         self.dependencies['win32libs/win_iconv']    = 'default'
         self.dependencies['win32libs/gettext']      = 'default'
         self.dependencies['win32libs/expat']        = 'default'
         self.dependencies['win32libs/zlib']         = 'default'
-        self.buildDependencies['virtual/base']          = 'default'
+        self.buildDependencies['virtual/base']      = 'default'
 
 from Package.CMakePackageBase import *
 
 class Package( CMakePackageBase ):
     def __init__( self, **args ):
         CMakePackageBase.__init__( self )
+        self.supportsNinja = False#something is wrong with cmake here
 
