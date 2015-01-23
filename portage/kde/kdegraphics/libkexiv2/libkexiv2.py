06eab9df59fe5f6fce9a428ca20cf504ce5d0899
Patrick von Reth <vonreth@kde.org>
2015-01-23 13:19:07 +0100

    fix libkexiv2 script

--- portage/kde/kdegraphics/libkexiv2/libkexiv2.py
+++ portage/kde/kdegraphics/libkexiv2/libkexiv2.py
@@ -1,20 +1,14 @@
 import info
-import kdedefaults as kd
 from EmergeConfig import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
-        for ver in ['0', '1', '2', '3', '4', '5']:
-            self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
-            self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver
-            self.targetDigestUrls[ kd.kdeversion + ver  ] = 'http://download.kde.org/stable/' + kd.kdeversion + ver + '/src/' + self.package + '-' + kd.kdeversion + ver + '.tar.xz.sha1'
-
+        self.svnTargets['frameworks'] = '[git]kde:kexiv2|frameworks'
+        
         self.shortDescription = "Libkexiv2 is a wrapper around Exiv2 library to manipulate pictures metadata as EXIF IPTC and XMP."
-        self.defaultTarget = 'gitHEAD'
+        self.defaultTarget = 'frameworks'
 
     def setDependencies( self ):
-        self.dependencies['kde/kdelibs'] = 'default'
         self.dependencies['win32libs/exiv2'] = 'default'
 
 from Package.CMakePackageBase import *
