4047d87957c489e2191c9e9d0fdf58ad8b391c34
Patrick von Reth <vonreth@kde.org>
2015-01-29 16:21:18 +0100

    reset some more settings

--- bin/prepare-sdk.py
+++ bin/prepare-sdk.py
@@ -17,6 +17,20 @@ def removeFromDB(category):
         for package in db.getInstalledPackages( c, p ):
             package.uninstall()
 
+
+def _replaceSetting(reg,new,text):
+        return re.compile(reg, re.MULTILINE).sub(new,text)
+
+def resetSettings():
+    with open( os.path.join( EmergeStandardDirs.etcDir(), "kdesettings.ini.backup"),"rt+") as fin:
+        text = fin.read()
+    text = _replaceSetting("^PACKAGE_IGNORES.*$", "PACKAGE_IGNORES =", text)
+    text = _replaceSetting("^EMERGE_USE_CCACHE.*$", "#EMERGE_USE_CCACHE = True", text)
+    text = _replaceSetting("^Python =.*$", "Python = C:\python34", text)
+    text = _replaceSetting("^DOWNLOADDIR =.*$", "#DOWNLOADDIR = C:\kde\download", text)
+    with open( os.path.join( EmergeStandardDirs.etcDir(), "kdesettings.ini"),"wt+") as fout:
+        fout.write(text)
+
 if __name__ == "__main__":
     utils.setVerbose(3)
     # we don't use the db directly and the file must not be locked
@@ -25,14 +39,10 @@ if __name__ == "__main__":
     removeFromDB("dev-util")
     removeFromDB("gnuwin32")
     # we cant use the ini support to modify the settings as it would kill the comments
-    with open( os.path.join( EmergeStandardDirs.etcDir(), "kdesettings.ini.backup"),"rt+") as fin:
-        text = fin.read()
-        text = re.compile("^PACKAGE_IGNORES.*$", re.MULTILINE).sub("PACKAGE_IGNORES =",text)
-        text = re.compile("^EMERGE_USE_CCACHE.*$", re.MULTILINE).sub("EMERGE_USE_CCACHE = False",text)
-        with open( os.path.join( EmergeStandardDirs.etcDir(), "kdesettings.ini"),"wt+") as fout:
-            fout.write(text)
+    resetSettings()
     utils.createDir(EmergeStandardDirs.tmpDir())
     archiveName = os.path.join(EmergeStandardDirs.tmpDir(), "framewroks-sdk.7z")
     utils.deleteFile(archiveName)
     utils.system("7za  a %s %s -x!build -x!msys -x!dev-utils -x!tmp -x!mingw* -x!etc/kdesettings.ini.backup -x!etc/portage/install.db.backup" % ( archiveName, EmergeStandardDirs.emergeRoot()))
+    #utils.copyFile(os.path.join( EmergeStandardDirs.etcDir(), "kdesettings.ini"), os.path.join( EmergeStandardDirs.etcDir(), "kdesettings.ini.backup2"),linkOnly=False)
     restore()
