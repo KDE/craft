002e05c8069a7676a7a5638a5b3233a658f02739
Patrick von Reth <vonreth@kde.org>
2015-01-29 15:30:06 +0100

    install ccache to dev-utils

--- portage/dev-util/ccache/ccache.py
+++ portage/dev-util/ccache/ccache.py
@@ -20,6 +20,7 @@ from Package.VirtualPackageBase import *
 class PackageMinGW(AutoToolsPackageBase):
     def __init__( self, **args ):
         AutoToolsPackageBase.__init__(self)
+        self.subinfo.options.merge.destinationPath = "dev-utils"
         self.subinfo.options.configure.defines = "--with-bundled-zlib "
         self.supportsCCACHE = False
 
