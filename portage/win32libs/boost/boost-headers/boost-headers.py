087e3b64abc00d5389a0700d4449e9c1a8f24286
Patrick von Reth <vonreth@kde.org>
2015-01-31 17:33:07 +0100

    update boost

--- portage/win32libs/boost/boost-headers/boost-headers.py
+++ portage/win32libs/boost/boost-headers/boost-headers.py
@@ -6,10 +6,11 @@ class subinfo(info.infoclass):
 
         self.targetDigests['1_55_0'] = '09203c60118442561d0ee196772058d80226762d'
         self.targetDigests['1_56_0'] = '3a65d31ad443a46ce6bb5011a5d5a0750765f157'
+        self.targetDigests['1_57_0'] = 'cc08224035b6aa642599bbf28c76ee5ec8061c10'
 
         self.patchToApply['1_55_0'] = [('boost_1_54_0-spirit-20131114.diff',1)]
         self.patchToApply['1_56_0'] = [('boost_1_56_0-spirit-20131114.diff',1)] # TODO: also include in future releases!
-
+        self.patchToApply['1_57_0'] = [('boost_1_56_0-spirit-20131114.diff',1)]
 
         self.homepage = 'http://www.boost.org/'
 
