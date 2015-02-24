badbb3c713d6a8e96a3bac4f295778c329568ee7
Cristian Onet <onet.cristian@gmail.com>
2015-02-24 22:53:32 +0200

    Add an initial package for KDiagram.

---
+++ portage/testing/kdiagram/kdiagram.py
@@ -0,0 +1,19 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['master'] = '[git]kde:kdiagram|master'
+        self.defaultTarget = 'master'
+
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
