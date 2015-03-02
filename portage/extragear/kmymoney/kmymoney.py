1f88aaa3fb5290bccfc5853d81159a8ccab3e537
Cristian Oneț <onet.cristian@gmail.com>
2015-03-02 23:04:28 +0200

    Moved KDiagram to extragear since all build issues are now fixed.

--- portage/extragear/kmymoney/kmymoney.py
+++ portage/extragear/kmymoney/kmymoney.py
@@ -9,13 +9,13 @@ class subinfo(info.infoclass):
         self.dependencies['frameworks/kcmutils'] = 'default'
         self.dependencies['frameworks/kdelibs4support'] = 'default'
         self.dependencies['frameworks/khtml'] = 'default'
-        self.dependencies['testing/kdiagram'] = 'default'
         self.dependencies['testing/gpgmepp'] = 'default'
         self.dependencies['binary/mysql-pkg'] = 'default'
         self.dependencies['win32libs/sqlite'] = 'default'
         #self.dependencies['win32libs/libofx'] = 'default'
         self.dependencies['win32libs/gettext'] = 'default'
         self.dependencies['extragear/libalkimia'] = 'default'
+        self.dependencies['extragear/kdiagram'] = 'default'
         self.buildDependencies['dev-util/gettext-tools'] = 'default'
         self.shortDescription = "a personal finance manager for KDE"
 
