5d0560abaa4091991077d7c2ff786a5d03540fb6
Patrick von Reth <vonreth@kde.org>
2015-03-05 16:28:44 +0100

    update putty

--- portage/dev-util/putty/putty.py
+++ portage/dev-util/putty/putty.py
@@ -3,12 +3,15 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.targets['HEAD'] = 'http://the.earth.li/~sgtatham/putty/latest/x86/putty.zip'
-        for ver in ['0.62', '0.63']:
+        for ver in ['0.62', '0.63', '0.64']:
             self.targets[ver] = 'http://the.earth.li/~sgtatham/putty/' + ver + '/x86/putty.zip'
+            self.archiveNames[ver] = "putty-%s.zip" % ver
             self.targetInstallPath[ver] = "bin"
+
         self.targetDigests['0.62'] = '953e7b2eb7844184ccfb24651c7829f3e1e30558'
         self.targetDigests['0.63'] = '573ffcaa7f3205ca77ee5f3502b7def3b0ec7e79'
-        self.defaultTarget = '0.63'
+        self.targetDigests['0.64'] = '66717b0acb20528e313657b3c69efc3badfe985c'
+        self.defaultTarget = '0.64'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/bin-base'] = 'default'
