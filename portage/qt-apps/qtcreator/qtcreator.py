dea1fd1e046d2847224d32bab2b0b0d1b85508a7
Patrick von Reth <vonreth@kde.org>
2015-03-27 14:45:15 +0100

    fix git url for qtcreator

--- portage/qt-apps/qtcreator/qtcreator.py
+++ portage/qt-apps/qtcreator/qtcreator.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):       
-        self.svnTargets[ "gitHEAD" ] = "git://gitorious.org/qt-creator/qt-creator.git"
+        self.svnTargets[ "gitHEAD" ] = "git://code.qt.io/qt-creator/qt-creator.git"
         self.targets[ "3.1.1" ] = "http://download.qt-project.org/official_releases/qtcreator/3.1/3.1.1/qt-creator-opensource-src-3.1.1.tar.gz"
         self.targetDigests['3.1.1'] = '95e0842447afb91259b0ead2fba7599821767001'
         self.targetInstSrc[ "3.1.1" ] = "qt-creator-opensource-src-3.1.1"
