30e49567a0a093267216840d15c94af5fa2c21bb
Patrick von Reth <vonreth@kde.org>
2015-04-06 13:12:28 +0200

    updated cmake

--- portage/dev-util/cmake/cmake.py
+++ portage/dev-util/cmake/cmake.py
@@ -3,14 +3,15 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets['3.0.2'] = 'http://www.cmake.org/files/v3.0/cmake-3.0.2-win32-x86.zip'
-        self.targetMergeSourcePath['3.0.2'] = 'cmake-3.0.2-win32-x86'
-        self.targetDigests['3.0.2'] = '8b562caca5ede42969e6e7393da961005f092c0c'
-
+        ver = '3.2.1'
+        self.targets[ver] = 'http://www.cmake.org/files/v3.2/cmake-%s-win32-x86.zip' % ver
+        self.targetMergeSourcePath[ver] = 'cmake-%s-win32-x86' % ver
+        self.targetDigests['3.2.1'] = '4011f4f18c002a9ff97c76ea1d397eca9b675f98'
+        
         self.shortDescription = "CMake, the cross-platform, open-source build system."
         self.homepage = "http://www.cmake.org/"
 
-        self.defaultTarget = '3.0.2'
+        self.defaultTarget = '3.2.1'
 
 
     def setDependencies( self ):
