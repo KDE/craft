899a844e514422e4cb7312196ea0122fb51e1f84
Patrick Spendrin <ps_ml@gmx.de>
2015-04-08 01:14:53 +0200

    update libidn to 1.30

--- portage/win32libs/libidn/libidn.py
+++ portage/win32libs/libidn/libidn.py
@@ -3,11 +3,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = '1.19'
-        self.targets[ ver ]       = 'ftp://ftp.gnu.org/gnu/libidn/libidn-%s.tar.gz' % ver
-        self.targetDigests[ ver ] = '2b6dcb500e8135a9444a250d7df76f545915f25f'
-        self.targetInstSrc[ ver ] = 'libidn-%s' % ver
-        self.patchToApply[ ver ] = ("libidn-1.19-20101213.diff", 1)
+        for ver in ['1.19', '1.30']:
+            self.targets[ ver ]       = 'ftp://ftp.gnu.org/gnu/libidn/libidn-%s.tar.gz' % ver
+            self.targetInstSrc[ ver ] = 'libidn-%s' % ver
+        self.patchToApply[ '1.19' ] = ("libidn-1.19-20101213.diff", 1)
+        self.patchToApply[ '1.30' ] = ("libidn-1.30-20150407.diff", 1)
+        self.targetDigests[ '1.19' ] = '2b6dcb500e8135a9444a250d7df76f545915f25f'
+        self.targetDigests['1.30'] = '557e1e37f0978e975b21bcdc243c198cb708bb75'
         self.shortDescription = "libidn internationalized domain names library"
         self.defaultTarget = ver
 
