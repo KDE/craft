420f662dfaf2e74919f5a8f6afec7b39a5be2f72
Patrick Spendrin <ps_ml@gmx.de>
2015-04-08 01:15:29 +0200

    update gloox and add tests

--- portage/testing/gloox/gloox.py
+++ portage/testing/gloox/gloox.py
@@ -8,15 +8,16 @@ class subinfo(info.infoclass):
         self.dependencies['win32libs/libidn'] = 'default'
 
     def setTargets( self ):
-        for ver in ['1.0']:
+        for ver in ['1.0', '1.0.13']:
             self.targets[ ver ] = "http://camaya.net/download/gloox-" + ver + ".tar.bz2"
             self.targetInstSrc[ ver ] = "gloox-" + ver
         self.patchToApply['1.0'] = [('gloox-1.0-20101111.diff', 1)]
+        self.patchToApply['1.0.13'] = [('gloox-1.0.13-20150408.diff', 1)]
 
         self.targetDigests['1.0'] = '8c788738f72b05fae7c05c744a67859419ffa09c'
+        self.targetDigests['1.0.13'] = '735c0507f4ac45e6990528fab6afc45f9cabcc3a'
 
-        self.defaultTarget = '1.0'
-        self.options.configure.defines = ""
+        self.defaultTarget = '1.0.13'
 
 from Package.CMakePackageBase import *
 
