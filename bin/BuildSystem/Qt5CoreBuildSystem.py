718052289e9770a8ac4cd575f40358b46b921841
Patrick von Reth <vonreth@kde.org>
2015-05-20 11:43:14 +0200

    party revert 85e0fc02de0c9d262b8ffeb1fde16d732c826f06 and fix qt install when short paths are not used

--- bin/BuildSystem/Qt5CoreBuildSystem.py
+++ bin/BuildSystem/Qt5CoreBuildSystem.py
@@ -17,6 +17,13 @@ class Qt5CoreBuildSystem( QMakeBuildSystem ):
         options += " INSTALL_ROOT=%s install" % self.imageDir( )
         if not QMakeBuildSystem.install( self, options ):
             return False
+            
+        badPrefix = os.path.join( self.installDir( ), EmergeStandardDirs.emergeRoot( )[ 3: ] )
+        if EmergeStandardDirs.emergeRoot( )[ 3: ] != "" and os.path.exists( badPrefix ):
+            for subdir in os.listdir( badPrefix ):
+                utils.moveFile( os.path.join( badPrefix, subdir ), self.installDir( ) )
+            utils.rmtree( badPrefix )
+ 
 
         if os.path.exists( os.path.join( self.installDir( ), "bin", "mkspecs" ) ):
             utils.moveFile( os.path.join( self.installDir( ), "bin", "mkspecs" ),
