21a5cdef1f8451ba19b6e27b2f8b1ba610fdb9db
Patrick von Reth <vonreth@kde.org>
2015-06-19 12:05:59 +0200

    downgrade winflex_bison as we still need the old version for qt5.4

--- portage/dev-util/winflexbison/winflexbison.py
+++ portage/dev-util/winflexbison/winflexbison.py
@@ -5,11 +5,11 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in ["2.5.5" ]:
+        for ver in ["2.4.2", "2.5.5" ]:
             self.targets[ ver ] = "http://downloads.sourceforge.net/winflexbison/win_flex_bison-%s.zip" % ver
         self.targetDigests['2.4.2'] = '9e6a3a0c2ca89c1afa068aa0a055c04f5e19b722'
         self.targetDigests['2.5.5'] = 'b86d22393f2e601523b60e529cecbd963628d4e8'
-        self.defaultTarget = "2.5.5"
+        self.defaultTarget = "2.4.2"
 
     def setDependencies( self ):
         self.buildDependencies['gnuwin32/wget'] = 'default'
