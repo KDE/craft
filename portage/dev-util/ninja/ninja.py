56722f3fbcb3e95b2d30b56cf388803f6b33919b
Patrick von Reth <vonreth@kde.org>
2015-07-21 12:20:42 +0200

    updated ninja

--- portage/dev-util/ninja/ninja.py
+++ portage/dev-util/ninja/ninja.py
@@ -9,16 +9,12 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         """ """
         self.svnTargets['gitHEAD'] = "git://github.com/martine/ninja.git"        
-        for ver in ["1.5.3" ]:
+        for ver in ["1.6.0" ]:
             self.targets[ ver ] = "https://github.com/martine/ninja/archive/v%s.tar.gz" % ver
             self.targetInstSrc[ ver ] = "ninja-%s" % ver
-        self.targetDigests['1.5.3'] = 'b3ff794461ff5e4e1e73fe6bd11e653bbe509e63'
-        self.patchToApply['1.5.3'] = ("ninja-1.5.3-20141203.diff",1)
-
-        if compiler.isMSVC2015():
-            self.defaultTarget = 'gitHEAD'
-        else:
-            self.defaultTarget = '1.5.3'
+            self.archiveNames[ ver] = "ninja-%s.tar.gz" % ver
+            self.defaultTarget = ver
+        self.targetDigests['1.6.0'] = 'a6ff055691f6d355234298c21cc18961b4ca2ed9'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -34,7 +30,7 @@ class Package(CMakePackageBase):
     def make(self):
         self.enterSourceDir()
         command = "python configure.py --bootstrap"
-        if compiler.isMinGW() and self.subinfo.buildTarget not in ("1.1.0", "1.2.0"):
+        if compiler.isMinGW():
             command += " --platform=mingw"
         print(command)
         return self.system( command, "make" )
