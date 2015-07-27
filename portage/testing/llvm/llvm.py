db5818d493677775fa79e3959f2b363169892270
Kevin Funk <kfunk@kde.org>
2015-07-27 12:50:55 +0200

    llvm: Update to 3.6.2, cleanup .py
    Don't compile cross-compilation support for now (this reduces the
    targets to build from ~1500 to ~1000)

--- portage/testing/llvm/llvm.py
+++ portage/testing/llvm/llvm.py
@@ -5,10 +5,16 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = "http://llvm.org/git/llvm.git"
-        self.targets[ "3.5.0" ] = "http://llvm.org/releases/3.5.0/llvm-3.5.0.src.tar.xz"
-        self.targetInstSrc[ "3.5.0" ] = "llvm-3.5.0.src"
-        self.targetDigests['3.5.0'] = '58d817ac2ff573386941e7735d30702fe71267d5'
-        self.defaultTarget = "3.5.0"
+        releaseVersion = "3.6.2"
+        self.targets[releaseVersion] = "http://llvm.org/releases/" + releaseVersion + "/llvm-" + releaseVersion + ".src.tar.xz"
+        self.targetInstSrc[releaseVersion] = "llvm-" + releaseVersion + ".src"
+        self.targetDigests[releaseVersion] = '7a00257eb2bc9431e4c77c3a36b033072c54bc7e'
+        self.defaultTarget = releaseVersion
+
+    def setBuildOptions(self):
+        info.infoclass.setBuildOptions(self)
+
+        self.options.configure.defines = '-DLLVM_TARGETS_TO_BUILD="X86"'
 
     def setDependencies( self ):
         self.dependencies['virtual/base'] = 'default'
@@ -19,7 +25,7 @@ class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
         # this program needs python 2.7
-        self.subinfo.options.configure.defines = "-DPYTHON_EXECUTABLE=%s/python.exe" % emergeSettings.get("Paths","PYTHON27","").replace("\\","/")
+        self.subinfo.options.configure.defines += " -DPYTHON_EXECUTABLE=%s/python.exe" % emergeSettings.get("Paths","PYTHON27","").replace("\\","/")
 
     def configure(self):
         if not ("Paths","Python27") in emergeSettings:
