898210eb8ea8f307cdcbe5b29c16cd4d1e904cb8
Michael Abrahams <miabraha@gmail.com>
2015-08-15 17:31:12 -0400

    Update Vc to Git Upstream

--- portage/win32libs/vc/vc.py
+++ portage/win32libs/vc/vc.py
@@ -7,8 +7,14 @@ class subinfo(info.infoclass):
         self.targetDigests['0.7.3'] = 'aa41aeddac59abc60f292de8fdedbe70a4b49108'
         self.targetInstSrc['0.7.3'] = "Vc-0.7.3"
         self.patchToApply['0.7.3'] = ("Vc-0.7.3-20140107.diff", 1)
+
+        # Note: at the moment Vc does not provide a stable MSVC-compatible release.
+        # Please update the default target to a tarball once one is made available.
+        self.svnTargets['gitHEAD'] = 'https://github.com/VcDevel/Vc.git|0.7'
+        self.targetSrcSuffix['gitHEAD'] = 'git'
+        self.targetConfigurePath['gitHEAD'] = ''
         self.shortDescription = 'Portable, zero-overhead SIMD library for C++'
-        self.defaultTarget = '0.7.3'
+        self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -18,3 +24,6 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__( self )
 
 
+        self.subinfo.options.configure.defines = " -DBUILD_TESTING=OFF "
+        if compiler.isMSVC():
+            self.subinfo.options.configure.defines += " -DCMAKE_CXX_FLAGS=/FS "
