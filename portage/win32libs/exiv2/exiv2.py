a8dd17bf2e930df885d0bb77a3c7de70cc20a5f5
Michael Abrahams <miabraha@gmail.com>
2015-08-15 17:22:18 -0400

    Update to Exiv 0.25

--- portage/win32libs/exiv2/exiv2.py
+++ portage/win32libs/exiv2/exiv2.py
@@ -3,16 +3,17 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
 
-        for ver in ['0.24']:
+        for ver in ['0.24', '0.25']:
             self.targets[ver]       = 'http://www.exiv2.org/exiv2-%s.tar.gz' % ver
             self.targetInstSrc[ver] = 'exiv2-%s' % ver
         self.targetDigests['0.24'] = '2f19538e54f8c21c180fa96d17677b7cff7dc1bb'
         self.patchToApply['0.24'] = ('exiv2-0.22-20120117.diff', 1)
+        self.targetDigests['0.25'] = 'adb8ffe63916e7c27bda9792e690d1330ec7273d'
             
         self.svnTargets['svnHEAD'] = 'svn://dev.exiv2.org/svn/trunk'
 
         self.shortDescription = "an image metadata library"
-        self.defaultTarget = '0.24'
+        self.defaultTarget = '0.25'
 
     def setDependencies( self ):
         self.dependencies['win32libs/win_iconv']    = 'default'
