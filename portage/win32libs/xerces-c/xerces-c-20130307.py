d0e89b4cdb1e5929834ef415485dda917b877719
Patrick Spendrin <ps_ml@gmx.de>
2015-08-19 14:38:49 +0200

    update xerces-c again for msvc

--- portage/win32libs/xerces-c/xerces-c-20130307.py
+++ portage/win32libs/xerces-c/xerces-c-20130307.py
@@ -3,21 +3,20 @@ import info
 class subinfo( info.infoclass ):
 
     def setTargets( self ):
-        for ver in [ '3.1.1' ]:
+        for ver in [ '3.1.2' ]:
             self.targets[ ver ] = 'http://www.apache.org/dist/xerces/c/3/sources/xerces-c-' + ver + '.tar.gz'
             self.targetInstSrc[ ver ] = 'xerces-c-' + ver
-        self.targetDigests['3.1.1'] = '177ec838c5119df57ec77eddec9a29f7e754c8b2'
+        self.targetDigests['3.1.2'] = '3f9ecc4956df069c1d95b885fc687eb4e474a4ad'
+        self.patchToApply['3.1.2'] = [("xerces-c-3.1.2-20150623.diff", 1)]
 
         self.shortDescription = "A Validating XML Parser"
-        self.defaultTarget = '3.1.1'
+        self.defaultTarget = '3.1.2'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
-        if compiler.isMSVC():
-            self.buildDependencies['binary/xerces-c-bin'] = 'default'
 
 from Package.AutoToolsPackageBase import *
-from Package.VirtualPackageBase import *
+from Package.CMakePackageBase import *
 
 class PackageMSys(AutoToolsPackageBase):
     def __init__( self ):
@@ -30,11 +29,10 @@ if compiler.isMinGW():
         def __init__( self ):
             PackageMSys.__init__( self )
 else:
-    class Package(VirtualPackageBase):
+    class Package(CMakePackageBase):
         def __init__( self ):
             self.subinfo = subinfo()
-            VirtualPackageBase.__init__( self )
-            
+            CMakePackageBase.__init__( self )
 
 if __name__ == '__main__':
       Package().execute()
