9085a56c95d7837b5d4c4c7efcf45cd8df753e2e
Patrick Spendrin <ps_ml@gmx.de>
2015-08-19 16:48:18 +0200

    build a newer version of gpg-error

--- portage/win32libs/gpg-error/gpg-error-1.10.py
+++ portage/win32libs/gpg-error/gpg-error-1.10.py
@@ -6,7 +6,7 @@ import compiler
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['1.9', '1.10', '1.12']:
+        for ver in ['1.9', '1.10', '1.12', '1.19']:
             self.targets[ver] = 'ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = 'libgpg-error-' + ver
         self.patchToApply['1.9'] = ('libgpg-error-1.9-20100801.diff', 1)
@@ -14,8 +14,11 @@ class subinfo(info.infoclass):
         self.targetDigests['1.9'] = '6836579e42320b057a2372bbcd0325130fe2561e'
         self.targetDigests['1.10'] = '95b324359627fbcb762487ab6091afbe59823b29'
         self.targetDigests['1.12'] = '259f359cd1440b21840c3a78e852afd549c709b8'
+        self.targetDigests['1.19'] = '4997951ab058788de48b989013668eb3df1e6939'
         self.patchToApply['1.12'] = [('libgpg-error-r267-20101205.diff', 1),
                                      ('libgpg-error-cmake-1.12.diff', 1)]
+        self.patchToApply['1.19'] = [('libgpg-error-r267-20101205.diff', 1),
+                                     ('libgpg-error-cmake-1.19.diff', 1)]
         # NOTE: the libgpg-error-cmake*.diff file contains the package number
         # in ConfigureChecks.cmake and must thus be changed with every version change!
 
