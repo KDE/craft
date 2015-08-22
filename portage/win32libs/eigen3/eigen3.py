dc4a96819cec34aef8da9055a2ee791a301cd21a
Michael Abrahams <miabraha@gmail.com>
2015-08-22 11:10:13 -0400

    Do not duplicate eigen3

--- portage/win32libs/eigen3/eigen3.py
+++ portage/win32libs/eigen3/eigen3.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 import info
+import compiler
 from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['3.2.4']:
+        for ver in ['3.2.5']:
             self.targets[ver] = 'http://bitbucket.org/eigen/eigen/get/%s.tar.bz2' % ver
             self.archiveNames[ver] = "eigen-%s.tar.bz2" % ver
-        self.targetInstSrc['3.2.4'] = 'eigen-eigen-10219c95fe65'
-        self.targetDigests['3.2.4'] = '64ea809acc449adbd8fe616def7d48ff4f0776a8'
+        self.targetInstSrc['3.2.5'] = 'eigen-eigen-bdd17ee3b1b3'
+        self.targetDigests['3.2.5'] = 'aa4667f0b134f5688c5dff5f03335d9a19aa9b3d'
 
         self.shortDescription = 'C++ template library for linear algebra'
-        self.defaultTarget = '3.2.4'
+        self.defaultTarget = '3.2.5'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -19,4 +20,5 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__( self )
-
+        if compiler.isMSVC():
+            self.subinfo.options.configure.defines = "-DBUILD_TESTS=OFF"
