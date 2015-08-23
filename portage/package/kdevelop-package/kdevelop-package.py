36ae18aed05014a36b1400f71af93e4ebc36f2e0
Gleb Popov <6yearold@gmail.com>
2015-08-23 18:14:41 +0300

    Use custom NSIS script for kdevelop-package.
    Filter out *.pdb's and other debugging stuff (stole this from amarok blacklist).
    
    Create etc/dbus-1/session.d directory upon installation. dbus-daemon --session was failing to start because of absence of this dir.

--- portage/package/kdevelop-package/kdevelop-package.py
+++ portage/package/kdevelop-package/kdevelop-package.py
@@ -19,5 +19,6 @@ class Package( NullsoftInstallerPackager, VirtualPackageBase ):
         blacklists = [ NSIPackagerLists.runtimeBlacklist, 'blacklist.txt', 'blacklist-mysql.txt', 'blacklist-virtuoso.txt' ]
         NullsoftInstallerPackager.__init__( self, blacklists=blacklists )
         VirtualPackageBase.__init__( self )
+        self.scriptname = os.path.join( os.path.dirname( __file__ ), "kdevelop.nsi" )
         self.defines[ "executable" ] = "bin\\kdevelop.exe"
 
