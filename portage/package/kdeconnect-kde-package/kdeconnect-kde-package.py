0fe5d13736dc50859d197e00c6c7f15e274f40e6
Patrick von Reth <vonreth@kde.org>
2015-09-11 17:23:15 +0200

    Add kdeconnect-package formula

---
+++ portage/package/kdeconnect-kde-package/kdeconnect-kde-package.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.VirtualPackageBase import *
+from Packager.PortablePackager import *
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        for pack in installdb.getInstalledPackages('qt-apps','kdeconnect-kde'):
+            version = pack.getVersion()
+            if version:
+                self.targets[ version ] = ""
+                self.defaultTarget = version
+            gitVersion = pack.getRevision()
+            if gitVersion:
+                self.svnTargets[ gitVersion  ] = ""
+                self.defaultTarget = gitVersion
+
+
+    def setDependencies( self ):
+        self.dependencies[ 'qt-apps/kdeconnect-kde' ] = 'default'
+
+class Package( PortablePackager, VirtualPackageBase ):
+    def __init__( self, **args ):
+        whitelists = [ 'whitelist.txt' ]
+        blacklists = [ 'blacklist.txt']
+        PortablePackager.__init__( self, whitelists, blacklists )
+        VirtualPackageBase.__init__( self )
+
