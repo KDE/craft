221774c5c9d4f413f3f1343c7c19ad1944b6e72d
Patrick Spendrin <ps_ml@gmx.de>
2015-10-04 23:10:29 +0200

    Add a build for lmdb
    LMDB is a small and fast key value store which is used in baloo.
    patch adds CMake build system and fixes problems with unistd.h

---
+++ portage/win32libs/lmdb/lmdb.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        for ver in [ '0.9.16' ]:
+            self.targets[ ver ] = 'https://github.com/LMDB/lmdb/archive/LMDB_' + ver + '.tar.gz'
+            self.targetInstSrc[ ver ] = 'lmdb-LMDB_' + ver + '/libraries/liblmdb'
+        self.patchToApply[ '0.9.16' ] = [('lmdb-LMDB_0.9.16-20151004.diff', 3)]
+        self.targetDigests[ '0.9.16' ] = '367182e1d9dbc314db76459a71be719209f131b4'
+
+        self.shortDescription = 'in memory database from the openldap project'
+        self.defaultTarget = '0.9.16'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.defines = "-DBUILD_TESTS=OFF -DBUILD_TOOLS=OFF"
+
