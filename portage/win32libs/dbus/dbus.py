aedc37c9a9a218442f92dfd549398e5e598fe5e4
Hannah von Reth <vonreth@kde.org>
2015-11-25 13:08:53 +0100

    Update dbus

--- portage/win32libs/dbus/dbus.py
+++ portage/win32libs/dbus/dbus.py
@@ -4,7 +4,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['1.6.8','1.6.14', '1.8.4']:
+        for ver in ['1.8.4', '1.10.4']:
             self.targets[ver] = 'http://dbus.freedesktop.org/releases/dbus/dbus-%s.tar.gz' % ver
             self.targetInstSrc[ver] = 'dbus-%s' % ver
             self.targetConfigurePath[ver] = 'cmake'
@@ -12,13 +12,14 @@ class subinfo(info.infoclass):
         self.svnTargets['gitHEAD'] = 'git://anongit.freedesktop.org/git/dbus/dbus'
         self.targetSrcSuffix['gitHEAD'] = 'git'
         self.targetConfigurePath['gitHEAD'] = 'cmake'
-        
-        self.patchToApply['1.6.14'] = [('dont_include_afxres.diff', 1)]
+
         self.patchToApply['1.8.4'] = [('dont_include_afxres.diff', 1)]
+        self.patchToApply['1.10.4'] = [('dont_include_afxres.diff', 1)]
+        self.targetDigests['1.10.4'] = 'ec1921a09199c81ea20b20448237146a414d51ae'
 
         self.shortDescription = "Freedesktop message bus system (daemon and clients)"
         self.homepage = "http://www.freedesktop.org/wiki/Software/dbus/"
-        self.defaultTarget = '1.8.4'
+        self.defaultTarget = '1.10.4'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -33,32 +34,19 @@ class Package(CMakePackageBase):
         self.subinfo.options.make.slnBaseName = 'dbus'
         self.subinfo.options.configure.defines = (
                 "-DDBUS_BUILD_TESTS=OFF "
-                "-DDBUS_ENABLE_XML_DOCS=OFF "
-                "-DDBUS_USE_EXPAT=ON "
-                "-DDBUS_REPLACE_LOCAL_DIR=ON ")
+                "-DDBUS_ENABLE_XML_DOCS=OFF ")
 
         if (self.buildType() == "Release"):
             self.subinfo.options.configure.defines += (
                     "-DDBUS_ENABLE_VERBOSE_MODE=OFF "
                     "-DDBUS_DISABLE_ASSERTS=ON ")
 
-        if self.buildTarget == "gitHEAD" or self.buildTarget == "1.8.4":
-            self.subinfo.options.configure.defines += (
-                "-DDBUS_SESSION_BUS_LISTEN_ADDRESS:STRING=autolaunch:scope=*install-path "
-                "-DDBUS_SESSION_BUS_CONNECT_ADDRESS:STRING=autolaunch:scope=*install-path ")
-            # kde uses debugger output, so dbus should do too
-            self.subinfo.options.configure.defines += (
-                    "-DDBUS_USE_OUTPUT_DEBUG_STRING=ON ")
-        else:
-            # for 1.4.1 and greater switch to official
-            # supported scopes -> autolaunch:scope=*install-path
-            self.subinfo.options.configure.defines += (
-                    "-DDBUS_SESSION_BUS_DEFAULT_ADDRESS:"
-                    "STRING=autolaunch:scope=install-path ")
-            # kde uses debugger output, so dbus should do too
-            # not sure if this works for wince too, so limited to win32
-            self.subinfo.options.configure.defines += (
-                    "-DDBUS_USE_OUTPUT_DEBUG_STRING=ON ")
+        self.subinfo.options.configure.defines += (
+            "-DDBUS_SESSION_BUS_LISTEN_ADDRESS:STRING=autolaunch:scope=*install-path "
+            "-DDBUS_SESSION_BUS_CONNECT_ADDRESS:STRING=autolaunch:scope=*install-path ")
+        # kde uses debugger output, so dbus should do too
+        self.subinfo.options.configure.defines += (
+                "-DDBUS_USE_OUTPUT_DEBUG_STRING=ON ")
 
     def install( self ):
         if not CMakePackageBase.install( self ): return False
