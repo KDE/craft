6a1bed1641ed6a05dbdae8d07cdae60579940085
Hannah von Reth <vonreth@kde.org>
2015-12-03 01:00:31 +0100

    update okular deps

--- portage/kde/applications/okular/okular.py
+++ portage/kde/applications/okular/okular.py
@@ -4,6 +4,7 @@ from EmergeConfig import *
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.versionInfo.setDefaultValues( )
+        self.svnTargets['master'] = "[git]kde:okular|frameworks"
         self.shortDescription = "KDE document viewer"
 
     def setDependencies( self ):
@@ -19,11 +20,22 @@ class subinfo(info.infoclass):
         self.dependencies['win32libs/ebook-tools'] = 'default'
         self.buildDependencies['win32libs/libspectre'] = 'default'
         self.dependencies['binary/ghostscript'] = 'default'
-        self.dependencies['frameworks/khtml'] = 'default'
-        self.dependencies['frameworks/kactivities'] = 'default'
-        self.dependencies['frameworks/kdelibs4support'] = 'default'
         self.dependencies['kde/libkexiv2'] = 'default'
-        
+        self.dependencies['frameworks/kactivities'] = 'default'
+        self.dependencies['frameworks/karchive'] = 'default'
+        self.dependencies['frameworks/kbookmarks'] = 'default'
+        self.dependencies['frameworks/kconfig'] = 'default'
+        self.dependencies['frameworks/kconfigwidgets'] = 'default'
+        self.dependencies['frameworks/kcoreaddons'] = 'default'
+        self.dependencies['frameworks/kdbusaddons'] = 'default'
+        self.dependencies['frameworks/kjs'] = 'default'
+        self.dependencies['frameworks/kdelibs4support'] = 'default'
+        self.dependencies['frameworks/kio'] = 'default'
+        self.dependencies['frameworks/kparts'] = 'default'
+        self.dependencies['frameworks/threadweaver'] = 'default'
+        self.dependencies['frameworks/kwallet'] = 'default'
+        self.dependencies['frameworks/khtml'] = 'default'
+
 from Package.CMakePackageBase import *
 
 class Package( CMakePackageBase ):
