8c36d927f6fa25fc646ec191cb965b848f21377a
Hannah von Reth <vonreth@kde.org>
2015-12-27 18:44:02 +0100

    Fix issue of dbus locking build dirs

--- bin/Notifier/Backends/Snore.py
+++ bin/Notifier/Backends/Snore.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import ctypes
+import EmergeConfig
 
 from Notifier.NotificationInterface import *
 
@@ -14,7 +15,8 @@ class Snore(NotificationInterface):
         icon = os.path.join(path, "..", "kde-logo.png" )
         wid = ctypes.windll.kernel32.GetConsoleWindow()
         try:
-            subprocess.Popen( """snoresend -t "%s" -m "%s" -i "%s" -a "Emerge" -c "%s" --silent --bring-window-to-front %s""" % (title , message , icon, alertClass, wid))
+            subprocess.Popen( """snoresend -t "%s" -m "%s" -i "%s" -a "Emerge" -c "%s" --silent --bring-window-to-front %s""" % (title , message , icon, alertClass, wid),
+                              cwd = EmergeConfig.EmergeStandardDirs.emergeRoot())# make sure that nothing is spawned in a build dir
         except Exception:
             return
 
