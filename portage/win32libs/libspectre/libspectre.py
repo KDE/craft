b0e0c1de11dfb8056e707dff947cea7cb6801fe9
Patrick Spendrin <ps_ml@gmx.de>
2015-12-29 01:09:17 +0100

    fix building libspectre with ghosty

--- portage/win32libs/libspectre/libspectre.py
+++ portage/win32libs/libspectre/libspectre.py
@@ -8,7 +8,7 @@ class subinfo(info.infoclass):
             self.targetInstSrc[ver] = "libspectre-" + ver
         self.patchToApply["0.2.1"] = ("spectre-0.2.1-cmake.diff", 1)
         self.patchToApply["0.2.6"] = ("libspectre-0.2.6-20101117.diff", 1)
-        self.patchToApply["0.2.7"] = ("libspectre-0.2.7-20131003.diff", 1)
+        self.patchToApply["0.2.7"] = [("libspectre-0.2.7-20131003.diff", 1), ("libspectre-new-ghostscript.diff", 1)]
         self.targetDigests['0.2.6'] = '819475c7e34a1e9bc2e876110fee530b42aecabd'
         self.targetDigests['0.2.7'] = 'a7efd97b82b84ff1bb7a0d88c7e35ad10cc84ea8'
 
@@ -16,7 +16,7 @@ class subinfo(info.infoclass):
         self.defaultTarget = '0.2.7'
 
     def setDependencies( self ):
-        self.dependencies['binary/ghostscript'] = 'default'
+        self.dependencies['testing/ghosty'] = 'default'
         self.buildDependencies['virtual/base'] = 'default'
 
 from Package.CMakePackageBase import *
