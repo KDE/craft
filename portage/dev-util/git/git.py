163abbdd2281c5429da7974813e6865e3adc81fc
Hannah von Reth <vonreth@kde.org>
2016-01-04 22:07:52 +0100

    Update git

--- portage/dev-util/git/git.py
+++ portage/dev-util/git/git.py
@@ -8,7 +8,7 @@ import utils
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = "2.6.3"
+        ver = "2.6.4"
         arch = 32
         if compiler.isX64():
             arch = 64
