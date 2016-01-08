99fada32c8aeda71d5e96c0bf81263f23b97777f
Hannah von Reth <vonreth@kde.org>
2016-01-08 17:37:16 +0100

    Add digests for 7zip

--- portage/dev-util/7zip/7zip.py
+++ portage/dev-util/7zip/7zip.py
@@ -7,6 +7,7 @@ class subinfo( info.infoclass ):
             self.targets[ ver ] = "http://downloads.sourceforge.net/sourceforge/sevenzip/7z%s-extra.7z" % ver
             self.targetInstallPath[ ver ] = "bin"
         self.targetDigests['1512'] = '0772dcf51fd7d22ac8ac04976e8c2a2f8cbe5ccd'
+        self.targetDigests['1514'] = (['4fb7b51e93cabbede23281eae0d024a63f485dc339c85e20c305f328a76e90c0'], EmergeHash.HashAlgorithm.SHA256)
         self.defaultTarget = '1514'
 
 
