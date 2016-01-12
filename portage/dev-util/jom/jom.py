2bea06e3743bdc41086ec1cae3d89bcb527015a0
Hannah von Reth <vonreth@kde.org>
2016-01-12 14:24:55 +0100

    Use provided md5 digests for jom

--- portage/dev-util/jom/jom.py
+++ portage/dev-util/jom/jom.py
@@ -12,8 +12,7 @@ class subinfo(info.infoclass):
     def setTargets( self ):
         for ver in ['1_0_14','1_0_15','1_0_16']:
             self.targets[ver] = 'http://download.qt.io/official_releases/jom/jom_' + ver + '.zip'
-        self.targetDigests['1_0_14'] = '5ae36ead8f0a877578b961acea80705416d23374'
-        self.targetDigests['1_0_16'] = 'c4149fe706b25738b4c4e54c73e180b9cab55832'
+            self.targetDigestUrls[ver] = (["http://qt-mirror.dannhauer.de/official_releases/jom/md5sums.txt"], EmergeHash.HashAlgorithm.MD5)
         self.defaultTarget = '1_0_16'
 
 
