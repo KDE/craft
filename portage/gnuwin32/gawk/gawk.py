608bde9483ae3eec4b561a5c9173d3df06d8dab4
Hannah von Reth <vonreth@kde.org>
2016-01-13 17:26:32 +0100

    fix gawk

--- portage/gnuwin32/gawk/gawk.py
+++ portage/gnuwin32/gawk/gawk.py
@@ -1,12 +1,8 @@
 import info
 
-SRC_URI = """
-http://downloads.sourceforge.net/sourceforge/gnuwin32/gawk-3.1.6-1-bin.zip
-"""
-
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['3.1.6-1'] = SRC_URI
+        self.targets['3.1.6-1'] = "http://downloads.sourceforge.net/sourceforge/gnuwin32/gawk-3.1.6-1-bin.zip"
         self.targetDigests['3.1.6-1'] = 'bda507655eb3d15059d8a55a0daf6d697a15f632'
         self.defaultTarget = '3.1.6-1'
 
