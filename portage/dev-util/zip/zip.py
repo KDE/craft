6f1c70b8cb5db51e2600ebe5070aca7daf41d95f
Hannah von Reth <vonreth@kde.org>
2016-01-14 13:40:24 +0100

    fix zip

--- portage/dev-util/zip/zip.py
+++ portage/dev-util/zip/zip.py
@@ -6,12 +6,10 @@ class subinfo(info.infoclass):
         repoUrl = """http://downloads.sourceforge.net/gnuwin32"""
 
         for version in ['2.31', '3.0']:
-            self.targets[ version ] = repoUrl + """/zip-""" + version + """-bin.zip
-                                """ + repoUrl + """/zip-""" + version + """-lib.zip
-                                """ + repoUrl + """/zip-""" + version + """-dep.zip"""
+            self.targets[ version ] = [repoUrl + """/zip-""" + version + """-bin.zip""",
+                                       repoUrl + """/zip-""" + version + """-dep.zip"""]
 
         self.targetDigests['3.0'] = ['58ae2b1f3e19811a1888f155c98297f763a4c5e7',
-                                     '81a004049348ab458cfcb4c4bdd2e42bf970fd4b',
                                      '1326746e38470a04e58fa2146d3455b81265e0d8']
         self.defaultTarget = '3.0'
 
