756264e18cf98e6aa4abb75bdc908c9fafd2e260
Hannah von Reth <vonreth@kde.org>
2016-01-15 12:14:23 +0100

    Add compiler and architecture promp

--- setup/install_emerge.ps1
+++ setup/install_emerge.ps1
@@ -2,9 +2,46 @@
 $Script:pythonUrl = "https://www.python.org/ftp/python/3.5.1/python-3.5.1.exe"
 
 $Script:installRoot = "C:\kde"
+$Script:architecture = "x86"
+$Script:compiler = "mingw4"
 #####
 $Script:python = where.exe python 2>$NULL
 $Script:pythonVersion = "0"
+
+function SelectArchitecture()
+{
+    switch($host.UI.PromptForChoice("Emerge build architecture", "Please select build architecture.",
+         @("x&86", "x&64"),
+        0))
+        {
+            0 {
+                $Script:architecture = "x86"
+                break
+            }
+            1 {
+                $Script:architecture = "x64"
+                break
+            }
+        }
+}
+
+
+function SelectCompiler()
+{
+    switch($host.UI.PromptForChoice("Emerge compiler", "Please select the compiler.",
+         @("&MinGW-w64", "Microsoft &Visual Studio 2015"),
+        0))
+        {
+            0 {
+                $Script:compiler = "mingw4"
+                break
+            }
+            1 {
+                $Script:compiler = "msvc2015"
+                break
+            }
+        }
+}
 
 function FetchPython()
 {
@@ -32,6 +69,13 @@ function FetchPython()
         }
 }
 
+function SetSetting([string[]] $ini, [string] $key, [string] $value)
+{
+    Write-Host ("^\s*{0}\s*=.*`$" -f $key), ("{0} = {1}" -f $key, $value)
+    $ini = $ini -replace ("^\s*{0}\s*=.*`$" -f $key), ("{0} = {1}" -f $key, $value)
+    return $ini
+}
+
 function FetchEmerge()
 {
     $Script:emergeTar = "$Script:installRoot\download\emerge.zip"
@@ -41,14 +85,37 @@ function FetchEmerge()
     }
     & "$Script:python" "-c" "import shutil; shutil.unpack_archive('$Script:emergeTar','$Script:installRoot')"
     mv "$Script:installRoot\emerge-master" "$Script:installRoot\emerge"
-    $content = (Get-Content "$Script:installRoot\emerge\kdesettings.ini") -replace "^Python = .*`$", ("Python = {0}" -f $Script:python.Substring(0,$Script:python.LastIndexOf("\"))) 
-    [System.IO.File]::WriteAllLines("$Script:installRoot\etc\kdesettings.ini" , $content)
+}
+
+function TestPython()
+{
+    if($Script:python -ne $NULL)
+    {  
+    if(($Script:python -split "\r\n").Length -eq 1)
+        {
+            (& "$Script:python" "--version" ) -match "\d.\d.\d" | Out-Null
+            $Script:pythonVersion = $Matches[0]    
+            if( [int]::Parse($Script:minPythonVersion -replace "\.") -ge [int]::Parse($Script:pythonVersion -replace "\."))
+            {
+                Write-Host "We found $Script:python version $Script:pythonVersion which is to old."
+            }
+        }
+        else
+        {
+            Write-Host "We failed to determine your python version."
+        }
+    }
+    else
+    {
+        Write-Host "We couldn't find python."
+    }
 }
 ####################################################
 # Start
 Write-Host "Start to boostrap emerge."
 Write-Host "Where to you want us to install emerge"
 $Script:installRoot = if (($result = Read-Host "Emerge install root: [$Script:installRoot]") -eq '') {$Script:installRoot} else {$result}
+
 
 if(Test-Path -Path $Script:installRoot){
     Write-Host "Directory $Script:installRoot already exists. Exiting."
@@ -58,25 +125,28 @@ mkdir $Script:installRoot -Force | Out-Null
 mkdir $Script:installRoot\download -Force | Out-Null
 mkdir $Script:installRoot\etc -Force | Out-Null
 
-if($Script:python -ne $NULL)
+if(!(TestPython))
 {
-    (& "$Script:python" "--version") -match "\d.\d.\d" | Out-Null
-    $Script:pythonVersion = $Matches[0]    
-    if( [int]::Parse($Script:minPythonVersion -replace "\.") -ge [int]::Parse($Script:pythonVersion -replace "\."))
-    {
-        Write-Host "We found $Script:python version $Script:pythonVersion which is to old."
-        FetchPython
-    }
-}
-else
-{
-    Write-Host "We couldn't find python."
     FetchPython
 }
+SelectArchitecture
+SelectCompiler
 
 FetchEmerge
+
+$iniContent = (Get-Content "$Script:installRoot\emerge\kdesettings.ini")
+Write-Host $iniContent.GetType().FullName
+$iniContent = SetSetting $iniContent "Python" $Script:python.Substring(0,$Script:python.LastIndexOf("\"))
+$iniContent = SetSetting $iniContent "Architecture" $Script:architecture
+$iniContent = SetSetting $iniContent "KDECOMPILER" $Script:compiler
+
+[System.IO.File]::WriteAllLines("$Script:installRoot\etc\kdesettings.ini" , $iniContent)
 . $Script:installRoot\emerge\kdeenv.ps1
 emerge git
 rm -Force -Recurse $Script:installRoot\emerge
 git clone kde:emerge $Script:installRoot\emerge
 cd $Script:installRoot
+
+#############
+Write-Host "You are ready to go!"
+Write-Host "Type `"emerge openssl`" to build your firts application."
