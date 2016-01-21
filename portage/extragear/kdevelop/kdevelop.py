f8459d241adc38b2c4caaf1b7b94f50b92b0344e
Kevin Funk <kfunk@kde.org>
2016-01-21 08:52:02 +0100

    kdevelop: Restore icon for installer

--- portage/extragear/kdevelop/kdevelop.py
+++ portage/extragear/kdevelop/kdevelop.py
@@ -61,6 +61,7 @@ class Package( CMakePackageBase, NullsoftInstallerPackager ):
     def createPackage(self):
         self.defines[ "productname" ] = "KDevelop"
         self.defines[ "executable" ] = "bin\\kdevelop.exe"
+        self.defines[ "icon" ] = "kdevelop.ico"
 
         self.ignoredPackages.append("binary/mysql-pkg")
 
