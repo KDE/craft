c5278cf02c23cf698033a136060f3cd74b0fbc2f
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2016-02-21 20:36:53 +0100

    Update rkward port - default to frameworks branch in git - adjust dependencies for frameworks branch - support building for 32 bit on 64 bit arch

--- portage/kdeapps/rkward/rkward.py
+++ portage/kdeapps/rkward/rkward.py
@@ -5,22 +5,32 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets[ 'svnHEAD' ] = 'http://svn.code.sf.net/p/rkward/code/trunk/rkward'
-        for ver in ['0.5.7', '0.6.0']:
-            self.targets[ver] = 'http://downloads.sourceforge.net/rkward/rkward-' + ver + '.tar.gz'
+        self.svnTargets[ 'gitHEAD' ] =  '[git]kde:rkward|frameworks'
+        for ver in ['0.6.4']:
+            self.targets[ver] = 'http://download.kde.org/stable/rkward/' + ver + '/rkward-' + ver + '.tar.gz'
             self.targetInstSrc[ ver] = 'rkward-' + ver
-        self.defaultTarget = 'svnHEAD'
+        self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.dependencies[ 'testing/r-base' ] = 'default'
-        self.dependencies[ 'kde/kate' ] = 'default'  # provides katepart
+        if self.buildTarget in ['0.6.4', '0.6.5']:
+            # Hm, will this still work at all? kate port does not seem to provide KDE 4 version, anymore
+            self.dependencies[ 'kde/kate' ] = 'default'  # provides katepart
+        else:
+            self.dependencies["frameworks/ki18n"] = "default"
+            self.dependencies["frameworks/ktexteditor"] = "default"
+            self.dependencies["frameworks/kwindowsystem"] = "default"
+            self.dependencies["frameworks/kdewebkit"] = "default"
 
 from Package.CMakePackageBase import *
 
 class Package( CMakePackageBase ):
     def __init__( self ):
         CMakePackageBase.__init__( self )
-        r_executable = os.path.join( self.mergeDestinationDir(), "lib", "R", "bin", "R.exe" )
+        if compiler.isX64():
+            r_executable = os.path.join( self.mergeDestinationDir(), "lib", "R", "bin", "x64", "R.exe" )
+        else:
+            r_executable = os.path.join( self.mergeDestinationDir(), "lib", "R", "bin", "i386", "R.exe" )
         self.subinfo.options.configure.defines = " -DR_EXECUTABLE=" + r_executable.replace( "\\\\", "/" )
 
 
