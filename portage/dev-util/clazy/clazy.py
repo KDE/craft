7537b192c3e62b56e53e817fceafe866490d55c3
Hannah von Reth <vonreth@kde.org>
2016-04-07 10:06:41 +0200

    Compile llvm dynamic if using mingw.
    This is needed to be able to use clazy.

--- portage/dev-util/clazy/clazy.py
+++ portage/dev-util/clazy/clazy.py
@@ -6,7 +6,7 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setDependencies( self ):
       self.buildDependencies['virtual/base'] = 'default'
-      self.dependencies['dev-util/clang'] = 'default'
+      self.dependencies['dev-util/llvm'] = 'default'
 
         
     def setTargets( self ):
@@ -17,5 +17,11 @@ class subinfo(info.infoclass):
 class Package( CMakePackageBase ):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
+        self.supportsClang = False
         self.subinfo.options.configure.defines = "-DCLAZY_ON_WINDOWS_HACK=ON"
 
+    def configureOptions(self, defines=""):
+        options = CMakePackageBase.configureOptions(self, defines)
+        # just expect that we don't want to debug our compiler
+        options += ' -DCMAKE_BUILD_TYPE=Release'
+        return options
--- portage/dev-util/llvm-meta/llvm/llvm.py
+++ portage/dev-util/llvm-meta/llvm/llvm.py
@@ -26,10 +26,24 @@ class Package(CMakePackageBase):
         self.subinfo.options.configure.defines = '-DLLVM_TARGETS_TO_BUILD="X86"'
         self.subinfo.options.configure.defines += " -DLLVM_EXTERNAL_LLD_SOURCE_DIR=\"%s\"" % portage.getPackageInstance('dev-util', 'lld').sourceDir().replace("\\", "/")
         self.subinfo.options.configure.defines += " -DLLVM_EXTERNAL_CLANG_SOURCE_DIR=\"%s\"" % portage.getPackageInstance('dev-util', 'clang').sourceDir().replace("\\", "/")
+        if compiler.isMinGW():
+            self.subinfo.options.configure.defines += " -DBUILD_SHARED_LIBS=ON"
 
     def configureOptions(self, defines=""):
         options = CMakePackageBase.configureOptions(self, defines)
-        if self.buildType().startswith("Rel"):
-            # forcing build in Release mode, RelWithDebInfo would take lots of disk space (around 10 G) and memory during link
-            options += ' -DCMAKE_BUILD_TYPE=Release'
+        # just expect that we don't want to debug our compiler
+        options += ' -DCMAKE_BUILD_TYPE=Release'
         return options
+
+    def install(self):
+        if not CMakePackageBase.install(self):
+            return False
+        if compiler.isMinGW():
+            files = os.listdir(os.path.join(self.buildDir(), "lib"))
+            for f in files:
+                if f.endswith("dll.a"):
+                    src = os.path.join(self.buildDir(), "lib", f)
+                    dest = os.path.join(self.imageDir(), "lib", f)
+                    if not os.path.exists(dest):
+                        utils.copyFile(src, dest, False)
+        return True
