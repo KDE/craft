94e1778818d9ff2665fcb62c489a632500509c74
Hannah von Reth <vonreth@kde.org>
2016-05-26 17:29:45 +0200

    Update ninja

--- portage/dev-util/ninja/ninja.py
+++ portage/dev-util/ninja/ninja.py
@@ -10,13 +10,14 @@ class subinfo(info.infoclass):
         self.buildDependencies['virtual/bin-base'] = 'default'
 
     def setTargets( self ):
-        for ver in ["1.6.0" ]:
+        for ver in ["1.6.0", "1.7.1" ]:
             self.targets[ ver ] = "https://github.com/ninja-build/ninja/releases/download/v%s/ninja-win.zip" % ver
             self.targetInstallPath[ ver ] = "bin"
             self.archiveNames[ ver] = "ninja-win%s.zip" % ver
 
         self.targetDigests['1.6.0'] = (['18f55bc5de27c20092e86ace8ef3dd3311662dc6193157e3b65c6bc94ce006d5'], EmergeHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.6.0"
+        self.targetDigests['1.7.1'] = (['4221b2cef768de64799077ba83ca9ca28e197415a34257ed8d54f93e899afc4e'], EmergeHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.7.1"
 
 
 
