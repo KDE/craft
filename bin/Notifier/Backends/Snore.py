6dd334fa810842498ac673bd1e17bd6e63dcaf89
Hannah von Reth <vonreth@kde.org>
2016-06-08 11:09:41 +0200

    Fix snore notifications on linux

--- bin/Notifier/Backends/Snore.py
+++ bin/Notifier/Backends/Snore.py
@@ -3,6 +3,8 @@ import subprocess
 import ctypes
 import EmergeConfig
 
+from EmergeOS.osutils import OsUtils
+
 from Notifier.NotificationInterface import *
 
 
@@ -13,10 +15,13 @@ class Snore(NotificationInterface):
     def notify(self,title,message,alertClass):
         path = os.path.dirname(os.path.realpath(__file__))
         icon = os.path.join(path, "..", "kde-logo.png" )
-        wid = ctypes.windll.kernel32.GetConsoleWindow()
         try:
-            subprocess.Popen( """snoresend -t "%s" -m "%s" -i "%s" -a "Emerge" -c "%s" --silent --bring-window-to-front %s""" % (title , message , icon, alertClass, wid),
+            command = """snoresend -t "%s" -m "%s" -i "%s" -a "Emerge" -c "%s" --silent """ % (title , message , icon, alertClass)
+            if OsUtils.isWin():
+                command += " --bring-window-to-front %s" % ctypes.windll.kernel32.GetConsoleWindow()
+            subprocess.Popen( command,
+                              shell = True,
                               cwd = EmergeConfig.EmergeStandardDirs.emergeRoot())# make sure that nothing is spawned in a build dir
-        except Exception:
+        except Exception as e:
             return
 
