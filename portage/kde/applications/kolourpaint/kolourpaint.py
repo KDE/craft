46e1ac04bfe9c928ccae4904872a52f14e003f9f
Albert Vaca <albertvaka@gmail.com>
2016-06-15 16:02:57 +0200

    Fixed files not being where they should in the package for kdeconnect and kolourpaint

--- portage/kde/applications/kolourpaint/kolourpaint.py
+++ portage/kde/applications/kolourpaint/kolourpaint.py
@@ -29,7 +29,7 @@ class Package( CMakePackageBase ):
             PackagerLists.runtimeBlacklist,
             os.path.join(os.path.dirname(__file__), "blacklist.txt")
         ]
-        self.changePackager( NullsoftInstallerPackager )
+        #self.changePackager( NullsoftInstallerPackager )
 
     def createPackage(self):
         self.defines[ "productname" ] = "Kolourpaint"
@@ -42,3 +42,14 @@ class Package( CMakePackageBase ):
         self.ignoredPackages.append("frameworks/kemoticons")
 
         return TypePackager.createPackage(self)
+
+    def preArchive(self):
+        archiveDir = self.archiveDir()
+
+        # move everything to the location where Qt expects it
+        binPath = os.path.join(archiveDir, "bin")
+        utils.mergeTree(os.path.join(archiveDir, "plugins"), binPath)
+        utils.mergeTree(os.path.join(archiveDir, "qml"), os.path.join(archiveDir, binPath))
+
+        # TODO: Just blacklisting this doesn't work. WTF?
+        utils.rmtree(os.path.join(archiveDir, "dev-utils"))
