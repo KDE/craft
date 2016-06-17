92bf0b2601221cc6a949dcd23c1f1ee615b909da
Hannah von Reth <vonreth@kde.org>
2016-06-17 11:08:22 +0200

    Cleanup getPackageVersion

--- bin/Packager/PackagerBase.py
+++ bin/Packager/PackagerBase.py
@@ -27,28 +27,14 @@ class PackagerBase(EmergeBase):
         """ return version information for the currently used package"""
         if self.subinfo.options.package.version != None:
             pkgVersion = self.subinfo.options.package.version
+        elif self.subinfo.hasSvnTarget():
+            pkgVersion = self.source.sourceVersion()
         else:
-            versionFile = os.path.join(self.buildDir(),'emerge-package-version')
-            if os.path.exists(versionFile):
-                with open( versionFile, "r" ) as f:
-                    pkgVersion = f.read()
-            elif self.version == "all": 
-                if self.subinfo.buildTarget == "gitHEAD":
-                    pkgVersion = str( datetime.date.today() ).replace('-', '')  + '-' + self.source.sourceVersion()
-                elif self.subinfo.buildTarget == "svnHEAD":
-                    pkgVersion = str( datetime.date.today() ).replace('-', '')  + '-r' + self.source.sourceVersion()
-                elif self.subinfo.buildTarget == "HEAD":
-                    pkgVersion = str( datetime.date.today() ).replace('-', '')  + '-' + self.subinfo.buildTarget
-                else:
-                    pkgVersion = self.subinfo.buildTarget
-            elif self.subinfo.buildTarget == "gitHEAD" or self.subinfo.buildTarget == "svnHEAD":
-                pkgVersion = str( datetime.date.today() ).replace('-', '')
-            else:
-                pkgVersion = self.subinfo.buildTarget
+            pkgVersion = self.subinfo.buildTarget
 
         pkgNotesVersion = pkgVersion
 
-        if ("General","EMERGE_PKGPATCHLVL") in emergeSettings:
+        if ("General","EMERGE_PKGPATCHLVL") in emergeSettings and emergeSettings.get("General","EMERGE_PKGPATCHLVL") != "":
             pkgVersion += "-" + emergeSettings.get("General","EMERGE_PKGPATCHLVL")
 
         return [pkgVersion, pkgNotesVersion]
