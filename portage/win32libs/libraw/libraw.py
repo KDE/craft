24a0f463e4c4f4f202f0a49ce7672f0446dc3588
Hannah von Reth <vonreth@kde.org>
2016-06-18 23:16:38 +0200

    Update libraw

--- portage/win32libs/libraw/libraw.py
+++ portage/win32libs/libraw/libraw.py
@@ -3,13 +3,15 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['0.16.0-Beta1'] = 'http://www.libraw.org/data/LibRaw-0.16.0-Beta1.tar.gz'
-        self.targetInstSrc['0.16.0-Beta1'] = "LibRaw-0.16.0-Beta1"
-        self.targetDigests['0.16.0-Beta1'] = '1bda8b14098b1897264c19728c147bdb43afda60'
-        self.patchToApply['0.16.0-Beta1'] = [('LibRaw-0.16.0-Beta1-20131223.diff', 1)]
-        
+        self.targets['0.17.2'] = [ "http://www.libraw.org/data/LibRaw-0.17.2.tar.gz",
+                                   "https://github.com/LibRaw/LibRaw-cmake/archive/master.zip" ]
+        self.archiveNames['0.17.2'] = [ "LibRaw-0.17.2.tar.gz",
+                                        "LibRaw-0.17.2-cmake.zip" ]
+        self.targetInstSrc['0.17.2'] = "LibRaw-0.17.2"
         self.shortDescription = "LibRaw is a library for reading RAW files obtained from digital photo cameras (CRW/CR2, NEF, RAF, DNG, and others)."
-        self.defaultTarget = '0.16.0-Beta1'
+        self.targetDigests['0.17.2'] = (
+        ['92b0c42c7666eca9307e5e1f97d6fefc196cf0b7ee089e22880259a76fafd15c'], EmergeHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '0.17.2'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -18,6 +20,13 @@ class subinfo(info.infoclass):
         self.dependencies['win32libs/jpeg'] = 'default'
 
 class Package(CMakePackageBase):
-  def __init__( self ):
-    CMakePackageBase.__init__(self)
+    def __init__( self ):
+        CMakePackageBase.__init__(self)
     
+
+    def unpack(self):
+        if not CMakePackageBase.unpack(self):
+            return False
+        utils.mergeTree( os.path.join(self.workDir(), "LibRaw-cmake-master"),
+                         self.sourceDir())
+        return True
