0312dbe9ad2ba9a3da80ae81e40752fc83effb18
Hannah von Reth <vonreth@kde.org>
2016-06-18 23:20:21 +0200

    Add gwenview

---
+++ portage/kde/applications/gwenview/gwenview.py
@@ -0,0 +1,27 @@
+import info
+from EmergeConfig import *
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "Gwenview is a fast and easy to use image viewer for KDE."
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.buildDependencies["libs/qtbase"] = "default"
+        self.dependencies["win32libs/jpeg"] = "default"
+        self.dependencies["win32libs/libpng"] = "default"
+        self.dependencies["win32libs/exiv2"] = "default"
+        self.dependencies["win32libs/lcms2"] = "default"
+        self.dependencies["frameworks/libkdcraw"] = "default"
+        self.dependencies["frameworks/kactivities"] = "default"
+        self.dependencies["frameworks/kdelibs4support"] = "default"
+        self.dependencies["qt-libs/phonon"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
