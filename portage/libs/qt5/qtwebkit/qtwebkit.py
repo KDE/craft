acfcca7bd4ee69d03e0231d1b0dbebb63b751e66
Hannah von Reth <vonreth@kde.org>
2016-06-26 16:48:11 +0200

    Fix qtwebkti with mingw

--- portage/libs/qt5/qtwebkit/qtwebkit.py
+++ portage/libs/qt5/qtwebkit/qtwebkit.py
@@ -38,7 +38,8 @@ from Package.Qt5CorePackageBase import *
 class Package( Qt5CorePackageBase ):
     def __init__( self, **args ):
         Qt5CorePackageBase.__init__( self )
-        os.putenv("SQLITE3SRCDIR",EmergeStandardDirs.emergeRoot())
+        utils.putenv("SQLITE3SRCDIR",EmergeStandardDirs.emergeRoot())
+        self.subinfo.options.configure.defines = """ "QT_CONFIG+=no-pkg-config" """
         if compiler.isMinGW():
-            self.subinfo.options.configure.defines = """ "QMAKE_CXXFLAGS += -g0 -O3" """
+            self.subinfo.options.configure.defines += """ "QMAKE_CXXFLAGS += -g0 -O3" """
 
