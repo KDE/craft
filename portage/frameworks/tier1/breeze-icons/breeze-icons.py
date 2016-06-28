149383d6540dd1b6bb271f2f11b83302cc27ff5a
Gleb Popov <6yearold@gmail.com>
2016-06-28 15:03:09 +0300

    Install breeze-icons into usual location on Unix.

--- portage/frameworks/tier1/breeze-icons/breeze-icons.py
+++ portage/frameworks/tier1/breeze-icons/breeze-icons.py
@@ -22,9 +22,12 @@ class Package(CMakePackageBase):
         self.subinfo.options.configure.defines = " -DBINARY_ICONS_RESOURCE=ON"
 
     def install(self):
-        dest = os.path.join(self.installDir(), "bin","data")
-        if not os.path.exists(dest):
-            os.makedirs(dest)
-        utils.copyFile(os.path.join(self.buildDir(), "icons", "breeze-icons.rcc"),
-                       os.path.join(dest, "icontheme.rcc"))
-        return True
+        if OsUtils.isWin():
+            dest = os.path.join(self.installDir(), "bin","data")
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+            utils.copyFile(os.path.join(self.buildDir(), "icons", "breeze-icons.rcc"),
+                        os.path.join(dest, "icontheme.rcc"))
+            return True
+        else:
+            return CMakePackageBase.install(self)
