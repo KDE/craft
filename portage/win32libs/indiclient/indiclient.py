3317c41c73b9fd1681a66c89230beffe613c315c
Jasem Mutlaq <mutlaqja@ikarustech.com>
2016-06-30 01:13:08 +0300

    Aggregate all XML and .xml.cmake files to indi data dir. Set all driver version to 1.0

--- portage/win32libs/indiclient/indiclient.py
+++ portage/win32libs/indiclient/indiclient.py
@@ -1,4 +1,7 @@
 import info
+import glob
+import os
+from xml.etree import ElementTree as et
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
@@ -18,3 +21,31 @@ class Package(CMakePackageBase):
     def __init__( self ):
         CMakePackageBase.__init__(self)
         self.subinfo.options.configure.defines = "-DENABLE_STATIC=ON"
+
+    # Need to copy all drivers XML files manually since indiserver and drivers are not built on Windows
+    def install(self):
+        dest = os.path.join(self.installDir(), "bin","data","indi")
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+        # File all drivers XML files including *.xml.cmake template files
+        rootDir = self.sourceDir()[:-7]
+        # Root directory of indiclient tree
+        globSearch = rootDir + '**\*.xml*'
+        for filename in glob.iglob(globSearch, recursive=True):
+            if not ("_sk" in filename):
+                if filename.endswith(".cmake"):
+                    destFilename = os.path.basename(filename)
+                    destFilename = dest + '\\' + destFilename[:-6]
+                    # Rename .xml.cmake to .xml and copy it to destination. NO LINKS
+                    utils.copyFile(filename, destFilename, False)
+                    xmlFile = destFilename
+                    tree = et.parse(destFilename)
+                    root = tree.getroot()
+                    # Set all version to 1.0 for now
+                    for version in root.iter('version'):
+                        version.text = '1.0'
+                    tree.write(destFilename)
+                # Regular .xml file get copied directly
+                else:
+                   utils.copyFile(filename, dest)
+        return CMakePackageBase.install(self)
