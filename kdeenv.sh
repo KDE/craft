a2f9c1096bc2c1f2184123d2abdeff85683bddc2
Hannah von Reth <vonreth@kde.org>
2016-07-06 21:32:51 +0200

    Fix kdeenv.sh

--- kdeenv.sh
+++ kdeenv.sh
@@ -1,8 +1,25 @@
-#!/usr/bin/env bash
-EMERGE_ENV=($(python3.5 $(dirname $(realpath $(which $0)))/bin/EmergeSetupHelper.py --setup --mode bash))
+emergeRoot="${BASH_SOURCE[0]}"
+if [[ -z "$emergeRoot" ]];then
+    emergeRoot="$0"
+fi
+if [[ -z "$emergeRoot" ]];then
+    emergeRoot="$_"
+fi
+if [[ -z "$emergeRoot" ]];then
+    echo "Failed to determine interpreter"
+    exit 1
+fi
 
-for line in $EMERGE_ENV; do
-  if [[ $line  =~ "=" ]];then
+if [[ ! -d "$emergeRoot" ]]; then
+    emergeRoot=$(dirname $(realpath "$emergeRoot"))
+fi
+
+EMERGE_ENV=($(python3.5 "$emergeRoot/bin/EmergeSetupHelper.py" --setup --mode bash))
+
+for line in "${EMERGE_ENV[@]}"; do
+  if [[ "$line"  =~ "=" ]];then
     export $line
   fi
 done
+
+cd "$KDEROOT"
