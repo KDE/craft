a37a0938424cb54e49741964414325414074f7bf
Kevin Funk <kfunk@kde.org>
2016-07-12 12:04:55 +0200

    Make gammaray work
    Depend on qtbase instead of generic libs/qt package.

--- portage/qt-apps/gammaray/gammaray.py
+++ portage/qt-apps/gammaray/gammaray.py
@@ -7,7 +7,7 @@ class subinfo( info.infoclass ):
 
     def setDependencies( self ):
         self.shortDescription = "GammaRay is a tool to poke around in a Qt-application and also to manipulate the application to some extent"
-        self.dependencies['libs/qt'] = 'default'
+        self.dependencies["libs/qtbase"] = "default"
         self.buildDependencies['virtual/base'] = 'default'
 
 from Package.CMakePackageBase import *
