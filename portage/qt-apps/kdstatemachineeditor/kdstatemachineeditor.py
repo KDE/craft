3c4d877f59fc9585fe68a1dbccc9b246db09f739
Kevin Funk <kfunk@kde.org>
2016-07-13 13:21:53 +0200

    Add kdstatemachineeditor recipe

---
+++ portage/qt-apps/kdstatemachineeditor/kdstatemachineeditor.py
@@ -0,0 +1,18 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets['gitHEAD'] = 'git://github.com/KDAB/KDStateMachineEditor.git'
+        self.defaultTarget = 'gitHEAD'
+
+    def setDependencies( self ):
+        self.shortDescription = "The KDAB State Machine Editor Library is a framework that can be used to help develop full-featured State Machine Editing graphical user interfaces and tools."
+        self.dependencies["libs/qtbase"] = "default"
+        self.dependencies["libs/qtdeclarative"] = "default"
+        self.buildDependencies['virtual/base'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
