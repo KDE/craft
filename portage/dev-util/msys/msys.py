d3c8bcb334ed15a18392502556ea89b3f7d83523
Hannah von Reth <vonreth@kde.org>
2016-09-02 15:16:51 +0200

    Updatre msys

--- portage/dev-util/msys/msys.py
+++ portage/dev-util/msys/msys.py
@@ -4,11 +4,13 @@ import shells
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = "20160205"
+        ver = "20160719"
+        # don't set an actual version  instead of base. Msys must be manually updated so doing a emerge update of msys wil break things.
         if compiler.isX86():
             self.targets[ "base" ] = "http://downloads.sourceforge.net/sourceforge/msys2/msys2-base-i686-%s.tar.xz" % ver
         else:
             self.targets[ "base" ] = "http://downloads.sourceforge.net/sourceforge/msys2/msys2-base-x86_64-%s.tar.xz" % ver
+            self.targetDigests['base'] = (['a3255ebba5888c3b4de7a01b6febce9336c66128953f061f7d80e1d8c56582ca'], EmergeHash.HashAlgorithm.SHA256)
         self.defaultTarget = "base"
 
 
