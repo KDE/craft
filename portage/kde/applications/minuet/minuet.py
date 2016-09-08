d34cbbc0bff21152800898ac9d99934078b9b752
Hannah von Reth <vonreth@kde.org>
2016-09-08 23:28:10 +0200

    Add minuet

---
+++ portage/kde/applications/minuet/minuet.py
@@ -0,0 +1,33 @@
+import info
+from EmergeConfig import *
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "A KDE Software for Music Education."
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/extra-cmake-modules"] = "default"
+        self.buildDependencies["dev-util/pkg-conf"] = "default"
+        self.dependencies["libs/qtbase"] = "default"
+        self.dependencies["libs/qtquickcontrols2"] = "default"
+        self.dependencies["win32libs/glib"] = "default"
+        self.dependencies["frameworks/kdoctools"] = "default"
+        self.dependencies["frameworks/kxmlgui"] = "default"
+        self.dependencies["frameworks/ki18n"] = "default"
+        self.dependencies["frameworks/kcoreaddons"] = "default"
+        self.dependencies["frameworks/kcompletion"] = "default"
+        self.dependencies["frameworks/kwidgetsaddons"] = "default"
+        self.dependencies["frameworks/kio"] = "default"
+        self.dependencies["frameworks/kcrash"] = "default"
+        self.dependencies["frameworks/plasma-framework"] = "default"
+
+
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
