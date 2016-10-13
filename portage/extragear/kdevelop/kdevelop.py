fb7cd82dd9250f326760d4af17bec85f05a56580
Kevin Funk <kfunk@kde.org>
2016-10-14 01:19:02 +0200

    kdevelop.py: Strip .pyc in installer, fix website

--- portage/extragear/kdevelop/kdevelop.py
+++ portage/extragear/kdevelop/kdevelop.py
@@ -57,6 +57,7 @@ class Package( CMakePackageBase ):
 
     def createPackage(self):
         self.defines[ "productname" ] = "KDevelop"
+        self.defines[ "website" ] = "https://kdevelop.org"
         self.defines[ "executable" ] = "bin\\kdevelop.exe"
         self.defines[ "icon" ] = os.path.join(os.path.dirname(__file__), "kdevelop.ico")
         self.defines[ "extrashortcuts" ] = r'CreateShortCut \"${startmenu}\KDevelop - Microsoft Visual C++ compiler.lnk\" \"$INSTDIR\\bin\\kdevelop-msvc.bat\"'
