6c8f70bbf105a65da3eeb2c75cd65b5b28395d6a
Hannah von Reth <vonreth@kde.org>
2016-10-27 21:53:25 +0200

    Add libjpeg-turbo

---
+++ portage/win32libs/libjpeg-turbo/libjpeg-turbo.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets["gitHEAD"] = "https://github.com/libjpeg-turbo/libjpeg-turbo.git"
+
+        for ver in ["1.5.1"]:
+            self.targets[ver] = "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/%s.tar.gz" % ver
+            self.archiveNames[ver] = "libjpeg-turbo-%s.tar.gz" % ver
+            self.targetInstSrc[ver] = 'libjpeg-turbo-%s' % ver
+
+        self.targetDigests['1.5.1'] = (
+        ['c15a9607892113946379ccea3ca8b85018301b200754f209453ab21674268e77'], EmergeHash.HashAlgorithm.SHA256)
+
+        self.shortDescription = "libjpeg-turbo is a JPEG image codec that uses SIMD instructions (MMX, SSE2, NEON, AltiVec) to accelerate baseline JPEG compression and decompression on x86, x86-64, ARM, and PowerPC systems"
+        self.homepage = "http://libjpeg-turbo.virtualgl.org/"
+        self.defaultTarget = '1.5.1'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.buildDependencies['dev-util/nasm'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        CMakePackageBase.__init__( self )
