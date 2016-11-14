bfe88b125a8efb214640ab4d661b3d1449f70f5f
Hannah von Reth <vonreth@kde.org>
2016-11-14 18:37:08 +0100

    Cleanup

--- bin/EmergeOS/win/osutils.py
+++ bin/EmergeOS/win/osutils.py
@@ -3,8 +3,13 @@ import EmergeDebug
 import ctypes
 import os
 
-class OsUtils(EmergeOS.OsUtilsBase.OsUtilsBase):
 
+class FileAttributes():
+    # https://msdn.microsoft.com/en-us/library/windows/desktop/gg258117(v=vs.85).aspx
+    FILE_ATTRIBUTE_READONLY = 0x1
+    FILE_ATTRIBUTE_REPARSE_POINT = 0x400
+
+class OsUtils(EmergeOS.OsUtilsBase.OsUtilsBase):
     @staticmethod
     def rm(path, force=False):
         EmergeDebug.debug("deleting file %s" % path, 3)
@@ -22,17 +27,16 @@ class OsUtils(EmergeOS.OsUtilsBase.OsUtilsBase):
     @staticmethod
     def isLink(path):
         return os.path.islink(path) \
-               | OsUtils.getFileAttributes(path) & 0x400 # FILE_ATTRIBUTE_REPARSE_POINT Detect a Junction
+               | OsUtils.getFileAttributes(path) & FileAttributes.FILE_ATTRIBUTE_REPARSE_POINT #Detect a Junction
 
     @staticmethod
     def getFileAttributes(path):
-        # https://msdn.microsoft.com/en-us/library/windows/desktop/gg258117(v=vs.85).aspx
         return ctypes.windll.kernel32.GetFileAttributesW(path)
 
     @staticmethod
     def removeReadOnlyAttribute(path):
         attributes =  OsUtils.getFileAttributes(path)
-        return ctypes.windll.kernel32.SetFileAttributesW(path,attributes & ~ 0x1) != 0
+        return ctypes.windll.kernel32.SetFileAttributesW(path,attributes & ~ FileAttributes.FILE_ATTRIBUTE_READONLY) != 0
 
     @staticmethod
     def setConsoleTitle(title):
