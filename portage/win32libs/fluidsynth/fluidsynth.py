95358b7463bd1c44f52db9c4239c7968573cef88
Hannah von Reth <vonreth@kde.org>
2016-11-14 13:05:07 +0100

    Fix fluidsynth src url

--- portage/win32libs/fluidsynth/fluidsynth.py
+++ portage/win32libs/fluidsynth/fluidsynth.py
@@ -4,7 +4,7 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         for ver in ['1.1.6']:
-            self.targets[ ver ] = "http://repo.msys2.org/mingw/x86_64/fluidsynth-%s.tar.bz2" % ver
+            self.targets[ ver ] = "http://downloads.sourceforge.net/sourceforge/fluidsynth/fluidsynth-%s.tar.bz2" % ver
             self.targetInstSrc[ver] = 'fluidsynth-%s' % ver
         self.targetDigests['1.1.6'] = (['d28b47dfbf7f8e426902ae7fa2981d821fbf84f41da9e1b85be933d2d748f601'], EmergeHash.HashAlgorithm.SHA256)
         self.patchToApply['1.1.6'] = [('fluidsynth-1.1.6-20160908.diff', 1)]
