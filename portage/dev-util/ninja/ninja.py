e12f82044eaa26f6eb6d2b8590eaddd730cd5b4a
Hannah von Reth <vonreth@kde.org>
2016-11-15 16:57:54 +0100

    Build ninja from source again and update the version

--- portage/dev-util/ninja/ninja.py
+++ portage/dev-util/ninja/ninja.py
@@ -1,30 +1,43 @@
 # -*- coding: utf-8 -*-
-
 import os
 
 import info
+from Package.CMakePackageBase import *
 
 
 class subinfo(info.infoclass):
-    def setDependencies( self ):
-        self.buildDependencies['virtual/bin-base'] = 'default'
-
     def setTargets( self ):
-        for ver in ["1.6.0", "1.7.1" ]:
-            self.targets[ ver ] = "https://github.com/ninja-build/ninja/releases/download/v%s/ninja-win.zip" % ver
-            self.targetInstallPath[ ver ] = "bin"
-            self.archiveNames[ ver] = "ninja-win%s.zip" % ver
+        """ """
+        self.svnTargets['gitHEAD'] = "https://github.com/martine/ninja.git"
+        for ver in ["1.6.0", "1.7.1", "1.7.2" ]:
+            self.targets[ ver ] = "https://github.com/martine/ninja/archive/v%s.tar.gz" % ver
+            self.targetInstSrc[ ver ] = "ninja-%s" % ver
+            self.archiveNames[ ver] = "ninja-%s.tar.gz" % ver
+        self.targetDigests['1.6.0'] = 'a6ff055691f6d355234298c21cc18961b4ca2ed9'
+        self.targetDigests['1.7.2'] = (['2edda0a5421ace3cf428309211270772dd35a91af60c96f93f90df6bc41b16d9'], EmergeHash.HashAlgorithm.SHA256)
 
-        self.targetDigests['1.6.0'] = (['18f55bc5de27c20092e86ace8ef3dd3311662dc6193157e3b65c6bc94ce006d5'], EmergeHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.7.1'] = (['4221b2cef768de64799077ba83ca9ca28e197415a34257ed8d54f93e899afc4e'], EmergeHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.7.1"
+        self.defaultTarget = "1.7.2"
 
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
 
-
-from Package.BinaryPackageBase import *
-
-class Package(BinaryPackageBase):
-    def __init__( self):
-        BinaryPackageBase.__init__(self)
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        CMakePackageBase.__init__( self )
         self.subinfo.options.merge.destinationPath = "dev-utils"
 
+    def configure(self):
+        return True
+        
+    def make(self):
+        self.enterSourceDir()
+        command = "python configure.py --bootstrap"
+        if compiler.isMinGW():
+            command += " --platform=mingw"
+        print(command)
+        return self.system( command, "make" )
+        
+    def install(self):
+        utils.copyFile(os.path.join(self.sourceDir(),"ninja.exe"),os.path.join(self.imageDir(),"bin","ninja.exe"))
+        return True
+        
