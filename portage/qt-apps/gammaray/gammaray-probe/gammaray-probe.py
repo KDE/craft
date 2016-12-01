a9ef5910045d8a0b4f222fd0f08e87100b8cee96
Hannah von Reth <vonreth@kde.org>
2016-12-01 15:48:02 +0100

    Package gammaray and use version.ini isntead of hack

--- portage/qt-apps/gammaray/gammaray-probe/gammaray-probe.py
+++ portage/qt-apps/gammaray/gammaray-probe/gammaray-probe.py
@@ -2,18 +2,17 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
+        self.versionInfo.setDefaultValues()
         self.shortDescription = "GammaRayProbe is only the probe for GammaRay"
+
+    def setDependencies(self):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.dependencies["libs/qtbase"] = "default"
 
 from Package.CMakePackageBase import *
 
 class Package( CMakePackageBase ):
     def __init__( self ):
         CMakePackageBase.__init__( self )
-        gammaray = portage.getPackageInstance('qt-apps', 'gammaray').subinfo
-        # TODO: omg
-        for var in vars(gammaray):
-            if var in ["parent", "_CraftBase__evilHack", "shortDescription"]:
-                continue
-            setattr(self.subinfo, var, getattr(gammaray, var))
         self.subinfo.options.configure.defines = "-DGAMMARAY_PROBE_ONLY_BUILD=ON"
 
--- portage/qt-apps/gammaray/gammaray/gammaray.py
+++ portage/qt-apps/gammaray/gammaray/gammaray.py
@@ -2,14 +2,8 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = 'https://github.com/KDAB/GammaRay.git'
-        for ver in ["2.6.0"]:
-            self.targets[ver] = 'https://github.com/KDAB/GammaRay/archive/v%s.tar.gz' % ver
-            self.archiveNames[ver] = "gammaray-%s.tar.gz" % ver
-            self.targetInstSrc[ver] = 'gammaray-%s' % ver
+        self.versionInfo.setDefaultValues()
         self.targetDigests['2.6.0'] = (['762fc1e61fb141462e72fe048b4a7bbf1063eea6a2209963c8aa1ad7696b0217'], CraftHash.HashAlgorithm.SHA256)
-
-        self.defaultTarget = '2.6.0'
 
         self.shortDescription = "GammaRay is a tool to poke around in a Qt-application and also to manipulate the application to some extent"
 
@@ -17,10 +11,25 @@ class subinfo( info.infoclass ):
         self.buildDependencies['virtual/base'] = 'default'
         self.dependencies["libs/qtbase"] = "default"
         self.dependencies['qt-apps/kdstatemachineeditor'] = 'default'
+        if self.getboolean("QtSDK", "Enabled", "False"):
+            self.dependencies['qt-apps/gammaray-probe'] = 'default'
 
 from Package.CMakePackageBase import *
 
 class Package( CMakePackageBase ):
     def __init__( self ):
         CMakePackageBase.__init__( self )
+        self.changePackager(NullsoftInstallerPackager)
+
+
+    def createPackage(self):
+        self.defines["productname"] = "GammaRay"
+        self.defines["website"] = "http://www.kdab.com/gammaray"
+        self.defines["executable"] = "bin\\gammaray.exe"
+#            self.defines["icon"] = os.path.join(os.path.dirname(__file__), "kdevelop.ico")
+
+        self.ignoredPackages.append("binary/mysql-pkg")
+
+        return TypePackager.createPackage(self)
+
 
