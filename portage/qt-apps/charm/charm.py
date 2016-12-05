8792245ad9bdea2009978b09f0725be255f8986a
Hannah von Reth <vonreth@kde.org>
2016-12-05 10:57:31 +0100

    Add packaging to charm

--- portage/qt-apps/charm/charm.py
+++ portage/qt-apps/charm/charm.py
@@ -17,10 +17,21 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.dependencies['libs/qtbase'] = 'default'
         self.dependencies['qt-libs/qtkeychain'] = 'default'
+        self.dependencies['win32libs/openssl'] = 'default'
 
 
 class Package( CMakePackageBase ):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.defines = " -DCHARM_PREPARE_DEPLOY=OFF "
+        self.subinfo.options.configure.defines = "-DCHARM_SIGN_INSTALLER=OFF"
 
+    def createPackage(self):
+        if craftSettings.getboolean("QtSDK", "Enabled", False):
+            keychain = portage.getPackageInstance("qt-libs", "qtkeychain")
+            utils.copyDir(keychain.imageDir(), os.path.join( craftSettings.get("QtSDK", "Path") , craftSettings.get("QtSDK", "Version"), craftSettings.get("QtSDK", "Compiler")))
+
+        old = self.subinfo.options.make.makeOptions
+        self.subinfo.options.make.makeOptions = "package"
+        out = CMakePackageBase.make(self)
+        self.subinfo.options.make.makeOptions = old
+        return out
