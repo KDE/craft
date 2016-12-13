189691ad28c07a6570dfd92851db31c0ed472dbb
Hannah von Reth <vonreth@kde.org>
2016-12-13 22:04:53 +0100

    Update Crafts Snore notifications

--- bin/Notifier/Backends/Snore.py
+++ bin/Notifier/Backends/Snore.py
@@ -2,25 +2,29 @@ import os
 import subprocess
 import ctypes
 import CraftConfig
+import CraftDebug
 
 from CraftOS.osutils import OsUtils
 
 from Notifier.NotificationInterface import *
+from utils import UtilsCache
 
 
 class Snore(NotificationInterface):
     def __init__(self):
         NotificationInterface.__init__(self,"Snore")
-        
+        self.icon = os.path.join(CraftConfig.CraftStandardDirs.craftBin(), "data", "icons", "craftyBENDER.png")
+
     def notify(self,title,message,alertClass):
-        path = os.path.dirname(os.path.realpath(__file__))
-        icon = os.path.join(path, "..", "kde-logo.png" )
         try:
-            command = """snoresend -t "%s" -m "%s" -i "%s" -a "Craft" -c "%s" --silent """ % (title , message , icon, alertClass)
+            command = """%s -t "%s" -m "%s" -i "%s" -a "Craft" -c "%s" --silent """ % (UtilsCache.findApplication("snoresend"), title, message , self.icon, alertClass)
             if OsUtils.isWin():
                 command += " --bring-window-to-front %s" % ctypes.windll.kernel32.GetConsoleWindow()
+            CraftDebug.craftDebug.log.debug(command)
             subprocess.Popen( command,
                               shell = True,
+                              stdout=subprocess.DEVNULL,
+                              stderr=subprocess.DEVNULL,
                               cwd = CraftConfig.CraftStandardDirs.craftRoot())# make sure that nothing is spawned in a build dir
         except Exception as e:
             return
