2a6a870fb77054223ab745c9284324e0c10e5a1c
Hannah von Reth <vonreth@kde.org>
2016-12-13 11:34:39 +0100

    Fix and update vlc

--- portage/binary/vlc/vlc.py
+++ portage/binary/vlc/vlc.py
@@ -7,57 +7,57 @@ import info
 import compiler
 
 class subinfo(info.infoclass):
-  vlc_ver = None
-  
-  def setTargets( self ):
-    self.vlcArch = "32"
-    if compiler.isX64():
-        self.vlcArch = "64"        
-    self.vlcBaseUrl = 'http://nightlies.videolan.org/build/win'+self.vlcArch+'/last/'
-    self.vlcTagName = '3.0.0-git' 
-    self.gitVer = utils.UtilsCache.getNightlyVersionsFromUrl(self.vlcBaseUrl, "\d\d\d\d\d\d\d\d-\d\d\d\d" )[0]
-    
+    vlc_ver = None
 
-    self.targets[ self.vlcTagName ]  =  "%svlc-%s-%s-win%s.7z" % (self.vlcBaseUrl, self.vlcTagName, self.gitVer, self.vlcArch  )
-    self.targetInstSrc[ self.vlcTagName ] = "vlc-%s-%s" % (self.vlcTagName, self.gitVer)
-    self.patchToApply[ self.vlcTagName ] = [("vlc-2.1.5.diff" ,1)]
-    
-    self.targets[ self.vlcTagName +"-debug" ]  = "%svlc-%s-%s-win%s-debug.7z" % (self.vlcBaseUrl, self.vlcTagName, self.gitVer, self.vlcArch  )
-    self.targetInstSrc[ self.vlcTagName + "-debug" ] = "vlc-%s-%s" % (self.vlcTagName,self.gitVer)
-    self.patchToApply[ self.vlcTagName + "-debug" ] = [("vlc-2.1.5.diff" ,1)]
-    
-    for releaseTag in [  '2.2.0', '2.2.1']:
-        self.targets[ releaseTag ] = "http://download.videolan.org/pub/videolan/vlc/%s/win%s/vlc-%s-win%s.7z" % ( releaseTag ,self.vlcArch,releaseTag , self.vlcArch )
-        self.targetInstSrc[ releaseTag ] = 'vlc-' + releaseTag
-        self.targetDigestUrls[ releaseTag ] = "http://download.videolan.org/pub/videolan/vlc/%s/win%s/vlc-%s-win%s.7z.sha1" % ( releaseTag ,self.vlcArch,releaseTag , self.vlcArch )
-        self.patchToApply[ releaseTag ] = [("vlc-2.1.5.diff" ,1)]
-    self.shortDescription = "an open-source multimedia framework"
-    
-    self.defaultTarget = '2.2.1'
+    def setTargets( self ):
+        vlcArch = "32"
+        if compiler.isX64():
+            vlcArch = "64"
+        vlcBaseUrl = "http://nightlies.videolan.org/build/win"+vlcArch+"/last/"
+        vlcTagName = "3.0.0"
 
 
-  def setDependencies( self ):
-    self.buildDependencies['virtual/bin-base'] = 'default'
+        for ver in utils.UtilsCache.getNightlyVersionsFromUrl(vlcBaseUrl, "\d\d\d\d\d\d\d\d-\d\d\d\d" ):
+            self.targets[ vlcTagName + "-git" ]  =  "%svlc-%s-%s-git-win%s.7z" % (vlcBaseUrl, vlcTagName, ver, vlcArch  )
+            self.targetInstSrc[ vlcTagName + "-git" ] = "vlc-%s-git" % (vlcTagName)
+            self.patchToApply[ vlcTagName  + "-git" ] = [("vlc-2.1.5.diff" ,1)]
+
+            self.targets[ vlcTagName +"-debug-git" ]  = "%s/vlc-%s-%s-git-win%s-debug.7z" % (vlcBaseUrl, vlcTagName, ver, vlcArch  )
+            self.targetInstSrc[ vlcTagName + "-debug-git" ] = "vlc-%s-git" % (vlcTagName)
+            self.patchToApply[ vlcTagName + "-debug-git" ] = [("vlc-2.1.5.diff" ,1)]
+
+        for releaseTag in [ "2.2.0", "2.2.1", "2.2.4" ]:
+            self.targets[ releaseTag ] = "http://download.videolan.org/pub/videolan/vlc/%s/win%s/vlc-%s-win%s.7z" % ( releaseTag ,vlcArch,releaseTag , vlcArch )
+            self.targetInstSrc[ releaseTag ] = "vlc-" + releaseTag
+            self.targetDigestUrls[ releaseTag ] = "http://download.videolan.org/pub/videolan/vlc/%s/win%s/vlc-%s-win%s.7z.sha1" % ( releaseTag ,vlcArch,releaseTag , vlcArch )
+            self.patchToApply[ releaseTag ] = [("vlc-2.1.5.diff" ,1)]
+        self.shortDescription = "an open-source multimedia framework"
+
+        self.defaultTarget = "2.2.4"
+
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/bin-base"] = "default"
 
 class Package(BinaryPackageBase):
-  def __init__(self):
-    BinaryPackageBase.__init__( self )
-    self.subinfo.options.package.packageName = 'vlc'
-    self.subinfo.options.package.packSources = False
+    def __init__(self):
+        BinaryPackageBase.__init__( self )
+        self.subinfo.options.package.packageName = "vlc"
+        self.subinfo.options.package.packSources = False
 
 
-  def install( self ):
-    utils.copyDir(   self.sourceDir() , os.path.join( self.imageDir(), "bin" ) )
-    if compiler.isMinGW():
-      utils.deleteFile(os.path.join( self.imageDir(), "bin", "libgcc_s_seh-1.dll" ) )
-    shutil.move( os.path.join( self.imageDir() , "bin" , "sdk" , "include") , os.path.join( self.imageDir(), "include" ) )
-    shutil.move( os.path.join( self.imageDir() , "bin" , "sdk" , "lib") , os.path.join( self.imageDir(), "lib" ) )
-    ver2 = self.subinfo.buildTarget.split('.')
-    if not (int(ver2[0]) >= 2 and int(ver2[0]) >= 1):
-      utils.copyFile( os.path.join( self.imageDir() , "lib" ,"libvlc.dll.a" ) , os.path.join( self.imageDir() , "lib" ,"libvlc.lib" ))
-      utils.copyFile( os.path.join( self.imageDir() , "lib" ,"libvlccore.dll.a" ) , os.path.join( self.imageDir() , "lib" ,"libvlccore.lib" ))
-    shutil.rmtree( os.path.join( self.imageDir() , "bin" , "sdk" ) )
-    os.makedirs( os.path.join( self.imageDir() , "share" , "applications") )
-    utils.copyFile( os.path.join( self.packageDir() ,  "vlc.desktop" ) , os.path.join( self.imageDir() , "share" , "applications", "vlc.desktop" ))
-    return True
+    def install( self ):
+        utils.copyDir(   self.sourceDir() , os.path.join( self.installDir(), "bin" ) )
+        if compiler.isMinGW():
+            utils.deleteFile(os.path.join( self.installDir(), "bin", "libgcc_s_seh-1.dll" ) )
+        shutil.move( os.path.join( self.installDir() , "bin" , "sdk" , "include") , os.path.join( self.installDir(), "include" ) )
+        shutil.move( os.path.join( self.installDir() , "bin" , "sdk" , "lib") , os.path.join( self.installDir(), "lib" ) )
+        ver2 = self.subinfo.buildTarget.split(".")
+        if not (int(ver2[0]) >= 2 and int(ver2[0]) >= 1):
+            utils.copyFile( os.path.join( self.installDir() , "lib" ,"libvlc.dll.a" ) , os.path.join( self.installDir() , "lib" ,"libvlc.lib" ))
+            utils.copyFile( os.path.join( self.installDir() , "lib" ,"libvlccore.dll.a" ) , os.path.join( self.installDir() , "lib" ,"libvlccore.lib" ))
+        shutil.rmtree( os.path.join( self.installDir() , "bin" , "sdk" ) )
+        os.makedirs( os.path.join( self.installDir() , "share" , "applications") )
+        utils.copyFile( os.path.join( self.packageDir() ,  "vlc.desktop" ) , os.path.join( self.installDir() , "share" , "applications", "vlc.desktop" ))
+        return True
 
