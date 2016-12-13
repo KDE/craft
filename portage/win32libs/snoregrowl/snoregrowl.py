1083d0c3620ff00f9e2345956023ac7b5a850ed6
Hannah von Reth <vonreth@kde.org>
2016-12-13 22:05:48 +0100

    Add 0.5.0 release and alow static build

--- portage/win32libs/snoregrowl/snoregrowl.py
+++ portage/win32libs/snoregrowl/snoregrowl.py
@@ -5,15 +5,16 @@ class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets[ 'gitHEAD' ] = 'https://github.com/Snorenotify/SnoreGrowl.git'
         
-        for ver in ['0.4.0']:
+        for ver in ['0.4.0', '0.5.0']:
             self.targets[ver] = 'https://github.com/Snorenotify/SnoreGrowl/archive/v%s.tar.gz' % ver
             self.archiveNames[ver] = "snoregrowl-%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'SnoreGrowl-%s' % ver
         self.targetDigests['0.4.0'] = '16b84d2fb673438c8250cefd95f7e4c145e4cf22'
+        self.targetDigests['0.5.0'] = (['5180628ce1c732abfc1001db48302b0d63534a3d62dc50a4655e0b4675a918be'], CraftHash.HashAlgorithm.SHA256)
         
         self.shortDescription = "SnoreGrowl is a library providing Growl network notifications"
         self.homepage = "https://github.com/Snorenotify/SnoreGrowl"
-        self.defaultTarget = '0.4.0'
+        self.defaultTarget = '0.5.0'
 
 
     def setDependencies( self ):
@@ -26,6 +27,7 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.staticDefine = "-DSNOREGROWL_STATIC=ON"
 
         
 
