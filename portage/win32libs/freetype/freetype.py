dbf9b43881588f269e76b29c73717ba1986df504
Dan Leinir Turthra Jensen <admin@leinir.dk>
2016-12-14 11:09:33 +0000

    Revert "Update freetype (and use autothings for mingw)"
    This reverts commit fd6777f0b5ddae5315ccdc873b8cc1a4482b979d.

--- portage/win32libs/freetype/freetype.py
+++ portage/win32libs/freetype/freetype.py
@@ -2,32 +2,22 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['2.3.12', '2.5.0.1', '2.7']:
-            self.targets[ver] = "http://sourceforge.net/projects/freetype/files/freetype2/" + ver + "/freetype-" + ver + ".tar.bz2"
+        for ver in ['2.3.12', '2.5.0.1']:
+            self.targets[ver] = "http://downloads.sourceforge.net/freetype/freetype-" + ver + ".tar.bz2"
             self.targetInstSrc[ver] = "freetype-" + ver
         self.patchToApply['2.3.12'] = ('freetype-2.3.12.diff', 1)
         self.patchToApply['2.5.0.1'] = ('freetype-2.5.0.1.diff', 1)
         self.targetDigests['2.3.12'] = 'ebf0438429c0bedd310059326d91646c3c91016b'
         self.targetDigests['2.5.0.1'] = '4bbd8357b4b723e1ff38414a9eaf50bf99dacb84'
 
-        self.defaultTarget = '2.7'
+        self.defaultTarget = '2.5.0.1'
         self.shortDescription = "A Free, High-Quality, and Portable Font Engine"
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
-        self.buildDependencies['dev-util/pkg-config'] = 'default'
 
 from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__( self )
 
-from Package.AutoToolsPackageBase import *
-class PackageMSys(AutoToolsPackageBase):
-    def __init__(self):
-        AutoToolsPackageBase.__init__(self)
-
-if compiler.isMinGW():
-    class Package(PackageMSys): pass
-else:
-    class Package(PackageMSVC): pass
