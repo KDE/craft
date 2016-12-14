8b766c24783119f2136562969da5cd3a7f2f7575
Dan Leinir Turthra Jensen <admin@leinir.dk>
2016-12-14 11:09:29 +0000

    Revert "Update libcurl, and build with autotools when using mingw"
    This reverts commit 45441e8d33ab9936072638ec92ae2c77314964ef.

--- portage/win32libs/libcurl/libcurl.py
+++ portage/win32libs/libcurl/libcurl.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import info
 from Package.CMakePackageBase import *
-from Package.AutoToolsPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = "https://github.com/bagder/curl.git"
-        for ver in [ '7.20.0', '7.28.1', '7.32.0', '7.46.0', '7.51.0' ]:
+        for ver in [ '7.20.0', '7.28.1', '7.32.0', '7.46.0' ]:
             self.targets[ver] = 'http://curl.haxx.se/download/curl-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = 'curl-' + ver
         self.patchToApply['7.20.0'] = ("7.20.0.diff", 1)
@@ -15,14 +14,12 @@ class subinfo(info.infoclass):
         self.patchToApply['7.32.0'] = [("curl-7.32.0-20130901.diff", 1),
                                        ("curl-7.28.1-installlocation.diff", 1)]
         self.patchToApply['7.46.0'] = [("curl-7.46.0-20151208.diff", 1)]
-        self.patchToApply['7.51.0'] = [("curl-7.51.0-20161129.diff", 1)]
         self.targetDigests['7.28.1'] = 'b5aff1afc4e40fcb78db7a5e27214e0035756f3d'
         self.targetDigests['7.32.0'] = 'f6989fca0dac0c35628523436fc17869972d4251'
         self.targetDigests['7.46.0'] = '96fbe5abe8ecfb923e4ab0a579b3d6be43ef0e96'
-        self.targetDigests['7.51.0'] = 'f02a14bbe580d2a8cf3bf45a79d39eb595220ac7'
 
         self.shortDescription = "a free and easy-to-use client-side URL transfer library"
-        self.defaultTarget = '7.51.0'
+        self.defaultTarget = '7.46.0'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -30,7 +27,7 @@ class subinfo(info.infoclass):
         self.dependencies['win32libs/openssl'] = 'default'
 
 
-class PackageCMake(CMakePackageBase):
+class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__( self )
         self.subinfo.options.configure.defines = " -DBUILD_CURL_TESTS=OFF -DBUILD_CURL_EXE=OFF"
@@ -38,12 +35,4 @@ class PackageCMake(CMakePackageBase):
         self.subinfo.options.configure.toolsDefine = "-DBUILD_CURL_EXE=ON"
         self.subinfo.options.configure.staticDefine = "-DCURL_STATICLIB=ON"
 
-class PackageMSys(AutoToolsPackageBase):
-    def __init__(self):
-        AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.defines = " --disable-tests --enable-static=no --with-winssl"
 
-if compiler.isMinGW():
-    class Package(PackageMSys): pass
-else:
-    class Package(PackageCMake): pass
