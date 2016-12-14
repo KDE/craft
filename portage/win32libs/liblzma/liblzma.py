430c8b2b4b6933e2fdfeea7fa90a7b24fcc175a3
Dan Leinir Turthra Jensen <admin@leinir.dk>
2016-12-14 11:09:24 +0000

    Revert "Update liblzma to 5.2.2 (and switch to autotools for mingw)"
    This reverts commit 7986ff99a5e90855d72d5187c594482e5bdd1be0.

--- portage/win32libs/liblzma/liblzma.py
+++ portage/win32libs/liblzma/liblzma.py
@@ -9,11 +9,10 @@ import compiler
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = '5.2.2'
+        ver = '5.0.5'
         self.targets[ver] = 'http://tukaani.org/xz/xz-' + ver + '.tar.xz'
         self.targetInstSrc[ver] = 'xz-' + ver
-        if not compiler.isMinGW():
-            self.patchToApply[ver] = [('xz-5.0.5.diff', 1), ('xz-cmake-5.0.5.diff', 1)]
+        self.patchToApply[ver] = [('xz-5.0.5.diff', 1), ('xz-cmake-5.0.5.diff', 1)]
         self.targetDigests['5.0.5'] = '56f1d78117f0c32bbb1cfd40117aa7f55bee8765'
 
         self.shortDescription = "free general-purpose data compression software with high compression ratio"
@@ -25,24 +24,6 @@ class subinfo(info.infoclass):
 
 from Package.CMakePackageBase import *
 
-class PackageCMake(CMakePackageBase):
+class Package(CMakePackageBase):
     def __init__( self ):
         CMakePackageBase.__init__( self )
-
-from Package.AutoToolsPackageBase import *
-
-class PackageMSys(AutoToolsPackageBase):
-    def __init__(self):
-        AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.make.supportsMultijob = False
-
-    def install( self ):
-        if not AutoToolsPackageBase.install(self):
-            return False
-        utils.copyFile(os.path.join(self.imageDir(), "bin", "liblzma-5.dll"), os.path.join(self.imageDir(), "bin", "liblzma.dll"), False)
-        return True
-
-if compiler.isMinGW():
-    class Package(PackageMSys): pass
-else:
-    class Package(PackageCMake): pass
