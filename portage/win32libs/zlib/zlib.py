ad99744c22a3890c6ce22ca6f47ed01fa989fdc9
Dan Leinir Turthra Jensen <admin@leinir.dk>
2016-12-14 11:09:26 +0000

    Revert "Change compile/install logic for zlib with mingw"
    This reverts commit 4bd892f4527d790a4389409f678f4e62a2660d19.

--- portage/win32libs/zlib/zlib.py
+++ portage/win32libs/zlib/zlib.py
@@ -10,8 +10,7 @@ class subinfo(info.infoclass):
         self.patchToApply['1.2.5'] = [("zlib-1.2.5-20110629.diff", 1)]
         self.patchToApply['1.2.6'] = [("zlib-1.2.6-20120421.diff", 1)]
         self.patchToApply['1.2.7'] = [("zlib-1.2.7-20130123.diff", 1)]
-        if not compiler.isMinGW():
-            self.patchToApply['1.2.8'] = [("zlib-1.2.8-20130901.diff", 1)]
+        self.patchToApply['1.2.8'] = [("zlib-1.2.8-20130901.diff", 1)]
         self.targetDigests['1.2.5'] = '8e8b93fa5eb80df1afe5422309dca42964562d7e'
         self.targetDigests['1.2.7'] = '4aa358a95d1e5774603e6fa149c926a80df43559'
         self.targetDigests['1.2.8'] = 'a4d316c404ff54ca545ea71a27af7dbc29817088'
@@ -22,35 +21,7 @@ class subinfo(info.infoclass):
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
 
-class PackageCMake(CMakePackageBase):
+class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__( self )
 
-
-from Package.AutoToolsPackageBase import *
-
-class PackageMSys(AutoToolsPackageBase):
-    def __init__(self):
-        AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.make.supportsMultijob = False
-        self.buildInSource = True
-
-    def configure(self):
-        return "all";
-
-    def make(self, dummyBuildType=None):
-        return self.shell.execute(self.sourceDir(), self.makeProgram, "-fwin32/Makefile.gcc")
-
-    def install(self):
-        self.enterSourceDir()
-        self.shell.environment[ "BINARY_PATH" ] = "%s/bin" % (self.shell.toNativePath(self.imageDir()))
-        self.shell.environment[ "LIBRARY_PATH"] = "%s/lib" % (self.shell.toNativePath(self.imageDir()))
-        self.shell.environment[ "INCLUDE_PATH"] = "%s/include" % (self.shell.toNativePath(self.imageDir()))
-        self.shell.execute(self.sourceDir(), self.makeProgram, "install -fwin32/Makefile.gcc SHARED_MODE=1")
-        utils.copyFile(os.path.join(self.imageDir(), "bin", "zlib1.dll"), os.path.join(self.imageDir(), "bin", "libz.dll"), False)
-        return True
-
-if compiler.isMinGW():
-    class Package(PackageMSys): pass
-else:
-    class Package(PackageCMake): pass
