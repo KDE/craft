58980f779ae7baccfbf503e11f08e3fe4a456000
Hannah von Reth <vonreth@kde.org>
2016-12-15 09:34:44 +0100

    Update wget

--- portage/gnuwin32/wget/wget.py
+++ portage/gnuwin32/wget/wget.py
@@ -4,22 +4,20 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets[ "1.17.1" ] = "http://downloads.sourceforge.net/sourceforge/tumagcc/wget-1.17.1_curl-7.46.0_win32_win64.7z"
-        self.targetInstallPath[ "1.17.1" ] = "bin"
-        self.targetDigests['1.17.1'] = (['0974c4576f24d38289b9958cc7fc41a64ef49e916ac7d4c148d741cd4981dac6'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.17.1"
+        self.targets[ "1.18" ] = "http://downloads.sourceforge.net/sourceforge/tumagcc/wget-1.18_curl-7.49.1_win32_win64.7z"
+        self.targetInstallPath[ "1.18" ] = "dev-utils"
+        self.targetDigests['1.18'] = (['19d4ae30ae35f212e95edb6f18dddab19e1c97e119c36c4231b741e7293f9b3c'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.18"
 
 from Package.BinaryPackageBase import *
 
 class Package(BinaryPackageBase):
     def __init__( self):
         BinaryPackageBase.__init__(self)
-        self.subinfo.options.merge.destinationPath = "dev-utils"
-
     def install( self ):
         if compiler.isX64():
-            utils.copyFile(os.path.join(self.sourceDir(), "wget64.exe"), os.path.join(self.installDir(), "wget.exe"))
+            utils.copyFile(os.path.join(self.sourceDir(), "wget64.exe"), os.path.join(self.installDir(), "bin", "wget.exe"))
         else:
-            utils.copyFile(os.path.join(self.sourceDir(), "wget.exe"), os.path.join(self.installDir(), "wget.exe"))
-        utils.copyFile(os.path.join(self.sourceDir(), "curl-ca-bundle.crt"), os.path.join(self.installDir(), "curl-ca-bundle.crt"))
+            utils.copyFile(os.path.join(self.sourceDir(), "wget.exe"), os.path.join(self.installDir(), "bin", "wget.exe"))
+        utils.copyFile(os.path.join(self.sourceDir(), "curl-ca-bundle.crt"), os.path.join(self.installDir(), "bin", "curl-ca-bundle.crt"))
         return True
