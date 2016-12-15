c4d1fb7da55753425c1062fd04b3428e9b3753f8
Hannah von Reth <vonreth@kde.org>
2016-12-15 16:28:42 +0100

    Update phonon and phonon-vlc

--- portage/qt-libs/phonon/phonon.py
+++ portage/qt-libs/phonon/phonon.py
@@ -10,21 +10,13 @@ class subinfo(info.infoclass):
 
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:phonon'        
-        for ver in ['4.7.0','4.7.1', '4.8.0', '4.8.1', '4.8.2']:
+        for ver in ['4.9.0']:
             self.targets[ver] = 'http://download.kde.org/stable/phonon/%s/phonon-%s.tar.xz' % (ver ,ver)
-            self.targetInstSrc[ver] = 'phonon-%s' % ver       
-        for ver in ['4.8.3']:
-            self.targets[ver] = 'http://download.kde.org/stable/phonon/%s/src/phonon-%s.tar.xz' % (ver ,ver)
             self.targetInstSrc[ver] = 'phonon-%s' % ver
-        self.patchToApply['4.7.0'] = ("phonon-4.7.0-fix-dll-linkage.diff", 1) # upstream
-        self.targetDigests['4.7.0'] = 'feda28afe016fe38eb253f2be01973fc0226d10f'
-        self.targetDigests['4.7.1'] = 'f1d3214a752d97028dc4ed910a832c1272951522'
-        self.targetDigests['4.8.0'] = 'b01da88ddba0d2d501bf1b6bb86abbff61ab6a12'
-        self.targetDigests['4.8.1'] = '652872be8144c88974aa1992de5ae705a90e3be1'
-        self.targetDigests['4.8.3'] = 'aac5dc44ae4821e6165c6735b9c6063dd111ac03'
-        
+        self.targetDigests['4.9.0'] = (['bb74b40f18ade1d9ab89ffcd7aeb7555be797ca395f1224c488b394da6deb0e0'], CraftHash.HashAlgorithm.SHA256)
+
         self.shortDescription = "a Qt based multimedia framework"
-        self.defaultTarget = 'gitHEAD'
+        self.defaultTarget = '4.9.0'
 
 from Package.CMakePackageBase import *
 
