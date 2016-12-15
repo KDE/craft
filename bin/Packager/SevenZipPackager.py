d16d46f65ebb4e8cb13d18767add214475516824
Hannah von Reth <vonreth@kde.org>
2016-12-15 16:32:11 +0100

    Update progress display

--- bin/Packager/SevenZipPackager.py
+++ bin/Packager/SevenZipPackager.py
@@ -20,26 +20,20 @@ class SevenZipPackager (PackagerBase):
     """Packager using the 7za command line tool from the dev-utils/7zip package"""
     def __init__( self, initialized = False ):
         if not initialized: PackagerBase.__init__( self )
-        self.packagerExe = utils.UtilsCache.findApplication("7za")
-        if not self.packagerExe:
-            self.packagerExe = os.path.join(CraftStandardDirs.craftRoot(), "craft", "bin", "craft_7za")
 
     def _compress(self, archiveName, sourceDir, destDir):
         utils.deleteFile(archiveName)
-        cmd = "%s a -r %s %s/*" % (self.packagerExe, os.path.join(destDir, archiveName), sourceDir )
-        cmd += " -bsp1"
-        if craftDebug.verbose() <= 1:
-            cmd += " -bso0"
-        if not utils.system(cmd):
+        app = utils.UtilsCache.findApplication("7za")
+        cmd = "%s a -r %s %s/*" % (app, os.path.join(destDir, archiveName), sourceDir )
+        if utils.UtilsCache.appSupportsCommand(app, "-bs"):
+            cmd += " -bsp1"
+            cmd += " -bso2"
+        if not utils.system(cmd, displayProgress=True):
             craftDebug.log.critical("while packaging. cmd: %s" % cmd)
         CraftHash.createDigestFiles(os.path.join(destDir, archiveName))
 
     def createPackage(self):
         """create 7z package with digest files located in the manifest subdir"""
-
-        if not self.packagerExe:
-            craftDebug.log.critical("could not find 7za in your path!")
-
         if craftSettings.getboolean("ContinuousIntegration", "CreateCache"):
             dstpath = self.cacheLocation()
         else:
