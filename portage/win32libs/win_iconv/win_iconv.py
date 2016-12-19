b404d11e049400376b933b27d7775ac90e05ec24
Hannah von Reth <vonreth@kde.org>
2016-12-19 12:51:38 +0100

    win_iconv: update to 0.0.8

--- portage/win32libs/win_iconv/win_iconv.py
+++ portage/win32libs/win_iconv/win_iconv.py
@@ -3,14 +3,15 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = 'https://github.com/win-iconv/win-iconv.git'
-        for ver in ['0.0.4', '0.0.6']:
-            self.targets[ver] = 'https://github.com/win-iconv/win-iconv/archive/%s.tar.gz' % ver
+        for ver in ['0.0.7', '0.0.8']:
+            self.targets[ver] = 'https://github.com/win-iconv/win-iconv/archive/v%s.tar.gz' % ver
             self.archiveNames[ver] = "win-iconv-%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'win-iconv-%s' % ver
 
-        self.targetDigests['0.0.6'] = '731bd257920ade27375e3794447d2b291ebfe751'
+        self.targetDigests['0.0.8'] = (['23adea990a8303c6e69e32a64a30171efcb1b73824a1c2da1bbf576b0ae7c520'], CraftHash.HashAlgorithm.SHA256)
+
         self.shortDescription = "a character set conversion library binary compatible with GNU iconv"
-        self.defaultTarget = '0.0.6'
+        self.defaultTarget = '0.0.8'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
