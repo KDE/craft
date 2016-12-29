a6956945c08047f27aaf1f00c3dd44ce7b4869d7
Kevin Funk <kfunk@kde.org>
2016-12-29 15:40:17 +0100

    Revive libssh, use current v0.7.3
    No custom patches needed, builds fine under MSVC2015

--- portage/win32libs/libssh/libssh.py
+++ portage/win32libs/libssh/libssh.py
@@ -3,31 +3,17 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ['0.4.4', '0.4.6', '0.4.7']:
-            self.targets[ ver ] = "http://www.libssh.org/files/0.4/libssh-" + ver + ".tar.gz"
-            self.targetInstSrc[ ver ] = "libssh-" + ver
-        for ver in ['0.5.0', '0.5.2', '0.5.3']:
-            self.targets[ ver ] = "http://www.libssh.org/files/0.5/libssh-" + ver + ".tar.gz"
-            self.targetInstSrc[ ver ] = "libssh-" + ver
-        self.targetDigests['0.4.4'] = 'bde1d4713a86b6256ce2d14e6de6326e52c4da44'
-        self.targetDigests['0.4.6'] = '52e7e68590fbcd835bc4a2eceb51e672641efb69'
-        self.targetDigests['0.4.7'] = '5e31736ff906e745e6da508828685d8389e37954'
-        self.targetDigests['0.5.0'] = 'ec72a2e23f97d412c465f8ba97d688679550ac18'
-        self.patchToApply['0.4.7'] = [("libssh-0.4.7-20110116.diff", 1)]
-        self.patchToApply['0.5.0'] = [("libssh-0.5.0-20110601.diff", 1)]
-        self.patchToApply['0.5.2'] = [("0001-implement-support-for-putty-s-pageant.patch", 1), 
-                                      ("0002-add-a-way-to-test-ssh-connections-on-windows.patch", 1)]
-        self.patchToApply['0.5.3'] = [ ("0002-add-a-way-to-test-ssh-connections-on-windows.patch", 1),
-                                       ("0001-implement-support-for-putty-s-pageant-0.5.3.patch", 1),
-                                       ("0003-intel-compiler-support.patch", 1),
-                                       ("0001-Make-compile-with-Windows-SDK-v8.0.patch", 1)]
+        for ver in [(195, '0.7.3')]:
+            # e.g. https://red.libssh.org/attachments/download/195/libssh-0.7.3.tar.xz
+            self.targets[ ver[1] ] = "https://red.libssh.org/attachments/download/{0}/libssh-{1}.tar.xz".format(ver[0], ver[1])
+            self.targetInstSrc[ ver[1] ] = "libssh-" + ver[1]
+        self.targetDigests['0.7.3'] = '9de2a8fde51aa7b7855008fafd5bf47ebb01289f'
 
         self.svnTargets['gitHEAD'] = "git://git.libssh.org/projects/libssh.git"
-        self.svnTargets['0.4'] = "git://git.libssh.org/projects/libssh.git|v0-4"
-        self.svnTargets['0.5'] = "git://git.libssh.org/projects/libssh.git|v0-5"
+
         self.shortDescription = "a working SSH implementation by the mean of a library"
-        self.defaultTarget = '0.5.3'
-        self.options.configure.defines = "-DWITH_STATIC_LIB=ON"
+        self.defaultTarget = '0.7.3'
+        #self.options.configure.defines = "-DWITH_STATIC_LIB=ON"
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
