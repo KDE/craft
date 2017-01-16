7df5f93905fce1f8010fe710edbc1ba5f4d3a2ed
Hannah von Reth <vonreth@kde.org>
2017-01-16 14:12:59 +0100

    DbgView: update recipe

--- portage/dev-util/dbgview/dbgview.py
+++ portage/dev-util/dbgview/dbgview.py
@@ -3,16 +3,13 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.targets['4.81'] = 'http://download.sysinternals.com/files/DebugView.zip'
+        self.targetInstallPath['4.81'] = "dev-utils/bin"
+        self.targetDigests['4.81'] = (['98edbe8d5e10d8c81f91c1d79668df8dd3924abd6bb64e3450613c43bf0c60f6'], CraftHash.HashAlgorithm.SHA256)
         self.defaultTarget = '4.81'
-        # the zip file does not have a bin dir, so we have to create it
-        # This attribute is in prelimary state
-        self.targetInstallPath['4.81'] = "bin"
 
 from Package.BinaryPackageBase import *
 
 class Package(BinaryPackageBase):
     def __init__( self):
         BinaryPackageBase.__init__(self)
-        self.subinfo.options.merge.ignoreBuildType = True
-        self.subinfo.options.merge.destinationPath = "dev-utils"
 
