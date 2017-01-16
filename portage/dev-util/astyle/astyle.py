17e0c4a26959c4a061f55f707dfde4b94a2a1381
Hannah von Reth <vonreth@kde.org>
2017-01-16 13:43:02 +0100

    Astyle: Update recipe

--- portage/dev-util/astyle/astyle.py
+++ portage/dev-util/astyle/astyle.py
@@ -2,9 +2,10 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.targets['1.22'] = "http://www.winkde.org/pub/kde/ports/win32/repository/other/astyle-1.22-bin.zip"
         self.targets["2.0.4"] = "http://downloads.sourceforge.net/sourceforge/astyle/AStyle_2.04_windows.zip"
-        self.targetMergeSourcePath["2.0.4"] = "AStyle"
+        self.targetInstSrc["2.0.4"] = "AStyle"
+        self.targetInstallPath["2.0.4"] = "dev-utils"
+        self.targetDigests['2.0.4'] = (['55af23dc101154f9645c10e3352142b6e8bc4992ec82953677f6a03f4a7e10be'], CraftHash.HashAlgorithm.SHA256)
         self.defaultTarget = '2.0.4'
 
 from Package.BinaryPackageBase import *
@@ -12,6 +13,5 @@ from Package.BinaryPackageBase import *
 class Package(BinaryPackageBase):
     def __init__( self):
         BinaryPackageBase.__init__(self)
-        self.subinfo.options.merge.destinationPath = "dev-utils"
         self.subinfo.options.merge.ignoreBuildType = True
 
