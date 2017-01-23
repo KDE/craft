0940c5248f88fea3c05ba4c475224ff789e3ab79
Hannah von Reth <vonreth@kde.org>
2017-01-23 09:30:37 +0100

    Update perl, use their installer....

--- portage/dev-util/perl/perl.py
+++ portage/dev-util/perl/perl.py
@@ -3,14 +3,17 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        ver = "5.22.1.2201"
-        build = "299574"
+        "http://downloads.activestate.com/ActivePerl/releases/5.24.1.2402/ActivePerl-5.24.1.2402-MSWin32-x64-401627.exe"
+        ver = "5.24.1.2402"
+        build = "401627"
         arch = "x86-64int"
         if compiler.isX64():
             arch = "x64"
-        self.targets[ver] = "http://downloads.activestate.com/ActivePerl/releases/%s/ActivePerl-%s-MSWin32-%s-%s.zip" % (ver, ver, arch, build)
-        self.targetDigestUrls[ver] = (["http://downloads.activestate.com/ActivePerl/releases/%s/SHA256SUM" % ver], CraftHash.HashAlgorithm.SHA256)
-        self.targetMergeSourcePath[ver] = "ActivePerl-%s-MSWin32-%s-%s\\perl" % (ver, arch, build)
+        self.targets[ver] = "http://downloads.activestate.com/ActivePerl/releases/{ver}/ActivePerl-{ver}-MSWin32-{arch}-{build}.exe".format(
+            ver=ver, arch=arch, build=build)
+        self.targetInstallPath[ver] = "dev-utils"
+        self.targetInstSrc[ver] = "6235264" #TODO: where is the number coming from ....
+        self.targetDigestUrls[ver] = (["http://downloads.activestate.com/ActivePerl/releases/{0}/SHA256SUM",format(ver)], CraftHash.HashAlgorithm.SHA256)
         self.defaultTarget = ver
 
     def setDependencies( self ):
@@ -22,5 +25,14 @@ from Package.BinaryPackageBase import *
 class Package(BinaryPackageBase):
     def __init__( self):
         BinaryPackageBase.__init__(self)
-        self.subinfo.options.merge.destinationPath = "dev-utils"
+        self.subinfo.options.unpack.runInstaller = True
+        self.subinfo.options.configure.defines = "/extract {0}".format(self.workDir())
+
+
+    def unpack(self):
+        if not BinaryPackageBase.unpack(self):
+            return False
+        _, name = os.path.split(self.subinfo.targets[self.subinfo.buildTarget])
+        utils.deleteFile(os.path.join(self.sourceDir(), "{0}.msi".format(name)))
+        return True
 
