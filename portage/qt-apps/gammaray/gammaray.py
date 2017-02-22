ea80b8f866ae7ad330a2e4f5bc2366733f2f001b
Kevin Funk <kfunk@kde.org>
2017-02-22 12:59:13 +0100

    Update gammaray to 2.7.0

--- portage/qt-apps/gammaray/gammaray.py
+++ portage/qt-apps/gammaray/gammaray.py
@@ -3,15 +3,15 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets["master"] = "[git]https://github.com/KDAB/GammaRay.git"
-        for ver in ["2.6.0"]:
+        for ver in ["2.7.0"]:
             self.targets[ver] = "https://github.com/KDAB/GammaRay/archive/v%s.tar.gz" % ver
             self.targetInstSrc[ver] = "gammaray-%s" % ver
             self.archiveNames[ver] = "gammaray-%s.tar.gz" % ver
-        self.targetDigests['2.6.0'] = (['762fc1e61fb141462e72fe048b4a7bbf1063eea6a2209963c8aa1ad7696b0217'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['2.7.0'] = (['74251d9de4bfa31994431c7a493e5de17d0b90853557a245bf3f7f4e0227fd14'], CraftHash.HashAlgorithm.SHA256)
 
         self.shortDescription = "GammaRay is a tool to poke around in a Qt-application and also to manipulate the application to some extent"
 
-        self.defaultTarget = "2.6.0"
+        self.defaultTarget = "2.7.0"
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
