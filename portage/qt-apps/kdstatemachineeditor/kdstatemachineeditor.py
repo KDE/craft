e2ac00c6f81b596db7fa9cc9940deb42503861b3
Kevin Funk <kfunk@kde.org>
2017-02-22 12:58:12 +0100

    Update kdstatemachineeditor

--- portage/qt-apps/kdstatemachineeditor/kdstatemachineeditor.py
+++ portage/qt-apps/kdstatemachineeditor/kdstatemachineeditor.py
@@ -3,15 +3,15 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets['master'] = 'https://github.com/KDAB/KDStateMachineEditor.git'
-        for ver in ["1.1.0"]:
+        for ver in ["1.2.0"]:
             self.targets[ver] = 'https://github.com/KDAB/KDStateMachineEditor/archive/v%s.tar.gz' % ver
             self.archiveNames[ver] = "kdstatemachineeditor-%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'KDStateMachineEditor-%s' % ver
 
-        self.targetDigests['1.1.0'] = (['3dcbb8925d3fc9e1ec760a486477f30856bd66e0086227dc2af0b0afac973a4a'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.2.0'] = (['c43b864e60c025b1d4eb03f0b3073e00ebb642aecf10dd8b5b29f4a2da2b1c07'], CraftHash.HashAlgorithm.SHA256)
 
         self.shortDescription = "The KDAB State Machine Editor Library is a framework that can be used to help develop full-featured State Machine Editing graphical user interfaces and tools."
-        self.defaultTarget = "1.1.0"
+        self.defaultTarget = "1.2.0"
 
     def setDependencies( self ):
         self.buildDependencies['frameworks/extra-cmake-modules'] = 'default'
