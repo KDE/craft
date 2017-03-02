7116b3144a4e70f8da8c6e9cb8f119d12d227872
Kevin Funk <kfunk@kde.org>
2017-03-02 22:28:56 +0100

    kdeenv-call.bat: Also add craft dir to PATH

--- kdeenv-call.bat
+++ kdeenv-call.bat
@@ -1,12 +1,17 @@
 @echo off
 
+rem Add the script's dir to PATH (for craft.bat)
+set PATH=%~dp0;%PATH%
+
+rem Add Python to PATH
 for /F "tokens=* USEBACKQ" %%F in (`powershell -ExecutionPolicy RemoteSigned %~dp0kdeenv.ps1 --get Paths Python`) do (
     set _PYTHONPATH=%%F
 )
 set PATH=%_PYTHONPATH%;%PATH%
 set _PYTHONPATH=
 
-rem Simulate a Unix eval here...
+rem Setup compiler env, etc.
+rem Note: Simulating a Unix eval here to eval the output of the setup helper script
 set rand=%random%
 set filename="%temp%\kdeenv_%random%.cmd"
 python %~dp0bin\CraftSetupHelper.py --setup --mode cmd > %filename%
