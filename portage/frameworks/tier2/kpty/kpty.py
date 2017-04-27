3d86323cfd0e1640d9e544c2a62dedcbc66e7402
Hannah von Reth <vonreth@kde.org>
2017-04-27 20:31:33 +0200

    Disable kpty on Windows

--- portage/frameworks/tier2/kpty/kpty.py
+++ portage/frameworks/tier2/kpty/kpty.py
@@ -13,10 +13,16 @@ class subinfo(info.infoclass):
         self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
 from Package.CMakePackageBase import *
 
-class Package(CMakePackageBase):
+class UnixPackage(CMakePackageBase):
     def __init__( self ):
         CMakePackageBase.__init__( self )
 
-
-
+if OsUtils.isUnix():
+    class Package(UnixPackage):
+        def __init__( self ):
+            UnixPackage.__init__( self )
+else:
+    class Package(VirtualPackageBase):
+        def __init__( self ):
+            VirtualPackageBase.__init__( self )
 
