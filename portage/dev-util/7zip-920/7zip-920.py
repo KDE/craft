095dcff2520b9c4f491055e00172c718a8e074a7
Hannah von Reth <vonreth@kde.org>
2017-05-02 17:45:12 +0200

    Cleanup 7zip bootstrap
    Reviewers: kfunk
    
    Reviewed By: kfunk
    
    Differential Revision: https://phabricator.kde.org/D5691

---
+++ portage/dev-util/7zip-920/7zip-920.py
@@ -0,0 +1,18 @@
+import info
+from CraftVersion import CraftVersion
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.targets["920"] = "http://www.7-zip.org/a/7za920.zip"
+        self.targetInstallPath["920"] = os.path.join("dev-utils", "bin")
+        self.targetDigests["920"] = (['2a3afe19c180f8373fa02ff00254d5394fec0349f5804e0ad2f6067854ff28ac'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "920"
+
+from Package.BinaryPackageBase import *
+
+class Package( BinaryPackageBase ):
+    def __init__( self ):
+        BinaryPackageBase.__init__( self )
+
+    def install( self ):
+        return utils.copyFile(os.path.join(self.sourceDir(), "7za.exe"), os.path.join(self.installDir(), "7za.exe"))
