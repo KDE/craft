39be8e18c3850d2602b6b42ae4a0831b060bf400
Hannah von Reth <vonreth@kde.org>
2017-05-04 21:17:20 +0200

    Disable cache for pip packages

--- bin/BuildSystem/PipBuildSystem.py
+++ bin/BuildSystem/PipBuildSystem.py
@@ -9,6 +9,7 @@ class PipBuildSystem(BuildSystemBase):
         self.python2 = True
         self.python3 = True
         self.allowExternal = False
+        self.subinfo.options.package.disableBinaryCache = True
 
     def configure( self ):
         return True
