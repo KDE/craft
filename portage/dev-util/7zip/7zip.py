f7842ac0d5570219fd376a92c7d984787aebcf8f
Hannah von Reth <vonreth@kde.org>
2017-05-09 12:00:25 +0200

    Trigger reinstall of 7zip after the addition of 7zip-920

--- portage/dev-util/7zip/7zip.py
+++ portage/dev-util/7zip/7zip.py
@@ -3,11 +3,15 @@ from CraftVersion import CraftVersion
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in [ "1602", "1604"]:
-            self.targets[ ver ] = f"http://www.7-zip.org/a/7z{ver}-extra.7z"
-            self.targetInstallPath[ ver ] = os.path.join("dev-utils", "bin")
-        self.targetDigests['1602'] = (['f6c412e8bc45e4a88e675976024c21ed7a23eeb7eb0af452aa7a9b9a97843aa2'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1604"
+        patchLvl = ".1"
+        for ver in [ "1604"]:
+            self.targets[ ver + patchLvl ] = f"http://www.7-zip.org/a/7z{ver}-extra.7z"
+            self.targetInstallPath[ ver + patchLvl ] = os.path.join("dev-utils", "bin")
+        self.targetDigests['1604.1'] = (['59f41025acc40cf2e0b30b5cc6e4bcb1e07573201e256fbe8edb3c9c514dd251'], CraftHash.HashAlgorithm.SHA256)
+
+        self.shortDescription = "7-Zip is a file archiver with a high compression ratio."
+        self.homepage = "http://www.7-zip.org/"
+        self.defaultTarget = "1604.1"
 
     def setDependencies( self ):
         self.buildDependencies["dev-util/7zip-920"] = "default"
@@ -19,6 +23,7 @@ class Package( BinaryPackageBase ):
         BinaryPackageBase.__init__( self )
 
     def install( self ):
+        utils.utilsCache.clear()
         if compiler.isX64():
             return utils.copyFile(os.path.join(self.sourceDir(), "x64", "7za.exe"), os.path.join(self.installDir(), "7za.exe"), linkOnly=False)
         else:
