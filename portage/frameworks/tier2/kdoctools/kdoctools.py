3e2984f5cfd57540828843329671ddc92ddb9cc0
Hannah von Reth <vonreth@kde.org>
2017-05-20 17:19:11 +0200

    Trigger rebuild of uncachable packages

--- portage/frameworks/tier2/kdoctools/kdoctools.py
+++ portage/frameworks/tier2/kdoctools/kdoctools.py
@@ -3,8 +3,8 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.versionInfo.setDefaultValues()
-        self.patchToApply["5.33.0"] = [("0001-Solve-a-visibility-conflict-for-meinproc5.patch", 1)]
+        self.versionInfo.setDefaultValues(patchLevel=1)
+        self.patchToApply["5.33.0-1"] = [("0001-Solve-a-visibility-conflict-for-meinproc5.patch", 1)]
         self.shortDescription = "Documentation generation from docbook "
 
 
