a61572f1f9ace0d0ce2767850f8a623fe4f7eaea
Montel Laurent <montel@kde.org>
2017-05-22 13:28:55 +0200

    Allow to build kdav lib

---
+++ portage/kde/pim/kdav/kdav.py
@@ -0,0 +1,21 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "KDav library"
+        self.defaultTarget = 'master'
+        
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.buildDependencies["libs/qtbase"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
