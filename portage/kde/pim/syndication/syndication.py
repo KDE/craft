5dd46841194c667afcd1638a8f69230a0990df4f
Montel Laurent <montel@kde.org>
2017-05-22 13:27:28 +0200

    Create syndication lib
    (cherry picked from commit 58a159a4bfe7a041378aee72d92d2eabca382b41)

---
+++ portage/kde/pim/syndication/syndication.py
@@ -0,0 +1,20 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "Syndication library"
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.buildDependencies["libs/qtbase"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
