8d5578e96330a9d7aeb32c4b2f48dea6ea7f7130
Hannah von Reth <vonreth@kde.org>
2017-05-23 23:52:47 +0200

    Drop msvc from libs/runtime and add it as dep to virtual/base
    (cherry picked from commit 8e1cfcf995b325e119904990e5dd13c78b93e605)

--- portage/libs/runtime/runtime.py
+++ portage/libs/runtime/runtime.py
@@ -1,11 +1,13 @@
 import info
+from Package.VirtualPackageBase import VirtualPackageBase
+
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
         # not used  yet only for reference
-        self.targets['0.0.2'] = "http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe"
-        self.shortDescription = "the compiler runtime package"
-        self.defaultTarget = '0.0.2'
+        self.svnTargets['master'] = ""
+        self.shortDescription = "the mingw compiler runtime package"
+        self.defaultTarget = 'master'
 
     def setDependencies( self ):
         self.buildDependencies[ 'virtual/base' ] = 'default'
@@ -14,7 +16,7 @@ from Package.BinaryPackageBase import *
 import compiler
 
 
-class Package( BinaryPackageBase ):
+class PackageMinGW( BinaryPackageBase ):
     def __init__( self ):
         BinaryPackageBase.__init__( self )
         if compiler.isMinGW():
@@ -34,47 +36,30 @@ class Package( BinaryPackageBase ):
 
         srcdir = None
         files = []
-        if compiler.isMinGW():
-            if self.subinfo.options.features.legacyGCC:
-                if compiler.isMinGW_W32():
-                    srcdir = os.path.join( self.rootdir, "mingw", "bin" )
-                elif compiler.isMinGW_W64():
-                    srcdir = os.path.join( self.rootdir, "mingw64", "bin" )
-                files = [ 'libgcc_s_sjlj-1.dll', 'libgomp-1.dll' ]
-            else:
-                files = [ 'libgomp-1.dll', 'libstdc++-6.dll', 'libwinpthread-1.dll' ]
-                if compiler.isMinGW_W32():
-                    files.append('libgcc_s_sjlj-1.dll')
-                    srcdir = os.path.join( self.rootdir, "mingw", "bin" )                    
-                elif compiler.isMinGW_W64():
-                    files.append('libgcc_s_seh-1.dll')
-                    srcdir = os.path.join( self.rootdir, "mingw64", "bin" )
+        if self.subinfo.options.features.legacyGCC:
+            if compiler.isMinGW_W32():
+                srcdir = os.path.join( self.rootdir, "mingw", "bin" )
+            elif compiler.isMinGW_W64():
+                srcdir = os.path.join( self.rootdir, "mingw64", "bin" )
+            files = [ 'libgcc_s_sjlj-1.dll', 'libgomp-1.dll' ]
+        else:
+            files = [ 'libgomp-1.dll', 'libstdc++-6.dll', 'libwinpthread-1.dll' ]
+            if compiler.isMinGW_W32():
+                files.append('libgcc_s_sjlj-1.dll')
+                srcdir = os.path.join( self.rootdir, "mingw", "bin" )
+            elif compiler.isMinGW_W64():
+                files.append('libgcc_s_seh-1.dll')
+                srcdir = os.path.join( self.rootdir, "mingw64", "bin" )
 
-        elif compiler.isMSVC():
-            if compiler.isX86() and os.environ["PROCESSOR_ARCHITECTURE"] == "AMD64":
-                srcdir = os.path.join( os.environ["SystemRoot"], "SysWOW64") 
-            else:
-                srcdir = os.path.join( os.environ["SystemRoot"], "System32")
-
-            if compiler.isMSVC2010():
-                files = [
-                    "msvcr100%s.dll" % postfix,
-                    "msvcp100%s.dll" % postfix
-                ]
-            elif compiler.isMSVC2013():
-                files = [
-                    "msvcr120%s.dll" % postfix,
-                    "msvcp120%s.dll" % postfix
-                ]
-            elif compiler.isMSVC2015():
-                files = [
-                    "concrt140%s.dll" % postfix,
-                    "msvcp140%s.dll" % postfix,
-                    "vccorlib140%s.dll" % postfix,
-                    "vcruntime140%s.dll" % postfix
-                ]
         for file in files:
-            utils.copyFile( os.path.join( srcdir, file ), os.path.join( destdir, file ), False )
-
+            utils.copyFile( os.path.join( srcdir, file ), os.path.join( destdir, file ), linkOnly=False )
         return True
 
+
+if compiler.isMinGW():
+    class Package(PackageMinGW):
+        def __init__( self ):
+            PackageMinGW.__init__( self )
+else:
+    class Package(VirtualPackageBase):
+        pass
