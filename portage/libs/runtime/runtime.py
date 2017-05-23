da91c6cb5b1708c6aeb5545d06d753d63f30a045
Hannah von Reth <vonreth@kde.org>
2017-05-23 23:58:21 +0200

    Fix runtime and cleanup

--- portage/libs/runtime/runtime.py
+++ portage/libs/runtime/runtime.py
@@ -5,7 +5,7 @@ from Package.VirtualPackageBase import VirtualPackageBase
 class subinfo( info.infoclass ):
     def setTargets( self ):
         # not used  yet only for reference
-        self.svnTargets['master'] = ""
+        self.targets['master'] = ""
         self.shortDescription = "the mingw compiler runtime package"
         self.defaultTarget = 'master'
 
@@ -19,8 +19,7 @@ import compiler
 class PackageMinGW( BinaryPackageBase ):
     def __init__( self ):
         BinaryPackageBase.__init__( self )
-        if compiler.isMinGW():
-            self.subinfo.options.package.version = compiler.getGCCVersion()
+        self.subinfo.options.package.version = compiler.getGCCVersion()
 
     def fetch(self):
         return True
@@ -30,26 +29,13 @@ class PackageMinGW( BinaryPackageBase ):
         utils.createDir( self.sourceDir() )
         utils.createDir( destdir )
 
-        postfix = ""
-        if self.buildType() == "Debug":
-            postfix = "d"
-
-        srcdir = None
-        files = []
-        if self.subinfo.options.features.legacyGCC:
-            if compiler.isMinGW_W32():
-                srcdir = os.path.join( self.rootdir, "mingw", "bin" )
-            elif compiler.isMinGW_W64():
-                srcdir = os.path.join( self.rootdir, "mingw64", "bin" )
-            files = [ 'libgcc_s_sjlj-1.dll', 'libgomp-1.dll' ]
-        else:
-            files = [ 'libgomp-1.dll', 'libstdc++-6.dll', 'libwinpthread-1.dll' ]
-            if compiler.isMinGW_W32():
-                files.append('libgcc_s_sjlj-1.dll')
-                srcdir = os.path.join( self.rootdir, "mingw", "bin" )
-            elif compiler.isMinGW_W64():
-                files.append('libgcc_s_seh-1.dll')
-                srcdir = os.path.join( self.rootdir, "mingw64", "bin" )
+        files = [ 'libgomp-1.dll', 'libstdc++-6.dll', 'libwinpthread-1.dll' ]
+        if compiler.isMinGW_W32():
+            files.append('libgcc_s_sjlj-1.dll')
+            srcdir = os.path.join( self.rootdir, "mingw", "bin" )
+        elif compiler.isMinGW_W64():
+            files.append('libgcc_s_seh-1.dll')
+            srcdir = os.path.join( self.rootdir, "mingw64", "bin" )
 
         for file in files:
             utils.copyFile( os.path.join( srcdir, file ), os.path.join( destdir, file ), linkOnly=False )
