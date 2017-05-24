db1ffbb226c24f4ac1752f8f16c0e7f5e5a23769
Montel Laurent <montel@kde.org>
2017-05-24 07:33:07 +0200

    Add kcalcore recipe. But there is a problem with kdelibs4support package
    (cherry picked from commit 9d72b7620ddb611be9320243d7b3f0a251330f09)

---
+++ portage/kde/pim/kcalcore/kcalcore.py
@@ -0,0 +1,22 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "KLDap library"
+        
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.buildDependencies["libs/qtbase"] = "default"
+        self.dependencies["win32libs/libical"] = "default"
+        self.buildDependencies["frameworks/kdelibs4support"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
