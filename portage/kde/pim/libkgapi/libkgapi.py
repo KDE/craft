61f09ff014e6038c682327caee85dea47cc63d0a
Montel Laurent <montel@kde.org>
2017-06-07 08:00:57 +0200

    It was move to pim. Adapt to new framework version

---
+++ portage/kde/pim/libkgapi/libkgapi.py
@@ -0,0 +1,21 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+        self.shortDescription = "KDE library for Google API"
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.dependencies['frameworks/kio'] = 'default'
+        self.dependencies['frameworks/kwindowsystem'] = 'default'
+        self.dependencies['kde/kcalcore'] = 'default'
+        self.dependencies['kde/kcontacts'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
+
