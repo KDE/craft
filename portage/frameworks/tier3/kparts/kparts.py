3018f9b7c409788089d836253bda5c759dc5877c
Kevin Funk <kfunk@kde.org>
2017-06-08 21:34:56 +0200

    Allow kparts to be built with ninja again
    This seems to work fine. CMake 3.7 fixed a lot of Ninja-Generator issues I think.

--- portage/frameworks/tier3/kparts/kparts.py
+++ portage/frameworks/tier3/kparts/kparts.py
@@ -27,8 +27,3 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self ):
         CMakePackageBase.__init__( self )
-        self.supportsNinja = False
-
-
-    
-
