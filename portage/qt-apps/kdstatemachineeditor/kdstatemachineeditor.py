e1c951403db19f079cdf4be54e047e951c6b996a
Hannah von Reth <vonreth@kde.org>
2017-06-09 09:26:44 +0200

    Build 1.2 branch by default and disable internal graphviz build

--- portage/qt-apps/kdstatemachineeditor/kdstatemachineeditor.py
+++ portage/qt-apps/kdstatemachineeditor/kdstatemachineeditor.py
@@ -3,6 +3,7 @@ import info
 class subinfo( info.infoclass ):
     def setTargets( self ):
         self.svnTargets['master'] = 'https://github.com/KDAB/KDStateMachineEditor.git'
+        self.svnTargets['1.2'] = 'https://github.com/KDAB/KDStateMachineEditor.git|1.2'
         for ver in ["1.2.0"]:
             self.targets[ver] = 'https://github.com/KDAB/KDStateMachineEditor/archive/v%s.tar.gz' % ver
             self.archiveNames[ver] = "kdstatemachineeditor-%s.tar.gz" % ver
@@ -11,7 +12,7 @@ class subinfo( info.infoclass ):
         self.targetDigests['1.2.0'] = (['c43b864e60c025b1d4eb03f0b3073e00ebb642aecf10dd8b5b29f4a2da2b1c07'], CraftHash.HashAlgorithm.SHA256)
 
         self.shortDescription = "The KDAB State Machine Editor Library is a framework that can be used to help develop full-featured State Machine Editing graphical user interfaces and tools."
-        self.defaultTarget = "1.2.0"
+        self.defaultTarget = "1.2"
 
     def setDependencies( self ):
         self.buildDependencies['frameworks/extra-cmake-modules'] = 'default'
@@ -24,3 +25,4 @@ from Package.CMakePackageBase import *
 class Package( CMakePackageBase ):
     def __init__( self ):
         CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DWITH_INTERNAL_GRAPHVIZ=OFF"
