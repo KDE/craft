437be1910931e2693e599cbe1e4787e8a9d776b0
Montel Laurent <montel@kde.org>
2017-06-09 13:16:31 +0200

    Add more recipes

---
+++ portage/kde/pim/kalarm/kalarm.py
@@ -0,0 +1,50 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "KAlarm"
+        
+    def setDependencies( self ):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.runtimeDependencies["libs/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/ki18n"] = "default"
+        self.runtimeDependencies["frameworks/kcodecs"] = "default"        
+        self.runtimeDependencies["frameworks/kcompletion"] = "default"
+        self.runtimeDependencies["frameworks/kcoreaddons"] = "default"
+        self.runtimeDependencies["frameworks/kconfig"] = "default"
+        self.runtimeDependencies["frameworks/kdbusaddons"] = "default"
+        
+        self.runtimeDependencies["frameworks/kiconthemes"] = "default"
+        self.runtimeDependencies["frameworks/kitemmodels"] = "default"
+        self.runtimeDependencies["frameworks/kio"] = "default"
+        self.runtimeDependencies["frameworks/kjobwidgets"] = "default"
+        self.runtimeDependencies["frameworks/knewstuff"] = "default"
+        self.runtimeDependencies["frameworks/kservice"] = "default"
+        self.runtimeDependencies["frameworks/kwallet"] = "default"
+        self.runtimeDependencies["frameworks/kwidgetsaddons"] = "default"
+        self.runtimeDependencies["frameworks/kwindowsystem"] = "default"
+        self.runtimeDependencies["frameworks/kxmlgui"] = "default"
+        self.runtimeDependencies["frameworks/kdelibs4support"] = "default"
+        
+        self.runtimeDependencies["kde/kmime"] = "default"
+        self.runtimeDependencies["kde/akonadi"] = "default"
+        self.runtimeDependencies["kde/akonadi-contact"] = "default"
+        self.runtimeDependencies["kde/akonadi-mime"] = "default"
+        self.runtimeDependencies["kde/kpimtextedit"] = "default"
+        self.runtimeDependencies["kde/kholidays"] = "default"
+        self.runtimeDependencies["kde/kidentitymanagement"] = "default"
+        self.runtimeDependencies["kde/kimap"] = "default"
+        self.runtimeDependencies["kde/mailcommon"] = "default"
+        self.runtimeDependencies["kde/mailtransport"] = "default"
+        
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
