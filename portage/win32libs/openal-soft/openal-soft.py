57a472c0e207dd7e2f58b84d130d55a857d7f1ea
Montel Laurent <montel@kde.org>
2017-06-09 13:31:05 +0200

    Fix Bug 380314 - openal-soft doesn"t compile against ms2015 (missing snprintf)
    Use new 1.18.0
    BUG: 380314
    
    (cherry picked from commit c59e293c274fdd5f5fba86b185880771af21c3ab)

--- portage/win32libs/openal-soft/openal-soft.py
+++ portage/win32libs/openal-soft/openal-soft.py
@@ -4,14 +4,14 @@ from Package.CMakePackageBase import *
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in [ '1.15.1' ]:
+        for ver in [ '1.15.1', '1.18.0' ]:
             self.targets[ ver ] = \
                 'http://kcat.strangesoft.net/openal-releases/openal-soft-'+ver+'.tar.bz2'
             self.targetInstSrc[ ver ] = 'openal-soft-' + ver
         self.patchToApply['1.15.1'] = ('openal-soft-1.15.1-20130411.diff', 1)
         self.targetDigests['1.15.1'] = 'a0e73a46740c52ccbde38a3912c5b0fd72679ec8'
         self.shortDescription = 'a library for audio support'
-        self.defaultTarget = '1.15.1'
+        self.defaultTarget = '1.18.0'
 
     def setDependencies( self ):
         self.dependencies['virtual/base'] = 'default'
