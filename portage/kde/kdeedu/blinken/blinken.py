b7c7812d93ee4b54ec338310763e4526c2d77c58
Montel Laurent <montel@kde.org>
2017-06-12 23:17:19 +0200

    Add more recipes
    (cherry picked from commit 337ba6da828625afcd32de003b93b05e913755a9)

---
+++ portage/kde/kdeedu/blinken/blinken.py
@@ -0,0 +1,25 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "Blinken"
+        
+    def setDependencies( self ):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.runtimeDependencies["libs/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/ki18n"] = "default"
+        self.runtimeDependencies["frameworks/kxmlgui"] = "default"
+        self.runtimeDependencies["frameworks/kdbusaddons"] = "default"
+        self.runtimeDependencies["frameworks/kcrash"] = "default"
+        self.runtimeDependencies["frameworks/kdoctools"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
---
+++ portage/kde/kdeedu/klettres/klettres.py
@@ -0,0 +1,28 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "KLetters"
+        
+    def setDependencies( self ):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.runtimeDependencies["libs/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/kwidgetsaddons"] = "default"
+        self.runtimeDependencies["frameworks/kcoreaddons"] = "default"
+        self.runtimeDependencies["frameworks/kconfig"] = "default"
+        self.runtimeDependencies["frameworks/kdoctools"] = "default"
+        self.runtimeDependencies["frameworks/ki18n"] = "default"
+        self.runtimeDependencies["frameworks/kcompletion"] = "default"
+        self.runtimeDependencies["frameworks/kemoticons"] = "default"
+        self.runtimeDependencies["frameworks/knewstuff"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
