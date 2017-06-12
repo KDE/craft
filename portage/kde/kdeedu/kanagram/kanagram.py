337ba6da828625afcd32de003b93b05e913755a9
Montel Laurent <montel@kde.org>
2017-06-12 23:17:19 +0200

    Add more recipes

---
+++ portage/kde/kdeedu/kanagram/kanagram.py
@@ -0,0 +1,30 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "Kanagram"
+        
+    def setDependencies( self ):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.runtimeDependencies["libs/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/ki18n"] = "default"
+        self.runtimeDependencies["frameworks/kcrash"] = "default"
+        self.runtimeDependencies["frameworks/sonnet"] = "default"
+        self.runtimeDependencies["frameworks/kcoreaddons"] = "default"
+        self.runtimeDependencies["frameworks/kdeclarative"] = "default"
+        self.runtimeDependencies["frameworks/kconfig"] = "default"
+        self.runtimeDependencies["frameworks/kdoctools"] = "default"
+        self.runtimeDependencies["frameworks/kio"] = "default"
+        self.runtimeDependencies["frameworks/knewstuff"] = "default"
+        self.runtimeDependencies["kde/libkeduvocdocument"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
