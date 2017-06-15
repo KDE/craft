c0def537de7877fdfd9e4396e9e43566adc91a2a
Hannah von Reth <vonreth@kde.org>
2017-06-15 16:04:23 +0200

    Fix install of cmake nightlies

--- portage/dev-util/cmake/cmake.py
+++ portage/dev-util/cmake/cmake.py
@@ -4,15 +4,15 @@ import CraftHash
 class subinfo( info.infoclass ):
     def setTargets( self ):
         for ver in ["3.2.1", "3.2.2", "3.2.3", "3.3.1", "3.4.1","3.4.3", "3.6.1", "3.7.0", "3.8.0", "3.8.1"]:
-            self.targets[ver] = "http://www.cmake.org/files/v%s/cmake-%s-win32-x86.zip" % (ver[:3], ver)
-            self.targetInstSrc[ver] = "cmake-%s-win32-x86" % ver
+            self.targets[ver] = f"https://www.cmake.org/files/v{ver[:3]}/cmake-{ver}-win32-x86.zip"
+            self.targetInstSrc[ver] = f"cmake-{ver}-win32-x86"
             self.targetInstallPath[ver] = os.path.join("dev-utils", "cmake")
             self.targetDigestUrls[ver] = ("https://cmake.org/files/v%s/cmake-%s-SHA-256.txt"% (ver[:3], ver), CraftHash.HashAlgorithm.SHA256)
 
         nightlyUrl = "https://cmake.org/files/dev/"
         for ver in utils.utilsCache.getNightlyVersionsFromUrl(nightlyUrl + "?C=M;O=D;F=0", "\d.\d.\d\d\d\d\d\d\d\d-[0-9A-Za-z]{5,8}" + re.escape("-win32-x86")):
-            self.targets[ver] = "%s/cmake-%s.zip" %(nightlyUrl, ver)
-            self.targetInstSrc[ver] = "cmake-%s-win32-x86" % ver
+            self.targets[ver] = f"{nightlyUrl}/cmake-{ver}.zip"
+            self.targetInstSrc[ver] = f"cmake-{ver}"
             self.targetInstallPath[ver] = os.path.join("dev-utils", "cmake")
 
         self.shortDescription = "CMake, the cross-platform, open-source build system."
