db8d2622487aadad980aedd0efbd6075abbe331d
Montel Laurent <montel@kde.org>
2017-06-16 07:12:31 +0200

    Create recipes for analitza
    (cherry picked from commit 87c791d63f10827d524f31b1df37c2d500391262)

---
+++ portage/kde/kdeedu/analitza/analitza.py
@@ -0,0 +1,22 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "Analitza Library"
+        
+    def setDependencies( self ):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.runtimeDependencies["libs/qtbase"] = "default"
+        
+        self.runtimeDependencies["win32libs/eigen3"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
