199b75fe43847399c96ea53605f7a6541146f50f
Hannah von Reth <vonreth@kde.org>
2017-06-18 00:50:22 +0200

    Build qt 5.9.0 by default

--- portage/libs/qt5/qt5.py
+++ portage/libs/qt5/qt5.py
@@ -3,19 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in self.versionInfo.tarballs():
-            self.targets[ver] = 'http://download.qt-project.org/official_releases/qt/%s/%s/submodules/%s-opensource-src-%s.zip' % ( ver[0:3], ver, self.versionInfo.packageName(), ver)
-            self.targetDigestUrls[ver] = 'http://download.qt-project.org/official_releases/qt/%s/%s/submodules/%s-opensource-src-%s.zip.sha1' % (ver[0:3], ver, self.versionInfo.packageName(), ver)
-            self.targetInstSrc[ver] = '%s-opensource-src-%s' % ( self.versionInfo.packageName(), ver)
-
-        for ver in self.versionInfo.branches():
-            self.svnTargets[ver] = '[git]git://code.qt.io/qt/%s.git|%s' % ( self.versionInfo.packageName(), ver)
-
-        for ver in self.versionInfo.tags():
-            self.svnTargets[ver] = '[git]git://code.qt.io/qt/%s.git||%s' % ( self.versionInfo.packageName(), ver)
-
-
-        self.defaultTarget = self.versionInfo.defaultTarget()
+        self.versionInfo.setDefaultValues( )
 
     def setDependencies( self ):
         self.dependencies['libs/qtbase'] = 'default'
