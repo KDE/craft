b5c9b2199c102040c87e31781bc5ba7a9253b3f9
Montel Laurent <montel@kde.org>
2017-06-23 14:05:01 +0200

    Add recipe for kxmlrpcclient

---
+++ portage/frameworks/tier3/kxmlrpcclient/kxmlrpcclient.py
@@ -0,0 +1,25 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "This library contains simple XML-RPC Client support"
+        
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.runtimeDependencies["frameworks/ki18n"] = "default"
+        self.runtimeDependencies["frameworks/kio"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
+
+
+    
+
