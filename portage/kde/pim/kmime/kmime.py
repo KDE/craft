119426938140a4fa911a263bb9687f86dc0a1b4b
Montel Laurent <montel@kde.org>
2017-06-27 13:03:53 +0200

    Fix typo

--- portage/kde/pim/kmime/kmime.py
+++ portage/kde/pim/kmime/kmime.py
@@ -15,7 +15,7 @@ class subinfo( info.infoclass ):
         self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
         self.runtimeDependencies["libs/qtbase"] = "default"
         self.runtimeDependencies["frameworks/ki18n"] = "default"
-        self.runtimeDependencies["frameworks/kcodec"] = "default"
+        self.runtimeDependencies["frameworks/kcodecs"] = "default"
 
 from Package.CMakePackageBase import *
 
