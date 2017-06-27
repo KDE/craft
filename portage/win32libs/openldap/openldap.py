152c29aecd73d63897563a11735964f643bbedaf
Montel Laurent <montel@kde.org>
2017-06-27 18:46:19 +0200

    Wierd patch doesn't work

--- portage/win32libs/openldap/openldap.py
+++ portage/win32libs/openldap/openldap.py
@@ -9,8 +9,8 @@ class subinfo( info.infoclass ):
             self.targetInstSrc[ ver ] = 'openldap-' + ver
         self.patchToApply['2.4.28'] = [('openldap-2.4.28-20120212.diff', 1)]
         self.patchToApply['2.4.33'] = [('openldap-2.4.33-20130124.diff', 1)]
-        #self.patchToApply['2.4.36'] = [('openldap-2.4.36-20131003.diff', 1)]
-        self.patchToApply['2.4.36'] = [('openldap-2.4.36-20170627.diff', 1)]
+        self.patchToApply['2.4.36'] = [('openldap-2.4.36-20131003.diff', 1)]
+        #self.patchToApply['2.4.36'] = [('openldap-2.4.36-20170627.diff', 1)]
         self.targetDigests['2.4.28'] = 'd888beae1723002a5a2ff5509d3040df40885774'
         self.targetDigests['2.4.33'] = '0cea642ba2dae1eb719da41bfedb9eba72ad504d'
         self.targetDigests['2.4.36'] = 'da0e18a28a5dade5c98d9a382fd8f0a676a12aca'
