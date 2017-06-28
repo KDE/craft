a6975db713906a4b1ea0f78deb17c9c8cd7e692c
Montel Laurent <montel@kde.org>
2017-06-28 08:17:56 +0200

    Look at to use 2.4.45

--- portage/win32libs/openldap/openldap.py
+++ portage/win32libs/openldap/openldap.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in ['2.4.28', '2.4.33', '2.4.36']:
+        for ver in ['2.4.28', '2.4.33', '2.4.36', '2.4.45']:
             self.targets[ ver ] = ('ftp://ftp.openldap.org/pub/OpenLDAP/'
                                    'openldap-release/openldap-' + ver + '.tgz')
             self.targetInstSrc[ ver ] = 'openldap-' + ver
@@ -11,6 +11,7 @@ class subinfo( info.infoclass ):
         self.patchToApply['2.4.33'] = [('openldap-2.4.33-20130124.diff', 1)]
         self.patchToApply['2.4.36'] = [('openldap-2.4.36-20131003.diff', 1)]
         #self.patchToApply['2.4.36'] = [('openldap-2.4.36-20170627.diff', 1)]
+        self.patchToApply['2.4.45'] = [('openldap-2.4.45-20170628.diff', 1)]
         self.targetDigests['2.4.28'] = 'd888beae1723002a5a2ff5509d3040df40885774'
         self.targetDigests['2.4.33'] = '0cea642ba2dae1eb719da41bfedb9eba72ad504d'
         self.targetDigests['2.4.36'] = 'da0e18a28a5dade5c98d9a382fd8f0a676a12aca'
