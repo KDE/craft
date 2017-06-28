cb81126daf82c90c90a3caaa357f49b12471b063
Hannah von Reth <hannah.vonreth@kdab.com>
2017-06-28 10:49:41 +0200

    Pass args to craftenv.ps1

--- kdeenv.bat
+++ kdeenv.bat
@@ -1,3 +1,10 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 set CraftDeprecatedEntryScript="kdeenv.bat"
+set PATH=%~dp0\bin;!PATH!
+
+if "%1" == "" goto :endargs
+powershell -NoProfile %~dp0\craftenv.ps1 %ComSpec% /C %*
+goto :eof
+:endargs
 powershell -NoProfile %~dp0\craftenv.ps1 %ComSpec%
--- kdeenv.ps1
+++ kdeenv.ps1
@@ -1,5 +1,5 @@
 $env:CraftRoot=[System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 $env:CraftDeprecatedEntryScript="kdeenv.ps1"
-& $env:CraftRoot\craftenv.ps1
+& $env:CraftRoot\craftenv.ps1 "$args"
 
 
