539c7f24b15615c4572e409e7191f04c1782d5c7
Hannah von Reth <vonreth@kde.org>
2017-06-29 08:55:01 +0200

    Cleanup

--- portage/qt-apps/fatcrm/fatcrm.py
+++ portage/qt-apps/fatcrm/fatcrm.py
@@ -31,17 +31,3 @@ class subinfo(info.infoclass):
 class Package( CMakePackageBase ):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.defines = "-DCHARM_SIGN_INSTALLER=OFF"
-
-    def createPackage(self):
-        if craftSettings.getboolean("QtSDK", "Enabled", False):
-            # windeployqt tries to deploy every lib starting with qt5....
-            # therefore we need to make sure it can find qt5keychain
-            keychain = portage.getPackageInstance("qt-libs", "qtkeychain")
-            utils.copyDir(keychain.imageDir(), os.path.join( craftSettings.get("QtSDK", "Path") , craftSettings.get("QtSDK", "Version"), craftSettings.get("QtSDK", "Compiler")))
-
-        old = self.subinfo.options.make.makeOptions
-        self.subinfo.options.make.makeOptions = "package"
-        out = CMakePackageBase.make(self)
-        self.subinfo.options.make.makeOptions = old
-        return out
