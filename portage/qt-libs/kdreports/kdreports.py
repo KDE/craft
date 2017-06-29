dd505e56df0dfd84c07b7ecae76c5992bc5b1753
Hannah von Reth <vonreth@kde.org>
2017-06-29 08:45:37 +0200

    Fix KDReports src dir

--- portage/qt-libs/kdreports/kdreports.py
+++ portage/qt-libs/kdreports/kdreports.py
@@ -5,17 +5,17 @@ from Package.CMakePackageBase import *
 
 class subinfo( info.infoclass ):
     def setDependencies( self ):
-        self.dependencies[ 'libs/qtbase' ] = 'default'
+        self.dependencies[ "libs/qtbase" ] = "default"
 
     def setTargets( self ):
-        self.svnTargets[ 'master' ] = '[git]https://github.com/KDAB/KDReports.git'
-        for ver in [ '1.7.1' ]:
-            self.targets[ ver ] = 'https://github.com/KDAB/KDReports/archive/kdreports-1.7.1.tar.gz'
-            self.targetInstSrc[ ver ] = "kdreports-%s" % ver
-            self.archiveNames[ ver ] = "kdreports-%s.tar.gz" % ver
-        self.targetDigests['1.7.1'] = (['d75f4bf3399bea51837b7a931be8640823168ba19d6dfd346db3e2270a26ca23'], CraftHash.HashAlgorithm.SHA256)
+        self.svnTargets[ "master" ] = "[git]https://github.com/KDAB/KDReports.git"
+        for ver in [ "1.7.1" ]:
+            self.targets[ ver ] = "https://github.com/KDAB/KDReports/archive/kdreports-1.7.1.tar.gz"
+            self.targetInstSrc[ ver ] = f"KDReports-kdreports-{ver}"
+            self.archiveNames[ ver ] = f"kdreports-{ver}.tar.gz"
+        self.targetDigests["1.7.1"] = (["d75f4bf3399bea51837b7a931be8640823168ba19d6dfd346db3e2270a26ca23"], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = '1.7.1'
+        self.defaultTarget = "1.7.1"
 
 
 class Package( CMakePackageBase ):
