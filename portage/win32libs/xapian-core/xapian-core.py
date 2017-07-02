995f966ceb527351019517ac29865bf837415f60
Montel Laurent <montel@kde.org>
2017-07-02 06:55:31 +0200

    xapian-core builds now.
    Patch based on https://phabricator.kde.org/D6434 from Hannah which is based
    on patch from Dan.
    Cool now I can launch sieveeditor on windows

---
+++ portage/win32libs/xapian-core/xapian-core.py
@@ -0,0 +1,21 @@
+import info
+
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.targets['1.2.24'] = 'http://oligarchy.co.uk/xapian/1.2.24/xapian-core-1.2.24.tar.xz'
+        self.targetDigests['1.2.24'] = '5c63be033157e030f41e128956b873fbd9ad6a1c'
+        self.targetInstSrc['1.2.24'] = 'xapian-core-1.2.24'
+        self.shortDescription = "Open Source Search Engine library"
+        self.patchToApply['1.2.24'] = [("xapian-core-1.2.24-20170626.diff", 1)]
+        self.defaultTarget = '1.2.24'
+
+    def setDependencies( self ):
+        self.dependencies['win32libs/libxslt'] = 'default'
+        self.dependencies['virtual/base'] = 'default'
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        CMakePackageBase.__init__( self )
+
