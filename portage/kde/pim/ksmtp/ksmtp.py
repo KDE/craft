f4dff7526770982dcb028ab3cf86662cf22a9433
Montel Laurent <montel@kde.org>
2017-07-03 22:32:52 +0200

    Add ksmtp recipe

---
+++ portage/kde/pim/ksmtp/ksmtp.py
@@ -0,0 +1,25 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+        self.shortDescription = "KSmtp library"
+        
+    def setDependencies( self ):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "master"
+        self.runtimeDependencies["libs/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/ki18n"] = "default"
+        self.runtimeDependencies["frameworks/kcoreaddons"] = "default"
+        self.runtimeDependencies["frameworks/kio"] = "default"
+        self.runtimeDependencies["kde/kmime"] = "default"
+        self.runtimeDependencies['win32libs/cyrus-sasl'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
