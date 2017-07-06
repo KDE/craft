815918469af7092d3d809ff91e3f0b91144ff502
Montel Laurent <montel@kde.org>
2017-07-06 13:35:15 +0200

    Add qca as dependency for okular

--- portage/kde/applications/okular/okular.py
+++ portage/kde/applications/okular/okular.py
@@ -30,6 +30,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies['frameworks/threadweaver'] = 'default'
         self.runtimeDependencies['frameworks/kwallet'] = 'default'
         self.runtimeDependencies['frameworks/khtml'] = 'default'
+        self.runtimeDependencies['kdesupport/qca'] = 'default'
 
 from Package.CMakePackageBase import *
 
