dbf099d5e6aaaddde81d572d1894b98ca340b445
Hannah von Reth <vonreth@kde.org>
2017-07-07 20:35:49 +0200

    Add 4.3 branch

--- portage/qt-apps/qtcreator/qtcreator.py
+++ portage/qt-apps/qtcreator/qtcreator.py
@@ -2,8 +2,9 @@
 import info
 
 class subinfo(info.infoclass):
-    def setTargets( self ):       
+    def setTargets( self ):
         self.svnTargets[ "master" ] = "git://code.qt.io/qt-creator/qt-creator.git"
+        self.svnTargets[ "4.3" ] = "git://code.qt.io/qt-creator/qt-creator.git|4.3"
         self.targets[ "4.0.2" ] = "http://download.qt-project.org/official_releases/qtcreator/4.0/4.0.2/qt-creator-opensource-src-4.0.2.tar.gz"
         self.targetDigests['4.0.2'] = 'ef7c5760d7dc72cb68ee1ddf84cb4245e41c39d5'
         self.targetInstSrc[ "4.0.2" ] = "qt-creator-opensource-src-4.0.2"
