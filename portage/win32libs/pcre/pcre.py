6ff6647f06f18240c688080ba532a7db452271ac
Montel Laurent <montel@kde.org>
2017-07-13 17:05:52 +0200

    Add support for pcre2-10.23. They removed pcre1 8.39 from ftp.
    it's time to increase version :)

--- portage/win32libs/pcre/pcre.py
+++ portage/win32libs/pcre/pcre.py
@@ -3,15 +3,15 @@ from Package.CMakePackageBase import *
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        for ver in [ '8.39' ]:
-            self.targets[ ver ] = 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-' + ver + '.tar.bz2'
+        for ver in [ '10.23' ]:
+            self.targets[ ver ] = 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-' + ver + '.tar.bz2'
             self.targetInstSrc[ ver ] = 'pcre-' + ver
-        self.patchToApply[ '8.39' ] = [ ( "pcre-8.10-20101125.diff", 1 ) ]
+        self.patchToApply[ '10.23' ] = [ ( "pcre-8.10-20101125.diff", 1 ) ]
 
         self.targetDigests['8.39'] = '5e38289fd1b4ef3e8426f31a01e34b6924d80b90'
 
         self.shortDescription = "Perl-Compatible Regular Expressions"
-        self.defaultTarget = '8.39'
+        self.defaultTarget = '10.23'
 
     def setDependencies( self ):
         self.buildDependencies[ 'virtual/base' ] = 'default'
