3d9e0d00424b64048f5ad5857df8de213c4a58f7
Hannah von Reth <vonreth@kde.org>
2017-07-21 16:30:13 +0200

    dbus:Upgrade to 1.11.8

--- portage/win32libs/dbus/dbus.py
+++ portage/win32libs/dbus/dbus.py
@@ -4,7 +4,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ["1.8.4", "1.10.4", "1.11.4", "1.11.14"]:
+        for ver in ["1.8.4", "1.10.4", "1.11.4", "1.11.8", "1.11.14"]:
             self.targets[ver] = f"http://dbus.freedesktop.org/releases/dbus/dbus-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"dbus-{ver}"
             self.targetConfigurePath[ver] = "cmake"
@@ -19,11 +19,12 @@ class subinfo(info.infoclass):
 
         self.targetDigests["1.10.4"] = "ec1921a09199c81ea20b20448237146a414d51ae"
         self.targetDigests["1.11.4"] = (["474de2afde8087adbd26b3fc5cbf6ec45559763c75b21981169a9a1fbac256c9"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.11.8'] = (['fa207530d694706e33378c87e65b2b4304eb99fff71fc6d6caa6f70591b9afd5'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.11.14'] = (['55cfc7fdd2cccb2fce1f75d2132ad4801b5ed6699fc2ce79ed993574adf90c80'], CraftHash.HashAlgorithm.SHA256)
 
         self.shortDescription = "Freedesktop message bus system (daemon and clients)"
         self.homepage = "http://www.freedesktop.org/wiki/Software/dbus/"
-        self.defaultTarget = "1.11.4"
+        self.defaultTarget = "1.11.8"
 
     def setDependencies( self ):
         self.runtimeDependencies["virtual/base"] = "default"
