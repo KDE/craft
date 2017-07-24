e90b055d1e1753732342fa83a454e2414070a025
Hannah von Reth <vonreth@kde.org>
2017-07-24 08:18:59 +0200

    Add libkipi

---
+++ portage/kde/kdegraphics/libkipi/libkipi.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues( )
+
+    def setDependencies( self ):
+        self.buildDependencies['dev-util/gettext-tools'] = 'default'
+        self.runtimeDependencies['win32libs/expat'] = 'default'
+        self.runtimeDependencies['win32libs/tiff'] = 'default'
+        self.runtimeDependencies['frameworks/extra-cmake-modules'] = 'default'
+        self.runtimeDependencies['frameworks/kconfig'] = 'default'
+        self.runtimeDependencies['frameworks/kxmlgui'] = 'default'
+
+
+class Package(CMakePackageBase):
+    def __init__( self):
+        CMakePackageBase.__init__(self)
