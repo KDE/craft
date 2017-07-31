c0d3dcbe4e669c250243d5443ff1bb4d6122af40
Hannah von Reth <vonreth@kde.org>
2017-07-31 13:44:15 +0200

    Add ruqola

---
+++ portage/kde/unreleased/ruqola/ruqola.py
@@ -0,0 +1,20 @@
+import info
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets["master"] = "[git]kde:ruqola"
+        self.defaultTarget = "master"
+
+    def setDependencies( self ):
+        self.runtimeDependencies['virtual/base'] = 'default'
+        self.runtimeDependencies["libs/qtbase"] = "default"
+        self.runtimeDependencies["libs/qtdeclarative"] = "default"
+        self.runtimeDependencies["libs/qtwebsockets"] = "default"
+        self.runtimeDependencies["libs/qtnetworkauth"] = "default"
+        self.runtimeDependencies["frameworks/kirigami"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__( self ):
+        CMakePackageBase.__init__( self )
