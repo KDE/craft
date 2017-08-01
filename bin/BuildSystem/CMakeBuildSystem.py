dfa491261778507282faf57cd0f43fffa72e5e4a
Hannah von Reth <vonreth@kde.org>
2017-08-01 15:02:40 +0200

    Add another workaround for SDK mode

--- bin/BuildSystem/CMakeBuildSystem.py
+++ bin/BuildSystem/CMakeBuildSystem.py
@@ -237,8 +237,9 @@ class CMakeBuildSystem(BuildSystemBase):
         if os.path.exists(tmp):
             utils.mergeTree(tmp, imagedir)
             utils.rmtree(os.path.join(tmp, rootpath.split(os.path.pathsep)[0]))
-        if craftSettings.getboolean("QtSDK", "Enabled", "False"):
-            qtDir = os.path.join(craftSettings.get("QtSDK", "Path"), craftSettings.get("QtSDK", "Version"),
+        if craftSettings.getboolean("QtSDK", "Enabled", False):
+            qtDir = os.path.join(craftSettings.get("QtSDK", "Path"),
+                                 craftSettings.get("QtSDK", "Version")[:3],# they don't get installed to 5.9.1 but 5.9....
                                  craftSettings.get("QtSDK", "Compiler"))
             # drop the drive letter and the first slash [3:]
             path = os.path.join(imagedir, qtDir[3:])
