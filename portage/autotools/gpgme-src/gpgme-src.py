2a25febbf38f3004cee1a5a7de8251e5db215c67
Hannah von Reth <vonreth@kde.org>
2017-08-01 22:23:21 +0200

    Disable broke doc

--- portage/autotools/gpgme-src/gpgme-src.py
+++ portage/autotools/gpgme-src/gpgme-src.py
@@ -7,7 +7,8 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"gpgme-{ver}"
 
-        self.targetDigests['1.9.0'] = (['1b29fedb8bfad775e70eafac5b0590621683b2d9869db994568e6401f4034ceb'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.9.0"] = (["1b29fedb8bfad775e70eafac5b0590621683b2d9869db994568e6401f4034ceb"], CraftHash.HashAlgorithm.SHA256)
+        self.patchToApply["1.9.0"] = [("gpgme-1.9.0-20170801.diff", 1)]
 
         self.shortDescription = "GnuPG cryptography support library (runtime)"
         self.defaultTarget = "1.9.0"
