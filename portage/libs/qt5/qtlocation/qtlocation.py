a7676ae62fa8ff56459dbd0ba345829cb50a634e
Hannah von Reth <vonreth@kde.org>
2017-08-08 08:10:58 +0200

    Add description to QtLocation, add Tag fro QtPositioning

--- portage/libs/qt5/qtlocation/qtlocation.py
+++ portage/libs/qt5/qtlocation/qtlocation.py
@@ -5,6 +5,9 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
+        self.description = "The Qt Location API helps you create viable mapping solutions" \
+                                " using the data available from some of the popular location services."
+        self.tags = "Qt5Positioning, Qt5Location"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = "default"
