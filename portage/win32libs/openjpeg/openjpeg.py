fc329b9bc0c79d6d5627f37de71f01a2fb1df030
Hannah von Reth <vonreth@kde.org>
2017-08-10 14:43:20 +0200

    Add openjpeg

---
+++ portage/win32libs/openjpeg/openjpeg.py
@@ -0,0 +1,28 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["master"] = "https://github.com/uclouvain/openjpeg.git"
+
+        for ver in ["2.1.2"]:
+            self.targets[ver] = f"https://github.com/uclouvain/openjpeg/archive/v{ver}.tar.gz"
+            self.archiveNames[ver] = f"openjpeg-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"openjpeg-{ver}"
+        self.targetDigests['2.1.2'] = (
+        ['4ce77b6ef538ef090d9bde1d5eeff8b3069ab56c4906f083475517c2c023dfa7'], CraftHash.HashAlgorithm.SHA256)
+
+        self.description = "OpenJPEG is an open-source JPEG 2000 codec written in C language."
+        self.webpage = "http://www.openjpeg.org/"
+        self.defaultTarget = "2.1.2"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
+        CMakePackageBase.__init__(self)
