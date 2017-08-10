960931da529ecd7b14bafea1b803aeff75a24ee3
Hannah von Reth <vonreth@kde.org>
2017-08-10 11:17:20 +0200

    Don't change git system settings

--- portage/libs/craft/craft.py
+++ portage/libs/craft/craft.py
@@ -33,7 +33,6 @@ class Package(SourceOnlyPackageBase):
             utils.system(f"\"{git}\" config --global url.git://anongit.kde.org/.insteadOf kde:")
             utils.system(f"\"{git}\" config --global url.ssh://git@git.kde.org/.pushInsteadOf kde:")
             utils.system(f"\"{git}\" config --global core.autocrlf false")
-            utils.system(f"\"{git}\" config --system core.autocrlf false")
 
         return SourceOnlyPackageBase.fetch(self)
 
