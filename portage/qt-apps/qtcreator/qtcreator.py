b80578a961ce272f2c704d09a1c4846750467580
Hannah von Reth <vonreth@kde.org>
2017-08-15 16:17:16 +0200

    Build 4.4 branch by default

--- portage/qt-apps/qtcreator/qtcreator.py
+++ portage/qt-apps/qtcreator/qtcreator.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
             "4.0.2"] = "http://download.qt-project.org/official_releases/qtcreator/4.0/4.0.2/qt-creator-opensource-src-4.0.2.tar.gz"
         self.targetDigests['4.0.2'] = 'ef7c5760d7dc72cb68ee1ddf84cb4245e41c39d5'
         self.targetInstSrc["4.0.2"] = "qt-creator-opensource-src-4.0.2"
-        self.defaultTarget = "4.0.2"
+        self.defaultTarget = "4.4"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = "default"
