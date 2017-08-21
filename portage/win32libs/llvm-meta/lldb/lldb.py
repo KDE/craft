ef41cebde23e323b499a88712539174d77be7a16
Hannah von Reth <vonreth@kde.org>
2017-08-21 11:02:35 +0200

    Apply cmake fix for lldb

--- portage/win32libs/llvm-meta/lldb/lldb.py
+++ portage/win32libs/llvm-meta/lldb/lldb.py
@@ -6,10 +6,12 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
+        self.patchToApply["4.0.1"] = [("lldb-4.0.1.src-20170821.diff", 1)]
+
+
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
-        self.runtimeDependencies["win32libs/llvm-meta/clang"] = "default"
-        self.runtimeDependencies["win32libs/llvm-meta/clang"] = "default"
+        self.runtimeDependencies["win32libs/llvm-meta/llvm"] = "default"
 
 
 from Package.CMakePackageBase import *
@@ -18,12 +20,6 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = "-DLLDB_PATH_TO_LLVM_BUILD=\"%s\" -DLLDB_PATH_TO_LLVM_SOURCE=\"%s\"" % (
-            CraftStandardDirs.craftRoot().replace("\\", "/"),
-            CraftPackageObject.get('win32libs/llvm-meta/llvm').instance.sourceDir().replace("\\", "/"))
-        self.subinfo.options.configure.args += " -DLLDB_PATH_TO_CLANG_BUILD=\"%s\" -DLLDB_PATH_TO_CLANG_SOURCE=\"%s\"" % (
-            CraftStandardDirs.craftRoot().replace("\\", "/"),
-            CraftPackageObject.get('win32libs/llvm-meta/clang').instance.sourceDir().replace("\\", "/"))
 
     def configureOptions(self, defines=""):
         options = CMakePackageBase.configureOptions(self, defines)
