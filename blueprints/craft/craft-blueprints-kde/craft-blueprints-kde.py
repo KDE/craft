20438b6a92983d0c4cf0290c807b3b8aa1becba6
Hannah von Reth <vonreth@kde.org>
2017-08-30 16:10:46 +0200

    Use full url

--- blueprints/craft/craft-blueprints-kde/craft-blueprints-kde.py
+++ blueprints/craft/craft-blueprints-kde/craft-blueprints-kde.py
@@ -4,7 +4,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["master"]:
-            self.svnTargets[ver] = f"[git]kde:craft-blueprints-kde|{ver}|"
+            self.svnTargets[ver] = f"[git]git://anongit.kde.org/craft-blueprints-kde|{ver}|"
         self.defaultTarget = "master"
 
     def setDependencies(self):
