ecc4398115dbcd717a0d9a60fd17c815b0d83ebf
Hannah von Reth <vonreth@kde.org>
2017-08-31 13:51:40 +0200

    Move more blueprints to core

---
+++ blueprints/dev-util/uactools-pkg/uactools-pkg.py
@@ -0,0 +1,21 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setDependencies(self):
+        # This Package provides the binaries for uactools-bin but virtual/base can
+        # not depend on it because it needs a compiler itself.
+        self.runtimeDependencies["virtual/base"] = "default"
+
+    def setTargets(self):
+        self.svnTargets['svnHEAD'] = 'trunk/kdesupport/kdewin/tools/mt'
+        self.defaultTarget = 'svnHEAD'
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args = "-DCMAKE_EXE_LINKER_FLAGS=-static"
