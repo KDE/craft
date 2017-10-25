2d56236e9f15f8502d83f74953e042422dbf2bd9
Hannah von Reth <vonreth@kde.org>
2017-10-25 08:49:29 +0200

    Fix snore notifications

--- bin/Notifier/Backends/Snore.py
+++ bin/Notifier/Backends/Snore.py
@@ -2,31 +2,29 @@ import ctypes
 import os
 import subprocess
 
-import CraftConfig
-import CraftDebug
-import CraftStandardDirs
 from CraftOS.osutils import OsUtils
 from Notifier.NotificationInterface import *
+from CraftCore import CraftCore
+import utils
 
 class Snore(NotificationInterface):
     def __init__(self):
         NotificationInterface.__init__(self, "Snore")
-        self.icon = os.path.join(CraftStandardDirs.CraftStandardDirs.craftBin(), "data", "icons", "craftyBENDER.png")
+        self.icon = os.path.join(CraftCore.standardDirs.craftBin(), "data", "icons", "craftyBENDER.png")
 
     def notify(self, title, message, alertClass):
         try:
             snore = CraftCore.cache.findApplication("snoresend")
             if not snore:
                 return
-            command = f"""{snore} -t "{title}" -m "{message}" -i "{self.icon}" -a "Craft" -c "{alertClass}" --silent """
+            command = [snore, "-t", "title", "-m", message, "-i", self.icon, "-a", "Craft", "-c", alertClass, "--silent"]
             if OsUtils.isWin():
-                command += f" --bring-window-to-front {ctypes.windll.kernel32.GetConsoleWindow()}"
-            CraftDebug.CraftCore.log.debug(command)
-            subprocess.Popen(command,
-                             shell=True,
-                             stdout=subprocess.DEVNULL,
-                             stderr=subprocess.DEVNULL,
-                             cwd=CraftStandardDirs.CraftStandardDirs.craftRoot())  # make sure that nothing is spawned in a build dir
+                command += ["--bring-window-to-front", str(ctypes.windll.kernel32.GetConsoleWindow())]
+            utils.systemWithoutShell(command,
+                                     logCommand=False,
+                                     stdout=subprocess.DEVNULL,
+                                     stderr=subprocess.DEVNULL,
+                                     cwd=CraftCore.standardDirs.craftRoot())  # make sure that nothing is spawned in a build dir
         except Exception as e:
-            CraftDebug.CraftCore.log.debug(e)
+            CraftCore.log.debug(e)
             return
