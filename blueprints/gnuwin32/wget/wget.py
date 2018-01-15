abe37e3457f8a233d61dfc5dd3f54d7090693053
Hannah von Reth <vonreth@kde.org>
2018-01-15 17:00:39 +0100

    Revert "Switch to a newer wget version that uses the windows system certificats"
    This reverts commit 1bc7e1d72f7abd214f465ca043f283fcd7634a76.
    
    This seems to cause even more problems with certs...

--- blueprints/gnuwin32/wget/wget.py
+++ blueprints/gnuwin32/wget/wget.py
@@ -3,12 +3,12 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.19.2"]:
-            self.targets[ver] = f"https://eternallybored.org/misc/wget/releases/wget-{ver}-win{CraftCore.compiler.bits}.zip"
-            self.targetInstallPath[ver] = "dev-utils/bin"
-        self.targetDigests["1.19.2"] = (['2350a4391c6c3b666b6aaf11842009869fba1deae1dfbf4c005e2dc135c69148'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigestsX64["1.19.2"] = (['34d76a8a75315ccacf9fdd6f0d928b663f74d8ff641de3bbc822f704b82e57ad'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.19.2"
+        self.targets[
+            "1.18"] = "http://downloads.sourceforge.net/sourceforge/tumagcc/wget-1.18_curl-7.49.1_win32_win64.7z"
+        self.targetInstallPath["1.18"] = "dev-utils"
+        self.targetDigests['1.18'] = (
+            ['19d4ae30ae35f212e95edb6f18dddab19e1c97e119c36c4231b741e7293f9b3c'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.18"
 
     def setDependencies(self):
         self.buildDependencies["dev-util/7zip"] = "default"
@@ -21,3 +21,13 @@ class Package(BinaryPackageBase):
     def __init__(self):
         BinaryPackageBase.__init__(self)
 
+    def install(self):
+        if CraftCore.compiler.isX64():
+            utils.copyFile(os.path.join(self.sourceDir(), "wget64.exe"),
+                           os.path.join(self.installDir(), "bin", "wget.exe"))
+        else:
+            utils.copyFile(os.path.join(self.sourceDir(), "wget.exe"),
+                           os.path.join(self.installDir(), "bin", "wget.exe"))
+        utils.copyFile(os.path.join(self.sourceDir(), "curl-ca-bundle.crt"),
+                       os.path.join(self.installDir(), "bin", "curl-ca-bundle.crt"))
+        return True
