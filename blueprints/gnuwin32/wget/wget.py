651a66afe790a8094d84c1af4f564cc25f1ef1a5
Hannah von Reth <vonreth@kde.org>
2018-01-25 16:31:29 +0100

    Update wget

--- blueprints/gnuwin32/wget/wget.py
+++ blueprints/gnuwin32/wget/wget.py
@@ -3,12 +3,10 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets[
-            "1.18"] = "http://downloads.sourceforge.net/sourceforge/tumagcc/wget-1.18_curl-7.49.1_win32_win64.7z"
-        self.targetInstallPath["1.18"] = "dev-utils"
-        self.targetDigests['1.18'] = (
-            ['19d4ae30ae35f212e95edb6f18dddab19e1c97e119c36c4231b741e7293f9b3c'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.18"
+        self.targets["1.19.4"] = "http://downloads.sourceforge.net/sourceforge/tumagcc/wget-1.19.4_curl-7.58_aria2-1.33.1_dwnl.7z"
+        self.targetInstallPath["1.19.4"] = "dev-utils"
+        self.targetDigests['1.19.4'] = (['8f927eaba0011aca50b92327a24143b47278bdb724a662e999ddb171b84b2420'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.19.4"
 
     def setDependencies(self):
         self.buildDependencies["dev-util/7zip"] = "default"
@@ -22,12 +20,6 @@ class Package(BinaryPackageBase):
         BinaryPackageBase.__init__(self)
 
     def install(self):
-        if CraftCore.compiler.isX64():
-            utils.copyFile(os.path.join(self.sourceDir(), "wget64.exe"),
-                           os.path.join(self.installDir(), "bin", "wget.exe"))
-        else:
-            utils.copyFile(os.path.join(self.sourceDir(), "wget.exe"),
-                           os.path.join(self.installDir(), "bin", "wget.exe"))
-        utils.copyFile(os.path.join(self.sourceDir(), "curl-ca-bundle.crt"),
-                       os.path.join(self.installDir(), "bin", "curl-ca-bundle.crt"))
+        utils.copyFile(os.path.join(self.sourceDir(), "wget.exe"),
+                       os.path.join(self.installDir(), "bin", "wget.exe"))
         return True
