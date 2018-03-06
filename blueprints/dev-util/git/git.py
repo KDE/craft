0ee537345479666f77b3f69a656736a3466c509b
Hannah von Reth <vonreth@kde.org>
2018-03-06 13:07:41 +0100

    Fix digests

--- blueprints/dev-util/git/git.py
+++ blueprints/dev-util/git/git.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
 
 import info
 from Package.MaybeVirtualPackageBase import *
@@ -11,7 +33,7 @@ class subinfo(info.infoclass):
         self.targets[ver] = f"https://github.com/git-for-windows/git/releases/download/v{ver}.windows.{build}/PortableGit-{ver}-{CraftCore.compiler.bits}-bit.7z.exe"
         self.archiveNames[ver] = f"PortableGit-{ver}-{CraftCore.compiler.bits}-bit.7z"
         self.targetInstallPath[ver] = os.path.join("dev-utils", "git")
-        self.targetDigests[ver] = (["dda9c9eacdec5bb1369351b48d84c7ba947c85cf5f6285e369eebc89075f8bb7"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests[ver] = (["b5a91978956ac14f59ccdbc70c9c8d2f105e730e81ae8316a59791d33f3d6a87"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigestsX64[ver] = (['f51853689ad8a9e30759fb263a31bcd59753b3a97272b0e76a4210528a8631a1'], CraftHash.HashAlgorithm.SHA256)
         self.defaultTarget = ver
 
