7727e7c901eef6cabe9e6467b56b828279cb531d
Hannah von Reth <vonreth@kde.org>
2018-03-12 20:06:08 +0100

    Fix dev-utils name

--- blueprints/gnuwin32/sed/sed.py
+++ blueprints/gnuwin32/sed/sed.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
         for ver in ['4.1.5', '4.2.1']:
             self.targets[ver] = ["http://downloads.sourceforge.net/sourceforge/gnuwin32/sed-%s-bin.zip" % ver,
                                  "http://downloads.sourceforge.net/sourceforge/gnuwin32/sed-%s-dep.zip" % ver]
-            self.targetInstallPath[ver] = "dev-utilss"
+            self.targetInstallPath[ver] = "dev-utils"
         self.targetDigests['4.1.5'] = ['abe52d0be25f1bb44b8a8e7a94e7afa9c15b3ae5',
                                        '736678616305fab80b4ec1a639d5ff0170183310']
         self.targetDigests['4.2.1'] = ['dfd3d1dae27a24784d7ab40eb074196509fa48fe',
