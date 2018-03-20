8139b94287fdefb2fbc86fd84509b051db7299c1
Hannah von Reth <vonreth@kde.org>
2018-03-20 14:37:43 +0100

    Add create-dmg

---
+++ blueprints/dev-util/create-dmg/create-dmg.py
@@ -0,0 +1,27 @@
+import info
+from Package.MakeFilePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["master"] = "https://github.com/andreyvit/create-dmg.git"
+        self.targetInstallPath["master"] = "dev-utils/create-dmg"
+        self.description = "A shell script to build fancy DMGs"
+        self.webpage = "https://github.com/andreyvit/create-dmg"
+        self.defaultTarget = 'master'
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+
+from Package.BinaryPackageBase import *
+
+
+class Package(BinaryPackageBase):
+    def __init__(self):
+        BinaryPackageBase.__init__(self)
+
+    def unpack(self):
+        return True
+
+    def postInstall(self):
+        return utils.createSymlink(os.path.join(self.imageDir(), "dev-utils", "bin", "create-dmg"),
+                                   os.path.join(self.imageDir(), "dev-utils", "create-dmg ", "create-dmg"))
