KDE/craft

commit 7aa9c0a2122684ccab8e788477a1906706d0573c

Handle environment variables with spaces in craftenv.sh
Summary:
craftenv.sh converts the CraftSetupHelper.py output to a bash array which
will split on any whitespace. Now only split on newlines.

This fixes my $PS1 being chopped off after the first space.

Test Plan: environment variables with spaces are no longer broken.

Reviewers: vonreth

Reviewed By: vonreth

Differential Revision: https://phabricator.kde.org/D11576

craftenv.sh
@@ -1,3 +1,5 @@
+set -e
+
 craftRoot="${BASH_SOURCE[0]}"
 if [[ -z "$craftRoot" ]];then
     craftRoot="$0"
@@ -17,12 +19,21 @@ fi
 export craftRoot
 
 CRAFT_ENV=($(python3.6 "$craftRoot/bin/CraftSetupHelper.py" --setup))
-
-for line in "${CRAFT_ENV[@]}"; do
-  if [[ "$line"  =~ "=" ]] && [[ $line != _=* ]];then
-    export $line || true
-  fi
-done
+# Split the CraftSetupHelper.py output by newlines instead of any whitespace
+# to also handled environment variables containing spaces (e.g. $PS1)
+# See https://stackoverflow.com/q/24628076/894271
+function export_lines() {
+    local IFS=$'\n'
+    local lines=($1)
+    local i
+    for (( i=0; i<${#lines[@]}; i++ )) ; do
+        local line=${lines[$i]}
+        if [[ "$line"  =~ "=" ]] && [[ $line != _=* ]] ; then
+            export "$line" || true
+        fi
+    done
+}
+export_lines "$CRAFT_ENV"
 
 cs() {
     dir=$($craftRoot/bin/craft -q --ci-mode --get "sourceDir()" $1)

bin/CraftSetupHelper.py
@@ -331,6 +331,10 @@ class SetupHelper(object):
     def printEnv(self):
         self.setupEnvironment()
         for key, val in os.environ.items():
+            if "\n" in val:
+                log(f"Not adding ${key} to environment since it contains "
+                     "a newline character and that breaks craftenv.sh")
+                continue
             if key.startswith("BASH_FUNC_"):
                 continue
             CraftCore.log.info(f"{key}={val}")

commit 28aafd7e26cc3cb8035462b8e8446a2e8ac646cb

Fix error I introduced in 7aa9c0a21226 (D11576)
Don't split into an array during CRAFT_ENV assignment. Not sure how it
worked for me before...

craftenv.sh
@@ -19,7 +19,7 @@ if [ -n "$PS1" ]; then
     export PS1="CRAFT: $PS1"
 fi
 
-CRAFT_ENV=($(python3.6 "$craftRoot/bin/CraftSetupHelper.py" --setup))
+CRAFT_ENV=$(python3.6 "$craftRoot/bin/CraftSetupHelper.py" --setup)
 # Split the CraftSetupHelper.py output by newlines instead of any whitespace
 # to also handled environment variables containing spaces (e.g. $PS1)
 # See https://stackoverflow.com/q/24628076/894271
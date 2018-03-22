28aafd7e26cc3cb8035462b8e8446a2e8ac646cb
Alex Richardson <arichardson.kde@gmail.com>
2018-03-22 14:57:13 +0000

    Fix error I introduced in 7aa9c0a21226 (D11576)
    Don't split into an array during CRAFT_ENV assignment. Not sure how it
    worked for me before...

--- craftenv.sh
+++ craftenv.sh
@@ -19,7 +19,7 @@ if [ -n "$PS1" ]; then
     export PS1="CRAFT: $PS1"
 fi
 
-CRAFT_ENV=($(python3.6 "$craftRoot/bin/CraftSetupHelper.py" --setup))
+CRAFT_ENV=$(python3.6 "$craftRoot/bin/CraftSetupHelper.py" --setup)
 # Split the CraftSetupHelper.py output by newlines instead of any whitespace
 # to also handled environment variables containing spaces (e.g. $PS1)
 # See https://stackoverflow.com/q/24628076/894271
