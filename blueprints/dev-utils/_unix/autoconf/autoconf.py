c3b3ad94a48cac4fcb448aff8e15b4dbc994f042
Hannah von Reth <vonreth@kde.org>
2018-03-24 21:20:22 +0100

    Install to dev-utils

--- blueprints/dev-utils/_unix/autoconf/autoconf.py
+++ blueprints/dev-utils/_unix/autoconf/autoconf.py
@@ -6,6 +6,7 @@ class subinfo( info.infoclass ):
         for ver in ["2.69"]:
             self.targets[ ver ] = f"https://ftp.gnu.org/gnu/autoconf/autoconf-{ver}.tar.xz"
             self.targetInstSrc[ ver ] = f"autoconf-{ver}"
+            self.targetInstallPath[ver] = "dev-utils"
 
         self.targetDigests["2.69"] = (['64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684'], CraftHash.HashAlgorithm.SHA256)
         self.description = " Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages."
