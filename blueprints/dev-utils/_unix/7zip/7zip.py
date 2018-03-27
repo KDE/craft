05c29bb8958e912a15a3c59bd96de62ce28c2353
Hannah von Reth <vonreth@kde.org>
2018-03-27 23:59:25 +0200

    Use cmake

--- blueprints/dev-utils/_unix/7zip/7zip.py
+++ blueprints/dev-utils/_unix/7zip/7zip.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
         for ver in ["16.02"]:
             self.targets[ver] = f"https://downloads.sourceforge.net/sourceforge/p7zip/p7zip_{ver}_src_all.tar.bz2"
             self.targetInstSrc[ver] = f"p7zip_{ver}"
+            self.targetConfigurePath[ver] = "CPP/7zip/CMAKE"
             self.targetInstallPath[ver] = "dev-utils"
 
         self.targetDigests["16.02"] = (['5eb20ac0e2944f6cb9c2d51dd6c4518941c185347d4089ea89087ffdd6e2341f'], CraftHash.HashAlgorithm.SHA256)
@@ -15,20 +16,12 @@ class subinfo(info.infoclass):
         self.webpage = "https://sourceforge.net/projects/p7zip/"
         self.defaultTarget = "16.02"
 
-from Package.MakeFilePackageBase import *
+from Package.CMakePackageBase import *
 
 
-class Package(MakeFilePackageBase):
+class Package(CMakePackageBase):
     def __init__(self):
-        MakeFilePackageBase.__init__(self)
-        self.subinfo.options.useShadowBuild = False
-
-    def configure(self):
-        if CraftCore.compiler.isMacOS:
-            makefile = os.path.join(self.sourceDir(), "makefile.machine")
-            utils.deleteFile(makefile)
-            return utils.copyFile(os.path.join(self.sourceDir(), "makefile.macosx_llvm_64bits"), makefile)
-        return True
+        CMakePackageBase.__init__(self)
 
     def install(self):
-        return utils.copyFile(os.path.join(self.sourceDir(), "bin", "7za"), os.path.join(self.installDir(), "bin", "7za"), linkOnly=False)
+        return utils.copyFile(os.path.join(self.buildDir(), "bin", "7za"), os.path.join(self.installDir(), "bin", "7za"), linkOnly=False)
