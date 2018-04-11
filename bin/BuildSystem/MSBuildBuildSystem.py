8ad6641116bd2ec4a18a728108a070f5ad185f89
Hannah von Reth <vonreth@kde.org>
2018-04-11 15:10:23 +0200

    Use glob here

--- bin/BuildSystem/MSBuildBuildSystem.py
+++ bin/BuildSystem/MSBuildBuildSystem.py
@@ -1,3 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+import glob
+
 from BuildSystem.BuildSystemBase import *
 
 
@@ -36,16 +62,22 @@ class MSBuildBuildSystem(BuildSystemBase):
             if not os.path.exists(os.path.join(self.installDir(), dir)):
                 os.makedirs(os.path.join(self.installDir(), dir))
 
-        for dir in buildDirs:
-            for root, dirs, files in os.walk(dir):
-                for f in files:
-                    path = os.path.join(root, f)
-                    if f.endswith((".pdb", ".lib")):
-                        utils.copyFile(path, os.path.join(self.imageDir(), "lib", f), linkOnly=False)
-                    elif f.endswith((".dll", ".exe")):
-                        utils.copyFile(path, os.path.join(self.imageDir(), "bin", f), linkOnly=False)
-                    elif installHeaders and f.endswith(".h"):
-                        utils.copyFile(path, os.path.join(self.imageDir(), "include", f), linkOnly=False)
+
+        def globCopy(sourceDir : str, destDir : str, patterns : [str]):
+            for pattern in patterns:
+                for f in glob.glob(os.path.join(sourceDir, pattern), recursive=True):
+                    if not utils.copyFile(f, os.path.join(destDir, os.path.basename(f)), linkOnly=False):
+                        return False
+            return True
+
+        for root in buildDirs:
+            if not globCopy(root, os.path.join(self.imageDir(), "lib"), ["**/*.pdb", "**/*.lib"]):
+                return False
+            if not globCopy(root, os.path.join(self.imageDir(), "bin"), ["**/*.pdb", "**/*.bin", "**/*.dll"]):
+                return False
+            if installHeaders:
+                if not globCopy(root, os.path.join(self.imageDir(), "include"), ["**/*.h", "**/*.hpp"]):
+                    return False
         return True
 
     def unittest(self):
