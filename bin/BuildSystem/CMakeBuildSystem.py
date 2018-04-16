28cd2e838b3d5ceb6ca4f7b21e3e926126795007
Hannah von Reth <vonreth@kde.org>
2018-04-16 12:22:29 +0200

    Try KDE_INSTALL_USE_QT_SYS_PATHS on mac

--- bin/BuildSystem/CMakeBuildSystem.py
+++ bin/BuildSystem/CMakeBuildSystem.py
@@ -54,13 +54,15 @@ class CMakeBuildSystem(BuildSystemBase):
             options += " -DKDE_L10N_AUTO_TRANSLATIONS=ON"
 
         if OsUtils.isWin():
-            options += " -DKDE_INSTALL_USE_QT_SYS_PATHS=ON"
             # people use InstallRequiredSystemLibraries.cmake wrong and unconditionally install the
             # msvc crt...
             options += " -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON"
 
         if OsUtils.isMac():
             options += f" -DKDE_INSTALL_BUNDLEDIR=\"{OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())}/Applications/KDE\" -DAPPLE_SUPPRESS_X11_WARNING=ON"
+
+        if CraftCore.compiler.isWindows or CraftCore.compiler.isMacOS:
+            options += " -DKDE_INSTALL_USE_QT_SYS_PATHS=ON"
 
         if self.subinfo.options.buildTools:
             options += " " + self.subinfo.options.configure.toolsDefine + " "
