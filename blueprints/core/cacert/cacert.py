c66cc374c072ef9f37d7f719ac451fccda2bcca9
Hannah von Reth <vonreth@kde.org>
2018-04-19 14:26:39 +0200

    Update cacert

--- blueprints/core/cacert/cacert.py
+++ blueprints/core/cacert/cacert.py
@@ -3,12 +3,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2018-01-17"]:
+        for ver in ["2018-01-17", "2018-04-07"]:
             self.targets[ver] = f"https://files.kde.org/craft/curl.haxx.se/cacert-{ver}.zip"
             self.targetInstallPath[ver] = "etc"
         self.targetDigests["2018-01-17"] = (["d4dd92eaf208c18ac8fcbe04aa37be0b83a0f7f21cb788699a3acebace891b38"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2018-04-07"] = (["d4989b605f083ff39a2c8220da5c0b3c5e1816a06635361ca777d4a4b4285cab"], CraftHash.HashAlgorithm.SHA256)
         self.webpage = "https://curl.haxx.se/docs/caextract.html"
-        self.defaultTarget = "2018-01-17"
+        self.defaultTarget = "2018-04-07"
 
 from Package.BinaryPackageBase import *
 
