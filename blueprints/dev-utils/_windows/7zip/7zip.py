db2c6e3e6949e28dc2cccc894768df4db1ebfbf5
Hannah von Reth <vonreth@kde.org>
2018-05-02 11:09:56 +0200

    Update 7z

--- blueprints/dev-utils/_windows/7zip/7zip.py
+++ blueprints/dev-utils/_windows/7zip/7zip.py
@@ -4,7 +4,7 @@ from Package.MaybeVirtualPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1801"]:
+        for ver in ["1801", "1805"]:
             self.targets[ver] = f"https://files.kde.org/craft/3rdparty/7zip/7z{ver}-extra.zip"
             self.targetInstSrc[ver] = f"7z{ver}-extra"
             self.targetInstallPath[ver] = os.path.join("dev-utils", "bin")
@@ -14,10 +14,11 @@ class subinfo(info.infoclass):
 
         self.targetDigests["1604.1"] = (["350a20ec1a005255713c39911982bdeb091fc94ad9800448505c4772f8e85074"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1801"] = (["094c1120f3af512855e1c1df86c5f170cb649f2ca97a23834ccebc04ba575a7a"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1805"] = (["74c82814716d0b9e4769e4e4f1ac0c05ed08a2d6c77104ee398cafe3e73832ba"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "7-Zip is a file archiver with a high compression ratio."
         self.webpage = "http://www.7-zip.org/"
-        self.defaultTarget = "1801"
+        self.defaultTarget = "1805"
 
 from Package.BinaryPackageBase import *
 
