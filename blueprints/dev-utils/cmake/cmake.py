5e4131daad8d1fbc30d782647a1a52fbe2e97302
Hannah von Reth <vonreth@kde.org>
2018-05-09 10:28:09 +0200

    Add linux package

--- blueprints/dev-utils/cmake/cmake.py
+++ blueprints/dev-utils/cmake/cmake.py
@@ -5,17 +5,20 @@ class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["3.8.0", "3.8.1", "3.9.1", "3.10.2", "3.10.3", "3.11.0"]:
             majorMinorStr = '.'.join(ver.split('.')[0:2])
-            if OsUtils.isWin():
+            if CraftCore.compiler.isWindows:
                 self.targets[ver] = f"https://www.cmake.org/files/v{majorMinorStr}/cmake-{ver}-win{CraftCore.compiler.bits}-{CraftCore.compiler.architecture}.zip"
                 self.targetInstSrc[ver] = f"cmake-{ver}-win{CraftCore.compiler.bits}-{CraftCore.compiler.architecture}"
-            elif OsUtils.isMac():
+            elif CraftCore.compiler.isMacOS:
                 self.targets[ver] = f"https://www.cmake.org/files/v{majorMinorStr}/cmake-{ver}-Darwin-{CraftCore.compiler.gnuArchitecture}.tar.gz"
                 self.targetInstSrc[ver] = f"cmake-{ver}-Darwin-{CraftCore.compiler.gnuArchitecture}"
+            elif CraftCore.compiler.isLinux:
+                self.targets[ver] = f"https://cmake.org/files/v3.11/cmake-{ver}-Linux-x86_64.tar.gz"
+                self.targetInstSrc[ver] = f"cmake-{ver}-Linux-x86_64"
             self.targetInstallPath[ver] = os.path.join("dev-utils", "cmake")
             self.targetDigestUrls[ver] = (f"https://cmake.org/files/v{majorMinorStr}/cmake-{ver}-SHA-256.txt", CraftHash.HashAlgorithm.SHA256)
 
-        if self.options.dynamic.checkForNightlies:
-            suffix = 'zip' if OsUtils.isWin() else 'tar.gz'
+        if CraftCore.compiler.isLinux and self.options.dynamic.checkForNightlies:
+            suffix = 'zip' if CraftCore.compiler.isWindows else 'tar.gz'
             for ver in CraftCore.cache.getNightlyVersionsFromUrl("https://cmake.org/files/dev/?C=M;O=D;F=0",
                                                                 f"\d.\d.\d\d\d\d\d\d\d\d-[0-9A-Za-z]{5,8}{re.escape('-win32-x86' if OsUtils.isWin() else '-Darwin-x86_64')}"):
                 self.targets[ver] = f"{nightlyUrl}/cmake-{ver}.{suffix}"
@@ -45,23 +48,17 @@ class CMakePackage(BinaryPackageBase):
         BinaryPackageBase.__init__(self)
 
     def postInstall(self):
-        binaryExe = '.exe'
         binaryPath = os.path.join(self.imageDir(), "dev-utils", "cmake", "bin")
         if OsUtils.isMac():
-            binaryExe = ''
             binaryPath = os.path.join(self.imageDir(), "dev-utils", "cmake", "CMake.app", "Contents", "bin")
 
         for name in ["cmake", "cmake-gui", "cmcldeps", "cpack", "ctest"]:
-            sourceBinary = os.path.join(binaryPath, f"{name}{binaryExe}")
-            targetBinary = os.path.join(self.imageDir(), "dev-utils", "bin", f"{name}{binaryExe}")
+            sourceBinary = os.path.join(binaryPath, f"{name}{CraftCore.compiler.executableSuffix}")
+            targetBinary = os.path.join(self.imageDir(), "dev-utils", "bin", f"{name}{CraftCore.compiler.executableSuffix}")
 
             if os.path.exists(sourceBinary):
-                if OsUtils.isWin():
-                    if not utils.createShim(targetBinary, sourceBinary):
-                        return False
-                elif OsUtils.isMac():
-                    if not utils.createSymlink(sourceBinary, targetBinary):
-                        return False
+                if not utils.createShim(targetBinary, sourceBinary):
+                    return False
         return True
 
 
