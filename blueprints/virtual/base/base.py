32ffb448e582811fd47c9c6925a53e550fc33d38
Hannah von Reth <vonreth@kde.org>
2018-05-15 16:36:01 +0200

    Another try to fix the bootstrap order

--- blueprints/virtual/base/base.py
+++ blueprints/virtual/base/base.py
@@ -8,39 +8,45 @@ class subinfo(info.infoclass):
         self.defaultTarget = '0.2'
 
     def setDependencies(self):
-        self.buildDependencies["core/cacert"] = "default"
-        self.buildDependencies["dev-utils/wget"] = "default"
-        self.buildDependencies["dev-utils/shimgen"] = "default"
-        self.buildDependencies["dev-utils/cmake"] = "default"
-        self.buildDependencies["dev-utils/7zip"] = "default"
-        self.buildDependencies["dev-utils/patch"] = "default"
-        self.buildDependencies["dev-utils/sed"] = "default"
-        self.buildDependencies["dev-utils/git"] = "default"
-        self.buildDependencies["dev-utils/automake"] = "default"
-        self.buildDependencies["dev-utils/libtool"] = "default"
+        # The order is important
+        self.buildDependencies["core/cacert"] = None
+        if CraftCore.compiler.isWindows:
+            self.buildDependencies["dev-utils/7zip"] = None
+            self.buildDependencies["dev-utils/wget"] = None
+            self.buildDependencies["dev-utils/shimgen"] = None
+            self.buildDependencies["dev-utils/cmake"] = None
+        else:
+            self.buildDependencies["dev-utils/cmake"] = None
+            self.buildDependencies["dev-utils/7zip"] = None
+
+        self.buildDependencies["dev-utils/patch"] = None
+        self.buildDependencies["dev-utils/sed"] = None
+        self.buildDependencies["dev-utils/git"] = None
+        self.buildDependencies["dev-utils/automake"] = None
+        self.buildDependencies["dev-utils/libtool"] = None
 
         if CraftCore.compiler.isMacOS:
-            self.buildDependencies["dev-utils/macdylibbundler"] = "default"
-            self.buildDependencies["dev-utils/create-dmg"] = "default"
+            self.buildDependencies["dev-utils/macdylibbundler"] = None
+            self.buildDependencies["dev-utils/create-dmg"] = None
 
 
         if CraftCore.compiler.isMinGW():
-            self.buildDependencies["dev-utils/mingw-w64"] = "default"
+            self.buildDependencies["dev-utils/mingw-w64"] = None
         if CraftCore.settings.get("Compile", "MakeProgram", "") == "jom":
-            self.buildDependencies["dev-utils/jom"] = "default"
+            self.buildDependencies["dev-utils/jom"] = None
         if CraftCore.settings.getboolean("Compile", "UseNinja", False):
-            self.buildDependencies["dev-utils/ninja"] = "default"
+            self.buildDependencies["dev-utils/ninja"] = None
         if CraftCore.settings.getboolean("Compile", "UseCCache", False):
-            self.buildDependencies["dev-utils/ccache"] = "default"
+            self.buildDependencies["dev-utils/ccache"] = None
 
         # needed by CollectionPackagerBase
         if (CraftCore.settings.getboolean("QtSDK", "Enabled", False) and
             CraftCore.settings.getboolean("QtSDK","PackageQtSDK",True)):
-            self.buildDependencies["dev-utils/dependencies"] = "default"
+            self.buildDependencies["dev-utils/dependencies"] = None
 
-        self.buildDependencies["craft/craft-blueprints-kde"] = "default"
-        self.buildDependencies["craft/craft-core"] = "default"
-        self.runtimeDependencies["libs/runtime"] = "default"
+        self.buildDependencies["craft/craft-blueprints-kde"] = None
+        self.buildDependencies["craft/craft-core"] = None
+        self.runtimeDependencies["libs/runtime"] = None
 
 
 class Package(VirtualPackageBase):
