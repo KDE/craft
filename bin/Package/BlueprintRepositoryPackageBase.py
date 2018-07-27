dcc6bf1c6166e4e59e8b08d95a9b27362326f770
Hannah von Reth <vonreth@kde.org>
2018-07-27 09:56:57 +0200

    Add Base class for blueprint repos

---
+++ bin/Package/BlueprintRepositoryPackageBase.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+from Package.SourceOnlyPackageBase import *
+
+class BlueprintRepositoryPackageBase(SourceOnlyPackageBase):
+    def __init__(self):
+        SourceOnlyPackageBase.__init__(self)
+        self.subinfo.options.package.disableBinaryCache = True
+        self.subinfo.options.dailyUpdate = True
+
+    def unpack(self):
+        return True
+
+    def install(self):
+        return True
+
+    def qmerge(self):
+        if not SourceOnlyPackageBase.qmerge(self):
+            return False
+        CraftCore.cache.clear()
+        return True
+
+    def createPackage(self):
+        return True
+
+    def checkoutDir(self, index=0):
+        return os.path.join(CraftStandardDirs.blueprintRoot(), self.package.name)
+
