780fd686881a113c573d76e7232aa6850463c58d
Hannah von Reth <vonreth@kde.org>
2018-09-27 10:43:51 +0200

    Fix

--- bin/Packager/MacDMGPackager.py
+++ bin/Packager/MacDMGPackager.py
@@ -81,7 +81,7 @@ class MacDMGPackager( CollectionPackagerBase ):
             if blackList.exists():
                 blackList = [self.read_blacklist(str(blackList))]
                 # use it as whitelist as we want only matches, ignore all others
-                matches = utils.filterDirectoryContent(appPath, whitelist=lambda x:self.blacklisted(x.path, blackList), blacklist=lambda x:True)
+                matches = utils.filterDirectoryContent(appPath, whitelist=lambda x:self.blacklisted(x, blackList), blacklist=lambda x:True)
                 for f in matches:
                     CraftCore.log.info(f"Remove blacklisted file: {f}")
                     utils.deleteFile(f)
