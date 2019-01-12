5ec308fa77623938f8ff37a6add2684895449141
Hannah von Reth <vonreth@kde.org>
2019-01-12 13:32:02 +0100

    Simplify pip support

--- bin/BuildSystem/PipBuildSystem.py
+++ bin/BuildSystem/PipBuildSystem.py
@@ -18,31 +18,14 @@ class PipBuildSystem(BuildSystemBase):
     def install(self):
         ok = True
 
-        pythons = {}  # dict: major version -> path
-        if self.python2 and ("Paths", "PYTHON27") in CraftCore.settings:
-            pythons[2] = CraftCore.settings.get("Paths", "PYTHON27")
+        pythons = []
+        if self.python2:
+            pythons.append("python2")
         if self.python3:
-            pythons[3] = CraftCore.settings.get("Paths", "PYTHON")
+            pythons.append("python3")
 
-        args = []
-
-        for pythonMajorVersion, pythonPath in pythons.items():
-            for app, path in [("pip", os.path.join(pythonPath, "Scripts")),
-                              (f"pip{pythonMajorVersion}", pythonPath),
-                              ("pip", pythonPath)]:
-                if not path.endswith(os.path.sep):
-                    path += os.path.sep
-                CraftCore.log.debug(f"Looking for {app} in {path}")
-                pipExe = shutil.which(app, path=path)
-                if pipExe:
-                    break
-
-            if not pipExe:
-                CraftCore.log.warning(
-                    "Could not find 'pip' executable for Python install: {0}, skipping install".format(pythonPath))
-                return False
-
-            command = [pipExe, "install", "--upgrade"] + args + [self.package.name]
+        for python in pythons:
+            command = [python, "-m", "pip", "install", "--upgrade", self.package.name]
             ok = ok and utils.system(command)
         return ok
 
