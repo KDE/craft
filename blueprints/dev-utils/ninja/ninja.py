0cce3385e4975ddb8d3d57bfc01eae25b16a4063
Hannah von Reth <vonreth@kde.org>
2019-01-30 20:54:26 +0100

    NINJA 1.9.0!!!!

--- blueprints/dev-utils/ninja/ninja.py
+++ blueprints/dev-utils/ninja/ninja.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
         """ """
         self.svnTargets['master'] = "https://github.com/martine/ninja.git"
 
-        for ver in ["1.6.0", "1.7.1", "1.7.2", "1.8.2"]:
+        for ver in ["1.6.0", "1.7.1", "1.7.2", "1.8.2", "1.9.0"]:
             self.targets[ver] = f"https://github.com/ninja-build/ninja/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"ninja-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"ninja-{ver}"
@@ -19,8 +19,9 @@ class subinfo(info.infoclass):
         self.targetDigests['1.6.0'] = 'a6ff055691f6d355234298c21cc18961b4ca2ed9'
         self.targetDigests['1.7.2'] = (['2edda0a5421ace3cf428309211270772dd35a91af60c96f93f90df6bc41b16d9'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.8.2'] = (['86b8700c3d0880c2b44c2ff67ce42774aaf8c28cbf57725cb881569288c1c6f4'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.9.0'] = (['5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9'], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = "1.8.2"
+        self.defaultTarget = "1.9.0"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/mingw-w64"] = None
