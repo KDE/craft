587c7597dab1932145bfb0aabb16905706d98864
Hannah von Reth <vonreth@kde.org>
2019-01-30 23:23:37 +0100

    Update 7z 1806

--- blueprints/dev-utils/_windows/7zip/7zip.py
+++ blueprints/dev-utils/_windows/7zip/7zip.py
@@ -4,7 +4,7 @@ from Package.MaybeVirtualPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1801", "1805"]:
+        for ver in ["1801", "1805", "1806"]:
             self.targets[ver] = f"https://files.kde.org/craft/3rdparty/7zip/7z{ver}-extra.zip"
             self.targetInstSrc[ver] = f"7z{ver}-extra"
             self.targetInstallPath[ver] = os.path.join("dev-utils", "bin")
@@ -15,10 +15,11 @@ class subinfo(info.infoclass):
         self.targetDigests["1604.1"] = (["350a20ec1a005255713c39911982bdeb091fc94ad9800448505c4772f8e85074"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1801"] = (["094c1120f3af512855e1c1df86c5f170cb649f2ca97a23834ccebc04ba575a7a"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1805"] = (["74c82814716d0b9e4769e4e4f1ac0c05ed08a2d6c77104ee398cafe3e73832ba"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1806"] = (['a4efc8471eb02411f2c09b5a0216b83da6a481094be8152c562b16007b2a8fa6'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "7-Zip is a file archiver with a high compression ratio."
         self.webpage = "http://www.7-zip.org/"
-        self.defaultTarget = "1805"
+        self.defaultTarget = "1806"
 
 from Package.BinaryPackageBase import *
 
@@ -30,12 +31,11 @@ class Package(BinaryPackageBase):
 
     def install(self):
         # wee need to keep the x86 exe around as we distribute it in the nsis installers
-        if CraftCore.compiler.isX64():
+        if CraftVersion(self.buildTarget) < "1806":
             return utils.copyFile(os.path.join(self.sourceDir(), "x64", "7za.exe"),
                                   os.path.join(self.installDir(), "7za.exe"), linkOnly=False)
         else:
-            return utils.copyFile(os.path.join(self.sourceDir(), "7za.exe"), os.path.join(self.installDir(), "7za.exe"),
-                                  linkOnly=False)
+            return super().install()
 
     def postQmerge(self):
         CraftCore.cache.clear()
