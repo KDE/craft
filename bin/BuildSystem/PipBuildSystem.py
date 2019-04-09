769901ecdbec10368fd1ede06a05860e53328bd9
Hannah von Reth <vonreth@kde.org>
2019-04-09 15:31:47 +0200

    Allow pip packages from git

--- bin/BuildSystem/PipBuildSystem.py
+++ bin/BuildSystem/PipBuildSystem.py
@@ -9,23 +9,34 @@ class PipBuildSystem(BuildSystemBase):
         self.python2 = True
         self.python3 = True
 
-    def configure(self):
-        return True
-
-    def make(self):
-        return True
-
-    def install(self):
-        ok = True
-
+    @property
+    def __pythons(self):
         pythons = []
         if self.python2:
             pythons.append("python2")
         if self.python3:
             pythons.append("python3")
+        return pythons
 
-        for python in pythons:
-            command = [python, "-m", "pip", "install", "--upgrade", self.package.name]
+    def configure(self):
+        return True
+
+    def make(self):
+        if self.subinfo.svnTarget():
+            for python in self.__pythons:
+                if not utils.system([python, "setup.py", "sdist"], cwd=self.sourceDir()):
+                    return True
+        return True
+
+    def install(self):
+        ok = True
+
+        for python in self.__pythons:
+            command = [python, "-m", "pip", "install", "--upgrade"]
+            if self.subinfo.svnTarget():
+                command += ["-e", self.sourceDir()]
+            else:
+                command += [self.package.name]
             ok = ok and utils.system(command)
         return ok
 
--- bin/Package/PipPackageBase.py
+++ bin/Package/PipPackageBase.py
@@ -1,40 +1,24 @@
 from BuildSystem.PipBuildSystem import *
 from Package.PackageBase import *
 from Packager.PackagerBase import *
-from Source.SourceBase import *
+from Package.VirtualPackageBase import *
+from Source.MultiSource import *
 
-
-class PipPackageBase(PackageBase, SourceBase, PipBuildSystem, PackagerBase):
+class PipPackageBase(PackageBase, PipBuildSystem, PackagerBase):
     """provides a base class for pip packages"""
 
     def __init__(self):
         CraftCore.log.debug("PipPackageBase.__init__ called")
         PackageBase.__init__(self)
-        SourceBase.__init__(self)
+        if self.subinfo.svnTarget():
+            self.__class__.__bases__ += (MultiSource,)
+            MultiSource.__init__(self)
+        else:
+            self.__class__.__bases__ += (VirtualPackageBase,)
+            VirtualPackageBase.__init__(self)
         PipBuildSystem.__init__(self)
         PackagerBase.__init__(self)
 
-    # from SourceBase:
-    def fetch(self):
-        return True
-
-    def unpack(self):
-        return True
-
-    def createPatch(self):
-        return True
-
-    def getUrls(self):
-        return True
-
-    def repositoryUrl(self, dummyIndex=0):
-        return ""
-
-    def repositoryUrlCount(self):
-        return 0
-
-    def localFileNamesBase(self):
-        return []
 
     # from PackagerBase
     def createPackage(self):
