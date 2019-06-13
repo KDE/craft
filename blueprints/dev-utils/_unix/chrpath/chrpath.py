1109602ba1a721aa38248fe80b94fd214f1005ae
Hannah von Reth <vonreth@kde.org>
2019-06-13 16:47:53 +0200

    chrpath is linux only

--- blueprints/dev-utils/_unix/chrpath/chrpath.py
+++ blueprints/dev-utils/_unix/chrpath/chrpath.py
@@ -2,6 +2,9 @@ import info
 
 
 class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.Linux
+
     def setTargets(self):
         self.targets["0.16"] = "https://src.fedoraproject.org/lookaside/extras/chrpath/chrpath-0.16.tar.gz/2bf8d1d1ee345fc8a7915576f5649982/chrpath-0.16.tar.gz"
         self.targetInstSrc["0.16"] = "chrpath-0.16"
