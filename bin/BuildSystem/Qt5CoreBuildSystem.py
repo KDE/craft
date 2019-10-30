7b5d7e91b046232013c0211a21ff59e489d12e07
Hannah von Reth <vonreth@kde.org>
2019-10-30 18:06:16 +0100

    Try to use system libtool with all qt modules

--- bin/BuildSystem/Qt5CoreBuildSystem.py
+++ bin/BuildSystem/Qt5CoreBuildSystem.py
@@ -11,15 +11,32 @@ class Qt5CoreBuildSystem(QMakeBuildSystem):
     def __init__(self):
         QMakeBuildSystem.__init__(self)
 
+
+    def _qtCoreEnv(self):
+        env = {}
+        if CraftCore.compiler.isMacOS:
+            # we need mac's version of libtool here
+            env["PATH"] = os.pathsep.join(["/usr/bin/", os.environ["PATH"]])
+        return env
+
+    def configure(self, configureDefines=""):
+        with utils.ScopedEnv(self._qtCoreEnv()):
+            return super().configure(configureDefines)
+
+    def make(self):
+        with utils.ScopedEnv(self._qtCoreEnv()):
+            return super().make()
+
     def install(self, options=""):
         """implements the make step for Qt projects"""
-        options += f" INSTALL_ROOT={os.path.splitdrive(self.imageDir())[1]} " + self.makeOptions(self.subinfo.options.install.args)
-        if not QMakeBuildSystem.install(self, options):
-            return False
-        self._fixInstallPrefix()
+        with utils.ScopedEnv(self._qtCoreEnv()):
+            options += f" INSTALL_ROOT={os.path.splitdrive(self.imageDir())[1]} " + self.makeOptions(self.subinfo.options.install.args)
+            if not super().install(options):
+                return False
+            self._fixInstallPrefix()
 
-        if OsUtils.isWin():
-            if os.path.exists(os.path.join(self.installDir(), "bin", "mkspecs")):
-                utils.moveFile(os.path.join(self.installDir(), "bin", "mkspecs"),
-                               os.path.join(self.installDir(), "mkspecs"))
-        return True
+            if OsUtils.isWin():
+                if os.path.exists(os.path.join(self.installDir(), "bin", "mkspecs")):
+                    utils.moveFile(os.path.join(self.installDir(), "bin", "mkspecs"),
+                                os.path.join(self.installDir(), "mkspecs"))
+            return True
