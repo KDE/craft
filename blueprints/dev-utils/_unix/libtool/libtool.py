cb3a3b8259c17f6b97d4063f1c515f377b330b26
Hannah von Reth <vonreth@kde.org>
2019-10-30 09:35:56 +0100

    Build libtool with g prefix on mac

--- blueprints/dev-utils/_unix/libtool/libtool.py
+++ blueprints/dev-utils/_unix/libtool/libtool.py
@@ -8,8 +8,9 @@ class subinfo( info.infoclass ):
             self.targetInstSrc[ ver ] = f"libtool-{ver}"
             self.targetInstallPath[ver] = "dev-utils"
 
-        #self.targetDigests["2.4.6"] = (['5d05bb38a23fd3312b10aea93840feec685bdf4a41146e78882848165d3ae921'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2.4.6"] = (['7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f'], CraftHash.HashAlgorithm.SHA256)
         self.description = "GNU libtool is a generic library support script."
+        self.patchLevel["2.4.6"] = 1
         self.defaultTarget = "2.4.6"
 
     def setDependencies( self ):
@@ -22,11 +23,16 @@ class Package( AutoToolsPackageBase ):
         AutoToolsPackageBase.__init__( self )
         self.subinfo.options.configure.autoreconf = False
         self.subinfo.options.configure.args += " --disable-static --enable-shared "
+        if CraftCore.compiler.isMacOS:
+            self.subinfo.options.configure.args += " --program-prefix=g"
 
 
     def postInstall(self):
-        return self.patchInstallPrefix([os.path.join(self.imageDir(), x) for x in ["dev-utils/bin/libtool",
-                                                                                   "dev-utils/bin/libtoolize"]],
+        tool = "libtool"
+        if CraftCore.compiler.isMacOS:
+            tool = f"g{tool}"
+        return self.patchInstallPrefix([os.path.join(self.imageDir(), x) for x in [f"dev-utils/bin/{tool}",
+                                                                                   f"dev-utils/bin/{tool}ize"]],
                                        self.subinfo.buildPrefix,
                                        CraftCore.standardDirs.craftRoot())
 
