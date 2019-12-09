ee6be1d03b599e398e7f5c95ef02d780eab533c8
Hannah von Reth <vonreth@kde.org>
2019-12-09 16:11:23 +0100

    Add packagesdev to create pkg file on mac

---
+++ blueprints/dev-utils/_mac/packagesdev/packagesdev.py
@@ -0,0 +1,44 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = "[git]https://github.com/packagesdev/packages.git"
+        self.patchToApply["master"] = [("packagesdev-20191209.patch", 1)]
+        for ver in ["1.2.6"]:
+            self.patchToApply[ver] = [("packagesdev-20191209.patch", 1)]
+            self.svnTargets[ver] = f"[git]https://github.com/packagesdev/packages.git||v{ver}"
+        self.description = "Integrated Packaging Environment for OS X "
+        self.webpage = "https://github.com/packagesdev/packages"
+        self.defaultTarget = '1.2.6'
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+
+
+from Package.MakeFilePackageBase import *
+
+
+class Package(MakeFilePackageBase):
+    def __init__(self):
+        MakeFilePackageBase.__init__(self)
+        self.subinfo.options.fetch.checkoutSubmodules = True
+        self.subinfo.options.useShadowBuild = False
+
+    def fetch(self):
+        if isinstance(self, GitSource):
+            utils.system(["git", "clean", "-xdf"], cwd=self.sourceDir())
+        return super().fetch()
+
+    def make(self):
+        return utils.system([Path(self.sourceDir()) / "build.sh" ])
+
+    def install(self):
+        dest = Path(self.imageDir()) / "bin"
+        src = Path(self.sourceDir()) / "distribution/build"
+        utils.createDir(dest)
+        files = ["packagesbuild", "packagesutil"]
+        for f in files:
+            if not utils.copyFile(src / f, dest / f):
+                return False
+        return True
