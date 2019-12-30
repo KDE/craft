1b0ff6df8242f7bb288a1afba0cbf7c4a25c35ac
Hannah von Reth <vonreth@kde.org>
2019-12-30 17:07:20 +0100

    Don't require venv for python2

--- blueprints/dev-utils/python2/python2.py
+++ blueprints/dev-utils/python2/python2.py
@@ -7,24 +7,28 @@ class subinfo(info.infoclass):
         self.targets["2"] = ""
         self.defaultTarget = "2"
         self.targetInstallPath["2"] = "dev-utils"
-        self.patchLevel["2"] = 2
+        self.patchLevel["2"] = 3
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/kshimgen"] = None
-        self.runtimeDependencies["python-modules/pip"] = None
-        self.runtimeDependencies["python-modules/virtualenv"] = None
 
 class Package(BinaryPackageBase):
     def __init__(self):
         BinaryPackageBase.__init__(self)
         self.subinfo.options.package.disableBinaryCache = True
-
     def install(self):
         if not BinaryPackageBase.install(self):
             return False
-        binDir = "bin"
+        python2 = CraftCore.cache.findApplication("python2.7")
         if CraftCore.compiler.isWindows:
-            binDir = "Scripts"
-        return utils.createShim(os.path.join(self.installDir(), "bin", f"python2{CraftCore.compiler.executableSuffix}"),
-                                Path(CraftCore.standardDirs.etcDir()) / f"virtualenv/2/{binDir}/python{CraftCore.compiler.executableSuffix}",
-                                useAbsolutePath=True)
+                if not python2 and ("Paths", "PYTHON27") in CraftCore.settings:
+                    python2 = CraftCore.cache.findApplication("python", CraftCore.settings.get("Paths", "PYTHON27"))
+        if not python2:
+            CraftCore.log.critical(f"Please have a look on {CraftCore.settings.iniPath} and make sure that\n"
+                                   "\t[Paths]\n"
+                                   "\tPYTHON27\n"
+                                   "Points to a valid Python installation.")
+            return False
+        return (utils.createShim(os.path.join(self.installDir(), "bin", f"python{CraftCore.compiler.executableSuffix}"), python2, useAbsolutePath=True) and
+                utils.createShim(os.path.join(self.installDir(), "bin", f"python2{CraftCore.compiler.executableSuffix}"), python2, useAbsolutePath=True))
+
--- blueprints/python-modules/pip/pip.py
+++ blueprints/python-modules/pip/pip.py
@@ -16,7 +16,6 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         self.buildDependencies["core/cacert"] = None
-        self.buildDependencies["dev-utils/python2"] = None
         self.buildDependencies["dev-utils/python3"] = None
         self.buildDependencies["python-modules/virtualenv"] = None
 
