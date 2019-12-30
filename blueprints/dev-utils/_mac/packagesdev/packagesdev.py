f794a00ce35b4020d732200e05f619b783d70a2b
Hannah von Reth <vonreth@kde.org>
2019-12-30 16:30:03 +0100

    Update packagesdev

--- blueprints/dev-utils/_mac/packagesdev/packagesdev.py
+++ blueprints/dev-utils/_mac/packagesdev/packagesdev.py
@@ -5,12 +5,12 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = "[git]https://github.com/packagesdev/packages.git"
         self.patchToApply["master"] = [("packagesdev-20191209.patch", 1)]
-        for ver in ["1.2.6"]:
-            self.patchToApply[ver] = [("packagesdev-20191209.patch", 1)]
+        for ver in ["1.2.6", "1.2.7"]:
+           # self.patchToApply[ver] = [("packagesdev-20191209.patch", 1)]
             self.svnTargets[ver] = f"[git]https://github.com/packagesdev/packages.git||v{ver}"
         self.description = "Integrated Packaging Environment for OS X "
         self.webpage = "https://github.com/packagesdev/packages"
-        self.defaultTarget = '1.2.6'
+        self.defaultTarget = '1.2.7'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
