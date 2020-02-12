15a2e6233b63ccd5676ecf04c7c6f614c575fca9
Hannah von Reth <vonreth@kde.org>
2020-02-12 16:51:37 +0100

    If a unbknown target was specified print it

--- bin/info.py
+++ bin/info.py
@@ -99,7 +99,7 @@ class infoclass(object):
         if target in self.targets or target in self.svnTargets:
             return target
         if target:
-            CraftCore.log.warning(f"You defined an invalid target for {self.parent.package.path}")
+            CraftCore.log.warning(f"You defined an invalid target {target} for {self.parent.package.path}")
         return self._defaultTarget
 
 
