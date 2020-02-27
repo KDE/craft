1c456009b97b4be96d6a9efa7a87d5d7f29711b0
Hannah von Reth <vonreth@kde.org>
2020-02-27 12:04:18 +0100

    Use cmake to build ninja

--- blueprints/dev-utils/ninja/ninja.py
+++ blueprints/dev-utils/ninja/ninja.py
@@ -11,38 +11,23 @@ class subinfo(info.infoclass):
         """ """
         self.svnTargets['master'] = "https://github.com/martine/ninja.git"
 
-        for ver in ["1.6.0", "1.7.1", "1.7.2", "1.8.2", "1.9.0", "1.10.0"]:
+        for ver in ["1.10.0"]:
             self.targets[ver] = f"https://github.com/ninja-build/ninja/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"ninja-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"ninja-{ver}"
             self.targetInstallPath[ver] = "dev-utils"
-        self.targetDigests['1.6.0'] = 'a6ff055691f6d355234298c21cc18961b4ca2ed9'
-        self.targetDigests['1.7.2'] = (['2edda0a5421ace3cf428309211270772dd35a91af60c96f93f90df6bc41b16d9'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.8.2'] = (['86b8700c3d0880c2b44c2ff67ce42774aaf8c28cbf57725cb881569288c1c6f4'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.9.0'] = (['5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.9.0'] = (['5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.10.0'] = (['3810318b08489435f8efc19c05525e80a993af5a55baa0dfeae0465a9d45f99f'], CraftHash.HashAlgorithm.SHA256)
+
+        self.patchToApply["1.10.0"] = [("34d1bf2f1dcc138f7cb3a54daf771931cd799785.patch", 1)]
+        self.patchLevel["1.10.0"] = 1
 
         self.defaultTarget = "1.10.0"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/mingw-w64"] = None
+        self.buildDependencies["dev-utils/cmake"] = None
 
 
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-
-    def configure(self):
-        return True
-
-    def make(self):
-        self.enterSourceDir()
-        command = [sys.executable, "configure.py", "--bootstrap"]
-        if CraftCore.compiler.isMinGW():
-            command += ["--platform=mingw"]
-        return utils.system(command)
-
-    def install(self):
-        utils.copyFile(os.path.join(self.sourceDir(), f"ninja{CraftCore.compiler.executableSuffix}"), os.path.join(self.installDir(), "bin", f"ninja{CraftCore.compiler.executableSuffix}"))
-        return True
