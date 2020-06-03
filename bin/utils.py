5ed6a9622dbdf56c988518900fc2ab9cbac7524a
Hannah von Reth <vonreth@kde.org>
2020-06-03 17:25:49 +0200

    Don't apply hidden files as patch

--- bin/utils.py
+++ bin/utils.py
@@ -667,9 +667,11 @@ def applyPatch(sourceDir, f, patchLevel='0'):
     """apply single patch"""
     if os.path.isdir(f):
         # apply a whole dir of patches
-        for patch in os.listdir(f):
-            if not applyPatch(sourceDir, os.path.join(f, patch), patchLevel):
-                return False
+        with os.scandir(f) as scan:
+            for patch in scan:
+                if patch.is_file() and not patch.name.startswith("."):
+                    if not applyPatch(sourceDir, os.path.join(f, patch), patchLevel):
+                        return False
         return True
     with tempfile.TemporaryDirectory() as tmp:
         # rewrite the patch, the gnu patch on Windows is only capable
