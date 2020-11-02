62cb0af4b533034b49f0eccb03c3279a331a2a72
Hannah von Reth <vonreth@kde.org>
2020-11-02 15:15:30 +0100

    Add digests

--- blueprints/core/cacert/cacert.py
+++ blueprints/core/cacert/cacert.py
@@ -8,6 +8,7 @@ class subinfo(info.infoclass):
             self.targetInstallPath[ver] = "etc"
         self.targetDigests["2018-12-05"] = (['6e3a346aac36e271fc23d4476608226059e76ffd0a7645e2a1ba25937cf3df2e'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2020-01-01"] = (['db0d0ddc3ed80b0f4fd59077c266113c4e7e5c031bd1011c42d8495864418edd'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2020-10-14"] = (['c333bd1e49b76f56424a086e02524f7243a864c78e0fe95626da280cd94010cc'], CraftHash.HashAlgorithm.SHA256)
         self.webpage = "https://curl.haxx.se/docs/caextract.html"
         self.defaultTarget = "2020-10-14"
 
