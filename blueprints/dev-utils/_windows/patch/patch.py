d22f4459258527fc1a84fc74d1784a0a6dd8b134
Hannah von Reth <vonreth@kde.org>
2020-11-18 17:22:17 +0100

    Update patch.exe with a binary from msys

--- blueprints/dev-utils/_windows/patch/patch.py
+++ blueprints/dev-utils/_windows/patch/patch.py
@@ -4,9 +4,12 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.targets['2.5.9'] = "http://downloads.sourceforge.net/sourceforge/gnuwin32/patch-2.5.9-7-bin.zip"
+        self.targets['2.7.6'] = "https://files.kde.org/craft/3rdparty/patch/msys-patch-2.7.6.7z"
         self.targetInstallPath["2.5.9"] = "dev-utils"
+        self.targetInstallPath["2.7.6"] = "dev-utils/patch"
         self.targetDigests['2.5.9'] = '7b2ec738881f4e962e54e0f330b67c42635266b7'
-        self.defaultTarget = '2.5.9'
+        self.targetDigests['2.7.6'] = (['2a8ed85190fab42861d64cdfd65015386ea82004e64df04b03eb9640429f9bcf'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '2.7.6'
 
     def setDependencies(self):
         if CraftCore.compiler.isMinGW():
@@ -24,6 +27,9 @@ class Package(BinaryPackageBase):
     def install(self):
         if not BinaryPackageBase.install(self):
             return False
-        manifest = os.path.join(self.packageDir(), "patch.exe.manifest")
-        patch = os.path.join(self.installDir(), "bin", "patch.exe")
-        return utils.embedManifest(patch, manifest)
+        if self.buildTarget == "2.5.9":
+            manifest = os.path.join(self.packageDir(), "patch.exe.manifest")
+            patch = os.path.join(self.installDir(), "bin", "patch.exe")
+            return utils.embedManifest(patch, manifest)
+        else:
+            return utils.createShim(self.imageDir() / "bin/patch.exe", self.installDir() / "patch.exe")
