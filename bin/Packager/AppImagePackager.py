5e23c9362cd9cbceacd9d0a0b5ee526e67746c13
Hannah von Reth <vonreth@kde.org>
2021-04-29 20:50:48 +0200

    Small cleanup

--- bin/Packager/AppImagePackager.py
+++ bin/Packager/AppImagePackager.py
@@ -5,6 +5,11 @@ class AppImagePackager(CollectionPackagerBase):
     def __init__( self, whitelists=None, blacklists=None):
         CollectionPackagerBase.__init__(self, whitelists, blacklists)
 
+    def setDefaults(self, defines: {str:str}) -> {str:str}:
+        defines = super().setDefaults(defines)
+        defines["setupname"] = f"{defines['setupname']}.AppImage"
+        return defines
+
     def createPackage( self ):
         """ create a package """
         CraftCore.log.debug("packaging using the AppImagePackager")
@@ -28,7 +33,7 @@ class AppImagePackager(CollectionPackagerBase):
         env = {"ARCH": "x86_64", "LD_LIBRARY_PATH": f"{archiveDir}/usr/lib:{archiveDir}/usr/lib/x86_64-linux-gnu"}
         if OsUtils.detectDocker:
             env["APPIMAGE_EXTRACT_AND_RUN"] = "1"
-        env["OUTPUT"] = defines["setupname"] + ".AppImage"
+        env["OUTPUT"] = defines["setupname"]
         env["VERSION"] = self.formatVersion(includeRevision=True, includeTimeStamp=False)
         with utils.ScopedEnv(env):
             return utils.system(["linuxdeploy-x86_64.AppImage", "--appdir", self.archiveDir(), "--plugin=qt", "--output=appimage", "--desktop-file", desktopFiles[0]], cwd=self.packageDestinationDir())
