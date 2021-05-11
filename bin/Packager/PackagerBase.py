d17026523bb0a51d9daa1871bd8a4ec1110b05dc
Hannah von Reth <vonreth@kde.org>
2021-05-11 12:50:55 +0200

    Don't use system 7z when creating the cache, it will break bootstraping

--- bin/Packager/PackagerBase.py
+++ bin/Packager/PackagerBase.py
@@ -95,7 +95,7 @@ class PackagerBase(CraftBase):
     def archiveExtension(self):
         extension = "." + CraftCore.settings.get("Packager", "7ZipArchiveType", "7z")
         if extension == ".7z" and CraftCore.compiler.isUnix:
-            if self.package.path == "dev-utils/7zip" or not CraftCore.cache.findApplication("7za"):
+            if not CraftCore.installdb.isInstalled("dev-utils/7zip"):
                 extension = ".tar.xz"
             else:
                 extension = ".tar.7z"
