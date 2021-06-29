e958ee7c1ef24a3fe312b3c0ac1a31703770dcbb
Dawid Wróbel <me@dawidwrobel.com>
2021-06-28 20:47:07 -0500

    cmake: update to 3.19.2, fix Apple Silicon builds
    3.19.2 is the first version that supports Apple Silicon
    
    BUG: 439283

--- blueprints/dev-utils/cmake-base/cmake-base.py
+++ blueprints/dev-utils/cmake-base/cmake-base.py
@@ -7,14 +7,18 @@ class subinfo(info.infoclass):
         self.options.dynamic.registerOption("checkForNightlies", False)
 
     def setTargets(self):
-        for ver in ["3.13.0", "3.13.2", "3.16.3", "3.17.0", "3.17.2", "3.17.3", "3.19.1"]:
+        for ver in ["3.13.0", "3.13.2", "3.16.3", "3.17.0", "3.17.2", "3.17.3", "3.19.1", "3.19.2"]:
             majorMinorStr = '.'.join(ver.split('.')[0:2])
             if CraftCore.compiler.isWindows:
                 self.targets[ver] = f"https://www.cmake.org/files/v{majorMinorStr}/cmake-{ver}-win{CraftCore.compiler.bits}-{CraftCore.compiler.architecture}.zip"
                 self.targetInstSrc[ver] = f"cmake-{ver}-win{CraftCore.compiler.bits}-{CraftCore.compiler.architecture}"
             elif CraftCore.compiler.isMacOS:
-                self.targets[ver] = f"https://www.cmake.org/files/v{majorMinorStr}/cmake-{ver}-Darwin-{CraftCore.compiler.gnuArchitecture}.tar.gz"
-                self.targetInstSrc[ver] = f"cmake-{ver}-Darwin-{CraftCore.compiler.gnuArchitecture}"
+                if ver == "3.19.2":
+                    self.targets[ver] = f"https://www.cmake.org/files/v{majorMinorStr}/cmake-{ver}-macos-universal.tar.gz"
+                    self.targetInstSrc[ver] = f"cmake-{ver}-macos-universal"
+                else:
+                    self.targets[ver] = f"https://www.cmake.org/files/v{majorMinorStr}/cmake-{ver}-Darwin-{CraftCore.compiler.gnuArchitecture}.tar.gz"
+                    self.targetInstSrc[ver] = f"cmake-{ver}-Darwin-{CraftCore.compiler.gnuArchitecture}"
             elif CraftCore.compiler.isLinux:
                 self.targets[ver] = f"https://cmake.org/files/v{majorMinorStr}/cmake-{ver}-Linux-x86_64.tar.gz"
                 self.targetInstSrc[ver] = f"cmake-{ver}-Linux-x86_64"
@@ -34,7 +38,7 @@ class subinfo(info.infoclass):
 
         self.patchLevel["3.13.2"] = 1
 
-        self.defaultTarget = "3.19.1"
+        self.defaultTarget = "3.19.2"
 
     def setDependencies(self):
         # needed for the extraction on windows
