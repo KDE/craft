29bff5f890116eac51a9c1eb2dfbb1ccde7aa0c2
Hannah von Reth <vonreth@kde.org>
2021-08-12 08:49:30 +0200

    Revert "The wrapper is no longer needed"
    This reverts commit b5a44adb074e2d029a55fd17f09e212f871171b9.

---
+++ blueprints/dev-utils/_unix/git/git.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+import info
+from Package.MaybeVirtualPackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.targets["latest"] = ""
+        self.description = "Craft integration package for git."
+        self.defaultTarget = "latest"
+
+    def setDependencies(self):
+        self.buildDependencies["dev-utils/kshimgen"] = None
+
+from Package.BinaryPackageBase import *
+
+
+class Package(BinaryPackageBase):
+    def __init__(self):
+        BinaryPackageBase.__init__(self)
+
+    def postInstall(self):
+        env = None
+        if CraftCore.compiler.isLinux:
+            env = {"LD_LIBRARY_PATH": ""}
+
+        gitPath = ""
+        for path in ["/usr/local/bin/git", "/usr/bin/git"]:
+            if os.path.isfile(path):
+                gitPath = path
+
+        if gitPath == "":
+            return False
+
+        return utils.createShim(os.path.join(self.imageDir(), "dev-utils", "bin", "git"),
+                                gitPath, env=env, useAbsolutePath=True)
+
+    def postQmerge(self):
+        CraftCore.cache.clear()
+        return True
