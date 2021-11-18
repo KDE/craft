2f0fc96bd6270dec648271616e4ebcf1e1bf981b
Hannah von Reth <vonreth@kde.org>
2021-11-18 11:23:57 +0100

    Use our centos build by default on linux

--- blueprints/dev-utils/7zip/7zip.py
+++ blueprints/dev-utils/7zip/7zip.py
@@ -5,7 +5,7 @@ from Package.MaybeVirtualPackageBase import *
 class subinfo(info.infoclass):
     def registerOptions(self):
         self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotFreeBSD & CraftCore.compiler.Platforms.NotAndroid
-        self.options.dynamic.registerOption("useCentosBasedBuild", False)
+        self.options.dynamic.registerOption("useCentosBasedBuild", CraftCore.compiler.isLinux)
 
     def setTargets(self):
         for ver in ["1900"]:
