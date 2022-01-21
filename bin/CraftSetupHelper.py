b569b74c0e2f68bdbbf387117c0473752501afce
Hannah von Reth <vonreth@kde.org>
2022-01-21 18:28:31 +0000

    Bump mac requirements to MacOs 10.15

--- bin/CraftSetupHelper.py
+++ bin/CraftSetupHelper.py
@@ -294,7 +294,7 @@ class SetupHelper(object):
         self.prependEnvVar("LDFLAGS", f"-Wl,-rpath,{os.path.join(CraftCore.standardDirs.craftRoot(), 'lib')}", sep=" ")
         self.prependEnvVar("BISON_PKGDATADIR", os.path.join(CraftCore.standardDirs.craftRoot(), "share", "bison"))
         self.prependEnvVar("M4", os.path.join(CraftCore.standardDirs.craftRoot(), "bin", "m4"))
-        self.addEnvVar("MACOSX_DEPLOYMENT_TARGET", CraftCore.settings.get("General", "MacDeploymentTarget", "10.13"))
+        self.addEnvVar("MACOSX_DEPLOYMENT_TARGET", CraftCore.settings.get("General", "MacDeploymentTarget", "10.15"))
         try:
             dbusInstalled = bool(CraftCore.installdb.isInstalled("libs/dbus"))
         except sqlite3.OperationalError:
