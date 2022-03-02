4855dc701f702405cd27f37e8a2249035abb4076
Hannah von Reth <vonreth@kde.org>
2022-03-02 15:39:58 +0000

    Remove isntallQt6 see https://invent.kde.org/packaging/craft-blueprints-kde/-/commit/eb5783bf9d07f93a3a379a6af597112944359d10

--- bin/BuildSystem/CMakeBuildSystem.py
+++ bin/BuildSystem/CMakeBuildSystem.py
@@ -156,32 +156,3 @@ class CMakeBuildSystem(BuildSystemBase):
         if not super().internalPostQmerge():
             return False
         return PostInstallRoutines.updateSharedMimeInfo(self)
-
-
-    def isntallQt6(self):
-        user_facing_tool_links = self.buildDir() / "user_facing_tool_links.txt"
-        if user_facing_tool_links.exists():
-            with user_facing_tool_links.open("rt") as links:
-                for line in links:
-                    line = line.strip()
-                    if not line:
-                        continue
-                    src, dest = line.split()
-                    src = Path(src).relative_to(CraftCore.standardDirs.craftRoot()).with_suffix(CraftCore.compiler.executableSuffix)
-                    if not utils.createShim(self.installDir() / dest,  self.installDir() / src):
-                        return False
-        if CraftCore.compiler.isWindows:
-            # TODO: there must be a more elegant way
-            dll = utils.filterDirectoryContent(self.installDir() / "lib/qt6/bin",
-                                                    whitelist=lambda x, root: x.name.endswith(".dll"),
-                                                    blacklist=lambda x, root: True)
-            utils.createDir(self.installDir() / "bin")
-            for d in dll:
-                src = Path(d)
-                if not utils.copyFile(src, self.installDir() / "bin" / src.name):
-                    return False
-                src = src.with_suffix(".pdb")
-                if src.exists():
-                    if not utils.copyFile(src, self.installDir() / "bin" / src.name):
-                        return False
-        return True
