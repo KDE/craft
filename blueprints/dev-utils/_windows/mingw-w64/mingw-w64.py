ec345252a4bf1e019de56378c1379628b8f57d41
Hannah von Reth <vonreth@kde.org>
2022-04-04 09:13:49 +0200

    Update mingw

--- blueprints/dev-utils/_windows/mingw-w64/mingw-w64.py
+++ blueprints/dev-utils/_windows/mingw-w64/mingw-w64.py
@@ -6,11 +6,11 @@ class subinfo(info.infoclass):
             self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NoPlatform
 
     def setTargets(self):
-        for ver, rev, rt in [("11.2.0", "3", "9")]:
+        for ver, rev, rt in [("11.2.0", "4", "9")]:
             #https://github.com/cristianadam/mingw-builds/releases/download/v11.2.0-rev1/x86_64-11.2.0-release-posix-seh-rt_v9-rev1.7z
             self.targets[f"{ver}-{rev}"] = f"https://github.com/cristianadam/mingw-builds/releases/download/v{ver}-rev{rev}/x86_64-{ver}-release-posix-seh-rt_v{rt}-rev{rev}.7z"
-        self.targetDigests["11.2.0-3"] = (['152c6d28deb7107ac67257bb21edf80f650b85567c5baf9a23d211a35dfbcc49'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "11.2.0-3"
+        self.targetDigests["11.2.0-4"] = (['01afee61056d30480c7758b043f667814b724181a21b1465da3e98d1551bfa4f'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "11.2.0-4"
 
     def setDependencies(self):
         self.buildDependencies["core/cacert"] = None
