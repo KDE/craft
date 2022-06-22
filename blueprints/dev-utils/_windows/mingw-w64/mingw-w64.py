498a823c575b2ffd1ba505093cfad9666d555818
Hannah von Reth <vonreth@kde.org>
2022-06-22 14:56:06 +0200

    Revert "mingw gcc 12.1.0"
    Perl compiled with that version crashes, so I expect more problems
    
    This reverts commit 90b8e22a09c263384651111fbc6543d8667616d5.

--- blueprints/dev-utils/_windows/mingw-w64/mingw-w64.py
+++ blueprints/dev-utils/_windows/mingw-w64/mingw-w64.py
@@ -6,12 +6,11 @@ class subinfo(info.infoclass):
             self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NoPlatform
 
     def setTargets(self):
-        for ver, rev, rt in [("11.2.0", "4", "9"), ("12.1.0", "0", "10")]:
+        for ver, rev, rt in [("11.2.0", "4", "9")]:
             #https://github.com/cristianadam/mingw-builds/releases/download/v11.2.0-rev1/x86_64-11.2.0-release-posix-seh-rt_v9-rev1.7z
             self.targets[f"{ver}-{rev}"] = f"https://github.com/cristianadam/mingw-builds/releases/download/v{ver}-rev{rev}/x86_64-{ver}-release-posix-seh-rt_v{rt}-rev{rev}.7z"
         self.targetDigests["11.2.0-4"] = (['01afee61056d30480c7758b043f667814b724181a21b1465da3e98d1551bfa4f'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["12.1.0-0"] = (['6e929c993c62819e0fc1ced58fad00fd4597ebe74b2f55d5026f06ff65c793a2'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "12.1.0-0"
+        self.defaultTarget = "11.2.0-4"
 
     def setDependencies(self):
         self.buildDependencies["core/cacert"] = None
