478b36aaeb0420674e416865b6e903a9692a3754
Hannah von Reth <vonreth@kde.org>
2022-07-06 22:48:36 +0200

    Add prebuild wget on mac

---
+++ blueprints/dev-utils/_mac/wget/wget.py
@@ -0,0 +1,30 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["1.21.2-18"]:
+            self.targets[ver] = f"https://files.kde.org/craft/prebuilt/packages/wget-{ver}-macos-64-clang.tar.7z"
+            #self.targetDigestUrls[ver] = f"https://files.kde.org/craft/prebuilt/packages/wget-{ver}-macos-64-clang.tar.7z.sha256"
+            self.targetInstallPath[ver] = "dev-utils/wget"
+        self.defaultTarget = "1.21.2-18"
+
+    def setDependencies(self):
+        self.buildDependencies["dev-utils/7zip"] = None
+        self.buildDependencies["core/cacert"] = None
+
+
+from Package.BinaryPackageBase import *
+
+
+class Package(BinaryPackageBase):
+    def __init__(self):
+        BinaryPackageBase.__init__(self)
+
+    def postInstall(self):
+        return utils.createShim(self.imageDir() / "dev-utils/bin/wget", self.imageDir() / "dev-utils/wget/bin/wget",
+         env = {"DYLD_LIBRARY_PATH": f"{CraftCore.standardDirs.craftRoot() / 'dev-utils/wget/lib'}"})
+
+    def postQmerge(self):
+        CraftCore.cache.clear()
+        return True
