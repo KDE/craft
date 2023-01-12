26698cf5365cd6f8b7cedf607571a5e2deaa7b1c
Hannah von Reth <vonreth@kde.org>
2023-01-12 10:36:22 +0100

    kshimgen v0.5.4

--- blueprints/dev-utils/kshimgen/kshimgen.py
+++ blueprints/dev-utils/kshimgen/kshimgen.py
@@ -10,15 +10,15 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://invent.kde.org/sdk/kshim.git"
 
-        for ver in ["0.5.3"]:
+        for ver in ["0.5.4"]:
             self.targets[ver] = f"https://invent.kde.org/sdk/kshim/-/archive/v{ver}/kshim-v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"kshim-v{ver}"
 
         self.targetDigests["0.5.3"] = (
-            ["7037c0d009240f1a20d6197e4d3a50195740837c3d851347b3da974742d64a39"],
+            ["f0df8b089c8464335c9599c73b83704798ff1adbe18707e7e91fc058345dbb4e"],
             CraftHash.HashAlgorithm.SHA256,
         )
-        self.defaultTarget = "0.5.3"
+        self.defaultTarget = "0.5.4"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/cmake-base"] = None
