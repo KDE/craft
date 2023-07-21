6bfadda8a7bee41388fa60de0d12c6d3a7d10f00
Hannah von Reth <vonreth@kde.org>
2023-07-21 13:26:06 +0200

    Test fix logged locations

--- bin/test/CraftTestBase.py
+++ bin/test/CraftTestBase.py
@@ -5,6 +5,7 @@ import unittest
 import CraftConfig
 import CraftStandardDirs
 import InstallDB
+from Blueprints.CraftPackageObject import CraftPackageObject
 from CraftCore import CraftCore
 from options import UserOptions
 
@@ -12,10 +13,8 @@ from options import UserOptions
 class CraftTestBase(unittest.TestCase):
     def setUp(self):
         CraftCore.debug.setVerbose(int(os.getenv("CRAFT_TEST_VERBOSITY")))
-        if "CRAFT_TEST_BLUEPRINTS_ROOT" not in os.environ:
-            blueprintsDir = CraftCore.standardDirs.blueprintRoot()
-        else:
-            blueprintsDir = os.environ["CRAFT_TEST_BLUEPRINTS_ROOT"]
+        if "CRAFT_TEST_BLUEPRINTS_ROOT" in os.environ:
+            CraftCore.settings.set("Blueprints", "BlueprintRoot", os.environ["CRAFT_TEST_BLUEPRINTS_ROOT"])
         self.kdeRoot = tempfile.TemporaryDirectory()
         CraftCore.settings = CraftConfig.CraftConfig(
             os.path.join(os.path.normpath(os.path.join(os.path.split(__file__)[0], "..", "..", "..")), "craft", "CraftSettings.ini.template")
@@ -23,7 +22,7 @@ class CraftTestBase(unittest.TestCase):
 
         CraftCore.standardDirs = CraftStandardDirs.CraftStandardDirs(self.kdeRoot.name)
         os.makedirs(CraftCore.standardDirs.etcDir())
-        CraftCore.settings.set("Blueprints", "BlueprintRoot", CraftCore.standardDirs.blueprintRoot())
+
         CraftCore.settings.set("Compile", "BuildType", "RelWithDebInfo")
         CraftCore.settings.set(
             "Blueprints",
@@ -37,7 +36,8 @@ class CraftTestBase(unittest.TestCase):
         CraftCore.installdb = InstallDB.InstallDB(dbPath)
         CraftCore.log.info(f"CraftRoot: {CraftCore.standardDirs.craftRoot()}")
         CraftCore.log.info(f"etcDir: {CraftCore.standardDirs.etcDir()}")
-        CraftCore.log.info(f"BlueprintRoot: {blueprintsDir}")
+        CraftCore.log.info(f"BlueprintRoot: {CraftCore.standardDirs.blueprintRoot()}")
+        CraftCore.log.info(f"BlueprintsRootDirectories {CraftPackageObject.rootDirectories()}")
         CraftCore.log.info(f"BlueprintSettings: {CraftCore.settings.get('Blueprints','Settings')}")
         CraftCore.log.info(f"dbPath: {dbPath}")
 
