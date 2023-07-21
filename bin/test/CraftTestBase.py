b5106249b8ab89ebef63db29a7c0ab5eec0954c8
Hannah von Reth <vonreth@kde.org>
2023-07-21 13:46:10 +0200

    Test: ensure blueprint path is valid

--- bin/test/CraftTestBase.py
+++ bin/test/CraftTestBase.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import unittest
+from pathlib import Path
 
 import CraftConfig
 import CraftStandardDirs
@@ -14,7 +15,10 @@ class CraftTestBase(unittest.TestCase):
     def setUp(self):
         CraftCore.debug.setVerbose(int(os.getenv("CRAFT_TEST_VERBOSITY")))
         if "CRAFT_TEST_BLUEPRINTS_ROOT" in os.environ:
-            CraftCore.settings.set("Blueprints", "BlueprintRoot", os.environ["CRAFT_TEST_BLUEPRINTS_ROOT"])
+            blueprintRoot = Path(os.environ["CRAFT_TEST_BLUEPRINTS_ROOT"]).resolve()
+            if not blueprintRoot.exists():
+                self.fail(f"blueprintRoot: {blueprintRoot} does not exist")
+            CraftCore.settings.set("Blueprints", "BlueprintRoot", str(blueprintRoot))
         self.kdeRoot = tempfile.TemporaryDirectory()
         CraftCore.settings = CraftConfig.CraftConfig(
             os.path.join(os.path.normpath(os.path.join(os.path.split(__file__)[0], "..", "..", "..")), "craft", "CraftSettings.ini.template")
