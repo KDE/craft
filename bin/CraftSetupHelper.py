23510b278c1858009f1e2e6e3d0adc06670a7fab
Hannah von Reth <vonreth@kde.org>
2023-09-08 13:34:09 +0200

    Fix LDFLAGS

--- bin/CraftSetupHelper.py
+++ bin/CraftSetupHelper.py
@@ -319,7 +319,7 @@ class SetupHelper(object):
 
         self.prependEnvVar("LD_LIBRARY_PATH", libraryPaths)
         if CraftCore.compiler.isLinux or CraftCore.compiler.isFreeBSD:
-            self.prependEnvVar("LDFLAGS", f"-Wl,-rpath,'$ORIGIN/../lib' -L{CraftCore.standardDirs.craftRoot() / 'lib'}", sep=" ")
+            self.prependEnvVar("LDFLAGS", ["-Wl,-rpath,'$ORIGIN/../lib'", f"-L{CraftCore.standardDirs.craftRoot() / 'lib'}"], sep=" ")
         self.prependEnvVar(
             "BISON_PKGDATADIR",
             os.path.join(CraftCore.standardDirs.craftRoot(), "share", "bison"),
@@ -335,7 +335,7 @@ class SetupHelper(object):
         # we will later replace the hard coded path in BuildSystemBase.internalPostInstall
         self.prependEnvVar(
             "LDFLAGS",
-            f"-Wl,-rpath,{CraftCore.standardDirs.craftRoot() / 'lib'} -L{CraftCore.standardDirs.craftRoot() / 'lib'}",
+            [f"-Wl,-rpath,{CraftCore.standardDirs.craftRoot() / 'lib'}", f"-L{CraftCore.standardDirs.craftRoot() / 'lib'}"],
             sep=" ",
         )
         self.prependEnvVar(
