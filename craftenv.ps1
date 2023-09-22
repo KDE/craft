df8d34d6abaeab84483580de994da635c65fa374
Hannah von Reth <vonreth@kde.org>
2023-09-22 10:02:12 +0200

    powershell parse craft env from json

--- craftenv.ps1
+++ craftenv.ps1
@@ -131,16 +131,12 @@ if($args.Length -ne 0)
 {
     craft --run @args
 } else {
-    $env2 = (& $env:CRAFT_PYTHON ([IO.PATH]::COMBINE("$env:CraftRoot", "bin", "CraftSetupHelper.py")) "--setup")
+    $env2 = ConvertFrom-Json (& $env:CRAFT_PYTHON ([IO.PATH]::COMBINE("$env:CraftRoot", "bin", "CraftSetupHelper.py")) @("--setup", "--format=json"))
     Get-ChildItem env: | ForEach-Object {
         Remove-Item ("ENV:{0}" -f ${_}.Name)
     }
-    foreach($_ in $env2) {
-        if ($_ -match "=") {
-            $v = $_.split("=", 2)
-            set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-            #Write-Host("$v[0]=$v[1]")
-        }
+    foreach ($property in $env2.PSObject.Properties) {
+        Set-Item -force -path "ENV:\$($property.Name)"  -value "$($property.Value)"
     }
     cr
 }
