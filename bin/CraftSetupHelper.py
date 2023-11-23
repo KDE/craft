c1f61c3acad6d40255f0f465efa43cca08a870e3
Hannah von Reth <vonreth@kde.org>
2023-11-23 11:05:15 +0100

    Fix circular import

--- bin/CraftSetupHelper.py
+++ bin/CraftSetupHelper.py
@@ -33,7 +33,6 @@ import subprocess
 import sys
 from pathlib import Path
 
-from Blueprints.CraftVersion import CraftVersion
 from CraftCore import CraftCore
 from CraftOS.osutils import OsUtils
 from Utils.CaseInsensitiveDict import CaseInsensitiveDict
@@ -328,7 +327,7 @@ class SetupHelper(object):
             [f"-Wl,-rpath,{CraftCore.standardDirs.craftRoot() / 'lib'}", f"-L{CraftCore.standardDirs.craftRoot() / 'lib'}"],
             sep=" ",
         )
-        if CraftVersion(CraftCore.settings.get("General", "MacDeploymentTarget", "10.15")) < "12":
+        if float(CraftCore.settings.get("General", "MacDeploymentTarget", "10.15")) < 12:
             # https://developer.apple.com/documentation/xcode-release-notes/xcode-15-release-notes
             # Binaries using symbols with a weak definition crash at runtime on iOS 14/macOS 12 or older. This impacts primarily C++ projects due to their extensive use of weak symbols. (114813650) (FB13097713)
             # Workaround: Bump the minimum deployment target to iOS 15, macOS 12, watchOS 8 or tvOS 15, or add -Wl,-ld_classic to the OTHER_LDFLAGS build setting.
