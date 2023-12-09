782b9d8eb1725354718867c0ddd8afb805af35f6
Hannah von Reth <vonreth@kde.org>
2023-12-09 14:20:04 +0100

    cmake add linux arm target

--- blueprints/dev-utils/cmake-base/cmake-base.py
+++ blueprints/dev-utils/cmake-base/cmake-base.py
@@ -17,8 +17,13 @@ class subinfo(info.infoclass):
                 self.targets[ver] = f"https://www.cmake.org/files/v{majorMinorStr}/cmake-{ver}-macos-universal.tar.gz"
                 self.targetInstSrc[ver] = f"cmake-{ver}-macos-universal"
             elif CraftCore.compiler.isLinux:
-                self.targets[ver] = f"https://cmake.org/files/v{majorMinorStr}/cmake-{ver}-linux-x86_64.tar.gz"
-                self.targetInstSrc[ver] = f"cmake-{ver}-linux-x86_64"
+                if CraftCore.compiler.architecture == CraftCore.compiler.Architecture.x86_64:
+                    self.targets[ver] = f"https://cmake.org/files/v{majorMinorStr}/cmake-{ver}-linux-x86_64.tar.gz"
+                    self.targetInstSrc[ver] = f"cmake-{ver}-linux-x86_64"
+                if CraftCore.compiler.architecture & CraftCore.compiler.Architecture.arm:
+                    self.targets[ver] = f"https://cmake.org/files/v{majorMinorStr}/cmake-{ver}-linux-aarch64.tar.gz"
+                    self.targetInstSrc[ver] = f"cmake-{ver}-linux-aarch64"
+
             self.targetInstallPath[ver] = os.path.join("dev-utils", "cmake-base")
             self.targetDigestUrls[ver] = (
                 f"https://cmake.org/files/v{majorMinorStr}/cmake-{ver}-SHA-256.txt",
