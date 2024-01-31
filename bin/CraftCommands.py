7962935f4f9ba0d16f9fbb7eaa9508cd7c0dc41d
Hannah von Reth <vonreth@kde.org>
2024-01-31 10:17:04 +0100

    Apply workaround for pinned version not known to master
    Blueprints.CraftPackageObject.BlueprintException: /github/home/craft/CraftMaster/linux-64-gcc/craft/blueprints/libs/zlib/zlib.py failed:
    You defined an invalid target 1.2.13 for libs/zlib, avaialble versions are ['1.3', '1.3.1']

--- bin/CraftCommands.py
+++ bin/CraftCommands.py
@@ -263,7 +263,15 @@ def unShelve(shelve, args):
                 continue
             opt.append(o)
         skip = False
-    if not __recurseCraft(["--options", "virtual.ignored=True", "--update"] + opt, ["craft"]):
+
+    # first lets check out the blueprints before we setup the rest
+    updateCommand = ["--update"] + opt
+    # workaround
+    # we use craft to update the blueprints, to prevent config issues with blueprints we disable them
+    # this is only a subsection of categories prone to act up
+    for category in ["virtual", "libs", "dev-utils"]:
+        updateCommand += ["--options", f"{category}.ignored=True"]
+    if not __recurseCraft(updateCommand, ["craft"]):
         return False
     return __recurseCraft(opt, list(packages.keys()))
 
