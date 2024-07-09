d6b00af4d52b406cdf35f9cb441df2a21170a0d7
Hannah von Reth <vonreth@kde.org>
2024-07-09 12:12:49 +0300

    libffi 3.4.6

--- blueprints/libs/libffi/libffi.py
+++ blueprints/libs/libffi/libffi.py
@@ -5,13 +5,14 @@ from Package.AutoToolsPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["3.3"]:
-            self.targets[ver] = f"ftp://sourceware.org/pub/libffi/libffi-{ver}.tar.gz"
+        for ver in ["3.3", "3.4.6"]:
+            self.targets[ver] = f"https://github.com/libffi/libffi/releases/download/v{ver}/libffi-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libffi-{ver}"
         self.targetDigests["3.3"] = (["72fba7922703ddfa7a028d513ac15a85c8d54c8d67f55fa5a4802885dc652056"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["3.4.6"] = (["b0dea9df23c863a7a50e825440f3ebffabd65df1497108e5d437747843895a4e"], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply["3.3"] = [("libffi-3.3-20210126.diff", 1)]
         self.patchLevel["3.3"] = 2
-        self.defaultTarget = "3.3"
+        self.defaultTarget = "3.4.6"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
