bd59218fa951739032b5897134e2e08e23ce255d
Hannah von Reth <vonreth@kde.org>
2024-09-10 17:41:28 +0200

    Build static by default on ios

--- bin/options.py
+++ bin/options.py
@@ -142,7 +142,7 @@ class UserOptions(object):
         _register("ignored", bool, persist=False, compatible=True)
         _register("buildTests", CraftCore.compiler.isNative(), persist=False, compatible=True)
         _register("buildTools", CraftCore.compiler.isNative(), persist=False, compatible=True)
-        _register("buildStatic", bool, persist=False)
+        _register("buildStatic", CraftCore.compiler.isIOS, persist=False)
 
         _register(
             "buildType",
