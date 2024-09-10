de6680cfc1d9d8ac2aa6b7eed4c214a5e81793c4
Hannah von Reth <vonreth@kde.org>
2024-09-10 15:54:02 +0200

    Env: add ios support

--- bin/CraftSetupHelper.py
+++ bin/CraftSetupHelper.py
@@ -325,26 +325,27 @@ class SetupHelper(object):
             [f"-Wl,-rpath,{CraftCore.standardDirs.craftRoot() / 'lib'}", f"-L{CraftCore.standardDirs.craftRoot() / 'lib'}"],
             sep=" ",
         )
-        if float(CraftCore.settings.get("General", "MacDeploymentTarget", "10.15")) < 12:
-            # https://developer.apple.com/documentation/xcode-release-notes/xcode-15-release-notes
-            # Binaries using symbols with a weak definition crash at runtime on iOS 14/macOS 12 or older. This impacts primarily C++ projects due to their extensive use of weak symbols. (114813650) (FB13097713)
-            # Workaround: Bump the minimum deployment target to iOS 15, macOS 12, watchOS 8 or tvOS 15, or add -Wl,-ld_classic to the OTHER_LDFLAGS build setting.
-            try:
-                # does ld support -ld_classic
-                subprocess.check_call(
-                    """echo "int main() {}" | /usr/bin/clang "-Wl,-ld_classic" -x c++ - -o /dev/null""", shell=True, stderr=subprocess.DEVNULL
-                )
-                self.prependEnvVar(
-                    "LDFLAGS",
-                    ["-Wl,-ld_classic"],
-                    sep=" ",
-                )
-            except:
-                pass
-        self.addEnvVar(
-            "MACOSX_DEPLOYMENT_TARGET",
-            CraftCore.settings.get("General", "MacDeploymentTarget", "10.15"),
-        )
+        if CraftCore.compiler.isNative():
+            if float(CraftCore.settings.get("General", "MacDeploymentTarget", "10.15")) < 12:
+                # https://developer.apple.com/documentation/xcode-release-notes/xcode-15-release-notes
+                # Binaries using symbols with a weak definition crash at runtime on iOS 14/macOS 12 or older. This impacts primarily C++ projects due to their extensive use of weak symbols. (114813650) (FB13097713)
+                # Workaround: Bump the minimum deployment target to iOS 15, macOS 12, watchOS 8 or tvOS 15, or add -Wl,-ld_classic to the OTHER_LDFLAGS build setting.
+                try:
+                    # does ld support -ld_classic
+                    subprocess.check_call(
+                        """echo "int main() {}" | /usr/bin/clang "-Wl,-ld_classic" -x c++ - -o /dev/null""", shell=True, stderr=subprocess.DEVNULL
+                    )
+                    self.prependEnvVar(
+                        "LDFLAGS",
+                        ["-Wl,-ld_classic"],
+                        sep=" ",
+                    )
+                except:
+                    pass
+            self.addEnvVar(
+                "MACOSX_DEPLOYMENT_TARGET",
+                CraftCore.settings.get("General", "MacDeploymentTarget", "10.15"),
+            )
         self.prependEnvVar(
             "BISON_PKGDATADIR",
             os.path.join(CraftCore.standardDirs.craftRoot(), "share", "bison"),
@@ -412,9 +413,9 @@ class SetupHelper(object):
                 ),
             )
 
-        if OsUtils.isWin():
+        if CraftCore.compiler.isWindows:
             self._setupWin()
-        elif OsUtils.isMac():
+        elif CraftCore.compiler.platform & (CraftCore.compiler.Platforms.MacOS | CraftCore.compiler.Platforms.iOS):
             self._setupMac()
         elif CraftCore.compiler.isAndroid:
             self._setupAndroid()
