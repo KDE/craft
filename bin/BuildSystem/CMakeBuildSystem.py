73daeb6330a62f5cfacf38164e190d50057ea5f0
Hannah von Reth <vonreth@kde.org>
2024-09-16 11:25:13 +0200

    Fix platform.isNative vs architecture.isNative

--- bin/BuildSystem/CMakeBuildSystem.py
+++ bin/BuildSystem/CMakeBuildSystem.py
@@ -57,9 +57,6 @@ class CMakeBuildSystem(BuildSystemBase):
         if self.buildType() is not None:
             options.append(f"-DCMAKE_BUILD_TYPE={self.buildType()}")
 
-        # if CraftCore.compiler.isGCC() and not CraftCore.compiler.platform.isNative:
-        #    options += " -DCMAKE_TOOLCHAIN_FILE=%s" % os.path.join(CraftStandardDirs.craftRoot(), "craft", "bin", "toolchains", "Toolchain-cross-mingw32-linux-%s.cmake" % CraftCore.compiler.architecture)
-
         if CraftCore.settings.getboolean("CMake", "KDE_L10N_AUTO_TRANSLATIONS", False):
             options.append("-DKDE_L10N_AUTO_TRANSLATIONS=ON")
             options.append("-DKDE_L10N_SYNC_TRANSLATIONS=ON")
