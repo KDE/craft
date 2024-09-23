9220c26aefaf0a4e1934eb64f50f8dc7694ffdcf
Hannah von Reth <vonreth@kde.org>
2024-09-23 09:36:10 +0200

    Fix isActive if modifiers are involved

--- blueprints/dev-utils/_unix/automake/automake.py
+++ blueprints/dev-utils/_unix/automake/automake.py
@@ -35,7 +35,6 @@ class subinfo(info.infoclass):
 class Package(AutoToolsPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        print(repr(self.package.categoryInfo.platforms), type(self.package.categoryInfo.platforms))
         self.subinfo.options.configure.autoreconf = False
         self.subinfo.options.configure.args += ["--disable-static", "--enable-shared"]
 
