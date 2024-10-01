3a99281358111e2fe8a8247de24f8cada42f8c13
Thomas Müller <thomas.mueller@tmit.eu>
2024-10-01 16:19:56 +0200

    feat: openssl 3.3.2

--- blueprints/libs/openssl/openssl.py
+++ blueprints/libs/openssl/openssl.py
@@ -45,7 +45,7 @@ class subinfo(info.infoclass):
             self.targetInstSrc[ver] = f"openssl-{ver}"
             self.targetDigestUrls[ver] = ([f"https://openssl.org/source/openssl-{ver}.tar.gz.sha256"], CraftHash.HashAlgorithm.SHA256)
 
-        for ver in ["3.1.1", "3.1.2", "3.1.3", "3.1.4", "3.2.0", "3.2.1", "3.3.1"]:
+        for ver in ["3.1.1", "3.1.2", "3.1.3", "3.1.4", "3.2.0", "3.2.1", "3.3.1", "3.3.2"]:
             self.targets[ver] = f"https://openssl.org/source/openssl-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"openssl-{ver}"
             self.targetDigestUrls[ver] = ([f"https://openssl.org/source/openssl-{ver}.tar.gz.sha256"], CraftHash.HashAlgorithm.SHA256)
