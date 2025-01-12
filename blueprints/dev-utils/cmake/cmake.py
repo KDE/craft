c3d1ca064ed24cf9b9050754a00eaccbbbd55554
Julius Künzel <julius.kuenzel@kde.org>
2025-01-12 21:19:06 +0100

    Fixes related to wrong merge (now correct)

--- blueprints/dev-utils/cmake/cmake.py
+++ blueprints/dev-utils/cmake/cmake.py
@@ -28,7 +28,7 @@ class Package(BinaryPackageBase):
         if not super().install():
             return False
         cmakePath = CraftCore.standardDirs.craftRoot() / "dev-utils/cmake-base"
-        if CraftCore.compiler.platform.isMacOS:
+        if CraftCore.compiler.isMacOS:
             cmakePath /= "CMake.app/Contents/bin"
         else:
             cmakePath /= "bin"
