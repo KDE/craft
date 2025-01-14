c9d93401fe4c0ab66d5398b7c478de36d56d171b
Hannah von Reth <vonreth@kde.org>
2025-01-14 13:50:44 +0100

    Sync MacDeploymentTarget with https://github.com/qt/qtbase/blob/6.8/.cmake.conf#L54C5-L54C35

--- bin/CraftSetupHelper.py
+++ bin/CraftSetupHelper.py
@@ -324,7 +324,7 @@ class SetupHelper(object):
             [f"-Wl,-rpath,{CraftCore.standardDirs.craftRoot() / 'lib'}", f"-L{CraftCore.standardDirs.craftRoot() / 'lib'}"],
             sep=" ",
         )
-        if float(CraftCore.settings.get("General", "MacDeploymentTarget", "10.15")) < 12:
+        if CraftCore.compiler.macOSDeploymentTarget < 12:
             # https://developer.apple.com/documentation/xcode-release-notes/xcode-15-release-notes
             # Binaries using symbols with a weak definition crash at runtime on iOS 14/macOS 12 or older. This impacts primarily C++ projects due to their extensive use of weak symbols. (114813650) (FB13097713)
             # Workaround: Bump the minimum deployment target to iOS 15, macOS 12, watchOS 8 or tvOS 15, or add -Wl,-ld_classic to the OTHER_LDFLAGS build setting.
@@ -342,7 +342,7 @@ class SetupHelper(object):
                 pass
         self.addEnvVar(
             "MACOSX_DEPLOYMENT_TARGET",
-            CraftCore.settings.get("General", "MacDeploymentTarget", "10.15"),
+            str(CraftCore.compiler.macOSDeploymentTarget),
         )
         self.prependEnvVar(
             "BISON_PKGDATADIR",
