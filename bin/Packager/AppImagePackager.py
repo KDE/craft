3ea26f9bbada02de8cea2d301677338317ac65af
Julius Künzel <julius.kuenzel@kde.org>
2025-01-19 11:35:33 +0100

    [AppImagePackager] Fix import

--- bin/Packager/AppImagePackager.py
+++ bin/Packager/AppImagePackager.py
@@ -2,7 +2,7 @@ import glob
 import os
 from pathlib import Path
 
-import CraftCore
+from CraftCore import CraftCore
 import utils
 from CraftBase import InitGuard
 from CraftOS.osutils import OsUtils
