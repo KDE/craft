a78202b5118f27e9ee04996906ab5122624d761c
Hannah von Reth <vonreth@kde.org>
2025-01-21 15:16:43 +0100

    Add mingw 14.2

--- blueprints/dev-utils/_windows/mingw-w64/mingw-w64.py
+++ blueprints/dev-utils/_windows/mingw-w64/mingw-w64.py
@@ -10,13 +10,17 @@ class subinfo(info.infoclass):
             self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NoPlatform
 
     def setTargets(self):
-        for ver, rev, rt in [("13.2.0", "1", "11")]:
+        for ver, rev, rt in [("13.2.0", "1", "11"), ("14.2.0", "1", "12")]:
             # https://github.com/niXman/mingw-builds-binaries/releases/download/13.2.0-rt_v11-rev1/x86_64-13.2.0-release-posix-seh-ucrt-rt_v11-rev1.7z
             self.targets[
                 f"{ver}-{rev}"
             ] = f"https://github.com/niXman/mingw-builds-binaries/releases/download/{ver}-rt_v{rt}-rev{rev}/x86_64-{ver}-release-posix-seh-ucrt-rt_v{rt}-rev{rev}.7z"
         self.targetDigests["13.2.0-1"] = (
             ["475ee72c5ce1bd54a3e3c334bdd3be5e6575334184fd9718013aa362c9819d2f"],
+            CraftHash.HashAlgorithm.SHA256,
+        )
+        self.targetDigests["14.2.0-1"] = (
+            ["9fa5768ba0e628203c4631d447ce533335cdd1fd9c318d84c774e729efa4edad"],
             CraftHash.HashAlgorithm.SHA256,
         )
         self.defaultTarget = "13.2.0-1"
