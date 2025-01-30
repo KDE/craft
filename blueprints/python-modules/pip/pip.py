f8b30c039ffd5d33ae5ff3398d1fb8435924727a
Julius Künzel <julius.kuenzel@kde.org>
2025-01-30 21:50:55 +0100

    [pip] Fix install on Windows

--- blueprints/python-modules/pip/pip.py
+++ blueprints/python-modules/pip/pip.py
@@ -25,7 +25,31 @@ class Package(PipPackageBase):
             # if its installed we get the help text if not we get an empty string
             with io.StringIO() as tmp:
                 utils.system([python, "-m", "pip"], stdout=tmp, stderr=subprocess.DEVNULL)
-                if not tmp.getvalue():
-                    if not utils.system([python, "-m", "ensurepip"]):
-                        return False
+                if tmp.getvalue():
+                    return True
+
+                if not utils.system([python, "-m", "ensurepip"]):
+                    return False
         return True
+
+    def install(self):
+        if not CraftCore.compiler.isWindows:
+            return super.install()
+
+        # Run "pip install pip" to install the lateste version
+        # We can't use the install step from PipBuildSystem,
+        # because installing pip itself causes issues when --prefix is used
+        # See https://github.com/pypa/pip/issues/11349
+        # TODO: re-evaluate when we use a newer python in libs/python
+        command = [
+            python,
+            "-m",
+            "pip",
+            "install",
+            "--upgrade",
+            "--upgrade-strategy",
+            "only-if-needed",
+            "pip"
+        ]
+
+        return utils.system(command)
