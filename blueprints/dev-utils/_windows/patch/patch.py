728e7478afe24d3cd709f2ac4c017a459efb3cf8
Hannah von Reth <vonreth@kde.org>
2025-02-15 13:02:50 +0100

    Windows: Replace some outdated utils with tools shipped with git

--- blueprints/dev-utils/_windows/patch/patch.py
+++ blueprints/dev-utils/_windows/patch/patch.py
@@ -1,39 +1,36 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2025 Hannah von Reth <vonreth@kde.org>
+
 import info
 import utils
 from CraftCore import CraftCore
 from Package.BinaryPackageBase import BinaryPackageBase
-from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets["2.5.9"] = "http://downloads.sourceforge.net/sourceforge/gnuwin32/patch-2.5.9-7-bin.zip"
-        self.targets["2.7.6"] = "https://files.kde.org/craft/3rdparty/patch/msys-patch-2.7.6.7z"
-        self.targetInstallPath["2.5.9"] = "dev-utils"
-        self.targetInstallPath["2.7.6"] = "dev-utils/patch"
-        self.targetDigests["2.5.9"] = "7b2ec738881f4e962e54e0f330b67c42635266b7"
-        self.targetDigests["2.7.6"] = (
-            ["2a8ed85190fab42861d64cdfd65015386ea82004e64df04b03eb9640429f9bcf"],
-            CraftHash.HashAlgorithm.SHA256,
-        )
-        self.defaultTarget = "2.7.6"
+        self.targets["latest"] = ""
+        self.description = "Craft integration package for patch."
+        self.defaultTarget = "latest"
 
     def setDependencies(self):
-        if CraftCore.compiler.isMinGW():
-            self.runtimeDependencies["dev-utils/uactools"] = None
-        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["dev-utils/kshimgen"] = None
+        self.buildDependencies["dev-utils/git"] = None
 
 
 class Package(BinaryPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.subinfo.shelveAble = False
 
-    def install(self):
-        if not BinaryPackageBase.install(self):
+    def postInstall(self):
+        gitPath = CraftCore.cache.findApplication("git")
+        if not gitPath:
             return False
-        if self.buildTarget == "2.5.9":
-            manifest = self.blueprintDir() / "patch.exe.manifest"
-            patch = self.installDir() / "bin/patch.exe"
-            return utils.embedManifest(patch, manifest)
-        else:
-            return utils.createShim(self.imageDir() / "bin/patch.exe", self.installDir() / "patch.exe")
+        gitPath = gitPath.parent
+
+        return utils.createShim(
+            self.imageDir() / "dev-utils/bin/patch",
+            gitPath.parent / "usr/bin/patch.exe",
+            useAbsolutePath=True,
+        )
--- blueprints/dev-utils/_windows/sed/sed.py
+++ blueprints/dev-utils/_windows/sed/sed.py
@@ -1,30 +1,36 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2025 Hannah von Reth <vonreth@kde.org>
+
 import info
+import utils
+from CraftCore import CraftCore
 from Package.BinaryPackageBase import BinaryPackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["4.1.5", "4.2.1"]:
-            self.targets[ver] = [
-                f"http://downloads.sourceforge.net/sourceforge/gnuwin32/sed-{ver}-bin.zip",
-                f"http://downloads.sourceforge.net/sourceforge/gnuwin32/sed-{ver}-dep.zip",
-            ]
-            self.targetInstallPath[ver] = "dev-utils"
-        self.targetDigests["4.1.5"] = [
-            "abe52d0be25f1bb44b8a8e7a94e7afa9c15b3ae5",
-            "736678616305fab80b4ec1a639d5ff0170183310",
-        ]
-        self.targetDigests["4.2.1"] = [
-            "dfd3d1dae27a24784d7ab40eb074196509fa48fe",
-            "f7edbd7152d8720c95d46dd128b87b8ba48a5d6f",
-        ]
-        self.defaultTarget = "4.2.1"
+        self.targets["latest"] = ""
+        self.description = "Craft integration package for sed."
+        self.defaultTarget = "latest"
 
     def setDependencies(self):
-        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["dev-utils/kshimgen"] = None
+        self.buildDependencies["dev-utils/git"] = None
 
 
 class Package(BinaryPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.subinfo.shelveAble = False
+
+    def postInstall(self):
+        gitPath = CraftCore.cache.findApplication("git")
+        if not gitPath:
+            return False
+        gitPath = gitPath.parent
+
+        return utils.createShim(
+            self.imageDir() / "dev-utils/bin/sed",
+            gitPath.parent / "usr/bin/sed.exe",
+            useAbsolutePath=True,
+        )
