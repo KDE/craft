77db4a1011d6977542d09ca820de1dbb47b5d27b
Hannah von Reth <vonreth@kde.org>
2025-02-22 17:09:38 +0100

    Add deps

--- blueprints/python-modules/flit-core/flit-core.py
+++ blueprints/python-modules/flit-core/flit-core.py
@@ -1,13 +1,14 @@
 # SPDX-License-Identifier: BSD-2-Clause
-# SPDX-FileCopyrightText: 2025 Julius Künzel <julius.kuenzel@kde.org>
+# SPDX-FileCopyrightText: 2025 Hannah von Reth <vonreth@kde.org>
 import info
 from Package.PipPackageBase import PipPackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets["master"] = ""
-        self.defaultTarget = "master"
+        self.svnTargets["latest"] = ""
+        self.defaultTarget = "latest"
+        self.description = "Distribution-building parts of Flit. See flit package for more information"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
---
+++ blueprints/python-modules/pathspec/pathspec.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2025 Hannah von Reth <vonreth@kde.org>
+import info
+from Package.PipPackageBase import PipPackageBase
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["latest"] = ""
+        self.defaultTarget = "latest"
+        self.description = "Distribution-building parts of Flit. See flit package for more information"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["python-modules/pip"] = None
+        self.buildDependencies["python-modules/setuptools"] = None
+        self.runtimeDependencies["libs/python"] = None
+        self.buildDependencies["dev-utils/system-python3"] = None
+
+
+class Package(PipPackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
