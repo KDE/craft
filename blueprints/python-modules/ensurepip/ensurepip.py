33a73d4f02c9830a00f958ae392c187952c00d55
Hannah von Reth <vonreth@kde.org>
2025-02-22 17:41:10 +0100

    Split ensurepip and pip

---
+++ blueprints/python-modules/ensurepip/ensurepip.py
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2025 Hannah von Reth <vonreth@kde.org>
+import io
+import subprocess
+
+import info
+import utils
+from Package.PipPackageBase import PipPackageBase
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["master"] = ""
+        self.defaultTarget = "master"
+
+    def setDependencies(self):
+        self.buildDependencies["core/cacert"] = None
+        self.buildDependencies["python-modules/virtualenv"] = None
+
+
+class Package(PipPackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def make(self):
+        for ver, python in self._pythons:
+            # if its installed we get the help text if not we get an empty string
+            with io.StringIO() as tmp:
+                utils.system([python, "-m", "pip"], stdout=tmp, stderr=subprocess.DEVNULL)
+                if tmp.getvalue():
+                    return True
+
+                if not utils.system([python, "-m", "ensurepip"]):
+                    return False
+        return True
+
+    def install(self):
+        return True
