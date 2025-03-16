8517c72d11a67827f12f852fb269c3748757acff
Hannah von Reth <vonreth@kde.org>
2025-03-16 18:28:20 +0100

    Fix linter

--- blueprints/python-modules/pip-system/pip-system.py
+++ blueprints/python-modules/pip-system/pip-system.py
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: BSD-2-Clause
 # SPDX-FileCopyrightText: 2025 Hannah von Reth <vonreth@kde.org>
-import info
-from Package.PipPackageBase import PipPackageBase
 from Blueprints.CraftPackageObject import CraftPackageObject
+from Package.PipPackageBase import PipPackageBase
 
 pipPackage = CraftPackageObject.get("python-modules/pip")
 subinfo = pipPackage.subinfo.__class__
+
 
 class Package(PipPackageBase):
     def __init__(self, **kwargs):
