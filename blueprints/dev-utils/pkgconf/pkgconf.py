767a55a80ad24ea1ce6e45d8f0d4695d590f39a2
Hannah von Reth <vonreth@kde.org>
2025-03-29 14:36:11 +0100

    pkgconf 2.4.3, fix windows path

--- blueprints/dev-utils/pkgconf/pkgconf.py
+++ blueprints/dev-utils/pkgconf/pkgconf.py
@@ -16,11 +16,12 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.description = "package compiler and linker metadata toolkit"
-        for ver in ["2.3.0"]:
+        for ver in ["2.3.0", "2.4.3"]:
             self.targets[ver] = f"https://distfiles.ariadne.space/pkgconf/pkgconf-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"pkgconf-{ver}"
         self.targetDigests["2.3.0"] = (["3a9080ac51d03615e7c1910a0a2a8df08424892b5f13b0628a204d3fcce0ea8b"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "2.3.0"
+        self.targetDigests["2.4.3"] = (["51203d99ed573fa7344bf07ca626f10c7cc094e0846ac4aa0023bd0c83c25a41"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "2.4.3"
 
 
 class Package(AutoToolsPackageBase):
@@ -32,8 +33,10 @@ class Package(AutoToolsPackageBase):
         # the trailing slash is important
         # else pkgconf skipps the prefix
         self.subinfo.options.configure.args += [
-            f"--with-system-includedir={CraftCore.standardDirs.craftRoot()}/include/",
-            f"--with-system-libdir={CraftCore.standardDirs.craftRoot()}/lib/",
+            f"--with-system-includedir={CraftCore.standardDirs.craftRoot().as_posix()}/include/",
+            f"--with-system-libdir={CraftCore.standardDirs.craftRoot().as_posix()}/lib/",
+            f"--with-pkg-config-dir={CraftCore.standardDirs.craftRoot().as_posix()}/lib/pkgconfig/",
+            f"--with-personality-dir={CraftCore.standardDirs.locations.data.as_posix()}/pkgconfig/personality.d/",
         ]
         if CraftCore.compiler.isMSVC():
             self.subinfo.options.configure.args += ["LIBS=-lAdvapi32"]
