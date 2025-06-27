5f9c0cf2f07787ee99dc4bf5dc76124cc544d02e
Hannah von Reth <h.vonreth@opencloud.eu>
2025-06-27 11:30:48 +0200

    ninja 1.13.0

--- blueprints/dev-utils/ninja/ninja.py
+++ blueprints/dev-utils/ninja/ninja.py
@@ -17,7 +17,7 @@ class subinfo(info.infoclass):
         """ """
         self.svnTargets["master"] = "https://github.com/martine/ninja.git"
 
-        for ver in ["1.10.0", "1.10.2", "1.11.0", "1.11.1", "1.12.0", "1.12.1"]:
+        for ver in ["1.10.0", "1.10.2", "1.11.0", "1.11.1", "1.12.0", "1.12.1", "1.13.0"]:
             self.targets[ver] = f"https://github.com/ninja-build/ninja/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"ninja-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"ninja-{ver}"
@@ -41,13 +41,14 @@ class subinfo(info.infoclass):
             ["8b2c86cd483dc7fcb7975c5ec7329135d210099a89bc7db0590a07b0bbfe49a5"],
             CraftHash.HashAlgorithm.SHA256,
         )
+        self.targetDigests["1.13.0"] = (["f08641d00099a9e40d44ec0146f841c472ae58b7e6dd517bee3945cfd923cedf"], CraftHash.HashAlgorithm.SHA256)
 
         self.patchToApply["1.10.0"] = [("34d1bf2f1dcc138f7cb3a54daf771931cd799785.patch", 1)]
         self.patchLevel["1.10.0"] = 1
         self.patchLevel["1.11.1"] = 3
         self.patchLevel["1.12.1"] = 1
 
-        self.defaultTarget = "1.12.1"
+        self.defaultTarget = "1.13.0"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/mingw-w64"] = None
