62325d3f1bdbb5232812da2f976147afc4fff390
Hannah von Reth <h.vonreth@opencloud.eu>
2025-09-11 10:48:48 +0200

    Update ninja to 1.13.1 to fix a regression affecting clazy

--- blueprints/dev-utils/ninja/ninja.py
+++ blueprints/dev-utils/ninja/ninja.py
@@ -17,7 +17,7 @@ class subinfo(info.infoclass):
         """ """
         self.svnTargets["master"] = "https://github.com/martine/ninja.git"
 
-        for ver in ["1.10.0", "1.10.2", "1.11.0", "1.11.1", "1.12.0", "1.12.1", "1.13.0"]:
+        for ver in ["1.10.0", "1.10.2", "1.11.0", "1.11.1", "1.12.0", "1.12.1", "1.13.0", "1.13.1"]:
             self.targets[ver] = f"https://github.com/ninja-build/ninja/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"ninja-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"ninja-{ver}"
@@ -42,13 +42,14 @@ class subinfo(info.infoclass):
             CraftHash.HashAlgorithm.SHA256,
         )
         self.targetDigests["1.13.0"] = (["f08641d00099a9e40d44ec0146f841c472ae58b7e6dd517bee3945cfd923cedf"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.13.1"] = (["f0055ad0369bf2e372955ba55128d000cfcc21777057806015b45e4accbebf23"], CraftHash.HashAlgorithm.SHA256)
 
         self.patchToApply["1.10.0"] = [("34d1bf2f1dcc138f7cb3a54daf771931cd799785.patch", 1)]
         self.patchLevel["1.10.0"] = 1
         self.patchLevel["1.11.1"] = 3
         self.patchLevel["1.12.1"] = 1
 
-        self.defaultTarget = "1.13.0"
+        self.defaultTarget = "1.13.1"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/mingw-w64"] = None
