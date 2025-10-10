2cb37e87004ff6a5e8e81fabbe8f8839768ab0d6
Hannah von Reth <vonreth@kde.org>
2025-10-10 13:50:26 +0200

    win-iconv 0.0.10

--- blueprints/libs/_windows/iconv/iconv.py
+++ blueprints/libs/_windows/iconv/iconv.py
@@ -6,16 +6,18 @@ from Utils import CraftHash
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/win-iconv/win-iconv.git"
-        for ver in ["0.0.7", "0.0.8"]:
+        for ver in ["0.0.7", "0.0.8", "0.0.10"]:
             self.targets[ver] = f"https://github.com/win-iconv/win-iconv/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"win-iconv-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"win-iconv-{ver}"
         self.patchToApply["0.0.8"] = [("iconv-0.0.8-20250413.diff", 1)]
+        self.patchToApply["0.0.10"] = [("iconv-0.0.8-20250413.diff", 1)]
 
         self.targetDigests["0.0.8"] = (["23adea990a8303c6e69e32a64a30171efcb1b73824a1c2da1bbf576b0ae7c520"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.0.10"] = (["58493387c7c9c70d61e711ec2feec5db0a59d164556642d2b427dde4ef756bc1"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "a character set conversion library binary compatible with GNU iconv"
-        self.defaultTarget = "0.0.8"
+        self.defaultTarget = "0.0.10"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
