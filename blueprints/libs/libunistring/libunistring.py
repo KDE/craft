5d4e1b675e23f8f13fc900f0ff2cec2a21c26597
Hannah von Reth <vonreth@kde.org>
2025-10-10 12:28:47 +0200

    libunistring 1.4.1
    Fixes: #36

--- blueprints/libs/libunistring/libunistring.py
+++ blueprints/libs/libunistring/libunistring.py
@@ -8,15 +8,18 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["0.9.10", "1.2", "1.3"]:
+        for ver in ["0.9.10", "1.2", "1.3", "1.4.1"]:
             self.targets[ver] = f"https://ftp.gnu.org/gnu/libunistring/libunistring-{ver}.tar.xz"
             self.archiveNames[ver] = f"libunistring-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"libunistring-{ver}"
         self.targetDigests["0.9.10"] = (["eb8fb2c3e4b6e2d336608377050892b54c3c983b646c561836550863003c05d7"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.2"] = (["632bd65ed74a881ca8a0309a1001c428bd1cbd5cd7ddbf8cedcd2e65f4dcdc44"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.3"] = (["f245786c831d25150f3dfb4317cda1acc5e3f79a5da4ad073ddca58886569527"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.4.1"] = (["67d88430892527861903788868c77802a217b0959990f7449f2976126a307763"], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = "1.3"
+        self.defaultTarget = "1.4.1"
+
+        self.webpage = "https://www.gnu.org/software/libunistring/"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/automake"] = None
