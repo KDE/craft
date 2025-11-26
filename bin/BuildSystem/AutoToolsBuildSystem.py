a617f024508772aa2e8bac9233b2f5c84a87c31a
Hannah von Reth <vonreth@kde.org>
2025-11-26 19:54:03 +0000

    Revert "always set aclocal include paths"
    This reverts commit 48ac59cbf41805f5feaa9a9d022a6417c2bcb696

--- bin/BuildSystem/AutoToolsBuildSystem.py
+++ bin/BuildSystem/AutoToolsBuildSystem.py
@@ -105,7 +105,10 @@ class AutoToolsBuildSystem(BuildSystemBase):
                 includesArgs = Arguments()
                 if self.subinfo.options.configure.useDefaultAutoreconfIncludes:
                     includes = []
-                    dataDirs = [CraftCore.standardDirs.craftRoot() / "dev-utils/cmake/share", CraftCore.standardDirs.locations.data]
+                    dataDirs = [CraftCore.standardDirs.craftRoot() / "dev-utils/cmake/share"]
+                    if CraftCore.compiler.isWindows:
+                        # on Windows data location lies outside of the autotools prefix (msys)
+                        dataDirs.append(CraftCore.standardDirs.locations.data)
                     for i in dataDirs:
                         aclocalDir = i / "aclocal"
                         if aclocalDir.is_dir():
