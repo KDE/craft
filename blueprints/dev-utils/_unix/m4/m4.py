5e76fd1cbc80ede109ee3eb99d7bc0507d2959ae
Julius Künzel <julius.kuenzel@kde.org>
2026-06-15 13:35:53 +0200

    [m4] Update to 1.4.21

--- blueprints/dev-utils/_unix/m4/m4.py
+++ blueprints/dev-utils/_unix/m4/m4.py
@@ -15,14 +15,10 @@ class subinfo(info.infoclass):
         self.webpage = "https://www.gnu.org/software/m4/"
         self.releaseManagerId = 1871
 
-        for ver in ["1.4.18", "1.4.19", "1.4.20"]:
+        for ver in ["1.4.19", "1.4.20", "1.4.21"]:
             self.targets[ver] = f"https://ftp.gnu.org/gnu/m4/m4-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"m4-{ver}"
 
-        self.targetDigests["1.4.18"] = (
-            ["f2c1e86ca0a404ff281631bdc8377638992744b175afb806e25871a24a934e07"],
-            CraftHash.HashAlgorithm.SHA256,
-        )
         self.targetDigests["1.4.19"] = (
             ["63aede5c6d33b6d9b13511cd0be2cac046f2e70fd0a07aa9573a04a82783af96"],
             CraftHash.HashAlgorithm.SHA256,
@@ -31,18 +27,13 @@ class subinfo(info.infoclass):
             ["e236ea3a1ccf5f6c270b1c4bb60726f371fa49459a8eaaebc90b216b328daf2b"],
             CraftHash.HashAlgorithm.SHA256,
         )
+        self.targetDigests["1.4.21"] = (
+            ["f25c6ab51548a73a75558742fb031e0625d6485fe5f9155949d6486a2408ab66"],
+            CraftHash.HashAlgorithm.SHA256,
+        )
         self.description = "GNU M4 is an implementation of the traditional Unix macro processor."
         self.releaseManagerId = 1871
-        self.defaultTarget = "1.4.20"
-
-        self.patchToApply["1.4.18"] = [
-            ("m4-1.4.18-20190506.diff", 1),
-            (
-                "m4-1.4.18-glibc-change-work-around.patch",
-                1,
-            ),  # http://git.openembedded.org/openembedded-core/plain/meta/recipes-devtools/m4/m4/m4-1.4.18-glibc-change-work-around.patch
-        ]
-        self.patchLevel["1.4.18"] = 2
+        self.defaultTarget = "1.4.21"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/7zip"] = None
