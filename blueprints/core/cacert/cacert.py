3216d7e7c4a777885931cbc22c2eca67b5c3be22
Julius Künzel <julius.kuenzel@kde.org>
2026-06-15 18:36:27 +0200

    [cacert] Update to 2026-05-14

--- blueprints/core/cacert/cacert.py
+++ blueprints/core/cacert/cacert.py
@@ -6,23 +6,15 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2023-01-10", "2023-12-12", "2025-09-09"]:
+        for ver in ["2025-09-09", "2026-05-14"]:
             self.targets[ver] = f"https://files.kde.org/craft/curl.haxx.se/cacert-{ver}.zip"
             self.targetInstallPath[ver] = "etc"
-        self.targetDigests["2023-01-10"] = (
-            ["d31764d1cf86e457199e820d5f0880933e6b0058afa7b5db19f8a526a6634a18"],
-            CraftHash.HashAlgorithm.SHA256,
-        )
-        self.targetDigests["2023-12-12"] = (
-            ["e7d3215c9e1273056e7beea4f84d2c7b22b22ff0b15c5c88ce2e3f857e6a45ea"],
-            CraftHash.HashAlgorithm.SHA256,
-        )
         self.targetDigests["2025-09-09"] = (
-            ["f976dfe287484e786c899e2f009efc2ba290eb1cdb204a537ca89aa5a16700a7"],
+            ["d1cdec8c94242b239123d2071fe5c1bf3d02b3c8c0c11ab7a994d9ae6281ff8e"],
             CraftHash.HashAlgorithm.SHA256,
         )
         self.webpage = "https://curl.haxx.se/docs/caextract.html"
-        self.defaultTarget = "2025-09-09"
+        self.defaultTarget = "2026-05-14"
 
 
 class Package(BinaryPackageBase):
