f772bed4d21780babdba9062091705c720050b6b
Julius Künzel <julius.kuenzel@kde.org>
2026-06-15 13:24:13 +0200

    [automake] Update to 1.18.1

--- blueprints/dev-utils/_unix/automake/automake.py
+++ blueprints/dev-utils/_unix/automake/automake.py
@@ -15,7 +15,7 @@ class subinfo(info.infoclass):
         self.webpage = "http://www.gnu.org/software/automake/"
         self.releaseManagerId = 144
 
-        for ver in ["1.16.1", "1.16.3", "1.16.5"]:
+        for ver in ["1.16.1", "1.16.3", "1.16.5", "1.18.1"]:
             self.targets[ver] = f"https://ftp.gnu.org/gnu/automake/automake-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"automake-{ver}"
 
@@ -31,8 +31,12 @@ class subinfo(info.infoclass):
             ["f01d58cd6d9d77fbdca9eb4bbd5ead1988228fdb73d6f7a201f5f8d6b118b469"],
             CraftHash.HashAlgorithm.SHA256,
         )
+        self.targetDigests["1.18.1"] = (
+            ["168aa363278351b89af56684448f525a5bce5079d0b6842bd910fdd3f1646887"],
+            CraftHash.HashAlgorithm.SHA256,
+        )
         self.patchLevel["1.16.3"] = 1
-        self.defaultTarget = "1.16.5"
+        self.defaultTarget = "1.18.1"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
@@ -46,13 +50,15 @@ class Package(AutoToolsPackageBase):
         self.subinfo.options.configure.autoreconf = False
 
     def postInstall(self):
+        major, minor, patch = self.subinfo.buildTarget.split(".")
+
         return self.patchInstallPrefix(
             [
                 self.installDir() / "bin/aclocal",
-                self.installDir() / "bin/aclocal-1.16",
+                self.installDir() / f"bin/aclocal-{major}.{minor}",
                 self.installDir() / "bin/automake",
-                self.installDir() / "bin/automake-1.16",
-                self.installDir() / "share/automake-1.16/Automake/Config.pm",
+                self.installDir() / f"bin/automake-{major}.{minor}",
+                self.installDir() / f"share/automake-{major}.{minor}/Automake/Config.pm",
             ],
             self.subinfo.buildPrefix,
             CraftCore.standardDirs.craftRoot(),
