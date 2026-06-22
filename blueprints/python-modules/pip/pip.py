45951ca24b2480c5dbaeb97b1f3a1eec61d28087
Hannah von Reth <vonreth@kde.org>
2026-06-22 13:40:06 +0200

    Update pip to 26.1.2
    25.0.1 now fails with an ssl error

--- blueprints/python-modules/pip/pip.py
+++ blueprints/python-modules/pip/pip.py
@@ -7,10 +7,12 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets["25.0.1"] = "https://bootstrap.pypa.io/pip/zipapp/pip-25.0.1.pyz"
+        for ver in ["25.0.1", "26.1.2"]:
+            self.targets[ver] = f"https://bootstrap.pypa.io/pip/zipapp/pip-{ver}.pyz"
         self.targetDigests["25.0.1"] = (["0a7353fc4c345a9589c1cff7b59eb1868079d3de5c2663846bcb4290a69e3b41"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["26.1.2"] = (["6ddc3444b803a48d83ccf1c4ad846717b42c8ffc9d74713a53ae829a97201365"], CraftHash.HashAlgorithm.SHA256)
         self.patchLevel["25.0.1"] = 1
-        self.defaultTarget = "25.0.1"
+        self.defaultTarget = "26.1.2"
 
     def setDependencies(self):
         self.buildDependencies["core/cacert"] = None
